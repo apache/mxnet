b2bc69f988314fee2a2b02bf1ab501853191969c
Yutian Li <hotpxless@gmail.com>
2015-08-10 23:23:30 +0800

    [storage] use old function

--- src/storage/cpu_storage.cc
+++ src/storage/cpu_storage.cc
@@ -1,16 +1,15 @@
 /*!
  * Copyright (c) 2015 by Contributors
  */
-#define _ISOC11_SOURCE
 #include "./cpu_storage.h"
 #include <dmlc/logging.h>
-#include <stdlib.h>
+#include <cstdlib>
 
 namespace mxnet {
 namespace storage {
 
 void* CpuStorage::Alloc(size_t size) {
-  return CHECK_NOTNULL(aligned_alloc(alignment_, size));
+  return CHECK_NOTNULL(memalign(alignment_, size));
 }
 
 void CpuStorage::Free(void* ptr) { free(ptr); }
