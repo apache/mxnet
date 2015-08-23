fae71c2ba92c20a2f5c9b0efb7a3ab92bc6476ba
Bing Xu <antinucleon@gmail.com>
2015-08-23 00:33:40 -0600

    add doxygen

--- doc/conf.py
+++ doc/conf.py
@@ -153,6 +153,7 @@ def run_doxygen(folder):
     """Run the doxygen make command in the designated folder."""
     try:
         retcode = subprocess.call("cd %s; make doxygen" % folder, shell=True)
+        retcode = subprocess.call("cp -rf doxygen/html _build/html/doxygen", shell=True)
         if retcode < 0:
             sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
     except OSError as e:
