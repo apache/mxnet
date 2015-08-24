fef28d2ef1a11ccde6bb4d1143470af0e69ac783
Yutian Li <hotpxless@gmail.com>
2015-08-25 00:19:37 +0800

    [simple-engine] some document

--- src/dag_engine/simple_engine.h
+++ src/dag_engine/simple_engine.h
@@ -17,9 +17,15 @@ namespace mxnet {
 
 namespace engine {
 
+/*!
+ * \brief Forward declarations.
+ */
 struct SimpleOpr;
 struct OprBlock;
 
+/*!
+ * \brief Variable with version information.
+ */
 struct VersionedVarBlock {
   VersionedVarBlock* next{nullptr};
   VersionedVarBlock* join{nullptr};
@@ -28,17 +34,26 @@ struct VersionedVarBlock {
   bool waiting{false};
 };  // struct VersionedVarBlock
 
+/*!
+ * \brief Operation in the queue.
+ */
 struct OprBlock {
   std::function<void()> fn;
   std::atomic<std::size_t> wait{0};
 };  // struct OprBlock
 
+/*!
+ * \brief Variable implementation.
+ */
 struct SimpleVar final : public Var {
   VersionedVarBlock* var{nullptr};
 
   static SimpleVar* CastFromBase(Var* ptr);
 };  // struct SimpleVar
 
+/*!
+ * \brief Operator implementation.
+ */
 struct SimpleOpr final : public Opr {
   DAGEngine::AsyncFn fn;
   std::vector<SimpleVar*> use_vars;
@@ -47,32 +62,58 @@ struct SimpleOpr final : public Opr {
   static SimpleOpr* CastFromBase(Opr* ptr);
 };  // struct SimpleOpr
 
+/*!
+ * \brief Engine implementation.
+ */
 class SimpleEngine final : public DAGEngine {
  public:
+  /*!
+   * \brief Constructor and destructor.
+   */
   SimpleEngine();
   ~SimpleEngine() noexcept(false);
+  /*!
+   * \brief Overriding methods.
+   */
   Variable NewVar() override;
   Operator NewOperator(AsyncFn, std::vector<Variable> const&,
                        std::vector<Variable> const&) override;
   void DeleteOperator(Operator op) override;
   void Push(Operator op, Context) override;
-
   void PushAsync(AsyncFn, Context, std::vector<Variable> const&,
                  std::vector<Variable> const&) override{};
-
   void PushDelete(Fn, Context, Variable) override{};
-
   void WaitForVar(Variable) override{};
-
   void WaitForAll() override{};
-
+  /*!
+   * \brief Callback on operation completion.
+   *
+   * On operation completion, this will trigger subsequent operations.
+   */
   void OnComplete(VersionedVarBlock* var);
+  /*!
+   * \brief Worker.
+   *
+   * The method to pass to thread pool to parallelize.
+   */
   void ThreadWorker();
 
  private:
+  /*!
+   * \brief Concurrency for thread pool.
+   */
   static constexpr std::size_t kNumWorkingThreads = 16;
+  /*!
+   * \brief Task queue.
+   */
   dmlc::ConcurrentBlockingQueue<OprBlock*> task_queue_;
+  /*!
+   * \brief Thread pool.
+   */
   ThreadPool<kNumWorkingThreads> thread_pool_;
+  /*!
+   * \brief Disallow copy construction and assignment.
+   */
   DISALLOW_COPY_AND_ASSIGN(SimpleEngine);
 };  // class SimpleEngine
 
--- src/dag_engine/thread_pool.h
+++ src/dag_engine/thread_pool.h
@@ -15,16 +15,34 @@ namespace mxnet {
 
 namespace engine {
 
+/*!
+ * \brief Thread pool.
+ */
 template <std::size_t kSize>
 class ThreadPool {
  public:
+  /*!
+   * \brief Constructor takes function to run and its arguments.
+   */
   template <typename Function, typename... Args>
   explicit ThreadPool(Function&& func, Args&&... args);
-  ~ThreadPool();
+  /*!
+   * \brief Destructor.
+   */
+  ~ThreadPool() noexcept(false);
 
  private:
+  /*!
+   * \brief Worker threads.
+   */
   std::array<std::thread, kSize> worker_threads_;
-  ThreadPool();
+  /*!
+   * \brief Disallow default construction.
+   */
+  ThreadPool() = delete;
+  /*!
+   * \brief Disallow copy construction and assignment.
+   */
   DISALLOW_COPY_AND_ASSIGN(ThreadPool);
 };
 
@@ -37,7 +55,7 @@ ThreadPool<kSize>::ThreadPool(Function&& func, Args&&... args) {
 }
 
 template <std::size_t kSize>
-ThreadPool<kSize>::~ThreadPool() {
+ThreadPool<kSize>::~ThreadPool() noexcept(false) {
   for (auto&& i : worker_threads_) {
     i.join();
   }
