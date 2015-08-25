be8312d0e56f0126217d0f5e6a7aee9b1ec1ae7e
Yutian Li <hotpxless@gmail.com>
2015-08-26 02:55:21 +0800

    [simple-engine] heavier test on engine

--- tests/test_simple_engine.cc
+++ tests/test_simple_engine.cc
@@ -26,22 +26,28 @@ int main() {
     oprs.push_back(engine->NewOperator(
         [i](mxnet::RunContext ctx, mxnet::DAGEngine::Callback cb) {
           Foo(ctx, i);
+          std::this_thread::sleep_for(std::chrono::seconds{1});
           cb();
         },
         {var}, {}));
     engine->Push(oprs.at(i), mxnet::Context{});
   }
   engine->WaitForAll();
+  printf("Going to push delete\n");
   // std::this_thread::sleep_for(std::chrono::seconds{1});
+  for (auto&& i : oprs) {
+    engine->DeleteOperator(i);
+  }
   engine->PushDelete([](mxnet::RunContext){}, mxnet::Context{}, var);
 
   printf("============= Test #2 ==============\n");
   var = engine->NewVar();
   oprs.clear();
-  for (int i = 0; i < 2; ++i) {
+  for (int i = 0; i < 10; ++i) {
     oprs.push_back(engine->NewOperator(
         [i](mxnet::RunContext ctx, mxnet::DAGEngine::Callback cb) {
           Foo(ctx, i);
+          std::this_thread::sleep_for(std::chrono::milliseconds{500});
           cb();
         },
         {}, {var}));
@@ -49,8 +55,16 @@ int main() {
   }
   // std::this_thread::sleep_for(std::chrono::seconds{1});
   engine->WaitForAll();
+  for (auto&& i : oprs) {
+    engine->DeleteOperator(i);
+  }
   engine->PushDelete([](mxnet::RunContext){}, mxnet::Context{}, var);
   engine->WaitForAll();
+
+  printf("============= Test #3 ==============\n");
+  var = engine->NewVar();
+  oprs.clear();
+  engine->WaitForVar(var);
 
   return 0;
 }
