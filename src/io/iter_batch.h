c1603d77168d62cccd760c974ae8bd251808b08a
sneakerkg <xiaotj1990327@gmail.com>
2015-09-06 07:47:04 +0800

    merge augmenter, modify param attribute

--- src/io/iter_batch.h
+++ src/io/iter_batch.h
@@ -16,26 +16,33 @@ namespace io {
 // Batch parameters
 struct BatchParam : public dmlc::Parameter<BatchParam> {
   /*! \brief label width */
-  index_t batch_size_;
+  index_t batch_size;
+  /*! \brief input shape */
+  // TODO: haven't modify all shape_
+  TShape input_shape;
   /*! \brief label width */
-  index_t label_width_;
+  index_t label_width;
   /*! \brief use round roubin to handle overflow batch */
-  bool round_batch_;
+  bool round_batch;
   /*! \brief skip read */
-  bool test_skipread_;
+  bool test_skipread;
   /*! \brief silent */
-  bool silent_;
+  bool silent;
   // declare parameters
   DMLC_DECLARE_PARAMETER(BatchParam) {
-    DMLC_DECLARE_FIELD(batch_size_).set_default(1)
+    DMLC_DECLARE_FIELD(batch_size)
         .describe("Batch size.");
-    DMLC_DECLARE_FIELD(label_width_).set_default(1)
+    float input_shape_default = {3, 224, 224};
+    DMLC_DECLARE_FIELD(input_shape).set_default(TShape(input_shape_default, input_shape_default + 3))
+        .set_expect_ndim(3).enforce_nonzero()
+        .describe("Input shape of the neural net");   
+    DMLC_DECLARE_FIELD(label_width).set_default(1)
         .describe("Label width.");
-    DMLC_DECLARE_FIELD(round_batch_).set_default(false)
+    DMLC_DECLARE_FIELD(round_batch).set_default(false)
         .describe("Use round robin to handle overflow batch.");
-    DMLC_DECLARE_FIELD(test_skipread_).set_default(false)
+    DMLC_DECLARE_FIELD(test_skipread).set_default(false)
         .describe("Skip read for testing.");
-    DMLC_DECLARE_FIELD(silent_).set_default(false)
+    DMLC_DECLARE_FIELD(silent).set_default(false)
         .describe("Whether to print batch information.");
   }
 };
@@ -54,20 +61,14 @@ public:
     std::vector<std::pair<std::string, std::string> > kwargs_left;
     // init batch param, it could have similar param with 
     kwargs_left = param_.InitAllowUnknown(kwargs);
-    for (size_t i = 0; i < kwargs_left.size(); i++) {
-      if (!strcmp(kwargs_left[i].first.c_str(), "input_shape")) {
-        CHECK(sscanf(kwargs_left[i].second.c_str(), "%u,%u,%u", &shape_[1], &shape_[2], &shape_[3]) == 3)
-          << "input_shape must be three consecutive integers without space example: 1,1,200 ";
-      }
-    }
     // init base iterator
     base_->Init(kwargs);
     mshadow::Shape<4> tshape = shape_;
-    tshape[0] = param_.batch_size_;
+    tshape[0] = param_.batch_size;
     AllocSpaceDense(false);
   }
   virtual void BeforeFirst(void) {
-    if (param_.round_batch_ == 0 || num_overflow_ == 0) {
+    if (param_.round_batch == 0 || num_overflow_ == 0) {
       // otherise, we already called before first
       base_->BeforeFirst();
     } else {
@@ -79,7 +80,7 @@ public:
     out_.num_batch_padd = 0;
 
     // skip read if in head version
-    if (param_.test_skipread_ != 0 && head_ == 0) return true;
+    if (param_.test_skipread != 0 && head_ == 0) return true;
     else this->head_ = 0;
 
     // if overflow from previous round, directly return false, until before first is called
@@ -92,17 +93,17 @@ public:
       out_.inst_index[top] = d.index;
       mshadow::Copy(data[top], d.data[0].get<mshadow::cpu, 3, float>());
 
-      if (++ top >= param_.batch_size_) {
+      if (++ top >= param_.batch_size) {
         out_.data[0] = TBlob(data);
         out_.data[1] = TBlob(label);
         return true;
       }
     }
     if (top != 0) {
-      if (param_.round_batch_ != 0) {
+      if (param_.round_batch != 0) {
         num_overflow_ = 0;
         base_->BeforeFirst();
-        for (; top < param_.batch_size_; ++top, ++num_overflow_) {
+        for (; top < param_.batch_size; ++top, ++num_overflow_) {
           CHECK(base_->Next()) << "number of input must be bigger than batch size";
           const DataInst& d = base_->Value();
           mshadow::Copy(label[top], d.data[1].get<mshadow::cpu, 1, float>());
@@ -111,7 +112,7 @@ public:
         }
         out_.num_batch_padd = num_overflow_;
       } else {
-        out_.num_batch_padd = param_.batch_size_ - top;
+        out_.num_batch_padd = param_.batch_size - top;
       }
       out_.data[0] = TBlob(data);
       out_.data[1] = TBlob(label);
@@ -128,8 +129,6 @@ private:
   BatchParam param_;
   /*! \brief base iterator */
   IIterator<DataInst> *base_;
-  /*! \brief input shape */
-  mshadow::Shape<4> shape_;
   /*! \brief output data */
   DataBatch out_;
   /*! \brief on first */
@@ -143,10 +142,10 @@ private:
   // Functions that allocate and free tensor space
   inline void AllocSpaceDense(bool pad = false) { 
     data = mshadow::NewTensor<mshadow::cpu>(shape_, 0.0f, pad);
-    mshadow::Shape<2> lshape = mshadow::Shape2(param_.batch_size_, param_.label_width_);
+    mshadow::Shape<2> lshape = mshadow::Shape2(param_.batch_size, param_.label_width);
     label = mshadow::NewTensor<mshadow::cpu>(lshape, 0.0f, pad);
-    out_.inst_index = new unsigned[param_.batch_size_];
-    out_.batch_size = param_.batch_size_;
+    out_.inst_index = new unsigned[param_.batch_size];
+    out_.batch_size = param_.batch_size;
     out_.data.resize(2);
   }
   /*! \brief auxiliary function to free space, if needed, dense only */
