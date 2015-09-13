5eb04f86f056c7d3d566484a283b64d87e5f766c
Bing Xu <antinucleon@gmail.com>
2015-09-13 16:55:54 -0600

    add LRN

--- src/operator/cudnn_lrn-inl.h
+++ src/operator/cudnn_lrn-inl.h
@@ -1,41 +1,28 @@
 /*!
  * Copyright (c) 2015 by Contributors
- * \file cudnn_activation-inl.h
+ * \file cudnn_lrn-inl.h
  * \brief
  * \author Bing Xu
 */
 
-#ifndef MXNET_OPERATOR_CUDNN_ACTIVATION_INL_H_
-#define MXNET_OPERATOR_CUDNN_ACTIVATION_INL_H_
-#include <algorithm>
+#ifndef MXNET_OPERATOR_CUDNN_LRN_INL_H_
+#define MXNET_OPERATOR_CUDNN_LRN_INL_H_
 #include <vector>
-#include "./activation-inl.h"
+#include "./lrn-inl.h"
 
 namespace mxnet {
 namespace op {
-class CuDNNActivationOp : public Operator {
+class CuDNNLocalResponseNormOp : public Operator {
  public:
-  explicit CuDNNActivationOp(ActivationParam param) {
+  explicit CuDNNLocalResponseNormOp(LRNParam param) {
     param_ = param;
     init_cudnn_ = false;
+    // TODO(xxx): fp16
     dtype_ = CUDNN_DATA_FLOAT;
-    switch (param_.act_type) {
-    case kReLU:
-      mode_ = CUDNN_ACTIVATION_RELU;
-      break;
-    case kSigmoid:
-      mode_ = CUDNN_ACTIVATION_SIGMOID;
-      break;
-    case kTanh:
-      mode_ = CUDNN_ACTIVATION_TANH;
-      break;
-    default:
-      LOG(FATAL) << "Not implmented";
-      break;
-    }
   }
 
-  ~CuDNNActivationOp() {
+  ~CuDNNLocalResponseNormOp() {
+    CHECK_EQ(cudnnDestroyLRNDescriptor(lrn_desc_), CUDNN_STATUS_SUCCESS);
     CHECK_EQ(cudnnDestroyTensorDescriptor(shape_desc_), CUDNN_STATUS_SUCCESS);
   }
 
@@ -48,23 +35,21 @@ class CuDNNActivationOp : public Operator {
     using namespace mshadow::expr;
     CHECK_EQ(in_data.size(), 1);
     CHECK_EQ(out_data.size(), 1);
+    float alpha = 1.0f;
+    float beta = 0.0f;
     Stream<gpu> *s = ctx.get_stream<gpu>();
     Tensor<gpu, 4> data = in_data[kData].get<gpu, 4, real_t>(s);
     Tensor<gpu, 4> out = out_data[kOut].get<gpu, 4, real_t>(s);
-    float alpha = 1.0f;
-    float beta = 0.0f;
     CHECK_EQ(s->dnn_handle_ownership_, mshadow::Stream<gpu>::OwnHandle);
-    if (!init_cudnn_) {
-      this->Init(s, in_data, out_data);
-    }
-    CHECK_EQ(cudnnActivationForward(s->dnn_handle_,
-                                    mode_,
-                                    &alpha,
-                                    shape_desc_,
-                                    data.dptr_,
-                                    &beta,
-                                    shape_desc_,
-                                    out.dptr_), CUDNN_STATUS_SUCCESS);
+    CHECK_EQ(cudnnLRNCrossChannelForward(s->dnn_handle_,
+                                         lrn_desc_,
+                                         CUDNN_LRN_CROSS_CHANNEL_DIM1,
+                                         &alpha,
+                                         shape_desc_,
+                                         data.dptr_,
+                                         &beta,
+                                         shape_desc_,
+                                         out.dptr_), CUDNN_STATUS_SUCCESS);
   }
 
   virtual void Backward(const OpContext &ctx,
@@ -89,18 +74,19 @@ class CuDNNActivationOp : public Operator {
     Tensor<gpu, 4> output_data = out_data[kOut].get<gpu, 4, real_t>(s);
     Tensor<gpu, 4> input_grad = in_grad[kData].get<gpu, 4, real_t>(s);
     CHECK_EQ(s->dnn_handle_ownership_, mshadow::Stream<gpu>::OwnHandle);
-    CHECK_EQ(cudnnActivationBackward(s->dnn_handle_,
-                                     mode_,
-                                     &alpha,
-                                     shape_desc_,
-                                     output_data.dptr_,
-                                     shape_desc_,
-                                     grad.dptr_,
-                                     shape_desc_,
-                                     data.dptr_,
-                                     &beta,
-                                     shape_desc_,
-                                     input_grad.dptr_), CUDNN_STATUS_SUCCESS);
+    CHECK_EQ(cudnnLRNCrossChannelBackward(s->dnn_handle_,
+                                          lrn_desc_,
+                                          CUDNN_LRN_CROSS_CHANNEL_DIM1,
+                                          &alpha,
+                                          shape_desc_,
+                                          output_data.dptr_,
+                                          shape_desc_,
+                                          grad.dptr_,
+                                          shape_desc_,
+                                          data.dptr_,
+                                          &beta,
+                                          shape_desc_,
+                                          input_grad.dptr_), CUDNN_STATUS_SUCCESS);
   }
 
  private:
@@ -114,7 +100,16 @@ class CuDNNActivationOp : public Operator {
       init_cudnn_ = true;
       Tensor<gpu, 4> data = in_data[kData].get<gpu, 4, real_t>(s);
       Tensor<gpu, 4> out = out_data[kOut].get<gpu, 4, real_t>(s);
+      unsigned lrn_n = param_.nsize;
+      double alpha = param_.alpha;
+      double beta = param_.beta;
+      double lrn_k = param_.knorm;
       CHECK_EQ(data.shape_, out.shape_);
+      CHECK_EQ(cudnnGetLRNDescriptor(lrn_desc_,
+                                     &lrn_n,
+                                     &alpha,
+                                     &beta,
+                                     &lrn_k), CUDNN_STATUS_SUCCESS);
       CHECK_EQ(cudnnCreateTensorDescriptor(&shape_desc_), CUDNN_STATUS_SUCCESS);
       CHECK_EQ(cudnnSetTensor4dDescriptor(shape_desc_,
                                           CUDNN_TENSOR_NCHW,
@@ -126,11 +121,11 @@ class CuDNNActivationOp : public Operator {
     }
   }
   bool init_cudnn_;
+  LRNParam param_;
   cudnnDataType_t dtype_;
-  cudnnActivationMode_t mode_;
+  cudnnLRNDescriptor_t lrn_desc_;
   cudnnTensorDescriptor_t shape_desc_;
-  ActivationParam param_;
-};  // class CuDNNActivationOp
+};  // class CuDNNLocalResponseNormOp
 }  // namespace op
 }  // namespace mxnet
-#endif  // MXNET_OPERATOR_CUDNN_ACTIVATION_INL_H_
+#endif  // MXNET_OPERATOR_CUDNN_LRN_INL_H_
