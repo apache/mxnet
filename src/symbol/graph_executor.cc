d5d6177b961b75c54f67d8b8747ebab9438092c3
Tianqi Chen <tqchen@users.noreply.github.com>
2015-09-17 21:56:16 -0700

    Update graph_executor.cc

--- src/symbol/graph_executor.cc
+++ src/symbol/graph_executor.cc
@@ -168,8 +168,7 @@ inline std::vector<std::pair<T, T> > GraphExecutor::GetInplaceOption(
     std::vector<std::pair<T, T> > remap(remap_index.size());
     for (size_t i = 0; i < remap_index.size(); ++i) {
       if (args_array[remap_index[i].first] == nullptr) {
-        LOG(FATAL) <<
-            "BackwardInplaceOption uses input that is returned by DeclareBackwardDependency";
+        LOG(FATAL) << "BackwardInplaceOption not consistent with DeclareBackwardDependency";
       }
       remap[i].first = *args_array[remap_index[i].first];
       remap[i].second = *static_cast<T*>(remap_index[i].second);
