6a434ce3ccf4bbef46cbde291c78cb4859070cfb
Bing Xu <antinucleon@gmail.com>
2015-09-21 17:09:24 -0600

    move mnist to ualberta server

--- tests/python/common/get_data.py
+++ tests/python/common/get_data.py
@@ -14,18 +14,14 @@ def GetMNIST_pkl():
 def GetMNIST_ubyte():
     if not os.path.isdir("data/"):
         os.system("mkdir data/")
-    if not os.path.exists('data/train-images-idx3-ubyte'):
-        os.system("wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -P data/")
-        os.system("gunzip data/train-images-idx3-ubyte.gz")
-    if not os.path.exists('data/train-labels-idx1-ubyte'):
-        os.system("wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -P data/")
-        os.system("gunzip data/train-labels-idx1-ubyte.gz")
-    if not os.path.exists('data/t10k-images-idx3-ubyte'):
-        os.system("wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -P data/")
-        os.system("gunzip data/t10k-images-idx3-ubyte.gz")
-    if not os.path.exists('data/t10k-labels-idx1-ubyte'):
-        os.system("wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -P data/")
-        os.system("gunzip data/t10k-labels-idx1-ubyte.gz")
+    if (not os.path.exists('data/train-images-idx3-ubyte')) or \
+       (not os.path.exists('data/train-labels-idx1-ubyte')) or \
+       (not os.path.exists('data/t10k-images-idx3-ubyte')) or \
+       (not os.path.exists('data/t10k-labels-idx1-ubyte')):
+        os.system("wget http://webdocs.cs.ualberta.ca/~bx3/data/mnist.zip -P data/")
+        os.chdir("./data")
+        os.system("unzip -u mnist.zip")
+        os.chdir("..")
 
 # download cifar
 def GetCifar10():
@@ -34,5 +30,5 @@ def GetCifar10():
     if not os.path.exists('data/cifar10.zip'):
         os.system("wget http://webdocs.cs.ualberta.ca/~bx3/data/cifar10.zip -P data/")
         os.chdir("./data")
-        os.system("unzip cifar10.zip")
+        os.system("unzip -u cifar10.zip")
         os.chdir("..")
