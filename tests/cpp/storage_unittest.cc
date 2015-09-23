ae06f8d29bc71b271f114bdba78e0e9868e07d56
muli <muli@cs.cmu.edu>
2015-09-23 19:24:29 +0000

    [unittest] bug fix

--- tests/cpp/storage_unittest.cc
+++ tests/cpp/storage_unittest.cc
@@ -8,27 +8,27 @@ TEST(Storage, basics) {
   auto&& storage = mxnet::Storage::Get();
   mxnet::Context context_cpu{};
   auto&& handle = storage->Alloc(kSize, context_cpu);
-  ASSERT_EQ(handle.ctx, context_cpu);
-  ASSERT_EQ(handle.size, kSize);
+  EXPECT_EQ(handle.ctx, context_cpu);
+  EXPECT_EQ(handle.size, kSize);
   auto ptr = handle.dptr;
   storage->Free(handle);
   handle = storage->Alloc(kSize, context_cpu);
-  ASSERT_EQ(handle.ctx, context_cpu);
-  ASSERT_EQ(handle.size, kSize);
-  ASSERT_EQ(handle.dptr, ptr);
+  EXPECT_EQ(handle.ctx, context_cpu);
+  EXPECT_EQ(handle.size, kSize);
+  EXPECT_EQ(handle.dptr, ptr);
   LOG(INFO) << "Success on CPU!\n";
 
 #if MXNET_USE_CUDA
-  mxnet::Context context_gpu{mxnet::gpu::kDevMask, 0};
+  mxnet::Context context_gpu = mxnet::Context::GPU(0);
   handle = storage->Alloc(kSize, context_gpu);
   assert(handle.ctx == context_gpu);
   assert(handle.size == kSize);
   ptr = handle.dptr;
   storage->Free(handle);
   handle = storage->Alloc(kSize, context_gpu);
-  ASSERT_EQ(handle.ctx, context_gpu);
-  ASSERT_EQ(handle.size, kSize);
-  ASSERT_EQ(handle.dptr, ptr);
+  EXPECT_EQ(handle.ctx, context_gpu);
+  EXPECT_EQ(handle.size, kSize);
+  EXPECT_EQ(handle.dptr, ptr);
   LOG(INFO) << "Success on GPU!\n";
 #endif  // MXNET_USE_CUDA
 }
