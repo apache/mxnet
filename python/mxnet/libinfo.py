2358618683e99542a317b0d9d8434250c65fa7b7
Tianqi Chen <tianqi.tchen@gmail.com>
2015-09-28 19:56:37 -0700

    Make python package install more easily

---
+++ python/mxnet/libinfo.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+"""Information about mxnet."""
+from __future__ import absolute_import
+import os
+import platform
+
+def find_lib_path():
+    """Find MXNet dynamic library files.
+
+    Returns
+    -------
+    lib_path : list(string)
+        List of all found path to the libraries
+    """
+    curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+    api_path = os.path.join(curr_path, '../../lib/')
+    dll_path = [curr_path, api_path]
+    if os.name == 'nt':
+        if platform.architecture()[0] == '64bit':
+            dll_path.append(os.path.join(api_path, '../../windows/x64/Release/'))
+        else:
+            dll_path.append(os.path.join(api_path, '../../windows/Release/'))
+    if os.name == 'nt':
+        dll_path = [os.path.join(p, 'mxnet.dll') for p in dll_path]
+    else:
+        dll_path = [os.path.join(p, 'libmxnet.so') for p in dll_path]
+    lib_path = [p for p in dll_path if os.path.exists(p) and os.path.isfile(p)]
+    if len(lib_path) == 0:
+        raise RuntimeError('Cannot find find the files.\n' +
+                           'List of candidates:\n' + str('\n'.join(dll_path)))
+    return lib_path
+
+
+# current version
+__version__ = "0.5.0"
--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,22 @@
-"""Setup script for mxnet."""
+# pylint: disable=invalid-name, exec-used
+"""Setup mxnet package."""
 from __future__ import absolute_import
-import sys
+import os
 from setuptools import setup
-sys.path.insert(0, '.')
-import mxnet
 
-LIB_PATH = mxnet.base.find_lib_path()
+# We can not import `mxnet.info.py` in setup.py directly since mxnet/__init__.py
+# Will be invoked which introduces dependences
+CURRENT_DIR = os.path.dirname(__file__)
+libinfo_py = os.path.join(CURRENT_DIR, 'mxnet/libinfo.py')
+libinfo = {'__file__': libinfo_py}
+exec(compile(open(libinfo_py, "rb").read(), libinfo_py, 'exec'), libinfo, libinfo)
+
+LIB_PATH = libinfo['find_lib_path']()
+__version__ = libinfo['__version__']
 
 setup(name='mxnet',
-      version=mxnet.__version__,
-      description=mxnet.__doc__,
+      version=__version__,
+      description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
       install_requires=[
           'numpy',
       ],
