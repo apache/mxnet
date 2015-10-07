7a66ef4a723ddad0a24da2dcb201d1b38ce92389
Chiyuan Zhang <pluskid@gmail.com>
2015-10-07 12:35:23 -0400

    enable osx build on travis

--- test/travis/setup_env.sh
+++ test/travis/setup_env.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo "##########################"
+echo $TRAVIS_OS_NAME
+
 if [ ${TRAVIS_OS_NAME} == "linux" ]; then
-  export CXX="g++-4.8"
-  export CC="gcc-4.8"
+  alias g++="g++-4.8"
+  alias gcc="gcc-4.8"
+fi
+
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+  brew update
+  brew tap homebrew/science
+  brew info opencv
+  brew install graphviz
+  brew install opencv
 fi
