b22c83f62eec779cf055cea46cfafa94ae52e3e4
Chiyuan Zhang <pluskid@gmail.com>
2015-10-07 12:42:07 -0400

    fix build script

--- test/travis/build_mxnet.sh
+++ test/travis/build_mxnet.sh
@@ -6,6 +6,20 @@ cd __mxnet_build
 if [ ! -f config.mk ]; then
     echo "Use the default config.m"
     cp make/config.mk config.mk
+
+    if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+      sed -i 's/export CC = gcc/export CC = gcc-4.8/g' config.mk
+      sed -i 's/export CXX = g++/export CXX = g++4.8/g' config.mk
+      echo "=============================="
+      gcc --version
+      gcc-4.8 --version
+    fi
+
+    if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+      sed -i 's%ADD_CFLAGS =%ADD_CFLAGS = -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/Headers/%' config.mk
+    fi
+
+    cat config.mk
 fi
 
 make -j4 || exit 1
--- test/travis/setup_env.sh
+++ test/travis/setup_env.sh
@@ -3,11 +3,6 @@
 echo "##########################"
 echo $TRAVIS_OS_NAME
 
-if [ ${TRAVIS_OS_NAME} == "linux" ]; then
-  alias g++="g++-4.8"
-  alias gcc="gcc-4.8"
-fi
-
 if [ ${TRAVIS_OS_NAME} == "osx" ]; then
   brew update
   brew tap homebrew/science
