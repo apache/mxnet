7713589761802ca5d22f5154b2ae286013fe346e
Chiyuan Zhang <pluskid@gmail.com>
2015-10-07 12:50:05 -0400

    fix sed option

--- test/travis/build_mxnet.sh
+++ test/travis/build_mxnet.sh
@@ -10,13 +10,10 @@ if [ ! -f config.mk ]; then
     if [ ${TRAVIS_OS_NAME} == "linux" ]; then
       sed -i 's/export CC = gcc/export CC = gcc-4.8/g' config.mk
       sed -i 's/export CXX = g++/export CXX = g++-4.8/g' config.mk
-      echo "=============================="
-      gcc --version
-      gcc-4.8 --version
     fi
 
     if [ ${TRAVIS_OS_NAME} == "osx" ]; then
-      sed -i 's%ADD_CFLAGS =%ADD_CFLAGS = -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/Headers/%' config.mk
+      sed -i -s 's%ADD_CFLAGS =%ADD_CFLAGS = -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/Headers/%' config.mk
     fi
 
     cat config.mk
