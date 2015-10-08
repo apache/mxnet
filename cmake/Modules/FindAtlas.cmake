a940a777362e958d3bfd8a8c5146d9116b15ecbd
Minjie Wang <wmjlyjemaine@gmail.com>
2015-10-08 02:06:11 -0400

    cmake works on linux

--- cmake/Modules/FindAtlas.cmake
+++ cmake/Modules/FindAtlas.cmake
@@ -2,6 +2,7 @@
 #
 # The following variables are optionally searched for defaults
 #  Atlas_ROOT_DIR:            Base directory where all Atlas components are found
+#  Atlas_NEED_LAPACK:         Whether need lapack libraries
 #
 # The following are set after configuration is done:
 #  Atlas_FOUND
@@ -14,6 +15,7 @@ set(Atlas_INCLUDE_SEARCH_PATHS
   /usr/include/atlas-base
   $ENV{Atlas_ROOT_DIR}
   $ENV{Atlas_ROOT_DIR}/include
+  $ENV{Atlas_ROOT_DIR}/include/atlas
 )
 
 set(Atlas_LIB_SEARCH_PATHS
@@ -24,20 +26,21 @@ set(Atlas_LIB_SEARCH_PATHS
 )
 
 find_path(Atlas_CBLAS_INCLUDE_DIR   NAMES cblas.h   PATHS ${Atlas_INCLUDE_SEARCH_PATHS})
-find_path(Atlas_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${Atlas_INCLUDE_SEARCH_PATHS})
-
 find_library(Atlas_CBLAS_LIBRARY NAMES  ptcblas_r ptcblas cblas_r cblas PATHS ${Atlas_LIB_SEARCH_PATHS})
 find_library(Atlas_BLAS_LIBRARY NAMES   atlas_r   atlas                 PATHS ${Atlas_LIB_SEARCH_PATHS})
-find_library(Atlas_LAPACK_LIBRARY NAMES alapack_r alapack lapack_atlas  PATHS ${Atlas_LIB_SEARCH_PATHS})
 
 set(LOOKED_FOR
   Atlas_CBLAS_INCLUDE_DIR
-  Atlas_CLAPACK_INCLUDE_DIR
 
   Atlas_CBLAS_LIBRARY
   Atlas_BLAS_LIBRARY
-  Atlas_LAPACK_LIBRARY
 )
+
+if(Atlas_NEED_LAPACK)
+  find_path(Atlas_CLAPACK_INCLUDE_DIR NAMES clapack.h PATHS ${Atlas_INCLUDE_SEARCH_PATHS})
+  find_library(Atlas_LAPACK_LIBRARY NAMES alapack_r alapack lapack_atlas  PATHS ${Atlas_LIB_SEARCH_PATHS})
+  set(LOOKED_FOR ${LOOKED_FOR} Atlas_CLAPACK_INCLUDE_DIR Atlas_LAPACK_LIBRARY)
+endif(Atlas_NEED_LAPACK)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Atlas DEFAULT_MSG ${LOOKED_FOR})
@@ -49,4 +52,3 @@ if(ATLAS_FOUND)
 
   message(STATUS "Found Atlas (include: ${Atlas_CBLAS_INCLUDE_DIR}, library: ${Atlas_BLAS_LIBRARY})")
 endif(ATLAS_FOUND)
-
--- src/io/io.cc
+++ src/io/io.cc
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 by Contributors
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE
+//#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_DEPRECATE
 
 #include <mxnet/io.h>
 #include <dmlc/registry.h>
