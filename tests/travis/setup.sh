8060dd5a367e9dd241b70441b0147d2b349a8b63
muli <muli@cs.cmu.edu>
2015-10-09 00:59:37 -0400

    [travis] python

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -6,13 +6,12 @@ if [ ${TRAVIS_OS_NAME} == "osx" ]; then
     brew info opencv
     brew install opencv
     brew install python3
+    if [ ${TASK} == "python_test" ]; then
+        python -m pip install nose numpy --user `whoami`
+        python3 -m pip install nose numpy --user `whoami`
+    fi
 fi
 
 if [ ${TASK} == "lint" ]; then
     pip install cpplint pylint graphviz --user `whoami`
-fi
-
-if [ ${TASK} == "python_test" ]; then
-    python -m pip install nose numpy --user `whoami`
-    python3 -m pip install nose numpy --user `whoami`
 fi
