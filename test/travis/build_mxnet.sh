1eb7ebcda9aec2b97ec88d208704411890d018c7
Chiyuan Zhang <pluskid@gmail.com>
2015-10-20 14:17:01 -0400

    use osx.mk to simplify travis OSX build

--- test/travis/build_mxnet.sh
+++ test/travis/build_mxnet.sh
@@ -4,19 +4,18 @@ git clone --recursive https://github.com/dmlc/mxnet __mxnet_build
 cd __mxnet_build
 
 if [ ! -f config.mk ]; then
-  echo "Use the default config.m"
-  cp make/config.mk config.mk
-
   if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+    cp make/config.mk config.mk
     sed -i 's/export CC = gcc/export CC = gcc-4.8/g' config.mk
     sed -i 's/export CXX = g++/export CXX = g++-4.8/g' config.mk
   fi
 
   if [ ${TRAVIS_OS_NAME} == "osx" ]; then
-    # add built-in blas header file to path
-    sed -i -s 's%ADD_CFLAGS =%ADD_CFLAGS = -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/Headers/%' config.mk
-    # disable openmp
-    sed -i -s 's%USE_OPENMP = 1%USE_OPENMP = 0%g' config.mk
+    cp make/osx.mk config.mk
+    ## add built-in blas header file to path
+    #sed -i -s 's%ADD_CFLAGS =%ADD_CFLAGS = -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Versions/Current/Headers/%' config.mk
+    ## disable openmp
+    #sed -i -s 's%USE_OPENMP = 1%USE_OPENMP = 0%g' config.mk
   fi
 fi
 
