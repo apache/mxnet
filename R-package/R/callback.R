71f09068e8c92eb2277f5216713ecab08b041fa1
tqchen <tianqi.tchen@gmail.com>
2015-10-20 10:53:12 -0600

    [R] Add auto layout selection to detect layout for rowmajor in simple MLP case.

--- R-package/R/callback.R
+++ R-package/R/callback.R
@@ -2,7 +2,7 @@
 #' @export
 mx.callback.log.train.metric <- function(period) {
   function(iteration, nbatch, env) {
-    if (nbatch %% period == 0) {
+    if (nbatch %% period == 0 && !is.null(env$metric)) {
       result <- env$metric$get(env$train.metric)
       cat(paste0("Batch [", nbatch, "] Train-", result$name, "=", result$value, "\n"))
     }
