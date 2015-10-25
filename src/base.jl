7e86c8f7f001977f5429762112824b0a7d50e32f
Chiyuan Zhang <pluskid@gmail.com>
2015-10-25 00:09:46 -0400

    improve defstruct macro

--- src/base.jl
+++ src/base.jl
@@ -157,35 +157,42 @@ the keyword arguments.
 """
 macro defstruct(name, super_name, fields)
   @assert fields.head == :tuple
-  fields = fields.args
+  fields     = fields.args
   @assert length(fields) > 0
-  name = esc(name)
+  name       = esc(name)
+  super_name = esc(super_name)
 
   field_defs     = Array(Expr, length(fields))        # :(field2 :: Int)
-  field_names    = Array(Base.Symbol, length(fields)) # :field2
-  field_defaults = Array(Expr, length(fields))        # :(field2 :: Int = 0)
+  field_names    = Array(Expr, length(fields))        # :field2
+  field_defaults = Array(Expr, length(fields))        # :(field2 = 0)
+  field_types    = Array(Expr, length(fields))        # Int
   field_asserts  = Array(Expr, length(fields))        # :(field2 >= 0)
 
   for i = 1:length(fields)
     field = fields[i]
     if field.head == :tuple
-      field_asserts[i] = field.args[2]
+      field_asserts[i] = esc(field.args[2])
       field = field.args[1]
     end
-    field_defs[i] = esc(field.args[1])
-    field_names[i] = field.args[1].args[1]
-    field_defaults[i] = Expr(:kw, field.args...)
+    field_defs[i]     = esc(field.args[1])
+    field_names[i]    = esc(field.args[1].args[1])
+    field_types[i]    = esc(field.args[1].args[2])
+    field_defaults[i] = Expr(:kw, field.args[1].args[1], esc(field.args[2]))
   end
 
   # body of layer type, defining fields
   type_body = Expr(:block, field_defs...)
 
   # constructor
+  converts = map(zip(field_names, field_types)) do param
+    f_name, f_type = param
+    :($f_name = convert($f_type, $f_name))
+  end
   asserts = map(filter(i -> isdefined(field_asserts,i), 1:length(fields))) do i
     :(@assert($(field_asserts[i])))
   end
   construct = Expr(:call, name, field_names...)
-  ctor_body = Expr(:block, asserts..., construct)
+  ctor_body = Expr(:block, converts..., asserts..., construct)
   ctor_def = Expr(:call, name, Expr(:parameters, field_defaults...))
   ctor = Expr(:(=), ctor_def, ctor_body)
 
