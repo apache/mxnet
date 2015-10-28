052471e72cee530573e348d11754c1b7268b601e
Junyuan Xie <eric.jy.xie@gmail.com>
2015-10-27 19:38:59 -0700

    move recordio interface to recordio.py

--- python/mxnet/io.py
+++ python/mxnet/io.py
@@ -258,47 +258,6 @@ class MXDataIter(DataIter):
         check_call(_LIB.MXDataIterGetPadNum(self.handle, ctypes.byref(pad)))
         return pad.value
 
-class MXRecordIO(object):
-    """Python interface for read/write RecordIO data formmat"""
-    def __init__(self, uri, flag):
-        uri = ctypes.c_char_p(uri)
-        self.handle = RecordIOHandle()
-        if flag == "w":
-            check_call(_LIB.MXRecordIOWriterCreate(uri, ctypes.byref(self.handle)))
-            self.writable = True
-        elif flag == "r":
-            check_call(_LIB.MXRecordIOReaderCreate(uri, ctypes.byref(self.handle)))
-            self.writable = False
-        else:
-            raise ValueError("Invalid flag %s"%flag)
-
-    def __del__(self):
-        if self.writable:
-            check_call(_LIB.MXRecordIOWriterFree(self.handle))
-        else:
-            check_call(_LIB.MXRecordIOReaderFree(self.handle))
-
-    def write(self, buf):
-        """Write a string buffer as a record"""
-        assert self.writable
-        check_call(_LIB.MXRecordIOWriterWriteRecord(self.handle,
-                                                    ctypes.c_char_p(buf),
-                                                    ctypes.c_size_t(len(buf))))
-
-    def read(self):
-        """Read a record as string"""
-        assert not self.writable
-        buf = ctypes.c_char_p()
-        size = ctypes.c_size_t()
-        check_call(_LIB.MXRecordIOReaderReadRecord(self.handle,
-                                                   ctypes.byref(buf),
-                                                   ctypes.byref(size)))
-        buf = ctypes.cast(buf, ctypes.POINTER(ctypes.c_char*size.value))
-        return buf.contents.raw
-
-
-
-
 def _make_io_iterator(handle):
     """Create an io iterator by handle."""
     name = ctypes.c_char_p()
---
+++ python/mxnet/recordio.py
@@ -0,0 +1,67 @@
+# coding: utf-8
+# pylint: disable=invalid-name, protected-access, fixme, too-many-arguments
+
+from __future__ import absolute_import
+
+import ctypes
+from .base import _LIB
+from .base import RecordIOHandle
+from .base import check_call
+
+class MXRecordIO(object):
+    """Python interface for read/write RecordIO data formmat
+
+    Parameters
+    ----------
+    uri : string
+        uri path to recordIO file.
+    flag : string
+        "r" for reading or "w" writing.
+    """
+    def __init__(self, uri, flag):
+        uri = ctypes.c_char_p(uri)
+        self.handle = RecordIOHandle()
+        if flag == "w":
+            check_call(_LIB.MXRecordIOWriterCreate(uri, ctypes.byref(self.handle)))
+            self.writable = True
+        elif flag == "r":
+            check_call(_LIB.MXRecordIOReaderCreate(uri, ctypes.byref(self.handle)))
+            self.writable = False
+        else:
+            raise ValueError("Invalid flag %s"%flag)
+
+    def __del__(self):
+        if self.writable:
+            check_call(_LIB.MXRecordIOWriterFree(self.handle))
+        else:
+            check_call(_LIB.MXRecordIOReaderFree(self.handle))
+
+    def write(self, buf):
+        """Write a string buffer as a record
+
+        Parameters
+        ----------
+        buf : string
+            buffer to write.
+        """
+        assert self.writable
+        check_call(_LIB.MXRecordIOWriterWriteRecord(self.handle,
+                                                    ctypes.c_char_p(buf),
+                                                    ctypes.c_size_t(len(buf))))
+
+    def read(self):
+        """Read a record as string
+
+        Returns
+        ----------
+        buf : string
+            buffer read.
+        """
+        assert not self.writable
+        buf = ctypes.c_char_p()
+        size = ctypes.c_size_t()
+        check_call(_LIB.MXRecordIOReaderReadRecord(self.handle,
+                                                   ctypes.byref(buf),
+                                                   ctypes.byref(size)))
+        buf = ctypes.cast(buf, ctypes.POINTER(ctypes.c_char*size.value))
+        return buf.contents.raw
