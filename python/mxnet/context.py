ce95a6f8b31c2effb7e8e8d251f2ea1716002b01
ZhengXu <xuzheng1111@gmail.com>
2015-11-12 23:25:23 -0600

    Remove the hack from context.py and add work_load_list to model.py

--- python/mxnet/context.py
+++ python/mxnet/context.py
@@ -2,7 +2,6 @@
 """Context management API of mxnet."""
 from __future__ import absolute_import
 
-
 class Context(object):
     """Constructing a context.
 
@@ -13,12 +12,6 @@ class Context(object):
 
     device_id : int (default=0)
         The device id of the device, needed for GPU
-
-    work_load : int (default=1)
-        The work_load in a list of devices. For example,
-            if we have a list of device with work_load [1, 2, 1],
-            the first and third device will take 1/4 work load,
-            while the second device will take 1/2 work_load.
 
     Note
     ----
@@ -38,16 +31,13 @@ class Context(object):
     default_ctx = None
     devtype2str = {1: 'cpu', 2: 'gpu', 3: 'cpu_pinned'}
     devstr2type = {'cpu': 1, 'gpu': 2, 'cpu_pinned': 3}
-
-    def __init__(self, device_type, device_id=0, work_load=1):
+    def __init__(self, device_type, device_id=0):
         if isinstance(device_type, Context):
             self.device_typeid = device_type.device_typeid
             self.device_id = device_type.device_id
-            self.work_load = device_type.work_load
         else:
             self.device_typeid = Context.devstr2type[device_type]
             self.device_id = device_id
-            self.work_load = work_load
         self._old_ctx = None
 
     @property
@@ -78,7 +68,7 @@ class Context(object):
 Context.default_ctx = Context('cpu', 0)
 
 
-def cpu(device_id=0, work_load=1):
+def cpu(device_id=0):
     """Return a CPU context.
 
     This function is a short cut for Context('cpu', device_id)
@@ -89,18 +79,15 @@ def cpu(device_id=0, work_load=1):
         The device id of the device. device_id is not needed for CPU.
         This is included to make interface compatible with GPU.
 
-    work_load : int (default=1)
-        The work_load in a list of devices.
-
     Returns
     -------
     context : Context
         The corresponding CPU context.
     """
-    return Context('cpu', device_id, work_load)
+    return Context('cpu', device_id)
 
 
-def gpu(device_id=0, work_load=1):
+def gpu(device_id=0):
     """Return a GPU context.
 
     This function is a short cut for Context('cpu', device_id)
@@ -110,15 +97,12 @@ def gpu(device_id=0, work_load=1):
     device_id : int, optional
         The device id of the device, needed for GPU
 
-    work_load : int (default=1)
-        The work_load in a list of devices.
-
     Returns
     -------
     context : Context
         The corresponding GPU context.
     """
-    return Context('gpu', device_id, work_load)
+    return Context('gpu', device_id)
 
 
 def current_context():
--- python/mxnet/model.py
+++ python/mxnet/model.py
@@ -77,14 +77,15 @@ def _check_arguments(symbol):
         aux_set.add(name)
 
 
-def _split_input_slice(batch_size, ctx):
+def _split_input_slice(batch_size, work_load_list):
     """Get input slice from the input shape.
     Parameters
     ----------
     batch_size : int
         The number of samples in a mini-batch.
-    ctx : list of Context class
-        The list of device context used in training
+    work_load_list : list of float or int, optional
+        The list of work load for different devices,
+        in the same order as ctx
     Returns
     -------
     slices : list of slice
@@ -94,7 +95,6 @@ def _split_input_slice(batch_size, ctx):
     ValueError
         If there are two many splits such that some slice can be empty.
     """
-    work_load_list = [float(c.work_load) for c in ctx]
     total_work_load = sum(work_load_list)
     batch_num_list = [round(work_load * batch_size / total_work_load)
                       for work_load in work_load_list]
@@ -157,7 +157,7 @@ def _train_multi_device(symbol, ctx, arg_names, param_names, aux_names,
                         kvstore, update_on_kvstore,
                         train_data, eval_data=None, eval_metric=None,
                         epoch_end_callback=None, batch_end_callback=None,
-                        logger=None):
+                        logger=None, work_load_list=None):
     """Internal training function on multiple devices.
     This function will also work for single device as well.
     Parameters
@@ -205,6 +205,9 @@ def _train_multi_device(symbol, ctx, arg_names, param_names, aux_names,
         whether or not perform weight updating on kvstore
     logger : logging logger
         When not specified, default logger will be used.
+    work_load_list : list of float or int, optional
+        The list of work load for different devices,
+        in the same order as ctx
     Notes
     -----
     - This function will inplace update the NDArrays in arg_parans and aux_states.
@@ -218,7 +221,11 @@ def _train_multi_device(symbol, ctx, arg_names, param_names, aux_names,
     # make sure the architecture is valid
     _check_arguments(symbol)
 
-    slices = _split_input_slice(train_data.batch_size, ctx)
+    if work_load_list is None:
+        work_load_list = [1] * num_device
+    assert isinstance(work_load_list, list) and len(work_load_list) == num_device, \
+        "Invalid settings for work load. "
+    slices = _split_input_slice(train_data.batch_size, work_load_list)
     train_execs = []
     for i in range(len(ctx)):
         data_shapes = {k: tuple([slices[i].stop-slices[i].start] + list(v[1:]))
@@ -658,7 +665,8 @@ class FeedForward(BASE_ESTIMATOR):
             return outputs
 
     def fit(self, X, y=None, eval_data=None, eval_metric='acc',
-            epoch_end_callback=None, batch_end_callback=None, kvstore='local', logger=None):
+            epoch_end_callback=None, batch_end_callback=None, kvstore='local', logger=None,
+            work_load_list=None):
         """Fit the model.
         Parameters
         ----------
@@ -692,6 +700,9 @@ class FeedForward(BASE_ESTIMATOR):
            In default uses 'local', often no need to change for single machiine.
         logger : logging logger, optional
             When not specified, default logger will be used.
+        work_load_list : float or int, optional
+            The list of work load for different devices,
+            in the same order as ctx
         """
 
         data = self._init_iter(X, y, is_train=True)
@@ -730,7 +741,7 @@ class FeedForward(BASE_ESTIMATOR):
                             epoch_end_callback=epoch_end_callback,
                             batch_end_callback=batch_end_callback,
                             kvstore=kvstore, update_on_kvstore=update_on_kvstore,
-                            logger=logger)
+                            logger=logger, work_load_list=work_load_list)
 
 
     def save(self, prefix, epoch=None):
@@ -788,7 +799,7 @@ class FeedForward(BASE_ESTIMATOR):
     def create(symbol, X, y=None, ctx=None,
                num_epoch=None, epoch_size=None, optimizer='sgd', initializer=Uniform(0.01),
                eval_data=None, eval_metric='acc', epoch_end_callback=None,
-               kvstore='local', logger=None, **kwargs):
+               kvstore='local', logger=None, work_load_list=None, **kwargs):
         """Functional style to create a model.
         This function will be more consistent with functional
         languages such as R, where mutation is not allowed.
@@ -829,11 +840,17 @@ class FeedForward(BASE_ESTIMATOR):
            'dist_sync' : multi-machines with BSP
            'dist_async' : multi-machines with partical asynchronous
            In default uses 'local', often no need to change for single machiine.
+        logger : logging logger, optional
+            When not specified, default logger will be used.
+        work_load_list : list of float or int, optional
+            The list of work load for different devices,
+            in the same order as ctx
         """
         model = FeedForward(symbol, ctx=ctx, num_epoch=num_epoch, epoch_size=epoch_size,
                             optimizer=optimizer, initializer=initializer, **kwargs)
         model.fit(X, y, eval_data=eval_data, eval_metric=eval_metric,
                   epoch_end_callback=epoch_end_callback,
                   kvstore=kvstore,
-                  logger=logger)
+                  logger=logger,
+                  work_load_list=work_load_list)
         return model
