1567263a24d200bf5412d5b8e438eac024b7707f
Junyuan Xie <eric.jy.xie@gmail.com>
2015-11-29 12:26:28 -0800

    python interface for creating image record binary

--- python/mxnet/recordio.py
+++ python/mxnet/recordio.py
@@ -1,13 +1,22 @@
 # coding: utf-8
-# pylint: disable=invalid-name, protected-access, fixme, too-many-arguments
+# pylint: disable=invalid-name, protected-access, fixme, too-many-arguments, no-member
 
 """Python interface for DLMC RecrodIO data format"""
 from __future__ import absolute_import
+from collections import namedtuple
 
 import ctypes
 from .base import _LIB
 from .base import RecordIOHandle
 from .base import check_call
+import struct
+import numpy as np
+try:
+    import cv, cv2
+    opencv_available = True
+except ImportError:
+    print('OpenCV is unavailable.')
+    opencv_available = False
 
 class MXRecordIO(object):
     """Python interface for read/write RecordIO data formmat
@@ -66,3 +75,52 @@ class MXRecordIO(object):
                                                    ctypes.byref(size)))
         buf = ctypes.cast(buf, ctypes.POINTER(ctypes.c_char*size.value))
         return buf.contents.raw
+
+IRHeader = namedtuple('HEADER', ['flag', 'label', 'id', 'id2'])
+_IRFormat = 'IfQQ'
+_IRSize = struct.calcsize(_IRFormat)
+
+def unpack_img(s, iscolor=-1):
+    """unpack a MXImageRecord
+
+    Parameters
+    ----------
+    s : str
+        string buffer from MXRecordIO.read
+    iscolor : int
+        image format option for cv2.imdecode
+
+    Returns
+    header : IRHeader
+        header of the image record
+    img : numpy.ndarray
+        unpacked image
+    """
+    header = IRHeader(*struct.unpack(_IRFormat, s[:_IRSize]))
+    img = np.fromstring(s[_IRSize:], dtype=np.uint8)
+    if opencv_available:
+        img = cv2.imdecode(img, iscolor)
+    return header, img
+
+def pack_img(header, img, quality=80):
+    """pack an image into MXImageRecord
+
+    Parameters
+    ----------
+    header : IRHeader
+        header of the image record
+    img : numpy.ndarray
+        image to pack
+    quality : int
+        quality for JPEG encoding. 1-100
+    """
+    header = IRHeader(*header)
+    s = struct.pack(_IRFormat, *header)
+    if isinstance(img, str):
+        s += img
+    else:
+        assert opencv_available
+        ret, buf = cv2.imencode('.JPEG', img, [cv.CV_IMWRITE_JPEG_QUALITY, quality])
+        assert ret
+        s += buf.tostring()
+    return s
