30e742b7a427ab0392d9713bc62478470607fc5d
Bing Xu <antinucleon@gmail.com>
2015-11-30 01:11:07 -0700

    update pylint version

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -15,5 +15,5 @@ if [ ${TRAVIS_OS_NAME} == "osx" ]; then
 fi
 
 if [ ${TASK} == "lint" ]; then
-    pip install cpplint pylint --user `whoami`
+    pip install cpplint 'pylint==1.4.4' 'astroid==1.3.6' --user `whoami`
 fi
