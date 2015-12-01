23ef4ff1039b90574d0ee0160eace6420f800cfe
tornadomeet <wuweigreat@qq.com>
2015-12-01 08:54:13 +0800

    fix deconvolution workspace

--- src/operator/deconvolution-inl.h
+++ src/operator/deconvolution-inl.h
@@ -239,8 +239,10 @@ class DeconvolutionOp : public Operator {
                                      oshape[1] / param_.num_group,
                                      oshape[2] * oshape[3]);
     const uint64_t workspace_size = param_.workspace;
-    index_t nstep = std::max(std::min(static_cast<index_t>(workspace_size / shape_colunit_.Size()),
-                               ishape[0]), 1U);
+    index_t nstep = std::max(
+	std::min(static_cast<index_t>(workspace_size / shape_colunit_.Size() + shape_dstunit_.Size()),
+        ishape[0]), 
+	1U);
     int nop = (ishape[0] + nstep - 1) / nstep;
     nstep_ = (ishape[0] + nop - 1) / nop;
     if (nstep_ == nstep) {
