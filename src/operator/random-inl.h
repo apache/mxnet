dc841fb88093f566b820f42d07002ca3660f0498
Sebastian Nowozin <nowozin@gmail.com>
2015-12-03 21:24:11 +0000

    Use general target_shape instead of num_input.

--- src/operator/random-inl.h
+++ src/operator/random-inl.h
@@ -29,11 +29,11 @@ namespace mxnet {
 namespace op {
 
 struct RandomParam : public dmlc::Parameter<RandomParam> {
-  int num_output;
+  TShape target_shape;
   int random_type;
   DMLC_DECLARE_PARAMETER(RandomParam) {
-    DMLC_DECLARE_FIELD(num_output).set_range(1, 1 << 30)
-    .describe("Number of output elements");
+    DMLC_DECLARE_FIELD(target_shape)
+    .describe("Target shape");
     DMLC_DECLARE_FIELD(random_type)
     .add_enum("uniform", random_enum::kUniform)
     .add_enum("gaussian", random_enum::kGaussian)
@@ -103,31 +103,9 @@ class RandomProp : public OperatorProperty {
   bool InferShape(std::vector<TShape> *in_shape,
                   std::vector<TShape> *out_shape,
                   std::vector<TShape> *aux_shape) const override {
-    using namespace mshadow;
-    // XXX: how to do automatic shape inference here?
-    //
-    // Ideally we would like to avoid specifying the num_output value
-    // altogether and instead fill an arbitrary sized array.
-    // For example, the Random symbol may be useful to implement variational
-    // Bayes parameter learning and any parametrized layer knows the size of
-    // its parameter array.  It seems current shape inference is purely
-    // feedforward which does not allow us to "backpropagate" shape
-    // information from output.
-    //
-    // I would like to be able to write:
-    // if (out_shape->size() == 0) {
-    //   return false;
-    // } else {
-    //   // Nothing to do.
-    //   return true;
-    // }
-    //
-    // By making multiple passes over the symbolic graph all dependencies
-    // could be resolved if the next layer consuming the random output can
-    // provide shape information about its input.
-
-    SHAPE_ASSIGN_CHECK(*out_shape, random_enum::kOut, Shape1(param_.num_output));
-
+    // Automatic shape inference for operators without inputs is not supported
+    // yet, therefore the target_shape needs to be provided by the user.
+    SHAPE_ASSIGN_CHECK(*out_shape, random_enum::kOut, param_.target_shape);
     return true;
   }
 
