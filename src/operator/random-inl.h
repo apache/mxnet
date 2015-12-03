4402921d34c52ec344d438a32f4032eb5110de42
Sebastian Nowozin <nowozin@gmail.com>
2015-12-03 21:34:30 +0000

    Undo.

--- src/operator/random-inl.h
+++ src/operator/random-inl.h
@@ -45,7 +45,6 @@ template<typename xpu>
 class RandomOp : public Operator {
  public:
   explicit RandomOp(RandomParam param) {
-    this->target_shape = param.target_shape;
     this->random_type_ = param.random_type;
   }
 
