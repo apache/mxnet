15790ba0ad3f3ead8f0f6aa68a1226ca9772538f
terrytangyuan <terrytangyuan@gmail.com>
2015-12-20 21:16:41 -0600

    Documentation for LRScheduler

--- scala-package/core/src/main/scala/ml/dmlc/mxnet/LRScheduler.scala
+++ scala-package/core/src/main/scala/ml/dmlc/mxnet/LRScheduler.scala
@@ -2,24 +2,38 @@ package ml.dmlc.mxnet
 import org.slf4j.LoggerFactory
 
 /**
- * Class for learning rate scheduler, which adaptively changes the learning rate
+ * Learning rate scheduler, which adaptively changes the learning rate
  * based on the training progress.
  * @author Yuan Tang
  */
 
-
 abstract class LRScheduler(var baseLR: Float = 0.01) {
   /**
    * Base class of a learning rate scheduler
+   *
+   * The training progress is presented by `num_update`, which can be roughly
+   * viewed as the number of minibatches executed so far. Its value is
+   * non-decreasing, and increases at most by one.
+   *
+   * The exact value is the upper bound of the number of updates applied to
+   * a weight/index.
+   *
+   * @param numUpdate Int, the maximal number of updates applied to a weight.
    */
-
   def apply(numUpdate: Int): Unit
 }
 
+/**
+ * Class for reducing learning rate in factor
+ *
+ * Assume the weight has been updated by n times, then the learning rate will
+ * be base_lr * factor^^(floor(n/step))
+ *
+ * @param step Int, schedule learning rate after n updates
+ * @param factor Float, the factor for reducing the learning rate
+ *
+ */
 class FactorScheduler(var step: Int, var factor: Float) extends LRScheduler {
-  /**
-   * Class for reducing learning rate in factor
-   */
 
   var count: Int = 0
   private val logger = LoggerFactory.getLogger(classOf[FactorScheduler])
