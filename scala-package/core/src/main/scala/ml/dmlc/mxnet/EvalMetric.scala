78e32401a9931676ab60ecb58f475725c66e96e0
terrytangyuan <terrytangyuan@gmail.com>
2015-12-22 21:40:53 -0600

    Custom metrics and docummentation

--- scala-package/core/src/main/scala/ml/dmlc/mxnet/EvalMetric.scala
+++ scala-package/core/src/main/scala/ml/dmlc/mxnet/EvalMetric.scala
@@ -1,22 +1,44 @@
 package ml.dmlc.mxnet
 
+/**
+ * Base class of all evaluation metrics
+ * @param name Metric name
+ */
 abstract class EvalMetric(protected val name: String) {
 
   protected var numInst: Int = 0
   protected var sumMetric: Float = 0.0f
 
+  /**
+   * Update the internal evaluation.
+   *
+   * @param labels The labels of the data
+   * @param preds Predicted values.
+   */
   def update(labels: NDArray, preds: NDArray): Unit
 
+  /**
+   * Clear the internal statistics to initial state.
+   */
   def reset: Unit = {
     this.numInst = 0
     this.sumMetric = 0.0f
   }
 
+  /**
+   * Get the current evaluation result.
+   * @return name, Name of the metric
+   *         value, Value of the evaluation
+   */
   def get: (String, Float) = {
     (this.name, this.sumMetric / this.numInst)
   }
 }
 
+
+/**
+ * Calculate accuracy
+ */
 class Accuracy extends EvalMetric("accuracy") {
   def update(labels: NDArray, preds: NDArray): Unit = {
 
@@ -31,6 +53,10 @@ class Accuracy extends EvalMetric("accuracy") {
   }
 }
 
+
+/**
+ * Calculate Mean Absolute Error loss
+ */
 class MAE extends EvalMetric("mae") {
   def update(labels: NDArray, preds: NDArray): Unit = {
 
@@ -43,6 +69,9 @@ class MAE extends EvalMetric("mae") {
 }
 
 
+/**
+ * Calculate Root Mean Squred Error loss
+ */
 class RMSE extends EvalMetric("rmse") {
   def update(labels: NDArray, preds: NDArray): Unit = {
 
@@ -55,6 +84,20 @@ class RMSE extends EvalMetric("rmse") {
 }
 
 
+/**
+ * Custom evaluation metric that takes a NDArray function.
+ * @param fEval Customized evaluation function.
+ */
+class CustomMetric(var fEval: () => Unit, var name: String) extends EvalMetric(name) {
+  def update(labels: NDArray, preds: NDArray): Unit = {
+
+    require(labels.size == preds.size, "labels and predictions should have the same length.")
+
+    for ( (label, pred) <- (labels.toArray zip preds.toArray)) {
+
+    }
+  }
+}
 
 
 
