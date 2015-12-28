84e00fde1214f15a7ac89230a13eb2a064808a0c
yanqingmen <toofooltosay@gmail.com>
2015-12-28 21:52:35 +0800

    modify linux settings

--- scala-package/core/get_data.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "./data" ]; then
-  mkdir -p ./data
-fi
-if [ ! -d "./data/mnist.zip" ]; then
-    wget http://webdocs.cs.ualberta.ca/~bx3/data/mnist.zip -P data/
-fi
-cd data
-rm *-ubyte
-unzip -u mnist.zip
--- scala-package/core/src/test/scala/ml/dmlc/mxnet/IOSuite.scala
+++ scala-package/core/src/test/scala/ml/dmlc/mxnet/IOSuite.scala
@@ -47,21 +47,24 @@ class IOSuite extends FunSuite with BeforeAndAfterAll {
 
 
   /**
-    * link error
+    * not work now
     */
 //  test("test ImageRecordIter") {
+//    //get data
+//    "./scripts/get_cifar_data.sh" !
+//
 //    val params = Map(
 //      "path_imgrec" -> "data/cifar/train.rec",
 //      "mean_img" -> "data/cifar/cifar10_mean.bin",
-//      "rand_crop" -> "false",
-//      "and_mirror" -> "false",
-//      "shuffle" -> "false",
+//      "rand_crop" -> "False",
+//      "and_mirror" -> "False",
+//      "shuffle" -> "False",
 //      "data_shape" -> "(3,28,28)",
 //      "batch_size" -> "100",
 //      "preprocess_threads" -> "4",
 //      "prefetch_buffer" -> "1"
 //    )
-//    val img_iter = IO.iterCreateFuncs("ImageRecordIter")(params)
+//    val img_iter = IO.createIterator("ImageRecordIter", params)
 //    img_iter.reset()
 //    while(img_iter.iterNext()) {
 //      val batch = img_iter.next()
