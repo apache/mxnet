95751566ccc3129c2f8852871ee715338cee1f41
terrytangyuan <terrytangyuan@gmail.com>
2015-12-27 20:15:04 -0600

    Constructor for Initializer and handle exception

--- scala-package/core/src/main/scala/ml/dmlc/mxnet/Initializer.scala
+++ scala-package/core/src/main/scala/ml/dmlc/mxnet/Initializer.scala
@@ -2,10 +2,37 @@ package ml.dmlc.mxnet
 
 import ml.dmlc.mxnet.NDArray.{array, zeros, ones}
 
+
 /**
- * Created by yuantang on 12/27/15.
+ *
+ * Base class for Initializer.
+ *
+ * @author Yuan Tang
+ *
+ * @param name name of corrosponding ndarray
+ * @param arr ndarray to be Initialized
  */
 abstract class Initializer(protected val name: String, protected var arr: NDArray) {
+
+  if (name.startsWith("upsampling")) {
+    _initBilinear()
+  } else if (name.endsWith("bias")) {
+    _initBias()
+  } else if (name.endsWith("gamma")) {
+    _initGamma()
+  } else if (name.endsWith("beta")) {
+    _initBeta()
+  } else if (name.endsWith("weight")) {
+    _initWeight()
+  } else if (name.endsWith("moving_mean")) {
+    _initZero()
+  } else if (name.endsWith("moving_var")) {
+    _initZero()
+  } else if (name.endsWith("moving_avg")) {
+    _initZero()
+  } else {
+    throw new IllegalArgumentException(s"Unknown initialization pattern for ${name}.")
+  }
 
   def _initBilinear() = {
     var weight = Array.fill[Float](arr.size)(0.0f)
@@ -20,7 +47,6 @@ abstract class Initializer(protected val name: String, protected var arr: NDArra
     }
 
     arr = array(weight)
-
   }
 
   def _initZero() = {
@@ -40,15 +66,30 @@ abstract class Initializer(protected val name: String, protected var arr: NDArra
   }
 
   def _initWeight()
-
 }
 
+
+/**
+ * Initialize the weight with uniform [-scale, scale]
+ *
+ * @param name name of corrosponding ndarray
+ * @param arr ndarray to be Initialized
+ * @param scale The scale of uniform distribution
+ */
 class Uniform(name: String, arr: NDArray, protected val scale: Float=0.07f) extends Initializer(name: String, arr: NDArray) {
   def _initWeight() = {
     Random.uniform(-scale, scale, out=arr)
   }
 }
 
+
+/**
+ * Initialize the weight with normal(0, sigma)
+ *
+ * @param name name of corrosponding ndarray
+ * @param arr ndarray to be Initialized
+ * @param sigma Standard deviation for gaussian distribution.
+ */
 class Normal(name: String, arr: NDArray, protected val sigma: Float=0.01f) extends Initializer(name: String, arr: NDArray) {
   def _initWeight() = {
     Random.normal(0, sigma, out=arr)
@@ -56,6 +97,15 @@ class Normal(name: String, arr: NDArray, protected val sigma: Float=0.01f) exten
 }
 
 
+/**
+ * Initialize the weight with Xavier or similar initialization scheme.
+ *
+ * @param name name of corrosponding ndarray
+ * @param arr ndarray to be Initialized
+ * @param rndType Options are: "gaussian" or "uniform"
+ * @param factorType Options are: "avg", "in", "out"
+ * @param magnitude scale of random number range
+ */
 class Xavier(name: String, arr: NDArray, protected val rndType: String ="uniform",
              protected val factorType: String ="avg", protected val magnitude: Int = 3)
   extends Initializer(name: String, arr: NDArray) {
