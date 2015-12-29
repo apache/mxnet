1d988d5a454ea9b10ff83200793eab6bc8981c18
Yuan (Terry) Tang <terrytangyuan@users.noreply.github.com>
2015-12-28 18:32:11 -0600

    Added note in error_detector.sh

--- tests/travis/error_detector.sh
+++ tests/travis/error_detector.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# This script is mainly used to catch errors from maven results for Scala
+# to avoid false positives/negatives from Travis's interpretation. 
+
 file=scala_test_results.txt
 
 testFail=$(grep -ci "All tests passed" $file)
+
 if [ "$testFail" == "0" ]; then
+  # print results if anything fails
   cat $file
   echo "Some unit tests failed. "
   exit 1
