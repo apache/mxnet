120dd4b78154a80d64d5e019969f96476176a6f5
qiao hai-jun <qiaohaijun@users.noreply.github.com>
2016-01-12 22:35:46 +0800

    minor fix network choice

--- example/image-classification/train_imagenet.py
+++ example/image-classification/train_imagenet.py
@@ -7,7 +7,7 @@ import train_model
 # don't use -n and -s, which are resevered for the distributed training
 parser = argparse.ArgumentParser(description='train an image classifer on imagenet')
 parser.add_argument('--network', type=str, default='inception-bn',
-                    choices = ['alexnet', 'vgg', 'googlenet', 'inception-bn', 'inception-bn-full.py'],
+                    choices = ['alexnet', 'vgg', 'googlenet', 'inception-bn', 'inception-bn-full'],
                     help = 'the cnn to use')
 parser.add_argument('--data-dir', type=str, required=True,
                     help='the input data directory')
