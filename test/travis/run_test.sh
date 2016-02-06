f277cd7598426295d0812f21b8b33e56e7c34234
Chiyuan Zhang <pluskid@gmail.com>
2016-02-06 12:27:32 -0500

    revert last commit (not useful)

--- test/travis/run_test.sh
+++ test/travis/run_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 if [[ -a .git/shallow ]]; then git fetch --unshallow; fi
-julia -e 'Pkg.rm("MXNet")' # in case Jenkins CI did not remove existing files
 julia -e 'Pkg.clone(pwd()); Pkg.build("MXNet"); Pkg.test("MXNet"; coverage=true)'
