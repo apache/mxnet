175d399d7a3f26d1b654638be59ebacec2c6855c
Chiyuan Zhang <pluskid@gmail.com>
2016-02-06 12:21:52 -0500

    workaround for jenkins CI

--- test/travis/run_test.sh
+++ test/travis/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 if [[ -a .git/shallow ]]; then git fetch --unshallow; fi
+julia -e 'Pkg.rm("MXNet")' # in case Jenkins CI did not remove existing files
 julia -e 'Pkg.clone(pwd()); Pkg.build("MXNet"); Pkg.test("MXNet"; coverage=true)'
