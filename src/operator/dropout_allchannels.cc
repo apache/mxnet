83bd14ec6846156619c9b09397332b08fdee5c70
kadeng <Kai.Londenberg@googlemail.com>
2016-02-16 14:49:57 +0100

    Resolved lint warnings

--- src/operator/dropout_allchannels.cc
+++ src/operator/dropout_allchannels.cc
@@ -22,8 +22,10 @@ Operator *DropoutAllchannelsProp::CreateOperator(Context ctx) const {
 DMLC_REGISTER_PARAMETER(DropoutAllchannelsParam);
 
 MXNET_REGISTER_OP_PROPERTY(DropoutAllchannels, DropoutAllchannelsProp)
-.describe("Apply dropout on a batch of image or image-like inputs, always dropping or keeping all channels of a given pixel.")
-.add_argument("data", "Symbol", "Input data to apply dropout to. Has to be a 4D Tensor with shape (samples, channels, rows, cols)")
+.describe("Apply dropout on a batch of image or image-like inputs, always dropping or keeping " \
+          "all channels of a given pixel.")
+.add_argument("data", "Symbol", "Input data to apply dropout to. Has to be a 4D Tensor with " \
+                                "shape (samples, channels, rows, cols)")
 .add_arguments(DropoutAllchannelsParam::__FIELDS__());
 
 }  // namespace op
