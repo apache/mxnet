8681ad3dfcefb7b86eff224eceaf89ee9cd72cbb
Ldpe2G <liangdepeng@gmail.com>
2016-04-13 09:16:52 +0800

    fix run_neuralstyle.sh

--- scala-package/examples/scripts/run_neuralstyle.sh
+++ scala-package/examples/scripts/run_neuralstyle.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MXNET_ROOT=/home/ldpe2g/Mxnet/mxnet_scala/mxnet/mxnet
+MXNET_ROOT=$(cd "$(dirname $0)/../../.."; pwd)
 CLASS_PATH=$MXNET_ROOT/scala-package/assembly/linux-x86_64-gpu/target/*:$MXNET_ROOT/scala-package/examples/target/*:$MXNET_ROOT/scala-package/examples/target/classes/lib/*
-INPUT_IMG=$MXNET_ROOT/example/neural-style/input/IMG_4343.jpg 
-STYLE_IMG=$MXNET_ROOT/example/neural-style/input/starry_night.jpg 
+INPUT_IMG=$1 
+STYLE_IMG=$2 
 MODEL_PATH=$MXNET_ROOT/example/neural-style/model/vgg19.params
 OUTPUT_DIR=$MXNET_ROOT/example/neural-style/output
 
