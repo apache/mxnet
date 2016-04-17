dd16c195fa3743bab84ec5b12d3066a805f118de
Chiyuan Zhang <pluskid@gmail.com>
2016-04-17 19:04:29 -0400

    differentiate different modules in the same file

--- docs/_static/js/auto_module_index.js
+++ docs/_static/js/auto_module_index.js
@@ -1,25 +1,25 @@
 function auto_index(module) {
   $(document).ready(function () {
     // find all classes or functions
-    var targets = $("dl[class='class'] > dt,dl[class='function'] > dt");
-    for (var i = 0; i < targets.length; ++i) {
-      console.log($(targets[i]).attr('id'));
-    }
+    var div_query = "div[class='section'][id='module-" + module + "']";
+    var class_query = div_query + " dl[class='class'] > dt";
+    var func_query = div_query + " dl[class='function'] > dt";
+    var targets = $(class_query + ',' + func_query);
 
     var li_node = $("li a[href='#module-" + module + "']").parent();
     var html = "<ul>";
 
     for (var i = 0; i < targets.length; ++i) {
       var id = $(targets[i]).attr('id');
-      id = id.replace(/^mxnet\./, ''); // remove 'mxnet.' prefix to make menus shorter
+      // remove 'mxnet.' prefix to make menus shorter
+      var id_simple = id.replace(/^mxnet\./, '');
       html += "<li><a class='reference internal' href='#";
       html += id;
-      html += "'>" + id + "</a></li>";
+      html += "'>" + id_simple + "</a></li>";
     }
 
     html += "</ul>";
     li_node.append(html);
-    console.log(module);
   });
 }
 
