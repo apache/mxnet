1c64b1193cab0ea0ca3e53ad978e089259b248cb
Chiyuan Zhang <pluskid@gmail.com>
2016-04-18 11:21:22 -0400

    more detailed doc for embedding layer (#892)

--- src/operator/embedding.cc
+++ src/operator/embedding.cc
@@ -20,7 +20,9 @@ Operator* EmbeddingProp::CreateOperator(Context ctx) const {
 DMLC_REGISTER_PARAMETER(EmbeddingParam);
 
 MXNET_REGISTER_OP_PROPERTY(Embedding, EmbeddingProp)
-.describe("Get embedding for one-hot input")
+.describe("Get embedding for one-hot input. A n-dimensional input tensor will "
+"be trainsformed into a (n+1)-dimensional tensor, where a new dimension is "
+"added for the embedding results.")
 .add_argument("data", "Symbol", "Input data to the EmbeddingOp.")
 .add_argument("weight", "Symbol", "Enbedding weight matrix.")
 .add_arguments(EmbeddingParam::__FIELDS__());
