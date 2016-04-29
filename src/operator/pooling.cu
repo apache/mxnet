62b54526409b03cf006a27241c1b9b0cdbeb0a24
Godricly <godricly_li@126.com>
2016-04-29 14:37:11 +0800

    Update pooling.cu

--- src/operator/pooling.cu
+++ src/operator/pooling.cu
@@ -15,7 +15,17 @@ namespace op {
 template<>
 Operator *CreateOp<gpu>(PoolingParam param) {
 #if MXNET_USE_CUDNN == 1
-  return new CuDNNPoolingOp(param);
+  switch (param.pool_type) {
+    case pool_enum::kMaxPooling:
+      return new CuDNNPoolingOp(param);
+    case pool_enum::kAvgPooling:
+      return new CuDNNPoolingOp(param);
+    case pool_enum::kSumPooling:
+      return new PoolingOp<gpu, mshadow::red::sum>(param);
+    default:
+      LOG(FATAL) << "unknown activation type";
+      return NULL;
+  }
 #else
   switch (param.pool_type) {
     case pool_enum::kMaxPooling:
