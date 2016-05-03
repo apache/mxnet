00a7d0652a4027a8941d55191a7b127ad334a216
muli <muli@cs.cmu.edu>
2016-05-03 14:23:50 -0700

    [op] add broadcast plus
    [op] bugfix with scalar tensor
    
    add broadcast plus to sym and unittest
    
    [op] add backward and test
    
    fix lint
    
    [op] add BroadcastPlus, BroadcastMinus, BroadcastMul
    
    Remove the stacking if
    
    Fix comment
    
    Update test
    
    Revise code comment
    
    Use the same name for NDArray/Symbolic
    
    Update Mshadow
    
    Fix lint
    
    Replace `keepdims` with `reshape`
    
    Replace tuple in axis to a for loop

---
+++ src/operator/elementwise_binary_broadcast_op.cu
@@ -0,0 +1,6 @@
+/*!
+ * Copyright (c) 2015 by Contributors
+ * \file elementwise_binary_broadcast_op.cu
+ * \brief elementwise binary broadcast operator
+*/
+#include "./elementwise_binary_broadcast_op-inl.h"
