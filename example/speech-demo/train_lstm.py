9e242bc9000da1bfcefbd0b0d7169759bc29e809
Chiyuan Zhang <pluskid@gmail.com>
2016-05-05 16:54:49 -0400

    a separate config_util module

--- example/speech-demo/train_lstm.py
+++ example/speech-demo/train_lstm.py
@@ -4,56 +4,18 @@ sys.path.insert(0, "../../python")
 import time
 import logging
 import os.path
-import argparse
-
-if sys.version_info >= (3, 0):
-    import configparser
-else:
-    import ConfigParser as configparser
 
 import mxnet as mx
 import numpy as np
 
 from lstm import lstm_unroll
 from io_util import BucketSentenceIter, TruncatedSentenceIter, DataReadStream
+from config_util import parse_args, get_checkpoint_path, parse_contexts
+
 
 # some constants
 METHOD_BUCKETING = 'bucketing'
 METHOD_TBPTT = 'truncated-bptt'
-
-def parse_args():
-    default_cfg = configparser.ConfigParser()
-    default_cfg.read(os.path.join(os.path.dirname(__file__), 'default.cfg'))
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--configfile", help="config file for training parameters")
-
-    # those allow us to overwrite the configs through command line
-    for sec in default_cfg.sections():
-        for name, _ in default_cfg.items(sec):
-            arg_name = '--%s_%s' % (sec, name)
-            doc = 'Overwrite %s in section [%s] of config file' % (name, sec)
-            parser.add_argument(arg_name, help=doc)
-
-    args = parser.parse_args()
-
-    if args.configfile is not None:
-        # now read the user supplied config file to overwrite some values
-        default_cfg.read(args.configfile)
-
-    # now overwrite config from command line options
-    for sec in default_cfg.sections():
-        for name, _ in default_cfg.items(sec):
-            arg_name = ('%s_%s' % (sec, name)).replace('-', '_')
-            if hasattr(args, arg_name) and getattr(args, arg_name) is not None:
-                sys.stderr.write('!! CMDLine overwriting %s.%s:\n' % (sec, name))
-                sys.stderr.write("    '%s' => '%s'\n" % (default_cfg.get(sec, name),
-                                            getattr(args, arg_name)))
-                default_cfg.set(sec, name, getattr(args, arg_name))
-
-    args.config = default_cfg
-    sys.stderr.write("="*80+"\n")
-    return args
 
 def prepare_data(args):
     batch_size = args.config.getint('train', 'batch_size')
@@ -88,12 +50,6 @@ def prepare_data(args):
     dev_sets = DataReadStream(dev_data_args, feat_dim)
 
     return (init_states, train_sets, dev_sets)
-
-def get_checkpoint_path(args):
-    prefix = args.config.get('train', 'prefix')
-    if os.path.isabs(prefix):
-        return prefix
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), 'checkpoints', prefix))
 
 def Acc_exclude_padding(labels, preds):
     labels = labels.T.reshape((-1,))
@@ -136,12 +92,14 @@ def score_with_state_forwarding(module, eval_data, eval_metric):
         for i in range(1, len(outputs)):
             outputs[i].copyto(eval_data.init_state_arrays[i-1])
 
+
 def get_initializer(args):
     init_type = getattr(mx.initializer, args.config.get('train', 'initializer'))
     init_scale = args.config.getfloat('train', 'init_scale')
     if init_type is mx.initializer.Xavier:
         return mx.initializer.Xavier(magnitude=init_scale)
     return init_type(init_scale)
+
 
 def do_training(training_method, args, module, data_train, data_val):
     from distutils.dir_util import mkpath
@@ -251,14 +209,7 @@ if __name__ == '__main__':
     args.config.write(sys.stdout)
 
     training_method = args.config.get('train', 'method')
-
-    # parse context into Context objects
-    contexts = re.split(r'\W+', args.config.get('train', 'context'))
-    for i, ctx in enumerate(contexts):
-        if ctx[:3] == 'gpu':
-            contexts[i] = mx.context.gpu(int(ctx[3:]))
-        else:
-            contexts[i] = mx.context.cpu(int(ctx[3:]))
+    contexts = parse_contexts(args)
 
     init_states, train_sets, dev_sets = prepare_data(args)
     state_names = [x[0] for x in init_states]
