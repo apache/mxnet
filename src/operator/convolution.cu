34beff3507d3b54e829319e7ddc41001d3f6cfa6
Kai Londenberg <K.Londenberg@searchmetrics.com>
2016-05-08 21:06:10 +0200

    Fixed indentation

--- src/operator/convolution.cu
+++ src/operator/convolution.cu
@@ -17,14 +17,14 @@ Operator* CreateOp<gpu>(ConvolutionParam param, int dtype) {
   Operator *op = NULL;
 #if MXNET_USE_CUDNN == 1
   if (param.dilate[0]==1 && param.dilate[1]==1) {
-	  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
-	    op = new CuDNNConvolutionOp<DType>(param);
-	  })
+    MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
+      op = new CuDNNConvolutionOp<DType>(param);
+    })
   } else {
-	  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
-	    op = new ConvolutionOp<gpu, DType>(param);
-	  })
- }
+    MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
+      op = new ConvolutionOp<gpu, DType>(param);
+    })
+  }
 #else
   MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
     op = new ConvolutionOp<gpu, DType>(param);
