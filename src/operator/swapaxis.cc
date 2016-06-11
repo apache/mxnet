4605518c3cb129ff51ffa55493c31e61cc14b402
Valentin Churavy <v.churavy@gmail.com>
2016-06-11 10:46:25 +0900

    enable other DTypes in SwapAxis

--- src/operator/swapaxis.cc
+++ src/operator/swapaxis.cc
@@ -11,12 +11,21 @@ namespace mxnet {
 namespace op {
 
 template<>
-Operator* CreateOp<cpu>(SwapAxisParam param) {
-  return new SwapAxisOp<cpu>(param);
+Operator* CreateOp<cpu>(SwapAxisParam param, int dtype) {
+  Operator *op = NULL;
+  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
+    op = new SwapAxisOp<cpu, DType>(param);
+  });
+  return op;
 }
 
-Operator* SwapAxisProp::CreateOperator(Context ctx) const {
-  DO_BIND_DISPATCH(CreateOp, param_);
+Operator* SwapAxisProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
+                                         std::vector<int> *in_type) const {
+  std::vector<TShape> out_shape, aux_shape;
+  std::vector<int> out_type, aux_type;
+  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
+  CHECK(InferType(in_type, &out_type, &aux_type));
+  DO_BIND_DISPATCH(CreateOp, param_, in_type->at(0));
 }
 
 
--- tests/python/gpu/test_operator_gpu.py
+++ tests/python/gpu/test_operator_gpu.py
@@ -123,6 +123,15 @@ def test_blockgrad_with_type():
                 {'ctx': mx.cpu(0), 'bg_data': (2, 2, 2, 10), 'type_dict': {'bg_data': np.float32}}]
     check_consistency(sym, ctx_list)
 
+def test_swapaxis_with_type():
+    sym = mx.sym.SwapAxis(name='swap', dim1=1)
+    ctx_list = [{'ctx': mx.gpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float64}},
+                {'ctx': mx.gpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float32}},
+                {'ctx': mx.gpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float16}},
+                {'ctx': mx.cpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float64}},
+                {'ctx': mx.cpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float32}}]
+    check_consistency(sym, ctx_list)
+
 def test_fullyconnected_with_type():
     sym = mx.sym.FullyConnected(num_hidden=3, name='inner')
     ctx_list = [{'ctx': mx.gpu(0), 'inner_data': (2, 10), 'type_dict': {'inner_data': np.float64}},
@@ -149,6 +158,7 @@ if __name__ == '__main__':
     test_concat_with_type()
     test_reshape_with_type()
     test_blockgrad_with_type()
+    test_swapaxis_with_type()
     test_fullyconnected_with_type()
     test_activation_with_type()
     #test_softmax_with_shape((3,4), mx.gpu())
