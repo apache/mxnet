f58b548345379dc078ea015cfd1cefd136fbee0f
seanlinx <515364970@qq.com>
2016-06-16 00:06:46 +0800

    Revert "Enable training fast rcnn with multiple GPUs (#2358)"
    This reverts commit 004c2383a623496b319753c735bf04fd94f15874.

--- example/rcnn/train.py
+++ example/rcnn/train.py
@@ -7,7 +7,7 @@ from tools.train_net import train_net
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
     parser.add_argument('--image_set', dest='image_set', help='can be trainval or train',
-                        default='trainval', type=str)
+                        default='train', type=str)
     parser.add_argument('--year', dest='year', help='can be 2007, 2010, 2012',
                         default='2007', type=str)
     parser.add_argument('--root_path', dest='root_path', help='output data folder',
@@ -20,23 +20,19 @@ def parse_args():
                         default=1, type=int)
     parser.add_argument('--prefix', dest='prefix', help='new model prefix',
                         default=os.path.join(os.getcwd(), 'model', 'frcnn'), type=str)
-    parser.add_argument('--gpus', dest='gpu_ids', help='GPU device to train with',
-                        default='0', type=str)
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device to train with',
+                        default=0, type=int)
     parser.add_argument('--begin_epoch', dest='begin_epoch', help='begin epoch of training',
                         default=0, type=int)
     parser.add_argument('--end_epoch', dest='end_epoch', help='end epoch of training',
                         default=8, type=int)
     parser.add_argument('--frequent', dest='frequent', help='frequency of logging',
                         default=20, type=int)
-    parser.add_argument('--kv_store', dest='kv_store', help='the kv-store type',
-                        default='local', type=str)
-    parser.add_argument('--work_load_list', dest='work_load_list', help='work load for different devices',
-                        default=None, type=list)
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = parse_args()
-    ctx = [mx.gpu(int(i)) for i in args.gpu_ids.split(',')]
+    ctx = mx.gpu(args.gpu_id)
     train_net(args.image_set, args.year, args.root_path, args.devkit_path, args.pretrained, args.epoch,
-              args.prefix, ctx, args.begin_epoch, args.end_epoch, args.frequent, args.kv_store, args.work_load_list)
+              args.prefix, ctx, args.begin_epoch, args.end_epoch, args.frequent)
