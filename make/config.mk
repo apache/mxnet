872a16cc29b11b53b201db45e77d03159f29d3aa
xlvector <xlvector@gmail.com>
2016-06-17 21:04:28 +0800

    add warpctc config to config.mk

--- make/config.mk
+++ make/config.mk
@@ -116,6 +116,9 @@ EXTRA_OPERATORS =
 # TORCH_PATH = $(HOME)/torch
 # MXNET_PLUGINS += plugin/torch/torch.mk
 
+# WARPCTC_PATH = $(HOME)/warp-ctc
+# MXNET_PLUGINS += plugin/warpctc/warpctc.mk
+
 # whether to use sframe integration. This requires build sframe
 # git@github.com:dato-code/SFrame.git
 # SFRAME_PATH = $(HOME)/SFrame
