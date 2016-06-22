9def5e1441dfb2821071edd2a7c08c25793014a8
Godricly <godricly.li@gmail.com>
2016-06-22 16:56:18 +0800

    embedding for DType
    embedding for DType
    need mshadow pull #136 to be merged

--- src/operator/embedding.cc
+++ src/operator/embedding.cc
@@ -1,4 +1,4 @@
-/*!
+﻿/*!
  * Copyright (c) 2015 by Contributors
  * \file embedding.cc
  * \brief
@@ -9,12 +9,32 @@
 namespace mxnet {
 namespace op {
 template<>
-Operator* CreateOp<cpu>(EmbeddingParam param) {
-  return new EmbeddingOp<cpu>(param);
+Operator* CreateOp<cpu>(EmbeddingParam param, int dtype) {
+  Operator *op = NULL;
+  switch (dtype) {
+  case mshadow::kFloat32:
+    op = new EmbeddingOp<cpu, float>(param);
+    break;
+  case mshadow::kFloat64:
+    op = new EmbeddingOp<cpu, double>(param);
+    break;
+  case mshadow::kFloat16:
+    op = new EmbeddingOp<cpu, mshadow::half::half_t>(param);
+    break;
+  default:
+    LOG(FATAL) << "Unsupported type " << dtype;
+  }
+  return op;
 }
 
-Operator* EmbeddingProp::CreateOperator(Context ctx) const {
-  DO_BIND_DISPATCH(CreateOp, param_);
+// DO_BIND_DISPATCH comes from operator_common.h
+Operator *EmbeddingProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
+                                     std::vector<int> *in_type) const {
+  std::vector<TShape> out_shape, aux_shape;
+  std::vector<int> out_type, aux_type;
+  CHECK(InferType(in_type, &out_type, &aux_type));
+  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
+  DO_BIND_DISPATCH(CreateOp, param_, (*in_type)[0]);
 }
 
 DMLC_REGISTER_PARAMETER(EmbeddingParam);
