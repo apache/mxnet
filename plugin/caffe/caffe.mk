703c13cef8b0dc14d83bed4d792b886008b81877
haoran <hrwangchengdu@hotmail.com>
2016-07-09 17:55:27 +0800

    [caffe-plugin] rewrite makefile

--- plugin/caffe/caffe.mk
+++ plugin/caffe/caffe.mk
@@ -1,25 +1,5 @@
-#include $(CAFFE_PATH)/make/caffe.mk
-
-# use boost
-CAFFE_LDFLAGS += -lboost_system -lboost_thread -lboost_filesystem
-
-# use gflags
-CAFFE_LDFLAGS += -lgflags
-
-# use glogs
-CAFFE_LDFLAGS += -lglog
-
-# use protobuf
-CAFFE_LDFLAGS += -lprotobuf
-
-CFLAGS += -I$(CAFFE_PATH)/include
-CFLAGS += -I$(CAFFE_PATH)/build/src
-LDFLAGS += $(CAFFE_LDFLAGS)
-
-LIB_DEP += $(CAFFE_PATH)/build/lib/libcaffe.a
-
-#$(CAFFE_PATH)/libcaffe.a:
-# + cd $(CAFFE_PATH); make libcaffe.a config=$(ROOTDIR)/$(config); cd $(ROOTDIR)
+CFLAGS += -I$(CAFFE_PATH)/include -I$(CAFFE_PATH)/build/src
+LDFLAGS += -lprotobuf -lboost_system -lboost_thread -lboost_filesystem -lgflags -lglog -L$(CAFFE_PATH)/build/lib -lcaffe
 
 CAFFE_SRC = $(wildcard plugin/caffe/*.cc)
 PLUGIN_OBJ += $(patsubst %.cc, build/%.o, $(CAFFE_SRC))
