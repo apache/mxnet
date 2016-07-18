3cd117239927d8b933a92360173a1a2bf2bf6992
ziyeqinghan <ziyeqinghan@gmail.com>
2016-07-18 10:07:02 +0800

    Add RMSProp optimizer in R

--- R-package/R/optimizer.R
+++ R-package/R/optimizer.R
@@ -66,6 +66,89 @@ mx.opt.sgd <- function(learning.rate,
   return(list(create.state=create.state, update=update))
 }
 
+#' Create an RMSProp optimizer with respective parameters.
+#' Reference: Tieleman T, Hinton G. Lecture 6.5-rmsprop: Divide the gradient by a running average of its recent magnitude[J]. COURSERA: Neural Networks for Machine Learning, 2012, 4(2).
+#' The code follows: http://arxiv.org/pdf/1308.0850v5.pdf Eq(38) - Eq(45) by Alex Graves, 2013.
+#' 
+#' @param learning.rate float, default=0.002
+#'      Step size.
+#' @param gamma1 float, default=0.95
+#'      decay factor of moving average for gradient, gradient^2.
+#' @param gamm2 float, default=0.9
+#'      "momentum" factor.
+#' @param wd float, default=0.0
+#'      L2 regularization coefficient add to all the weights.
+#' @param rescale.grad float, default=1.0
+#'      rescaling factor of gradient.
+#' @param clip_gradient float, optional
+#'      clip gradient in range [-clip_gradient, clip_gradient].
+#' @param lr_scheduler function, optional
+#'      The learning rate scheduler.
+#'
+mx.opt.rmsprop <- function(learning.rate=0.002,
+                           gamma1=0.95,
+                           gamma2=0.9,
+                           wd=0,
+                           rescale.grad=1,
+                           clip_gradient = NULL, 
+                           lr_scheduler = NULL) {
+  # use lr as short for learing rate.
+  lr <- learning.rate
+  count       <- 0
+  num_update  <- 0
+
+  rmsprop <- new.env()
+  rmsprop$lr <- lr
+  rmsprop$count <- 0
+  rmsprop$num_update <- 0
+
+  create.state <- function(index, weight) {
+      return (list(n=mx.nd.zeros(dim(weight), ctx(weight)),
+                   g=mx.nd.zeros(dim(weight), ctx(weight)),
+                   delta=mx.nd.zeros(dim(weight), ctx(weight))))
+  }
+
+  update <- function(index, weight, grad, state) {
+    if (!is.null(lr_scheduler)){
+      lr_scheduler(rmsprop) ## changing lr
+      lr <- rmsprop$lr
+      ## update count
+      indexKey <- paste0('ik', index)
+      if (!exists(envir = rmsprop, x = indexKey)){
+        assign(x = indexKey, value = 0, envir = rmsprop)
+      } else {
+        indexValue <- get(envir = rmsprop, x = indexKey)
+        assign(x = indexKey, value = indexValue + 1, envir = rmsprop)
+        rmsprop$num_update <- max(rmsprop$num_update, get(envir = rmsprop, x = indexKey))
+      }
+    }
+    grad <- grad * rescale.grad
+    if (!is.null(clip_gradient)){
+      if(clip_gradient >= 0){
+          grad_ctx <- ctx(grad)
+          grad <- as.array(grad)
+          grad <- pmax(grad, -1 * clip_gradient)
+          grad <- pmin(grad, clip_gradient)
+          grad <- mx.nd.array(grad, grad_ctx)
+      } else {
+        stop("Error: clip_gradient should be positive number.")
+      }
+    }
+
+    n <- state$n
+    g <- state$g
+    delta <- state$delta
+    n <- gamma1 * n + (1 - gamma1) * (grad * grad)
+    g <- gamma1 * g + (1 - gamma1) * grad
+    delta <- gamma2 * delta - lr * (grad / mx.nd.sqrt(n - g*g + 1e-4) + wd * weight)
+    weight <- weight + delta
+    state <- list(n=n, g=g, delta=delta)
+
+    return(list(weight=weight, state=state))
+  }
+  return(list(create.state=create.state, update=update))
+}
+
 #' Create an optimizer by name and parameters
 #'
 #' @param name The name of the optimizer
@@ -75,6 +158,9 @@ mx.opt.sgd <- function(learning.rate,
 mx.opt.create <- function(name, ...) {
   if (name == "sgd") {
     return(mx.opt.sgd(...))
+  }
+  else if (name == "rmsprop") {
+    return (mx.opt.rmsprop(...))
   }
   stop(paste("Unknown optimizer ", name))
 }
