6b7f912e57b4d5daa45ed8b7e6873fbf4bf3550d
Jian Guo <guojian710@gmail.com>
2016-07-20 19:33:26 +0800

    update rcnn example to Faster R-CNN

--- example/rcnn/helper/processing/bbox_transform.py
+++ example/rcnn/helper/processing/bbox_transform.py
@@ -1,8 +1,35 @@
 """
-This file has functions about bounding box post processing.
+This file has functions about bounding box processing.
 """
 
 import numpy as np
+
+
+def bbox_transform(ex_rois, gt_rois):
+    """
+    compute bounding box regression targets from ex_rois to gt_rois
+    :param ex_rois: [N, 4]
+    :param gt_rois: [N, 4]
+    :return: [N, 4]
+    """
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_ctr_x = ex_rois[:, 0] + 0.5 * (ex_widths - 1.0)
+    ex_ctr_y = ex_rois[:, 1] + 0.5 * (ex_heights - 1.0)
+
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_ctr_x = gt_rois[:, 0] + 0.5 * (gt_widths - 1.0)
+    gt_ctr_y = gt_rois[:, 1] + 0.5 * (gt_heights - 1.0)
+
+    targets_dx = (gt_ctr_x - ex_ctr_x) / (ex_widths + 1e-14)
+    targets_dy = (gt_ctr_y - ex_ctr_y) / (ex_heights + 1e-14)
+    targets_dw = np.log(gt_widths / ex_widths)
+    targets_dh = np.log(gt_heights / ex_heights)
+
+    targets = np.vstack(
+        (targets_dx, targets_dy, targets_dw, targets_dh)).transpose()
+    return targets
 
 
 def bbox_pred(boxes, box_deltas):
@@ -17,10 +44,10 @@ def bbox_pred(boxes, box_deltas):
         return np.zeros((0, box_deltas.shape[1]))
 
     boxes = boxes.astype(np.float, copy=False)
-    widths = boxes[:, 2] - boxes[:, 0] + 1e-14
-    heights = boxes[:, 3] - boxes[:, 1] + 1e-14
-    ctr_x = boxes[:, 0] + 0.5 * widths
-    ctr_y = boxes[:, 1] + 0.5 * heights
+    widths = boxes[:, 2] - boxes[:, 0] + 1.0
+    heights = boxes[:, 3] - boxes[:, 1] + 1.0
+    ctr_x = boxes[:, 0] + 0.5 * (widths - 1.0)
+    ctr_y = boxes[:, 1] + 0.5 * (heights - 1.0)
 
     dx = box_deltas[:, 0::4]
     dy = box_deltas[:, 1::4]
@@ -34,13 +61,13 @@ def bbox_pred(boxes, box_deltas):
 
     pred_boxes = np.zeros(box_deltas.shape)
     # x1
-    pred_boxes[:, 0::4] = pred_ctr_x - 0.5 * pred_w
+    pred_boxes[:, 0::4] = pred_ctr_x - 0.5 * (pred_w - 1.0)
     # y1
-    pred_boxes[:, 1::4] = pred_ctr_y - 0.5 * pred_h
+    pred_boxes[:, 1::4] = pred_ctr_y - 0.5 * (pred_h - 1.0)
     # x2
-    pred_boxes[:, 2::4] = pred_ctr_x + 0.5 * pred_w
+    pred_boxes[:, 2::4] = pred_ctr_x + 0.5 * (pred_w - 1.0)
     # y2
-    pred_boxes[:, 3::4] = pred_ctr_y + 0.5 * pred_h
+    pred_boxes[:, 3::4] = pred_ctr_y + 0.5 * (pred_h - 1.0)
 
     return pred_boxes
 
@@ -53,11 +80,11 @@ def clip_boxes(boxes, im_shape):
     :return: [N, 4* num_classes]
     """
     # x1 >= 0
-    boxes[:, 0::4] = np.maximum(boxes[:, 0::4], 0)
+    boxes[:, 0::4] = np.maximum(np.minimum(boxes[:, 0::4], im_shape[1] - 1), 0)
     # y1 >= 0
-    boxes[:, 1::4] = np.maximum(boxes[:, 1::4], 0)
+    boxes[:, 1::4] = np.maximum(np.minimum(boxes[:, 1::4], im_shape[0] - 1), 0)
     # x2 < im_shape[1]
-    boxes[:, 2::4] = np.minimum(boxes[:, 2::4], im_shape[1] - 1)
+    boxes[:, 2::4] = np.maximum(np.minimum(boxes[:, 2::4], im_shape[1] - 1), 0)
     # y2 < im_shape[0]
-    boxes[:, 3::4] = np.minimum(boxes[:, 3::4], im_shape[0] - 1)
+    boxes[:, 3::4] = np.maximum(np.minimum(boxes[:, 3::4], im_shape[0] - 1), 0)
     return boxes
--- example/rcnn/rcnn/config.py
+++ example/rcnn/rcnn/config.py
@@ -3,26 +3,65 @@ from easydict import EasyDict as edict
 
 config = edict()
 
+# image processing config
 config.EPS = 1e-14
 config.PIXEL_MEANS = np.array([[[123.68, 116.779, 103.939]]])
+config.SCALES = (600, )  # single scale training and testing
+config.MAX_SIZE = 1000
+
+# nms config
+config.USE_GPU_NMS = True
+config.GPU_ID = 0
 
 config.TRAIN = edict()
 
-config.TRAIN.SCALES = (600, )
-config.TRAIN.MAX_SIZE = 1000
+# R-CNN and RPN
+config.TRAIN.FINETUNE = False
+config.TRAIN.BATCH_SIZE = 128  # used in grad_scale
 
+# R-CNN
+config.TRAIN.HAS_RPN = False
+config.TRAIN.ASPECT_GROUPING = True
 config.TRAIN.BATCH_IMAGES = 2
-config.TRAIN.BATCH_SIZE = 128
 config.TRAIN.FG_FRACTION = 0.25
 config.TRAIN.FG_THRESH = 0.5
 config.TRAIN.BG_THRESH_HI = 0.5
 config.TRAIN.BG_THRESH_LO = 0.1
 
+# R-CNN bounding box regression
 config.TRAIN.BBOX_REGRESSION_THRESH = 0.5
 config.TRAIN.BBOX_INSIDE_WEIGHTS = np.array([1.0, 1.0, 1.0, 1.0])
 
+# RPN anchor loader
+config.TRAIN.RPN_BATCH_SIZE = 256
+config.TRAIN.RPN_FG_FRACTION = 0.5
+config.TRAIN.RPN_POSITIVE_OVERLAP = 0.7
+config.TRAIN.RPN_NEGATIVE_OVERLAP = 0.3
+config.TRAIN.RPN_CLOBBER_POSITIVES = False
+config.TRAIN.RPN_BBOX_INSIDE_WEIGHTS = (1.0, 1.0, 1.0, 1.0)
+config.TRAIN.RPN_POSITIVE_WEIGHT = -1.0
+
+# used for end2end training
+# RPN proposal
+config.TRAIN.RPN_NMS_THRESH = 0.7
+config.TRAIN.RPN_PRE_NMS_TOP_N = 12000
+config.TRAIN.RPN_POST_NMS_TOP_N = 6000
+config.TRAIN.RPN_MIN_SIZE = 16
+# approximate bounding box regression
+config.TRAIN.BBOX_NORMALIZATION_PRECOMPUTED = False
+config.TRAIN.BBOX_MEANS = (0.0, 0.0, 0.0, 0.0)
+config.TRAIN.BBOX_STDS = (0.1, 0.1, 0.2, 0.2)
+
 config.TEST = edict()
 
-config.TEST.SCALES = (600, )
+# R-CNN testing
+config.TEST.HAS_RPN = False
+config.TEST.BATCH_IMAGES = 1
 config.TEST.NMS = 0.3
 config.TEST.DEDUP_BOXES = 1. / 16.
+
+# RPN proposal
+config.TEST.RPN_NMS_THRESH = 0.7
+config.TEST.RPN_PRE_NMS_TOP_N = 6000
+config.TEST.RPN_POST_NMS_TOP_N = 300
+config.TEST.RPN_MIN_SIZE = 16
