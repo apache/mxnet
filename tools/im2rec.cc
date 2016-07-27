7890815688017b7f7ab013d1b20a2366d6a98993
tornadomeet <wuweigreat@qq.com>
2016-07-27 10:17:30 +0800

    update help info

--- tools/im2rec.cc
+++ tools/im2rec.cc
@@ -48,6 +48,8 @@ int main(int argc, char *argv[]) {
            "Possible additional parameters:\n"\
            "\tcolor=USE_COLOR[default=1] Force color (1), gray image (0) or keep source unchanged (-1).\n"\
            "\tresize=newsize resize the shorter edge of image to the newsize, original images will be packed by default\n"\
+		   "\tmin_size=minsize resize the shorter edge of image randomly with minimum size, original images will be packed by default\n"\
+		   "\tmax_size=maxsize resize the shorter edge of image randomly with maximum size, original images will be packed by default\n"\
            "\tlabel_width=WIDTH[default=1] specify the label_width in the list, by default set to 1\n"\
            "\tnsplit=NSPLIT[default=1] used for part generation, logically split the image.list to NSPLIT parts by position\n"\
            "\tpart=PART[default=0] used for part generation, pack the images from the specific part in image.list\n"\
@@ -74,8 +76,8 @@ int main(int argc, char *argv[]) {
     char key[128], val[128];
     if (sscanf(argv[i], "%[^=]=%s", key, val) == 2) {
       if (!strcmp(key, "resize")) new_size = atoi(val);
-      if (!strcmp(key, "min_size")) min_size = atoi(val);
-      if (!strcmp(key, "max_size")) max_size = atoi(val);
+	  if (!strcmp(key, "min_size")) min_size = atoi(val);
+	  if (!strcmp(key, "max_size")) max_size = atoi(val);
       if (!strcmp(key, "label_width")) label_width = atoi(val);
       if (!strcmp(key, "nsplit")) nsplit = atoi(val);
       if (!strcmp(key, "part")) partid = atoi(val);
@@ -102,7 +104,7 @@ int main(int argc, char *argv[]) {
   if (min_size > 0 && max_size > 0 && min_size < max_size) {
 	  LOG(INFO) << "Using ramdom short edge beteween [" << min_size << ", " << max_size << "]";
   } else {
-	  LOG(INFO) << "Keep origin image size or using resize for shor edge instead";
+	  LOG(INFO) << "Keep origin image size or using resize for short edge instead";
   }
   if (center_crop) {
     LOG(INFO) << "Center cropping to square";
