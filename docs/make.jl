85ec1112333a5dd6432c6a54b1308feea4d2a3b4
Valentin Churavy <v.churavy@gmail.com>
2016-08-02 03:24:46 +0900

    finalize mkdocs setup

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,11 @@
 using Documenter, MXNet
 
 makedocs(
-  modules = [MXNet]
+  modules = MXNet,
+  doctest = false
+)
+
+deploydocs(
+  deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+  repo = "github.com/dmlc/MXNet.jl.git",
 )
