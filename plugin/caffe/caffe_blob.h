bbf4512bde7e7ce7c114362a71b68ebab76012b0
haoran <hrwangchengdu@hotmail.com>
2016-08-05 23:03:12 +0800

    [caffe-plugin] Change to mxnet::TBlob mxnet::TShape.

--- plugin/caffe/caffe_blob.h
+++ plugin/caffe/caffe_blob.h
@@ -7,8 +7,7 @@
 #ifndef PLUGIN_CAFFE_CAFFE_BLOB_H_
 #define PLUGIN_CAFFE_CAFFE_BLOB_H_
 
-#include <mshadow/tensor.h>
-#include <mshadow/tensor_blob.h>
+#include <mxnet/tensor_blob.h>
 #include <vector>
 #include <caffe/blob.hpp>
 #include <caffe/layer.hpp>
@@ -21,14 +20,14 @@ namespace caffe {
 // Declare Memory Type for Caffe blob
 enum caffeMemoryTypes {Data, Grad, Non};
 
-mshadow::TShape Vector2TShape(const std::vector<int> &vec_int);
-std::vector<int> TShape2Vector(const mshadow::TShape &tshape);
+TShape Vector2TShape(const std::vector<int> &vec_int);
+std::vector<int> TShape2Vector(const TShape &tshape);
 
 // implementation of tensor to blob, called by TensorToBlob
 template<typename Device, typename Dtype>
 void SetDataGradToBlob(caffeMemoryTypes memType,
                        typename std::vector< ::caffe::Blob<Dtype>*>::iterator blob,
-                       typename std::vector<mshadow::TBlob>::const_iterator itr);
+                       typename std::vector<TBlob>::const_iterator itr);
 
 /**
  * \brief The interface to convert mxnet's tensor to caffe's blob
@@ -37,7 +36,7 @@ void SetDataGradToBlob(caffeMemoryTypes memType,
 template<typename Device, typename Dtype>
 void TBlob2CaffeBlob(caffeMemoryTypes memType,
                      typename std::vector< ::caffe::Blob<Dtype>*>::iterator blob,
-                     typename std::vector<mshadow::TBlob>::const_iterator tblob,
+                     typename std::vector<TBlob>::const_iterator tblob,
                      int n = 1) {
   for (int i = 0; i < n; ++i, ++blob, ++tblob) {
     (*blob)->Reshape(TShape2Vector((*tblob).shape_));
