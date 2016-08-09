a804f6869c3ea32e1a2aaef61522e51736f39d85
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-08-09 12:56:08 -0700

    Fix resources

--- src/resource.cc
+++ src/resource.cc
@@ -67,7 +67,7 @@ class ResourceManagerImpl : public ResourceManager {
   ResourceManagerImpl() noexcept(false)
       : global_seed_(0) {
     cpu_temp_space_copy_ = dmlc::GetEnv("MXNET_CPU_TEMP_COPY", 16);
-    gpu_temp_space_copy_ = dmlc::GetEnv("MXNET_GPU_TEMP_COPY", 4);
+    gpu_temp_space_copy_ = dmlc::GetEnv("MXNET_GPU_TEMP_COPY", 1);
     engine_ref_ = Engine::_GetSharedRef();
     storage_ref_ = Storage::_GetSharedRef();
     cpu_rand_.reset(new ResourceRandom<cpu>(
@@ -250,7 +250,7 @@ void* Resource::get_host_space_internal(size_t size) const {
 }
 
 ResourceManager* ResourceManager::Get() {
-  static resource::ResourceManagerImpl inst;
+  thread_local resource::ResourceManagerImpl inst;
   return &inst;
 }
 }  // namespace mxnet
