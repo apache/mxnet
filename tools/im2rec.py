c97e03d1a07ffbe4bdd34faa4e58201de7693030
Trangle <kw_w@foxmail.com>
2016-08-10 02:31:45 +0800

    Change the multiprocessing relevant code
    change the multiprocessing relevant code to adapt Windows environment, and a little revise of arguments deal about path.

--- tools/im2rec.py
+++ tools/im2rec.py
@@ -8,7 +8,7 @@ import random
 import argparse
 import cv2
 import time
-
+import multiprocessing
 
 def list_image(root, recursive, exts):
     image_list = []
@@ -74,104 +74,66 @@ def read_list(path_in):
     return image_list
 
 
-def write_record(args, image_list, fname):
-    source = image_list
-    tic = [time.time()]
+def image_encode(args, item, q_out):
     color_modes = {-1: cv2.IMREAD_UNCHANGED,
                    0: cv2.IMREAD_GRAYSCALE,
                    1: cv2.IMREAD_COLOR}
-    total = len(source)
 
-    def image_encode(item, q_out):
-        try:
-            img = cv2.imread(os.path.join(args.root, item[1]), color_modes[args.color])
-        except:
-            print 'imread error:', item[1]
-            return
-        if img is None:
-            print 'read none error:', item[1]
-            return
-        if args.center_crop:
-            if img.shape[0] > img.shape[1]:
-                margin = (img.shape[0] - img.shape[1]) / 2;
-                img = img[margin:margin + img.shape[1], :]
-            else:
-                margin = (img.shape[1] - img.shape[0]) / 2;
-                img = img[:, margin:margin + img.shape[0]]
-        if args.resize:
-            if img.shape[0] > img.shape[1]:
-                newsize = (args.resize, img.shape[0] * args.resize / img.shape[1])
-            else:
-                newsize = (img.shape[1] * args.resize / img.shape[0], args.resize)
-            img = cv2.resize(img, newsize)
-        header = mx.recordio.IRHeader(0, item[2], item[0], 0)
-
-        try:
-            s = mx.recordio.pack_img(header, img, quality=args.quality, img_fmt=args.encoding)
-            q_out.put(('data', s, item))
-        except:
-            print 'pack_img error:', item[1]
-            return
-
-    def read_worker(q_in, q_out):
-        while not q_in.empty():
-            item = q_in.get()
-            image_encode(item, q_out)
-
-    def write_worker(q_out, fname, saving_folder):
-        pre_time = time.time()
-        sink = []
-        os.chdir(saving_folder)
-        fname_rec = fname[:fname.rfind('.')]
-        record = mx.recordio.MXRecordIO(fname_rec + '.rec', 'w')
-        while True:
-            stat, s, item = q_out.get()
-            if stat == 'finish':
-                write_list(fname_rec + '.lst', sink)
-                break
-            record.write(s)
-            sink.append(item)
-            if len(sink) % 1000 == 0:
-                cur_time = time.time()
-                print 'time:', cur_time - pre_time, ' count:', len(sink)
-                pre_time = cur_time
+    pari_path = os.path.relpath(os.path.join(args.root, '..'), '.')
+    try:
+        img = cv2.imread(os.path.join(pari_path, item[1]), color_modes[args.color])
+    except:
+        print 'imread error:', item[1]
+        return
+    if img is None:
+        print 'read none error:', item[1]
+        return
+    if args.center_crop:
+        if img.shape[0] > img.shape[1]:
+            margin = (img.shape[0] - img.shape[1]) / 2;
+            img = img[margin:margin + img.shape[1], :]
+        else:
+            margin = (img.shape[1] - img.shape[0]) / 2;
+            img = img[:, margin:margin + img.shape[0]]
+    if args.resize:
+        if img.shape[0] > img.shape[1]:
+            newsize = (args.resize, img.shape[0] * args.resize / img.shape[1])
+        else:
+            newsize = (img.shape[1] * args.resize / img.shape[0], args.resize)
+        img = cv2.resize(img, newsize)
+    header = mx.recordio.IRHeader(0, item[2], item[0], 0)
 
     try:
-        import multiprocessing
-        q_in = [multiprocessing.Queue() for i in range(args.num_thread)]
-        q_out = multiprocessing.Queue(1024)
-        for i in range(len(image_list)):
-            q_in[i % len(q_in)].put(image_list[i])
-        read_process = [multiprocessing.Process(target=read_worker, args=(q_in[i], q_out)) \
-                        for i in range(args.num_thread)]
-        for p in read_process:
-            p.start()
-        write_process = multiprocessing.Process(target=write_worker, args=(q_out, fname, args.saving_folder))
-        write_process.start()
-        for p in read_process:
-            p.join()
-        q_out.put(('finish', '', []))
-        write_process.join()
-    except ImportError:
-        print('multiprocessing not available, fall back to single threaded encoding')
-        import Queue
-        q_out = Queue.Queue()
-        os.chdir(args.saving_folder)
-        fname_rec = fname[:fname.rfind('.')]
-        record = mx.recordio.MXRecordIO(fname_rec + '.rec', 'w')
-        cnt = 0
-        pre_time = time.time()
-        for item in image_list:
-            image_encode(item, q_out)
-            if q_out.empty():
-                continue
-            _, s, _ = q_out.get()
-            record.write(s)
-            cnt += 1
-            if cnt % 1000 == 0:
-                cur_time = time.time()
-                print 'time:', cur_time - pre_time, ' count:', cnt
-                pre_time = cur_time
+        s = mx.recordio.pack_img(header, img, quality=args.quality, img_fmt=args.encoding)
+        q_out.put(('data', s, item))
+    except:
+        print 'pack_img error:', item[1]
+        return
+
+
+def read_worker(args, q_in, q_out):
+    while not q_in.empty():
+        item = q_in.get()
+        image_encode(args, item, q_out)
+
+
+def write_worker(q_out, fname, saving_folder):
+    pre_time = time.time()
+    sink = []
+    os.chdir(saving_folder)
+    fname_rec = fname[:fname.rfind('.')]
+    record = mx.recordio.MXRecordIO(fname_rec + '.rec', 'w')
+    while True:
+        stat, s, item = q_out.get()
+        if stat == 'finish':
+            write_list(fname_rec + '.lst', sink)
+            break
+        record.write(s)
+        sink.append(item)
+        if len(sink) % 1000 == 0:
+            cur_time = time.time()
+            print 'time:', cur_time - pre_time, ' count:', len(sink)
+            pre_time = cur_time
 
 
 def main():
@@ -223,16 +185,58 @@ def main():
     rgroup.add_argument('--shuffle', default=True, help='If this is set as True, \
         im2rec will randomize the image order in <prefix>.lst')
     args = parser.parse_args()
+
+    # data_path = os.path.abspath(args.root)
+    data_path = args.root
     if args.list:
         make_list(args)
     else:
-        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+        files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
         for f in files:
             if f.startswith(args.prefix) is True and f.endswith('.lst') is True:
                 print 'Creating .rec file from', f, 'in', args.saving_folder
-                image_list = read_list(f)
-                write_record(args, image_list, f)
+                image_list = read_list(os.path.join(data_path, f))
+                return (args, image_list, f)
 
 
 if __name__ == '__main__':
-    main()
+    (args, image_list, fname) = main()
+    source = image_list
+    tic = [time.time()]
+    try:
+        multiprocessing.freeze_support()
+        q_in = [multiprocessing.Queue() for i in range(args.num_thread)]
+        q_out = multiprocessing.Queue(1024)
+        for i in range(len(image_list)):
+            q_in[i % len(q_in)].put(image_list[i])
+        read_process = [multiprocessing.Process(target=read_worker, args=(args, q_in[i], q_out)) \
+                        for i in range(args.num_thread)]
+        for p in read_process:
+            p.start()
+        write_process = multiprocessing.Process(target=write_worker, args=(q_out, fname, args.saving_folder))
+        write_process.start()
+        for p in read_process:
+            p.join()
+        q_out.put(('finish', '', []))
+        write_process.join()
+    except EOFError:
+        print('multiprocessing not available, fall back to single threaded encoding')
+        import Queue
+        q_out = Queue.Queue()
+        # os.chdir(args.saving_folder)
+        fname_rec = fname[:fname.rfind('.')]
+        record = mx.recordio.MXRecordIO(fname_rec + '.rec', 'w')
+        cnt = 0
+        pre_time = time.time()
+        for item in image_list:
+            image_encode(args, item, q_out)
+            if q_out.empty():
+                continue
+            _, s, _ = q_out.get()
+            record.write(s)
+            cnt += 1
+            if cnt % 1000 == 0:
+                cur_time = time.time()
+                print 'time:', cur_time - pre_time, ' count:', cnt
+                pre_time = cur_time
+    total = len(source)
