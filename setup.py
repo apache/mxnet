d8cc839b9b342e650a63ece25d8be5364050dfd5
Gestalt LUR <gestapolur@users.noreply.github.com>
2016-08-10 17:29:30 +0800

    auto compile on pkg install

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@
 """Setup mxnet package."""
 from __future__ import absolute_import
 import os
+import subprocess
 from setuptools import setup
 
+
+subprocess.Popen("make -j$(nproc)",
+                 stdin=subprocess.PIPE,
+                 stderr=subprocess.PIPE,
+                 stdout=subprocess.PIPE,
+                 shell=True).communicate()
 # We can not import `mxnet.info.py` in setup.py directly since mxnet/__init__.py
 # Will be invoked which introduces dependences
 CURRENT_DIR = os.path.dirname(__file__)
-libinfo_py = os.path.join(CURRENT_DIR, 'mxnet/libinfo.py')
+libinfo_py = os.path.join(CURRENT_DIR, 'python/mxnet/libinfo.py')
 libinfo = {'__file__': libinfo_py}
 exec(compile(open(libinfo_py, "rb").read(), libinfo_py, 'exec'), libinfo, libinfo)
-
 LIB_PATH = libinfo['find_lib_path']()
 __version__ = libinfo['__version__']
+
 
 setup(name='mxnet',
       version=__version__,
@@ -21,6 +28,6 @@ setup(name='mxnet',
           'numpy',
       ],
       zip_safe=False,
-      packages=['mxnet', 'mxnet.module'],
+      packages=['python/mxnet', 'python/mxnet.module'],
       data_files=[('mxnet', [LIB_PATH[0]])],
       url='https://github.com/dmlc/mxnet')
