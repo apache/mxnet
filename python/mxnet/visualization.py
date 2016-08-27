99ce89d94d80b785f0d960d1b2056cb00484b766
Trangle <kw_w@foxmail.com>
2016-08-28 05:15:44 +0800

    Add function 'print_summary' and some revise
    Add function 'print_summary' for print detail information of network, and format argument was add in 'plot_network'.
    You can use 'print_summary' like:
    """
    net = get_symbol(1000)
    shape = {'softmax_label': (64, 12), 'data': (64, 3, 224, 224)}
    mx.viz.print_summary(net, shape=shape)
    """
    If without shape, the number of arguments would be nonsense currently.

--- python/mxnet/visualization.py
+++ python/mxnet/visualization.py
@@ -25,8 +25,145 @@ def _str2tuple(string):
     """
     return re.findall(r"\d+", string)
 
+def print_summary(symbol, title="summary", shape=None, line_length=100, positions=[.33, .55, .67, 1.]):
+    """convert symbol for detail information
 
-def plot_network(symbol, title="plot", shape=None, node_attrs={}):
+    Parameters
+    ----------
+    title: str
+        title of the dot graph
+    symbol: Symbol
+        symbol to be visualized
+    shape: dict
+        dict of shapes, str->shape (tuple), given input shapes
+    line_length: int
+        total length of printed lines
+    positions: list
+        relative or absolute positions of log elements in each line
+    Returns
+    ------
+    dot: Diagraph
+        dot object of symbol
+    """
+    if not isinstance(symbol, Symbol):
+        raise TypeError("symbol must be Symbol")
+
+    show_shape = False
+    if shape != None:
+        show_shape = True
+        interals = symbol.get_internals()
+        _, out_shapes, _ = interals.infer_shape(**shape)
+        if out_shapes == None:
+            raise ValueError("Input shape is incompete")
+        shape_dict = dict(zip(interals.list_outputs(), out_shapes))
+
+    conf = json.loads(symbol.tojson())
+    nodes = conf["nodes"]
+    heads = set(conf["heads"][0])
+    if positions[-1] <= 1:
+        positions = [int(line_length * p) for p in positions]
+    # header names for the different log elements
+    to_display = ['Layer (type)', 'Output Shape', 'Param #', 'Connected to']
+
+    def print_row(fields, positions):
+        line = ''
+        for i in range(len(fields)):
+            line += str(fields[i])
+            line = line[:positions[i]]
+            line += ' ' * (positions[i] - len(line))
+        print(line)
+
+    print('_' * line_length)
+    print_row(to_display, positions)
+    print('=' * line_length)
+
+    def print_layer_summary(node, out_shape):
+        node_name = node['name']
+        op = node["op"]
+        pre_node = []
+        pre_filter = 0
+        if op == '_Plus':
+            if op != "null":
+                inputs = node["inputs"]
+                item = inputs[0]
+                input_node = nodes[item[0]]
+                input_name = input_node["name"]
+                pre_node.append(input_name)
+                if input_node["op"] != "null" or item[0] in heads:
+                    if show_shape:
+                        if input_node["op"] != "null":
+                            key = input_name + "_output"
+                            shape = shape_dict[key][1:]
+                            pre_filter = int(shape[0])
+                        else:
+                            key = input_name
+                            shape = shape_dict[key][1:]
+                            pre_filter = int(shape[0])
+        else:
+            if op != "null":
+                inputs = node["inputs"]
+                for item in inputs:
+                    input_node = nodes[item[0]]
+                    input_name = input_node["name"]
+                    if input_node["op"] != "null":
+                        pre_node.append(input_name)
+                    if input_node["op"] != "null" or item[0] in heads:
+                        # add shapes
+                        if show_shape:
+                            if input_node["op"] != "null":
+                                key = input_name + "_output"
+                                shape = shape_dict[key][1:]
+                                pre_filter = pre_filter + int(shape[0])
+                            else:
+                                key = input_name
+                                shape = shape_dict[key][1:]
+                                pre_filter = pre_filter + int(shape[0])
+        cur_param = 0
+        if op == 'Convolution':
+            cur_param =  pre_filter * int(_str2tuple(node["param"]["kernel"])[0]) * int(
+                _str2tuple(node["param"]["kernel"])[1]) * int(node["param"]["num_filter"]) + int(
+                node["param"]["num_filter"])
+        elif op == 'FullyConnected':
+            cur_param = pre_filter * (int(node["param"]["num_hidden"]) + 1)
+
+        if not pre_node:
+            first_connection = ''
+        else:
+            first_connection = pre_node[0]
+
+        fields = [node['name'] + '(' + op + ')', "x".join([str(x) for x in out_shape]), cur_param, first_connection]
+        if len(pre_node) > 1:
+            for i in range(1, len(pre_node)):
+                fields = ['', '', '', pre_node[i]]
+                print_row(fields, positions)
+        print_row(fields, positions)
+        return cur_param
+
+    total_params = 0
+    for i in range(len(nodes)):
+        node = nodes[i]
+        out_shape = []
+        op = node["op"]
+        if op == "null" and i > 0:
+            continue
+        if op != "null" or i in heads:
+            if show_shape:
+                if op != "null":
+                    key = node["name"] + "_output"
+                    out_shape = shape_dict[key][1:]
+                else:
+                    key = node["name"]
+                    out_shape = shape_dict[key][1:]
+        total_params += print_layer_summary(nodes[i], out_shape)
+        if i == len(nodes) - 1:
+            print('=' * line_length)
+        else:
+            print('_' * line_length)
+
+    print('Total params: %s' % total_params)
+    print('_' * line_length)
+
+def plot_network(symbol, title="plot", format='pdf', shape=None, node_attrs={}):
     """convert symbol to dot object for visualization
 
     Parameters
@@ -70,7 +207,7 @@ def plot_network(symbol, title="plot", shape=None, node_attrs={}):
                  "width": "1.3", "height": "0.8034", "style": "filled"}
     # merge the dict provided by user and the default one
     node_attr.update(node_attrs)
-    dot = Digraph(name=title)
+    dot = Digraph(name=title, format=format)
     # color map
     cm = ("#8dd3c7", "#fb8072", "#ffffb3", "#bebada", "#80b1d3",
           "#fdb462", "#b3de69", "#fccde5")
