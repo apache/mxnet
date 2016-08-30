7bec91dd360a614936c820e4bb2df986007c8765
Yizhi Liu <javelinjs@gmail.com>
2016-08-31 00:20:42 +0800

    [scala] macro generate first Symbol operator

--- scala-package/macros/src/main/scala/ml/dmlc/mxnet/SymbolMacro.scala
+++ scala-package/macros/src/main/scala/ml/dmlc/mxnet/SymbolMacro.scala
@@ -13,6 +13,12 @@ object ImplMacros {
     impl(c)(false, annottees: _*)
   }
 
+  /*
+  def LeakyReLU(name: String = null, attr: Map[String, String] = null): SymbolCreateNamedFunc = {
+    createFromNamedSymbolsNoCheck("LeakyReLU", name, attr)
+  }
+  */
+
   def impl(c: Context)(addSuper: Boolean, annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
     val inputs = annottees.map(_.tree).toList
@@ -28,21 +34,40 @@ object ImplMacros {
         Apply(Select(Ident(TermName("a")), TermName("$plus")), List(Ident(TermName("b"))))),
       DefDef(Modifiers(), TermName("f3"), List(), List(List(ValDef(Modifiers(),
         TermName("a"), Ident(TypeName("Int")), EmptyTree))), TypeTree(),
-        Apply(Ident(TermName("showA")), List(Ident(TermName("a")))))
+        Apply(Ident(TermName("showA")), List(Ident(TermName("a"))))),
+      DefDef(Modifiers(), TermName("f4"), List(), List(List(ValDef(Modifiers(),
+        TermName("a"), Ident(TypeName("Int")), EmptyTree),
+        ValDef(Modifiers(), TermName("b"), Ident(TypeName("String")), EmptyTree))), TypeTree(),
+        Apply(Select(Ident(TermName("a")), TermName("$plus")), List(Ident(TermName("b"))))),
+      DefDef(Modifiers(), TermName("LeakyReLU2"), List(), List(List(
+        ValDef(Modifiers(), TermName("name"), Ident(TypeName("String")), EmptyTree),
+        ValDef(Modifiers(), TermName("kwargs"),
+          AppliedTypeTree(Ident(TypeName("Map")),
+            List(Ident(TypeName("String")), Ident(TypeName("Any")))), EmptyTree))), TypeTree(),
+        Apply(
+          Apply(Ident(TermName("createFromNamedSymbolsNoCheck")),
+          List(Literal(Constant("LeakyReLU")))),
+        List(Ident(TermName("kwargs")))
+        ))
       /*
+      DefDef(Modifiers(), TermName("f"), List(), List(List(ValDef(Modifiers(),
+        TermName("a"), Ident(TypeName("Int")), EmptyTree)),
+        List(ValDef(Modifiers(), TermName("b"),
+        Ident(TypeName("String")), EmptyTree))), TypeTree(),
+        Apply(Select(Ident(TermName("a")), TermName("$plus")), List(Ident(TermName("b")))))
       DefDef(Modifiers(), newTermName("f"), List(),
         List(List(ValDef(Modifiers(PARAM), newTermName("a"),
         Ident(newTypeName("Int")), EmptyTree)), List(ValDef(Modifiers(PARAM),
         newTermName("b"), Ident(newTypeName("String")), EmptyTree))),
         TypeTree(), Apply(Select(Ident(newTermName("a")), newTermName("$plus")),
         List(Ident(newTermName("b")))))
-        */
+      */
     )
 
     // pattern match on the inputs
     val modDefs = inputs map { tree => tree match {
       case ClassDef(mods, name, something, template) =>
-        println(s"DEBUG: $mods | $name | $something | $template")
+        // println(s"DEBUG: $mods | $name | $something | $template")
         val q = template match {
           case Template(superMaybe, emptyValDef, defs) =>
             Template(superMaybe, emptyValDef, defs ++ newDefDefs)
@@ -51,10 +76,10 @@ object ImplMacros {
         }
         ClassDef(mods, name, something, q)
       case ModuleDef(mods, name, template) =>
-        println(s"DEBUG Module: $mods | $name | $template")
+        // println(s"DEBUG Module: $mods | $name | $template")
         val q = template match {
           case Template(superMaybe, emptyValDef, defs) =>
-            println(s"DEBUG Template: $superMaybe | $emptyValDef | $defs")
+            // println(s"DEBUG Template: $superMaybe | $emptyValDef | $defs")
             Template(superMaybe, emptyValDef, defs ++ newDefDefs)
           case ex =>
             throw new IllegalArgumentException(s"Invalid template: $ex")
