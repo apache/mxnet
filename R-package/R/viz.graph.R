65385ccae96f18f1b9f1d07515deb0164d14ef46
Tianqi Chen <tqchen@users.noreply.github.com>
2016-09-01 21:17:33 -0700

    NNVM Refactor (#3194)
    * Init nnvm change
    
    * temp checkin
    
    * Move TShape to NNVM
    
    * Redirect Symbolic API to NNVM
    
    * Add Op Prop Adapter
    
    * Finish migrate in shape infer
    
    * Pass all symbolic test
    
    * temp commit
    
    * enable aux data
    
    * [EXEC] Basic version of exec for forward only
    
    * [EXEC] Enable most optimizations, still wait grad and context
    
    * fix legacy op with latest one
    
    * Update NNVM NodeRef
    
    * Adapt to newer interface
    
    * ALl registry of backop is complete
    
    * temp commit
    
    * Hack finish backward pass
    
    * [EXEC] One day pass
    
    * [EXEC] Pass all operator unittest
    
    * [EXEC] enable model parallel
    
    * Fully pass all legacy tests
    
    * Remove legacy symbolic code
    
    * update news
    
    * Make travis compile
    
    * Fix python3
    
    * Update viz module to new json format

--- R-package/R/viz.graph.R
+++ R-package/R/viz.graph.R
@@ -1,6 +1,6 @@
-#' 
+#'
 #' Convert symbol to dot object for visualization purpose.
-#' 
+#'
 #' @importFrom magrittr %>%
 #' @importFrom stringr str_extract_all
 #' @importFrom magrittr %>%
@@ -14,14 +14,14 @@
 #' @importFrom DiagrammeR create_edges
 #' @importFrom DiagrammeR combine_edges
 #' @importFrom DiagrammeR render_graph
-#' 
+#'
 #' @param model a \code{string} representing the path to a file containing the \code{JSon} of a model dump or the actual model dump.
 #' @param graph.title a \code{string} displayed on top of the viz.
 #' @param graph.title.font.name a \code{string} representing the font to use for the title.
 #' @param  graph.title.font.size a \code{numeric} representing the size of the font to use for the title.
 #' @param graph.width.px a \code{numeric} representing the size (width) of the graph. In pixels
 #' @param graph.height.px a \code{numeric} representing the size (height) of the graph. In pixels
-#'     
+#'
 #' @return a graph object ready to be displayed with the \code{print} function.
 #'
 #' @export
@@ -44,7 +44,7 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
       "#fccde5" # default value
     )
   }
-  
+
   get.shape <- function(type) {
     switch(
       EXPR = type,
@@ -62,35 +62,35 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
       "box" # default value
     )
   }
-  
+
   # extract IDs from string list
   str2tuple <- function(str) str_extract_all(str, "\\d+") %>% unlist %>% as.numeric
-  
+
   # generate text content for each node.
   get.label <- function(type, mat.row) {
     switch(
       EXPR = type,
-      "FullyConnected" = mat.row[,param.num_hidden] %>% paste("FullyConnected", ., sep = "\n"),
+      "FullyConnected" = mat.row[,attr.num_hidden] %>% paste("FullyConnected", ., sep = "\n"),
       "Convolution" = {
-        kernel.parameters <- mat.row[,param.kernel] %>% str2tuple
-        stride.parameters <- mat.row[,param.stride] %>% str2tuple
-        num_filter.parameters <- mat.row[,param.num_filter] %>% str2tuple
+        kernel.parameters <- mat.row[,attr.kernel] %>% str2tuple
+        stride.parameters <- mat.row[,attr.stride] %>% str2tuple
+        num_filter.parameters <- mat.row[,attr.num_filter] %>% str2tuple
         paste0("Convolution\n", kernel.parameters[1], "x", kernel.parameters[2],
                "/", stride.parameters[1], ", ", num_filter.parameters)
-      }, 
+      },
       "LeakyReLU" = ,
-      "Activation" = mat.row[,param.act_type] %>% paste0(type, "\n", .),
+      "Activation" = mat.row[,attr.act_type] %>% paste0(type, "\n", .),
       "Pooling" = {
-        pool_type.parameters <- mat.row[,param.pool_type] %>% str2tuple
-        kernel.parameters <- mat.row[,param.kernel] %>% str2tuple
-        stride.parameters <- mat.row[,param.stride] %>% str2tuple
-        paste0("Pooling\n", pool_type.parameters, "\n", kernel.parameters[1], "x", 
+        pool_type.parameters <- mat.row[,attr.pool_type] %>% str2tuple
+        kernel.parameters <- mat.row[,attr.kernel] %>% str2tuple
+        stride.parameters <- mat.row[,attr.stride] %>% str2tuple
+        paste0("Pooling\n", pool_type.parameters, "\n", kernel.parameters[1], "x",
                kernel.parameters[2], "/", stride.parameters[1])
       },
       type # default value
     )
   }
-  
+
   mx.model.json <- fromJSON(model, flatten = T)
   mx.model.nodes <- mx.model.json$nodes %>% as.data.table
   mx.model.nodes[,id:= .I - 1]
@@ -99,12 +99,12 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
   mx.model.nodes[,color:= get.color(op), by = id] # by=id to have an execution row per row
   mx.model.nodes[,shape:= get.shape(op), by = id] # by=id to have an execution row per row
   mx.model.nodes[,label:= get.label(op, .SD), by = id] # by=id to have an execution row per row
-  
+
   nodes.to.keep <-
     mx.model.nodes[op != "null",id] %>% unique %>% sort
   nodes.to.remove <-
     mx.model.nodes[,id] %>% unique %>% setdiff(nodes.to.keep) %>% sort
-  
+
   nodes <-
     create_nodes(
       nodes = mx.model.nodes[id %in% nodes.to.keep, id],
@@ -118,14 +118,14 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
       width = "1.3",
       height = "0.8034"
     )
-  
+
   mx.model.nodes[,has.connection:= sapply(inputs, function(x)
     length(x) > 0)]
-  
+
   nodes.to.insert <-
     mx.model.nodes[id %in% nodes.to.keep &
                      has.connection == T, .(id, inputs)]
-  
+
   edges <- NULL
   for (i in 1:nrow(nodes.to.insert)) {
     current.id <- nodes.to.insert[i, id]
@@ -140,7 +140,7 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
     else
       edges <- combine_edges(edges.temp, edges)
   }
-  
+
   graph <-
     create_graph(
       nodes_df = nodes,
@@ -151,7 +151,7 @@ graph.viz <- function(model, graph.title = "Computation graph", graph.title.font
       # node_attrs = "fontname = Helvetica",
       edge_attrs = c("color = gray20", "arrowsize = 0.8", "arrowhead = vee")
     )
-  
+
   return(render_graph(graph, width = graph.width.px, height = graph.height.px))
 }
 
