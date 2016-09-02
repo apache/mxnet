bcc75473ad5d2b60c4d2eb3156232d617a2bf68e
Tianqi Chen <tqchen@users.noreply.github.com>
2016-09-01 21:17:33 -0700

    NNVM Refactor (#3194)
    * Init nnvm change
    
    * temp checkin
    
    * Move TShape to NNVM
    
    * Redirect Symbolic API to NNVM
    
    * Add Op Prop Adapter
    
    * Finish migrate in shape infer
    
    * Pass all symbolic test
    
    * temp commit
    
    * enable aux data
    
    * [EXEC] Basic version of exec for forward only
    
    * [EXEC] Enable most optimizations, still wait grad and context
    
    * fix legacy op with latest one
    
    * Update NNVM NodeRef
    
    * Adapt to newer interface
    
    * ALl registry of backop is complete
    
    * temp commit
    
    * Hack finish backward pass
    
    * [EXEC] One day pass
    
    * [EXEC] Pass all operator unittest
    
    * [EXEC] enable model parallel
    
    * Fully pass all legacy tests
    
    * Remove legacy symbolic code
    
    * update news
    
    * Make travis compile
    
    * Fix python3
    
    * Update viz module to new json format

--- include/mxnet/base.h
+++ include/mxnet/base.h
@@ -11,6 +11,10 @@
 #include <dmlc/type_traits.h>
 #include <dmlc/parameter.h>
 #include <mshadow/tensor.h>
+// nnvm headers for symbolic construction.
+#include <nnvm/op.h>
+#include <nnvm/tuple.h>
+#include <nnvm/symbolic.h>
 #include <string>
 
 /*!
@@ -80,6 +84,10 @@ typedef mshadow::gpu gpu;
 typedef mshadow::index_t index_t;
 /*! \brief data type that will be used to store ndarray */
 typedef mshadow::default_real_t real_t;
+/*! \brief Shape data structure used to record shape information */
+using TShape = nnvm::TShape;
+/*! \brief operator structure from NNVM */
+using Op = nnvm::Op;
 
 /*! \brief Context information about the execution environment */
 struct Context {
