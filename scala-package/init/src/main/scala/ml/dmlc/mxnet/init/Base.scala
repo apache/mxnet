9511fdd2da7a4fb873af95f3e77afe59540562b0
Yizhi Liu <javelinjs@gmail.com>
2016-09-03 00:24:54 +0800

    [scala] init jnilib pom refinement

--- scala-package/init/src/main/scala/ml/dmlc/mxnet/init/Base.scala
+++ scala-package/init/src/main/scala/ml/dmlc/mxnet/init/Base.scala
@@ -1,38 +1,21 @@
 package ml.dmlc.mxnet.init
 
 object Base {
-  println("Current Dir: " + System.getProperty("user.dir"))
-  System.load("/Users/lewis/Workspace/source-codes/forks/mxnet/scala-package/init-native/osx-x86_64-cpu/target/libmxnet-init-scala-osx-x86_64-cpu.jnilib")
+  tryLoadInitLibrary()
   val _LIB = new LibInfo
 
   @throws(classOf[UnsatisfiedLinkError])
-  private def tryLoadLibraryOS(libname: String): Unit = {
-    try {
-      System.loadLibrary(libname)
-    } catch {
-      case e: UnsatisfiedLinkError =>
-        val os = System.getProperty("os.name")
-        // ref: http://lopica.sourceforge.net/os.html
-        if (os.startsWith("Linux")) {
-          tryLoadLibraryXPU(libname, "linux-x86_64")
-        } else if (os.startsWith("Mac")) {
-          tryLoadLibraryXPU(libname, "osx-x86_64")
-        } else {
-          // TODO(yizhi) support windows later
-          throw new UnsatisfiedLinkError()
-        }
+  private def tryLoadInitLibrary(): Unit = {
+    val baseDir = System.getProperty("user.dir") + "/init-native"
+    val os = System.getProperty("os.name")
+    // ref: http://lopica.sourceforge.net/os.html
+    if (os.startsWith("Linux")) {
+      System.load(s"$baseDir/linux-x86_64/target/libmxnet-init-scala-linux-x86_64.so")
+    } else if (os.startsWith("Mac")) {
+      System.load(s"$baseDir/osx-x86_64/target/libmxnet-init-scala-osx-x86_64.jnilib")
+    } else {
+      // TODO(yizhi) support windows later
+      throw new UnsatisfiedLinkError()
     }
   }
-
-  @throws(classOf[UnsatisfiedLinkError])
-  private def tryLoadLibraryXPU(libname: String, arch: String): Unit = {
-    try {
-      // try gpu first
-      System.loadLibrary(s"$libname-$arch-gpu")
-    } catch {
-      case e: UnsatisfiedLinkError =>
-        System.loadLibrary(s"$libname-$arch-cpu")
-    }
-  }
-
 }
