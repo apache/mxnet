b6ed1c9260920c98f2dd976269baef98be444ea4
Yizhi Liu <javelinjs@gmail.com>
2016-09-04 00:31:35 +0800

    [scala] macros functions with default arguments

--- scala-package/macros/src/main/scala/ml/dmlc/mxnet/SymbolMacro.scala
+++ scala-package/macros/src/main/scala/ml/dmlc/mxnet/SymbolMacro.scala
@@ -5,6 +5,7 @@ import scala.collection.mutable.ListBuffer
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 
+
 import ml.dmlc.mxnet.init.Base._
 
 private[mxnet] class FillDefs extends StaticAnnotation {
@@ -12,6 +13,9 @@ private[mxnet] class FillDefs extends StaticAnnotation {
 }
 
 object ImplMacros {
+  case class SymbolFunction(handle: SymbolHandle, keyVarNumArgs: String)
+
+
   def addDefs(c: Context)(annottees: c.Expr[Any]*) = {
     impl(c)(false, annottees: _*)
   }
@@ -21,10 +25,16 @@ object ImplMacros {
     createFromNamedSymbolsNoCheck("LeakyReLU", name, attr)
   }
   */
-  initSymbolModule().foreach(addr => println(s"Symbol addr: $addr"))
+  val symbolFunctions: Map[String, SymbolFunction] = initSymbolModule()
 
   def impl(c: Context)(addSuper: Boolean, annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
+
+    val AST_TYPE_MAP_STRING_ANY = AppliedTypeTree(Ident(TypeName("Map")),
+      List(Ident(TypeName("String")), Ident(TypeName("Any"))))
+    val AST_TYPE_MAP_STRING_STRING = AppliedTypeTree(Ident(TypeName("Map")),
+      List(Ident(TypeName("String")), Ident(TypeName("String"))))
+
     val inputs = annottees.map(_.tree).toList
     // create the definitions we're going to add
     val newDefDefs = List(
@@ -43,29 +53,24 @@ object ImplMacros {
         TermName("a"), Ident(TypeName("Int")), EmptyTree),
         ValDef(Modifiers(), TermName("b"), Ident(TypeName("String")), EmptyTree))), TypeTree(),
         Apply(Select(Ident(TermName("a")), TermName("$plus")), List(Ident(TermName("b"))))),
-      DefDef(Modifiers(), TermName("LeakyReLU2"), List(), List(List(
-        ValDef(Modifiers(), TermName("name"), Ident(TypeName("String")), EmptyTree),
-        ValDef(Modifiers(), TermName("kwargs"),
-          AppliedTypeTree(Ident(TypeName("Map")),
-            List(Ident(TypeName("String")), Ident(TypeName("Any")))), EmptyTree))), TypeTree(),
+      DefDef(Modifiers(), TermName("LeakyReLU2"), List(),
+        List(
+          List(
+            ValDef(Modifiers(Flag.PARAM | Flag.DEFAULTPARAM), TermName("name"), Ident(TypeName("String")), Literal(Constant(null))),
+            ValDef(Modifiers(Flag.PARAM | Flag.DEFAULTPARAM), TermName("attr"), AST_TYPE_MAP_STRING_STRING, Literal(Constant(null)))
+          ),
+          List(
+            ValDef(Modifiers(), TermName("kwargs"), AST_TYPE_MAP_STRING_ANY, EmptyTree)
+          )
+        ), TypeTree(),
         Apply(
-          Apply(Ident(TermName("createFromNamedSymbolsNoCheck")),
-          List(Literal(Constant("LeakyReLU")))),
-        List(Ident(TermName("kwargs")))
-        ))
-      /*
-      DefDef(Modifiers(), TermName("f"), List(), List(List(ValDef(Modifiers(),
-        TermName("a"), Ident(TypeName("Int")), EmptyTree)),
-        List(ValDef(Modifiers(), TermName("b"),
-        Ident(TypeName("String")), EmptyTree))), TypeTree(),
-        Apply(Select(Ident(TermName("a")), TermName("$plus")), List(Ident(TermName("b")))))
-      DefDef(Modifiers(), newTermName("f"), List(),
-        List(List(ValDef(Modifiers(PARAM), newTermName("a"),
-        Ident(newTypeName("Int")), EmptyTree)), List(ValDef(Modifiers(PARAM),
-        newTermName("b"), Ident(newTypeName("String")), EmptyTree))),
-        TypeTree(), Apply(Select(Ident(newTermName("a")), newTermName("$plus")),
-        List(Ident(newTermName("b")))))
-      */
+          Apply(
+            Ident(TermName("createFromNamedSymbolsNoCheck")),
+            List(Literal(Constant("LeakyReLU")))
+          ),
+          List(Ident(TermName("kwargs")))
+        )
+      )
     )
 
     // pattern match on the inputs
@@ -98,9 +103,45 @@ object ImplMacros {
     result
   }
 
-  private def initSymbolModule(): List[Long] = {
-    val symbolList = ListBuffer.empty[Long]
+  // List and add all the atomic symbol functions to current module.
+  private def initSymbolModule(): Map[String, SymbolFunction] = {
+    val symbolList = ListBuffer.empty[SymbolHandle]
     _LIB.mxSymbolListAtomicSymbolCreators(symbolList)
-    symbolList.toList
+    symbolList.map(makeAtomicSymbolFunction).toMap
+  }
+
+  // Create an atomic symbol function by handle and function name.
+  private def makeAtomicSymbolFunction(handle: SymbolHandle): (String, SymbolFunction) = {
+    val name = new RefString
+    val desc = new RefString
+    val keyVarNumArgs = new RefString
+    val numArgs = new RefInt
+    val argNames = ListBuffer.empty[String]
+    val argTypes = ListBuffer.empty[String]
+    val argDescs = ListBuffer.empty[String]
+
+    _LIB.mxSymbolGetAtomicSymbolInfo(
+      handle, name, desc, numArgs, argNames, argTypes, argDescs, keyVarNumArgs)
+    val paramStr = ctypes2docstring(argNames, argTypes, argDescs)
+    val extraDoc: String = if (keyVarNumArgs.value != null && keyVarNumArgs.value.length > 0) {
+        s"This function support variable length of positional input (${keyVarNumArgs.value})."
+      } else {
+        ""
+      }
+    val docStr = s"${name.value}\n${desc.value}\n\n$paramStr\n$extraDoc\n"
+    println("Atomic Symbol function defination:\n" + docStr)
+    (name.value, new SymbolFunction(handle, keyVarNumArgs.value))
+  }
+
+  // Convert ctypes returned doc string information into parameters docstring.
+  def ctypes2docstring(argNames: Seq[String],
+                       argTypes: Seq[String],
+                       argDescs: Seq[String]): String = {
+    val params =
+      (argNames zip argTypes zip argDescs) map { case ((argName, argType), argDesc) =>
+        val desc = if (argDesc.isEmpty) "" else s"\n$argDesc"
+        s"$argName : $argType$desc"
+      }
+    s"Parameters\n----------\n${params.mkString("\n")}\n"
   }
 }
