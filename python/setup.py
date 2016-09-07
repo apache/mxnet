2ec7bbe46058a81c2c17c2bb5f18cbc8161adb0f
Tianqi Chen <tqchen@users.noreply.github.com>
2016-09-07 16:08:55 -0700

    [PYTHON] Optional Cython Module for Symbols (#3242)
    * [CYTHON] Checkin cython enhancement
    
    * fix lint
    
    * [DOC] Move common doc to base

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,8 @@
 # pylint: disable=invalid-name, exec-used
 """Setup mxnet package."""
 from __future__ import absolute_import
-import os
-from setuptools import setup
+import os, sys
+from distutils.core import setup
 
 # We can not import `mxnet.info.py` in setup.py directly since mxnet/__init__.py
 # Will be invoked which introduces dependences
@@ -14,6 +14,30 @@ exec(compile(open(libinfo_py, "rb").read(), libinfo_py, 'exec'), libinfo, libinf
 LIB_PATH = libinfo['find_lib_path']()
 __version__ = libinfo['__version__']
 
+def config_cython():
+    try:
+        from Cython.Build import cythonize
+        from distutils.extension import Extension
+        if sys.version_info >= (3, 0):
+            subdir = "_cy3"
+        else:
+            subdir = "_cy2"
+        ret = []
+        path = "mxnet/cython"
+
+        for fn in os.listdir(path):
+            if not fn.endswith(".pyx"):
+                continue
+            ret.append(Extension(
+                "mxnet/%s/%s" % (subdir, fn[:-4]),
+                ["mxnet/cython/%s" % fn],
+                include_dirs=["../include/", "../nnvm/include"],
+                language="c++"))
+        return cythonize(ret)
+    except:
+        print("WARNING: Cython is not installed, will compile without cython module")
+        return []
+
 setup(name='mxnet',
       version=__version__,
       description=open(os.path.join(CURRENT_DIR, 'README.md')).read(),
@@ -23,4 +47,5 @@ setup(name='mxnet',
       zip_safe=False,
       packages=['mxnet', 'mxnet.module'],
       data_files=[('mxnet', [LIB_PATH[0]])],
-      url='https://github.com/dmlc/mxnet')
+      url='https://github.com/dmlc/mxnet',
+      ext_modules=config_cython())
