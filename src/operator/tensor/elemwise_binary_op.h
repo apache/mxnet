c78b397ab9e694b1dfed2ff776581c02e26c19c5
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-09-10 23:41:04 -0500

    [WIP] Element wise op refactor (#3245)
    * [OPERATOR] Refactor Unary Ops
    
    * [OPERATOR] Refactor Binary Scalar Ops
    
    * Use alias

---
+++ src/operator/tensor/elemwise_binary_op.h
@@ -0,0 +1,51 @@
+/*!
+ *  Copyright (c) 2016 by Contributors
+ * \file elemwise_binary_op.h
+ * \brief Function defintion of elementwise binary operators
+ */
+#ifndef MXNET_OPERATOR_TENSOR_ELEMWISE_BINARY_OP_H_
+#define MXNET_OPERATOR_TENSOR_ELEMWISE_BINARY_OP_H_
+
+#include <mxnet/operator_util.h>
+#include <vector>
+#include <utility>
+#include "../mshadow_op.h"
+#include "../elemwise_op_common.h"
+
+namespace mxnet {
+namespace op {
+template<typename xpu, typename OP>
+void BinaryCompute(const nnvm::NodeAttrs& attrs,
+                   const OpContext& ctx,
+                   const std::vector<TBlob>& inputs,
+                   const std::vector<OpReqType>& req,
+                   const std::vector<TBlob>& outputs) {
+  using namespace mshadow;
+  using namespace mshadow::expr;
+  Stream<xpu> *s = ctx.get_stream<xpu>();
+  CHECK_EQ(inputs[0].type_flag_, outputs[0].type_flag_);
+  CHECK_EQ(inputs[1].type_flag_, outputs[0].type_flag_);
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    Tensor<xpu, 1, DType> out = outputs[0].FlatTo1D<xpu, DType>(s);
+    Tensor<xpu, 1, DType> lhs = inputs[0].FlatTo1D<xpu, DType>(s);
+    Tensor<xpu, 1, DType> rhs = inputs[1].FlatTo1D<xpu, DType>(s);
+    ASSIGN_DISPATCH(out, req[0], F<OP>(lhs, rhs));
+  });
+}
+
+#define MXNET_OPERATOR_REGISTER_BINARY(name)                    \
+  NNVM_REGISTER_OP(name)                                        \
+  .set_num_inputs(2)                                            \
+  .set_num_outputs(1)                                           \
+  .attr<nnvm::FInferShape>("FInferShape", ElemwiseShape<2, 1>)  \
+  .attr<nnvm::FInferType>("FInferType", ElemwiseType<2, 1>)     \
+  .attr<nnvm::FInplaceOption>("FInplaceOption",                 \
+    [](const NodeAttrs& attrs){                                 \
+      return std::vector<std::pair<int, int> >{{0, 0}, {1, 0}}; \
+    })                                                          \
+  .add_argument("lhs", "NDArray", "first input")                \
+  .add_argument("rhs", "NDArray", "second input")
+
+}  // namespace op
+}  // namespace mxnet
+#endif  // MXNET_OPERATOR_TENSOR_ELEMWISE_BINARY_OP_H_
