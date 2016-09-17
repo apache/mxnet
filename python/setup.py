d90f2f26aa59393e6e3f9c01682333eb552c2abc
Hu Shiwen <yajiedesign@gmail.com>
2016-09-18 00:34:46 +0800

    fix cpython in windows (#3309)

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,8 @@ import os
 import sys
 # need to use distutils.core for correct placement of cython dll
 from distutils.core import setup
-#from setuptools import setup
+
+# from setuptools import setup
 
 # We can not import `mxnet.info.py` in setup.py directly since mxnet/__init__.py
 # Will be invoked which introduces dependences
@@ -17,11 +18,12 @@ exec(compile(open(libinfo_py, "rb").read(), libinfo_py, 'exec'), libinfo, libinf
 LIB_PATH = libinfo['find_lib_path']()
 __version__ = libinfo['__version__']
 
+
 def config_cython():
     """Try to configure cython and retyurn cython configuration"""
     try:
         from Cython.Build import cythonize
-        #from setuptools.extension import Extension
+        # from setuptools.extension import Extension
         from distutils.extension import Extension
         if sys.version_info >= (3, 0):
             subdir = "_cy3"
@@ -29,6 +31,12 @@ def config_cython():
             subdir = "_cy2"
         ret = []
         path = "mxnet/cython"
+        if os.name == 'nt':
+            library_dirs = ['mxnet']
+            libraries = ['libmxnet']
+        else:
+            library_dirs = None
+            libraries = None
 
         for fn in os.listdir(path):
             if not fn.endswith(".pyx"):
@@ -37,11 +45,14 @@ def config_cython():
                 "mxnet/%s/.%s" % (subdir, fn[:-4]),
                 ["mxnet/cython/%s" % fn],
                 include_dirs=["../include/", "../nnvm/include"],
+                library_dirs=library_dirs,
+                libraries=libraries,
                 language="c++"))
         return cythonize(ret)
     except ImportError:
         print("WARNING: Cython is not installed, will compile without cython module")
         return []
+
 
 setup(name='mxnet',
       version=__version__,
@@ -53,7 +64,7 @@ setup(name='mxnet',
       packages=[
           'mxnet', 'mxnet.module', 'mxnet._ctypes',
           'mxnet._cy2', 'mxnet._cy3',
-          ],
+      ],
       data_files=[('mxnet', [LIB_PATH[0]])],
       url='https://github.com/dmlc/mxnet',
       ext_modules=config_cython())
