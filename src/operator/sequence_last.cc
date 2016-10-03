7de889be863a7816426888adf56e66a544523153
Sebastian Bodenstein <sebastianb@wolfram.com>
2016-10-03 23:48:39 +0200

    improving documentation of sequence layers

--- src/operator/sequence_last.cc
+++ src/operator/sequence_last.cc
@@ -8,18 +8,18 @@
 
 namespace mxnet {
 namespace op {
-template<>
+template <>
 Operator *CreateOp<cpu>(SequenceLastParam param, int dtype) {
   Operator *op = NULL;
-  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
-    op = new SequenceLastOp<cpu, DType>(param);
-  })
+  MSHADOW_REAL_TYPE_SWITCH(dtype, DType,
+                           { op = new SequenceLastOp<cpu, DType>(param); })
   return op;
 }
 
 // DO_BIND_DISPATCH comes from operator_common.h
-Operator *SequenceLastProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                                     std::vector<int> *in_type) const {
+Operator *SequenceLastProp::CreateOperatorEx(Context ctx,
+                                             std::vector<TShape> *in_shape,
+                                             std::vector<int> *in_type) const {
   std::vector<TShape> out_shape, aux_shape;
   std::vector<int> out_type, aux_type;
   CHECK(InferType(in_type, &out_type, &aux_type));
@@ -30,20 +30,21 @@ Operator *SequenceLastProp::CreateOperatorEx(Context ctx, std::vector<TShape> *i
 DMLC_REGISTER_PARAMETER(SequenceLastParam);
 
 MXNET_REGISTER_OP_PROPERTY(SequenceLast, SequenceLastProp)
-.describe("Takes the last element of a sequence. Takes an n-dimensional tensor of "
-"the form (max sequence length, batchsize, other dims) and return s an (n-1)-dimensional tensor"
-"of the form (batchsize, other dims). This layer takes an optional input tensor "
-"sequence_length\" (list of sequence lengths of length batchsize) when the "
-"\"sequence_length\" option is set to true. This allows the layer to handle "
-"variable-length sequences. If \"sequence_length\" is false, then each example "
+    .describe(
+"Takes the last element of a sequence. Takes an n-dimensional tensor of "
+"the form [max sequence length, batchsize, other dims] and returns a (n-1)-dimensional tensor "
+"of the form [batchsize, other dims]. This operator takes an optional input tensor "
+"sequence_length of positive ints of dimension [batchsize] when the "
+"sequence_length option is set to true. This allows the operator to handle "
+"variable-length sequences. If sequence_length is false, then each example "
 "in the batch is assumed to have the max sequence length."
 )
-.add_argument("data", "Symbol",
-"n-dimensional input tensor of the form (max sequence length, batchsize, other dims)")
-.add_argument("sequence_length", "Symbol",
-"vector of sequence lengths of size batchsize")
-.add_arguments(SequenceLastParam::__FIELDS__());
-
+    .add_argument("data", "Symbol",
+                  "n-dimensional input tensor of the form [max sequence "
+                  "length, batchsize, other dims]")
+    .add_argument("sequence_length", "Symbol",
+                  "vector of sequence lengths of size batchsize")
+    .add_arguments(SequenceLastParam::__FIELDS__());
 
 }  // namespace op
 }  // namespace mxnet
--- src/operator/sequence_reverse.cc
+++ src/operator/sequence_reverse.cc
@@ -8,18 +8,18 @@
 
 namespace mxnet {
 namespace op {
-template<>
+template <>
 Operator *CreateOp<cpu>(SequenceReverseParam param, int dtype) {
   Operator *op = NULL;
-  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, {
-    op = new SequenceReverseOp<cpu, DType>(param);
-  })
+  MSHADOW_REAL_TYPE_SWITCH(dtype, DType,
+                           { op = new SequenceReverseOp<cpu, DType>(param); })
   return op;
 }
 
 // DO_BIND_DISPATCH comes from operator_common.h
-Operator *SequenceReverseProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                                     std::vector<int> *in_type) const {
+Operator *SequenceReverseProp::CreateOperatorEx(
+    Context ctx, std::vector<TShape> *in_shape,
+    std::vector<int> *in_type) const {
   std::vector<TShape> out_shape, aux_shape;
   std::vector<int> out_type, aux_type;
   CHECK(InferType(in_type, &out_type, &aux_type));
@@ -30,20 +30,20 @@ Operator *SequenceReverseProp::CreateOperatorEx(Context ctx, std::vector<TShape>
 DMLC_REGISTER_PARAMETER(SequenceReverseParam);
 
 MXNET_REGISTER_OP_PROPERTY(SequenceReverse, SequenceReverseProp)
-.describe("Reverses the elements in each sequence. Takes an "
-"n-dimensional tensor of the form (max sequence length, batchsize, other dims) and "
-"returns a tensor of the same shape. This operator takes an optional input tensor "
-"sequence_length\" (list of sequence lengths of length batchsize) when the "
-"\"sequence_length\" option is set to true. This allows the layer to handle "
-"variable-length sequences. If \"sequence_length\" is false, then each example"
+    .describe(
+"Reverses the elements of each sequence. Takes an n-dimensional tensor of the form "
+"[max sequence length, batchsize, other dims] and returns a tensor of the same shape. "
+"This operator takes an optional input tensor sequence_length of positive ints of dimension "
+"[batchsize] when the sequence_length option is set to true. This allows the operator to "
+"handle variable-length sequences. If sequence_length is false, then each example "
 "in the batch is assumed to have the max sequence length."
 )
-.add_argument("data", "Symbol",
-"n-dimensional input tensor of the form (max sequence length, batchsize, other dims)")
-.add_argument("sequence_length", "Symbol",
-"vector of sequence lengths of size batchsize")
-.add_arguments(SequenceReverseParam::__FIELDS__());
-
+    .add_argument("data", "Symbol",
+                  "n-dimensional input tensor of the form [max sequence "
+                  "length, batchsize, other dims]")
+    .add_argument("sequence_length", "Symbol",
+                  "vector of sequence lengths of size batchsize")
+    .add_arguments(SequenceReverseParam::__FIELDS__());
 
 }  // namespace op
 }  // namespace mxnet
