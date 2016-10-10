1e2a00d9c813ae1ddb5fc3ae6926db1ab64be2be
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-10-10 08:53:32 +0200

    Nnvm (#3485)
    * remove ccsgd
    
    remove cc optimizer
    
    fix
    
    * fix slice
    
    * Move broadcast reduce op to nnvm
    
    * move smooth l1 and softmax cross entropy to nnvm
    
    * remove simple op
    
    * lint
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/operator/tensor/elemwise_binary_scalar_op.cc
+++ src/operator/tensor/elemwise_binary_scalar_op.cc
@@ -94,5 +94,14 @@ MXNET_OPERATOR_REGISTER_BINARY(_backward_hypot_scalar)
 .set_attr_parser([](NodeAttrs* attrs) {attrs->parsed = std::stod(attrs->dict["scalar"]); })
 .set_attr<FCompute>("FCompute<cpu>", BinaryScalarBackward<cpu, mshadow_op::hypot_grad_left>);
 
+MXNET_OPERATOR_REGISTER_BINARY_SCALAR(smooth_l1)
+.MXNET_DESCRIBE("Calculate Smooth L1 Loss(lhs, scalar)")
+.set_attr<FCompute>("FCompute<cpu>", BinaryScalarCompute<cpu, mshadow_op::smooth_l1_loss>)
+.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{ "_backward_smooth_l1" });
+
+MXNET_OPERATOR_REGISTER_BINARY(_backward_smooth_l1)
+.set_attr_parser([](NodeAttrs* attrs) {attrs->parsed = std::stod(attrs->dict["scalar"]); })
+.set_attr<FCompute>("FCompute<cpu>", BinaryScalarBackward<cpu, mshadow_op::smooth_l1_gradient>);
+
 }  // namespace op
 }  // namespace mxnet
