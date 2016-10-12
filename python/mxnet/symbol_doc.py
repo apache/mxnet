2d5c5c8e1e879675b6d1b86b54e5349ab0de7a69
Chiyuan Zhang <pluskid@gmail.com>
2016-10-12 18:21:03 -0400

    add regression test demo for FullyConnected

--- python/mxnet/symbol_doc.py
+++ python/mxnet/symbol_doc.py
@@ -1,13 +1,60 @@
 # coding: utf-8
 """Extra symbol documents"""
 
+import numpy
+from .context import cpu, gpu
+from .ndarray import array
+
 class SymbolDoc(object):
     """The base class for attaching doc to operators."""
     
     @staticmethod
     def get_output_shape(sym, **input_shapes):
+        """Get user friendly information of the output shapes."""
         s_inputs, s_outputs, s_aux = sym.infer_shape(**input_shapes)
         return dict(zip(sym.list_outputs(), s_outputs))
+
+    @staticmethod
+    def default_context():
+        """Get default context for regression test."""
+        # TODO: get context from environment variable to support
+        # testing with GPUs
+        return cpu()
+
+    @staticmethod
+    def default_dtype():
+        """Get default data type for regression test."""
+        # TODO: get default dtype from environment variable
+        return numpy.float32
+
+    @staticmethod
+    def default_numerical_threshold():
+        """Get default numerical threshold for regression test."""
+        # TODO: get from env variable, different threshold might
+        # be needed for different device and dtype
+        return 1e-6
+
+    @staticmethod
+    def random_arrays(*shapes):
+        """Generate some random numpy arrays."""
+        return [numpy.random.randn(*s).astype(SymbolDoc.default_dtype())
+                for s in shapes]
+
+    @staticmethod
+    def almost_equal(a, b, threshold=None):
+        threshold = threshold or SymbolDoc.default_numerical_threshold()
+        return numpy.max(numpy.abs(a-b)) <= threshold
+
+    @staticmethod
+    def simple_forward(sym, ctx=None, **inputs):
+        ctx = ctx or SymbolDoc.default_context()
+        inputs = {k: array(v) for k, v in inputs.iteritems()}
+        exe = sym.bind(ctx, args=inputs)
+        exe.forward()
+        outputs = [x.asnumpy() for x in exe.outputs]
+        if len(outputs) == 1:
+            outputs = outputs[0]
+        return outputs
 
 
 class FullyConnectedDoc(SymbolDoc):
@@ -35,6 +82,16 @@ class FullyConnectedDoc(SymbolDoc):
     >>> net = FullyConnected(data=net, num_hidden=10, name='pred')
     >>> net
     <Symbol pred>
+
+    Regression Test
+    ---------------
+    >>> dim_in, dim_out = (3, 4)
+    >>> x, w, b = SymbolDoc.random_arrays((10, dim_in), (dim_out, dim_in), (dim_out,))
+    >>> op = FullyConnected(num_hidden=dim_out, name='FC')
+    >>> out = SymbolDoc.simple_forward(op, FC_data=x, FC_weight=w, FC_bias=b)
+    >>> out_np = numpy.dot(x, w.T) + b
+    >>> SymbolDoc.almost_equal(out, out_np)
+    True
     """
     pass
 
