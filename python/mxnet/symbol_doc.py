2713e21cb137076faa0fde40a67ed8c27394542d
Chiyuan Zhang <pluskid@gmail.com>
2016-10-12 17:44:55 -0400

    add infrastructure for symbol doctest

--- python/mxnet/symbol_doc.py
+++ python/mxnet/symbol_doc.py
@@ -2,93 +2,102 @@
 """Extra symbol documents"""
 
 class SymbolDoc(object):
-    """The basic class"""
+    """The base class for attaching doc to operators."""
+    
+    @staticmethod
+    def get_output_shape(sym, **input_shapes):
+        s_inputs, s_outputs, s_aux = sym.infer_shape(**input_shapes)
+        return dict(zip(sym.list_outputs(), s_outputs))
+
+
+class FullyConnectedDoc(SymbolDoc):
+    """
+    Examples
+    --------
+    Construct a fully connected operator with target dimension 512.
+
+    >>> data = Variable('data')  # or some constructed NN
+    >>> op = FullyConnected(data=data,
+    ...                     num_hidden=512,
+    ...                     name='FC1')
+    >>> op
+    <Symbol FC1>
+    >>> SymbolDoc.get_output_shape(op, data=(128, 100))
+    {'FC1_output': (128L, 512L)}
+
+    A simple 3-layer MLP with ReLU activation:
+
+    >>> net = Variable('data')
+    >>> for i, dim in enumerate([128, 64]):
+    ...     net = FullyConnected(data=net, num_hidden=dim, name='FC%d' % i)
+    ...     net = Activation(data=net, act_type='relu', name='ReLU%d' % i)
+    >>> # 10-class predictor (e.g. MNIST)
+    >>> net = FullyConnected(data=net, num_hidden=10, name='pred')
+    >>> net
+    <Symbol pred>
+    """
     pass
+
 
 class ConcatDoc(SymbolDoc):
     """
     Examples
     --------
-    >>> import mxnet as mx
-    >>> data = mx.nd.array(range(6)).reshape((2,1,3))
-    >>> print "input shape = %s" % data.shape
-    >>> print "data = %s" % (data.asnumpy(), )
-    input shape = (2L, 1L, 3L)
-    data = [[[ 0.  1.  2.]]
-     [[ 3.  4.  5.]]]
+    Concat two (or more) inputs along a specific dimension:
 
-    >>> # concat two variables on different dimensions
-    >>> a = mx.sym.Variable('a')
-    >>> b = mx.sym.Variable('b')
-    >>> for dim in range(3):
-    ...     cat = mx.sym.Concat(a, b, dim=dim)
-    ...     exe = cat.bind(ctx=mx.cpu(), args={'a':data, 'b':data})
-    ...     exe.forward()
-    ...     out = exe.outputs[0]
-    ...     print "concat at dim = %d" % dim
-    ...     print "shape = %s" % (out.shape, )
-    ...     print "results = %s" % (out.asnumpy(), )
-    concat at dim = 0
-    shape = (4L, 1L, 3L)
-    results = [[[ 0.  1.  2.]]
-     [[ 3.  4.  5.]]
-     [[ 0.  1.  2.]]
-     [[ 3.  4.  5.]]]
-    concat at dim = 1
-    shape = (2L, 2L, 3L)
-    results = [[[ 0.  1.  2.]
-      [ 0.  1.  2.]]
-     [[ 3.  4.  5.]
-      [ 3.  4.  5.]]]
-    concat at dim = 2
-    shape = (2L, 1L, 6L)
-    results = [[[ 0.  1.  2.  0.  1.  2.]]
-     [[ 3.  4.  5.  3.  4.  5.]]]
+    >>> a = Variable('a')
+    >>> b = Variable('b')
+    >>> c = Concat(a, b, dim=1, name='my-concat')
+    >>> c
+    <Symbol my-concat>
+    >>> SymbolDoc.get_output_shape(c, a=(128, 10, 3, 3), b=(128, 15, 3, 3))
+    {'my-concat_output': (128L, 25L, 3L, 3L)}
+
+    Note the shape should be the same except on the dimension that is being
+    concatenated.
     """
     pass
 
-class BroadcastPlusDoc(SymbolDoc):
-    """add with broadcast
 
+class BroadcastPlusDoc(SymbolDoc):
+    """
     Examples
     --------
-    >>> a = mx.sym.Variable('a')
-    >>> b = mx.sym.Variable('b')
-    >>> c = mx.sym.BroadcastPlus(a, b)
 
-    >>> dev = mx.cpu();
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((2,2)), 'b' : mx.nd.ones((2,2))})
+    >>> a = Variable('a')
+    >>> b = Variable('b')
+    >>> c = broadcast_plus(a, b)
+
+    Normal summation with matching shapes:
+
+    >>> dev = mxnet.context.cpu();
+    >>> x = c.bind(dev, args={'a': mxnet.nd.ones((2, 2)), 'b' : mxnet.nd.ones((2, 2))})
     >>> x.forward()
+    [<NDArray 2x2 @cpu(0)>]
     >>> print x.outputs[0].asnumpy()
     [[ 2.  2.]
      [ 2.  2.]]
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((2,2)), 'b' : mx.nd.ones((1,1))})
+
+    Broadcasting:
+
+    >>> x = c.bind(dev, args={'a': mxnet.nd.ones((2, 2)), 'b' : mxnet.nd.ones((1, 1))})
     >>> x.forward()
+    [<NDArray 2x2 @cpu(0)>]
     >>> print x.outputs[0].asnumpy()
     [[ 2.  2.]
      [ 2.  2.]]
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((2,1)), 'b' : mx.nd.ones((1,2))})
+
+    >>> x = c.bind(dev, args={'a': mxnet.nd.ones((2, 1)), 'b' : mxnet.nd.ones((1, 2))})
     >>> x.forward()
+    [<NDArray 2x2 @cpu(0)>]
     >>> print x.outputs[0].asnumpy()
     [[ 2.  2.]
      [ 2.  2.]]
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((1,2)), 'b' : mx.nd.ones((2,1))})
+
+    >>> x = c.bind(dev, args={'a': mxnet.nd.ones((1, 2)), 'b' : mxnet.nd.ones((2, 1))})
     >>> x.forward()
+    [<NDArray 2x2 @cpu(0)>]
     >>> print x.outputs[0].asnumpy()
     [[ 2.  2.]
      [ 2.  2.]]
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((2,2,2)), 'b' : mx.nd.ones((1,2,1))}
-    >>> x.forward()
-    >>> print x.outputs[0].asnumpy()
-    [[[ 2.  2.]
-      [ 2.  2.]]
-     [[ 2.  2.]
-      [ 2.  2.]]]
-    >>> x = c.bind(dev, args={'a': mx.nd.ones((2,1,1)), 'b' : mx.nd.ones((2,2,2))})
-    >>> x.forward()
-    >>> print x.outputs[0].asnumpy()
-    [[[ 2.  2.]
-      [ 2.  2.]]
-     [[ 2.  2.]
-      [ 2.  2.]]]
     """
