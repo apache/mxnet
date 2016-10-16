2e9d9e6b533a0e11f7c0f7a8eecd2e96ba77593a
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-10-17 01:59:09 +0200

    jenkins test (#3534)

---
+++ tests/jenkins/run_test.sh
@@ -0,0 +1,32 @@
+echo "BUILD make"
+cp make/config.mk .
+echo "USE_CUDA=1" >> config.mk
+echo "USE_CUDA_PATH=/usr/local/cuda" >> config.mk
+echo "USE_CUDNN=1" >> config.mk
+make -j 4 || exit -1
+
+echo "BUILD lint"
+make lint || exit -1
+
+echo "BUILD cpp_test"
+make -j 4 test || exit -1
+export MXNET_ENGINE_INFO=true
+for test in tests/cpp/*_test; do
+    ./$test || exit -1
+done
+export MXNET_ENGINE_INFO=false
+
+echo "BUILD python_test"
+nosetests --verbose tests/python/unittest || exit -1
+nosetests --verbose tests/python/gpu/test_operator_gpu.py || exit -1
+nosetests --verbose tests/python/train || exit -1
+
+echo "BUILD python3_test"
+nosetests3 --verbose tests/python/unittest || exit -1
+nosetests3 --verbose tests/python/gpu/test_operator_gpu.py || exit -1
+nosetests3 --verbose tests/python/train || exit -1
+
+# echo "BUILD julia_test"
+# export PATH="${PATH}:${HOME}/julia/bin"
+# export MXNET_HOME="${PWD}"
+# julia -e 'Pkg.clone("MXNet"); Pkg.checkout("MXNet"); Pkg.build("MXNet"); Pkg.test("MXNet")' || exit -1
