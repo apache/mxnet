ba4972022d7f791452258148c4be167c5ac5938b
zhenlin <zhenlin.luo@intel.com>
2016-10-22 19:50:56 -0700

    add new MKL 1005 support

--- prepare_mkl.sh
+++ prepare_mkl.sh
@@ -64,11 +64,10 @@ echo $VERSION_LINE  # Return Version Line
 # MKL
 DST=`dirname $0`
 OMP=0
-VERSION_MATCH=20160706
-ARCHIVE_BASENAME=mklml_lnx_2017.0.0.20160801.tgz
+VERSION_MATCH=20120601
+ARCHIVE_BASENAME=mklml_lnx_2017.0.1.20161005.tgz
 MKL_CONTENT_DIR=`echo $ARCHIVE_BASENAME | rev | cut -d "." -f 2- | rev`
-GITHUB_RELEASE_TAG=self_containted_MKLGOLD
-MKLURL="https://github.com/intel/caffe/releases/download/$GITHUB_RELEASE_TAG/$ARCHIVE_BASENAME"
+MKLURL="https://github.com/dmlc/web-data/raw/master/mxnet/mklml-release/$ARCHIVE_BASENAME"
 # there are diffrent MKL lib to be used for GCC and for ICC
 reg='^[0-9]+$'
 VERSION_LINE=`GetVersionName $MKLROOT`
