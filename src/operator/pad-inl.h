05e58ae5c0c35e35e3e28ebb249cc41156fb6b30
Sebastian Bodenstein <sebastianb@wolfram.com>
2016-10-24 20:35:39 -0500

    first commit of pad layer

---
+++ src/operator/pad-inl.h
@@ -0,0 +1,182 @@
+/*!
+ * Copyright (c) 2016 by Contributors
+ * \file pad-inl.h
+ * \brief
+ * \author Sebastian Bodenstien
+*/
+
+#ifndef MXNET_OPERATOR_PAD_INL_H_
+#define MXNET_OPERATOR_PAD_INL_H_
+
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+#include "./operator_common.h"
+#include "./mshadow_op.h"
+
+namespace mxnet {
+namespace op {
+
+namespace pad_enum {
+enum PadOpInputs { kData };
+enum PadOpType { kConstant, kReflect, kReplicate };
+enum PadOpOutputs { kOut };
+}
+
+struct PadParam : public dmlc::Parameter<PadParam> {
+  int pad_type;
+  double padding_constant;
+  TShape pad_shape;
+  DMLC_DECLARE_PARAMETER(PadParam) {
+    DMLC_DECLARE_FIELD(pad_type)
+        .add_enum("constant", pad_enum::kConstant)
+        .add_enum("reflect", pad_enum::kReflect)
+        .add_enum("replicate", pad_enum::kReplicate)
+        .describe("Pooling type to be applied.");
+
+    DMLC_DECLARE_FIELD(pad_shape).describe("pad for pooling. ");
+    DMLC_DECLARE_FIELD(padding_constant)
+        .describe("pad for pooling. ")
+        .set_default(0.0);
+  }
+};
+
+template <typename xpu, typename DType>
+class PadOp : public Operator {
+ public:
+  explicit PadOp(PadParam p) { this->param_ = p; }
+
+  virtual void Forward(const OpContext &ctx, const std::vector<TBlob> &in_data,
+                       const std::vector<OpReqType> &req,
+                       const std::vector<TBlob> &out_data,
+                       const std::vector<TBlob> &aux_args) {
+    using namespace mshadow;
+    using namespace mshadow::expr;
+    CHECK_EQ(in_data.size(), 1);
+    CHECK_EQ(out_data.size(), 1);
+    Stream<xpu> *s = ctx.get_stream<xpu>();
+
+    // Get any size input + output into required form
+    int rank = in_data[pad_enum::kData].ndim();
+    DType padding_constant = param_.padding_constant;
+
+    if (rank == 4) {
+      Tensor<xpu, 4, DType> data =
+          in_data[pad_enum::kData].get<xpu, 4, DType>(s);
+      Tensor<xpu, 4, DType> out =
+          out_data[pad_enum::kOut].get<xpu, 4, DType>(s);
+      pad_image_2d(out, data, param_.pad_shape, param_.pad_type,
+                   padding_constant);
+    } else {
+      CHECK(false) << "Only ... ";
+    }
+
+    // Assign(out, req[pad_enum::kOut], F<mshadow_op::identity>(data));
+  }
+
+  virtual void Backward(const OpContext &ctx,
+                        const std::vector<TBlob> &out_grad,
+                        const std::vector<TBlob> &in_data,
+                        const std::vector<TBlob> &out_data,
+                        const std::vector<OpReqType> &req,
+                        const std::vector<TBlob> &in_grad,
+                        const std::vector<TBlob> &aux_args) {
+    using namespace mshadow;
+    using namespace mshadow::expr;
+    CHECK_EQ(out_grad.size(), 1);
+    CHECK_EQ(out_data.size(), 1);
+    Stream<xpu> *s = ctx.get_stream<xpu>();
+
+    // Get any size input + output into required form
+    int rank = in_grad[pad_enum::kData].ndim();
+    DType padding_constant = param_.padding_constant;
+
+    // Currently only support rank 4
+    Tensor<xpu, 4, DType> in = in_grad[pad_enum::kData].get<xpu, 4, DType>(s);
+    Tensor<xpu, 4, DType> out = out_grad[pad_enum::kOut].get<xpu, 4, DType>(s);
+    in = 0.0f;
+    std::cout << in[0][0][0][0] << std::endl;
+    pad_image_2d_grad(in, out, param_.pad_shape, param_.pad_type,
+                      padding_constant);
+  }
+
+ private:
+  PadParam param_;
+};  // class PadOp
+
+template <typename xpu>
+Operator *CreateOp(PadParam param, int dtype);
+
+#if DMLC_USE_CXX11
+class PadProp : public OperatorProperty {
+ public:
+  int NumVisibleOutputs() const override { return 1; }
+
+  int NumOutputs() const override { return 1; }
+
+  std::vector<std::string> ListArguments() const override { return {"data"}; }
+
+  std::vector<std::string> ListOutputs() const override { return {"output"}; }
+
+  void Init(const std::vector<std::pair<std::string, std::string> > &kwargs)
+      override {
+    param_.Init(kwargs);
+  }
+
+  std::map<std::string, std::string> GetParams() const override {
+    return param_.__DICT__();
+  }
+
+  bool InferShape(std::vector<TShape> *in_shape, std::vector<TShape> *out_shape,
+                  std::vector<TShape> *aux_shape) const override {
+    using namespace mshadow;
+    CHECK_EQ(in_shape->size(), 1) << "Can only be one input to symbol.";
+
+    const TShape &dshape = (*in_shape)[pad_enum::kData];
+    if (dshape.ndim() == 0) return false;
+
+    TShape oshape = dshape;
+    for (int i = 0; i < dshape.ndim(); ++i) {
+      oshape[i] =
+          param_.pad_shape[2 * i] + param_.pad_shape[2 * i + 1] + dshape[i];
+    }
+
+    out_shape->clear();
+    out_shape->push_back(oshape);
+    return true;
+  }
+
+  OperatorProperty *Copy() const override {
+    auto ptr = new PadProp();
+    ptr->param_ = param_;
+    return ptr;
+  }
+
+  std::string TypeString() const override { return "Pad"; }
+
+  std::vector<int> DeclareBackwardDependency(
+      const std::vector<int> &out_grad, const std::vector<int> &in_data,
+      const std::vector<int> &out_data) const override {
+    return {out_grad[pad_enum::kOut]};
+  }
+
+  Operator *CreateOperator(Context ctx) const override {
+    LOG(FATAL) << "Not Implemented.";
+    return NULL;
+  }
+
+  Operator *CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
+                             std::vector<int> *in_type) const override;
+
+ private:
+  PadParam param_;
+};      // class PadProp
+#endif  // DMLC_USE_CXX11
+}  // namespace op
+}  // namespace mxnet
+#endif  // MXNET_OPERATOR_SEQUENCE_MASK_INL_H_
---
+++ src/operator/pad.cc
@@ -0,0 +1,233 @@
+/*!
+ * Copyright (c) 2015 by Contributors
+ * \file pad.cc
+ * \brief
+ * \author Sebastian Bodenstein
+*/
+#include "./pad-inl.h"
+
+namespace mshadow {
+
+////////////////////////////////////////////////////////////////////////////////
+// Special Case: 2d image (so only pad width + height)
+
+// Case 1: Replication Padding
+// single_image_2d_replicate adapted from Torch
+// https://github.com/torch/nn/blob/master/lib/THNN/generic/SpatialReplicationPadding.c
+template <typename DType>
+void single_image_2d_replicate(const Tensor<cpu, 3, DType> &dst,
+                               const Tensor<cpu, 3, DType> src,
+                               mxnet::TShape pad) {
+  const int nslices = src.size(0);
+  const int iheight = src.size(1);
+  const int iwidth = src.size(2);
+
+  const int oheight = dst.size(1);
+  const int owidth = dst.size(2);
+
+  const int pad_t = pad[4];
+  const int pad_l = pad[6];
+  int iStartX = std::max(0, -pad_l);
+  int iStartY = std::max(0, -pad_t);
+  int oStartX = std::max(0, pad_l);
+  int oStartY = std::max(0, pad_t);
+
+  int k, ip_x, ip_y;
+#pragma omp parallel for private(k, ip_x, ip_y)
+  for (k = 0; k < nslices; k++) {
+    int i, j;
+    for (i = 0; i < oheight; i++) {
+      for (j = 0; j < owidth; j++) {
+        if (j < pad_l) {
+          ip_x = pad_l;
+        } else if (j >= pad_l && j < iwidth + pad_l) {
+          ip_x = j;
+        } else {
+          ip_x = iwidth + pad_l - 1;
+        }
+        ip_x = ip_x - oStartX + iStartX;
+
+        if (i < pad_t) {
+          ip_y = pad_t;
+        } else if (i >= pad_t && i < iheight + pad_t) {
+          ip_y = i;
+        } else {
+          ip_y = iheight + pad_t - 1;
+        }
+        ip_y = ip_y - oStartY + iStartY;
+
+        DType *dest_p = dst.dptr_ + k * owidth * oheight + i * owidth + j;
+        DType *src_p = src.dptr_ + k * iwidth * iheight + ip_y * iwidth + ip_x;
+        *dest_p = *src_p;
+      }
+    }
+  }
+}
+
+template <typename DType>
+void single_image_2d_replicate_grad(const Tensor<cpu, 3, DType> &grad_in,
+                                    const Tensor<cpu, 3, DType> grad_out,
+                                    mxnet::TShape pad) {
+  const int nslices = grad_in.size(0);
+  const int iheight = grad_in.size(1);
+  const int iwidth = grad_in.size(2);
+
+  const int oheight = grad_out.size(1);
+  const int owidth = grad_out.size(2);
+
+  const int pad_t = pad[4];
+  const int pad_l = pad[6];
+  int iStartX = std::max(0, -pad_l);
+  int iStartY = std::max(0, -pad_t);
+  int oStartX = std::max(0, pad_l);
+  int oStartY = std::max(0, pad_t);
+
+  int k, ip_x, ip_y;
+#pragma omp parallel for private(k, ip_x, ip_y)
+  for (k = 0; k < nslices; k++) {
+    int i, j;
+    for (i = 0; i < oheight; i++) {
+      for (j = 0; j < owidth; j++) {
+        if (j < pad_l) {
+          ip_x = pad_l;
+        } else if (j >= pad_l && j < iwidth + pad_l) {
+          ip_x = j;
+        } else {
+          ip_x = iwidth + pad_l - 1;
+        }
+        ip_x = ip_x - oStartX + iStartX;
+
+        if (i < pad_t) {
+          ip_y = pad_t;
+        } else if (i >= pad_t && i < iheight + pad_t) {
+          ip_y = i;
+        } else {
+          ip_y = iheight + pad_t - 1;
+        }
+        ip_y = ip_y - oStartY + iStartY;
+
+        DType *src_p = grad_out.dptr_ + k * owidth * oheight + i * owidth + j;
+        DType *dest_p =
+            grad_in.dptr_ + k * iwidth * iheight + ip_y * iwidth + ip_x;
+        *dest_p += *src_p;
+      }
+    }
+  }
+}
+
+// Case 2: Zero Padding
+template <typename DType>
+void single_image_2d_constant(const Tensor<cpu, 3, DType> &dst,
+                              const Tensor<cpu, 3, DType> src,
+                              mxnet::TShape pad, DType padding_constant) {
+  const int pad_t = pad[4];
+  const int pad_l = pad[6];
+#pragma omp parallel for private(c, w, h)
+  for (index_t c = 0; c < dst.size(0); ++c) {
+    for (index_t h = 0; h < dst.size(1); ++h) {
+      for (index_t w = 0; w < dst.size(2); ++w) {
+        if ((w < pad_l) || (h < pad_t) || (h >= (src.size(1) + pad_t)) ||
+            (w >= (src.size(2) + pad_l))) {
+          dst[c][h][w] = padding_constant;
+        } else {
+          dst[c][h][w] = src[c][h - pad_t][w - pad_l];
+        }
+      }
+    }
+  }
+}
+
+template <typename DType>
+void single_image_2d_constant_grad(const Tensor<cpu, 3, DType> &in_grad,
+                                   const Tensor<cpu, 3, DType> out_grad,
+                                   mxnet::TShape pad, DType padding_constant) {
+  const int pad_t = pad[4];
+  const int pad_l = pad[6];
+#pragma omp parallel for private(c, w, h)
+  for (index_t c = 0; c < in_grad.size(0); ++c) {
+    for (index_t h = 0; h < in_grad.size(1); ++h) {
+      for (index_t w = 0; w < in_grad.size(2); ++w) {
+        in_grad[c][h][w] += out_grad[c][h + pad_t][w + pad_l];
+      }
+    }
+  }
+}
+
+// General 2d image case
+template <typename DType>
+void pad_image_2d(const Tensor<cpu, 4, DType> &dst,
+                  const Tensor<cpu, 4, DType> src, mxnet::TShape pad,
+                  int pad_type, DType padding_constant) {
+  for (index_t n = 0; n < dst.size(0); ++n) {
+    switch (pad_type) {
+      case mxnet::op::pad_enum::kReplicate:
+        single_image_2d_replicate(dst[n], src[n], pad);
+        break;
+      case mxnet::op::pad_enum::kConstant:
+        single_image_2d_constant(dst[n], src[n], pad, padding_constant);
+        break;
+    }
+  }
+}
+
+template <typename DType>
+void pad_image_2d_grad(const Tensor<cpu, 4, DType> &in_grad,
+                       const Tensor<cpu, 4, DType> out_grad, mxnet::TShape pad,
+                       int pad_type, DType padding_constant) {
+  for (index_t n = 0; n < in_grad.size(0); ++n) {
+    switch (pad_type) {
+      case mxnet::op::pad_enum::kReplicate:
+        single_image_2d_replicate_grad(in_grad[n], out_grad[n], pad);
+        break;
+      case mxnet::op::pad_enum::kConstant:
+        single_image_2d_constant_grad(in_grad[n], out_grad[n], pad,
+                                 padding_constant);
+        break;
+    }
+  }
+}
+
+// #pragma omp parallel for private(p)
+//     for (p = 0; p < nbatch; p++) {
+//       THNN_(SpatialReplicationPadding_updateOutput_frame)
+//       (input_data + p * nslices * iwidth * iheight,
+//        output_data + p * nslices * owidth * oheight, nslices, iwidth,
+//        iheight,
+//        owidth, oheight, pad_l, pad_r, pad_t, pad_b);
+//     }
+//   }
+
+//   /* cleanup */
+//   THTensor_(free)(input);
+// }
+
+}  // namespace mshadow
+
+namespace mxnet {
+namespace op {
+template <>
+Operator *CreateOp<cpu>(PadParam param, int dtype) {
+  Operator *op = NULL;
+  MSHADOW_REAL_TYPE_SWITCH(dtype, DType, { op = new PadOp<cpu, DType>(param); })
+  return op;
+}
+
+// DO_BIND_DISPATCH comes from operator_common.h
+Operator *PadProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
+                                    std::vector<int> *in_type) const {
+  std::vector<TShape> out_shape, aux_shape;
+  std::vector<int> out_type, aux_type;
+  CHECK(InferType(in_type, &out_type, &aux_type));
+  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
+  DO_BIND_DISPATCH(CreateOp, param_, (*in_type)[0]);
+}
+
+DMLC_REGISTER_PARAMETER(PadParam);
+
+MXNET_REGISTER_OP_PROPERTY(Pad, PadProp)
+    .describe("")
+    .add_argument("data", "Symbol", "n-dimensional input tensor.")
+    .add_arguments(PadParam::__FIELDS__());
+
+}  // namespace op
+}  // namespace mxnet
