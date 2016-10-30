9cda709261f9ac802b77b749ccb021a14fe5e6f1
Yizhi Liu <javelinjs@gmail.com>
2016-10-30 16:49:39 +0800

    [Scala] DType support (#3658)
    * fix nnvm scala compile err
    
    * scala test in jenkins
    
    * [scala] init NDArray with DType
    
    * [scala] dtype support in Executor

--- scala-package/core/src/main/scala/ml/dmlc/mxnet/DType.scala
+++ scala-package/core/src/main/scala/ml/dmlc/mxnet/DType.scala
@@ -7,4 +7,13 @@ object DType extends Enumeration {
   val Float16 = Value(2)
   val UInt8 = Value(3)
   val Int32 = Value(4)
+  private[mxnet] def numOfBytes(dtype: DType): Int = {
+    dtype match {
+      case DType.UInt8 => 1
+      case DType.Int32 => 4
+      case DType.Float16 => 2
+      case DType.Float32 => 4
+      case DType.Float64 => 8
+    }
+  }
 }
