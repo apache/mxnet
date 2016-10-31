1ca0ab0c0a1163291ecb4a69b98a1712ba2fd5d4
carwin <clcarwin@gmail.com>
2016-10-31 15:27:50 +0800

    Fix header declaration

--- src/operator/nnpack/nnpack_convolution-inl.h
+++ src/operator/nnpack/nnpack_convolution-inl.h
@@ -1,24 +1,9 @@
-/*******************************************************************************
-* Copyright 2016 Intel Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* \file mkl_convolution-inl.h
-* \brief
-* \author lingyan.guo@intel.com
-*         zhenlin.luo@intel.com
-*
-*******************************************************************************/
+/*!
+ * Copyright (c) 2016 by Contributors
+ * \file nnpack_convolution-inl.h
+ * \brief 
+ * \author Carwin
+*/
 #ifndef MXNET_OPERATOR_NNPACK_CONVOLUTION_INL_H_
 #define MXNET_OPERATOR_NNPACK_CONVOLUTION_INL_H_
 
