d2970a59df3490ebe9ffaec239033aba7713e2b2
Valeriu Lacatusu <lacatusu.valeriu@gmail.com>
2016-11-12 19:23:40 +0100

    Add missing initializer

--- example/ssd/train/initializer.py
+++ example/ssd/train/initializer.py
@@ -13,3 +13,22 @@ class ScaleInitializer(mx.init.Initializer):
             self._init_one(name, arr)
         else:
             raise ValueError('Unknown initialization pattern for %s' % name)
+            
+class CustomInitializer(mx.init.Xavier):
+    """
+    Customized initializer for ssd models
+    """
+    def __init__(self, rnd_type="uniform", factor_type="avg", magnitude=3):
+        super(CustomInitializer, self).__init__(rnd_type, factor_type, magnitude)
+
+    def _init_default(self, name, arr):
+        if name.endswith('init'):
+            self._init_zero(name, arr)
+        elif name.endswith('scale'):
+            try:
+                s = float(name[:-6].split('_')[-1])  # read scale from name
+                arr[:] = s
+            except:
+                self._init_one(name, arr)
+        else:
+            raise ValueError('Unknown initialization pattern for %s' % name)
