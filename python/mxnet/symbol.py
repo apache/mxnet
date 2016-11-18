bf43dcca9d6f2fd475e9348cd07b138c8440cb98
Leo Dirac <leopd@users.noreply.github.com>
2016-11-18 11:52:22 -0800

    Fixing pylint errors.

--- python/mxnet/symbol.py
+++ python/mxnet/symbol.py
@@ -459,10 +459,10 @@ class Symbol(object):
             return self._infer_shape_impl(False, *args, **kwargs)
         except MXNetError:
             print("infer_shape error. Arguments:")
-            for i,arg in enumerate(args):
-                print("  #%d: %s" % (i,arg))
-            for k,v in kwargs.items():
-                print("  %s: %s" % (k,v))
+            for i, arg in enumerate(args):
+                print("  #%d: %s" % (i, arg))
+            for k, v in kwargs.items():
+                print("  %s: %s" % (k, v))
             raise
 
     def infer_shape_partial(self, *args, **kwargs):
