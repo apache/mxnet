d824df4a48351c5e7d688efd649ff53a230445b2
Wei Wu <lazyparser@gmail.com>
2016-11-24 18:04:40 +0800

    fix a few dependence issues in install-mxnet-ubuntu-r.sh

--- setup-utils/install-mxnet-ubuntu-r.sh
+++ setup-utils/install-mxnet-ubuntu-r.sh
@@ -4,8 +4,7 @@
 # We recommend to install Microsoft RServer together with Intel MKL library for optimal performance
 # More information can be found here: 
 # https://blogs.technet.microsoft.com/machinelearning/2016/09/15/building-deep-neural-networks-in-the-cloud-with-azure-gpu-vms-mxnet-and-microsoft-r-server/
-# Tested on Ubuntu 14.0+ distro.
-# This script assumes that R is already installed in the system
+# Tested on Ubuntu 14.04+ distro.
 ######################################################################
 set -e
 
@@ -17,9 +16,13 @@ cd "$MXNET_HOME"
 make -j$(nproc)
 
 echo "Installing R dependencies. This can take few minutes..."
-Rscript -e "install.packages('devtools', repo = 'https://cran.rstudio.com')"
+
+# make sure we have essential tools installed
+sudo apt-get install r-base-core libcurl4-openssl-dev libssl-dev
+
+sudo Rscript -e "install.packages('devtools', repo = 'https://cran.rstudio.com')"
 cd R-package
-Rscript -e "library(devtools); library(methods); options(repos=c(CRAN='https://cran.rstudio.com')); install_deps(dependencies = TRUE)"
+sudo Rscript -e "library(devtools); library(methods); options(repos=c(CRAN='https://cran.rstudio.com')); install_deps(dependencies = TRUE)"
 cd ..
 
 echo "Compiling R package. This can take few minutes..."
@@ -29,3 +32,4 @@ echo "Installing R package..."
 sudo R CMD INSTALL mxnet_0.7.tar.gz
 
 echo "Done! MXNet for R installation is complete. Go ahead and explore MXNet with R :-)"
+
