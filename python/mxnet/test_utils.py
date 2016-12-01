use max reldiff + fix gradient of L2Normalization

python/mxnet/test_utils.py
@@ -35,7 +35,7 @@ def default_numerical_threshold():
     """Get default numerical threshold for regression test."""
     # _TODO: get from env variable, different threshold might
     # be needed for different device and dtype
-    return 1e-6
+    return 1e-5
 
 
 def random_arrays(*shapes):
@@ -89,23 +89,34 @@ def same(a, b):
     """
     return np.array_equal(a, b)
 
-def reldiff(a, b):
+def reldiff(a, b, return_index=False, thresh=1E-3):
     """Calculate the relative difference between two input arrays
 
-    Calculated by :math:`\\frac{|a-b|_1}{|a|_1 + |b|_1}`
+    Calculated by :math:`\\max(frac{|a-b|}{max(|a|, |b|) + 1E-7})`
+    Also If |a-b| is smaller than "thresh", the absolute difference will be used.
 
     Parameters
     ----------
     a : np.ndarray
     b : np.ndarray
+    return_index : bool, optional
+        Whether to return the index that has the maximum difference
+    thresh : float, optional
+        If |a-b| is smaller than "thresh", the absolute difference will be used.
     """
-    diff = np.sum(np.abs(a - b))
-    norm = np.sum(np.abs(a)) + np.sum(np.abs(b))
-    if diff == 0:
-        return 0
-    ret = diff / norm
-    return ret
-
+    diff = np.abs(a - b)
+    norm = np.maximum(np.abs(a), np.abs(b)) + 1e-7
+    relative_diff = diff / norm
+    if isinstance(diff, np.ndarray):
+        relative_diff[diff < thresh] = diff[diff < thresh]
+    else:
+        relative_diff = diff if diff < thresh else relative_diff
+    ret = np.max(relative_diff)
+    if not return_index:
+        return ret
+    else:
+        index = np.unravel_index(np.argmax(diff / norm), dims=a.shape)
+        return ret, index
 
 def almost_equal(a, b, threshold=None):
     """Test if two numpy arrays are almost equal."""
@@ -125,11 +136,13 @@ def assert_almost_equal(a, b, threshold=None):
         The checking threshold. Default threshold will be used if set to None
     """
     threshold = threshold or default_numerical_threshold()
-    rel = reldiff(a, b)
+    rel, index = reldiff(a, b, True)
     if np.isnan(rel) or rel > threshold:
         np.set_printoptions(threshold=4, suppress=True)
         msg = npt.build_err_msg([a, b],
-                                err_msg="Rel Err=%f, Expected <=%f" % (rel, threshold),
+                                err_msg="Rel Err=%f, Expected <=%f,"
+                                        " Index of maximum error:%s, a=%f, b=%f"
+                                % (rel, threshold, str(index), a[index], b[index]),
                                 names=["a", "b"])
         raise Exception(msg)
     return rel
@@ -373,13 +386,13 @@ def check_numeric_gradient(sym, location, aux_states=None, numeric_eps=1e-4, che
         orig_grad = args_grad_npy[name]
         sym_grad = symbolic_grads[name]
         if grad_req[name] == 'write':
-            rel = reldiff(fd_grad, sym_grad)
+            rel, index = reldiff(fd_grad, sym_grad, True)
             arr_l = [fd_grad, sym_grad]
         elif grad_req[name] == 'add':
-            rel = reldiff(fd_grad, sym_grad - orig_grad)
+            rel, index = reldiff(fd_grad, sym_grad - orig_grad, True)
             arr_l = [fd_grad, sym_grad - orig_grad]
         elif grad_req[name] == 'null':
-            rel = reldiff(orig_grad, sym_grad)
+            rel, index = reldiff(orig_grad, sym_grad, True)
             arr_l = [orig_grad, sym_grad]
         else:
             raise ValueError
@@ -388,8 +401,10 @@ def check_numeric_gradient(sym, location, aux_states=None, numeric_eps=1e-4, che
             msg = npt.build_err_msg(arr_l,
                                     err_msg="In symbol \"%s\", ctx=%s, "
                                             "numeric check failed for \"%s\", grad_req= \"%s\". "
-                                            "Rel Err=%f, Expected <=%f"
-                                    %(sym.name, str(ctx), name, grad_req[name], rel, check_eps),
+                                            "Rel Err=%f, Expected <=%f,"
+                                            " Index of maximum error:%s, Numeric=%f, Calculated=%f"
+                                    %(sym.name, str(ctx), name, grad_req[name], rel, check_eps,
+                                      str(index), arr_l[0][index], arr_l[1][index]),
                                     names=["NUMERICAL", "BACKWARD"])
             raise Exception(msg)
 
@@ -443,14 +458,16 @@ def check_symbolic_forward(sym, location, expected, check_eps=1E-4, aux_states=N
     outputs = [x.asnumpy() for x in executor.outputs]
 
     for output_name, expect, output in zip(sym.list_outputs(), expected, outputs):
-        rel = reldiff(expect, output)
+        rel, index = reldiff(expect, output, True)
         if rel > check_eps:
             np.set_printoptions(threshold=4, suppress=True)
             msg = npt.build_err_msg([expect, output],
                                     err_msg="In symbol \"%s\", ctx=%s, "
                                             "forward check failed for \"%s\". "
-                                            "Rel Err=%f, Expected <=%f"
-                                    %(sym.name, str(ctx), output_name, rel, check_eps),
+                                            "Rel Err=%f, Expected <=%f,"
+                                            " Index of maximum error:%s, Expect=%f, Forward=%f"
+                                    %(sym.name, str(ctx), output_name, rel, check_eps,
+                                      str(index), expect[index], output[index]),
                                     names=["EXPECTED", "FORWARD"])
             raise Exception(msg)
 
@@ -519,13 +536,13 @@ def check_symbolic_backward(sym, location, out_grads, expected, check_eps=1e-5,
     grads = {k: v.asnumpy() for k, v in args_grad_data.items()}
     for name in expected:
         if grad_req[name] == 'write':
-            rel = reldiff(expected[name], grads[name])
+            rel, index = reldiff(expected[name], grads[name], True)
             arr_l = [expected[name], grads[name]]
         elif grad_req[name] == 'add':
-            rel = reldiff(expected[name], grads[name] - args_grad_npy[name])
+            rel, index = reldiff(expected[name], grads[name] - args_grad_npy[name], True)
             arr_l = [expected[name], grads[name] - args_grad_npy[name]]
         elif grad_req[name] == 'null':
-            rel = reldiff(args_grad_npy[name], grads[name])
+            rel, index = reldiff(args_grad_npy[name], grads[name], True)
             arr_l = [args_grad_npy[name], grads[name]]
         else:
             raise ValueError
@@ -534,8 +551,10 @@ def check_symbolic_backward(sym, location, out_grads, expected, check_eps=1e-5,
             msg = npt.build_err_msg(arr_l,
                                     err_msg="In symbol \"%s\", ctx=%s, "
                                             "backward check failed for \"%s\". "
-                                            "Rel Err=%f, Expected <=%f"
-                                    %(sym.name, str(ctx), name, rel, check_eps),
+                                            "Rel Err=%f, Expected <=%f,"
+                                            " Index of maximum error:%s, Expect=%f, Backward=%f"
+                                    %(sym.name, str(ctx), name, rel, check_eps,
+                                      index, arr_l[0][index], arr_l[1][index]),
                                     names=["EXPECTED", "BACKWARD"])
             raise Exception(msg)
 

tests/python/unittest/test_operator.py
@@ -363,7 +363,7 @@ def test_pow_fn():
     exp = mx.symbol.Variable("exp")
     y = mx.sym.pow(2, exp)
     x = np.ones(shape)*3
-    check_numeric_gradient(y, [x])
+    check_numeric_gradient(y, [x], numeric_eps=1E-3)
     check_symbolic_forward(y, [x], [2**x])
     check_symbolic_backward(y, [x], [np.ones(shape)], [np.log(2) * 2**x])
 
@@ -595,7 +595,7 @@ def check_deconvolution_forward_backward(input_shape, num_filter, kernel, stride
     exe.forward()
     out = exe.outputs[0].asnumpy()
     exe.backward(out_grad)
-    assert reldiff(out, args_grad[0].asnumpy()) < 1e-6
+    assert_almost_equal(out, args_grad[0].asnumpy(), 1E-4)
 
 def check_deconvolution_gradient(input_shape, num_filter, pad):
     """configure A: input --> conv --> output.
@@ -638,7 +638,7 @@ def check_deconvolution_gradient(input_shape, num_filter, pad):
     exe_deconv.forward(is_train=True)
     deconv_out_grad = conv_data[:]
     exe_deconv.backward(deconv_out_grad)
-    assert reldiff(conv_args_grad[1].asnumpy(), deconv_args_grad[1].asnumpy()) < 1e-6
+    assert_almost_equal(conv_args_grad[1].asnumpy(), deconv_args_grad[1].asnumpy(), 1E-2)
 
 def check_deconvolution_target_shape(input_shape, kernel, stride, pad, adj, target_shape=None):
     data = mx.sym.Variable(name="data")
@@ -1466,15 +1466,17 @@ def test_support_vector_machine_l2_svm():
 def test_roipooling():
     data = mx.symbol.Variable(name='data')
     rois = mx.symbol.Variable(name='rois')
-    test = mx.symbol.ROIPooling(data=data, rois=rois, pooled_size=(6, 6), spatial_scale=1)
+    test = mx.symbol.ROIPooling(data=data, rois=rois, pooled_size=(4, 4), spatial_scale=1)
 
     x1 = np.random.rand(4, 3, 12, 8)
-    x2 = np.array([[0, 1, 1, 6, 6], [2, 6, 2, 7, 11], [1, 3, 1, 5, 10], [0, 3, 3, 3, 3]])
+    x2 = np.array([[0, 1.1, 1.1, 6.2, 6.2], [2, 6, 2, 8, 11], [1, 3, 1, 5, 10], [0, 3, 3, 3, 3]])
 
-    check_numeric_gradient(test, [x1, x2], numeric_eps=1e-3, check_eps=1e-2)
     check_numeric_gradient(sym=test, location=[x1, x2],
-                           grad_nodes={'data':'add', 'rois':'write'},
-                           numeric_eps=1e-3, check_eps=1e-2)
+                           grad_nodes={'data':'add', 'rois':'null'},
+                           numeric_eps=1e-3, check_eps=1e-1)
+    check_numeric_gradient(sym=test, location=[x1, x2],
+                           grad_nodes={'data':'add', 'rois':'null'},
+                           numeric_eps=1e-3, check_eps=1e-1)
 
 def check_pad_with_shape(shape, xpu, pad_width, mode):
     # bind with label
@@ -1492,7 +1494,7 @@ def check_pad_with_shape(shape, xpu, pad_width, mode):
     # compare numpy + mxnet
     assert_allclose(out, np_out, rtol=1e-5)
     # grad check
-    check_numeric_gradient(Y, [x.asnumpy()], numeric_eps=1e-3, check_eps=1e-2)
+    check_numeric_gradient(Y, [x.asnumpy()], numeric_eps=1e-2, check_eps=1e-2)
 
 def test_pad():
     shape1 = (2, 3, 2, 3)
@@ -1522,22 +1524,23 @@ def np_instance_norm(data, weight, bias, eps):
 
 def check_instance_norm_with_shape(shape, xpu):
     # bind with label
-    eps = 0.0234
+    eps = 0.001
     X = mx.symbol.Variable('X')
     G = mx.symbol.Variable('G')
     B = mx.symbol.Variable('B')
 
     Y = mx.symbol.InstanceNorm(data=X, beta=B, gamma=G, eps=eps)
-    x = mx.random.uniform(-1, 1, shape, ctx=mx.cpu()).copyto(xpu)
-    gamma = mx.random.uniform(-1, 1, shape[1], ctx=mx.cpu()).copyto(xpu)
-    beta = mx.random.uniform(-1, 1, shape[1], ctx=mx.cpu()).copyto(xpu)
+    x = mx.random.normal(0, 1, shape, ctx=mx.cpu()).copyto(xpu)
+    gamma = mx.random.normal(0, 1, shape[1], ctx=mx.cpu()).copyto(xpu)
+    beta = mx.random.normal(0, 1, shape[1], ctx=mx.cpu()).copyto(xpu)
 
     np_out = np_instance_norm(x.asnumpy(), gamma.asnumpy(), beta.asnumpy(), eps)
     exec1 = Y.bind(xpu, args = {'X':x, 'G':gamma, 'B':beta})
     exec1.forward(is_train=False)
     out = exec1.outputs[0].asnumpy()
     assert_allclose(out, np_out, rtol=1e-4)
-    check_numeric_gradient(Y, {'X':x.asnumpy(), 'G':gamma.asnumpy(), 'B':beta.asnumpy()}, numeric_eps=1e-2, check_eps=0.16)
+    check_numeric_gradient(Y, {'X':x.asnumpy(), 'G':gamma.asnumpy(), 'B':beta.asnumpy()},
+                           numeric_eps=1e-2, check_eps=0.2)
 
 def test_instance_normalization():
     check_instance_norm_with_shape((2,4,5,6), default_context())
@@ -1573,7 +1576,7 @@ def check_l2_normalization(in_shape, mode, ctx=default_context(), norm_eps=1e-10
     # compare numpy + mxnet
     assert_allclose(exe.outputs[0].asnumpy(), np_out, rtol=1e-5)
     # check gradient
-    check_numeric_gradient(out, [in_data], numeric_eps=1e-3, check_eps=1e-2)
+    check_numeric_gradient(out, [in_data], numeric_eps=1e-2, check_eps=5e-2)
 
 def test_l2_normalization():
     for mode in ['channel', 'spatial', 'instance']:
@@ -1773,7 +1776,7 @@ def mathematical_core(name, forward_mxnet_call, forward_numpy_call, backward_num
     exe_test.forward()
     out = exe_test.outputs[0].asnumpy()
     npout = forward_numpy_call(data_tmp)
-    assert reldiff(out, npout) < 1e-6, "%s mathematical forward failed\n%s\n\n%s" % (name, out, npout)
+    assert reldiff(out, npout) < 1e-4, "%s mathematical forward failed\n%s\n\n%s" % (name, out, npout)
 
     out_grad = mx.nd.empty(shape)
     out_grad[:] = grad_init
@@ -1785,7 +1788,7 @@ def mathematical_core(name, forward_mxnet_call, forward_numpy_call, backward_num
     # print(name)
     # print(arr_grad)
     # print(npout_grad)
-    assert reldiff(arr_grad, npout_grad) < 1e-6, "%s mathematical backward failed\n%s\n\n%s" % (
+    assert reldiff(arr_grad, npout_grad) < 1e-4, "%s mathematical backward failed\n%s\n\n%s" % (
         name, arr_grad, npout_grad)
 
 