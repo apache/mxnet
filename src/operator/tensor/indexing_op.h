89d1987b974b6f5c9a96785d64ab9a1958b5455f
flyers <lisiyi.zju@gmail.com>
2016-12-02 20:19:24 +0800

    fix Dtype

--- src/operator/tensor/indexing_op.h
+++ src/operator/tensor/indexing_op.h
@@ -10,11 +10,14 @@
 #include <dmlc/logging.h>
 #include <dmlc/parameter.h>
 #include <mxnet/operator.h>
+#include <mxnet/operator_util.h>
+#include "./operator_common.h"
+#include "../mshadow_op.h"
+#include "../elemwise_op_common.h"
 #include <map>
 #include <vector>
 #include <string>
 #include <utility>
-#include "./operator_common.h"
 
 namespace mxnet {
 namespace op {
@@ -77,7 +80,7 @@ inline bool EmbeddingOpType(const nnvm::NodeAttrs& attrs,
   return true;
 }
 
-template<typename xpu, typename DType>
+template<typename xpu>
 void EmbeddingOpForward(const nnvm::NodeAttrs& attrs,
                         const OpContext& ctx,
                         const std::vector<TBlob>& inputs,
@@ -97,15 +100,17 @@ void EmbeddingOpForward(const nnvm::NodeAttrs& attrs,
   const TShape& oshape = outputs[embedding::kOut].shape_;
 
   Stream<xpu> *s = ctx.get_stream<xpu>();
-  Tensor<xpu, 1, DType> data = inputs[embedding::kData].get_with_shape<xpu, 1, DType>(
-          Shape1(ishape.ProdShape(0, ishape.ndim())), s);
-  Tensor<xpu, 2, DType> wmat = inputs[embedding::kWeight].get<xpu, 2, DType>(s);
-  Tensor<xpu, 2, DType> out = outputs[embedding::kOut].get_with_shape<xpu, 2, DType>(
-          Shape2(oshape.ProdShape(0, oshape.ndim()-1), oshape[oshape.ndim()-1]), s);
-  out = take(data, wmat);
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    Tensor<xpu, 1, DType> data = inputs[embedding::kData].get_with_shape<xpu, 1, DType>(
+      Shape1(ishape.ProdShape(0, ishape.ndim())), s);
+    Tensor<xpu, 2, DType> wmat = inputs[embedding::kWeight].get<xpu, 2, DType>(s);
+    Tensor<xpu, 2, DType> out = outputs[embedding::kOut].get_with_shape<xpu, 2, DType>(
+      Shape2(oshape.ProdShape(0, oshape.ndim()-1), oshape[oshape.ndim()-1]), s);
+    out = take(data, wmat);
+  });
 }
 
-template<typename xpu, typename DType>
+template<typename xpu>
 void EmbeddingOpBackward(const nnvm::NodeAttrs& attrs,
                          const OpContext& ctx,
                          const std::vector<TBlob>& inputs,
@@ -122,37 +127,40 @@ void EmbeddingOpBackward(const nnvm::NodeAttrs& attrs,
   const TShape& oshape = outputs[0].shape_;
 
   Stream<xpu> *s = ctx.get_stream<xpu>();
-  Tensor<xpu, 1, DType> data = inputs[1].get_with_shape<xpu, 1, DType>(
-          Shape1(ishape.ProdShape(0, ishape.ndim())), s);
-  Tensor<xpu, 2, DType> grad_out = inputs[0].get_with_shape<xpu, 2, DType>(
-          Shape2(oshape.ProdShape(0, oshape.ndim()-1), oshape[oshape.ndim()-1]), s);
-  Tensor<xpu, 2, DType> grad_in = outputs[1].get<xpu, 2, DType>(s);
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    Tensor < xpu, 1, DType > data = inputs[1].get_with_shape<xpu, 1, DType>(
+      Shape1(ishape.ProdShape(0, ishape.ndim())), s);
+    Tensor<xpu, 2, DType> grad_out = inputs[0].get_with_shape<xpu, 2, DType>(
+    Shape2(oshape.ProdShape(0, oshape.ndim()-1), oshape[oshape.ndim()-1]), s);
+    Tensor<xpu, 2, DType> grad_in = outputs[1].get<xpu, 2, DType>(s);
 
-  if (req[embedding::kWeight] == kWriteTo || req[embedding::kWeight] == kAddTo) {
-    if (req[embedding::kWeight] == kWriteTo) {
+
+    if (req[embedding::kWeight] == kWriteTo || req[embedding::kWeight] == kAddTo) {
+      if (req[embedding::kWeight] == kWriteTo) {
 #ifdef __CUDACC__
-      cudaMemsetAsync(grad_in.dptr_, 0, grad_in.MSize() * sizeof(DType),
-                        Stream<gpu>::GetStream(s));
+        cudaMemsetAsync(grad_in.dptr_, 0, grad_in.MSize() * sizeof(DType),
+                          Stream<gpu>::GetStream(s));
 #else
-      grad_in = scalar<DType>(0.0f);
+        grad_in = scalar<DType>(0.0f);
 #endif
-    }
-    if ((grad_out.shape_[0] < grad_out.shape_[1]) && (grad_out.shape_[0] < 512)) {
-      AddTakeGrad(grad_in, data, grad_out);
+      }
+      if ((grad_out.shape_[0] < grad_out.shape_[1]) && (grad_out.shape_[0] < 512)) {
+        AddTakeGrad(grad_in, data, grad_out);
+      } else {
+        Tensor<xpu, 2, int> workspace =
+          ctx.requested[embedding::kTempSpace].get_space_typed<xpu, 2, int>(
+            mshadow::Shape2(2, data.shape_.Size()), s);
+        Tensor<xpu, 1, int> sorted_data = workspace[0];
+        Tensor<xpu, 1, int> original_index = workspace[1];
+        sorted_data = tcast<int>(data);
+        original_index = range<int>(0, data.shape_.Size());
+        SortByKey(sorted_data, original_index, true);
+        AddTakeGradLargeBatch(grad_in, sorted_data, original_index, grad_out);
+      }
     } else {
-      Tensor<xpu, 2, int> workspace =
-              ctx.requested[embedding::kTempSpace].get_space_typed<xpu, 2, int>(
-                      mshadow::Shape2(2, data.shape_.Size()), s);
-      Tensor<xpu, 1, int> sorted_data = workspace[0];
-      Tensor<xpu, 1, int> original_index = workspace[1];
-      sorted_data = tcast<int>(data);
-      original_index = range<int>(0, data.shape_.Size());
-      SortByKey(sorted_data, original_index, true);
-      AddTakeGradLargeBatch(grad_in, sorted_data, original_index, grad_out);
+      LOG(FATAL) << "wrong req";
     }
-  } else {
-    LOG(FATAL) << "wrong req";
-  }
+  });
 }
 
 
