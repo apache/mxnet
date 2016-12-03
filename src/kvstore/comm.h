a1cc8f0308fda4e5196e4a285c3c28ddae750147
Mu Li <muli@cs.cmu.edu>
2016-12-02 22:51:52 -0800

    [kvstore] no warning if p2p is already enabled (#4075)

--- src/kvstore/comm.h
+++ src/kvstore/comm.h
@@ -278,7 +278,7 @@ class CommDevice : public Comm {
         cudaDeviceCanAccessPeer(&access, gpus[i], gpus[j]);
         if (access) {
           cudaError_t e = cudaDeviceEnablePeerAccess(gpus[j], 0);
-          if (e == cudaSuccess) {
+          if (e == cudaSuccess || e == cudaErrorPeerAccessAlreadyEnabled) {
             ++enabled;
             p2p[i*n+j] = 1;
           }
