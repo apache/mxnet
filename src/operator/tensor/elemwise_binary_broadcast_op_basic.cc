3b0dc3274dcbc0076109980ab74445cb5d5d9e84
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-12-08 23:50:56 -0800

    split tensor operator files (#4150)
    * fix examples
    
    * fix
    
    * split tensor operator files

--- src/operator/tensor/elemwise_binary_broadcast_op_basic.cc
+++ src/operator/tensor/elemwise_binary_broadcast_op_basic.cc
@@ -83,73 +83,9 @@ NNVM_REGISTER_OP(_backward_div)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastBackwardUseIn<cpu, mshadow_op::div_grad,
                                                               mshadow_op::div_rgrad>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_power)
-.add_alias("_power").add_alias("_Power")
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::power>)
-.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_power"});
-
-NNVM_REGISTER_OP(_backward_power)
-.set_num_inputs(3)
-.set_num_outputs(2)
-.set_attr<nnvm::TIsBackward>("TIsBackward", true)
-.set_attr<nnvm::FInplaceOption>("FInplaceOption",
-  [](const NodeAttrs& attrs){
-    return std::vector<std::pair<int, int> >{{0, 1}};
-  })
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastBackwardUseIn<cpu, mshadow_op::power_grad,
-                                                              mshadow_op::power_rgrad>);
-
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_maximum)
-.add_alias("broadcast_maximum").add_alias("_Maximum")
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::maximum>)
-.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_maximum"});
-
-NNVM_REGISTER_OP(_backward_maximum)
-.set_num_inputs(3)
-.set_num_outputs(2)
-.set_attr<nnvm::TIsBackward>("TIsBackward", true)
-.set_attr<nnvm::FInplaceOption>("FInplaceOption",
-  [](const NodeAttrs& attrs){
-    return std::vector<std::pair<int, int> >{{0, 1}};
-  })
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastBackwardUseIn<cpu, mshadow_op::ge,
-                                                              mshadow_op::lt>);
-
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_minimum)
-.add_alias("broadcast_minimum").add_alias("_Minimum")
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::minimum>)
-.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_minimum"});
-
-NNVM_REGISTER_OP(_backward_minimum)
-.set_num_inputs(3)
-.set_num_outputs(2)
-.set_attr<nnvm::TIsBackward>("TIsBackward", true)
-.set_attr<nnvm::FInplaceOption>("FInplaceOption",
-  [](const NodeAttrs& attrs){
-    return std::vector<std::pair<int, int> >{{0, 1}};
-  })
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastBackwardUseIn<cpu, mshadow_op::le,
-                                                              mshadow_op::gt>);
-
 MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_equal)
 .add_alias("broadcast_equal").add_alias("_Equal")
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::eq>);
-
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_hypot)
-.add_alias("broadcast_hypot").add_alias("_Hypot")
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::hypot>)
-.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{ "_backward_hypot" });
-
-NNVM_REGISTER_OP(_backward_hypot)
-.set_num_inputs(3)
-.set_num_outputs(2)
-.set_attr<nnvm::TIsBackward>("TIsBackward", true)
-.set_attr<nnvm::FInplaceOption>("FInplaceOption",
-[](const NodeAttrs& attrs) {
-  return std::vector<std::pair<int, int> > {{0, 1}};
-})
-.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastBackwardUseIn<cpu, mshadow_op::hypot_grad_left,
-                    mshadow_op::hypot_grad_right>);
 
 }  // namespace op
 }  // namespace mxnet
