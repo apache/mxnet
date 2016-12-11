20d7d701998cc5e0f8565efaa98b4a1a9c1abd30
shivshi <shivshi@cisco.com>
2016-12-11 00:37:18 -0800

    [Keras] Add support for element wise comparison operators #4182
    Added element wise !=, >, >=, < and <= comparison operators

--- src/operator/tensor/elemwise_binary_broadcast_op_basic.cc
+++ src/operator/tensor/elemwise_binary_broadcast_op_basic.cc
@@ -86,6 +86,26 @@ NNVM_REGISTER_OP(_backward_div)
 MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_equal)
 .add_alias("broadcast_equal").add_alias("_Equal")
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::eq>);
+    
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_not_equal)
+.add_alias("broadcast_not_equal").add_alias("_Not_Equal")
+.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::ne>);
+    
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_greater)
+.add_alias("broadcast_greater").add_alias("_Greater")
+.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::gt>);
+    
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_greater_equal)
+.add_alias("broadcast_greater_equal").add_alias("_Greater_Equal")
+.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::ge>);
+    
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_lesser)
+.add_alias("broadcast_lesser").add_alias("_Lesser")
+.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::lt>);
+    
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_lesser_equal)
+.add_alias("broadcast_lesser_equal").add_alias("_Lesser_Equal")
+.set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::le>);
 
 }  // namespace op
 }  // namespace mxnet
