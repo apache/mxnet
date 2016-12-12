8ed54799d15c9def9d1fe87a80a2730fc6aecc28
Bing Xu <antinucleon@gmail.com>
2016-12-11 22:20:26 -0800

    fix

--- amalgamation/mxnet_predict0.cc
+++ amalgamation/mxnet_predict0.cc
@@ -23,40 +23,56 @@
 #define MXNET_PREDICT_ONLY 	1
 #define DISABLE_OPENMP 1
 
+//  NDArray
 #include "src/ndarray/ndarray_function.cc"
 #include "src/ndarray/ndarray.cc"
+
+// Engine
 #include "src/engine/engine.cc"
 #include "src/engine/naive_engine.cc"
-#include "src/symbol/graph_executor.cc"
-#include "src/symbol/graph_memory_allocator.cc"
-#include "src/symbol/static_graph.cc"
-#include "src/symbol/symbol.cc"
-#include "src/operator/operator.cc"
-#include "src/operator/operator_util.cc"
+
+// Executor
+#include "src/executor/graph_executor.cc"
+#include "src/executor/attach_op_execs_pass.cc"
+#include "src/executor/attach_op_resource_pass.cc"
+#include "src/executor/inplace_addto_detect_pass.cc"
+
+// Operators
+#include "src/nnvm/legacy_json_util.cc"
+#include "src/nnvm/legacy_op_util.cc"
+
+//  Tensor Op
+#include "src/operator/tensor/sample_op.cc"
+#include "src/operator/tensor/matrix_op.cc"
+#include "src/operator/tensor/init_op.cc"
+#include "src/operator/tensor/indexing_op.cc"
+#include "src/operator/tensor/elemwise_unary_op.cc"
+#include "src/operator/tensor/elemwise_sum.cc"
+#include "src/operator/tensor/elemwise_binary_scalar_op_extended.cc"
+#include "src/operator/tensor/elemwise_binary_scalar_op_basic.cc"
+#include "src/operator/tensor/elemwise_binary_broadcast_op_extended.cc"
+#include "src/operator/tensor/elemwise_binary_broadcast_op_basic.cc"
+#include "src/operator/tensor/broadcast_reduce_op_value.cc"
+#include "src/operator/tensor/broadcast_reduce_op_index.cc"
+
+//  Common Op
 #include "src/operator/activation.cc"
 #include "src/operator/batch_norm.cc"
-#include "src/operator/block_grad.cc"
 #include "src/operator/concat.cc"
 #include "src/operator/convolution.cc"
+#include "src/operator/deconvolution.cc"
 #include "src/operator/dropout.cc"
-#include "src/operator/elementwise_unary_op.cc"
-#include "src/operator/elementwise_binary_op.cc"
-#include "src/operator/elementwise_sum.cc"
 #include "src/operator/fully_connected.cc"
 #include "src/operator/leaky_relu.cc"
-#include "src/operator/lrn.cc"
+#include "src/operator/make_loss.cc"
 #include "src/operator/pooling.cc"
 #include "src/operator/regression_output.cc"
-#include "src/operator/reshape.cc"
-#include "src/operator/slice_channel.cc"
 #include "src/operator/softmax_output.cc"
-#include "src/operator/deconvolution.cc"
-#include "src/operator/elementwise_binary_scalar_op.cc"
-#include "src/operator/elementwise_unary_op.cc"
-#include "src/operator/embedding.cc"
-#include "src/storage/storage.cc"
 
+// Storages
+#include "src/storage/storage.cc"
 #include "src/resource.cc"
 
+//  C_API
 #include "src/c_api/c_predict_api.cc"
 #include "src/c_api/c_api_error.cc"
