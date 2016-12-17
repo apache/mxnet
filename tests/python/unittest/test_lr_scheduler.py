8c270ca2372e39f6e64ea4564c9b91e66d0b5b6f
Tao Wei <taowei@buffalo.edu>
2016-12-17 22:43:23 +0800

    add test for slow_step

--- tests/python/unittest/test_lr_scheduler.py
+++ tests/python/unittest/test_lr_scheduler.py
@@ -44,3 +44,16 @@ def test_multi_factor_scheduler():
     assert reldiff(lr_scheduler(91), 0.001) < 1e-5
     assert reldiff(lr_scheduler(95), 0.001) < 1e-5
     assert reldiff(lr_scheduler(96), 0.0001) < 1e-5
+    # slow step
+    lr_scheduler = MultiFactorScheduler([30,60,95], 0.1, slow_step=10)
+    lr_scheduler.base_lr = 0.1
+    assert reldiff(lr_scheduler(0), 0.01) < 1e-5
+    assert reldiff(lr_scheduler(10), 0.01) < 1e-5
+    assert reldiff(lr_scheduler(11), 0.1) < 1e-5
+    assert reldiff(lr_scheduler(30), 0.1) < 1e-5
+    assert reldiff(lr_scheduler(31), 0.01) < 1e-5
+    # slow step, continual training
+    lr_scheduler = MultiFactorScheduler([30,60,95], 0.1, slow_step=10)
+    lr_scheduler.base_lr = 0.1
+    assert reldiff(lr_scheduler(60), 0.01) < 1e-5
+    assert reldiff(lr_scheduler(61), 0.001) < 1e-5
