c598a7325f24ab18dbfc534146889c2d54476315
Xiaodong <XD-DENG@users.noreply.github.com>
2016-12-19 17:09:36 +0800

    Refine the Document Format

--- R-package/R/mxnet_generated.R
+++ R-package/R/mxnet_generated.R
@@ -607,7 +607,7 @@ mx.io.MNISTIter <- function(...) {
   mx.varg.io.MNISTIter(list(...))
 }
 
-#' Apply activation function to input
+#' Apply Activation Function to Input
 #'
 #' Apply activation function to input. Softmax Activation is only available with CUDNN on GPU and will be computed at each location across channel if input is 4D.
 #' 
