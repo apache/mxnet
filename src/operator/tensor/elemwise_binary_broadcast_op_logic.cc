16032a97401239c00b7e673cdeb2c0f802a342a3
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2016-12-20 00:57:26 -0800

    refactor elementwise ops (#4227)
    * separate broadcasting op and non broadcasting op
    
    * fix
    
    * fix

--- src/operator/tensor/elemwise_binary_broadcast_op_logic.cc
+++ src/operator/tensor/elemwise_binary_broadcast_op_logic.cc
@@ -10,28 +10,22 @@
 namespace mxnet {
 namespace op {
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_equal)
-.add_alias("broadcast_equal").add_alias("_Equal")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_equal)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::eq>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_not_equal)
-.add_alias("broadcast_not_equal").add_alias("_Not_Equal")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_not_equal)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::ne>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_greater)
-.add_alias("broadcast_greater").add_alias("_Greater")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_greater)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::gt>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_greater_equal)
-.add_alias("broadcast_greater_equal").add_alias("_Greater_Equal")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_greater_equal)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::ge>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_lesser)
-.add_alias("broadcast_lesser").add_alias("_Lesser")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_lesser)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::lt>);
 
-MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(_lesser_equal)
-.add_alias("broadcast_lesser_equal").add_alias("_Lesser_Equal")
+MXNET_OPERATOR_REGISTER_BINARY_BROADCAST(broadcast_lesser_equal)
 .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::le>);
 
 }  // namespace op
