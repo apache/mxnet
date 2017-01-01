d91aedb779abed75a142dee3f90c21b4eda4e802
Howard Su <howard0su@gmail.com>
2017-01-01 18:32:17 +0800

    Build SSD operators in jenkins

--- tests/jenkins/run_test.sh
+++ tests/jenkins/run_test.sh
@@ -5,6 +5,7 @@ echo "USE_CUDA_PATH=/usr/local/cuda" >> config.mk
 echo "USE_CUDNN=1" >> config.mk
 echo "USE_PROFILER=1" >> config.mk
 echo "DEV=1" >> config.mk
+echo "EXTRA_OPERATORS=example/ssd/operator" >> config.mk
 make -j$(nproc) || exit -1
 
 echo "BUILD lint"
