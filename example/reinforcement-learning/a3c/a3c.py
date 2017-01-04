f4b8317adc60612463d1a4fb844eea11cba7d7ff
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-01-03 22:58:32 -0800

    A3C example (#4516)

---
+++ example/reinforcement-learning/a3c/a3c.py
@@ -0,0 +1,222 @@
+import mxnet as mx
+import numpy as np
+import rl_data
+import sym
+import argparse
+import logging
+import os
+import gym
+from datetime import datetime
+import time
+
+parser = argparse.ArgumentParser(description='Traing A3C with OpenAI Gym')
+parser.add_argument('--test', action='store_true', help='run testing', default=False)
+parser.add_argument('--log-file', type=str, help='the name of log file')
+parser.add_argument('--log-dir', type=str, default="./log", help='directory of the log file')
+parser.add_argument('--model-prefix', type=str, help='the prefix of the model to load')
+parser.add_argument('--save-model-prefix', type=str, help='the prefix of the model to save')
+parser.add_argument('--load-epoch', type=int, help="load the model on an epoch using the model-prefix")
+
+parser.add_argument('--kv-store', type=str, default='device', help='the kvstore type')
+parser.add_argument('--gpus', type=str, help='the gpus will be used, e.g "0,1,2,3"')
+
+parser.add_argument('--num-epochs', type=int, default=120, help='the number of training epochs')
+parser.add_argument('--num-examples', type=int, default=1000000, help='the number of training examples')
+parser.add_argument('--batch-size', type=int, default=32)
+parser.add_argument('--input-length', type=int, default=4)
+
+parser.add_argument('--lr', type=float, default=0.0001)
+parser.add_argument('--wd', type=float, default=0)
+parser.add_argument('--t-max', type=int, default=4)
+parser.add_argument('--gamma', type=float, default=0.99)
+parser.add_argument('--beta', type=float, default=0.08)
+
+args = parser.parse_args()
+
+def log_config(log_dir=None, log_file=None, prefix=None, rank=0):
+    reload(logging)
+    head = '%(asctime)-15s Node[' + str(rank) + '] %(message)s'
+    if log_dir:
+        logging.basicConfig(level=logging.DEBUG, format=head)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        if not log_file:
+            log_file = (prefix if prefix else '') + datetime.now().strftime('_%Y_%m_%d-%H_%M.log')
+            log_file = log_file.replace('/', '-')
+        else:
+            log_file = log_file
+        log_file_full_name = os.path.join(log_dir, log_file)
+        handler = logging.FileHandler(log_file_full_name, mode='w')
+        formatter = logging.Formatter(head)
+        handler.setFormatter(formatter)
+        logging.getLogger().addHandler(handler)
+        logging.info('start with arguments %s', args)
+    else:
+        logging.basicConfig(level=logging.DEBUG, format=head)
+        logging.info('start with arguments %s', args)
+
+def train():
+    # kvstore
+    kv = mx.kvstore.create(args.kv_store)
+
+    model_prefix = args.model_prefix
+    if model_prefix is not None:
+        model_prefix += "-%d" % (kv.rank)
+    save_model_prefix = args.save_model_prefix
+    if save_model_prefix is None:
+        save_model_prefix = model_prefix
+
+    log_config(args.log_dir, args.log_file, save_model_prefix, kv.rank)
+
+    devs = mx.cpu() if args.gpus is None else [
+        mx.gpu(int(i)) for i in args.gpus.split(',')]
+
+    epoch_size = args.num_examples / args.batch_size
+
+    if args.kv_store == 'dist_sync':
+        epoch_size /= kv.num_workers
+    
+    # disable kvstore for single device
+    if 'local' in kv.type and (
+            args.gpus is None or len(args.gpus.split(',')) is 1):
+        kv = None
+
+    # module
+    dataiter = rl_data.GymDataIter('Breakout-v0', args.batch_size, args.input_length, web_viz=True)
+    net = sym.get_symbol_atari(dataiter.act_dim)
+    module = mx.mod.Module(net, data_names=[d[0] for d in dataiter.provide_data], label_names=('policy_label', 'value_label'), context=devs)
+    module.bind(data_shapes=dataiter.provide_data,
+                label_shapes=[('policy_label', (args.batch_size,)), ('value_label', (args.batch_size, 1))],
+                grad_req='add')
+
+    # load model
+
+    if args.load_epoch is not None:
+        assert model_prefix is not None
+        _, arg_params, aux_params = mx.model.load_checkpoint(model_prefix, args.load_epoch)
+    else:
+        arg_params = aux_params = None
+
+    # save model
+    checkpoint = None if save_model_prefix is None else mx.callback.do_checkpoint(save_model_prefix)
+
+    init = mx.init.Mixed(['fc_value_weight|fc_policy_weight', '.*'],
+                         [mx.init.Uniform(0.001), mx.init.Xavier(rnd_type='gaussian', factor_type="in", magnitude=2)])
+    module.init_params(initializer=init,
+                       arg_params=arg_params, aux_params=aux_params)
+
+    # optimizer
+    module.init_optimizer(kvstore=kv, optimizer='adam',
+                          optimizer_params={'learning_rate': args.lr, 'wd': args.wd, 'epsilon': 1e-3})
+
+    # logging
+    np.set_printoptions(precision=3, suppress=True)
+
+    T = 0
+    dataiter.reset()
+    score = np.zeros((args.batch_size, 1))
+    final_score = np.zeros((args.batch_size, 1))
+    for epoch in range(args.num_epochs):
+        if save_model_prefix:
+            module.save_params('%s-%04d.params'%(save_model_prefix, epoch))
+
+
+        for _ in range(epoch_size/args.t_max):
+            tic = time.time()
+            # clear gradients
+            for exe in module._exec_group.grad_arrays:
+                for g in exe:
+                    g[:] = 0
+
+            S, A, V, r, D = [], [], [], [], []
+            for t in range(args.t_max + 1):
+                data = dataiter.data()
+                module.forward(mx.io.DataBatch(data=data, label=None), is_train=False)
+                act, _, val = module.get_outputs()
+                V.append(val.asnumpy())
+                if t < args.t_max:
+                    act = act.asnumpy()
+                    act = [np.random.choice(dataiter.act_dim, p=act[i]) for i in range(act.shape[0])]
+                    reward, done = dataiter.act(act)
+                    S.append(data)
+                    A.append(act)
+                    r.append(reward.reshape((-1, 1)))
+                    D.append(done.reshape((-1, 1)))
+
+            err = 0
+            R = V[args.t_max]
+            for i in reversed(range(args.t_max)):
+                R = r[i] + args.gamma * (1 - D[i]) * R
+                adv = np.tile(R - V[i], (1, dataiter.act_dim))
+
+                batch = mx.io.DataBatch(data=S[i], label=[mx.nd.array(A[i]), mx.nd.array(R)])
+                module.forward(batch, is_train=True)
+
+                pi = module.get_outputs()[1]
+                h = args.beta*(mx.nd.log(pi+1e-6)+1)
+                module.backward([mx.nd.array(adv), h])
+
+                print 'pi', pi[0].asnumpy()
+                print 'h', h[0].asnumpy()
+                err += (adv**2).mean()
+                score += r[i]
+                final_score *= (1-D[i]) 
+                final_score += score * D[i]
+                score *= 1-D[i]
+                T += D[i].sum()
+
+            module.update()
+            logging.info('fps: %f err: %f score: %f final: %f T: %f'%(args.batch_size/(time.time()-tic), err/args.t_max, score.mean(), final_score.mean(), T))
+            print score.squeeze()
+            print final_score.squeeze()
+
+def test():
+    log_config()
+
+    devs = mx.cpu() if args.gpus is None else [
+        mx.gpu(int(i)) for i in args.gpus.split(',')]
+
+    # module
+    dataiter = robo_data.RobosimsDataIter('scenes', args.batch_size, args.input_length, web_viz=True)
+    print dataiter.provide_data
+    net = sym.get_symbol_thor(dataiter.act_dim)
+    module = mx.mod.Module(net, data_names=[d[0] for d in dataiter.provide_data], label_names=('policy_label', 'value_label'), context=devs)
+    module.bind(data_shapes=dataiter.provide_data,
+                label_shapes=[('policy_label', (args.batch_size,)), ('value_label', (args.batch_size, 1))],
+                for_training=False)
+
+    # load model
+    assert args.load_epoch is not None
+    assert args.model_prefix is not None
+    module.load_params('%s-%04d.params'%(args.model_prefix, args.load_epoch))
+
+    N = args.num_epochs * args.num_examples / args.batch_size
+
+    R = 0
+    T = 1e-20
+    score = np.zeros((args.batch_size,))
+    for t in range(N):
+        dataiter.clear_history()
+        data = dataiter.next()
+        module.forward(data, is_train=False)
+        act = module.get_outputs()[0].asnumpy()
+        act = [np.random.choice(dataiter.act_dim, p=act[i]) for i in range(act.shape[0])]
+        dataiter.act(act)
+        time.sleep(0.05)
+        _, reward, _, done = dataiter.history[0]
+        T += done.sum()
+        score += reward
+        R += (done*score).sum()
+        score *= (1-done)
+
+        if t % 100 == 0:
+            logging.info('n %d score: %f T: %f'%(t, R/T, T))
+
+
+if __name__ == '__main__':
+    if args.test:
+        test()
+    else:
+        train()
+
+
---
+++ example/reinforcement-learning/a3c/rl_data.py
@@ -0,0 +1,154 @@
+import mxnet as mx
+import numpy as np
+import gym
+import cv2
+import math
+import Queue
+from threading import Thread
+import time
+import multiprocessing
+import multiprocessing.pool
+from flask import Flask, render_template, Response
+import signal
+
+def make_web(queue):
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    def gen():
+        while True:
+            frame = queue.get()
+            _, frame = cv2.imencode('.JPEG', frame)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame.tostring() + b'\r\n')
+
+    @app.route('/video_feed')
+    def video_feed():
+        return Response(gen(),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    try:
+        app.run(host='0.0.0.0', port=8889)
+    except:
+        print 'unable to open port' 
+
+def visual(X, show=True):
+    X = X.transpose((0, 2, 3, 1))
+    N = X.shape[0]
+    n = int(math.ceil(math.sqrt(N)))
+    h = X.shape[1]
+    w = X.shape[2]
+    buf = np.zeros((h*n, w*n, X.shape[3]), dtype=np.uint8)
+    for i in range(N):
+        x = i%n
+        y = i/n
+        buf[h*y:h*(y+1), w*x:w*(x+1), :] = X[i]
+    if show:
+        cv2.imshow('a', buf)
+        cv2.waitKey(1)
+    return buf
+
+def env_step(args):
+    return args[0].step(args[1])
+
+class RLDataIter(object):
+    def __init__(self, batch_size, input_length, nthreads=6, web_viz=False):
+        super(RLDataIter, self).__init__()
+        self.batch_size = batch_size
+        self.input_length = input_length
+        self.env = [self.make_env() for _ in range(batch_size)]
+        self.act_dim = self.env[0].action_space.n
+
+        self.state_ = None
+
+        self.reset()
+
+        self.provide_data = [mx.io.DataDesc('data', self.state_.shape, np.uint8)]
+
+        self.web_viz = web_viz
+        if web_viz:
+            self.queue = Queue.Queue()
+            self.thread = Thread(target=make_web, args=(self.queue,))
+            self.thread.daemon = True
+            self.thread.start()
+
+        self.nthreads = nthreads
+        if nthreads > 1:
+            self.pool = multiprocessing.pool.ThreadPool(6)
+
+    def make_env(self):
+        raise NotImplementedError()
+
+    def reset(self):
+        self.state_ = np.tile(
+            np.asarray([env.reset() for env in self.env], dtype=np.uint8).transpose((0, 3, 1, 2)),
+            (1, self.input_length, 1, 1))
+
+    def visual(self):
+        raise NotImplementedError()
+
+    def act(self, action):
+        if self.nthreads > 1:
+            new = self.pool.map(env_step, zip(self.env, action))
+        else:
+            new = [env.step(act) for env, act in zip(self.env, action)]
+
+        reward = np.asarray([i[1] for i in new], dtype=np.float32)
+        done = np.asarray([i[2] for i in new], dtype=np.float32)
+
+        channels = self.state_.shape[1]/self.input_length
+        state = np.zeros_like(self.state_)
+        state[:,:-channels,:,:] = self.state_[:,channels:,:,:]
+        for i, (ob, env) in enumerate(zip(new, self.env)):
+            if ob[2]:
+                state[i,-channels:,:,:] = env.reset().transpose((2,0,1))
+            else:
+                state[i,-channels:,:,:] = ob[0].transpose((2,0,1))
+        self.state_ = state
+
+        if self.web_viz:
+            try:
+                while self.queue.qsize() > 10:
+                    self.queue.get(False)
+            except Empty:
+                pass
+            frame = self.visual()
+            self.queue.put(frame)
+
+        return reward, done
+
+    def data(self):
+        return [mx.nd.array(self.state_, dtype=np.uint8)]
+
+
+class GymDataIter(RLDataIter):
+    def __init__(self, game, batch_size, input_length, web_viz=False):
+        self.game = game
+        super(GymDataIter, self).__init__(batch_size, input_length, web_viz=web_viz)
+
+    def make_env(self):
+        return gym.make(self.game)
+
+    def visual(self):
+        data = self.state_[:4, -self.state_.shape[1]/self.input_length:, :, :]
+        return visual(np.asarray(data, dtype=np.uint8), False)
+
+if __name__ == '__main__':
+    batch_size = 64
+    dataiter = GymDataIter('Breakout-v0', batch_size, 4)
+    dataiter.reset()
+    tic = time.time()
+    for _ in range(10):
+        #data = dataiter.next().data[0].asnumpy().astype(np.uint8)
+        #visual(data[:,-data.shape[1]/dataiter.input_length:,:,:])
+        for _ in range(100):
+            dataiter.act([env.action_space.sample() for env in dataiter.env])
+            dataiter.clear_history()
+            dataiter.next()
+        print batch_size*100/(time.time() - tic)
+        tic = time.time()
+
+
