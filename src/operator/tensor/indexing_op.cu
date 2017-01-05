1fea72ea6b2ea29bd7ae05adfd6ee766705b8782
Welly Zhang <wellyzhangc@gmail.com>
2017-01-05 14:57:45 +0800

    add new op of take

--- src/operator/tensor/indexing_op.cu
+++ src/operator/tensor/indexing_op.cu
@@ -1,8 +1,8 @@
 /*!
- * Copyright (c) 2016 by Contributors
+ * Copyright (c) 2017 by Contributors
  * \file indexing_op.cu
  * \brief
- * \author Siyi Li
+ * \author Siyi Li, Chi Zhang
 */
 
 #include "./indexing_op.h"
@@ -13,6 +13,12 @@ NNVM_REGISTER_OP(Embedding)
 
 NNVM_REGISTER_OP(_backward_Embedding)
 .set_attr<FCompute>("FCompute<gpu>", EmbeddingOpBackward<gpu>);
+
+NNVM_REGISTER_OP(take)
+.set_attr<FCompute>("FCompute<gpu>", TakeOpForward<gpu>);
+
+NNVM_REGISTER_OP(_backward_take)
+.set_attr<FCompute>("FCompute<gpu>", TakeOpBackward<gpu>);
 }  // namespace op
 }  // namespace mxnet
 
