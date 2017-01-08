98fcce0a01608e8f0ee2a217b9eef5b2855c0d75
Xingjian Shi <xshiab@ust.hk>
2017-01-08 20:09:58 +0800

    fix compile error (#4590)

--- src/operator/tensor/broadcast_reduce_op_index.cc
+++ src/operator/tensor/broadcast_reduce_op_index.cc
@@ -12,7 +12,7 @@ MXNET_OPERATOR_REGISTER_REDUCE_AXIS(argmax)
 .set_attr<FCompute>("FCompute<cpu>", SearchAxisCompute<cpu, mshadow::red::maximum>)
 .set_attr<nnvm::FGradient>("FGradient",
   [](const nnvm::NodePtr& n, const std::vector<nnvm::NodeEntry>& ograds) {
-    return MakeGradNode("_zeros", n, {}, {});
+    return MakeGradNode("_zeros", n, {}, std::unordered_map<std::string, std::string>());
 });
 
 MXNET_OPERATOR_REGISTER_REDUCE_AXIS(argmin)
@@ -20,7 +20,7 @@ MXNET_OPERATOR_REGISTER_REDUCE_AXIS(argmin)
 .set_attr<FCompute>("FCompute<cpu>", SearchAxisCompute<cpu, mshadow::red::minimum>)
 .set_attr<nnvm::FGradient>("FGradient",
   [](const nnvm::NodePtr& n, const std::vector<nnvm::NodeEntry>& ograds) {
-    return MakeGradNode("_zeros", n, {}, {});
+    return MakeGradNode("_zeros", n, {}, std::unordered_map<std::string, std::string>());
 });
 
 // Legacy support
