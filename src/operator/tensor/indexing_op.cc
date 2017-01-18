e8f66614ebe60eecc8710e226d610c18cb23131c
Welly Zhang <wellyzhangc@gmail.com>
2017-01-18 12:52:49 +0800

    add new OP take

--- src/operator/tensor/indexing_op.cc
+++ src/operator/tensor/indexing_op.cc
@@ -1,8 +1,8 @@
 /*!
- * Copyright (c) 2016 by Contributors
+ * Copyright (c) 2017 by Contributors
  * \file indexing_op.cc
  * \brief
- * \author Siyi Li
+ * \author Siyi Li, Chi Zhang
 */
 
 #include "./indexing_op.h"
@@ -48,5 +48,47 @@ NNVM_REGISTER_OP(_backward_Embedding)
   })
 .set_attr<nnvm::TIsBackward>("TIsBackward", true)
 .set_attr<FCompute>("FCompute<cpu>", EmbeddingOpBackward<cpu>);
+
+DMLC_REGISTER_PARAMETER(TakeParam);
+
+NNVM_REGISTER_OP(take)
+.MXNET_DESCRIBE("Take row vectors from an NDArray according to the indices"
+                " For an input of index with shape (d1, ..., dK), the output"
+                " shape is (d1, ..., dK, row_vector_length).All the input"
+                " values should be integers in the range"
+                " [0, column_vector_length).")
+.set_num_inputs(2)
+.set_num_outputs(1)
+.set_attr_parser(TakeParamParser<TakeParam>)
+.set_attr<nnvm::FListInputNames>("FListInputNames",
+  [](const NodeAttrs& attrs) {
+    return std::vector<std::string>{"a", "indices"};
+  })
+.set_attr<nnvm::FInferShape>("FInferShape", TakeOpShape)
+.set_attr<nnvm::FInferType>("FInferType", TakeOpType)
+.set_attr<FResourceRequest>("FResourceRequest",
+  [](const NodeAttrs& attrs) {
+    return std::vector<ResourceRequest>{ResourceRequest::kTempSpace};
+  })
+.set_attr<FCompute>("FCompute<cpu>", TakeOpForward<cpu>)
+.set_attr<nnvm::FGradient>("FGradient",
+  [](const nnvm::NodePtr& n,  const std::vector<nnvm::NodeEntry>& ograds) {
+    std::vector<nnvm::NodeEntry> heads(ograds.begin(), ograds.end());
+    heads.push_back(n->inputs[1]);
+    return MakeGradNode("_backward_take", n, heads, n->attrs.dict);
+  })
+.add_argument("a", "Symbol", "The source array.")
+.add_argument("indices", "Symbol", "The indices of the values to extract.")
+.add_arguments(TakeParam::__FIELDS__());
+
+NNVM_REGISTER_OP(_backward_take)
+.set_num_inputs(2)
+.set_num_outputs(2)
+.set_attr<FResourceRequest>("FResourceRequest",
+  [](const NodeAttrs& attrs) {
+    return std::vector<ResourceRequest>{ResourceRequest::kTempSpace};
+  })
+.set_attr<nnvm::TIsBackward>("TIsBackward", true)
+.set_attr<FCompute>("FCompute<cpu>", TakeOpBackward<cpu>);
 }  // namespace op
 }  // namespace mxnet
