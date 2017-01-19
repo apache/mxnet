808ca69c7d43e8838738c3a37597d50917fa713b
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-01-19 15:37:07 -0800

    Add command

--- tests/ci_build/install/install_opencv.sh
+++ tests/ci_build/install/install_opencv.sh
@@ -10,4 +10,5 @@ mkdir -p build
 cd build
 cmake -D BUILD_opencv_gpu=OFF -D WITH_EIGEN=ON -D WITH_TBB=ON -D WITH_CUDA=OFF -D WITH_1394=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make PREFIX=/usr/local install
+pip install opencv-python
 
