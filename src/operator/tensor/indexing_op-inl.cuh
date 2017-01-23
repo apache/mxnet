c37ab025baf41ff5ae905172d1ca7d77be27e734
Antti-Pekka Hynninen <aphynninen@hotmail.com>
2017-01-23 17:37:22 -0600

    Fixed tabs

--- src/operator/tensor/indexing_op-inl.cuh
+++ src/operator/tensor/indexing_op-inl.cuh
@@ -15,149 +15,149 @@ const int kWarpSize = 32;
 
 template<int SZ, typename DType, typename IdxType>
 __global__ void AddTakeGradLargeBatchKernel(DType* dst,
-																					 // If idx_start == NULL, then in-kernel edge
-																					 // detection is used
-																					 const IdxType *idx_start,
-																					 // idx_start_size_ptr ignored if idx_start == NULL
-																					 const int* idx_start_size_ptr,
-																					 const IdxType *sorted, const IdxType *index,
-																					 const DType *src,
-																					 int ymax, int xmax) {
-	// Size of the shared memory is [blockDim.x*SZ*blockDim.y]*sizeof(DType)
-	extern __shared__ char sh_grad_weight_char[];
-	DType* sh_grad_weight = (DType*)sh_grad_weight_char;
+                                           // If idx_start == NULL, then in-kernel edge
+                                           // detection is used
+                                           const IdxType *idx_start,
+                                           // idx_start_size_ptr ignored if idx_start == NULL
+                                           const int* idx_start_size_ptr,
+                                           const IdxType *sorted, const IdxType *index,
+                                           const DType *src,
+                                           int ymax, int xmax) {
+  // Size of the shared memory is [blockDim.x*SZ*blockDim.y]*sizeof(DType)
+  extern __shared__ char sh_grad_weight_char[];
+  DType* sh_grad_weight = (DType*)sh_grad_weight_char;
 
-	int iidx_end = (idx_start == NULL) ? ymax : *idx_start_size_ptr;
+  int iidx_end = (idx_start == NULL) ? ymax : *idx_start_size_ptr;
 
-	for (int iidx = blockIdx.y;iidx < iidx_end;iidx += gridDim.y) {
+  for (int iidx = blockIdx.y;iidx < iidx_end;iidx += gridDim.y) {
 
-		// Thread block sums up elements in the range [idx_begin, idx_end-1]
-		int idx_begin, idx_end;
-		int sorted_value;
-		if (idx_start == NULL) {
-			idx_begin = iidx;
-			sorted_value = static_cast<int>(sorted[idx_begin]);
-			if (idx_begin > 0 && sorted_value == static_cast<int>(sorted[idx_begin - 1])) continue;
-			// Algorithm is explained using an example:
-			//   blockDim.x = 32
-			//   blockDim.y = 4
-			//   sorted[idx_begin:] = [4 4 4 9]
-			//   (3,4) denotes threadIdx.x=3, threadIdx.y=4, ":" is used for ranges 
-			//   (0:31,0:3) sorted_value = 4
-			idx_end = idx_begin + 1;
-			unsigned int* sh_ballot = (unsigned int*)sh_grad_weight_char;
-			int no_edge = 0;
-			do {
-				int idx = idx_end + threadIdx.x + threadIdx.y*blockDim.x;
-				// Example:
-				//   (0:1,0) sorted_idx = 4
-				//   (rest)  sorted_idx = -1
-				int sorted_idx = (idx < ymax) ? static_cast<int>(sorted[idx]) : -1;
-				// Example:
-				//   (0:31,0) sh_ballot[0]     = 0b100
-				//   (rest)   sh_ballot[1...3] = 0
-				// sh_ballot[] tells us which thread within the warp found the edge
-				sh_ballot[threadIdx.y] = __ballot(sorted_value != sorted_idx);
-				__syncthreads();
-				// No edge if sh_ballot[threadIdx.x] == 0
-				// NOTE: All warps have the same value for no_edge
-				// Example:
-				//   (0,:)  no_edge = 0
-				//   (rest) no_edge = 1
-				no_edge = (threadIdx.x < blockDim.y) ? (sh_ballot[threadIdx.x] == 0) : 1;
-				idx_end += blockDim.x*blockDim.y;
-				// Example:
-				//   __all(no_edge) = 0 since no_edge = 0 for threadIdx.x = 0, hence we leave the loop
-			} while (__all(no_edge));
-			idx_end -= blockDim.x*blockDim.y;
-			// Find the first edge
-			// Example:
-			//   (0,:)  val = 1 << 0 = 1
-			//   (rest) val = 0
-			unsigned int val = (threadIdx.x < blockDim.y && sh_ballot[threadIdx.x] != 0) ?
-				(1 << threadIdx.x) : 0;
-			// NOTE: We do butterfly reduction on the entire warp width
-			//       so that all threads have the same result
-			// Example:
-			//   (all) val = 1
-			#pragma unroll
-			for (int i=warpSize/2;i>=1;i/=2) val |= __shfl_xor(val, i);
-			// __ffs() returns the position of first set bit, 1...32. __ffs(1) = 1
-			// j will be the warp index where edge was found
-			// Example:
-			//   (all) j = 1 - 1 = 0
-			int j = __ffs(val) - 1;
-			// j = warp index where the edge was found
-			// __ffs(sh_ballot[j]) - 1 = warp lane where the edge was found
-			// idx_end points to the one over the last value.
-			// Example:
-			//  idx_end += 0*blockDim.x + _ffs(0b100) - 1 = 0 + 3 - 1 = 2
-			//  sorted[idx_end] = 9
-			idx_end += j*blockDim.x + __ffs(sh_ballot[j]) - 1;
-			__syncthreads();
-		} else {
-			idx_begin = idx_start[iidx];
-			idx_end   = ((iidx + 1) < iidx_end) ? idx_start[iidx + 1] : ymax;
-			sorted_value = static_cast<int>(sorted[idx_begin]);
-		}
+    // Thread block sums up elements in the range [idx_begin, idx_end-1]
+    int idx_begin, idx_end;
+    int sorted_value;
+    if (idx_start == NULL) {
+      idx_begin = iidx;
+      sorted_value = static_cast<int>(sorted[idx_begin]);
+      if (idx_begin > 0 && sorted_value == static_cast<int>(sorted[idx_begin - 1])) continue;
+      // Algorithm is explained using an example:
+      //   blockDim.x = 32
+      //   blockDim.y = 4
+      //   sorted[idx_begin:] = [4 4 4 9]
+      //   (3,4) denotes threadIdx.x=3, threadIdx.y=4, ":" is used for ranges 
+      //   (0:31,0:3) sorted_value = 4
+      idx_end = idx_begin + 1;
+      unsigned int* sh_ballot = (unsigned int*)sh_grad_weight_char;
+      int no_edge = 0;
+      do {
+        int idx = idx_end + threadIdx.x + threadIdx.y*blockDim.x;
+        // Example:
+        //   (0:1,0) sorted_idx = 4
+        //   (rest)  sorted_idx = -1
+        int sorted_idx = (idx < ymax) ? static_cast<int>(sorted[idx]) : -1;
+        // Example:
+        //   (0:31,0) sh_ballot[0]     = 0b100
+        //   (rest)   sh_ballot[1...3] = 0
+        // sh_ballot[] tells us which thread within the warp found the edge
+        sh_ballot[threadIdx.y] = __ballot(sorted_value != sorted_idx);
+        __syncthreads();
+        // No edge if sh_ballot[threadIdx.x] == 0
+        // NOTE: All warps have the same value for no_edge
+        // Example:
+        //   (0,:)  no_edge = 0
+        //   (rest) no_edge = 1
+        no_edge = (threadIdx.x < blockDim.y) ? (sh_ballot[threadIdx.x] == 0) : 1;
+        idx_end += blockDim.x*blockDim.y;
+        // Example:
+        //   __all(no_edge) = 0 since no_edge = 0 for threadIdx.x = 0, hence we leave the loop
+      } while (__all(no_edge));
+      idx_end -= blockDim.x*blockDim.y;
+      // Find the first edge
+      // Example:
+      //   (0,:)  val = 1 << 0 = 1
+      //   (rest) val = 0
+      unsigned int val = (threadIdx.x < blockDim.y && sh_ballot[threadIdx.x] != 0) ?
+        (1 << threadIdx.x) : 0;
+      // NOTE: We do butterfly reduction on the entire warp width
+      //       so that all threads have the same result
+      // Example:
+      //   (all) val = 1
+      #pragma unroll
+      for (int i=warpSize/2;i>=1;i/=2) val |= __shfl_xor(val, i);
+      // __ffs() returns the position of first set bit, 1...32. __ffs(1) = 1
+      // j will be the warp index where edge was found
+      // Example:
+      //   (all) j = 1 - 1 = 0
+      int j = __ffs(val) - 1;
+      // j = warp index where the edge was found
+      // __ffs(sh_ballot[j]) - 1 = warp lane where the edge was found
+      // idx_end points to the one over the last value.
+      // Example:
+      //  idx_end += 0*blockDim.x + _ffs(0b100) - 1 = 0 + 3 - 1 = 2
+      //  sorted[idx_end] = 9
+      idx_end += j*blockDim.x + __ffs(sh_ballot[j]) - 1;
+      __syncthreads();
+    } else {
+      idx_begin = idx_start[iidx];
+      idx_end   = ((iidx + 1) < iidx_end) ? idx_start[iidx + 1] : ymax;
+      sorted_value = static_cast<int>(sorted[idx_begin]);
+    }
 
-		const int start_feature = threadIdx.x + blockIdx.x * blockDim.x * SZ;
-		const int dst_row = sorted_value * xmax;
+    const int start_feature = threadIdx.x + blockIdx.x * blockDim.x * SZ;
+    const int dst_row = sorted_value * xmax;
 
-		int num_idx = idx_end - idx_begin;
-		int idx0 = idx_begin + threadIdx.y*num_idx/blockDim.y;
-		int idx1 = idx_begin + (threadIdx.y + 1)*num_idx/blockDim.y;
+    int num_idx = idx_end - idx_begin;
+    int idx0 = idx_begin + threadIdx.y*num_idx/blockDim.y;
+    int idx1 = idx_begin + (threadIdx.y + 1)*num_idx/blockDim.y;
 
-		// Read and sum data into grad_weight[]
-		DType grad_weight[SZ];
-		#pragma unroll
-		for (int ii = 0; ii < SZ; ii++) {
-			grad_weight[ii] = (DType)0;
-		}
-		for (int idx=idx0; idx < idx1;idx++) {
-			const int src_row = static_cast<int>(index[idx]) * xmax;
-			#pragma unroll
-			for (int ii = 0; ii < SZ; ii++)
-			{
-				int feature_dim = start_feature + ii * blockDim.x;
-				if (feature_dim < xmax)
-				{
-					grad_weight[ii] += src[src_row + feature_dim];
-				}
-			}
-		}
-		#pragma unroll
-		for (int ii = 0; ii < SZ; ii++) {
-			sh_grad_weight[threadIdx.x + ii*blockDim.x + threadIdx.y*blockDim.x*SZ] = grad_weight[ii];
-		}
-		__syncthreads();
-		// We now have grad_weight[] values, reduce within thread block
-		for (int t=1;t < blockDim.y;t <<= 1) {
-			DType tmp[SZ];
-			#pragma unroll
-			for (int ii = 0; ii < SZ; ii++) {
-				tmp[ii] = (threadIdx.y + t < blockDim.y) ?
-					sh_grad_weight[threadIdx.x + ii*blockDim.x + (threadIdx.y + t)*blockDim.x*SZ] : (DType)0;
-			}
-			__syncthreads();
-			#pragma unroll
-			for (int ii = 0; ii < SZ; ii++) {
-				sh_grad_weight[threadIdx.x + ii*blockDim.x + threadIdx.y*blockDim.x*SZ] += tmp[ii];
-			}
-			__syncthreads();
-		}
-		// Result is in sh_grad_weight[threadIdx.x + ii*blockDim.x]
-		if (threadIdx.y == 0) {
-			#pragma unroll
-			for (int ii = 0; ii < SZ; ii++) {
-				int feature_dim = start_feature + ii * blockDim.x;
-				if (feature_dim < xmax) {
-					dst[dst_row + feature_dim] += sh_grad_weight[threadIdx.x + ii*blockDim.x];
-				}
-			}
-		}
-	
-	}
+    // Read and sum data into grad_weight[]
+    DType grad_weight[SZ];
+    #pragma unroll
+    for (int ii = 0; ii < SZ; ii++) {
+      grad_weight[ii] = (DType)0;
+    }
+    for (int idx=idx0; idx < idx1;idx++) {
+      const int src_row = static_cast<int>(index[idx]) * xmax;
+      #pragma unroll
+      for (int ii = 0; ii < SZ; ii++)
+      {
+        int feature_dim = start_feature + ii * blockDim.x;
+        if (feature_dim < xmax)
+        {
+          grad_weight[ii] += src[src_row + feature_dim];
+        }
+      }
+    }
+    #pragma unroll
+    for (int ii = 0; ii < SZ; ii++) {
+      sh_grad_weight[threadIdx.x + ii*blockDim.x + threadIdx.y*blockDim.x*SZ] = grad_weight[ii];
+    }
+    __syncthreads();
+    // We now have grad_weight[] values, reduce within thread block
+    for (int t=1;t < blockDim.y;t <<= 1) {
+      DType tmp[SZ];
+      #pragma unroll
+      for (int ii = 0; ii < SZ; ii++) {
+        tmp[ii] = (threadIdx.y + t < blockDim.y) ?
+          sh_grad_weight[threadIdx.x + ii*blockDim.x + (threadIdx.y + t)*blockDim.x*SZ] : (DType)0;
+      }
+      __syncthreads();
+      #pragma unroll
+      for (int ii = 0; ii < SZ; ii++) {
+        sh_grad_weight[threadIdx.x + ii*blockDim.x + threadIdx.y*blockDim.x*SZ] += tmp[ii];
+      }
+      __syncthreads();
+    }
+    // Result is in sh_grad_weight[threadIdx.x + ii*blockDim.x]
+    if (threadIdx.y == 0) {
+      #pragma unroll
+      for (int ii = 0; ii < SZ; ii++) {
+        int feature_dim = start_feature + ii * blockDim.x;
+        if (feature_dim < xmax) {
+          dst[dst_row + feature_dim] += sh_grad_weight[threadIdx.x + ii*blockDim.x];
+        }
+      }
+    }
+  
+  }
 }
 
 template <typename IndexType, typename xpu>
