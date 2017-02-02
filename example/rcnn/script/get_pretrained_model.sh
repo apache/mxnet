46de61559c8dcad1479726595c8798ac2daa18ae
Guo Haria <haria@users.noreply.github.com>
2017-02-03 01:57:16 +0800

    fix broken url (#4859)

--- example/rcnn/script/get_pretrained_model.sh
+++ example/rcnn/script/get_pretrained_model.sh
@@ -8,6 +8,6 @@ fi
 
 # download pretrained model
 pushd model
-wget http://data.dmlc.ml/models/imagenet/vgg/vgg16-0000.params
-wget http://data.dmlc.ml/models/imagenet/resnet/101-layers/resnet-101-0000.params
+wget http://data.dmlc.ml/mxnet/models/imagenet/vgg/vgg16-0000.params
+wget http://data.dmlc.ml/mxnet/models/imagenet/resnet/101-layers/resnet-101-0000.params
 popd
