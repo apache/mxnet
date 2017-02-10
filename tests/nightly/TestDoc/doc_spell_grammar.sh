1c2434bceb0da26adc0e69fa05958eb7cdd1ba93
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-02-10 15:24:42 -0800

    Fix spell path

--- tests/nightly/TestDoc/doc_spell_grammar.sh
+++ tests/nightly/TestDoc/doc_spell_grammar.sh
@@ -26,9 +26,10 @@ echo "Install other dependencies"
 cd ..
 yum -y install enchant
 pip install pyenchant
-pwd
-ls
-cp /tests/nightly/TestDoc/{en_US-large.aff, en_US-large.dic, en_US.aff, en_US.dic} /pyenchant/enchant/share/enchant/myspell
+mkdir /usr/local/lib/python2.7/site-packages/enchant/share
+mkdir /usr/local/lib/python2.7/site-packages/enchant/share/enchant
+mkdir /usr/local/lib/python2.7/site-packages/enchant/share/enchant/myspell
+cp /tests/nightly/TestDoc/{en_US-large.aff, en_US-large.dic, en_US.aff, en_US.dic} /usr/local/lib/python2.7/site-packages/enchant/share/enchant/myspell
 pip install grammar-check
 pip install html2text
 pip install sphinx==1.5.1 CommonMark==0.5.4 breathe mock==1.0.1 recommonmark
