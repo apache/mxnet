a636e7289c33def92c30edf533c245063c90cfc3
piiswrong <eric.jy.xie@gmail.com>
2017-02-14 07:52:53 +0000

    fix

--- src/operator/tensor/broadcast_reduce_op_value.cc
+++ src/operator/tensor/broadcast_reduce_op_value.cc
@@ -90,8 +90,8 @@ MXNET_OPERATOR_REGISTER_BROADCAST(broadcast_axis)
 
 MXNET_OPERATOR_REGISTER_BROADCAST(broadcast_to)
 .MXNET_DESCRIBE(
-	"Broadcast src to shape. If shape[i] is 0, "
-	"input size will be preserved for axis i.")
+  "Broadcast src to shape. If shape[i] is 0, "
+  "input size will be preserved for axis i.")
 .set_attr_parser(ParamParser<BroadcastToParam>)
 .add_arguments(BroadcastToParam::__FIELDS__())
 .set_attr<nnvm::FInferShape>("FInferShape", BroadcastToShape)
