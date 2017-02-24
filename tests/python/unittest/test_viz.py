9209b7d6fdea90b2b78e2e7307db85755f97909c
EC2 Default User <ec2-user@ip-172-31-46-28.us-west-2.compute.internal>
2017-02-24 11:40:13 +0000

    test of the symbol with multiple outputs was added

--- tests/python/unittest/test_viz.py
+++ tests/python/unittest/test_viz.py
@@ -9,10 +9,11 @@ def test_print_summary():
     mp1 = mx.symbol.Pooling(data = act1, name = 'mp1', kernel=(2,2), stride=(2,2), pool_type='max')
     fc1 = mx.sym.FullyConnected(data=mp1, bias=bias, name='fc1', num_hidden=10, lr_mult=0)
     fc2 = mx.sym.FullyConnected(data=fc1, name='fc2', num_hidden=10, wd_mult=0.5)
-    mx.viz.print_summary(fc2)
+    sc1 = mx.symbol.SliceChannel(data=fc2, num_outputs=10, name="slice_1", squeeze_axis=0)
+    mx.viz.print_summary(sc1)
     shape = {}
     shape["data"]=(1,3,28,28)
-    mx.viz.print_summary(fc2, shape)
+    mx.viz.print_summary(sc1, shape)
 
 if __name__ == "__main__":
     test_print_summary()
