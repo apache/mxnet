8c6327d491b1a80bb583308f81da9d3d655b97d3
Jencir Lee <jli05@icloud.com>
2017-03-05 14:21:24 +0000

    fix lint
    add tests for CP Decomposition
    
    rename files
    
    working copy of CP Decomposition
    
    use tensors generated from orthonormal factor matrices for test
    
    add check of eigvals and factors_T dimensions

--- src/operator/tensor/cp_decomp.h
+++ src/operator/tensor/cp_decomp.h
@@ -1,9 +1,18 @@
-#include "./cp_decomp-inl.h"
+/*!
+ *  Copyright (c) 2014 by Contributors
+ */
+#ifndef MXNET_OPERATOR_TENSOR_CP_DECOMP_H_
+#define MXNET_OPERATOR_TENSOR_CP_DECOMP_H_
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <mshadow/tensor.h>
+#include <vector>
 #include <cmath>
 #include <random>
-#include <vector>
 #include <utility>
 #include <numeric>
+#include <iostream>
 
 #if MSHADOW_USE_MKL
 extern "C" {
@@ -15,46 +24,47 @@ extern "C" {
 
 
 namespace mxnet {
-namespace op {  
+namespace op {
 
 using namespace mshadow;
 using namespace mshadow::expr;
 
+template <typename DType>
+inline void print1DTensor_(const Tensor<cpu, 1, DType> &t);
+
+template <typename DType>
+inline void print2DTensor_(const Tensor<cpu, 2, DType> &t);
+
+
 template <typename T>
-std::vector<std::vector<T> > cart_product
-	(const std::vector<std::vector<T> > &v) {
-	std::vector<std::vector<T> > s = {{}};
-	for (const auto &u : v) {
-		std::vector<std::vector<T> > r;
-		for (const auto y : u) {
-			for (const auto &prod : s) {
-			  r.push_back(prod);
-			  r.back().push_back(y);
-			}
-		}
-		s = std::move(r);
-	}
-	return s;
+inline std::vector<std::vector<T> > cart_product
+  (const std::vector<std::vector<T> > &v) {
+  std::vector<std::vector<T> > s = {{}};
+  for (const auto &u : v) {
+    std::vector<std::vector<T> > r;
+    for (const auto y : u) {
+      for (const auto &prod : s) {
+        r.push_back(prod);
+        r.back().push_back(y);
+      }
+    }
+    s = std::move(r);
+  }
+  return s;
 }
 
 template <int order, typename DType>
-DType TensorAt(const Tensor<cpu, order, DType> &t,
-  const std::vector<index_t> &coord) {
-  if (coord.size() == 1)
-    return t[coord[0]].dptr_[0];
-
-  std::vector<index_t> coord_tail(coord.begin() + 1, coord.end());
-  return TensorAt(t[coord[0]], coord_tail);
-}
+inline DType TensorAt(const Tensor<cpu, order, DType> &t,
+  const std::vector<index_t> &coord);
 
 template <int order, typename DType>
 inline void Unfold
-  (Tensor<cpu, 2, DType> &unfolding,
+  (Tensor<cpu, 2, DType> unfolding,
   const Tensor<cpu, order, DType> &t,
   int mode,
-  Stream<cpu> *stream) {
-	std::vector<std::vector<index_t> > v;
-  for (size_t id_mode = 0; id_mode < order; ++id_mode) {
+  Stream<cpu> *stream = NULL) {
+  std::vector<std::vector<index_t> > v;
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
     if (id_mode == mode)
       continue;
 
@@ -75,15 +85,15 @@ inline void Unfold
 }
 
 
-template <int order, typename DType>
+template <typename DType>
 inline int CPDecompUpdate
-  (Tensor<cpu, 1, DType> &eigvals,
-  std::vector<Tensor<cpu, 2, DType> > &factors,
-  const Tensor<cpu, order, DType> &unfolding,
+  (Tensor<cpu, 1, DType> eigvals,
+  std::vector<Tensor<cpu, 2, DType> > factors,
+  const Tensor<cpu, 2, DType> &unfolding,
   int mode,
-  Tensor<cpu, 2, DType> &kr_prod,
-  Tensor<cpu, 2, DType> &hd_prod,
-  Stream<cpu> *stream = NULL); 
+  std::vector<TensorContainer<cpu, 2, DType> > kr_prod,
+  TensorContainer<cpu, 2, DType> hd_prod,
+  Stream<cpu> *stream = NULL);
 
 template <typename DType>
 inline bool CPDecompConverged
@@ -95,18 +105,18 @@ inline bool CPDecompConverged
 
 template <int order, typename DType>
 inline int CPDecompForward
-  (Tensor<cpu, 1, DType> &eigvals,
-  std::vector<Tensor<cpu, 2, DType> > &factors_T,
+  (Tensor<cpu, 1, DType> eigvals,
+  std::vector<Tensor<cpu, 2, DType> > factors_T,
   const Tensor<cpu, order, DType> &in,
-  int k, 
-  DType eps,
-  int max_iter,
-  Stream<cpu> *stream) {
-  CHECK_EQ(eigvals.size(0), k);
-  CHECK_EQ(factors_T.size(), order);
-  CHECK(k >= 1);
-  for (size_t i = 0; i < order; ++i) {
-    CHECK_EQ(factors_T[i].size(0), k);
+  int k,
+  DType eps = 1e-6,
+  int max_iter = 100,
+  Stream<cpu> *stream = NULL) {
+  CHECK_EQ((int) eigvals.size(0), k);
+  CHECK_EQ((int) factors_T.size(), order);
+  CHECK_GE(k, 1);
+  for (int i = 0; i < order; ++i) {
+    CHECK_EQ((int) factors_T[i].size(0), k);
     CHECK_EQ(factors_T[i].size(1), in.size(i));
   }
 
@@ -116,36 +126,41 @@ inline int CPDecompForward
   // in is unfolded mode-1 tensor
   // Transform it into mode-2, mode-3 tensors
   int tensor_size = 1;
-  for (size_t i = 0; i < order; ++i)
+  for (int i = 0; i < order; ++i)
     tensor_size *= in.size(i);
 
-  std::vector<TensorContainer<cpu, 2, DType> > unfoldings;
-  for (size_t id_mode = 0; id_mode < order; ++id_mode) {
+  std::vector<Tensor<cpu, 2, DType> > unfoldings;
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
     unfoldings.emplace_back
       (Shape2(in.size(id_mode), tensor_size / in.size(id_mode)));
+    AllocSpace(&unfoldings[id_mode]);
+
     Unfold(unfoldings[id_mode], in, id_mode);
   }
 
   // Allocate space for old factor matrices A, B, C, etc,
   // transposed as well, with the same shapes as factors_T
-  TensorContainer<cpu, 1, DType> oldEigvals(Shape1(k));
-  std::vector<TensorContainer<cpu, 2, DType> > oldFactors_T;
-  for (size_t id_mode = 0; id_mode < order; ++id_mode)
+  Tensor<cpu, 1, DType> oldEigvals(Shape1(k));
+  AllocSpace(&oldEigvals);
+  std::vector<Tensor<cpu, 2, DType> > oldFactors_T;
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
     oldFactors_T.emplace_back(factors_T[id_mode].shape_);
+    AllocSpace(&oldFactors_T[id_mode]);
+  }
 
   // The intermediate tensors are reused for efficiency
   // We store the transpose of all intermediate and final
   // Khatri-Rao products for convenience of computation
   //
   // As across the modes, the intermediate Khatri-Rao products
-  // will eventually take different shapes, kr_prod_T is 
+  // will eventually take different shapes, kr_prod_T is
   // first indexed by mode, then by the number of Khatri-Rao
   // products already done
   std::vector<std::vector<TensorContainer<cpu, 2, DType> > > kr_prod_T;
   int kr_length;
-  for (size_t id_mode = 0; id_mode < order; ++id_mode) {
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
     kr_length = 1;
-   
+
     std::vector<TensorContainer<cpu, 2, DType> > kr_prod_T_;
     kr_prod_T_.emplace_back(Shape2(k, kr_length));
     kr_prod_T_[0] = 1;
@@ -165,29 +180,29 @@ inline int CPDecompForward
   TensorContainer<cpu, 2, DType> hd_prod(Shape2(k, k));
 
   // Randomly initialise factor matrices
-  // TODO implement seed for random generator
+  // TODO(jli05): implement seed for random generator
   std::default_random_engine generator;
   std::normal_distribution<double> normal(0.0, 1.0);
 
-  for (size_t id_mode = 0; id_mode < order; ++id_mode) {
-    for (size_t i = 0; i < k; ++i) { 
-      for (size_t j = 0; j < factors_T[id_mode].size(1); ++j)
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
+    for (int i = 0; i < k; ++i) {
+      for (int j = 0; j < (int) factors_T[id_mode].size(1); ++j)
         factors_T[id_mode][i][j] = normal(generator);
     }
   }
 
-  // ALS  
+  // ALS
   int iter = 0;
-  while (iter < max_iter 
-      && (iter == 0 || !CPDecompConverged(eigvals, factors_T, 
+  while (iter < max_iter
+      && (iter == 0 || !CPDecompConverged(eigvals, factors_T,
                                    oldEigvals, oldFactors_T, eps))) {
     Copy(oldEigvals, eigvals);
-    for (size_t id_mode = 0; id_mode < order; ++id_mode)
+    for (int id_mode = 0; id_mode < order; ++id_mode)
       Copy(oldFactors_T[id_mode], factors_T[id_mode]);
 
-    for (size_t id_mode = 0; id_mode < order; ++id_mode) {
+    for (int id_mode = 0; id_mode < order; ++id_mode) {
       info = CPDecompUpdate
-        (eigvals, factors_T, 
+        (eigvals, factors_T,
         unfoldings[id_mode], id_mode,
         kr_prod_T[id_mode], hd_prod,
         stream);
@@ -198,54 +213,60 @@ inline int CPDecompForward
     ++iter;
   }
 
+  // Free up space
+  for (int id_mode = 0; id_mode < order; ++id_mode) {
+    FreeSpace(&unfoldings[id_mode]);
+    FreeSpace(&oldFactors_T[id_mode]);
+  }
+
+  FreeSpace(&oldEigvals);
+
   return 0;
 }
 
-template <typename Device, typename DType>
-int posv(int n, int nrhs, DType *a, int lda, DType *b, int ldb);
+template <typename DType>
+inline int posv(int n, int nrhs, DType *a, int lda, DType *b, int ldb);
 
 template <>
-int posv<cpu, float>(int n, int nrhs, 
+inline int posv<float>(int n, int nrhs,
     float *a, int lda, float *b, int ldb) {
-  return LAPACKE_sposv(LAPACK_ROW_MAJOR, 'U', n, nrhs, a, lda, b, ldb); 
+  return LAPACKE_sposv(LAPACK_ROW_MAJOR, 'U', n, nrhs, a, lda, b, ldb);
 }
 
 template <>
-int posv<cpu, double>(int n, int nrhs, 
+inline int posv<double>(int n, int nrhs,
     double *a, int lda, double *b, int ldb) {
-  return LAPACKE_dposv(LAPACK_ROW_MAJOR, 'U', n, nrhs, a, lda, b, ldb); 
+  return LAPACKE_dposv(LAPACK_ROW_MAJOR, 'U', n, nrhs, a, lda, b, ldb);
 }
 
-template <typename Device, typename DType>
-DType nrm2(int n, DType *a, int lda);
+template <typename DType>
+inline DType nrm2(int n, DType *a, int lda);
 
 template <>
-float nrm2<cpu, float>(int n, float *a, int lda) {
+inline float nrm2<float>(int n, float *a, int lda) {
   return cblas_snrm2(n, a, lda);
 }
 
 template <>
-double nrm2<cpu, double>(int n, double *a, int lda) {
+inline double nrm2<double>(int n, double *a, int lda) {
   return cblas_dnrm2(n, a, lda);
 }
 
-template <int order, typename DType>
+template <typename DType>
 inline int CPDecompUpdate
-  (Tensor<cpu, 1, DType> &eigvals,
-  std::vector<Tensor<cpu, 2, DType> > &factors_T,
-  const Tensor<cpu, order, DType> &unfolding,
+  (Tensor<cpu, 1, DType> eigvals,
+  std::vector<Tensor<cpu, 2, DType> > factors_T,
+  const Tensor<cpu, 2, DType> &unfolding,
   int mode,
-  std::vector<Tensor<cpu, 2, DType> > &kr_prod_T,
-  Tensor<cpu, 2, DType> &hd_prod,
-  Stream<cpu> *stream = NULL) {
+  std::vector<TensorContainer<cpu, 2, DType> > kr_prod_T,
+  TensorContainer<cpu, 2, DType> hd_prod,
+  Stream<cpu> *stream) {
+  int order = (int) factors_T.size();
   int k = eigvals.size(0);
 
-  CHECK_EQ(factors_T.size(), order);
-  for (size_t i = 0; i < order; ++i)
-    CHECK_EQ(factors_T[i].size(0), k);
+  for (auto &m : factors_T)
+    CHECK_EQ((int) m.size(0), k);
 
-  CHECK(mode >= 0 && mode < order);
-  
   // Return value
   int info;
 
@@ -260,9 +281,9 @@ inline int CPDecompUpdate
   for (int id_mode = order - 1; id_mode >= 0; --id_mode) {
     if (id_mode == mode)
       continue;
-    
+
     d = factors_T[id_mode].size(1);
-    for (index_t i = 0; i < k; ++i) {
+    for (int i = 0; i < k; ++i) {
       expr::BLASEngine<cpu, DType>::SetStream
         (kr_prod_T[id_kr_prod][i].stream_);
       expr::BLASEngine<cpu, DType>::ger
@@ -276,32 +297,35 @@ inline int CPDecompUpdate
     kr_length *= d;
     ++id_kr_prod;
 
-    hd_prod = hd_prod * 
+    hd_prod = hd_prod *
         implicit_dot(factors_T[id_mode], factors_T[id_mode].T());
   }
 
   TensorContainer<cpu, 2, DType> rhs_T(Shape2(k, unfolding.size(0)));
   rhs_T = implicit_dot(kr_prod_T[order - 1], unfolding.T());
 
-  // In order to compute rhs pinv(hd_prod) we try to solve for X 
+  // In order to compute rhs pinv(hd_prod) we try to solve for X
   // such that
   //
   //     hd_prod X^T = rhs^T
   //
   // and update factors_T[mode] to be X^T
 
-  info = posv<cpu, DType>(k, unfolding.size(0), 
-      hd_prod.dptr_, hd_prod.stride_, 
+  info = posv(k, unfolding.size(0),
+      hd_prod.dptr_, hd_prod.stride_,
       rhs_T.dptr_, rhs_T.stride_);
   if (info != 0) {
-    return info; 
+    return info;
   }
   Copy(factors_T[mode], rhs_T);
 
-  for (index_t j = 0; j < k; ++j) {
+  std::cerr << "After update\n";
+  print2DTensor_(factors_T[mode]);
+
+  for (int j = 0; j < k; ++j) {
     // Compute the L2-norm of Column j of factors[mode]
     eigvals[j] = nrm2(factors_T[mode].size(1), factors_T[mode][j].dptr_, 1);
-    
+
     // Normalise Column j of factors[mode]
     factors_T[mode][j] = factors_T[mode][j] / eigvals[j];
   }
@@ -324,12 +348,12 @@ inline bool CPDecompConverged
     return false;
 
   int d;
-  for (size_t p = 0; p < factors_T.size(); ++p) {
+  for (int p = 0; p < (int) factors_T.size(); ++p) {
     d = factors_T[p].size(1);
     TensorContainer<cpu, 2, DType> factors_diff(factors_T[p].shape_);
     factors_diff = factors_T[p] - oldFactors_T[p];
 
-    for (size_t i = 0; i < k; ++i) {
+    for (int i = 0; i < k; ++i) {
       if (nrm2(d, factors_diff[i].dptr_, 1)
           > eps * nrm2(d, oldFactors_T[p][i].dptr_, 1))
         return false;
@@ -339,7 +363,42 @@ inline bool CPDecompConverged
   return true;
 }
 
-}
+template <int order, typename DType>
+inline DType TensorAt(const Tensor<cpu, order, DType> &t,
+  const std::vector<index_t> &coord) {
+  std::vector<index_t> sub_coord(coord.begin() + 1, coord.end());
+  return TensorAt(t[coord[0]], sub_coord);
 }
 
+template <>
+inline float TensorAt<1, float>(const Tensor<cpu, 1, float> &t,
+  const std::vector<index_t> &coord) {
+  return t[coord[0]];
+}
 
+template <>
+inline double TensorAt<1, double>(const Tensor<cpu, 1, double> &t,
+  const std::vector<index_t> &coord) {
+  return t[coord[0]];
+}
+
+template <typename DType>
+inline void print1DTensor_(const Tensor<cpu, 1, DType> &t) {
+  for (int i = 0; i < (int) t.size(0); ++i)
+    std::cerr << t[i] << " ";
+  std::cerr << "\n";
+}
+
+template <typename DType>
+inline void print2DTensor_(const Tensor<cpu, 2, DType> &t) {
+  for (int i = 0; i < (int) t.size(0); ++i) {
+    for (int j = 0; j < (int) t.size(1); ++j)
+      std::cerr << t[i][j] << " ";
+    std::cerr << "\n";
+  }
+}
+
+}  // namespace op
+}  // namespace mxnet
+
+#endif  // MXNET_OPERATOR_TENSOR_CP_DECOMP_H_
---
+++ tests/cpp/cp_decomp_test.cc
@@ -0,0 +1,180 @@
+#include <dmlc/logging.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <functional>
+#include "../src/operator/tensor/cp_decomp.h"
+
+namespace mxnet {
+namespace op {
+
+using namespace std;
+using namespace mshadow;
+using DType = double;
+
+void outer2D
+  (Tensor<cpu, 2, DType> ts,
+  const Tensor<cpu, 1, DType> &eigvals,
+  const std::vector<Tensor<cpu, 2, DType> > &factors_T) {
+  int k = (int) factors_T[0].size(0);
+
+  assert((int) eigvals.size(0) == k);
+  assert(factors_T.size() == 2);
+  for (int id_mode = 0; id_mode < (int) factors_T.size(); ++id_mode) {
+    assert((int) factors_T[id_mode].size(0) == k);
+    assert(factors_T[id_mode].size(1) == ts.size(id_mode));
+  }
+
+  ts = 0;
+  for (int i = 0; i < (int) factors_T[0].size(1); ++i)
+    for (int j = 0; j < (int) factors_T[1].size(1); ++j)
+      for (int p = 0; p < k; ++p)
+        ts[i][j] += eigvals[p] * factors_T[0][p][i] * factors_T[1][p][j];
+}
+
+void outer3D
+  (Tensor<cpu, 3, DType> ts,
+  const Tensor<cpu, 1, DType> &eigvals,
+  const std::vector<Tensor<cpu, 2, DType> > &factors_T) {
+  int k = (int) factors_T[0].size(0);
+
+  assert((int) eigvals.size(0) == k);
+  assert(factors_T.size() == 3);
+  for (int id_mode = 0; id_mode < (int) factors_T.size(); ++id_mode) {
+    assert((int) factors_T[id_mode].size(0) == k);
+    assert(factors_T[id_mode].size(1) == ts.size(id_mode));
+  }
+
+  ts = 0;
+  for (int i = 0; i < (int) factors_T[0].size(1); ++i)
+    for (int j = 0; j < (int) factors_T[1].size(1); ++j)
+      for (int l = 0; l < (int) factors_T[2].size(1); ++l)
+        for (int p = 0; p < k; ++p)
+          ts[i][j][l] += eigvals[p] * factors_T[0][p][i] 
+            * factors_T[1][p][j] * factors_T[2][p][l];
+}
+
+
+TEST(CPDecomp, 2DTensor) {
+  Tensor<cpu, 2, DType> ts(Shape2(5, 4));
+  AllocSpace(&ts);
+
+  int k0 = 3;
+  int k = 2;
+
+  // Generate 2D tensor
+  DType eigvals0_[k0] {10, 6, 1};
+  DType *mat_0_T_ = new DType[k0 * ts.size(0)] {-0.43467446, -0.5572915 , -0.15002647, -0.52690359, -0.44760359,
+            0.27817005, -0.35825313, -0.55669702,  0.60305847, -0.34739751,
+                   -0.08818202,  0.63349627, -0.69893409, -0.2969217 , -0.11931069};
+  DType *mat_1_T_ = new DType[k0 * ts.size(1)] {-0.38177064, -0.22495485, -0.67352004, -0.59162256,  0.5703576 ,
+           -0.64595109,  0.26986906, -0.42966275,  0.3282279 ,  0.72630226,
+                   0.09523822, -0.59639011};
+  Tensor<cpu, 1, DType> eigvals0(eigvals0_, Shape1(k0));
+  std::vector<Tensor<cpu, 2, DType> > factors0_T;
+  factors0_T.emplace_back(mat_0_T_, Shape2(k0, ts.size(0)), ts.size(0), nullptr);
+  factors0_T.emplace_back(mat_1_T_, Shape2(k0, ts.size(1)), ts.size(1), nullptr);
+
+  outer2D(ts, eigvals0, factors0_T);
+
+  // Allocate space for results
+  Tensor<cpu, 1, DType> eigvals(Shape1(k));
+  std::vector<Tensor<cpu, 2, DType> > factors_T;
+  factors_T.emplace_back(Shape2(k, ts.size(0)));
+  factors_T.emplace_back(Shape2(k, ts.size(1)));
+  AllocSpace(&eigvals);
+  for (auto &m : factors_T) 
+    AllocSpace(&m);
+
+  int info;
+  info = CPDecompForward(eigvals, factors_T, ts, k);
+
+  std::cerr << "Eigvals expected:\n";
+  print1DTensor_(eigvals0);
+  std::cerr << "Eigvals obtained:\n";
+  print1DTensor_(eigvals);
+
+  for (int id_mode = 0; id_mode < 2; ++id_mode) {
+    std::cerr << "Factor matrix transpose " << id_mode << " expected:\n";
+    print2DTensor_(factors0_T[id_mode]);
+    std::cerr << "Factor matrix transpose " << id_mode << " obtained:\n";
+    print2DTensor_(factors_T[id_mode]);
+  }
+
+  FreeSpace(&eigvals);
+  for (auto m : factors_T) 
+    FreeSpace(&m);
+  FreeSpace(&ts);
+
+  EXPECT_EQ(info, 0);
+}
+
+TEST(CPDecomp, 3DTensor) {
+  Tensor<cpu, 3, DType> ts(Shape3(5, 4, 3));
+  AllocSpace(&ts);
+
+  int k0 = 3;
+  int k = 2;
+
+  // Generate 2D tensor
+  DType eigvals0_[k0] {10, 6, 1};
+  DType *mat_0_T_ = new DType[k0 * ts.size(0)] {-0.43467446, -0.5572915 , -0.15002647, -0.52690359, -0.44760359,
+            0.27817005, -0.35825313, -0.55669702,  0.60305847, -0.34739751,
+                   -0.08818202,  0.63349627, -0.69893409, -0.2969217 , -0.11931069};
+  DType *mat_1_T_ = new DType[k0 * ts.size(1)] {-0.38177064, -0.22495485, -0.67352004, -0.59162256,  0.5703576 ,
+           -0.64595109,  0.26986906, -0.42966275,  0.3282279 ,  0.72630226,
+                   0.09523822, -0.59639011};
+  DType *mat_2_T_ = new DType[k0 * ts.size(2)] {-0.66722764, -0.52088417, -0.53243494,  0.63742185, -0.02948216,
+           -0.76995077,  0.38535783, -0.8531181 ,  0.35169427};
+  Tensor<cpu, 1, DType> eigvals0(eigvals0_, Shape1(k0));
+  std::vector<Tensor<cpu, 2, DType> > factors0_T;
+  factors0_T.emplace_back(mat_0_T_, Shape2(k0, ts.size(0)), ts.size(0), nullptr);
+  factors0_T.emplace_back(mat_1_T_, Shape2(k0, ts.size(1)), ts.size(1), nullptr);
+  factors0_T.emplace_back(mat_2_T_, Shape2(k0, ts.size(2)), ts.size(2), nullptr);
+
+  outer3D(ts, eigvals0, factors0_T);
+
+  // Allocate space for results
+  Tensor<cpu, 1, DType> eigvals(Shape1(k));
+  std::vector<Tensor<cpu, 2, DType> > factors_T;
+  factors_T.emplace_back(Shape2(k, ts.size(0)));
+  factors_T.emplace_back(Shape2(k, ts.size(1)));
+  factors_T.emplace_back(Shape2(k, ts.size(2)));
+  AllocSpace(&eigvals);
+  for (auto &m : factors_T) 
+    AllocSpace(&m);
+
+  int info;
+  info = CPDecompForward(eigvals, factors_T, ts, k);
+
+  std::cerr << "Eigvals expected:\n";
+  print1DTensor_(eigvals0);
+  std::cerr << "Eigvals obtained:\n";
+  print1DTensor_(eigvals);
+
+  for (int id_mode = 0; id_mode < 3; ++id_mode) {
+    std::cerr << "Factor matrix transpose " << id_mode << " expected:\n";
+    print2DTensor_(factors0_T[id_mode]);
+    std::cerr << "Factor matrix transpose " << id_mode << " obtained:\n";
+    print2DTensor_(factors_T[id_mode]);
+  }
+
+  FreeSpace(&eigvals);
+  for (auto m : factors_T) 
+    FreeSpace(&m);
+  FreeSpace(&ts);
+
+  EXPECT_EQ(info, 0);
+}
+}  // op
+}  // mxnet
+
+int main(int argc, char ** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
+  return RUN_ALL_TESTS();
+}
+
