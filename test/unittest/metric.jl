96191ea24346816b36c029b1bcd1bcf9dfd25781
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-10 11:00:11 +0800

    metric: add test cases for NMSE

--- test/unittest/metric.jl
+++ test/unittest/metric.jl
@@ -57,10 +57,46 @@ function test_ace()
 end
 
 
+function test_nmse()
+    info("EvalMetric::NMSE")
+
+    @testset "EvalMetric::NMSE::update!" begin
+        metric = mx.NMSE()
+        labels = Array{mx.NDArray}(
+            [mx.NDArray([100.0, 0.0]), mx.NDArray([10.0, 0.0])])
+        preds = Array{mx.NDArray}(
+            [mx.NDArray([20.0, 0.0]), mx.NDArray([2.0, 0.0])])
+
+        mx.update!(metric, labels, preds)
+        @test metric.nmse_sum ≈ 0.64 * 2
+    end
+
+    @testset "EvalMetric::NMSE::reset!" begin
+        metric = mx.NMSE()
+        metric.nmse_sum = sum(rand(10))
+        metric.n_sample = 42
+
+        mx.reset!(metric)
+
+        @test metric.nmse_sum == 0.0
+        @test metric.n_sample == 0
+    end
+
+    @testset "EvalMetric::NMSE::get" begin
+        metric = mx.NMSE()
+        metric.nmse_sum = 100.0
+        metric.n_sample = 20
+
+        @test mx.get(metric) == [(:NMSE, 5.0)]
+    end
+end
+
+
 ################################################################################
 # Run tests
 ################################################################################
 test_ace()
+test_nmse()
 
 
 end
