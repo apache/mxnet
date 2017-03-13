f12a642b2e4796a70304af4a33ac4dfce1d5d225
Mu Li <muli@cs.cmu.edu>
2017-03-13 17:15:16 +0000

    add timer to nose

--- tests/ci_build/install/ubuntu_install_python.sh
+++ tests/ci_build/install/ubuntu_install_python.sh
@@ -5,5 +5,5 @@ apt-get update && apt-get install -y \
     python-pip python-dev \
     python3-pip python3-dev
 
-pip install nose pylint numpy
-pip3 install nose pylint numpy
+pip install nose pylint numpy nose-timer
+pip3 install nose pylint numpy nose-timer
