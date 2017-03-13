e900c93ba83f91f5a4c68772047e545e37560f76
Mu Li <muli@cs.cmu.edu>
2017-03-13 05:28:06 +0000

    update

--- tests/ci_build/install/ubuntu_install_python.sh
+++ tests/ci_build/install/ubuntu_install_python.sh
@@ -2,8 +2,8 @@
 # install libraries for mxnet's python package on ubuntu
 
 apt-get update && apt-get install -y \
-    python-pip python-dev python-numpy \
-    python3-pip python3-dev python3-numpy
+    python-pip python-dev \
+    python3-pip python3-dev
 
-pip install nose pylint
-pip3 install nose pylint
+pip install nose pylint numpy
+pip3 install nose pylint numpy
