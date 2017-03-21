289ff56215cfea16bf73599d8ef4865d9552bed5
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-03-21 11:37:53 -0700

    add contrib package (#5499)
    * add contrib package
    
    * fix

--- src/operator/contrib/proposal.cc
+++ src/operator/contrib/proposal.cc
@@ -123,17 +123,17 @@ inline void IoUTransformInv(const mshadow::Tensor<cpu, 2>& boxes,
 
 // filter box by set confidence to zero
 // * height or width < rpn_min_size
-inline void FilterBox(mshadow::Tensor<cpu, 2>& dets,
+inline void FilterBox(mshadow::Tensor<cpu, 2> *dets,
                       const float min_size) {
-  for (index_t i = 0; i < dets.size(0); i++) {
-    float iw = dets[i][2] - dets[i][0] + 1.0f;
-    float ih = dets[i][3] - dets[i][1] + 1.0f;
+  for (index_t i = 0; i < dets->size(0); i++) {
+    float iw = (*dets)[i][2] - (*dets)[i][0] + 1.0f;
+    float ih = (*dets)[i][3] - (*dets)[i][1] + 1.0f;
     if (iw < min_size || ih < min_size) {
-      dets[i][0] -= min_size / 2;
-      dets[i][1] -= min_size / 2;
-      dets[i][2] += min_size / 2;
-      dets[i][3] += min_size / 2;
-      dets[i][4] = -1.0f;
+      (*dets)[i][0] -= min_size / 2;
+      (*dets)[i][1] -= min_size / 2;
+      (*dets)[i][2] += min_size / 2;
+      (*dets)[i][3] += min_size / 2;
+      (*dets)[i][4] = -1.0f;
     }
   }
 }
@@ -161,19 +161,19 @@ struct ReverseArgsortCompl {
 
 // copy score and init order
 inline void CopyScore(const mshadow::Tensor<cpu, 2>& dets,
-                      mshadow::Tensor<cpu, 1>& score,
-                      mshadow::Tensor<cpu, 1>& order) {
+                      mshadow::Tensor<cpu, 1> *score,
+                      mshadow::Tensor<cpu, 1> *order) {
   for (index_t i = 0; i < dets.size(0); i++) {
-    score[i] = dets[i][4];
-    order[i] = i;
+    (*score)[i] = dets[i][4];
+    (*order)[i] = i;
   }
 }
 
 // sort order array according to score
 inline void ReverseArgsort(const mshadow::Tensor<cpu, 1>& score,
-                           mshadow::Tensor<cpu, 1>& order) {
+                           mshadow::Tensor<cpu, 1> *order) {
   ReverseArgsortCompl cmpl(score.dptr_);
-  std::sort(order.dptr_, order.dptr_ + score.size(0), cmpl);
+  std::sort(order->dptr_, order->dptr_ + score.size(0), cmpl);
 }
 
 // reorder proposals according to order and keep the pre_nms_top_n proposals
@@ -181,12 +181,12 @@ inline void ReverseArgsort(const mshadow::Tensor<cpu, 1>& score,
 inline void ReorderProposals(const mshadow::Tensor<cpu, 2>& prev_dets,
                              const mshadow::Tensor<cpu, 1>& order,
                              const index_t pre_nms_top_n,
-                             mshadow::Tensor<cpu, 2>& dets) {
-  CHECK_EQ(dets.size(0), pre_nms_top_n);
-  for (index_t i = 0; i < dets.size(0); i++) {
+                             mshadow::Tensor<cpu, 2> *dets) {
+  CHECK_EQ(dets->size(0), pre_nms_top_n);
+  for (index_t i = 0; i < dets->size(0); i++) {
     const index_t index = order[i];
-    for (index_t j = 0; j < dets.size(1); j++) {
-      dets[i][j] = prev_dets[index][j];
+    for (index_t j = 0; j < dets->size(1); j++) {
+      (*dets)[i][j] = prev_dets[index][j];
     }
   }
 }
@@ -195,20 +195,20 @@ inline void ReorderProposals(const mshadow::Tensor<cpu, 2>& prev_dets,
 inline void NonMaximumSuppression(const mshadow::Tensor<cpu, 2>& dets,
                                   const float thresh,
                                   const index_t post_nms_top_n,
-                                  mshadow::Tensor<cpu, 1>& area,
-                                  mshadow::Tensor<cpu, 1>& suppressed,
-                                  mshadow::Tensor<cpu, 1>& keep,
+                                  mshadow::Tensor<cpu, 1> *area,
+                                  mshadow::Tensor<cpu, 1> *suppressed,
+                                  mshadow::Tensor<cpu, 1> *keep,
                                   index_t *out_size) {
   CHECK_EQ(dets.shape_[1], 5) << "dets: [x1, y1, x2, y2, score]";
   CHECK_GT(dets.shape_[0], 0);
   CHECK_EQ(dets.CheckContiguous(), true);
-  CHECK_EQ(area.CheckContiguous(), true);
-  CHECK_EQ(suppressed.CheckContiguous(), true);
-  CHECK_EQ(keep.CheckContiguous(), true);
+  CHECK_EQ(area->CheckContiguous(), true);
+  CHECK_EQ(suppressed->CheckContiguous(), true);
+  CHECK_EQ(keep->CheckContiguous(), true);
   // calculate area
   for (index_t i = 0; i < dets.size(0); ++i) {
-    area[i] = (dets[i][2] - dets[i][0] + 1) *
-              (dets[i][3] - dets[i][1] + 1);
+    (*area)[i] = (dets[i][2] - dets[i][0] + 1) *
+                 (dets[i][3] - dets[i][1] + 1);
   }
 
   // calculate nms
@@ -218,15 +218,15 @@ inline void NonMaximumSuppression(const mshadow::Tensor<cpu, 2>& dets,
     float iy1 = dets[i][1];
     float ix2 = dets[i][2];
     float iy2 = dets[i][3];
-    float iarea = area[i];
+    float iarea = (*area)[i];
 
-    if (suppressed[i] > 0.0f ) {
+    if ((*suppressed)[i] > 0.0f) {
       continue;
     }
 
-    keep[(*out_size)++] = i;
+    (*keep)[(*out_size)++] = i;
     for (index_t j = i + 1; j < dets.size(0); j ++) {
-      if (suppressed[j] > 0.0f) {
+      if ((*suppressed)[j] > 0.0f) {
         continue;
       }
       float xx1 = std::max(ix1, dets[j][0]);
@@ -236,9 +236,9 @@ inline void NonMaximumSuppression(const mshadow::Tensor<cpu, 2>& dets,
       float w = std::max(0.0f, xx2 - xx1 + 1.0f);
       float h = std::max(0.0f, yy2 - yy1 + 1.0f);
       float inter = w * h;
-      float ovr = inter / (iarea + area[j] - inter);
+      float ovr = inter / (iarea + (*area)[j] - inter);
       if (ovr > thresh) {
-        suppressed[j] = 1.0f;
+        (*suppressed)[j] = 1.0f;
       }
     }
   }
@@ -270,7 +270,8 @@ class ProposalOp : public Operator{
     CHECK_EQ(out_data.size(), 2);
     CHECK_GT(req.size(), 1);
     CHECK_EQ(req[proposal::kOut], kWriteTo);
-    CHECK_EQ(in_data[proposal::kClsProb].shape_[0], 1) << "Sorry, multiple images each device is not implemented.";
+    CHECK_EQ(in_data[proposal::kClsProb].shape_[0], 1)
+      << "Sorry, multiple images each device is not implemented.";
 
     Stream<xpu> *s = ctx.get_stream<xpu>();
 
@@ -278,7 +279,8 @@ class ProposalOp : public Operator{
                                    in_data[proposal::kClsProb].shape_[1] / 2,
                                    in_data[proposal::kClsProb].shape_[2],
                                    in_data[proposal::kClsProb].shape_[3]);
-    real_t* foreground_score_ptr = reinterpret_cast<real_t *>(in_data[proposal::kClsProb].dptr_) + scores_shape.Size();
+    real_t* foreground_score_ptr = reinterpret_cast<real_t *>(in_data[proposal::kClsProb].dptr_)
+                                    + scores_shape.Size();
     Tensor<cpu, 4> scores = Tensor<cpu, 4>(foreground_score_ptr, scores_shape);
     Tensor<cpu, 4> bbox_deltas = in_data[proposal::kBBoxPred].get<cpu, 4, real_t>(s);
     Tensor<cpu, 2> im_info = in_data[proposal::kImInfo].get<cpu, 2, real_t>(s);
@@ -302,7 +304,8 @@ class ProposalOp : public Operator{
     start += count * 5;
     Tensor<cpu, 2> workspace_pre_nms(workspace.dptr_ + start, Shape2(2, count));
     start += 2 * count;
-    Tensor<cpu, 2> workspace_ordered_proposals(workspace.dptr_ + start, Shape2(rpn_pre_nms_top_n, 5));
+    Tensor<cpu, 2> workspace_ordered_proposals(workspace.dptr_ + start,
+                                               Shape2(rpn_pre_nms_top_n, 5));
     start += rpn_pre_nms_top_n * 5;
     Tensor<cpu, 2> workspace_nms(workspace.dptr_ + start, Shape2(3, rpn_pre_nms_top_n));
     start += 3 * rpn_pre_nms_top_n;
@@ -319,13 +322,13 @@ class ProposalOp : public Operator{
     utils::GenerateAnchors(base_anchor,
                            param_.ratios.info,
                            param_.scales.info,
-                           anchors);
+                           &anchors);
     std::memcpy(workspace_proposals.dptr_, &anchors[0], sizeof(float) * anchors.size());
 
-    //Enumerate all shifted anchors
-    for (index_t i = 0; i < num_anchors; ++i){
-      for (index_t j = 0; j < height; ++j){
-        for (index_t k = 0; k < width; ++k){
+    // Enumerate all shifted anchors
+    for (index_t i = 0; i < num_anchors; ++i) {
+      for (index_t j = 0; j < height; ++j) {
+        for (index_t k = 0; k < width; ++k) {
           index_t index = j * (width * num_anchors) + k * (num_anchors) + i;
           workspace_proposals[index][0] = workspace_proposals[i][0] + k * param_.feature_stride;
           workspace_proposals[index][1] = workspace_proposals[i][1] + j * param_.feature_stride;
@@ -349,20 +352,20 @@ class ProposalOp : public Operator{
       utils::BBoxTransformInv(workspace_proposals, bbox_deltas, im_info[0][0], im_info[0][1],
                               real_height, real_width, &(workspace_proposals));
     }
-    utils::FilterBox(workspace_proposals, param_.rpn_min_size * im_info[0][2]);
+    utils::FilterBox(&workspace_proposals, param_.rpn_min_size * im_info[0][2]);
 
     Tensor<cpu, 1> score = workspace_pre_nms[0];
     Tensor<cpu, 1> order = workspace_pre_nms[1];
 
     utils::CopyScore(workspace_proposals,
-                     score,
-                     order);
+                     &score,
+                     &order);
     utils::ReverseArgsort(score,
-                          order);
+                          &order);
     utils::ReorderProposals(workspace_proposals,
                             order,
                             rpn_pre_nms_top_n,
-                            workspace_ordered_proposals);
+                            &workspace_ordered_proposals);
 
     index_t out_size = 0;
     Tensor<cpu, 1> area = workspace_nms[0];
@@ -373,14 +376,14 @@ class ProposalOp : public Operator{
     utils::NonMaximumSuppression(workspace_ordered_proposals,
                                  param_.threshold,
                                  rpn_post_nms_top_n,
-                                 area,
-                                 suppressed,
-                                 keep,
+                                 &area,
+                                 &suppressed,
+                                 &keep,
                                  &out_size);
 
     // fill in output rois
     for (index_t i = 0; i < out.size(0); ++i) {
-      //batch index 0
+      // batch index 0
       out[i][0] = 0;
       if (i < out_size) {
         index_t index = keep[i];
@@ -400,8 +403,7 @@ class ProposalOp : public Operator{
       if (i < out_size) {
         index_t index = keep[i];
         out_score[i][0] = workspace_ordered_proposals[index][4];
-      }
-      else {
+      } else {
         index_t index = keep[i % out_size];
         out_score[i][0] = workspace_ordered_proposals[index][4];
       }
@@ -445,7 +447,7 @@ Operator* ProposalProp::CreateOperator(Context ctx) const {
 
 DMLC_REGISTER_PARAMETER(ProposalParam);
 
-MXNET_REGISTER_OP_PROPERTY(Proposal, ProposalProp)
+MXNET_REGISTER_OP_PROPERTY(_contrib_Proposal, ProposalProp)
 .describe("Generate region proposals via RPN")
 .add_argument("cls_score", "Symbol", "Score of how likely proposal is object.")
 .add_argument("bbox_pred", "Symbol", "BBox Predicted deltas from anchors for proposals")
