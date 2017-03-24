3085a14d1b833bd220b39cea514bd3b3b686fd32
Ubuntu <ubuntu@ip-172-31-25-105.ec2.internal>
2017-03-24 00:45:37 +0000

    Fix docstring test for symbol.py and ndarray.py examples

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -106,10 +106,14 @@ if [ ${TASK} == "python_test" ]; then
         # export MXNET_ENFORCE_CYTHON=1
         # python3 -m nose tests/python/unittest || exit -1
         python3 -m nose tests/python/train || exit -1
+        python -m nose tests/python/doctest || exit -1
+        python3 -m nose tests/python/doctest || exit -1
     else
         nosetests tests/python/unittest || exit -1
         nosetests3 tests/python/unittest || exit -1
         nosetests3 tests/python/train || exit -1
+        nosetests tests/python/doctest || exit -1
+        nosetests3 tests/python/doctest || exit -1
     fi
     exit 0
 fi
