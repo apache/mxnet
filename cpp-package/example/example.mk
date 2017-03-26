1e2e157df875a1ed977c620a4e6d4f209c449f7f
cjolivier01 <cjolivier01@gmail.com>
2017-03-25 18:19:12 -0700

    Better example.mk

--- cpp-package/example/example.mk
+++ cpp-package/example/example.mk
@@ -1,67 +1,22 @@
 CPPEXMAKE_FILE := $(abspath $(lastword $(MAKEFILE_LIST)))
 CPPEX_THISDIR := $(dir $(CPPEXMAKE_FILE))
 
+CPPEX_SRC = $(wildcard cpp-package/example/*.cpp)
+CPPEX_EXE = $(patsubst cpp-package/example/%.cpp, build/cpp-package/example/%, $(CPPEX_SRC))
+
 CFLAGS += -I$(CPPEX_THISDIR)/../include
 EXTRA_LDFLAGS := -L$(ROOTDIR)/lib -lmxnet
 
-.PHONY += cpp-package-example-all cpp-package-example-clean temp
+.PHONY += cpp-package-example-all cpp-package-example-clean
 
-cpp-package-example-all: cpp-package-all mlp lenet lenet_with_mxdataiter alexnet googlenet resnet
+cpp-package-example-all: $(CPPEX_EXE)
 
-temp:
-	echo "CPPEXMAKE_FILE: " $(CPPEXMAKE_FILE)
-	echo "CPPEX_THISDIR: " $(CPPEX_THISDIR)
-
-lenet_with_mxdataiter: $(CPPEX_THISDIR)/lenet_with_mxdataiter.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-lenet: $(CPPEX_THISDIR)/lenet.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-mlp: $(CPPEX_THISDIR)/mlp.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-alexnet: $(CPPEX_THISDIR)/alexnet.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-googlenet: $(CPPEX_THISDIR)/googlenet.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-inception_bn: $(CPPEX_THISDIR)/inception_bn.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-resnet: $(CPPEX_THISDIR)/resnet.cpp
-	$(CXX) -c -std=c++11 $(CFLAGS) $^
-	$(CXX) $(basename $@).o -o $@ $(LDFLAGS) $(EXTRA_LDFLAGS)
-	-rm -f $(basename $@).o
-
-# For simplicity, no link here
-#cpp-package-example-travis:
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./mlp.cpp && rm -f mlp.o
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./lenet.cpp && rm -f lenet.o
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./lenet_with_mxdataiter.cpp && rm -f lenet_with_mxdataiter.o
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./alexnet.cpp && rm -f alexnet.o
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./googlenet.cpp && rm -f googlenet.o
-#	$(CXX) -c -std=c++11 $(CFLAGS) ./resnet.cpp && rm -f resnet.o
-
+build/cpp-package/example/% : cpp-package/example/%.cpp
+	@mkdir -p $(@D)
+	$(CXX) -std=c++0x $(CFLAGS) -I$(GTEST_INC) -MM -MT cpp-package/example/$* $< >build/cpp-package/example//$*.d
+	$(CXX) -std=c++0x $(CFLAGS) -I$(GTEST_INC) -o $@ $(filter %.cpp %.a, $^) $(LDFLAGS) $(EXTRA_LDFLAGS)
 
 cpp-package-example-clean:
-	-rm -f mlp
-	-rm -f lenet
-	-rm -f lenet_with_mxdataiter
-	-rm -f alexnet
-	-rm -f googlenet
-	-rm -f resnet
-	-rm -f inception_bn
+	-rm -f build/cpp-package/example/*
+
+-include build/cpp-package/example/*.d
