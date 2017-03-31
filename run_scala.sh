440e4aebad2694cf9a524d988c89cb8f850aecee
qiyuangong <qiyuan.gong@intel.com>
2017-03-31 18:38:59 +0800

    Remove helping scripts

--- run_scala.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-java -Xmx4G -cp scala-package/assembly/osx-x86_64-cpu/target/*:scala-package/examples/target/*:scala-package/examples/target/classes/lib/* \
-  ml.dmlc.mxnetexamples.imclassification.TrainMnist \
-  --data-dir=/Users/QYGong/Downloads/mnist/ \
-  --num-epochs=10 \
-  --network=mlp \
-  --cpus=0,1,2,3
