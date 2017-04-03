34f99b4d922e0a42706aa40e5ebf3240afcdb4aa
Naveen Swamy <wamy@amazon.com>
2017-04-03 11:04:49 -0700

    Temporarily Revert to fix formatting issues "modify mx.nd.reshape documentation (#5609)"
    This reverts commit 138344683e65c87af20250e3f4cdcc5a72ac3cc5.

--- include/mxnet/ndarray.h
+++ include/mxnet/ndarray.h
@@ -39,7 +39,7 @@ class NDArray {
 #endif
   }
   /*!
-   * \brief constructs a new dynamic NDArray
+   * \brief constructing a new dynamic NDArray
    * \param shape the shape of array
    * \param ctx context of NDArray
    * \param delay_alloc whether delay the allocation
