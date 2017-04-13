1cc7637ab6f4bf897931c0dd36bcbf8e2f8199b5
moveforever <zhengzhongyi0321@gmail.com>
2017-04-13 22:07:54 +0800

    implement ftrl

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -5,7 +5,7 @@ import ctypes
 from .base import _LIB, check_call
 from .base import c_array, mx_uint, mx_float, c_str
 from .base import OptimizerHandle, OptimizerCreator
-from .ndarray import NDArray, zeros, clip, sqrt
+from .ndarray import NDArray, zeros, clip, sqrt, abs, sign
 from .random import normal
 
 
@@ -775,6 +775,45 @@ class AdaDelta(Optimizer):
 
         # update weight
         weight[:] -= current_delta + wd * weight
+
+@register
+class Ftrl(Optimizer):
+
+    def __init__(self, lamda1=0.01, lamda2=1, alpha=0.1, beta=1, epsilon=1e-5, **kwargs):
+        super(Ftrl, self).__init__(**kwargs)
+        self.lamda1 = lamda1
+        self.lamda2 = lamda2
+        self.alpha = alpha
+        self.beta = beta
+
+
+    def create_state(self, index, weight):
+        return (zeros(weight.shape, weight.context),  # sigma
+            zeros(weight.shape, weight.context),  # z
+            zeros(weight.shape, weight.context))  # n
+
+    def update(self, index, weight, grad, state):
+        assert(isinstance(weight, NDArray))
+        assert(isinstance(grad, NDArray))
+        wd = self._get_wd(index)
+        self._update_count(index)
+
+        # preprocess grad
+        grad *= self.rescale_grad
+        if self.clip_gradient is not None:
+            grad = clip(grad, -self.clip_gradient, self.clip_gradient)
+
+        # accumulated g and delta initlization
+        sigma, z, n = state
+
+
+        #update sigma, z, n
+        sigma[:] = (sqrt(n + grad * grad) - sqrt(n)) / self.alpha
+        z[:] += grad - sigma * weight
+        n[:] += grad * grad
+
+        # update weight
+        weight[:] = (sign(z) * self.lamda1 - z) / ((self.beta + sqrt(n))/self.alpha + self.lamda2) * (abs(z) > self.lamda1)
 
 
 @register
