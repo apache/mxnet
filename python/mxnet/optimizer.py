183b1d4bd5314b18b9b2a8e971143c391956400b
moveforever <zhengzhongyi0321@gmail.com>
2017-04-16 23:15:42 +0800

    fix code style

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -652,10 +652,10 @@ class AdaDelta(Optimizer):
 @register
 class Ftrl(Optimizer):
     """
-    Reference:Ad Click Prediction: a View from the Trenches.H. Brendan McMahan, Gary Holt, D. Sculley et al.
+    Reference:Ad Click Prediction: a View from the Trenches
     """
 
-    def __init__(self, lamda1=0.01, lamda2=1, alpha=0.1, beta=1, epsilon=1e-5, **kwargs):
+    def __init__(self, lamda1=0.01, lamda2=1, alpha=0.1, beta=1, **kwargs):
         super(Ftrl, self).__init__(**kwargs)
         self.lamda1 = lamda1
         self.lamda2 = lamda2
@@ -665,13 +665,12 @@ class Ftrl(Optimizer):
 
     def create_state(self, index, weight):
         return (zeros(weight.shape, weight.context),  # sigma
-            zeros(weight.shape, weight.context),  # z
-            zeros(weight.shape, weight.context))  # n
+                zeros(weight.shape, weight.context),  # z
+                zeros(weight.shape, weight.context))  # n
 
     def update(self, index, weight, grad, state):
         assert(isinstance(weight, NDArray))
         assert(isinstance(grad, NDArray))
-        wd = self._get_wd(index)
         self._update_count(index)
 
         # preprocess grad
@@ -682,14 +681,14 @@ class Ftrl(Optimizer):
         # accumulated g and delta initlization
         sigma, z, n = state
 
-
         #update sigma, z, n
         sigma[:] = (sqrt(n + grad * grad) - sqrt(n)) / self.alpha
         z[:] += grad - sigma * weight
         n[:] += grad * grad
 
         # update weight
-        weight[:] = (sign(z) * self.lamda1 - z) / ((self.beta + sqrt(n))/self.alpha + self.lamda2) * (abs(z) > self.lamda1)
+        weight[:] = (sign(z) * self.lamda1 - z) /
+            ((self.beta + sqrt(n))/self.alpha + self.lamda2) * (abs(z) > self.lamda1)
 
 @register
 class Test(Optimizer):
