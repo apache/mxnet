134134b8f37373acd3783fb9c0da2433eab2b3b8
Nguyen <lynguyen@amazon.com>
2017-04-20 12:03:59 -0700

    Remove debug statements & comments

--- tests/jenkins/run_test_pip_installations.sh
+++ tests/jenkins/run_test_pip_installations.sh
@@ -3,13 +3,16 @@
 set -e
 
 if (( $# < 1 )); then
-    echo "no!"
-    exit -1
+    echo ""
+    echo "Usage: $(basename $0) WORKSPACE_PATH"
+    echo ""
+    exit 1
 fi
+
 WORKSPACE=$( echo "$1" | tr '[:upper:]' '[:lower:]' )
 
 PYTHON_VERSIONS=('2.7' '3.4' '3.5' '3.6')
-DEVICES=('pip_cu80') #'pip_cpu' 'pip_cu75' 'pip_cu80')
+DEVICES=('pip_cpu' 'pip_cu75' 'pip_cu80')
 
 # build Docker images and test pip installation for each device
 for DEV in "${DEVICES[@]}"; do
@@ -35,7 +38,7 @@ for DEV in "${DEVICES[@]}"; do
     # test each python version of mxnet
     for VERSION in "${PYTHON_VERSIONS[@]}"; do
         PYTHON="python${VERSION}"
-        echo "Testing ${PYTHON} ${DOCKER_TAG}"
+        echo "Testing ${PYTHON}"
         DOCKER_CMD="virtualenv -p \"/usr/bin/${PYTHON}\" ${PYTHON}; source \"${PYTHON}/bin/activate\"; cd ${WORKSPACE};"
         if [[ "${DEV}" == *"cpu"* ]]; then
             DOCKER_CMD="${DOCKER_CMD} pip install mxnet; python tests/python/train/test_conv.py"
@@ -46,7 +49,6 @@ for DEV in "${DEVICES[@]}"; do
         fi
 	
         ${DOCKER_BINARY} run --rm -v ${WORKSPACE}:${WORKSPACE} ${DOCKER_TAG} bash -c "${DOCKER_CMD}"
-	#${DOCKER_BINARY} run --rm -v ${WORKSPACE}:${WORKSPACE} ${DOCKER_TAG} bash -c "virtualenv -p \"/usr/bin/${PYTHON}\" ${PYTHON}; source \"${PYTHON}/bin/activate\"; cd ${WORKSPACE}; echo ${PWD} ${WORKSPACE}; pip install mxnet; python tests/python/train/test_conv.py"
     done
 
 done
