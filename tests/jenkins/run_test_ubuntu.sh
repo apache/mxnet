3308a2cf787e663017555b88da6f2f61f31a2c3e
Olivier <coolivie@amazon.com>
2017-04-21 16:30:52 -0700

    use CUDNN for this test

--- tests/jenkins/run_test_ubuntu.sh
+++ tests/jenkins/run_test_ubuntu.sh
@@ -19,7 +19,7 @@ fi
 cp make/config.mk .
 echo "USE_CUDA=1" >> config.mk
 echo "USE_CUDA_PATH=/usr/local/cuda" >> config.mk
-echo "USE_CUDNN=0" >> config.mk
+echo "USE_CUDNN=1" >> config.mk
 echo "USE_PROFILER=1" >> config.mk
 echo "DEV=1" >> config.mk
 echo "EXTRA_OPERATORS=example/ssd/operator" >> config.mk
