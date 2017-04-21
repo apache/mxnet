bb7ad9daac5ba670c8e411930d54337d0e5bf7a6
Travis Johnston <jtjohnston@users.noreply.github.com>
2017-04-21 12:43:46 -0400

    Fixed argparse usage for --exts to accept one or more arguments (a list) instead of specifying the type=list (which stores a list of lists and does not work). (#5928)

--- tools/im2rec.py
+++ tools/im2rec.py
@@ -198,7 +198,7 @@ def parse_args():
                         help='If this is set im2rec will create image list(s) by traversing root folder\
         and output to <prefix>.lst.\
         Otherwise im2rec will read <prefix>.lst and create a database at <prefix>.rec')
-    cgroup.add_argument('--exts', type=list, default=['.jpeg', '.jpg'],
+    cgroup.add_argument('--exts', nargs='+', default=['.jpeg', '.jpg'],
                         help='list of acceptable image extensions.')
     cgroup.add_argument('--chunks', type=int, default=1, help='number of chunks.')
     cgroup.add_argument('--train-ratio', type=float, default=1.0,
