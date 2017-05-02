bee66c60ccf3f73273a90ad16ff87514949d71de
Nguyen <lynguyen@amazon.com>
2017-05-01 22:29:10 -0700

    Function documentation

--- tests/jenkins/run_test_installation_docs.sh
+++ tests/jenkins/run_test_installation_docs.sh
@@ -44,8 +44,22 @@ function remove_out_of_range() {
     echo "${return_arr[@]}"
 }
 
-# given a number and an array of numbers, retrieves the index whose value is equal the number or the next greater thing
-# assumes array is sorted
+# given a number and an array of numbers, retrieves the index whose value is equal to the number or the next greatest
+# thing. assumes array is sorted.
+#
+# $1 - array of numbers
+# $2 - number
+#
+# Example:
+#   a=(1 3 5 7 9 10)
+#   retrieve_closest_index a[@] 5
+#
+#   returns 2
+#
+#   retrieve_closest_index a[@] 8
+#
+#   returns 4
+#
 function retrieve_closest_index() {
     declare -a arr=("${!1}")
     number=${2}
@@ -56,11 +70,6 @@ function retrieve_closest_index() {
         exit 1
     fi
 
-    #echo ""
-    #echo "retrieve_closest_index:"
-    #echo ${number}
-    #printf "${arr[*]}"
-
     for (( i=0; i < ${#arr[@]}; i++ ))
     do
         cur_num=${arr[${i}]}
@@ -70,11 +79,17 @@ function retrieve_closest_index() {
             return
         fi
     done
-
-    #echo ""
 }
 
 # retrieves all bash commands between two given line numbers in a file
+#
+# $1 - Start line number
+# $2 - End line number
+#
+# Example:
+#   a=(4 1 3)
+#   sort a[@]
+#
 function retrieve_commands() {
     section_start_index=${1}
     section_end_index=${2}
@@ -96,7 +111,7 @@ function retrieve_commands() {
         do
             # 1) get the line from file given the line number
             # 2) remove everything up to the prompt character '$'
-	    # 3) remove everything after a comment character"#"
+	        # 3) remove everything after a comment character"#"
             # 4) trim leading and trailing spaces
             cmd=`sed -n ${j}p ${FILE} | sed 's/^.*$\(.*\).*$/\1/' | sed 's/\(.*\)#.*$/\1/' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'`
             if [[ ! -z $cmd ]];
@@ -108,10 +123,17 @@ function retrieve_commands() {
     echo ${commands}
 }
 
+# Sorts array of numbers.
+#
+# $1 - Start line number
+# $2 - End line number
+#
+# Example:
+#   a=(4 1 3)
+#   sort a[@]
+#
 function sort() {
     declare -a lineno_array=("${!1}")
-    return_arr=()
-    index=0
     size=${#lineno_array[@]}
     for (( i=1; i<=$(( $size-1)); i++ ))
     do
@@ -126,21 +148,28 @@ function sort() {
     printf "${lineno_array[*]}"
 }
 
-FILE=install.md
+FILE=../../docs/get_started/install.md
 
 # get all line numbers with "```" signifying start or end of source section and put them in an array
 SOURCE_REGEX="\`\`\`"
 SOURCE_LINES=($(grep -n "${SOURCE_REGEX}" "${FILE}" | cut -d : -f 1))
 
 # line numbers of the start of installation method instructions regardless of platform
-PYTHON_VIRTUALENV_LINENO_ALL=($(grep -n "<div class=\"virtualenv\">" "${FILE}" | cut -d : -f 1))
-PYTHON_PIP_LINENO_ALL=($(grep -n "<div class=\"pip\">" "${FILE}" | cut -d : -f 1))
-PYTHON_DOCKER_LINENO_ALL=($(grep -n "<div class=\"docker\">" "${FILE}" | cut -d : -f 1))
-PYTHON_BUILDFROMSOURCE_LINENO_ALL=($(grep -n "<div class=\"build-from-source\">" "${FILE}" | cut -d : -f 1))
+VIRTUALENV_LINENO_ALL=($(grep -n "<div class=\"virtualenv\">" "${FILE}" | cut -d : -f 1))
+PIP_LINENO_ALL=($(grep -n "<div class=\"pip\">" "${FILE}" | cut -d : -f 1))
+DOCKER_LINENO_ALL=($(grep -n "<div class=\"docker\">" "${FILE}" | cut -d : -f 1))
+BUILDFROMSOURCE_LINENO_ALL=($(grep -n "<div class=\"build-from-source\">" "${FILE}" | cut -d : -f 1))
 
-function get_instruction_set() {
-    # pass start and end line numbers
-    # set 4 arrays
+# Given two line numbers, collects instruction sets for installing via Virtualenv, Pip, Docker, and source within the
+# two lines assuming there is one of each.
+#
+# $1 - Start line number
+# $2 - End line number
+#
+# Example:
+#   set_instruction_set 0 64
+#
+function set_instruction_set() {
 
     if [[ -z ${1} || -z ${2} ]]
     then
@@ -148,38 +177,49 @@ function get_instruction_set() {
         exit 1
     fi
 
+    if [[ ${1} -gt ${2} ]]
+    then
+        echo "Error: start line number must be smaller then end line number"
+        exit 1
+    fi
+
     # range of all lines inside Linux-Python-CPU instructions
-    LINUX_PYTHON_CPU_START_LINENO=${1}
-    LINUX_PYTHON_CPU_END_LINENO=${2}
+    START_LINENO=${1}
+    END_LINENO=${2}
 
     # get line numbers of the start of each installation method instructions for Linux-Python-CPU
-    LINUX_PYTHON_CPU_VIRTUALENV_LINENO=($(remove_out_of_range PYTHON_VIRTUALENV_LINENO_ALL[@] ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}))
-    LINUX_PYTHON_CPU_PIP_LINENO=($(remove_out_of_range PYTHON_PIP_LINENO_ALL[@] ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}))
-    LINUX_PYTHON_CPU_DOCKER_LINENO=($(remove_out_of_range PYTHON_DOCKER_LINENO_ALL[@] ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}))
-    LINUX_PYTHON_CPU_BUILDFROMSOURCE_LINENO=($(remove_out_of_range PYTHON_BUILDFROMSOURCE_LINENO_ALL[@] ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}))
+    VIRTUALENV_LINENO=($(remove_out_of_range VIRTUALENV_LINENO_ALL[@] ${START_LINENO} ${END_LINENO}))
+    PIP_LINENO=($(remove_out_of_range PIP_LINENO_ALL[@] ${START_LINENO} ${END_LINENO}))
+    DOCKER_LINENO=($(remove_out_of_range DOCKER_LINENO_ALL[@] ${START_LINENO} ${END_LINENO}))
+    BUILDFROMSOURCE_LINENO=($(remove_out_of_range BUILDFROMSOURCE_LINENO_ALL[@] ${START_LINENO} ${END_LINENO}))
 
     # get indices (or the next closest thing) of the instruction sets' starting line numbers
-    start_virtualenv_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${LINUX_PYTHON_CPU_VIRTUALENV_LINENO[0]})
-    start_pip_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${LINUX_PYTHON_CPU_PIP_LINENO[0]})
-    start_docker_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${LINUX_PYTHON_CPU_DOCKER_LINENO[0]})
-    start_buildfromsource_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${LINUX_PYTHON_CPU_BUILDFROMSOURCE_LINENO[0]})
-    end_pythoncpulinux_index=$(retrieve_closest_index SOURCE_LINES[@] ${LINUX_PYTHON_CPU_END_LINENO[0]})
+    start_virtualenv_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${VIRTUALENV_LINENO[0]})
+    start_pip_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${PIP_LINENO[0]})
+    start_docker_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${DOCKER_LINENO[0]})
+    start_buildfromsource_command_index=$(retrieve_closest_index SOURCE_LINES[@] ${BUILDFROMSOURCE_LINENO[0]})
+    end_index=$(retrieve_closest_index SOURCE_LINES[@] ${END_LINENO[0]})
 
     # sort the indices of the instruction sets' line numbers
-    unsorted_indexes=(${start_virtualenv_command_index} ${start_pip_command_index} ${start_docker_command_index} ${start_buildfromsource_command_index} ${end_pythoncpulinux_index})
+    unsorted_indexes=(${start_virtualenv_command_index} ${start_pip_command_index} ${start_docker_command_index} \
+        ${start_buildfromsource_command_index} ${end_index})
     sorted_indexes=($(sort unsorted_indexes[@]))
 
     # figure out the index of the instruction sets' ending line numbers
-    end_virtualenv_command_index=$(retrieve_closest_index sorted_indexes[@] $(( ${start_virtualenv_command_index} + 1 )))
+    end_virtualenv_command_index=$(retrieve_closest_index sorted_indexes[@] \
+        $(( ${start_virtualenv_command_index} + 1 )))
     end_pip_command_index=$(retrieve_closest_index sorted_indexes[@] $(( ${start_pip_command_index} + 1)))
     end_docker_command_index=$(retrieve_closest_index sorted_indexes[@] $(( ${start_docker_command_index} + 1 )))
-    end_buildfromsource_command_index=$(retrieve_closest_index sorted_indexes[@] $(( ${start_buildfromsource_command_index} +1 )))
+    end_buildfromsource_command_index=$(retrieve_closest_index sorted_indexes[@] \
+        $(( ${start_buildfromsource_command_index} +1 )))
 
     # retrieve the instruction sets' commands using the starting and ending line numbers' indices
-    virtualenv_commands=$(retrieve_commands ${start_virtualenv_command_index} ${sorted_indexes[$end_virtualenv_command_index]})
+    virtualenv_commands=$(retrieve_commands ${start_virtualenv_command_index} \
+        ${sorted_indexes[$end_virtualenv_command_index]})
     pip_commands=$(retrieve_commands ${start_pip_command_index} ${sorted_indexes[$end_pip_command_index]})
     docker_commands=$(retrieve_commands ${start_docker_command_index} ${sorted_indexes[$end_docker_command_index]})
-    buildfromsource_commands=$(retrieve_commands ${start_buildfromsource_command_index} ${sorted_indexes[$end_buildfromsource_command_index]})
+    buildfromsource_commands=$(retrieve_commands ${start_buildfromsource_command_index} \
+        ${sorted_indexes[$end_buildfromsource_command_index]})
 }
 
 
@@ -192,7 +232,7 @@ echo
 LINUX_PYTHON_CPU_START_LINENO=$(grep -n "START - Linux Python CPU Installation Instructions" "${FILE}" | cut -d : -f 1)
 LINUX_PYTHON_CPU_END_LINENO=$(grep -n "END - Linux Python CPU Installation Instructions" "${FILE}" | cut -d : -f 1)
 
-get_instruction_set ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}
+set_instruction_set ${LINUX_PYTHON_CPU_START_LINENO} ${LINUX_PYTHON_CPU_END_LINENO}
 
 echo
 echo "### Testing Virtualenv ###"
@@ -227,7 +267,8 @@ echo
 # range of all lines inside Linux-Python-GPU instructions
 LINUX_PYTHON_GPU_START_LINENO=$(grep -n "START - Linux Python GPU Installation Instructions" "${FILE}" | cut -d : -f 1)
 LINUX_PYTHON_GPU_END_LINENO=$(grep -n "END - Linux Python GPU Installation Instructions" "${FILE}" | cut -d : -f 1)
-#VERIFY ONLY ONE ELEMENT FOR EACH OF ABOVE
+
+set_instruction_set ${LINUX_PYTHON_GPU_START_LINENO} ${LINUX_PYTHON_GPU_END_LINENO}
 
 echo
 echo "### Testing Virtualenv ###"
