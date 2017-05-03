876f2aad171147388a9b27fb6fe9d9e958d24e83
Olivier <coolivie@amazon.com>
2017-05-03 14:27:54 -0700

    Fix visual c++ compile problem

--- src/operator/batch_norm.cu
+++ src/operator/batch_norm.cu
@@ -80,7 +80,8 @@ static unsigned GetMaxThreadCount(const bool print) {
 static const unsigned WARP_SIZE = 32;
 
 // The maximum number of threads in a block
-static const unsigned MAX_BLOCK_SIZE = std::min(GetMaxThreadCount(false), 512U);
+static const unsigned MAX_BLOCK_SIZE = 512U;
+//static const unsigned MAX_BLOCK_SIZE = std::min(GetMaxThreadCount(false), 512U);
 
 template<typename In, typename Out>
 struct ScalarConvert {
