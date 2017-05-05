8f502cc9428562b58aed85ee606267b338a8c639
Jha <madhavjh@amazon.com>
2017-05-04 17:08:09 -0700

    Adding docstring for IRHeader

--- python/mxnet/recordio.py
+++ python/mxnet/recordio.py
@@ -272,6 +272,19 @@ class MXIndexedRecordIO(MXRecordIO):
 
 
 IRHeader = namedtuple('HEADER', ['flag', 'label', 'id', 'id2'])
+"""An alias for HEADER used for storing metadata (e.g. labels) that accompanies a record.
+See mxnet.recordio.pack and mxnet.recordio.pack_img for example uses.
+Parameters
+----------
+flag : int
+    available for convenience, can be set arbitrarily
+label : float or an array of float
+    typically used to store label(s) for a record
+id: int
+    usually a unique id representing record
+id2: int
+    higher order bits of the unique id, should be set to 0 (in most cases)
+"""
 _IR_FORMAT = 'IfQQ'
 _IR_SIZE = struct.calcsize(_IR_FORMAT)
 
