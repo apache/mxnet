048b11054a99773aa4c50e5ab6f6c5563b437be8
Sean Morgan <seanmorgan91@gmail.com>
2017-05-08 09:10:08 -0400

    Fix python3 compatibilities (#6143)

--- example/dec/dec.py
+++ example/dec/dec.py
@@ -98,7 +98,7 @@ class DECModel(model.MXModel):
         test_iter = mx.io.NDArrayIter({'data': X}, batch_size=batch_size, shuffle=False,
                                       last_batch_handle='pad')
         args = {k: mx.nd.array(v.asnumpy(), ctx=self.xpu) for k, v in self.args.items()}
-        z = model.extract_feature(self.feature, args, None, test_iter, N, self.xpu).values()[0]
+        z = list(model.extract_feature(self.feature, args, None, test_iter, N, self.xpu).values())[0]
         kmeans = KMeans(self.num_centers, n_init=20)
         kmeans.fit(z)
         args['dec_mu'][:] = kmeans.cluster_centers_
@@ -113,7 +113,7 @@ class DECModel(model.MXModel):
         self.y_pred = np.zeros((X.shape[0]))
         def refresh(i):
             if i%update_interval == 0:
-                z = model.extract_feature(self.feature, args, None, test_iter, N, self.xpu).values()[0]
+                z = list(model.extract_feature(self.feature, args, None, test_iter, N, self.xpu).values())[0]
                 p = np.zeros((z.shape[0], self.num_centers))
                 self.dec_op.forward([z, args['dec_mu'].asnumpy()], [p])
                 y_pred = p.argmax(axis=1)
