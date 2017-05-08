e20f0fa394efe7c9dedccbbb83c80d51d0e2ba20
Sean Morgan <seanmorgan91@gmail.com>
2017-05-08 09:09:22 -0400

    Fix for invalid numpy float indexing (#6144)

--- example/dec/dec.py
+++ example/dec/dec.py
@@ -21,7 +21,7 @@ def cluster_acc(Y_pred, Y):
   D = max(Y_pred.max(), Y.max())+1
   w = np.zeros((D,D), dtype=np.int64)
   for i in range(Y_pred.size):
-    w[Y_pred[i], Y[i]] += 1
+    w[Y_pred[i], int(Y[i])] += 1
   ind = linear_assignment(w.max() - w)
   return sum([w[i,j] for i,j in ind])*1.0/Y_pred.size, w
 
