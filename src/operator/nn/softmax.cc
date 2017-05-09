22188a608b2acc7852a4b4901f0c29bec3a12a47
Jiajie (George) Chen <jiajiechen0319@gmail.com>
2017-05-09 16:58:36 -0700

    doc improvement - softmax, metrics, and initializer (#5945)
    * doc improvement, softmaxoutput, initializer-constant, minor fixes
    
    * doc improvement, metrics
    
    * fix softmax doc, fix metric lint
    
    * softmax more fixes
    
    * add doc change in initializer.py. some minor fix in softmax_cross_entropy
    
    * doc change in initializer.py
    
    * fix grammer
    
    * fix
    
    * fix
    
    * fix
    
    * minor fix
    
    * fix
    
    * minor fix

--- src/operator/nn/softmax.cc
+++ src/operator/nn/softmax.cc
@@ -12,12 +12,13 @@ namespace op {
 DMLC_REGISTER_PARAMETER(SoftmaxParam);
 
 MXNET_OPERATOR_REGISTER_UNARY(softmax)
-.describe(R"code(Applies the softmax function. The resulting array contains
- elements in the range (0,1) and the elements along the given axis sum up to 1.
+.describe(R"code(Applies the softmax function.
+
+The resulting array contains elements in the range (0,1) and the elements along the given axis sum up to 1.
 
 .. math::
    softmax(\mathbf{z})_j = \frac{e^{z_j}}{\sum_{k=1}^K e^{z_k}}
-  
+
 for :math:`j = 1, ..., K`
 
 Example::
@@ -30,7 +31,7 @@ Example::
 
   softmax(x,axis=1) = [[ 0.33333334,  0.33333334,  0.33333334],
                        [ 0.33333334,  0.33333334,  0.33333334]]
- 
+
 )code" ADD_FILELINE)
 .set_attr_parser(ParamParser<SoftmaxParam>)
 .set_attr<FCompute>("FCompute<cpu>", SoftmaxCompute<cpu, mxnet_op::softmax_fwd>)
