0c4b751defae3979042fcfaaa444bc9a6b87fd9e
piiswrong <eric.jy.xie@gmail.com>
2017-05-17 21:15:57 +0000

    fix

--- python/mxnet/rnn/rnn_cell.py
+++ python/mxnet/rnn/rnn_cell.py
@@ -373,7 +373,7 @@ class RNNCell(RecurrentCell):
         return 'rnn'
 
     def generic_forward(self, F, inputs, states, i2h_weight, i2h_bias,
-                       h2h_weight, h2h_bias):
+                        h2h_weight, h2h_bias):
         name = self._curr_prefix
         i2h = F.FullyConnected(data=inputs, weight=i2h_weight, bias=i2h_bias,
                                num_hidden=self._num_hidden,
@@ -428,7 +428,7 @@ class LSTMCell(RecurrentCell):
         return 'lstm'
 
     def generic_forward(self, F, inputs, states, i2h_weight, i2h_bias,
-                       h2h_weight, h2h_bias):
+                        h2h_weight, h2h_bias):
         name = self._curr_prefix
         i2h = F.FullyConnected(data=inputs, weight=i2h_weight, bias=i2h_bias,
                                num_hidden=self._num_hidden*4,
@@ -490,7 +490,7 @@ class GRUCell(RecurrentCell):
         return 'gru'
 
     def generic_forward(self, F, inputs, states, i2h_weight, i2h_bias,
-                       h2h_weight, h2h_bias):
+                        h2h_weight, h2h_bias):
         # pylint: disable=too-many-locals
         name = self._curr_prefix
         prev_state_h = states[0]
