f58033a847cc2b03f903d8359c03a4446203f86b
Hanting Xie <hanting.xie@gamesparks.com>
2017-05-17 13:00:07 +0100

    Added EarlyStopping strategy into callback.py for python users.

--- python/mxnet/module/base_module.py
+++ python/mxnet/module/base_module.py
@@ -450,10 +450,10 @@ class BaseModule(object):
         self.init_optimizer(kvstore=kvstore, optimizer=optimizer,
                             optimizer_params=optimizer_params)
 
-        if validation_metric is None:
-            validation_metric = eval_metric
         if not isinstance(eval_metric, metric.EvalMetric):
             eval_metric = metric.create(eval_metric)
+        if validation_metric is None:
+            validation_metric = eval_metric
 
         ################################################################################
         # training loop
@@ -505,15 +505,30 @@ class BaseModule(object):
                 for callback in _as_list(epoch_end_callback):
                     callback(epoch, self.symbol, arg_params, aux_params)
 
-            #----------------------------------------
+            # ----------------------------------------
             # evaluation on validation set
             if eval_data:
                 res = self.score(eval_data, validation_metric,
                                  score_end_callback=eval_end_callback,
                                  batch_end_callback=eval_batch_end_callback, epoch=epoch)
+                unpacked_result = dict(res)
                 #TODO: pull this into default
-                for name, val in res:
+                for name, val in unpacked_result.items():
                     self.logger.info('Epoch[%d] Validation-%s=%f', epoch, name, val)
+                if type(validation_metric) is not str:
+                    measurement_for_stopping = validation_metric.name
+                else:
+                    measurement_for_stopping = validation_metric
+                metric_for_stopping = unpacked_result[measurement_for_stopping]
+            else:
+                measurement_for_stopping = eval_metric.name
+                metric_for_stopping = dict(eval_metric.get_name_value())[measurement_for_stopping]
+
+            if epoch_end_callback is not None:
+                for callback in _as_list(epoch_end_callback):
+                    if not callback(epoch, self.symbol, arg_params, aux_params, measurement_for_stopping,
+                                    metric_for_stopping):
+                        return
 
             # end of 1 epoch, reset the data-iter for another epoch
             train_data.reset()
