2892fd0c54cb3f859e72f9c229e58a406d139c5d
Xin Li <yakumolx@gmail.com>
2017-05-20 14:41:00 +0800

    ci test debug (#6363)

--- cpp-package/tests/ci_test.sh
+++ cpp-package/tests/ci_test.sh
@@ -1,5 +1,10 @@
-cd $(dirname $(readlink -f $0))/../example && \
-  ln -sf ../../lib/libmxnet.so . &&
-  cp ../../build/cpp-package/example/test_score . && \
-  ./get_mnist.sh && \
-  ./test_score 0.93
+set -e # exit on the first error
+cd $(dirname $(readlink -f $0))/../example
+echo $PWD
+ls -l ../..
+ln -sf ../../lib/libmxnet.so .
+ls -l libmxnet.so
+
+cp ../../build/cpp-package/example/test_score .
+./get_mnist.sh
+./test_score 0.93
