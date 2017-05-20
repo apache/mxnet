bb145bbd4c4a90e1a87e3c7a566e7871a55b005c
Xin Li <yakumolx@gmail.com>
2017-05-20 15:41:59 +0800

    Fix ci test

--- cpp-package/tests/ci_test.sh
+++ cpp-package/tests/ci_test.sh
@@ -1,9 +1,9 @@
 set -e # exit on the first error
 cd $(dirname $(readlink -f $0))/../example
 echo $PWD
-ls -l ../..
-ln -sf ../../lib/libmxnet.so .
-ls -l libmxnet.so
+export LD_LIBRARY_PATH=$(readlink -f ../../lib):$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+ls -l ../../lib/
 
 cp ../../build/cpp-package/example/test_score .
 ./get_mnist.sh
