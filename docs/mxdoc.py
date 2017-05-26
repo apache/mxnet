f2241e5889f73eb0018558e34f1f3d99cefc3860
Yao Wang <kevinthesunwy@gmail.com>
2017-05-25 22:27:49 -0700

    Fix linear regression (#6432)
    * Fix Linear Regression Tutorial
    
    * Small fix

--- docs/mxdoc.py
+++ docs/mxdoc.py
@@ -239,7 +239,7 @@ def _get_src_download_btn(out_prefix, langs, lines):
         for f in [ipynb, src]:
             f = f.split('/')[-1]
             btn += '<button type="button" class="btn btn-default download" '
-            btn += 'onclick="window.location=\'%s\'"><span class="glyphicon glyphicon-download-alt"></span> %s </a></button>\n' % (f, f)
+            btn += 'onclick="window.location=\'%s\'"><span class="glyphicon glyphicon-download-alt"></span> %s </button>\n' % (f, f)
     btn += '</div>\n'
     return btn
 
