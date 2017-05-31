d423030dcf014d5e5f7eefe05cc776ab92bda485
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-05-31 09:56:32 -0700

    nn interface (#6501)
    * mixed nn interface
    
    * fix loss
    
    * fix
    
    * fix prefix kwargs
    
    * refactor names
    
    * fix
    
    * fix
    
    * rnn lint
    
    * fix
    
    * fix
    
    * ignore invalid grad in optimizer
    
    * fix loss shape
    
    * fix
    
    * fix
    
    * post rebase fix
    
    * move nn to mxnet.
    
    * fix
    
    * use cached
    
    * dcgan
    
    * fix
    
    * fix
    
    * lint

---
+++ example/autograd/data.py
@@ -0,0 +1,69 @@
+# pylint: skip-file
+""" data iterator for mnist """
+import sys
+import os
+# code to automatically download dataset
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.append(os.path.join(curr_path, "../../tests/python/common"))
+import get_data
+import mxnet as mx
+
+def mnist_iterator(batch_size, input_shape):
+    """return train and val iterators for mnist"""
+    # download data
+    get_data.GetMNIST_ubyte()
+    flat = False if len(input_shape) == 3 else True
+
+    train_dataiter = mx.io.MNISTIter(
+        image="data/train-images-idx3-ubyte",
+        label="data/train-labels-idx1-ubyte",
+        input_shape=input_shape,
+        batch_size=batch_size,
+        shuffle=True,
+        flat=flat)
+
+    val_dataiter = mx.io.MNISTIter(
+        image="data/t10k-images-idx3-ubyte",
+        label="data/t10k-labels-idx1-ubyte",
+        input_shape=input_shape,
+        batch_size=batch_size,
+        flat=flat)
+
+    return (train_dataiter, val_dataiter)
+
+
+def cifar10_iterator(batch_size, data_shape, resize=-1):
+    train = mx.io.ImageRecordIter(
+        path_imgrec = "data/cifar/train.rec",
+        # mean_img    = "data/cifar/mean.bin",
+        resize      = resize,
+        data_shape  = data_shape,
+        batch_size  = batch_size,
+        rand_crop   = True,
+        rand_mirror = True)
+
+    val = mx.io.ImageRecordIter(
+        path_imgrec = "data/cifar/test.rec",
+        # mean_img    = "data/cifar/mean.bin",
+        resize      = resize,
+        rand_crop   = False,
+        rand_mirror = False,
+        data_shape  = data_shape,
+        batch_size  = batch_size)
+
+    return train, val
+
+class DummyIter(mx.io.DataIter):
+    def __init__(self, batch_size, data_shape):
+        self.data_shape = (batch_size,) + data_shape
+        self.label_shape = (batch_size,)
+        self.provide_data = [('data', self.data_shape)]
+        self.provide_label = [('softmax_label', self.label_shape)]
+
+    def next(self):
+        return mx.io.DataBatch(data=[mx.nd.zeros(self.data_shape)],
+                               label=[mx.nd.zeros(self.label_shape)])
+
+
+def dummy_iterator(batch_size, data_shape):
+    return DummyIter(batch_size, data_shape), DummyIter(batch_size, data_shape)
