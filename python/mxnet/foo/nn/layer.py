bf1837dac29812a433eaa7491370905481fc6f89
Gai Yu <yg1246@nyu.edu>
2017-06-07 11:19:13 +0800

    remove operator overloading

--- python/mxnet/foo/nn/layer.py
+++ python/mxnet/foo/nn/layer.py
@@ -181,21 +181,6 @@ class Layer(object):
         # pylint: disable= invalid-name
         raise NotImplementedError
 
-    def __add__(self, other):
-        return Add(self, other)
-
-    def __sub__(self, other):
-        return Sub(self, other)
-
-    def __mul__(self, other):
-        return Mul(self, other)
-
-    def __div__(self, other):
-        return Div(self, other)
-
-    def __truediv__(self, other):
-        return TrueDiv(self, other)
-
 
 class Sequential(Layer):
     """Stack Layers sequentially.
