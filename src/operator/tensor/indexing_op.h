35f06426ade0945fb3c4dbf46e54cb79933696fa
reminisce <wujun.nju@gmail.com>
2017-06-30 22:07:18 -0700

    Move dot and batch_dot to a single file

--- src/operator/tensor/indexing_op.h
+++ src/operator/tensor/indexing_op.h
@@ -22,7 +22,7 @@
 #include "../elemwise_op_common.h"
 #include "../mxnet_op.h"
 #include "./sort_op.h"
-#include "./matrix_op-inl.h"
+#include "../nn/matrix_dot-inl.h"
 
 namespace mxnet {
 namespace op {
