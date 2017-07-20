4f1fad3ac25da30d122d64c7302fa61bd362e0cb
Przemyslaw Tredak <ptrendx@gmail.com>
2017-07-20 15:55:48 -0700

    New algo for batchnorm for cuDNN 7 (#7131)
    * New algo for batchnorm for cuDNN 7
    
    * Fix Windows build

--- src/operator/cudnn_batch_norm-inl.h
+++ src/operator/cudnn_batch_norm-inl.h
@@ -94,6 +94,11 @@ class CuDNNBatchNormOp : public Operator {
 
     Tensor<gpu, 4, DType> y =
       out_data[cudnnbatchnorm::kOut].get_with_shape<gpu, 4, DType>(shape_, s);
+#if CUDNN_VERSION >= 7000
+    auto mode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+#else
+    auto mode = CUDNN_BATCHNORM_SPATIAL;
+#endif
 
     MSHADOW_REAL_TYPE_SWITCH(dtype_param_, DTypeParam, {
       Tensor<gpu, 1, DTypeParam> gamma =
@@ -118,7 +123,7 @@ class CuDNNBatchNormOp : public Operator {
           out_data[cudnnbatchnorm::kInvVar]
           .get_with_shape<gpu, 1, DTypeParam>(Shape1(shape_[1]), s);
         CUDNN_CALL(cudnnBatchNormalizationForwardTraining(s->dnn_handle_,
-                                                          CUDNN_BATCHNORM_SPATIAL,
+                                                          mode,
                                                           &a,
                                                           &b,
                                                           io_desc_,
@@ -178,6 +183,11 @@ class CuDNNBatchNormOp : public Operator {
       out_grad[cudnnbatchnorm::kOut].get_with_shape<gpu, 4, DType>(shape_, s);
 
 #if CUDNN_VERSION >= 4007
+#if CUDNN_VERSION >= 7000
+    auto mode = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+#else
+    auto mode = CUDNN_BATCHNORM_SPATIAL;
+#endif
     MSHADOW_REAL_TYPE_SWITCH(dtype_param_, DTypeParam, {
       Tensor<gpu, 1, DTypeParam> gamma =
         in_data[cudnnbatchnorm::kGamma].get_with_shape<gpu, 1, DTypeParam>(Shape1(shape_[1]), s);
@@ -199,7 +209,7 @@ class CuDNNBatchNormOp : public Operator {
 
       CUDNN_CALL(cudnnBatchNormalizationBackward(
         s->dnn_handle_,
-        CUDNN_BATCHNORM_SPATIAL,
+        mode,
         &a,
         &b,
         &a,
