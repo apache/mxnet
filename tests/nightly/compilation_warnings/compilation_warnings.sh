a0b59b89acb2596fdcbd530988135befe4ce8caa
Rahul Huilgol <huilgolr@amazon.com>
2017-07-26 16:45:53 -0700

    update jenkins script for mail and faster runtime

--- tests/nightly/compilation_warnings/compilation_warnings.sh
+++ tests/nightly/compilation_warnings/compilation_warnings.sh
@@ -1,4 +1,5 @@
-
+#!/bin/bash
+set -e
 runme() {
 	cmd=$*
 	echo "$cmd"
@@ -21,7 +22,6 @@ echo "Starting make"
 cp make/config.mk .
 sed -i -e 's/gcc/gcc-5/g' config.mk
 sed -i -e 's/g++/g++-5/g' config.mk
-runme /usr/bin/time -f "%e" make -j$(nproc) &> build/compile_output.txt
-cat build/compile_output.txt
+runme /usr/bin/time -f "%e" make -j$(nproc) 2>&1 | tee build/compile_output.txt
 echo "Finished make. Now processing output"
 python tests/nightly/compilation_warnings/process_output.py build/compile_output.txt
