340be836cdb4bc43070b7312f1c19f7dce3edf3a
Sheng Zha <zhasheng@amazon.com>
2017-07-30 14:09:03 -0700

    add tests

--- tests/python/unittest/test_gluon_model_zoo.py
+++ tests/python/unittest/test_gluon_model_zoo.py
@@ -1,6 +1,8 @@
+from __future__ import print_function
 import mxnet as mx
 from mxnet.gluon import nn
 from mxnet.gluon.model_zoo.custom_layers import HybridConcurrent, Identity
+from mxnet.gluon.model_zoo.vision import get_model
 
 
 def test_concurrent():
@@ -26,6 +28,26 @@ def test_identity():
     x = mx.nd.random_uniform(shape=(128, 33, 64))
     mx.test_utils.assert_almost_equal(model(x).asnumpy(),
                                       x.asnumpy())
+
+
+def test_models():
+    all_models = ['resnet18_v1', 'resnet34_v1', 'resnet50_v1', 'resnet101_v1', 'resnet152_v1',
+                  'resnet18_v2', 'resnet34_v2', 'resnet50_v2', 'resnet101_v2', 'resnet152_v2',
+                  'vgg11', 'vgg13', 'vgg16', 'vgg19',
+                  'vgg11_bn', 'vgg13_bn', 'vgg16_bn', 'vgg19_bn',
+                  'alexnet', 'inceptionv3',
+                  'densenet121', 'densenet161', 'densenet169', 'densenet201',
+                  'squeezenet1.0', 'squeezenet1.1']
+    pretrained_to_test = set(['squeezenet1.1'])
+
+    for model_name in all_models:
+        test_pretrain = model_name in pretrained_to_test
+        model = get_model(model_name, pretrained=test_pretrain)
+        data_shape = (7, 3, 224, 224) if 'inception' not in model_name else (7, 3, 299, 299)
+        print(model)
+        if not test_pretrain:
+            model.collect_params().initialize()
+        model(mx.nd.random_uniform(shape=data_shape))
 
 
 if __name__ == '__main__':
