f7130fe1de64264dcfe2e66623daad98895d5202
Pracheer Gupta <pracheer@Pracheers-MacBook-Pro.local>
2017-08-02 20:19:24 -0700

    Instead of commenting out a test, skip it.

--- tools/core_ml/test/test_mxnet_converter.py
+++ tools/core_ml/test/test_mxnet_converter.py
@@ -731,28 +731,29 @@ class SingleLayerTest(unittest.TestCase):
         # test the mxnet model
         self._test_mxnet_model(net, input_shape=input_shape, mode='random')
 
-#     # TODO Unable to get padding working for deconv layer
-#     def test_deconv_random_padding(self):
-#         np.random.seed(1988)
-#         input_shape = (1, 10, 9, 9)
-#         num_filter = 3
-#         kernel = (3, 3)
-#         stride = (3, 3)
-#         pad = (2, 2)
-#
-#         # define a model
-#         net = mx.sym.Variable('data')
-#         net = mx.symbol.Deconvolution(
-#                 data=net,
-#                 num_filter=num_filter,
-#                 kernel=kernel,
-#                 stride=stride,
-#                 pad=pad,
-#                 no_bias=False,
-#                 name = 'deconv_1')
-#
-#         # test the mxnet model
-#         self._test_mxnet_model(net, input_shape=input_shape, mode='random')
+    # TODO Unable to get padding working for deconv layer
+    @unittest.skip("Currently deconv with padding gives incorrect predictions.")
+    def test_deconv_random_padding(self):
+        np.random.seed(1988)
+        input_shape = (1, 10, 9, 9)
+        num_filter = 3
+        kernel = (3, 3)
+        stride = (3, 3)
+        pad = (2, 2)
+
+        # define a model
+        net = mx.sym.Variable('data')
+        net = mx.symbol.Deconvolution(
+                data=net,
+                num_filter=num_filter,
+                kernel=kernel,
+                stride=stride,
+                pad=pad,
+                no_bias=False,
+                name = 'deconv_1')
+
+        # test the mxnet model
+        self._test_mxnet_model(net, input_shape=input_shape, mode='random')
 
     def test_conv_random_padding_odd(self):
         np.random.seed(1988)
