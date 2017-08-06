b0c8c6c6e316d5d9b1a54146ba88caa63bfe4dc0
Sergey Kolychev <sergeykolychev.github@gmail.com>
2017-08-06 13:28:35 -0700

    [Perl] Fix for CI (#7343)
    * auto determiming the mxnet home.
    
    * autograd needs rework.

--- perl-package/test.sh
+++ perl-package/test.sh
@@ -1,16 +1,15 @@
-MXNET_HOME=$1
-HOME=$2
+MXNET_HOME=${PWD}
 export LD_LIBRARY_PATH=${MXNET_HOME}/lib
-export PERL5LIB=${HOME}/perl5/lib/perl5
+export PERL5LIB=${MXNET_HOME}/perl5/lib/perl5
 
 cd ${MXNET_HOME}/perl-package/AI-MXNetCAPI/
-perl Makefile.PL INSTALL_BASE=${HOME}/perl5
+perl Makefile.PL INSTALL_BASE=${MXNET_HOME}/perl5
 make install || exit -1
 
 cd ${MXNET_HOME}/perl-package/AI-NNVMCAPI/
-perl Makefile.PL INSTALL_BASE=${HOME}/perl5
+perl Makefile.PL INSTALL_BASE=${MXNET_HOME}/perl5
 make install || exit -1
 
 cd ${MXNET_HOME}/perl-package/AI-MXNet/
-perl Makefile.PL INSTALL_BASE=${HOME}/perl5
+perl Makefile.PL INSTALL_BASE=${MXNET_HOME}/perl5
 make test || exit -1
