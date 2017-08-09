253ae57b93817a9bb22d4cebad869afb85407766
Haibin Lin <linhaibin.eric@gmail.com>
2017-08-08 17:12:53 -0700

    add license to all new files in sparse brnach (#154)

--- benchmark/python/sparse_op.py
+++ benchmark/python/sparse_op.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import ctypes
 
 from mxnet.test_utils import *
@@ -68,7 +85,7 @@ def test_dot_real(data_dict):
             data_dict['data_origin_name']
         )
         assert os.path.exists(path)
-    
+
     k = data_dict['feature_dim']
     m = data_dict['m']
     density = estimate_density(path, data_dict['feature_dim'])
@@ -77,7 +94,7 @@ def test_dot_real(data_dict):
     if not os.path.exists(mini_path):
         os.system("head -n 2000 %r > %r" % (path, mini_path))
         assert os.path.exists(mini_path)
-    
+
     print "Running Benchmarking on %r data" % data_dict['data_mini']
     for batch_size in data_dict['batch_size']:  # iterator through different batch size of choice
         print "batch_size is %d" % batch_size
