81a02d8a7d0b1d762bc31409ab727f9c5d3d4a3e
Nguyen <lynguyen@amazon.com>
2017-08-11 09:59:43 -0700

    Bump up version to 0.11.0

--- include/mxnet/base.h
+++ include/mxnet/base.h
@@ -103,9 +103,9 @@
 /*! \brief major version */
 #define MXNET_MAJOR 0
 /*! \brief minor version */
-#define MXNET_MINOR 10
+#define MXNET_MINOR 11
 /*! \brief patch version */
-#define MXNET_PATCH 1
+#define MXNET_PATCH 0
 /*! \brief mxnet version */
 #define MXNET_VERSION (MXNET_MAJOR*10000 + MXNET_MINOR*100 + MXNET_PATCH)
 /*! \brief helper for making version number */
