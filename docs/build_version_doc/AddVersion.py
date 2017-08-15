b51515f29667cd432e18c7b43f086a27f70948bf
Yao Wang <kevinthesunwy@gmail.com>
2017-08-14 17:21:09 -0700

    Fix apache link (#7468)

--- docs/build_version_doc/AddVersion.py
+++ docs/build_version_doc/AddVersion.py
@@ -64,6 +64,7 @@ if __name__ == '__main__':
                     'https://github.com/apache/incubator-mxnet/tree/%s/example' % (args.current_version)
             navbar = content.find(id="main-nav")
             navbar_mobile = content.find(id="burgerMenu")
+            outstr = str(content)
             if navbar and navbar_mobile:
                 version_tag = content.find(id="dropdown-menu-position-anchor-version")
                 version_tag_mobile = content.find(id="dropdown-menu-position-anchor-version-mobile")
@@ -74,5 +75,17 @@ if __name__ == '__main__':
                 navbar.append(version_str)
                 navbar_mobile.append(version_str_mobile)
                 outstr = str(content).replace('&lt;', '<').replace('&gt;', '>')
-                with open(os.path.join(path, name), "w") as outf:
-                    outf.write(outstr)
+            # Fix link
+            if args.current_version == tag_list[0]:
+                print("Fixing" + os.path.join(path, name))
+                outstr = outstr.replace('https://mxnet.io', 'https://mxnet.incubator.apache.org')
+                outstr = outstr.replace('http://mxnet.io', 'https://mxnet.incubator.apache.org')
+            else:
+                outstr = outstr.replace('https://mxnet.io', 'https://mxnet.incubator.apache.org/'
+                                                                'versions/%s' % (args.current_version))
+                outstr = outstr.replace('http://mxnet.io', 'https://mxnet.incubator.apache.org/'
+                                                               'versions/%s' % (args.current_version))
+
+            with open(os.path.join(path, name), "w") as outf:
+                outf.write(outstr)
+
