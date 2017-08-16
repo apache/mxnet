d7d31b2d9ce2eee98a3b9f41bc8c526a3125ce78
shuqian.qu <qsq0519@gmail.com>
2017-08-17 01:32:14 +0800

    add depthwise convolution's gpu version optimization (#7393)
    * add depthwise convolution's gpu version optimization
    
    * add more config for test_depthwise_convolution
    
    * remove CUDA_1D_KERNEL_LOOP
    
    * fix windows compiling error
    
    * add support for kAddTo when cal input's backward
    
    * remove depthwise_conv_off params
    
    * Update convolution.cu
    
    * Update test_operator.py

--- src/common/cuda_utils.h
+++ src/common/cuda_utils.h
@@ -153,6 +153,16 @@ inline const char* CurandGetErrorString(curandStatus_t status) {
   return "Unknown cuRAND status";
 }
 
+template <typename DType>
+inline DType __device__ CudaMax(DType a, DType b) {
+    return a > b ? a : b;
+}
+
+template <typename DType>
+inline DType __device__ CudaMin(DType a, DType b) {
+    return a < b ? a : b;
+}
+
 }  // namespace cuda
 }  // namespace common
 }  // namespace mxnet
@@ -218,6 +228,14 @@ inline const char* CurandGetErrorString(curandStatus_t status) {
     CHECK_EQ(e, CURAND_STATUS_SUCCESS)                          \
         << "cuRAND: " << common::cuda::CurandGetErrorString(e); \
   }
+
+#if !defined(_MSC_VER)
+#define CUDA_UNROLL _Pragma("unroll")
+#define CUDA_NOUNROLL _Pragma("nounroll")
+#else
+#define CUDA_UNROLL
+#define CUDA_NOUNROLL
+#endif
 
 /*!
  * \brief Determine major version number of the gpu's cuda compute architecture.
@@ -291,7 +309,6 @@ inline bool GetEnvAllowTensorCore() {
   return dmlc::GetEnv("MXNET_CUDA_ALLOW_TENSOR_CORE",
                       dmlc::optional<bool>(default_value)).value();
 }
-
 #endif  // MXNET_USE_CUDA
 
 #if MXNET_USE_CUDNN
@@ -400,6 +417,15 @@ static inline __device__ void atomicAdd(mshadow::half::half_t *address,
               : (old & 0xffff0000) | hsum.half_;
     old = atomicCAS(address_as_ui, assumed, old);
   } while (assumed != old);
+}
+
+template <typename DType>
+__device__ inline DType ldg(const DType* address) {
+#if __CUDA_ARCH__ >= 350
+    return __ldg(address);
+#else
+    return *address;
+#endif
 }
 #endif
 
