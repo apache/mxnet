f72ce9823d00e5f8215511a56e8b239d59154940
Hanqing Huang <huanghanqing4@huawei.com>
2017-08-28 18:11:18 +0800

    modify symbol_pvanet end2end only

--- example/rcnn/script/pvanet_voc07.sh
+++ example/rcnn/script/pvanet_voc07.sh
@@ -4,4 +4,4 @@ export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 export PYTHONUNBUFFERED=1
 
 python train_end2end.py --network pvanet --gpu $1
-#python test.py --network pvanet --gpu $gpu
+python test.py --network pvanet --gpu $gpu
