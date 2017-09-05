47d054fad8b862ffcc0846a16887915af957e2f3
Hang Zhang <zhang.hang@rutgers.edu>
2017-09-05 16:10:45 -0400

    spacing

--- python/mxnet/gluon/nn/basic_layers.py
+++ python/mxnet/gluon/nn/basic_layers.py
@@ -452,7 +452,7 @@ class InstanceNorm(HybridBlock):
 
     .. math::
 
-    out = \frac{x - mean[data]}{ \sqrt{Var[data]} + \epsilon} * gamma + beta
+      out = \frac{x - mean[data]}{ \sqrt{Var[data]} + \epsilon} * gamma + beta
 
     This layer is similar to batch normalization layer (`BatchNorm`)
     with two differences: first, the normalization is
