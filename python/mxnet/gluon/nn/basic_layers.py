c3b46f780e982f8f8b0a04f1a04eca6c30066aa5
Hang Zhang <zhang.hang@rutgers.edu>
2017-09-05 15:23:14 -0400

    comment

--- python/mxnet/gluon/nn/basic_layers.py
+++ python/mxnet/gluon/nn/basic_layers.py
@@ -443,7 +443,36 @@ class Flatten(HybridBlock):
     def __repr__(self):
         return self.__class__.__name__
 
+
 class InstanceNorm(HybridBlock):
+    """
+    Applies instance normalization to the n-dimensional input array.
+    This operator takes an n-dimensional input array where (n>2) and normalizes 
+    the input using the following formula:
+    .. math::
+    out = \frac{x - mean[data]}{ \sqrt{Var[data]} + \epsilon} * gamma + beta
+    This layer is similar to batch normalization layer (`BatchNorm`)
+    with two differences: first, the normalization is
+    carried out per example (instance), not over a batch. Second, the
+    same normalization is applied both at test and train time. This
+    operation is also known as `contrast normalization`.
+    If the input data is of shape [batch, channel, spacial_dim1, spacial_dim2, ...],
+    `gamma` and `beta` parameters must be vectors of shape [channel].
+    This implementation is based on paper:
+    .. [1] Instance Normalization: The Missing Ingredient for Fast Stylization,
+       D. Ulyanov, A. Vedaldi, V. Lempitsky, 2016 (arXiv:1607.08022v2).
+    Examples::
+      // Input of shape (2,1,2)
+      x = mx.nd.array([[[ 1.1,  2.2]],
+                      [[ 3.3,  4.4]]])
+      // Instance normalization is calculated with the above formula
+      layer = InstanceNorm()
+      layer.initialize(ctx=mx.cpu(0))
+      layer(x)
+      [[[-0.99998355  0.99998331]]
+       [[-0.99998319  0.99998361]]]
+      <NDArray 2x1x2 @cpu(0)>
+    """
     def __init__(self, axis=1, momentum=0.9, epsilon=1e-5, center=True, scale=False,
                  beta_initializer='zeros', gamma_initializer='ones',
                  in_channels=0, **kwargs):
@@ -472,9 +501,26 @@ class InstanceNorm(HybridBlock):
                                            for k, v in self._kwargs.items()]))
 
 
-class ReflectancePadding(HybridBlock):
+class ReflectionPad(HybridBlock):
+    """Pads the input tensor using the reflection of the input boundary.
+
+    Args:
+        pad_width int: the size of the padding. If is int, uses the same
+            padding in all boundaries. 
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})` where
+          :math:`H_{out} = H_{in} + paddingTop + paddingBottom`
+          :math:`W_{out} = W_{in} + paddingLeft + paddingRight`
+
+    Examples::
+        m = nn.ReflectionPad(3)
+        input = mx.nd.random_normal(shape=(16, 3, 224, 224))
+        output = m(input)
+    """
     def __init__(self, pad_width=None, **kwargs):
-        super(ReflectancePadding, self).__init__(**kwargs)
+        super(ReflectionPad(, self).__init__(**kwargs)
         self.pad_width = pad_width
         
     def forward(self, x):
