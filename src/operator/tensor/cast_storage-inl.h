705499851775050ecd3cf7e9a7d7c83fce456103
Chengji Yao <yaochengji101@gmail.com>
2017-09-06 08:05:22 +0000

    Use memcopy instead of assigning each individual element

--- src/operator/tensor/cast_storage-inl.h
+++ src/operator/tensor/cast_storage-inl.h
@@ -108,7 +108,6 @@ inline void CastStorageDnsRspImpl(const OpContext& ctx,
   });
 }
 
-// TODO(haibin) Use memcopy instead will be much faster than assigning each individual element
 struct CastStorageRspDnsKernel {
   template<typename DType, typename IType>
   MSHADOW_XINLINE static void Map(int i,
@@ -120,9 +119,7 @@ struct CastStorageRspDnsKernel {
     IType rid = idx[i];
     dim_t dns_offset = rid * row_length;
     dim_t rsp_offset = i * row_length;
-    for (dim_t col = 0; col < row_length; col++) {
-      dns[dns_offset + col] = data[rsp_offset + col];
-    }
+    std::copy(data + rsp_offset, data + rsp_offset + row_length, dns + dns_offset);
   }
 };
 
