939aff746d2dbd0a17ec51bf6d98d20b8f459650
CNevd <CNevd@users.noreply.github.com>
2017-09-14 11:43:38 +0800

    Update optimizer.py

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -24,6 +24,7 @@ import pickle
 import logging
 import warnings
 import numpy
+from .base import py_str
 from .ndarray import (NDArray, zeros, clip, sqrt, array, maximum, abs as NDabs)
 from .ndarray import (sgd_update, sgd_mom_update, adam_update, rmsprop_update, rmspropalex_update,
                       mp_sgd_update, mp_sgd_mom_update, ftrl_update)
