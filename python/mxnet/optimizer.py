53959b3d9263c98e54917820691a7dca877dd73f
Olivier <coolivie@amazon.com>
2017-09-14 12:45:09 -0700

    lint

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -26,7 +26,7 @@ import numpy
 from .base import py_str
 from .ndarray import (NDArray, zeros, clip, sqrt, sign, array, maximum, abs as NDabs)
 from .ndarray import (sgd_update, sgd_mom_update, adam_update, rmsprop_update, rmspropalex_update,
-                      mp_sgd_update, mp_sgd_mom_update, mp_sgd_update, mp_sgd_mom_update, square)
+                      mp_sgd_update, mp_sgd_mom_update, square)
 from .ndarray import _internal
 from .ndarray import op
 from .random import normal
