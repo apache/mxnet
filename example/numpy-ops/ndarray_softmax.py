014c06fc9a5b774c5b70bcbc64095d7706741e80
Sheng Zha <zhasheng@amazon.com>
2017-09-26 23:40:06 -0700

    rename

--- example/numpy-ops/ndarray_softmax.py
+++ example/numpy-ops/ndarray_softmax.py
@@ -20,7 +20,7 @@ import os
 import sys
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 sys.path.append(os.path.join(curr_path, "../../tests/python/common"))
-from get_data import mnist_iterator
+from get_data import MNISTIterator
 import mxnet as mx
 import numpy as np
 import logging
@@ -97,14 +97,14 @@ mlp = mysoftmax(data=fc3, name = 'softmax')
 
 # data
 
-train, val = mnist_iterator(batch_size=100, input_shape = (784,))
+train, val = MNISTIterator(batch_size=100, input_shape = (784,))
 
 # train
 
 logging.basicConfig(level=logging.DEBUG)
 
 model = mx.model.FeedForward(
-    ctx = mx.gpu(0), symbol = mlp, num_epoch = 20,
+    ctx = mx.cpu(0), symbol = mlp, num_epoch = 20,
     learning_rate = 0.1, momentum = 0.9, wd = 0.00001)
 
 model.fit(X=train, eval_data=val)
