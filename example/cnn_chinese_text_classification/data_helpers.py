920afd529edbd1a2b50b4480743b294a18b92797
wt <d4yuer@gmail.com>
2017-09-27 08:36:41 +0800

    switch download source

--- example/cnn_chinese_text_classification/data_helpers.py
+++ example/cnn_chinese_text_classification/data_helpers.py
@@ -51,7 +51,7 @@ def get_chinese_text():
         os.system("mkdir data/")
     if (not os.path.exists('data/pos.txt')) or \
        (not os.path.exists('data/neg')):
-        os.system("wget -q https://raw.githubusercontent.com/dmlc/web-data/master/mxnet/example/chinese_text.zip -P data/")
+        os.system("wget -q https://raw.githubusercontent.com/wut0n9/web-data/master/mxnet/example/chinese_text.zip -P data/")
         os.chdir("./data")
         os.system("unzip -u chinese_text.zip")
         os.chdir("..")
