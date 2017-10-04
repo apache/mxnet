9324a777ca1d56090d9eb3dd8af22b55fcf03586
eric-haibin-lin <linhaibin.eric@gmail.com>
2017-10-04 00:37:45 +0000

    add stype log to graph executor

--- src/executor/graph_executor.cc
+++ src/executor/graph_executor.cc
@@ -592,6 +592,7 @@ void GraphExecutor::Init(nnvm::Symbol symbol,
 
   g.attrs["storage_type"] = std::make_shared<dmlc::any>(std::move(arg_stypes));
   g = InferStorageType(std::move(g), std::move(StorageTypeVector()), "");
+  common::LogStorageType(g);
   if (g.GetAttr<size_t>("storage_type_num_unknown_nodes") != 0U) {
     HandleInferStorageTypeError(num_forward_inputs_, g.indexed_graph(),
                                 g.GetAttr<StorageTypeVector>("storage_type"));
@@ -971,6 +972,7 @@ void GraphExecutor::Init(nnvm::Symbol symbol,
   }
 
   g = InferStorageType(std::move(g), std::move(arg_stypes), "__storage_type__");
+  common::LogStorageType(g);
   if (g.GetAttr<size_t>("storage_type_num_unknown_nodes") != 0U) {
     HandleInferStorageTypeError(num_forward_inputs_, g.indexed_graph(),
                                 g.GetAttr<StorageTypeVector>("storage_type"));
