4317583b301480b032c52ac4cca9f22b2071efad
Viacheslav Kovalevskyi <vsk@amazon.com>
2017-10-05 15:30:26 -0700

    remove

--- tests/ci_build/ci_build.sh
+++ tests/ci_build/ci_build.sh
@@ -104,7 +104,7 @@ DOCKER_IMG_NAME=$(echo "${DOCKER_IMG_NAME}" | tr '[:upper:]' '[:lower:]')
 # skip with_the_same_user for non-linux
 uname=`uname`
 if [[ "$uname" == "Linux" ]]; then
-    PRE_COMMAND="tests/ci_build/with_the_same_user"
+    PRE_COMMAND="ln /dev/null /dev/raw1394; tests/ci_build/with_the_same_user"
 else
     PRE_COMMAND=""
 fi
