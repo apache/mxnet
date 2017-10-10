9e823abab1e229e37c2ef156cac3a21fe93816a2
Chris Olivier <cjolivier01@gmail.com>
2017-10-10 08:39:03 -0700

    Update matrix_op.cu
    Trivial change to trigger build

--- src/operator/tensor/matrix_op.cu
+++ src/operator/tensor/matrix_op.cu
@@ -21,7 +21,6 @@
  * \file matrix_op.cu
  * \brief GPU Implementation of matrix operations
  */
-// this will be invoked by gcc and compile GPU version
 #include "./matrix_op-inl.h"
 #include "./elemwise_unary_op.h"
 
