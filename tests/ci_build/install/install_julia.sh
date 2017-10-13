1b234fb0b97780a109087f69ac757d78361efde7
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-10-13 19:17:01 +0800

    WIP: Julia CI build (#8175)
    * Delete workspace before build. (#8195)
    
    * Delete workspace before build.
    
    * Move 'deleteDir' outside the retry.
    
    * Jenkins: fix Jenkinsfile indentation
    
    * add Julia package as a submodule and enable CPU build

--- tests/ci_build/install/install_julia.sh
+++ tests/ci_build/install/install_julia.sh
@@ -20,10 +20,10 @@
 
 set -e
 
-wget https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.0-linux-x86_64.tar.gz
-mv julia-0.5.0-linux-x86_64.tar.gz /tmp/
-tar xfvz /tmp/julia-0.5.0-linux-x86_64.tar.gz
-rm -f /tmp/julia-0.5.0-linux-x86_64.tar.gz
+wget https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.0-linux-x86_64.tar.gz
+mv julia-0.6.0-linux-x86_64.tar.gz /tmp/
+tar xfvz /tmp/julia-0.6.0-linux-x86_64.tar.gz
+rm -f /tmp/julia-0.6.0-linux-x86_64.tar.gz
 
 # tar extracted in current directory
-ln -s -f ${PWD}/julia-3c9d75391c/bin/julia /usr/bin/julia
+ln -s -f ${PWD}/julia-903644385b/bin/julia /usr/bin/julia
