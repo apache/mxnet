671a205c50d75b51fe6415b32e609260f2d4f2c5
Haibin Lin <linhaibin.eric@gmail.com>
2017-10-16 22:18:40 -0700

    Update elemwise_unary_op.cc

--- src/operator/tensor/elemwise_unary_op.cc
+++ src/operator/tensor/elemwise_unary_op.cc
@@ -139,20 +139,6 @@ MXNET_OPERATOR_REGISTER_UNARY(make_loss)
 MXNET_ADD_SPARSE_OP_ALIAS(make_loss)
 .describe(R"code(Make your own loss function in network construction.
 
-This operator accepts a customized loss function symbol as a terminal loss and
-the symbol should be an operator with no backward dependency.
-The output of this function is the gradient of loss with respect to the input data.
-
-For example, if you are a making a cross entropy loss function. Assume ``out`` is the
-predicted output and ``label`` is the true label, then the cross entropy can be defined as::
-
-  cross_entropy = label * log(out) + (1 - label) * log(1 - out)
-  loss = make_loss(cross_entropy)
-
-We will need to use ``make_loss`` when we are creating our own loss function or we want to
-combine multiple loss functions. Also we may want to stop some variables' gradients
-from backpropagation. See more detail in ``BlockGrad`` or ``stop_gradient``.
-
 The storage type of ``make_loss`` output depends upon the input storage type:
 
    - make_loss(default) = default
