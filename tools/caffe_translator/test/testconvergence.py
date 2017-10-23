46db8b2186ac1f9c0b08f43b1333593a46dfbc7c
Indhu Bharathi <indhubharathi@gmail.com>
2017-10-23 19:29:47 +0000

    - Bug fixes - Code refactoring

--- tools/caffe_translator/test/testconvergence.py
+++ tools/caffe_translator/test/testconvergence.py
@@ -9,7 +9,7 @@ class Criteria:
     def __init__(self, metric_name, relation, value):
         self.metric_name = metric_name
         self.relation = relation
-        self.value = value
+        self.value = float(value)
         
         self.lt = True if relation.lower() == "lt" else False
         self.gt = True if relation.lower() == "gt" else False
@@ -24,61 +24,57 @@ class Test:
         self.criterions = criterions
 
     def __str__(self):
-        out  = "train_val_prototxt: %s\n" % train_val_prototxt
-        out += "solver_prototxt: %s\n" % solver_prototxt
+        out  = "train_val_prototxt: %s\n" % self.train_val_prototxt
+        out += "solver_prototxt: %s\n" % self.solver_prototxt
         # Apologies for using 'Criteria' as singular in the interest of readability.
-        out += ("Criterions" if len(criterions)>1 else "Criteria") + ":\n"
-        for criteria in criterions:
+        out += ("Criterions" if len(self.criterions)>1 else "Criteria") + ":\n"
+        for criteria in self.criterions:
             out += "  %s\n" % criteria
         return out
 
-input_dir = "CaffeModels"
-output_dir = "out"
-converted_file_name = "converted.py"
-train_log_name = "train.log"
-test_desc_path = input_dir + "/test.txt"
+def parse_test_description(test_desc_path):
 
-tests = []
-criterions = []
-train_val_prototxt = None
-solver_prototxt = None
+    with open(test_desc_path) as f:
 
-with open(test_desc_path) as f:
-    
-    for line in f:
-        
-        if len(line) == 0 or line.isspace():
-            continue
-        
-        # If this line is a criteria definition
-        if re.match(r'\s', line):
-            # Extract the criteria and append it to the criterions list
-            line = line.strip()
-            metric_name, relation, value = line.split()
-            criteria = Criteria(metric_name, relation, value)
-            criterions.append(criteria)
+        tests = []
+        criterions = []
+        train_val_prototxt = None
+        solver_prototxt = None
 
-        else:
-            # If we have finished seeing one test definition and starting to see the next one 
-            if (len(criterions) > 0) and (train_val_prototxt is not None) and (solver_prototxt is not None):
+        for line in f:
 
-                # Create a test with the information we have and append it to the tests list
-                test = Test(train_val_prototxt, solver_prototxt, criterions)
-                tests.append(test)
-                
-                # reset criterions
-                criterions = []
+            if len(line) == 0 or line.isspace():
+                continue
+
+            # If this line is a criteria definition
+            if re.match(r'\s', line):
+                # Extract the criteria and append it to the criterions list
+                line = line.strip()
+                metric_name, relation, value = line.split()
+                criteria = Criteria(metric_name, relation, value)
+                criterions.append(criteria)
+
             else:
-                # The first test definition
-                train_val_prototxt, solver_prototxt = line.split()
-    
-    # Process the last test definition
-    test = Test(train_val_prototxt, solver_prototxt, criterions)
-    tests.append(test)
+                # If we have finished seeing one test definition and starting to see the next one 
+                if (len(criterions) > 0) and (train_val_prototxt is not None) and (solver_prototxt is not None):
 
+                    # Create a test with the information we have and append it to the tests list
+                    test = Test(train_val_prototxt, solver_prototxt, criterions)
+                    tests.append(test)
 
-for test in tests:
-    print(test)
+                    # reset criterions
+                    criterions = []
+                    
+                    # Read the new train_val and solver prototxt
+                    train_val_prototxt, solver_prototxt = line.split()
+                else:
+                    # The first test definition
+                    train_val_prototxt, solver_prototxt = line.split()
+
+        # Process the last test definition
+        test = Test(train_val_prototxt, solver_prototxt, criterions)
+        tests.append(test)
+    return tests
 
 def create_dir(path):
     if not os.path.exists(path):
@@ -87,27 +83,36 @@ def create_dir(path):
     else:
         print("%s already exists" % path)
 
-create_dir(output_dir)
-out_dir_num = 0
-
-report_path = output_dir + "/" + "test_report.txt"
-test_report = open(report_path, 'w')
-
 def translate(train_prototxt_path, solver_prototxt, out_dir_path):
 	# Run caffetranslator --training-prototxt <train_val_prototxt_path> --solver <solver_prototxt_path> --output-file <output_file_path>
     print("Translating %s and %s. Writing output to %s" % (train_prototxt_path, solver_prototxt, out_dir_path))
     converted_file_path = out_dir_path + "/" + converted_file_name
     args = "--training-prototxt %s --solver %s --output-file %s" % (train_prototxt_path, solver_prototxt, converted_file_path)
-    
+
     try:
         subprocess.run("caffetranslator " + args, shell=True, check=True, timeout=5)
     except:
         print("Failed to translate")
         traceback.print_exc(file=sys.stdout)
         return 1
-   
+
     return 0
-	
+
+def train_network(out_dir_path):
+    # Run python <output_dir_path>/<converted_file_name>
+    print("Training translated network at %s" % out_dir_path)
+    converted_file_path = out_dir_path + "/" + converted_file_name
+    train_log_path = out_dir_path + "/" + train_log_name 
+    command = "python " + converted_file_path + " 2>&1 | tee " + train_log_path
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except:
+        print("Failed training the converted network")
+        traceback.print_exc(file=sys.stdout)
+        return 1
+
+    return 0
+
 def get_test_result(out_dir_path, criteria):
     log_file_path = out_dir_path + "/" + train_log_name
     log_full_text = open(log_file_path).read()
@@ -126,48 +131,63 @@ def get_test_result(out_dir_path, criteria):
             best_metric = min(best_metric, metric)
         elif criteria.gt is True:
             best_metric = max(best_metric, metric)
-    
+
     result = False
     if criteria.lt is True:
         result = True if best_metric <= criteria.value else False
     elif criteria.gt is True:
         result = True if best_metric >= criteria.value else False
-    
+
     result_str = "passed" if result == True else "failed"
     result_str = "%s: %f (%s)" % (criteria.metric_name, best_metric, result_str)
 
     return (result, result_str)
 
-def train_network(out_dir_path):
-    # Run python <output_dir_path>/<converted_file_name>
-    print("Training translated network at %s" % out_dir_path)
-    converted_file_path = out_dir_path + "/" + converted_file_name
-    train_log_path = out_dir_path + "/" + train_log_name 
-    command = "python " + converted_file_path + " > " + train_log_path + " 2>&1"
-    try:
-        subprocess.run(command, shell=True, check=True, timeout=5)
-    except:
-        print("Failed training the converted network")
-        traceback.print_exc(file=sys.stdout)
-        return 1
-    
-    return 0
+input_dir = "CaffeModels"
+output_dir = "out"
+converted_file_name = "converted.py"
+train_log_name = "train.log"
+test_desc_path = "test.txt"
+
+# Read tests description from the provided test description file
+tests = parse_test_description(test_desc_path)
+
+for test in tests:
+    print(test)
+
+# Create the output directory if it doesn't already exist
+create_dir(output_dir)
+
+# Open the report file
+report_path = output_dir + "/" + "test_report.txt"
+test_report = open(report_path, 'w')
+
+out_dir_num = 0
 
 for test in tests:
 
+    # Create the output directory where the translated network and training logs will be saved
     out_dir_path = output_dir + "/" + str(out_dir_num) + "/"
     create_dir(out_dir_path)
     out_dir_num += 1
 
-    train_prototxt_path = input_dir + "/" + test.train_val_prototxt
-    solver_prototxt     = input_dir + "/" + test.solver_prototxt
+    # Translate the given network
+    train_prototxt_path = test.train_val_prototxt
+    solver_prototxt     = test.solver_prototxt
     translate(train_prototxt_path, solver_prototxt, out_dir_path)
+    
+    # Train the translated network
     train_network(out_dir_path)
 
     all_tests_passed = True
-    
+
+    # Observed metrics for this test (List of human readable string)
     observed_metrics = ""
+    
     for criteria in test.criterions:
+        # result is boolean denoting whether the observed metric satisfies the 
+        # requirement mentioned in the test.
+        # result_str is a descriptive human readable format of 'result'
         result, result_str = get_test_result(out_dir_path, criteria)
         observed_metrics += result_str + "\n"
 
@@ -180,5 +200,3 @@ for test in tests:
     test_report.write(str(observed_metrics) + "\n")
     test_report.write("Result: " + ("Passed" if all_tests_passed else "Failed") + "\n\n")
     test_report.write("------------------------------------------------------------------\n\n")
-
-
