e50d221840db7a9d2469be8a918fcf0be46c7ac4
ZiyueHuang <zyhuang94@gmail.com>
2017-10-24 20:30:17 +0800

    fix indentation

--- src/operator/tensor/ordering_op-inl.h
+++ src/operator/tensor/ordering_op-inl.h
@@ -191,12 +191,9 @@ void TopKImpl(RunContext ctx,
                  &target_shape, &batch_size, &element_num, &axis, &k, &do_transpose, &is_ascend);
   Tensor<xpu, 3, real_t> dat = src.FlatTo3D<xpu, real_t>(axis, axis, s);
   size_t temp_size = mxnet::op::SortByKeyWorkspaceSize<int, int, xpu>(src.Size());
-  temp_size = std::max(temp_size,
-                mxnet::op::SortByKeyWorkspaceSize<int, real_t, xpu>(src.Size()));
-  temp_size = std::max(temp_size,
-                mxnet::op::SortByKeyWorkspaceSize<real_t, int, xpu>(src.Size()));
-  size_t workspace_size = temp_size + sizeof(real_t) * src.Size() +
-                            sizeof(int) * src.Size() * 2;
+  temp_size = std::max(temp_size, mxnet::op::SortByKeyWorkspaceSize<int, real_t, xpu>(src.Size()));
+  temp_size = std::max(temp_size, mxnet::op::SortByKeyWorkspaceSize<real_t, int, xpu>(src.Size()));
+  size_t workspace_size = temp_size + sizeof(real_t) * src.Size() + sizeof(int) * src.Size() * 2;
   if (param.ret_typ == topk_enum::kReturnMask) {
     workspace_size += sizeof(int) * batch_size * k + sizeof(real_t) * batch_size * k;
   }
