5294d4dc18974feb7f753e19c456e467d5cc193a
Rahul Huilgol <rahulhuilgol@gmail.com>
2017-10-27 18:39:31 -0700

    minor fix

--- src/ndarray/ndarray_function.cu
+++ src/ndarray/ndarray_function.cu
@@ -218,7 +218,7 @@ void Dequantize2BitDispatch(mshadow::Stream<gpu>* s, const std::vector<TBlob>& i
 template<>
 void Quantize2BitDispatch(mshadow::Stream<gpu>* s, const std::vector<TBlob>& inputs,
                               const float neg_threshold, const float pos_threshold) {
-  mxnet::op::Quantize2BitImplMShadowPskv<gpu>(s, inputs, neg_threshold, pos_threshold);
+  mxnet::op::Quantize2BitImplMShadow<gpu>(s, inputs, neg_threshold, pos_threshold);
 }
 
 }  // namespace ndarray
