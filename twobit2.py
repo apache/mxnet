5c7a1ff3a8899348a36c972a3a4980417ccfe2f9
Rahul Huilgol <rahulhuilgol@gmail.com>
2017-10-28 01:07:33 +0000

    small updates

--- twobit2.py
+++ twobit2.py
@@ -7,7 +7,6 @@ from numpy.testing import assert_allclose, assert_array_equal
 from mxnet.test_utils import *
 import unittest
 import timeit
-mx.profiler.profiler_set_config(mode='all',filename='profiler.json')
 #shape = [(268435456)] #(25,),(16,),(1121),(14400),(144000),
 grad = mx.nd.load('example/image-classification/quant_data')[0]
 print(grad.shape)
@@ -15,16 +14,18 @@ print(grad.shape)
 residual = mx.nd.random_uniform(-0.4,0.4,shape=grad.shape, ctx=default_context())
 res = mx.nd.array(residual)
 compressed = mx.contrib.nd.create_2bit(grad)
+
 def run():
     compr = mx.nd.zeros(compressed.shape)
     decompr = mx.nd.zeros(grad.shape)
     mx.contrib.ndarray.quantize_2bit(grad, res, compr, -0.5, 0.5)
     mx.contrib.ndarray.dequantize_2bit(compr, decompr)
     mx.nd.waitall()
-mx.profiler.profiler_set_state('run')
-d = timeit.repeat(run, repeat=10, number=1)
-#mx.profiler.profiler_set_state('stop')
-print(d)
+#priming
+run()
+
+#d = timeit.repeat(run, repeat=10, number=1)
+#print(d)
 
 
 
@@ -35,5 +36,4 @@ def run_mshadow():
     mx.contrib.ndarray.dequantize_mshadow_2bit(compr, decompr)
     mx.nd.waitall()
 d2 = timeit.repeat(run_mshadow, repeat=25, number=1)
-mx.profiler.profiler_set_state('stop')
-# print( d2)
+print( d2)
