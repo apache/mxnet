246c1f1d5b5eb7cb8d24e2c8bd06fb3e1c7fbdd6
Da zheng <zhengda1936@gmail.com>
2017-10-31 15:52:55 +0000

    Support MKLDNN storage type in python.

--- python/mxnet/ndarray/sparse.py
+++ python/mxnet/ndarray/sparse.py
@@ -49,7 +49,7 @@ except ImportError:
     pass
 from ._internal import _set_ndarray_class
 from .ndarray import NDArray, _storage_type, _DTYPE_NP_TO_MX, _DTYPE_MX_TO_NP
-from .ndarray import _STORAGE_TYPE_STR_TO_ID, _STORAGE_TYPE_ROW_SPARSE, _STORAGE_TYPE_CSR
+from .ndarray import _STORAGE_TYPE_STR_TO_ID, _STORAGE_TYPE_ROW_SPARSE, _STORAGE_TYPE_CSR, _STORAGE_TYPE_MKLDNN
 from .ndarray import _STORAGE_TYPE_UNDEFINED, _STORAGE_TYPE_DEFAULT
 from .ndarray import zeros as _zeros_ndarray
 from .ndarray import array as _array
@@ -1138,6 +1138,8 @@ def _ndarray_cls(handle, writable=True, stype=_STORAGE_TYPE_UNDEFINED):
         stype = _storage_type(handle)
     if stype == _STORAGE_TYPE_DEFAULT:
         return NDArray(handle, writable=writable)
+    elif stype == _STORAGE_TYPE_MKLDNN:
+        return NDArray(handle, writable=False)
     elif stype == _STORAGE_TYPE_CSR:
         return CSRNDArray(handle, writable=writable)
     elif stype == _STORAGE_TYPE_ROW_SPARSE:
