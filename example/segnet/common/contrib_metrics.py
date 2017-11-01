464a88f81ac611ed679d14ef73d7b8aa609c00b5
lipengfei <lipengfei44@huawei.com>
2017-11-01 09:48:27 +0800

    add license

--- example/segnet/common/contrib_metrics.py
+++ example/segnet/common/contrib_metrics.py
@@ -1,6 +1,22 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import numpy
 import mxnet as mx
-
 
 class Accuracy(mx.metric.EvalMetric):
     """Computes accuracy classification score.
@@ -126,14 +142,11 @@ class CrossEntropy(mx.metric.EvalMetric):
             if len(pred.shape) > 2:
                 for i in range(len(pred.shape) - 1, 1, -1):
                     pred = pred.swapaxes(1, i)
-            #pred = pred.swapaxes(1,3)
-            #pred = pred.swapaxes(1,2)
             pred = pred.reshape((-1, pred.shape[-1]))
             label = label.asnumpy()
             pred = pred.asnumpy()
 
             label = label.ravel()
-            #pdb.set_trace()
             assert label.shape[0] == pred.size / pred.shape[-1]
             while label.max() >= pred.shape[1]:
                 pred = numpy.append(pred, numpy.ones((pred.shape[0], 1)), 1)
