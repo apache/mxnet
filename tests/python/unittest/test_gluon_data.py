5aed28480763a7fca6027bd13aac550d78844f01
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-11-04 17:11:29 -0700

    Update test_gluon_data.py

--- tests/python/unittest/test_gluon_data.py
+++ tests/python/unittest/test_gluon_data.py
@@ -91,7 +91,6 @@ def test_image_folder_dataset():
     assert dataset.synsets == ['test_images']
     assert len(dataset.items) == 16
 
-
 def test_multi_worker():
     class Dataset(gluon.data.Dataset):
         def __len__(self):
