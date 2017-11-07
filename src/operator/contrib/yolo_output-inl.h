f49d6df747463a0ebad707958ca1dcd5d07409cf
Joshua Zhang <cheungchih@gmail.com>
2017-11-07 11:23:13 -0800

    backup

--- src/operator/contrib/yolo_output-inl.h
+++ src/operator/contrib/yolo_output-inl.h
@@ -18,13 +18,12 @@
 #include "../operator_common.h"
 #include "../mshadow_op.h"
 #include "../mxnet_op.h"
-#include "../tensor/sort_op.h"
 
 namespace mxnet {
 namespace op {
 namespace yoloout_enum {
 enum YoloOutputOpInputs {kData, kLabel};
-enum YoloOutputOpOutputs {kOut, kTemp};
+enum YoloOutputOpOutputs {kOut, kTemp, kCopy};
 enum YoloOutputOpAuxiliary {kCounter};
 enum YoloOutputOpResource {kTempSpace};
 }  // namespace yoloout_enum
@@ -206,15 +205,18 @@ struct box_grad {
       grad[out_offset + 1] = scale * (py - gy * height + row); // y
       grad[out_offset + 2] = scale * (pw - log(gw * width / aw));  // w
       grad[out_offset + 3] = scale * (ph - log(gh * height / ah)); // h
-      // printf("scale:%f, tx:%f, ty:%f, tw:%f, th:%f, x:%f, y:%f, w:%f, h:%f, dx:%f, dy:%f, dw:%f, dh:%f\n",
-      //   scale, gx * width - col, gy * height - row, log(gw * width / aw), log(gh * height / ah),
-      //   px, py, pw, ph, grad[out_offset], grad[out_offset+1], grad[out_offset+2], grad[out_offset+3]);
+      // if (fabs(grad[out_offset + 3] > 5)) {
+      //   printf("best_anchor: %d, gw:%f, gh:%f, scale:%f, tx:%f, ty:%f, tw:%f, th:%f, x:%f, y:%f, w:%f, h:%f, dx:%f, dy:%f, dw:%f, dh:%f\n",
+      //     best_anchor, gw, gh, scale, gx * width - col, gy * height - row, log(gw * width / aw), log(gh * height / ah),
+      //     px, py, pw, ph, grad[out_offset], grad[out_offset+1], grad[out_offset+2], grad[out_offset+3]);
+      // }
+
 
       // object grad
-      DType iou = IOU(px - pw / 2, py - ph / 2, px + pw / 2, py + ph / 2, gl, gt, gr, gb);
+      //DType iou = IOU(px - pw / 2, py - ph / 2, px + pw / 2, py + ph / 2, gl, gt, gr, gb);
       --out_offset;  // layout : num_class + 1 + 4
       --offset;
-      grad[out_offset] = object_scale * (pred[offset] - iou);
+      grad[out_offset] = object_scale * (pred[offset] - 1);
 
       // class target
       offset = i * width * height * num_anchor + row * width * num_anchor +
@@ -268,6 +270,7 @@ struct nms_impl {
     DType pb = input[pos_offset + 3];
     DType iou = IOU(refl, reft, refr, refb, pl, pt, pr, pb);
     if (iou > thresh) {
+      // printf("batch: %d, ref: %d, pos: %d, iou: %f\n", b, ref, pos, iou);
       index[b * num + pos] = -1;
     }
   }
@@ -282,14 +285,19 @@ struct nms_assign {
       int location = static_cast<int>(index[i * num + j]);
       if (location >= 0) {
         // copy to output
+        // printf("Batch: %d, Pos: %d, index: %d, %d\n", i, j, location, location % num);
         int out_location = (i * num + count) * stride;
         int in_location = location * stride;
         for (int s = 0; s < stride; ++s) {
           out[out_location + s] = input[in_location + s];
         }
+        // if (i == 0) {
+        //   printf("pos: %d, score: %f\n", j, float(out[out_location + 1]));
+        // }
         ++count;
       }
     }
+    // printf("batch: %d, count: %d\n", i, count);
   }
 };
 
@@ -309,13 +317,15 @@ class YoloOutputOp : public Operator {
      using namespace mshadow::expr;
      using namespace mxnet_op;
      CHECK_EQ(in_data.size(), 2U) << "YoloOutput Input: [data, label]";
-     CHECK_EQ(out_data.size(), 2U) << "YoloOutput Output: [output, temp]";
+     CHECK_EQ(out_data.size(), 3U) << "YoloOutput Output: [output, temp, copy]";
      Stream<xpu> *s = ctx.get_stream<xpu>();
      Tensor<xpu, 4, DType> data = in_data[yoloout_enum::kData]
       .get<xpu, 4, DType>(s);
      Tensor<xpu, 3, DType> out = out_data[yoloout_enum::kOut]
       .get<xpu, 3, DType>(s);
      Tensor<xpu, 3, DType> temp = out_data[yoloout_enum::kTemp]
+      .get<xpu, 3, DType>(s);
+     Tensor<xpu, 3, DType> out_copy = out_data[yoloout_enum::kCopy]
       .get<xpu, 3, DType>(s);
      Shape<3> tshape = temp.shape_;
      int nc = param_.num_class;
@@ -383,20 +393,23 @@ class YoloOutputOp : public Operator {
      slice<2>(out, 4, 5) += slice<2>(out, 2, 3);
      slice<2>(out, 5, 6) += slice<2>(out, 3, 4);
 
+     // make copy for backward
+     out_copy = F<mshadow_op::identity>(out);
+
      // clip to boundaries
      slice<2>(out, 2, 6) = F<clip_zero_one>(slice<2>(out, 2, 6));
 
      // apply nms
      if (param_.nms_threshold > 0 && param_.nms_threshold < 1) {
-       int keep = param_.nms_topk < 0 ? out.shape_[1] : 0;
+       int keep = param_.nms_topk < 0 ? out.shape_[1] : param_.nms_topk;
        keep = keep > out.shape_[1] ? out.shape_[1] : keep;
        if (keep > 0) {
          // descend sort by score
          int num_batch = out.shape_[0];
          int num_elem = out.shape_[1];
          Shape<1> sort_index_shape = Shape1(num_batch * num_elem);
-         Shape<3> out_shape = Shape3(num_batch, num_elem, out.shape_[2]);
-         index_t nms_ws_size = 3 * sort_index_shape.Size() + out_shape.Size();
+         Shape<3> buffer_shape = Shape3(num_batch, num_elem, out.shape_[2]);
+         index_t nms_ws_size = 3 * sort_index_shape.Size() + buffer_shape.Size();
          Tensor<xpu, 1, DType> nms_workspace = ctx.requested[yoloout_enum::kTempSpace]
           .get_space_typed<xpu, 1, DType>(Shape1(nms_ws_size), s);
          CHECK_EQ(nms_workspace.CheckContiguous(), true);
@@ -405,17 +418,17 @@ class YoloOutputOp : public Operator {
           sort_index_shape, s);
          Tensor<xpu, 1, DType> batch_id(scores.dptr_ + scores.MSize(),
           sort_index_shape, s);
-         Tensor<xpu, 3, DType> out_copy(batch_id.dptr_ + batch_id.MSize(),
-          out_shape, s);
+         Tensor<xpu, 3, DType> buffer(batch_id.dptr_ + batch_id.MSize(),
+          buffer_shape, s);
 
          // copy score to buffer, sort accordingly to get sorted index
-         scores = reshape(slice<1>(out, 1, 2), scores.shape_);
+         scores = reshape(slice<2>(out, 1, 2), scores.shape_);
          sorted_index = range<DType>(0, num_batch * num_elem);
-         mxnet::op::SortByKey(scores, sorted_index, false);
+         mshadow::SortByKey(scores, sorted_index, false);
          batch_id = F<mshadow_op::floor>(sorted_index / ScalarExp<DType>(num_elem));
-         mxnet::op::SortByKey(batch_id, scores, true);
+         mshadow::SortByKey(batch_id, scores, true);
          batch_id = F<mshadow_op::floor>(sorted_index / ScalarExp<DType>(num_elem));
-         mxnet::op::SortByKey(batch_id, sorted_index, true);
+         mshadow::SortByKey(batch_id, sorted_index, true);
 
          // go through each box as reference, suppress if overlap > threshold
          // sorted_index with -1 is marked as suppressed
@@ -427,12 +440,11 @@ class YoloOutputOp : public Operator {
              param_.nms_threshold, param_.force_suppress);
          }
 
-         // store the result in buffer
-         out_copy = -1;
-         Kernel<nms_assign, xpu>::Launch(s, num_batch, out_copy.dptr_,
-          out.dptr_, sorted_index.dptr_, keep, num_elem, out.shape_[2]);
-         // copy back to output
-         out = out_copy;
+         // store the result
+         buffer = F<mshadow_op::identity>(out);
+         out = -1;
+         Kernel<nms_assign, xpu>::Launch(s, num_batch, out.dptr_,
+          buffer.dptr_, sorted_index.dptr_, keep, num_elem, out.shape_[2]);
        }
      }
 
@@ -463,18 +475,18 @@ class YoloOutputOp : public Operator {
     using namespace mshadow;
     using namespace mshadow::expr;
     CHECK_EQ(in_data.size(), 2U) << "YoloOutput Input: [data, label]";
-    CHECK_EQ(out_data.size(), 2U) << "YoloOutput Output: [output, temp]";
+    CHECK_EQ(out_data.size(), 3U) << "YoloOutput Output: [output, temp, copy]";
     Stream<xpu> *s = ctx.get_stream<xpu>();
     Tensor<xpu, 3, DType> label = in_data[yoloout_enum::kLabel].get<xpu, 3, DType>(s);
     Tensor<xpu, 4, DType> grad = in_grad[yoloout_enum::kData].get<xpu, 4, DType>(s);
     Tensor<xpu, 3, DType> temp_out = out_data[yoloout_enum::kTemp].get<xpu, 3, DType>(s);
-    Tensor<xpu, 3, DType> out = out_data[yoloout_enum::kOut].get<xpu, 3, DType>(s);
+    Tensor<xpu, 3, DType> out_copy = out_data[yoloout_enum::kCopy].get<xpu, 3, DType>(s);
     Tensor<xpu, 1> counter = aux_states[yoloout_enum::kCounter].get<xpu, 1, real_t>(s);
     index_t num_batch = label.shape_[0];
     index_t num_label = label.shape_[1];
     index_t label_width = label.shape_[2];
     index_t pred_width = temp_out.shape_[2];
-    index_t num_box = out.shape_[1];
+    index_t num_box = temp_out.shape_[1];
     index_t in_width = grad.shape_[3];
     index_t in_height = grad.shape_[2];
     index_t nc = param_.num_class;
@@ -517,12 +529,12 @@ class YoloOutputOp : public Operator {
 
     Shape<3> tshape = Shape3(num_batch, num_box, num_label);
     for (int i = 0; i < 4; ++i) {
-      // gt_x, gt_y, gt_w, gt_h
+      // gt_x1, gt_y1, gt_x2, gt_y2
       buffer[i] = reshape(broadcast_with_axis(slice<2>(label, i + 1, i + 2), 0,
        num_box), tshape);
-      // pred_x, pred_y, pred_w, pred_h
-      buffer[i + 4] = reshape(broadcast_with_axis(slice<2>(out, i + 2, i + 3),
-       1, num_label), tshape);
+      // o_x1, o_y1, o_x2, o_y2
+      buffer[i + 4] = reshape(broadcast_with_axis(slice<2>(out_copy, i + 2,
+       i + 3), 1, num_label), tshape);
     }
     mxnet_op::Kernel<calc_overlap, xpu>::Launch(s, tshape.Size(), buffer[8].dptr_,
      buffer[0].dptr_, buffer[1].dptr_, buffer[2].dptr_, buffer[3].dptr_,
@@ -544,7 +556,7 @@ class YoloOutputOp : public Operator {
     // objectness grad
     temp_grad = DType(0);
     slice<2>(temp_grad, nc, nc + 1) = ScalarExp<DType>(param_.background_grad_scale) *
-     slice<2>(out, 1, 2);
+     slice<2>(temp_out, nc, nc + 1);
     // mask out when iou > thresh
     slice<2>(temp_grad, nc, nc + 1) *= F<mshadow_op::lt>(
      reduce_keepdim<red::maximum, false>(buffer[8], 2),
@@ -559,8 +571,7 @@ class YoloOutputOp : public Operator {
       counter = 0;
     }
     if (cpu_counter.dptr_[0] < param_.warmup_samples) {
-      const ScalarExp<DType> init_scale = ScalarExp<DType>(param_.warmup_grad_scale *
-        param_.coord_grad_scale);
+      const ScalarExp<DType> init_scale = ScalarExp<DType>(param_.warmup_grad_scale);
       slice<2>(temp_grad, nc + 1, nc + 2) = init_scale * (
         slice<2>(temp_out, nc + 1, nc + 2) - ScalarExp<DType>(0.5));
       slice<2>(temp_grad, nc + 2, nc + 3) = init_scale * (
@@ -610,9 +621,24 @@ class YoloOutputOp : public Operator {
     //       for (int nn = 0; nn < 25; ++nn) {
     //         ss << "(" << nn << ")" << debug[kk * 25  + nn][jj][ii];
     //       }
-    //       LOG(INFO) << ss.str();
+    //       if (debug[kk * 25][jj][ii] > 1e10 || debug[kk * 25][jj][ii] < -1e10) {
+    //         LOG(INFO) << ss.str();
+    //       }
     //     }
     //   }
+    // }
+
+    // Tensor<cpu, 2, DType> debug = ctx.requested[yoloout_enum::kTempSpace]
+    //  .get_host_space_typed<2, DType>(Shape2(845, 25));
+    // Copy(debug, temp_grad[0], s);
+    // for (int ii = 0; ii < 845; ++ii) {
+    //   if (fabs(debug[ii].dptr_[0]) < 1e-10 ) continue;
+    //   std::stringstream ss;
+    //   ss << ii << ": ";
+    //   for (int jj = 0; jj < 25; ++jj) {
+    //     ss << jj << "-" << debug[ii].dptr_[jj] << ", ";
+    //   }
+    //   LOG(INFO) << ss.str();
     // }
 
     // store loss in temp_output for metric, [0]:softmax, [1]:object, [2]:box
@@ -662,7 +688,7 @@ class YoloOutputProp : public OperatorProperty {
   }
 
   std::vector<std::string> ListOutputs() const override {
-    return {"output", "temp"};
+    return {"output", "temp", "copy"};
   }
 
   std::vector<std::string> ListAuxiliaryStates() const override {
@@ -670,7 +696,7 @@ class YoloOutputProp : public OperatorProperty {
   }
 
   int NumVisibleOutputs() const override {
-    return 2;
+    return 1;
   }
 
   bool InferShape(std::vector<TShape> *in_shape,
@@ -691,15 +717,21 @@ class YoloOutputProp : public OperatorProperty {
     SHAPE_ASSIGN_CHECK(*in_shape, yoloout_enum::kData, data_shape);
     // label shape
     TShape lshape = in_shape->at(yoloout_enum::kLabel);
-    CHECK_EQ(lshape.ndim(), 3) << "Label should be [batch-num_labels-(>=5)] tensor";
-    CHECK_GT(lshape[1], 0) << "Padded label should > 0";
-    CHECK_GE(lshape[2], 5) << "Label width must >=5";
+    if (lshape.ndim() > 0) {
+      CHECK_EQ(lshape.ndim(), 3) << "Label should be [batch-num_labels-(>=5)] tensor";
+      CHECK_GT(lshape[1], 0) << "Padded label should > 0";
+      CHECK_GE(lshape[2], 5) << "Label width must >=5";
+    } else {
+      lshape = Shape3(dshape[0], 2, 5);
+    }
+    SHAPE_ASSIGN_CHECK(*in_shape, yoloout_enum::kLabel, lshape);
     // output shape
     TShape oshape = Shape3(dshape[0], param_.num_anchor * dshape[2] * dshape[3], 6);
     out_shape->clear();
     out_shape->push_back(oshape);
     out_shape->push_back(Shape3(dshape[0], param_.num_anchor * dshape[2] * dshape[3],
       param_.num_class + 4 + 1));
+    out_shape->push_back(oshape);
     aux_shape->clear();
     aux_shape->push_back(Shape1(1));
     return true;
@@ -723,8 +755,9 @@ class YoloOutputProp : public OperatorProperty {
     aux_type->clear();
     aux_type->push_back(mshadow::kFloat32);
     out_type->clear();
-    out_type->push_back(dtype);
-    out_type->push_back(dtype);
+    out_type->push_back(dtype);  // out
+    out_type->push_back(dtype);  // temp
+    out_type->push_back(dtype);  // out copy
     return true;
   }
 
@@ -742,8 +775,8 @@ class YoloOutputProp : public OperatorProperty {
     const std::vector<int> &out_grad,
     const std::vector<int> &in_data,
     const std::vector<int> &out_data) const override {
-    return {in_data[yoloout_enum::kLabel], out_data[yoloout_enum::kOut],
-      out_data[yoloout_enum::kTemp]};
+    return {in_data[yoloout_enum::kLabel], out_data[yoloout_enum::kTemp],
+     out_data[yoloout_enum::kCopy]};
   }
 
   std::vector<ResourceRequest> ForwardResource(
