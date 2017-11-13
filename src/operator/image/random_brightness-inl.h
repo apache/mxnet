302b270f81438f2c40df2094f24be763a9179b03
Hu Shiwen <yajiedesign@gmail.com>
2017-11-14 00:21:36 +0800

    add RandomContrast

--- src/operator/image/random_brightness-inl.h
+++ src/operator/image/random_brightness-inl.h
@@ -27,9 +27,11 @@
 
 #include <vector>
 #include <mxnet/base.h>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/mat.hpp>
 #include "mxnet/op_attr_types.h"
 #include "image_common.h"
+
 
 namespace mxnet {
 namespace op {
@@ -50,16 +52,47 @@ static void RandomBrightness(const nnvm::NodeAttrs &attrs,
                              const std::vector<OpReqType> &req,
                              const std::vector<TBlob> &outputs) {
   auto input = inputs[0];
+  auto output = outputs[0];
   int hight = input.shape_[0];
   int weight = input.shape_[1];
   int channel = input.shape_[2];
 
-  auto m = mat_convert(input,hight, weight, channel);
+  auto input_mat = mat_convert(input, hight, weight, channel);
+  auto output_mat = mat_convert(output, hight, weight, channel);
+  //input_mat.convertTo(output_mat, -1, 1/255.0, 0);
   std::default_random_engine generator;
   const RandomBrightnessParam &param = nnvm::get<RandomBrightnessParam>(attrs.parsed);
   float alpha_b = 1.0 + std::uniform_real_distribution<float>(-param.max_brightness, param.max_brightness)(generator);
-  m.convertTo(m, -1, alpha_b, 0);
+  output_mat.convertTo(output_mat, -1, alpha_b, 0);
 }
+
+
+template<typename xpu>
+static void RandomContrast(const nnvm::NodeAttrs &attrs,
+  const OpContext &ctx,
+  const std::vector<TBlob> &inputs,
+  const std::vector<OpReqType> &req,
+  const std::vector<TBlob> &outputs) {
+  auto input = inputs[0];
+  auto output = outputs[0];
+  int hight = input.shape_[0];
+  int weight = input.shape_[1];
+  int channel = input.shape_[2];
+
+  auto input_mat = mat_convert(input, hight, weight, channel);
+  auto output_mat = mat_convert(output, hight, weight, channel);
+  //input_mat.convertTo(output_mat, -1, 1/255.0, 0);
+  std::default_random_engine generator;
+  const RandomBrightnessParam &param = nnvm::get<RandomBrightnessParam>(attrs.parsed);
+  float alpha_c = 1.0 + std::uniform_real_distribution<float>(-param.max_brightness, param.max_brightness)(generator);
+  cv::Mat temp_;
+  cv::cvtColor(input_mat, temp_,  CV_RGB2GRAY);
+  float gray_mean = cv::mean(temp_)[0];
+  input_mat.convertTo(output_mat, -1, alpha_c, (1 - alpha_c) * gray_mean);
+
+}
+
+
 } // namespace op
 } // namespace mxnet
 
