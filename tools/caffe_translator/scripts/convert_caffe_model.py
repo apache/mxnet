5232ba5f64d03ebb80a8a24f37eeed8391606451
Indhu Bharathi <indhubharathi@gmail.com>
2017-11-15 05:37:41 +0000

    Add ability to convert weights for Deconvolution, BatchNorm and Scale.

--- tools/caffe_translator/scripts/convert_caffe_model.py
+++ tools/caffe_translator/scripts/convert_caffe_model.py
@@ -10,28 +10,71 @@ class CaffeModelConverter:
     def __init__(self):
         self.dict_param = {}
     
-    def add_arg_param(self, param_name, blob_index):
-        self.dict_param['arg:%s' % param_name] = mx.nd.array(caffe.io.blobproto_to_array(self.blobs[blob_index]))
+    def add_param(self, param_name, layer_index, blob_index):
+        blobs = self.layers[layer_index].blobs
+        self.dict_param[param_name] = mx.nd.array(caffe.io.blobproto_to_array(blobs[blob_index]))
+    
+    def add_arg_param(self, param_name, layer_index, blob_index):
+        self.add_param('arg:%s' % param_name, layer_index, blob_index)
 
-    def add_optional_arg_param(self, param_name, blob_index):
-        if(blob_index < len(self.blobs)):
-            self.add_arg_param(param_name, blob_index)
+    def add_aux_param(self, param_name, layer_index, blob_index):
+        self.add_param('aux:%s' % param_name, layer_index, blob_index)
+
+    def add_optional_arg_param(self, param_name, layer_index, blob_index):
+        blobs = self.layers[layer_index].blobs
+        if(blob_index < len(blobs)):
+            self.add_arg_param(param_name, layer_index, blob_index)
 
     def convert(self, caffemodel_path, outmodel_path):
         self.net_param = caffe_pb2.NetParameter()
         with open(caffemodel_path, 'rb') as f:
             self.net_param.ParseFromString(f.read())
         
-        for layer in self.net_param.layer:
+        layers = self.net_param.layer
+        self.layers = layers
+        
+        for idx, layer in enumerate(layers):
             layer_name = str(layer.name)
 
-            self.blobs = layer.blobs
+            if len(layer.blobs) > 0:
 
-            if len(self.blobs) > 0:
                 layer_type = layer.type
-                if layer_type == 'Convolution' or layer_type == 'InnerProduct':
-                    self.add_arg_param('%s_weight' % layer_name, blob_index=0)
-                    self.add_optional_arg_param('%s_bias' % layer_name, blob_index=1)
+
+                # If this is a layer that has only weight and bias as parameter
+                if layer_type == 'Convolution' or layer_type == 'InnerProduct' or layer_type == 'Deconvolution':
+
+                    # Add weight and bias to the dictionary
+                    self.add_arg_param('%s_weight' % layer_name, layer_index=idx, blob_index=0)
+                    self.add_optional_arg_param('%s_bias' % layer_name, layer_index=idx, blob_index=1)
+
+                elif layer_type == 'BatchNorm':
+
+                    gamma_param_name = '%s_gamma' % layer_name
+                    beta_param_name = '%s_beta' % layer_name
+
+                    next_layer = layers[idx + 1]
+
+                    if next_layer.type == 'Scale':
+                        # If next layer is scale layer, get gamma and beta from there
+                        self.add_arg_param(gamma_param_name, layer_index=idx+1, blob_index=0)
+                        self.add_arg_param(beta_param_name, layer_index=idx+1, blob_index=1)
+                    
+                    mean_param_name = '%s_moving_mean' % layer_name
+                    var_param_name = '%s_moving_var' % layer_name
+                    
+                    self.add_aux_param(mean_param_name, layer_index=idx, blob_index=0)
+                    self.add_aux_param(var_param_name, layer_index=idx, blob_index=1)
+                    
+                elif layer_type == 'Scale':
+                
+                    prev_layer = layers[idx - 1]
+                    
+                    if prev_layer.type == 'BatchNorm':
+                        continue
+                    else:
+                        # Use the naming convention used by CaffeOp
+                        self.add_arg_param('%s_0_weight' % layer_name, layer_index=idx, blob_index=0)
+                        self.add_optional_arg_param('%s_1_bias' % layer_name, layer_index=idx, blob_index=1)
         
         mx.nd.save(outmodel_path, self.dict_param)
 
