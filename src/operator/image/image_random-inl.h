31be9a1848b91ab24ec967055f5ad5439e05c55c
Hu Shiwen <yajiedesign@gmail.com>
2017-11-15 10:58:03 +0800

    fix

--- src/operator/image/image_random-inl.h
+++ src/operator/image/image_random-inl.h
@@ -31,6 +31,7 @@
 #include <opencv2/core/mat.hpp>
 #include "../mxnet_op.h"
 #include "image_common.h"
+#include "operator/operator_common.h"
 
 
 namespace mxnet {
@@ -116,10 +117,10 @@ struct mul_add {
 
 template<typename xpu>
 static void RandomContrast(const nnvm::NodeAttrs &attrs,
-                           const OpContext &ctx,
-                           const std::vector<TBlob> &inputs,
-                           const std::vector<OpReqType> &req,
-                           const std::vector<TBlob> &outputs) {
+  const OpContext &ctx,
+  const std::vector<TBlob> &inputs,
+  const std::vector<OpReqType> &req,
+  const std::vector<TBlob> &outputs) {
   using namespace mshadow;
   auto input = inputs[0];
   auto output = outputs[0];
@@ -139,33 +140,82 @@ static void RandomContrast(const nnvm::NodeAttrs &attrs,
   static const float coeffs0[] = { R2YF, G2YF, B2YF };
 
   MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-    auto input_3d = input.FlatTo3D<xpu, DType>(s);
-    DType sum = (DType)0.0;
-    for (int c = 0; c < channel; ++c) {
-      for (int h = 0; h < hight; ++h) {
-        for (int w = 0; w < weight; ++w) {
-          sum += input_3d[c][h][w] * coeffs0[c];
-        }
+    auto input_3d = input.get<xpu, 3, DType>(s);
+  DType sum = (DType)0.0;
+  for (int c = 0; c < channel; ++c) {
+    for (int h = 0; h < hight; ++h) {
+      for (int w = 0; w < weight; ++w) {
+        sum += input_3d[c][h][w] * coeffs0[c];
       }
     }
-    float gray_mean = sum / (float)(hight * weight);
-    float beta = (1 - alpha_c) * gray_mean;
+  }
+  float gray_mean = sum / (float)(hight * weight);
+  float beta = (1 - alpha_c) * gray_mean;
 
-    MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-      mxnet_op::Kernel<mxnet_op::op_with_req<mul_add, Req>, xpu>::Launch(
-        s, inputs[0].Size(), outputs[0].dptr<DType>(), inputs[0].dptr<DType>(), DType(alpha_c), DType(beta));
-    });
+  MXNET_ASSIGN_REQ_SWITCH(req[0], Req,{
+    mxnet_op::Kernel<mxnet_op::op_with_req<mul_add, Req>, xpu>::Launch(
+      s, inputs[0].Size(), outputs[0].dptr<DType>(), inputs[0].dptr<DType>(), DType(alpha_c), DType(beta));
+  });
 
   });
 
 }
 
+struct RandomSaturationParam : public dmlc::Parameter<RandomSaturationParam> {
+  float max_saturation;
+  DMLC_DECLARE_PARAMETER(RandomSaturationParam) {
+    DMLC_DECLARE_FIELD(max_saturation)
+      .set_default(0.0)
+      .describe("Max Saturation.");
+  }
+};
+
 template<typename xpu>
 static void RandomSaturation(const nnvm::NodeAttrs &attrs,
-  const OpContext &ctx,
-  const std::vector<TBlob> &inputs,
-  const std::vector<OpReqType> &req,
-  const std::vector<TBlob> &outputs) {
+                             const OpContext &ctx,
+                             const std::vector<TBlob> &inputs,
+                             const std::vector<OpReqType> &req,
+                             const std::vector<TBlob> &outputs) {
+  using namespace mshadow;
+  auto input = inputs[0];
+  auto output = outputs[0];
+  int channel = input.shape_[0];
+  int hight = input.shape_[1];
+  int weight = input.shape_[2];
+  Stream<xpu> *s = ctx.get_stream<xpu>();
+  Random<xpu> *prnd = ctx.requested[kRandom].get_random<xpu, real_t>(s);
+  const RandomSaturationParam &param = nnvm::get<RandomSaturationParam>(attrs.parsed);
+  float alpha_s = 1.0 + std::uniform_real_distribution<float>(-param.max_saturation, param.max_saturation)(prnd->GetRndEngine());
+  float alpha_o = 1 - alpha_s;
+  const float R2YF = 0.299f;
+  const float G2YF = 0.587f;
+  const float B2YF = 0.114f;
+  static const float coeffs0[] = { R2YF, G2YF, B2YF };
+
+
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
+      auto input_3d =  input.get<xpu, 3, DType>(s);
+      auto output_3d = output.get<xpu, 3, DType>(s);
+      switch (channel) {
+      case 1:
+        Assign(output_3d, Req, input_3d)
+          break;
+      case 3:
+        for (int h = 0; h < hight; ++h) {
+          for (int w = 0; w < weight; ++w) {
+            float gray = input_3d[0][h][w] * R2YF + input_3d[1][h][w] * G2YF + input_3d[2][h][w] * B2YF;
+            Assign(output_3d[0][h][w], Req, DType(gray * alpha_s + input_3d[0][h][w] * alpha_o))
+          }
+        }
+        break;
+      default:
+        LOG(FATAL) << "not support channel" << channel;
+
+      }
+    });
+  });
+
 }
 
 template<typename xpu>
