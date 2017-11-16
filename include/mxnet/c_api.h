938eda901e10e87d469fd3cff43091060a6e1033
Anirudh Subramanian <anirudh2290@gmail.com>
2017-11-16 15:11:55 -0800

    Support sparse for custom python operators (#8620)
    * Add asscipy support and coo format support
    
    * Comment misalignment change
    
    * Add documentation for Sparse NDarray
    
    * Change comment
    
    * Adding comments and support for dtype
    
    * Modifying tests
    
    * Add spsp None check
    
    * Fix lint
    
    * Custom operators for sparse
    
    * Use DISPATCH_MODE_ASSIGN_CHECK
    
    * Change NDArray to _ndarray_cls
    
    * Remove redundant code
    
    * Add a test to make sure the NDArray is an instance of CSRNDArray
    
    * Fix lint
    
    * Fix test
    
    * Trigger CI

--- include/mxnet/c_api.h
+++ include/mxnet/c_api.h
@@ -146,7 +146,9 @@ enum CustomOpPropCallbacks {
   kCustomOpPropInferShape,
   kCustomOpPropDeclareBackwardDependency,
   kCustomOpPropCreateOperator,
-  kCustomOpPropInferType
+  kCustomOpPropInferType,
+  kCustomOpPropInferStorageType,
+  kCustomOpPropBackwardInferStorageType
 };
 
 
@@ -158,6 +160,10 @@ typedef int (*CustomOpListFunc)(char*** /*args*/, void* /*state*/);
 typedef int (*CustomOpInferShapeFunc)(int /*num_input*/, int* /*ndims*/,
                                       unsigned** /*shapes*/, void* /*state*/);
 typedef int (*CustomOpInferTypeFunc)(int /*num_input*/, int* /*types*/, void* /*state*/);
+typedef int (*CustomOpInferStorageTypeFunc)(int /*num_input*/, int* /*stypes*/, void* /*state*/);
+typedef int (*CustomOpBackwardInferStorageTypeFunc)(int /*num_input*/,
+                                                    int * /*stypes*/,
+                                                    void * /*state*/);
 typedef int (*CustomOpBwdDepFunc)(const int* /*out_grad*/, const int* /*in_data*/,
                                   const int* /*out_data*/, int* /*num_deps*/,
                                   int** /*rdeps*/, void* /*state*/);
