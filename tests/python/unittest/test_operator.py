713e3f4c370279c3d8f9092f0290e3ad1c0ad5e8
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-11-19 00:41:17 -0800

    disable test causing mklml-cpu to fail

--- tests/python/unittest/test_operator.py
+++ tests/python/unittest/test_operator.py
@@ -1043,6 +1043,7 @@ def test_convolution_grouping():
         np.testing.assert_allclose(arr1.asnumpy(), arr2.asnumpy(), rtol=1e-3, atol=1e-4)
 
 
+@unittest.skip("test fails intermittently. temporarily disabled till it gets fixed. tracked at https://github.com/apache/incubator-mxnet/issues/8712")
 def test_depthwise_convolution():
     for num_base in [1, 4, 16, 32, 64]:
         for kernel in [(3,3), (5,5)]:
