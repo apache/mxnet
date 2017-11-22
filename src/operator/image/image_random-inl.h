476513984e9c7b502f8a00f9af51b62ef6658d8e
Eric Junyuan Xie <piiswrong@users.noreply.github.com>
2017-11-21 17:43:21 -0800

    Refactor image operators (#8761)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * refactor
    
    * fix

--- src/operator/image/image_random-inl.h
+++ src/operator/image/image_random-inl.h
@@ -30,16 +30,11 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/mat.hpp>
 #include "../mxnet_op.h"
-#include "image_common.h"
-#include "../../operator/operator_common.h"
+#include "../operator_common.h"
 
 namespace mxnet {
 namespace op {
 
-
-enum ImageRandomResource { kRandom };
-
-template<typename xpu>
 static void RandomFlip(const nnvm::NodeAttrs &attrs,
                        const OpContext &ctx,
                        const std::vector<TBlob> &inputs,
@@ -73,37 +68,25 @@ inline bool ToTensorShape(const nnvm::NodeAttrs& attrs,
   return true;
 }
 
-template<typename xpu>
 static void ToTensor(const nnvm::NodeAttrs &attrs,
                      const OpContext &ctx,
                      const std::vector<TBlob> &inputs,
                      const std::vector<OpReqType> &req,
                      const std::vector<TBlob> &outputs) {
-  auto input = inputs[0];
-  auto output = outputs[0];
-
-  int height = input.shape_[0];
-  int weight = input.shape_[1];
-  int channel = input.shape_[2];
-
-  typedef float   DstDType;
-  typedef uint8_t SrcDType;
-
   CHECK_EQ(req[0], kWriteTo)
     << "`to_tensor` does not support inplace";
 
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-    auto input_3d =  input.get<xpu, 3, SrcDType>(s);
-    auto output_3d = output.get<xpu, 3, DstDType>(s);
-    for (int h = 0; h < height; ++h) {
-      for (int w = 0; w < weight; ++w) {
-        for (int c = 0; c < channel; ++c) {
-          Assign(output_3d[c][h][w], Req, DstDType(input_3d[h][w][c] / 255.0));
-        }
-      }
+  int length = inputs[0].shape_[0] * inputs[0].shape_[1];
+  int channel = inputs[0].shape_[2];
+
+  float* output = outputs[0].dptr<float>();
+  uint8_t* input = inputs[0].dptr<uint8_t>();
+
+  for (int l = 0; l < length; ++l) {
+    for (int c = 0; c < channel; ++c) {
+      output[c*length + l] = static_cast<float>(input[l*channel + c]) / 255.0f;
     }
-  });
+  }
 }
 
 struct NormalizeParam : public dmlc::Parameter<NormalizeParam> {
@@ -117,93 +100,47 @@ struct NormalizeParam : public dmlc::Parameter<NormalizeParam> {
   }
 };
 
-struct normalize {
-  template<typename DType>
-  MSHADOW_XINLINE static void Map(int i, DType *out, const DType *in,
-                                  const OpReqType req,
-                                  const int nchannel, const int size,
-                                  const float *mean, const float *std) {
-    int c = 0;
-    switch (nchannel) {
-      case 1:
-        break;
-      case 3:
-        if (i < size) {
-          c = 0;
-        } else if (i < (size << 1)) {
-          c = 1;
-        } else {
-          c = 2;
-        }
-        break;
-      default:
-        LOG(FATAL) << "not support channel" << nchannel;
-    }
-    float m = (mean ? mean[c] : 0);
-    KERNEL_ASSIGN(out[i], req, static_cast<DType>((in[i] - m) / std[c]));
-  }
-};
 
-static void NormalizeCheckParam(const nnvm::Tuple<float> &mean,
-                                const nnvm::Tuple<float> &std,
-                                const int nchannel) {
-  CHECK(mean.ndim() == 1 || mean.ndim() == 3)
-    << "Mean must be in dimension 1 or 3.";
-  CHECK(std.ndim() == 1 || std.ndim() == 3)
-    << "Standard deviations must be in dimension 1 or 3.";
-  CHECK(nchannel == 1 || nchannel == 3) << "Image channel must be 1 or 3.";
-  CHECK_EQ(mean.ndim(), nchannel)
-    << "Mean dimension does not agree with image channel.";
-  CHECK_EQ(std.ndim(), nchannel)
-    << "Standard deviations dimension does not agree with image channel.";
-  for (uint32_t c = 0; c < std.ndim(); ++c) {
-    CHECK(std[c] > 0) << "Invalid standard deviation " << std[c];
-  }
+inline bool NormalizeShape(const nnvm::NodeAttrs& attrs,
+                          std::vector<TShape> *in_attrs,
+                          std::vector<TShape> *out_attrs) {
+  const NormalizeParam &param = nnvm::get<NormalizeParam>(attrs.parsed);
+  const auto& dshape = (*in_attrs)[0];
+  if (!dshape.ndim()) return false;
+  CHECK_EQ(dshape.ndim(), 3)
+      << "Input must have 3 dimensions";
+
+  auto nchannels = dshape[0];
+  CHECK(param.mean.ndim() == 1 || param.mean.ndim() == nchannels)
+      << "mean must have either 1 or " << nchannels << " elements";
+  CHECK(param.std.ndim() == 1 || param.std.ndim() == nchannels)
+      << "std must have either 1 or " << nchannels << " elements";
+
+  SHAPE_ASSIGN_CHECK(*out_attrs, 0, dshape);
 }
 
-template<typename xpu>
+
 static void Normalize(const nnvm::NodeAttrs &attrs,
                       const OpContext &ctx,
                       const std::vector<TBlob> &inputs,
                       const std::vector<OpReqType> &req,
                       const std::vector<TBlob> &outputs) {
   const NormalizeParam &param = nnvm::get<NormalizeParam>(attrs.parsed);
-  auto mean = param.mean;
-  auto std = param.std;
 
-  int nchannel = inputs[0].shape_[0];
-  NormalizeCheckParam(mean, std, nchannel);
+  int nchannels = inputs[0].shape_[0];
+  int length = inputs[0].shape_[1] * inputs[0].shape_[2];
 
-  int size = inputs[0].Size() / nchannel;
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-    MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-      mxnet_op::Kernel<normalize, xpu>::Launch(
-        s, inputs[0].Size(), outputs[0].dptr<DType>(), inputs[0].dptr<DType>(),
-        Req, nchannel, size, mean.begin(), std.begin());
-    });
-  });
-}
+  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    DType* input = inputs[0].dptr<DType>();
+    DType* output = outputs[0].dptr<DType>();
 
-template<typename xpu>
-static void NormalizeBackward(const nnvm::NodeAttrs &attrs,
-                              const OpContext &ctx,
-                              const std::vector<TBlob> &inputs,
-                              const std::vector<OpReqType> &req,
-                              const std::vector<TBlob> &outputs) {
-  const NormalizeParam &param = nnvm::get<NormalizeParam>(attrs.parsed);
-  int nchannel = inputs[0].shape_[0];
-
-  NormalizeCheckParam(param.mean, param.std, nchannel);
-
-  int size = inputs[0].Size() / nchannel;
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-    MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-      mxnet_op::Kernel<normalize, xpu>::Launch(
-        s, inputs[0].Size(), outputs[0].dptr<DType>(), inputs[0].dptr<DType>(),
-        Req, nchannel, size, nullptr, param.std.begin());
-      });
+    for (int i = 0; i < nchannels; ++i) {
+      DType mean = param.mean[param.mean.ndim() > 1 ? i : 0];
+      DType std = param.std[param.std.ndim() > 1 ? i : 0];
+      for (int j = 0; j < length; ++j) {
+        output[i*length + j] = (input[i*length + j] - mean) / std;
+      }
+    }
   });
 }
 
@@ -211,99 +148,83 @@ struct RandomBrightnessParam : public dmlc::Parameter<RandomBrightnessParam> {
   float max_brightness;
   DMLC_DECLARE_PARAMETER(RandomBrightnessParam) {
     DMLC_DECLARE_FIELD(max_brightness)
-    .set_default(0.0)
+    .set_lower_bound(0.0)
     .describe("Max Brightness.");
   }
 };
 
-template<typename xpu>
 static void RandomBrightness(const nnvm::NodeAttrs &attrs,
                              const OpContext &ctx,
                              const std::vector<TBlob> &inputs,
                              const std::vector<OpReqType> &req,
                              const std::vector<TBlob> &outputs) {
   using namespace mshadow;
-  auto input = inputs[0];
-  auto output = outputs[0];
-  int channel = input.shape_[0];
-  int height = input.shape_[1];
-  int weight = input.shape_[2];
-  Stream<xpu> *s = ctx.get_stream<xpu>();
-  Random<xpu> *prnd = ctx.requested[kRandom].get_random<xpu, real_t>(s);
-
   const RandomBrightnessParam &param = nnvm::get<RandomBrightnessParam>(attrs.parsed);
+
+  int length = inputs[0].Size();
+
+  uint8_t* output = outputs[0].dptr<uint8_t>();
+  uint8_t* input = inputs[0].dptr<uint8_t>();
+
+  Stream<cpu> *s = ctx.get_stream<cpu>();
+  Random<cpu> *prnd = ctx.requested[0].get_random<cpu, float>(s);
   float alpha_b = 1.0 + std::uniform_real_distribution<float>(
-    -param.max_brightness, param.max_brightness)(prnd->GetRndEngine());
-  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-    MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-      mxnet_op::Kernel<mxnet_op::op_with_req<mshadow::op::mul, Req>, xpu>::Launch(
-        s, inputs[0].Size(), outputs[0].dptr<DType>(), inputs[0].dptr<DType>(), DType(alpha_b));
-    });
-  });
+      -param.max_brightness, param.max_brightness)(prnd->GetRndEngine());
+
+  for (int l = 0; l < length; ++l) {
+    float val = static_cast<float>(input[l]) * alpha_b;
+    val = std::min(std::max(val, 0.f), 255.f);
+    output[l] = static_cast<uint8_t>(val);
+  }
 }
+
 
 struct RandomContrastParam : public dmlc::Parameter<RandomContrastParam> {
   float max_contrast;
   DMLC_DECLARE_PARAMETER(RandomContrastParam) {
     DMLC_DECLARE_FIELD(max_contrast)
-    .set_default(0.0)
+    .set_lower_bound(0.0)
     .describe("Max Contrast.");
   }
 };
 
-/*! \brief mul_add operator */
-struct mul_add {
-  /*! \brief map a, b, c to result using defined operation */
-  template<typename DType>
-  MSHADOW_XINLINE static DType Map(DType a, DType b, DType c) {
-    return a * b + c;
-  }
-};
 
-template<typename xpu>
 static void RandomContrast(const nnvm::NodeAttrs &attrs,
                            const OpContext &ctx,
                            const std::vector<TBlob> &inputs,
                            const std::vector<OpReqType> &req,
                            const std::vector<TBlob> &outputs) {
   using namespace mshadow;
-  auto input = inputs[0];
-  auto output = outputs[0];
-  int channel = input.shape_[0];
-  int height = input.shape_[1];
-  int weight = input.shape_[2];
-  Stream<xpu> *s = ctx.get_stream<xpu>();
-  Random<xpu> *prnd = ctx.requested[kRandom].get_random<xpu, real_t>(s);
-
-
+  static const float coef[] = { 0.299f, 0.587f, 0.114f };
   const RandomContrastParam &param = nnvm::get<RandomContrastParam>(attrs.parsed);
+
+  int length = inputs[0].shape_[0] * inputs[0].shape_[1];
+  int nchannels = inputs[0].shape_[2];
+
+  uint8_t* output = outputs[0].dptr<uint8_t>();
+  uint8_t* input = inputs[0].dptr<uint8_t>();
+
+  Stream<cpu> *s = ctx.get_stream<cpu>();
+  Random<cpu> *prnd = ctx.requested[0].get_random<cpu, real_t>(s);
   float alpha_c = 1.0 + std::uniform_real_distribution<float>(
     -param.max_contrast, param.max_contrast)(prnd->GetRndEngine());
 
-  const float R2YF = 0.299f;
-  const float G2YF = 0.587f;
-  const float B2YF = 0.114f;
-  static const float coeffs0[] = { R2YF, G2YF, B2YF };
-
-  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-    auto input_3d = input.get<xpu, 3, DType>(s);
-    DType sum = (DType)0.0;
-    for (int c = 0; c < channel; ++c) {
-      for (int h = 0; h < height; ++h) {
-        for (int w = 0; w < weight; ++w) {
-          sum += input_3d[c][h][w] * coeffs0[c];
-        }
-      }
+  float sum = 0.f;
+  if (nchannels > 1) {
+    for (int l = 0; l < length; ++l) {
+      for (int c = 0; c < nchannels; ++c) sum += input[l*nchannels + c] * coef[c];
     }
-    float gray_mean = sum / static_cast<float>(height * weight);
-    float beta = (1 - alpha_c) * gray_mean;
+  } else {
+    for (int l = 0; l < length; ++l) sum += input[l];
+  }
+  float gray_mean = sum / static_cast<float>(length);
+  float beta = (1 - alpha_c) * gray_mean;
 
-    MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-      mxnet_op::Kernel<mxnet_op::op_with_req<mul_add, Req>, xpu>::Launch(
-        s, inputs[0].Size(), outputs[0].dptr<DType>(),
-        inputs[0].dptr<DType>(), DType(alpha_c), DType(beta));
-    });
-  });
+  for (int l = 0; l < length * nchannels; ++l) {
+    float val = input[l] * alpha_c + beta;
+    val = std::min(std::max(val, 0.f), 255.f);
+    output[l] = static_cast<uint8_t>(val);
+  }
 }
 
 struct RandomSaturationParam : public dmlc::Parameter<RandomSaturationParam> {
@@ -315,55 +236,46 @@ struct RandomSaturationParam : public dmlc::Parameter<RandomSaturationParam> {
   }
 };
 
-template<typename xpu>
 static void RandomSaturation(const nnvm::NodeAttrs &attrs,
                              const OpContext &ctx,
                              const std::vector<TBlob> &inputs,
                              const std::vector<OpReqType> &req,
                              const std::vector<TBlob> &outputs) {
   using namespace mshadow;
-  auto input = inputs[0];
-  auto output = outputs[0];
-  int channel = input.shape_[0];
-  int height = input.shape_[1];
-  int weight = input.shape_[2];
-  Stream<xpu> *s = ctx.get_stream<xpu>();
-  Random<xpu> *prnd = ctx.requested[kRandom].get_random<xpu, real_t>(s);
   const RandomSaturationParam &param = nnvm::get<RandomSaturationParam>(attrs.parsed);
-  float alpha_s = 1.0 + std::uniform_real_distribution<float>(
+  static const float coef[] = { 0.299f, 0.587f, 0.114f };
+
+  int length = inputs[0].shape_[0] * inputs[0].shape_[1];
+  int nchannels = inputs[0].shape_[2];
+
+  uint8_t* output = outputs[0].dptr<uint8_t>();
+  uint8_t* input = inputs[0].dptr<uint8_t>();
+
+  Stream<cpu> *s = ctx.get_stream<cpu>();
+  Random<cpu> *prnd = ctx.requested[0].get_random<cpu, real_t>(s);
+  float alpha_s = 1.f + std::uniform_real_distribution<float>(
     -param.max_saturation, param.max_saturation)(prnd->GetRndEngine());
-  float alpha_o = 1 - alpha_s;
-  const float R2YF = 0.299f;
-  const float G2YF = 0.587f;
-  const float B2YF = 0.114f;
-  static const float coeffs0[] = { R2YF, G2YF, B2YF };
+  float alpha_o = 1.f - alpha_s;
 
+  if (nchannels == 1) {
+    for (int l = 0; l < length * nchannels; ++l) output[l] = input[l];
+    return;
+  }
 
-  MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-    MXNET_ASSIGN_REQ_SWITCH(req[0], Req, {
-      auto input_3d =  input.get<xpu, 3, DType>(s);
-      auto output_3d = output.get<xpu, 3, DType>(s);
-      switch (channel) {
-        case 1:
-          Assign(output_3d, Req, input_3d)
-          break;
-        case 3:
-          for (int h = 0; h < height; ++h) {
-            for (int w = 0; w < weight; ++w) {
-              float gray =
-                input_3d[0][h][w] * R2YF + input_3d[1][h][w] * G2YF + input_3d[2][h][w] * B2YF;
-              Assign(output_3d[0][h][w], Req, DType(gray * alpha_s + input_3d[0][h][w] * alpha_o))
-            }
-          }
-          break;
-        default:
-          LOG(FATAL) << "not support channel" << channel;
-      }
-    });
-  });
+  for (int l = 0; l < length; ++l) {
+    float gray = 0.f;
+    for (int c = 0; c < nchannels; ++c) {
+      gray = input[l*nchannels + c] * coef[c];
+    }
+    gray *= alpha_o;
+    for (int c = 0; c < nchannels; ++c) {
+      float val = gray + input[l*nchannels + c] * alpha_s;
+      val = std::min(std::max(val, 0.f), 255.f);
+      output[l*nchannels + c] = static_cast<uint8_t>(val);
+    }
+  }
 }
 
-template<typename xpu>
 static void RandomHue(const nnvm::NodeAttrs &attrs,
                       const OpContext &ctx,
                       const std::vector<TBlob> &inputs,
@@ -371,7 +283,6 @@ static void RandomHue(const nnvm::NodeAttrs &attrs,
                       const std::vector<TBlob> &outputs) {
 }
 
-template<typename xpu>
 static void RandomColorJitter(const nnvm::NodeAttrs &attrs,
                               const OpContext &ctx,
                               const std::vector<TBlob> &inputs,
@@ -379,7 +290,6 @@ static void RandomColorJitter(const nnvm::NodeAttrs &attrs,
                               const std::vector<TBlob> &outputs) {
 }
 
-template<typename xpu>
 static void RandomLighting(const nnvm::NodeAttrs &attrs,
                            const OpContext &ctx,
                            const std::vector<TBlob> &inputs,
