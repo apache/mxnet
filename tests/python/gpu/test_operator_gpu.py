91ffd691bf865833a51ca793eda125b7f9befc18
Sheng Zha <szha@users.noreply.github.com>
2017-11-26 13:58:45 -0800

    Update test_operator_gpu.py (#8819)
    change clip norm test to assume 1 gpu only

--- tests/python/gpu/test_operator_gpu.py
+++ tests/python/gpu/test_operator_gpu.py
@@ -1425,7 +1425,7 @@ def test_cuda_rtc():
 
 def test_global_norm_clip_multi_device():
     x1 = mx.nd.ones((3,3), ctx=mx.gpu(0))
-    x2 = mx.nd.ones((4,4), ctx=mx.gpu(1))
+    x2 = mx.nd.ones((4,4), ctx=mx.cpu(0))
     norm = gluon.utils.clip_global_norm([x1, x2], 1.0)
     assert norm == 5.0
     assert_almost_equal(x1.asnumpy(), np.ones((3,3))/5)
