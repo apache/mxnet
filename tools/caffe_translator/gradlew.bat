05507c258348632d090edd8f0342d19c0ca19db6
Indu Bharathi <indhubharathi@gmail.com>
2017-11-27 22:53:15 +0000

    Update gradle wrapper to use shadow plugin.

--- tools/caffe_translator/gradlew.bat
+++ tools/caffe_translator/gradlew.bat
@@ -66,7 +66,6 @@ goto fail
 @rem Get command-line arguments, handling Windows variants
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
-if "%@eval[2+2]" == "4" goto 4NT_args
 
 :win9xME_args
 @rem Slurp the command line arguments.
@@ -77,11 +76,6 @@ set _SKIP=2
 if "x%~1" == "x" goto execute
 
 set CMD_LINE_ARGS=%*
-goto execute
-
-:4NT_args
-@rem Get arguments from the 4NT Shell from JP Software
-set CMD_LINE_ARGS=%$
 
 :execute
 @rem Setup the command line
