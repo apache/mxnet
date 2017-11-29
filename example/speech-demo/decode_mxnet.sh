591ece5525c1a163ca89d093cd5dfa6cd8c1258f
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-11-29 13:43:27 -0800

    Remove ASF Licenses from some files

--- example/speech-demo/decode_mxnet.sh
+++ example/speech-demo/decode_mxnet.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-
 # Copyright 2012-2013 Karel Vesely, Daniel Povey
 # 	    2015 Yu Zhang
 # Apache 2.0
@@ -42,7 +24,7 @@ skip_scoring=false
 use_gpu_id=-1 # disable gpu
 #parallel_opts="-pe smp 2" # use 2 CPUs (1 DNN-forward, 1 decoder)
 parallel_opts= # use 2 CPUs (1 DNN-forward, 1 decoder)
-# End configuration section.
+# End configuration section.o
 
 echo "$0 $@"  # Print the command line for logging
 
