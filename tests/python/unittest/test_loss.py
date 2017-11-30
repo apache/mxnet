8df5b0c59a44d750acac3db8da11baca29f659be
Marco de Abreu <marco.g.abreu+github@gmail.com>
2017-11-30 19:10:04 +0100

    Disable flaky test https://github.com/apache/incubator-mxnet/issues/8892

--- tests/python/unittest/test_loss.py
+++ tests/python/unittest/test_loss.py
@@ -19,7 +19,7 @@ import mxnet as mx
 import numpy as np
 from mxnet import gluon
 from mxnet.test_utils import assert_almost_equal, default_context
-
+import unittest
 
 def test_loss_ndarray():
     output = mx.nd.array([1, 2, 3, 4])
@@ -160,6 +160,7 @@ def test_l1_loss():
     assert mod.score(data_iter, eval_metric=mx.metric.Loss())[0][1] < 0.1
 
 
+@unittest.skip("flaky test. https://github.com/apache/incubator-mxnet/issues/8892")
 def test_ctc_loss():
     loss = gluon.loss.CTCLoss()
     l = loss(mx.nd.ones((2,20,4)), mx.nd.array([[1,0,-1,-1],[2,1,1,-1]]))
@@ -186,6 +187,7 @@ def test_ctc_loss():
     mx.test_utils.assert_almost_equal(l.asnumpy(), np.array([18.82820702, 16.50581741]))
 
 
+@unittest.skip
 def test_ctc_loss_train():
     np.random.seed(1234)
     N = 20
