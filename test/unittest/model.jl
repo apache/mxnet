c43d0ddf26b65a4ff55bc1d2f9ce9bc4d82777d4
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-12-02 14:26:22 +0800

    model: fix test cases macro (#355)

--- test/unittest/model.jl
+++ test/unittest/model.jl
@@ -8,21 +8,21 @@ function test_feedforward()
   info("Model::FeedForward::constructor")
   let x = @mx.var x
     m = mx.FeedForward(x)
-    @assert m.arch === x
-    @assert length(m.ctx) == 1
+    @test m.arch === x
+    @test length(m.ctx) == 1
   end
 
   info("Model::FeedForward::constructor::keyword context")
   let x = @mx.var x
     m = mx.FeedForward(x, context = mx.cpu())
-    @assert m.arch === x
-    @assert length(m.ctx) == 1
+    @test m.arch === x
+    @test length(m.ctx) == 1
   end
 
   let x = @mx.var x
     m = mx.FeedForward(x, context = [mx.cpu(), mx.cpu(1)])
-    @assert m.arch === x
-    @assert length(m.ctx) == 2
+    @test m.arch === x
+    @test length(m.ctx) == 2
   end
 end
 
