c1a9b1080ecc9c6f3d61ba42ddeb215e8f79fad5
ZiyueHuang <zyhuang94@gmail.com>
2017-12-13 13:43:35 +0000

    fix resource

--- src/executor/attach_op_resource_pass.cc
+++ src/executor/attach_op_resource_pass.cc
@@ -38,7 +38,7 @@ Graph AttachOpResources(Graph g) {
   const auto& vdispatch = g.GetAttr<DispatchModeVector>("dispatch_mode");
   const auto& idx = g.indexed_graph();
   // Use global resource pool for each executor for now.
-  std::map<Context, Resource> cached_temp;
+  std::map<Context, std::vector<Resource> > cached_temp;
   // Resource allocation
   for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
     const auto& inode = idx[nid];
@@ -50,14 +50,21 @@ Graph AttachOpResources(Graph g) {
     if (fresource.count(op) != 0) {
       auto reqs = fresource[op](inode.source->attrs);
       // Get the resource of temporal space.
-      for (const ResourceRequest& req : reqs) {
+      for (size_t i=0; i < reqs.size(); i++) {
+        const ResourceRequest& req = reqs[i];
         if (req.type == ResourceRequest::kTempSpace) {
           if (cached_temp.count(ctx) != 0) {
-            requested.push_back(cached_temp.at(ctx));
+            if (i < cached_temp.at(ctx).size()) {
+              requested.push_back(cached_temp.at(ctx)[i]);
+            } else {
+              Resource r = ResourceManager::Get()->Request(ctx, req);
+              requested.push_back(r);
+              cached_temp[ctx].push_back(r);
+            }
           } else {
             Resource r = ResourceManager::Get()->Request(ctx, req);
             requested.push_back(r);
-            cached_temp[ctx] = r;
+            cached_temp[ctx] = std::vector<Resource>(1, r);
           }
         } else if (req.type == ResourceRequest::kRandom) {
           requested.push_back(ResourceManager::Get()->Request(ctx, req));
