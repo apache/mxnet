7c2b9646815be92bdda71619bb37aba85adaca5e
Sina Afrooze <sina.beh@gmail.com>
2017-12-14 18:11:26 -0800

    Fixes and improvements to example/nce-loss - No functional change
    * pylint conformity is significantly improved
    * executing with python 2.7 and 3.6 fixed
    * README.md improved to include better information on the dataset as well as example scripts
    * Added images used in README to local repo

--- example/nce-loss/wordvec.py
+++ example/nce-loss/wordvec.py
@@ -15,38 +15,41 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# pylint:skip-file
+# pylint: disable=invalid-name, missing-docstring, deprecated-module, len-as-condition
+# pylint: disable=too-many-instance-attributes
 from __future__ import print_function
+
 import logging
-import sys, random, time, math
-import mxnet as mx
-import numpy as np
-from collections import namedtuple
-from nce import *
-from operator import itemgetter
+import math
+import random
 from optparse import OptionParser
 
-def get_net(vocab_size, num_input, num_label):
+import mxnet as mx
+from nce import nce_loss, NceAuc
+
+
+def get_net(vocab_size, num_input, num_label_):
     data = mx.sym.Variable('data')
     label = mx.sym.Variable('label')
     label_weight = mx.sym.Variable('label_weight')
     embed_weight = mx.sym.Variable('embed_weight')
-    data_embed = mx.sym.Embedding(data = data, input_dim = vocab_size,
-                                  weight = embed_weight,
-                                  output_dim = 100, name = 'data_embed')
-    datavec = mx.sym.SliceChannel(data = data_embed,
-                                     num_outputs = num_input,
-                                     squeeze_axis = 1, name = 'data_slice')
+    data_embed = mx.sym.Embedding(data=data, input_dim=vocab_size,
+                                  weight=embed_weight,
+                                  output_dim=100, name='data_embed')
+    datavec = mx.sym.SliceChannel(data=data_embed,
+                                  num_outputs=num_input,
+                                  squeeze_axis=1, name='data_slice')
     pred = datavec[0]
     for i in range(1, num_input):
         pred = pred + datavec[i]
-    return nce_loss(data = pred,
-                    label = label,
-                    label_weight = label_weight,
-                    embed_weight = embed_weight,
-                    vocab_size = vocab_size,
-                    num_hidden = 100,
-                    num_label = num_label)
+    return nce_loss(data=pred,
+                    label=label,
+                    label_weight=label_weight,
+                    embed_weight=embed_weight,
+                    vocab_size=vocab_size,
+                    num_hidden=100,
+                    num_label=num_label_)
+
 
 def load_data(name):
     buf = open(name).read()
@@ -71,6 +74,7 @@ def load_data(name):
         negative += [i for _ in range(v)]
     return data, negative, vocab, freq
 
+
 class SimpleBatch(object):
     def __init__(self, data_names, data, label_names, label):
         self.data = data
@@ -88,34 +92,32 @@ class SimpleBatch(object):
 
 
 class DataIter(mx.io.DataIter):
-    def __init__(self, name, batch_size, num_label):
+    def __init__(self, name, batch_size_, num_label_):
         super(DataIter, self).__init__()
-        self.batch_size = batch_size
+        self.batch_size = batch_size_
         self.data, self.negative, self.vocab, self.freq = load_data(name)
         self.vocab_size = 1 + len(self.vocab)
-        print(self.vocab_size)
-        self.num_label = num_label
-        self.provide_data = [('data', (batch_size, num_label - 1))]
-        self.provide_label = [('label', (self.batch_size, num_label)),
-                              ('label_weight', (self.batch_size, num_label))]
+        print("Vocabulary Size: {}".format(self.vocab_size))
+        self.num_label = num_label_
+        self.provide_data = [('data', (batch_size_, num_label_ - 1))]
+        self.provide_label = [('label', (self.batch_size, num_label_)),
+                              ('label_weight', (self.batch_size, num_label_))]
 
     def sample_ne(self):
         return self.negative[random.randint(0, len(self.negative) - 1)]
 
     def __iter__(self):
-        print('begin')
         batch_data = []
         batch_label = []
         batch_label_weight = []
         start = random.randint(0, self.num_label - 1)
         for i in range(start, len(self.data) - self.num_label - start, self.num_label):
-            context = self.data[i: i + self.num_label / 2] \
-                      + self.data[i + 1 + self.num_label / 2: i + self.num_label]
-            target_word = self.data[i + self.num_label / 2]
+            context = self.data[i: i + self.num_label // 2] \
+                      + self.data[i + 1 + self.num_label // 2: i + self.num_label]
+            target_word = self.data[i + self.num_label // 2]
             if self.freq[target_word] < 5:
                 continue
-            target = [target_word] \
-                     + [self.sample_ne() for _ in range(self.num_label - 1)]
+            target = [target_word] + [self.sample_ne() for _ in range(self.num_label - 1)]
             target_weight = [1.0] + [0.0 for _ in range(self.num_label - 1)]
             batch_data.append(context)
             batch_label.append(target)
@@ -133,13 +135,16 @@ class DataIter(mx.io.DataIter):
     def reset(self):
         pass
 
+
 if __name__ == '__main__':
     head = '%(asctime)-15s %(message)s'
     logging.basicConfig(level=logging.DEBUG, format=head)
 
     parser = OptionParser()
-    parser.add_option("-g", "--gpu", action = "store_true", dest = "gpu", default = False,
-                      help = "use gpu")
+    parser.add_option("-g", "--gpu", action="store_true", dest="gpu", default=False,
+                      help="use gpu")
+    options, args = parser.parse_args()
+
     batch_size = 256
     num_label = 5
 
@@ -147,21 +152,25 @@ if __name__ == '__main__':
 
     network = get_net(data_train.vocab_size, num_label - 1, num_label)
 
-    options, args = parser.parse_args()
     devs = mx.cpu()
-    if options.gpu == True:
+    if options.gpu:
         devs = mx.gpu()
-    model = mx.model.FeedForward(ctx = devs,
-                                 symbol = network,
-                                 num_epoch = 20,
-                                 learning_rate = 0.3,
-                                 momentum = 0.9,
-                                 wd = 0.0000,
-                                 initializer=mx.init.Xavier(factor_type="in", magnitude=2.34))
 
+    model = mx.mod.Module(
+        symbol=network,
+        data_names=[x[0] for x in data_train.provide_data],
+        label_names=[y[0] for y in data_train.provide_label],
+        context=[devs]
+    )
 
+    print("Training on {}".format("GPU" if options.gpu else "CPU"))
     metric = NceAuc()
-    model.fit(X = data_train,
-              eval_metric = metric,
-              batch_end_callback = mx.callback.Speedometer(batch_size, 50),)
-
+    model.fit(
+        train_data=data_train,
+        num_epoch=20,
+        optimizer='sgd',
+        optimizer_params={'learning_rate': 0.3, 'momentum': 0.9, 'wd': 0.0000},
+        initializer=mx.init.Xavier(factor_type='in', magnitude=2.34),
+        eval_metric=metric,
+        batch_end_callback=mx.callback.Speedometer(batch_size, 50)
+    )
