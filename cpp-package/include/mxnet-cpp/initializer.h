0dfa39b0e9115b34fd87b31711c05c09b5a1d233
nihui <shuizhuyuanluo@126.com>
2017-12-18 18:52:47 +0800

    change indent

--- cpp-package/include/mxnet-cpp/initializer.h
+++ cpp-package/include/mxnet-cpp/initializer.h
@@ -196,9 +196,9 @@ class Xavier : public Initializer {
 };
 
 class MSRAPrelu : public Xavier {
-  public:
-    MSRAPrelu(FactorType factor_type = avg, float slope = 0.25f)
-        : Xavier(gaussian, factor_type, 2. / (1 + slope * slope)) {}
+ public:
+  MSRAPrelu(FactorType factor_type = avg, float slope = 0.25f)
+      : Xavier(gaussian, factor_type, 2. / (1 + slope * slope)) {}
 };
 
 }  // namespace cpp
