66ae47da3345d75d8c1f728b357008fbbc76632d
ZiyueHuang <zyhuang94@gmail.com>
2017-12-20 16:53:10 +0000

    trigger windows build

--- src/kvstore/utils.cu
+++ src/kvstore/utils.cu
@@ -40,6 +40,50 @@
 namespace mxnet {
 namespace kvstore {
 
+
+template<typename IType>
+size_t UniqueImplGPU(const Resource& rsc, mshadow::Stream<gpu> *s,
+                     IType *dptr, nnvm::dim_t size) {
+#ifndef SORT_WITH_THRUST
+  size_t sort_temp_bytes = 0;
+  cub::DeviceRadixSort::SortKeys(NULL, sort_temp_bytes,
+    dptr, dptr, size, 0, sizeof(IType)*8, mshadow::Stream<gpu>::GetStream(s));
+  mshadow::Tensor<gpu, 1, char> sort_space = rsc
+    .get_space_typed<gpu, 1, char>(
+      mshadow::Shape1(sort_temp_bytes), s);
+  void *sort_temp_storage = static_cast<void*>(sort_space.dptr_);
+  cub::DeviceRadixSort::SortKeys(sort_temp_storage, sort_temp_bytes,
+    dptr, dptr, size, 0, sizeof(IType)*8, mshadow::Stream<gpu>::GetStream(s));
+#else
+  thrust::sort(thrust::cuda::par.on(mshadow::Stream<gpu>::GetStream(s)),
+    dptr, dptr + size, thrust::greater<IType>());
+#endif
+  size_t unique_temp_bytes = 0;
+  mshadow::Tensor<gpu, 1, char> dummy_space = rsc
+    .get_space_typed<gpu, 1, char>(
+      mshadow::Shape1(sizeof(size_t)), s);
+  size_t *dummy_ptr = reinterpret_cast<size_t*>(dummy_space.dptr_);
+  cub::DeviceSelect::Unique(NULL, unique_temp_bytes, dptr, dptr,
+    dummy_ptr, size, mshadow::Stream<gpu>::GetStream(s));
+
+  mshadow::Tensor<gpu, 1, char> unique_space = rsc
+    .get_space_typed<gpu, 1, char>(
+      mshadow::Shape1((unique_temp_bytes + sizeof(size_t) + 7) / 8 * 8), s);
+
+  void *unique_temp_storage = static_cast<void*>(
+    unique_space.dptr_);
+  size_t *d_num_selected_out = reinterpret_cast<size_t*>(
+    unique_space.dptr_ + (unique_temp_bytes + 7) / 8 * 8);
+
+  cub::DeviceSelect::Unique(unique_temp_storage, unique_temp_bytes, dptr, dptr,
+    d_num_selected_out, size, mshadow::Stream<gpu>::GetStream(s));
+
+  size_t num_selected_out = 0;
+  CUDA_CALL(cudaMemcpy(&num_selected_out, d_num_selected_out, sizeof(size_t),
+     cudaMemcpyDeviceToHost));
+  return num_selected_out;
+}
+
 /*!
  * \brief sort and get unique values.
  */
@@ -48,44 +92,7 @@ void UniqueImpl<gpu>(const Resource& rsc, mshadow::Stream<gpu> *s,
                      NDArray *out, nnvm::dim_t size) {
   MSHADOW_IDX_TYPE_SWITCH(out->data().type_flag_, IType, {
     IType *dptr = out->data().dptr<IType>();
-#ifndef SORT_WITH_THRUST
-    size_t sort_temp_bytes = 0;
-    cub::DeviceRadixSort::SortKeys(NULL, sort_temp_bytes,
-      dptr, dptr, size, 0, sizeof(IType)*8, mshadow::Stream<gpu>::GetStream(s));
-    mshadow::Tensor<gpu, 1, char> sort_space = rsc
-      .get_space_typed<gpu, 1, char>(
-        mshadow::Shape1(sort_temp_bytes), s);
-    void *sort_temp_storage = static_cast<void*>(sort_space.dptr_);
-    cub::DeviceRadixSort::SortKeys(sort_temp_storage, sort_temp_bytes,
-      dptr, dptr, size, 0, sizeof(IType)*8, mshadow::Stream<gpu>::GetStream(s));
-#else
-    thrust::sort(thrust::cuda::par.on(mshadow::Stream<gpu>::GetStream(s)),
-      dptr, dptr + size, thrust::greater<IType>());
-#endif
-    size_t unique_temp_bytes = 0;
-    mshadow::Tensor<gpu, 1, char> dummy_space = rsc
-      .get_space_typed<gpu, 1, char>(
-        mshadow::Shape1(sizeof(size_t)), s);
-    size_t *dummy_ptr = reinterpret_cast<size_t*>(dummy_space.dptr_);
-    cub::DeviceSelect::Unique(NULL, unique_temp_bytes, dptr, dptr,
-      dummy_ptr, size, mshadow::Stream<gpu>::GetStream(s));
-
-    mshadow::Tensor<gpu, 1, char> unique_space = rsc
-      .get_space_typed<gpu, 1, char>(
-        mshadow::Shape1((unique_temp_bytes + sizeof(size_t) + 7) / 8 * 8), s);
-
-    void *unique_temp_storage = static_cast<void*>(
-      unique_space.dptr_);
-    size_t *d_num_selected_out = reinterpret_cast<size_t*>(
-     unique_space.dptr_ + (unique_temp_bytes + 7) / 8 * 8);
-
-    cub::DeviceSelect::Unique(unique_temp_storage, unique_temp_bytes, dptr, dptr,
-      d_num_selected_out, size, mshadow::Stream<gpu>::GetStream(s));
-
-    size_t num_selected_out = 0;
-    CUDA_CALL(cudaMemcpy(&num_selected_out, d_num_selected_out, sizeof(size_t),
-       cudaMemcpyDeviceToHost));
-
+    size_t num_selected_out = UniqueImplGPU(rsc, s, dptr, size);
     *out = out->Reshape(mshadow::Shape1(num_selected_out));
   });
 }
