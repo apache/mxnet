7dbe2145f151d2e927d8326815c0eb4107e40b2f
Jin Huang <jin1.huang@intel.com>
2017-12-20 10:11:41 +0800

    prepare_mkl.sh issue fix and add test case

---
+++ tests/python/cpu/test_mklml.py
@@ -0,0 +1,43 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# This script will check if MXNet is built/installed correctly 
+# when compiling with Intel MKLML library
+import logging
+import os
+
+def test_mklml_install():
+    logging.basicConfig(level=logging.INFO)
+    try:
+        import mxnet as mx
+    except (ImportError, OSError) as e:
+        assert 0, "Import mxnet error: %s. Please double check your build/" \
+               "install steps or environment variable settings" % str(e)
+    
+    pid = os.getpid()
+    rc = os.system("cat /proc/" + str(pid) + 
+                       "/maps | grep libmklml_ > /dev/null")
+
+    if rc == 0:
+        logging.info("MXNet is built/installed correctly with MKLML")
+    else:
+        assert 0, "MXNet is built/installed incorrectly with MKLML, please " \
+               "double check your build/install steps or environment " \
+               "variable settings"
+
+if __name__ == '__main__':
+    test_mklml_install()
