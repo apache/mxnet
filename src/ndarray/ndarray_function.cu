33cae331983b2e82e9dad0299004bfb9c723addf
hecli <hecli@microsoft.com>
2018-01-11 00:43:13 -0800

    fix LNK error in test for Windows GPU build

--- src/ndarray/ndarray_function.cu
+++ src/ndarray/ndarray_function.cu
@@ -36,65 +36,6 @@
 
 namespace mxnet {
 namespace ndarray {
-template<>
-void Copy<cpu, gpu>(const TBlob &from, TBlob *to,
-                    Context from_ctx, Context to_ctx,
-                    RunContext ctx) {
-  CHECK_EQ(to->type_flag_, from.type_flag_)
-    << "Source and target must have the same data type when copying across devices.";
-  MSHADOW_TYPE_SWITCH(to->type_flag_, DType, {
-    mshadow::Copy(to->FlatTo1D<gpu, DType>(),
-                  from.FlatTo1D<cpu, DType>(),
-                  ctx.get_stream<gpu>());
-  });
-}
-
-template<>
-void Copy<gpu, cpu>(const TBlob &from, TBlob *to,
-                    Context from_ctx, Context to_ctx,
-                    RunContext ctx) {
-  CHECK_EQ(to->type_flag_, from.type_flag_)
-    << "Source and target must have the same data type when copying across devices.";
-  MSHADOW_TYPE_SWITCH(to->type_flag_, DType, {
-    mshadow::Copy(to->FlatTo1D<cpu, DType>(),
-                  from.FlatTo1D<gpu, DType>(),
-                  ctx.get_stream<gpu>());
-  });
-}
-
-template<>
-void Copy<gpu, gpu>(const TBlob &from, TBlob *to,
-                    Context from_ctx, Context to_ctx,
-                    RunContext ctx) {
-  if (from_ctx.dev_id == to_ctx.dev_id) {
-    mshadow::Stream<gpu>* s = ctx.get_stream<gpu>();
-    MSHADOW_TYPE_SWITCH(to->type_flag_, DType, {
-      if (to->type_flag_ == from.type_flag_) {
-        mshadow::Copy(to->FlatTo1D<gpu, DType>(s),
-                      from.FlatTo1D<gpu, DType>(s),
-                      s);
-      } else {
-        MSHADOW_TYPE_SWITCH(from.type_flag_, SrcDType, {
-          to->FlatTo1D<gpu, DType>(s) =
-            mshadow::expr::tcast<DType>(from.FlatTo1D<gpu, SrcDType>(s));
-        })
-      }
-    })
-  } else {
-    CHECK(from.CheckContiguous() && to->CheckContiguous())
-      << "copy across only support continugous memory";
-    CHECK_EQ(to->type_flag_, from.type_flag_)
-      << "Source and target must have the same data type when copying across devices.";
-    mshadow::Stream<gpu> *s = ctx.get_stream<gpu>();
-    CHECK(s != NULL) << "need stream in GPU context";
-    cudaMemcpyPeerAsync(to->dptr_,
-                        to_ctx.dev_id,
-                        from.dptr_,
-                        from_ctx.dev_id,
-                        from.shape_.Size() * mshadow::mshadow_sizeof(to->type_flag_),
-                        s->stream_);
-  }
-}
 
 /*!
  * \brief GPU impl of elemwise sum for rowsparse tensors.
