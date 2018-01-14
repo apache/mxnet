9649fc6e1863259bc4b402fb1125831f7015255a
Da Zheng <zhengda1936@gmail.com>
2018-01-14 17:28:15 +0000

    Fix a compilation error.

--- src/operator/nn/mkldnn/mkldnn_ops-inl.h
+++ src/operator/nn/mkldnn/mkldnn_ops-inl.h
@@ -77,6 +77,13 @@ void MKLDNNCopy(const nnvm::NodeAttrs& attrs, const OpContext &ctx,
                 const NDArray &in_data, const OpReqType &req,
                 const NDArray &out_data);
 
+void MKLDNNConcat_Forward(const nnvm::NodeAttrs& attrs, const OpContext &ctx,
+    const std::vector<NDArray> &in_data, const std::vector<OpReqType> &req,
+    const std::vector<NDArray> &out_data);
+void MKLDNNConcat_Backward(const nnvm::NodeAttrs& attrs, const OpContext &ctx,
+    const std::vector<NDArray>& inputs, const std::vector<OpReqType>& req,
+    const std::vector<NDArray>& outputs);
+
 }  // namespace op
 }  // namespace mxnet
 #endif  // MXNET_USE_MKLDNN == 1
