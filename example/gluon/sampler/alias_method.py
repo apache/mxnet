81d09e5074d6299381f52dca2838d3574d04dbc5
zihaolucky <zihaolucky@gmail.com>
2018-01-16 17:10:50 +0800

    * Add speed comparison between Numpy as NDArray.  * Add chi-square test for distribution generator.

--- example/gluon/sampler/alias_method.py
+++ example/gluon/sampler/alias_method.py
@@ -17,30 +17,138 @@
 
 # coding: utf-8
 # pylint: skip-file
-
+import mxnet.ndarray as nd
 import numpy as np
-import numpy.random as npr
-from collections import Counter
+from mxnet.test_utils import verify_generator
 
-import mxnet.gluon as gluon
 
-K = 5
-N = 1000
+class AliasMethodSampler(object):
+    """ The Alias Method: Efficient Sampling with Many Discrete Outcomes.
+    Can be use in NCELoss.
 
-# Get a random probability vector.
-probs = npr.dirichlet(np.ones(K), 1).ravel()
+    Parameters
+    ----------
+    K : int
+        Number of events.
+    probs : array
+        Probability of each events, corresponds to K.
 
-# Construct the table.
-alias_method_sampler = gluon.data.AliasMethodSampler(K, probs)
+    References
+    -----------
+        https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+    """
 
-# Generate variates.
-X = alias_method_sampler.draw(N).asnumpy()
-c = Counter(X)
+    def __init__(self, F, K, probs):
+        if K != len(probs):
+            raise ValueError("K should be equal to len(probs). K:%d, len(probs):%d" % (K, len(probs)))
+        self.K = K
+        self.prob = F.zeros(K)
+        self.alias = F.zeros(K).astype('int32')
 
-# check sampled probabilities
-sampled_probs = [float(x)/N for x in c.values()]
+        # Sort the data into the outcomes with probabilities
+        # that are larger and smaller than 1/K.
+        smaller = []
+        larger = []
+        for kk, prob in enumerate(probs):
+            self.prob[kk] = K * prob
+            if self.prob[kk] < 1.0:
+                smaller.append(kk)
+            else:
+                larger.append(kk)
 
-print(probs)
-print(sampled_probs)
+        # Loop though and create little binary mixtures that
+        # appropriately allocate the larger outcomes over the
+        # overall uniform mixture.
+        while len(smaller) > 0 and len(larger) > 0:
+            small = smaller.pop()
+            large = larger.pop()
 
+            self.alias[small] = large
+            self.prob[large] = (self.prob[large] - 1.0) + self.prob[small]
+
+            if self.prob[large] < 1.0:
+                smaller.append(large)
+            else:
+                larger.append(large)
+
+        for last_one in smaller + larger:
+            self.prob[last_one] = 1
+
+    def draw(self, F, n):
+        """Draw N samples from multinomial
+        """
+        samples = F.zeros(n, dtype='int32')
+
+        kk = F.floor(F.random.uniform(0, self.K, n)).astype('int32')
+        rand = F.random.uniform(0, 1, n)
+
+        prob = self.prob[kk]
+        alias = self.alias[kk]
+
+        for i in xrange(n):
+            if rand[i] < prob[i]:
+                samples[i] = kk[i]
+            else:
+                samples[i] = alias[i]
+        return samples
+
+
+def speed():
+    # use numpy
+    import time
+    import numpy.random as npr
+
+    K = 500
+    N = 10000
+
+    # Get a random probability vector.
+    probs = npr.dirichlet(np.ones(K), 1).ravel()
+
+    # Construct the table.
+    elaps = []
+    F = nd
+    alias_method_sampler = AliasMethodSampler(F, K, probs)
+    for i in range(100):
+        start = time.time()
+        X = alias_method_sampler.draw(F, N)
+        time_elaps = time.time() - start
+        elaps.append(time_elaps)
+    print(
+        'Use NDArray. Avg:%.2f ms, Min:%.2f ms, Max:%.2f ms, Std:%.2f ms'
+        % (np.average(elaps), np.min(elaps), np.max(elaps), np.std(elaps)))
+
+    # Construct the table.
+    elaps = []
+    F = nd
+    alias_method_sampler = AliasMethodSampler(F, K, probs)
+    for i in range(100):
+        start = time.time()
+        X = alias_method_sampler.draw(F, N)
+        time_elaps = time.time() - start
+        elaps.append(time_elaps)
+    print(
+        'Use Numpy. Avg:%.2f ms, Min:%.2f ms, Max:%.2f ms, Std:%.2f ms'
+        % (np.average(elaps), np.min(elaps), np.max(elaps), np.std(elaps)))
+
+
+def chi_square_test():
+    probs = [0.1, 0.2, 0.3, 0.05, 0.15, 0.2]
+    buckets = list(range(6))
+
+    F = np
+    alias_method_sampler = AliasMethodSampler(F, len(probs), probs)
+
+    generator_mx = lambda x: alias_method_sampler.draw(F, x)
+    verify_generator(generator_mx, buckets, probs)
+
+    generator_mx_same_seed = \
+        lambda x: np.concatenate(
+            [alias_method_sampler.draw(F, x // 10) for _ in range(10)]
+        )
+    verify_generator(generator=generator_mx_same_seed, buckets=buckets, probs=probs)
+
+
+if __name__ == '__main__':
+    speed()
+    chi_square_test()
 
