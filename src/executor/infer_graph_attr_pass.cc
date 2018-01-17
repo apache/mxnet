36f2e9fd4a938cebf39f21a0e4abb2a6d4d557b5
eric-haibin-lin <linhaibin.eric@gmail.com>
2018-01-17 05:07:04 +0000

    fix bug. support imperative

--- src/executor/infer_graph_attr_pass.cc
+++ src/executor/infer_graph_attr_pass.cc
@@ -50,7 +50,7 @@ bool ApplyOpInferAttr<int, FInferStorageType>(const nnvm::Graph& g,
                                               std::vector<int>* out_attrs,
                                               DispatchMode* dispatch_mode) {
   const DevMaskVector& dev_masks = g.GetAttr<DevMaskVector>("dev_mask");
-  static bool success = finfer(attrs, dev_masks[nid], dispatch_mode, in_attrs, out_attrs);
+  const bool success = finfer(attrs, dev_masks[nid], dispatch_mode, in_attrs, out_attrs);
   if (!success) {
     LOG(FATAL) << "Operator not implemented: "
                << common::operator_stype_string(attrs, dev_masks[nid], *in_attrs, *out_attrs);
@@ -58,7 +58,7 @@ bool ApplyOpInferAttr<int, FInferStorageType>(const nnvm::Graph& g,
   if (*dispatch_mode == DispatchMode::kFComputeFallback) {
     common::LogStorageFallback(attrs, dev_masks[nid], in_attrs, out_attrs);
   }
-  return success;
+  return true;
 }
 
 /*!\brief
