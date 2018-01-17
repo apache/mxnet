ad2ea13f855f318ae1dad056a0180546cb8f1e3f
mbaijal <30911248+mbaijal@users.noreply.github.com>
2018-01-16 18:08:51 -0800

    Remove ASF header from nms_kernel.cu

--- tools/license_header.py
+++ tools/license_header.py
@@ -73,6 +73,7 @@ _WHITE_LIST = ['R-package/',
                'src/operator/nn/im2col.cuh',
                'example/ssd/dataset/pycocotools/coco.py',
                'example/rcnn/rcnn/cython/setup.py',
+               'example/rcnn/rcnn/cython/nms_kernel.cu',
                'example/image-classification/predict-cpp/image-classification-predict.cc',
                'src/operator/contrib/ctc_include/']
 
