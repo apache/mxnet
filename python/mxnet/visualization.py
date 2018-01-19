56615e84671679785b5828a127ccee158aa6cccd
chinakook <chinakook@msn.com>
2018-01-19 15:00:46 +0800

    fix print_summary bug and add groups of convolution
    1. fix "int(node["attrs"]["no_bias"])" bug
    2. add groups of convolution param calculation

--- python/mxnet/visualization.py
+++ python/mxnet/visualization.py
@@ -134,17 +134,17 @@ def print_summary(symbol, shape=None, line_length=120, positions=[.44, .64, .74,
                             pre_filter = pre_filter + int(shape[0])
         cur_param = 0
         if op == 'Convolution':
-            if ("no_bias" in node["attrs"]) and int(node["attrs"]["no_bias"]):
-                cur_param = pre_filter * int(node["attrs"]["num_filter"])
+            if ("no_bias" in node["attrs"]) and node["attrs"]["no_bias"] == 'True':
+                cur_param = (pre_filter * int(node["attrs"]["num_filter"])) // int(node["attrs"]["num_group"])
                 for k in _str2tuple(node["attrs"]["kernel"]):
                     cur_param *= int(k)
             else:
-                cur_param = pre_filter * int(node["attrs"]["num_filter"])
+                cur_param = (pre_filter * int(node["attrs"]["num_filter"])) // int(node["attrs"]["num_group"])
                 for k in _str2tuple(node["attrs"]["kernel"]):
                     cur_param *= int(k)
                 cur_param += int(node["attrs"]["num_filter"])
         elif op == 'FullyConnected':
-            if ("no_bias" in node["attrs"]) and int(node["attrs"]["no_bias"]):
+            if ("no_bias" in node["attrs"]) and node["attrs"]["no_bias"] == 'True':
                 cur_param = pre_filter * (int(node["attrs"]["num_hidden"]))
             else:
                 cur_param = (pre_filter+1) * (int(node["attrs"]["num_hidden"]))
