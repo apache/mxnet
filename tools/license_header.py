03195881908cba49e4d224bda80bc71cad71bd6f
mbaijal <30911248+mbaijal@users.noreply.github.com>
2018-01-21 19:43:46 -0800

    Adding Apache Header to .m, .cfg, R and .mk files (#9499)

--- tools/license_header.py
+++ tools/license_header.py
@@ -82,7 +82,7 @@ _WHITE_LIST = ['R-package/',
 _LANGS = {'.cc':'*', '.h':'*', '.cu':'*', '.cuh':'*', '.py':'#',
           '.pm':'#', '.scala':'*', '.cc':'*', '.sh':'#', '.cmake':'#',
           '.java':'*', '.sh':'#', '.cpp':'*', '.hpp':'*', '.c':'*',
-          '.bat':'rem', '.pl':'#'}
+          '.bat':'rem', '.pl':'#', '.m':'%', '.R':'#', '.mk':'#', '.cfg':'#'}
 
 # Previous license header, which will be removed
 _OLD_LICENSE = re.compile('.*Copyright.*by Contributors')
