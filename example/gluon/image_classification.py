7fbdad98775662875b094cb75090819f7d8bce5a
Joshua Zhang <cheungchih@gmail.com>
2018-01-26 12:19:29 -0600

    backup

--- example/gluon/image_classification.py
+++ example/gluon/image_classification.py
@@ -27,6 +27,7 @@ from mxnet.gluon import nn
 from mxnet.gluon.model_zoo import vision as models
 from mxnet import autograd as ag
 from mxnet.test_utils import get_mnist_iterator
+from mxnet.metric import Accuracy, TopKAccuracy, CompositeEvalMetric
 import numpy as np
 
 from data import *
@@ -84,6 +85,7 @@ parser.add_argument('--log-interval', type=int, default=50,
 parser.add_argument('--profile', action='store_true',
                     help='Option to turn on memory profiling for front-end, '\
                          'and prints out the memory usage by python function at the end.')
+parser.add_argument('--top-k', type=int, default=0, help='add top-k metric if > 1')
 opt = parser.parse_args()
 
 logging.info(opt)
@@ -98,30 +100,27 @@ context = [mx.gpu(int(i)) for i in opt.gpus.split(',')] if opt.gpus.strip() else
 num_gpus = len(context)
 
 batch_size *= max(1, num_gpus)
-context = [mx.gpu(i) for i in range(num_gpus)] if num_gpus > 0 else [mx.cpu()]
 
-model_name = opt.model
-
-kwargs = {'ctx': context, 'pretrained': opt.use_pretrained, 'classes': classes}
-if model_name.startswith('resnet'):
-    kwargs['thumbnail'] = opt.use_thumbnail
-elif model_name.startswith('vgg'):
-    kwargs['batch_norm'] = opt.batch_norm
-
-net = models.get_model(opt.model, **kwargs)
-
-def get_model(model, resume, pretrained, dtype='float32'):
+def get_model(model, ctx, opt):
     """Model initialization."""
-    net = gluon.model_zoo.vision.get_model(model, pretrained=pretrained, classes=1000)
-    if resume:
-        net.load_params(resume)
-    elif not pretrained:
+    kwargs = {'ctx': ctx, 'pretrained': opt.use_pretrained, 'classes': classes}
+    if model.startswith('resnet'):
+        kwargs['thumbnail'] = opt.use_thumbnail
+    elif model.startswith('vgg'):
+        kwargs['batch_norm'] = opt.batch_norm
+
+    net = models.get_model(model, ctx, **kwargs)
+    if opt.resume:
+        net.load_params(opt.resume)
+    elif not opt.use_pretrained:
         if model in ['alexnet']:
             net.initialize(mx.init.Normal())
         else:
             net.initialize(mx.init.Xavier(magnitude=2))
-    net.cast(dtype)
+    net.cast(opt.dtype)
     return net
+
+net = get_model(opt.model, context, opt)
 
 def get_data_iters(dataset, batch_size, num_workers=1, rank=0):
     """get dataset iterators"""
@@ -133,13 +132,9 @@ def get_data_iters(dataset, batch_size, num_workers=1, rank=0):
                                                     num_parts=num_workers, part_index=rank)
     elif dataset == 'imagenet':
         if model_name == 'inceptionv3':
-            train_data, val_data = get_imagenet_iterator(opt.train_data, opt.val_data,
-                                                         batch_size, (3, 299, 299),
-                                                         num_parts=num_workers, part_index=rank)
+            train_data, val_data = get_imagenet_iterator(opt.data, batch_size, opt.num_workers, 299, opt.dtype)
         else:
-            train_data, val_data = get_imagenet_iterator(opt.train_data, opt.val_data,
-                                                         batch_size, (3, 224, 224),
-                                                         num_parts=num_workers, part_index=rank)
+            train_data, val_data = get_imagenet_iterator(opt.data, batch_size, opt.num_workers, 224, opt.dtype)
     elif dataset == 'dummy':
         if model_name == 'inceptionv3':
             train_data, val_data = dummy_iterator(batch_size, (3, 299, 299))
@@ -147,8 +142,7 @@ def get_data_iters(dataset, batch_size, num_workers=1, rank=0):
             train_data, val_data = dummy_iterator(batch_size, (3, 224, 224))
     return train_data, val_data
 
-def test(ctx, val_data):
-    metric = mx.metric.Accuracy()
+def test(ctx, metric, val_data):
     val_data.reset()
     for batch in val_data:
         data = gluon.utils.split_and_load(batch.data[0], ctx_list=ctx, batch_axis=0)
@@ -163,13 +157,12 @@ def test(ctx, val_data):
 def train(epochs, ctx):
     if isinstance(ctx, mx.Context):
         ctx = [ctx]
-    net.initialize(mx.init.Xavier(magnitude=2), ctx=ctx)
     kv = mx.kv.create(opt.kvstore)
     train_data, val_data = get_data_iters(dataset, batch_size, kv.num_workers, kv.rank)
     trainer = gluon.Trainer(net.collect_params(), 'sgd',
                             {'learning_rate': opt.lr, 'wd': opt.wd, 'momentum': opt.momentum},
                             kvstore = kv)
-    metric = mx.metric.Accuracy()
+    metric = CompositeEvalMetric([Accuracy(), TopKAccuracy(opt.top_k)]) if opt.top_k > 1 else Accuracy()
     loss = gluon.loss.SoftmaxCrossEntropyLoss()
 
     for epoch in range(epochs):
@@ -190,8 +183,7 @@ def train(epochs, ctx):
                     # on all GPUs for better speed on multiple GPUs.
                     Ls.append(L)
                     outputs.append(z)
-                for L in Ls:
-                    L.backward()
+                ag.backward(Ls)
             trainer.step(batch.data[0].shape[0])
             metric.update(label, outputs)
             if opt.log_interval and not (i+1)%opt.log_interval:
@@ -203,7 +195,7 @@ def train(epochs, ctx):
         name, acc = metric.get()
         logging.info('[Epoch %d] training: %s=%f'%(epoch, name, acc))
         logging.info('[Epoch %d] time cost: %f'%(epoch, time.time()-tic))
-        name, val_acc = test(ctx, val_data)
+        name, val_acc = test(ctx, metric, val_data)
         logging.info('[Epoch %d] validation: %s=%f'%(epoch, name, val_acc))
 
     net.save_params('image-classifier-%s-%d.params'%(opt.model, epochs))
