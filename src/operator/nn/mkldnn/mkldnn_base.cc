f16fdfd95b51a770050b33144c9259adfc9c246b
Da Zheng <zhengda1936@gmail.com>
2018-01-26 20:07:23 +0000

    Implement align myself.

--- src/operator/nn/mkldnn/mkldnn_base.cc
+++ src/operator/nn/mkldnn/mkldnn_base.cc
@@ -24,19 +24,36 @@
 
 namespace mxnet {
 
+void *AlignMem(void *mem, size_t size, size_t alignment, size_t *space) {
+  if (size > *space)
+    return nullptr;
+  intptr_t addr = reinterpret_cast<intptr_t>(mem);
+  // If the address has been aligned, don't do anything.
+  intptr_t last_chunk = addr % alignment;
+  if (last_chunk == 0)
+    return mem;
+  intptr_t padding = alignment - last_chunk;
+  // If the buffer doesn't have enough space, we should return null here.
+  if (padding + size > *space)
+    return nullptr;
+  addr += padding;
+  *space -= padding;
+  CHECK_EQ(addr % alignment, 0);
+  return reinterpret_cast<void *>(addr);
+}
+
 mkldnn::memory *TmpMemMgr::Alloc(const mkldnn::memory::primitive_desc &pd) {
   // We need to include the size of the memory used for alignment.
   this->est_size += pd.get_size() + alignment;
-  void *this_mem = this->curr_mem;
-  void *mem = std::align(alignment, pd.get_size(), this_mem, this->curr_size);
+  void *mem = AlignMem(this->curr_mem, pd.get_size(), alignment, &this->curr_size);
   if (mem) {
     // The memory is allocated from the temporary memory space in the
     // operator. It'll only become invalid after we exit from the operator.
-    mkldnn_mem_ptr ret(new mkldnn::memory(pd, this_mem));
+    mkldnn_mem_ptr ret(new mkldnn::memory(pd, mem));
     MKLDNNStream::Get()->RegisterMem(ret);
-    CHECK_EQ(this_mem, mem);
+    CHECK_EQ(mem, mem);
     this->curr_size -= pd.get_size();
-    this->curr_mem = static_cast<char *>(this_mem) + pd.get_size();
+    this->curr_mem = static_cast<char *>(mem) + pd.get_size();
     return ret.get();
   } else {
     LOG(WARNING) << "Allocate " << pd.get_size()
---
+++ tests/cpp/operator/mkldnn.cc
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ *  \file mkldnn.cc
+ *  \brief test functions in mkldnn.
+ *  \author Da Zheng
+ */
+
+#include "gtest/gtest.h"
+#include "operator/nn/mkldnn/mkldnn_base-inl.h"
+
+bool test_mem_align(void *mem, size_t size, size_t alignment, size_t space) {
+  void *ret1, *ret2;
+  size_t space1, space2;
+  space1 = space;
+  space2 = space;
+  ret1 = mxnet::AlignMem(mem, size, alignment, &space1);
+  ret2 = std::align(alignment, size, mem, space2);
+  printf("%p, %p\n", ret1, ret2);
+  EXPECT_EQ(ret1, ret2);
+  EXPECT_EQ(space1, space2);
+  return ret1 == ret2;
+}
+
+TEST(MKLDNN_UTIL_FUNC, AlignMem) {
+  size_t alignment = 4096;
+  void *mem;
+  size_t size, space;
+
+  // When mem has been aligned.
+  printf("AlignMem test1\n");
+  mem = reinterpret_cast<void *>(0x10000);
+  size = 1000;
+  space = 10000;
+  test_mem_align(mem, size, alignment, space);
+
+  // When mem isn't aligned and we have enough space for alignment.
+  printf("AlignMem test2\n");
+  mem = reinterpret_cast<void *>(0x10010);
+  size = 1000;
+  space = 10000;
+  test_mem_align(mem, size, alignment, space);
+
+  // When mem isn't aligned and we don't have enough memory for alignment
+  printf("AlignMem test3\n");
+  mem = reinterpret_cast<void *>(0x10010);
+  size = 1000;
+  space = 1001;
+  test_mem_align(mem, size, alignment, space);
+
+  printf("AlignMem test4\n");
+  for (size_t i = 0; i < 10000; i++) {
+    mem = reinterpret_cast<void *>(random());
+    size = random() % 2000;
+    space = random() % 2000;
+    printf("mem: %p, size: %ld, space: %ld\n", mem, size, space);
+    bool ret = test_mem_align(mem, size, alignment, space);
+    if (!ret)
+      break;
+  }
+}
