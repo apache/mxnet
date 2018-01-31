4b1d5b4238c27a6fdece645b01be268b8858a2e5
Pedro Larroy <pllarroy@amazon.com>
2018-01-31 14:22:01 +0100

    Add set -x and set -e to install scripts to fail on exec and show commands

--- tests/ci_build/install/install_testdeps.sh
+++ tests/ci_build/install/install_testdeps.sh
@@ -16,8 +16,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 set -e
+set -x
 
 pip install cpplint 'pylint==1.4.4' 'astroid==1.3.6'
 
