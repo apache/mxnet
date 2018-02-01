60577d54b32643e96062b4c21909bd723525eb6d
ZiyueHuang <zyhuang94@gmail.com>
2018-02-01 00:03:58 +0000

    fix lint

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -27,8 +27,6 @@ from .ndarray import (NDArray, zeros, clip, sqrt, cast, maximum, abs as NDabs)
 from .ndarray import (sgd_update, sgd_mom_update, adam_update, rmsprop_update, rmspropalex_update,
                       mp_sgd_update, mp_sgd_mom_update, square, ftrl_update, ftml_update,
                       signsgd_update, signum_update)
-from .ndarray import _internal
-from .ndarray import op
 from .ndarray import sparse
 from .random import normal
 
