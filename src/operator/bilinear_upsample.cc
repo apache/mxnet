99e824fcf5246b3f1ed6737138d217d9f4750ec6
Zhang <zhang.hang@rutgers.edu>
2018-02-02 18:05:45 -0800

    bilinear upsample from PYTorch

---
+++ src/operator/bilinear_upsample.cc
@@ -0,0 +1,185 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*!
+ * Copyright (c) 2018 by Contributors
+ * \file bilinear_upsample.cc
+ * \brief bilinear upsample operator
+ * \author Hang Zhang
+ * Adapted from PyTorch
+*/
+#include "devicetensor.h"
+#include "bilinear_upsample-inl.h"
+#include "elemwise_op_common.h"
+
+namespace mxnet {
+namespace op {
+
+
+template<typename xpu, typename DType, typename AccReal>
+void SpatialUpSamplingBilinearUpdateOutput(mshadow::Stream<cpu> *s,
+                                           const std::vector<TBlob> &input,
+                                           const std::vector<TBlob> &output) {
+  DeviceTensor<DType, 4> itensor = devicetensor<DType, 4>(input[0]);
+  DeviceTensor<DType, 4> otensor = devicetensor<DType, 4>(output[0]);
+  int nbatch = otensor.getSize(0);
+  int channels = otensor.getSize(1);
+  int outputHeight = otensor.getSize(2);
+  int outputWidth = otensor.getSize(3);
+  int inputHeight = itensor.getSize(2);
+  int inputWidth = itensor.getSize(3);
+
+  DType *idata = itensor.data_ptr();
+  DType *odata = otensor.data_ptr();
+  channels = nbatch * channels;
+  // special case: just copy
+  if (inputHeight == outputHeight && inputWidth == outputWidth) {
+    for (int h2 = 0; h2 < outputHeight; ++h2) {
+      const int h1 = h2;
+      for (int w2 = 0; w2 < outputWidth; ++w2) {
+        const int w1 = w2;
+        const DType* pos1 = &idata[h1 * inputWidth + w1];
+        DType* pos2 = &odata[h2 * outputWidth + w2];
+        for (int c = 0; c < channels; ++c) {
+          pos2[0] = pos1[0];
+          pos1 += inputWidth * inputHeight;
+          pos2 += outputWidth * outputHeight;
+        }
+      }
+    }
+    return;
+  }
+  const float rheight =(outputHeight > 1) ? (float)(inputHeight - 1)/(outputHeight - 1) : 0.f;
+  const float rwidth = (outputWidth > 1) ? (float)(inputWidth - 1) / (outputWidth - 1) : 0.f;
+  for (int h2 = 0; h2 < outputHeight; ++h2) {
+    const float h1r = rheight * h2;
+    const int h1 = h1r;
+    const int h1p = (h1 < inputHeight - 1) ? 1 : 0;
+    const DType h1lambda = h1r - h1;
+    const DType h0lambda = (DType)1. - h1lambda;
+    for (int w2 = 0; w2 < outputWidth; ++w2) {
+      const float w1r = rwidth * w2;
+      const int w1 = w1r;
+      const int w1p = (w1 < inputWidth - 1) ? 1 : 0;
+      const DType w1lambda = w1r - w1;
+      const DType w0lambda = (DType)1. - w1lambda;
+      const DType* pos1 = &idata[h1 * inputWidth + w1];
+      DType* pos2 = &odata[h2 * outputWidth + w2];
+      for (int c = 0; c < channels; ++c) {
+        pos2[0] = h0lambda * (w0lambda * pos1[0]+ w1lambda * pos1[w1p])
+                  + h1lambda * (w0lambda * pos1[h1p * inputWidth]
+                  + w1lambda * pos1[h1p * inputWidth + w1p]);
+        pos1 += inputWidth * inputHeight;
+        pos2 += outputWidth * outputHeight;
+      }
+    }
+  }
+}
+
+
+template<typename xpu, typename DType, typename AccReal>
+void SpatialUpSamplingBilinearUpdateGradInput(mshadow::Stream<cpu> *s,
+                                              const std::vector<TBlob> &input,
+                                              const std::vector<TBlob> &output) {
+  DeviceTensor<DType, 4> gradOutput = devicetensor<DType, 4>(input[0]);
+  DeviceTensor<DType, 4> gradInput = devicetensor<DType, 4>(output[0]);
+  int nbatch = gradInput.getSize(0);
+  int channels = gradInput.getSize(1);
+  int outputHeight = gradInput.getSize(2);
+  int outputWidth = gradInput.getSize(3);
+  int inputHeight = gradOutput.getSize(2);
+  int inputWidth = gradOutput.getSize(3);
+
+  DType *data1 = gradInput.data_ptr();
+  DType *data2 = gradOutput.data_ptr();
+  channels = nbatch * channels;
+
+  // special case: same-size matching grids
+  if (inputHeight == outputHeight && inputWidth == outputWidth) {
+    for (int h2 = 0; h2 < outputHeight; ++h2) {
+      const int h1 = h2;
+      for (int w2 = 0; w2 < outputWidth; ++w2) {
+        const int w1 = w2;
+        DType* pos1 = &data1[h1 * inputWidth + w1];
+        const DType* pos2 = &data2[h2 * outputWidth + w2];
+        for (int c = 0; c < channels; ++c) {
+          pos1[0] += pos2[0];
+          pos1 += inputWidth * inputHeight;
+          pos2 += outputWidth * outputHeight;
+        }
+      }
+    }
+    return;
+  }
+  const float rheight =(outputHeight > 1) ? (float)(inputHeight - 1)/(outputHeight - 1) : 0.f;
+  const float rwidth = (outputWidth > 1) ? (float)(inputWidth - 1)/(outputWidth - 1) : 0.f;
+  for (int h2 = 0; h2 < outputHeight; ++h2) {
+    const float h1r = rheight * h2;
+    const int h1 = h1r;
+    const int h1p = (h1 < inputHeight - 1) ? 1 : 0;
+    const DType h1lambda = h1r - h1;
+    const DType h0lambda = (DType)1. - h1lambda;
+    for (int w2 = 0; w2 < outputWidth; ++w2) {
+      const float w1r = rwidth * w2;
+      const int w1 = w1r;
+      const int w1p = (w1 < inputWidth - 1) ? 1 : 0;
+      const DType w1lambda = w1r - w1;
+      const DType w0lambda = (DType)1. - w1lambda;
+      DType* pos1 = &data1[h1 * inputWidth + w1];
+      const DType* pos2 = &data2[h2 * outputWidth + w2];
+      for (int c = 0; c < channels; ++c) {
+        pos1[0] += h0lambda * w0lambda * pos2[0];
+        pos1[w1p] += h0lambda * w1lambda * pos2[0];
+        pos1[h1p * inputWidth] += h1lambda * w0lambda * pos2[0];
+        pos1[h1p * inputWidth + w1p] += h1lambda * w1lambda * pos2[0];
+        pos1 += inputWidth * inputHeight;
+        pos2 += outputWidth * outputHeight;
+      }
+    }
+  }
+}
+
+
+DMLC_REGISTER_PARAMETER(BilinearSampleParam);
+
+NNVM_REGISTER_OP(BilinearUpsample2D)
+.describe(R"code(TODO docs
+)code" ADD_FILELINE)
+.set_attr_parser(ParamParser<BilinearSampleParam>)
+.set_num_inputs(1)
+.set_num_outputs(1)
+.set_attr<nnvm::FInferShape>("FInferShape", BilinearSampleOpInferShape)
+.set_attr<nnvm::FInferType>("FInferType", BilinearSampleOpInferType)
+.set_attr<FInferStorageType>("FInferStorageType", BilinearSampleOpStorageType)
+.set_attr<FCompute>("FCompute<cpu>", BilinearSampleOpForward<cpu>)
+.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseNone{"_backward_BilinearUpsample2D"})
+.add_argument("data", "NDArray-or-Symbol", "Input data")
+;
+
+NNVM_REGISTER_OP(_backward_BilinearUpsample2D)
+.set_attr_parser(ParamParser<BilinearSampleParam>)
+.set_num_inputs(1)
+.set_num_outputs(1)
+.set_attr<nnvm::TIsBackward>("TIsBackward", true)
+.set_attr<FInferStorageType>("FInferStorageType", BilinearSampleOpStorageType)
+.set_attr<FCompute>("FCompute<cpu>", BilinearSampleOpBackward<cpu>)
+;
+
+
+}  // namespace op
+}  // namespace mxnet
---
+++ src/operator/devicetensor.h
@@ -0,0 +1,156 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ /*!
+ * Copyright (c) 2018 by Contributors
+ * \file device-tensor.h
+ * \brief  bilinear upsample operator
+ * \author Hang Zhang
+ */
+#ifndef MXNET_OPERATOR_DEVICE_TENSOR_H_
+#define MXNET_OPERATOR_DEVICE_TENSOR_H_
+
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <mxnet/ndarray.h>
+#include <vector>
+#include <string>
+#include <utility>
+#include "../ndarray/ndarray_function.h"
+#include "./operator_common.h"
+#include "./mxnet_op.h"
+#include "./mshadow_op.h"
+
+namespace mxnet {
+namespace op {
+
+template<typename DType, int Dim>
+struct DeviceTensor {
+ public:
+  MSHADOW_XINLINE DeviceTensor(DType *p, const int *size)
+    : dptr_(p) {
+    for (int i = 0; i < Dim; ++i) {
+      size_[i] = size ? size[i] : 0;
+    }
+  }
+
+  MSHADOW_XINLINE unsigned getSize(const int i) const {
+    assert(i < Dim);
+    return size_[i];
+  }
+
+  MSHADOW_XINLINE int numElements() const {
+    int n = 1;
+    for (int i = 0; i < Dim; ++i) {
+      n *= size_[i];
+    }
+    return n;
+  }
+
+  MSHADOW_XINLINE DeviceTensor<DType, Dim-1> select(const size_t x) const {
+    assert(Dim > 1);
+    int offset = x;
+    for (int i = 1; i < Dim; ++i) {
+      offset *= size_[i];
+    }
+    DeviceTensor<DType, Dim-1> tensor(dptr_ + offset, nullptr);
+    for (int i = 0; i < Dim - 1; ++i) {
+      tensor.size_[i] = this->size_[i+1];
+    }
+    return tensor;
+  }
+
+  MSHADOW_XINLINE DeviceTensor<DType, Dim-1> operator[](const size_t x) const {
+    assert(Dim > 1);
+    int offset = x;
+    for (int i = 1; i < Dim; ++i) {
+      offset *= size_[i];
+    }
+    DeviceTensor<DType, Dim-1> tensor(dptr_ + offset, nullptr);
+    for (int i = 0; i < Dim - 1; ++i) {
+      tensor.size_[i] = this->size_[i+1];
+    }
+    return tensor;
+  }
+
+  MSHADOW_XINLINE size_t InnerSize() const {
+    assert(Dim >= 3);
+    size_t sz = 1;
+    for (size_t i = 2; i < Dim; ++i) {
+      sz *= size_[i];
+    }
+    return sz;
+  }
+
+  MSHADOW_XINLINE size_t ChannelCount() const {
+    assert(Dim >= 3);
+    return size_[1];
+  }
+
+  MSHADOW_XINLINE DType* data_ptr() const {
+    return dptr_;
+  }
+
+  DType *dptr_;
+  int size_[Dim];
+};
+
+template<typename DType>
+struct DeviceTensor<DType, 1> {
+  MSHADOW_XINLINE DeviceTensor(DType *p, const int *size)
+    : dptr_(p) {
+    size_[0] = size ? size[0] : 0;
+  }
+
+  MSHADOW_XINLINE unsigned getSize(const int i) const {
+    assert(i == 0);
+    return size_[0];
+  }
+
+  MSHADOW_XINLINE int numElements() const {
+    return size_[0];
+  }
+
+  MSHADOW_XINLINE DType &operator[](const size_t x) const {
+      return *(dptr_ + x);
+  }
+
+  MSHADOW_XINLINE DType* data_ptr() const {
+    return dptr_;
+  }
+
+  DType *dptr_;
+  int size_[1];
+};
+
+template<typename DType, int Dim>
+static DeviceTensor<DType, Dim> devicetensor(const TBlob &blob) {
+  DType *data = blob.dptr<DType>();
+  const int inDim = blob.shape_.ndim();
+  assert(inDim == Dim);
+  DeviceTensor<DType, Dim> tensor(data, nullptr);
+  for (int i = 0; i < Dim; ++i) {
+    tensor.size_[i] = blob.size(i);
+  }
+  return tensor;
+}
+
+}  // namespace op
+}  // namespace mxnet
+#endif  //MXNET_OPERATOR_DEVICE_TENSOR_H_
