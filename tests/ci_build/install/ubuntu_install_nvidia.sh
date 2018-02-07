60c6434f3512da06f54e2cabe31f181bcb1a15c0
Marco de Abreu <marcoabreu@users.noreply.github.com>
2018-02-07 15:48:49 -0800

    Specify lint-versions and fix docker build issues due to nvidia-384 (#9723)
    * Move from nvidia-384 to cuda-8.0
    
    * Specify lint versions

--- tests/ci_build/install/ubuntu_install_nvidia.sh
+++ tests/ci_build/install/ubuntu_install_nvidia.sh
@@ -28,4 +28,4 @@ add-apt-repository -y ppa:graphics-drivers
 # Retrieve ppa:graphics-drivers and install nvidia-drivers.
 # Note: DEBIAN_FRONTEND required to skip the interactive setup steps
 apt update && \
-    DEBIAN_FRONTEND=noninteractive apt install -y nvidia-384
+    DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends cuda-8-0
