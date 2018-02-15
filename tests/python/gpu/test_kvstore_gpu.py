ebc142084bc3cc63051ee474f705912161361b6e
Dick Carter <dcarter@nvidia.com>
2018-02-14 20:06:39 -0800

    Adds with_seed() decorator to gpu tests.  Will fail CI due to demo of known failing seeds.

--- tests/python/gpu/test_kvstore_gpu.py
+++ tests/python/gpu/test_kvstore_gpu.py
@@ -16,10 +16,15 @@
 # under the License.
 
 # pylint: skip-file
+import sys
+import os
 import mxnet as mx
 import numpy as np
 import unittest
 from mxnet.test_utils import assert_almost_equal, default_context
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, os.path.join(curr_path, '../unittest'))
+from common import setup_module, with_seed
 
 shape = (4, 4)
 keys = [5, 7, 11]
@@ -36,6 +41,7 @@ def init_kv_with_str(stype='default', kv_type='local'):
     return kv
 
 
+@with_seed()
 def test_rsp_push_pull():
     def check_rsp_push_pull(kv_type, is_push_cpu=True):
         kv = init_kv_with_str('row_sparse', kv_type)
