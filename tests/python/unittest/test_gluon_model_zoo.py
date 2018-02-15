6012ada83d4ffe45a0f38cebd082b57c95518b25
dwSun <dwSun@Capsule>
2018-02-15 14:25:57 +0800

    add mobilenetv2 to unittest.

--- tests/python/unittest/test_gluon_model_zoo.py
+++ tests/python/unittest/test_gluon_model_zoo.py
@@ -33,7 +33,8 @@ def test_models():
                   'alexnet', 'inceptionv3',
                   'densenet121', 'densenet161', 'densenet169', 'densenet201',
                   'squeezenet1.0', 'squeezenet1.1',
-                  'mobilenet1.0', 'mobilenet0.75', 'mobilenet0.5', 'mobilenet0.25']
+                  'mobilenet1.0', 'mobilenet0.75', 'mobilenet0.5', 'mobilenet0.25',
+                  'mobilenetv2_1.0', 'mobilenetv2_0.75', 'mobilenetv2_0.5', 'mobilenetv2_0.25']
     pretrained_to_test = set(['squeezenet1.1'])
 
     for model_name in all_models:
