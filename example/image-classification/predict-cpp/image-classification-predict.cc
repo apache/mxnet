6cc28a0bde7ace3ff65a49185566b1b94076a567
Anton Chernov <mechernov@gmail.com>
2018-02-16 11:39:48 +0100

    Adressed review comments, made other small improvements

--- example/image-classification/predict-cpp/image-classification-predict.cc
+++ example/image-classification/predict-cpp/image-classification-predict.cc
@@ -33,65 +33,65 @@
 
 #include <cstdio>
 #include <cstdlib>
-
-// Path for c_predict_api
-#include <mxnet/c_predict_api.h>
-
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <memory>
+#include <iomanip>
 #include <opencv2/opencv.hpp>
+// Path for c_predict_api
+#include <mxnet/c_predict_api.h>
 
 const mx_float DEFAULT_MEAN = 117.0;
+
+std::string trim(const std::string& input) {
+  auto not_space = [](int ch) {
+    return !std::isspace(ch);
+  };
+  auto output = input;
+  output.erase(output.begin(), std::find_if(output.begin(), output.end(), not_space));
+  output.erase(std::find_if(output.rbegin(), output.rend(), not_space).base(), output.end());
+  return output;
+}
 
 // Read file to buffer
 class BufferFile {
  public :
   std::string file_path_;
-  int length_;
-  char* buffer_;
+  std::size_t length_ = 0;
+  std::unique_ptr<char[]> buffer_;
 
-  explicit BufferFile(std::string file_path)
+  explicit BufferFile(const std::string& file_path)
     : file_path_(file_path) {
 
     std::ifstream ifs(file_path.c_str(), std::ios::in | std::ios::binary);
     if (!ifs) {
       std::cerr << "Can't open the file. Please check " << file_path << ". \n";
-      length_ = 0;
-      buffer_ = NULL;
       return;
     }
 
     ifs.seekg(0, std::ios::end);
-    length_ = ifs.tellg();
+    length_ = static_cast<std::size_t>(ifs.tellg());
     ifs.seekg(0, std::ios::beg);
     std::cout << file_path.c_str() << " ... " << length_ << " bytes\n";
 
-    buffer_ = new char[sizeof(char) * length_];
-    ifs.read(buffer_, length_);
+    buffer_.reset(new char[length_]);
+    ifs.read(buffer_.get(), length_);
     ifs.close();
   }
 
-  int GetLength() {
+  std::size_t GetLength() {
     return length_;
   }
 
   char* GetBuffer() {
-    return buffer_;
-  }
-
-  ~BufferFile() {
-    if (buffer_) {
-      delete[] buffer_;
-      buffer_ = NULL;
-    }
+    return buffer_.get();
   }
 };
 
-void GetImageFile(const std::string image_file,
-                  mx_float* image_data, const int channels,
-                  const cv::Size resize_size, const mx_float* mean_data = nullptr) {
+void GetImageFile(const std::string& image_file,
+                  mx_float* image_data, int channels,
+                  cv::Size resize_size, const mx_float* mean_data = nullptr) {
   // Read all kinds of file into a BGR color 3 channels image
   cv::Mat im_ori = cv::imread(image_file, cv::IMREAD_COLOR);
 
@@ -114,7 +114,7 @@ void GetImageFile(const std::string image_file,
   mean_b = mean_g = mean_r = DEFAULT_MEAN;
 
   for (int i = 0; i < im.rows; i++) {
-    uchar* data = im.ptr<uchar>(i);
+    auto data = im.ptr<uchar>(i);
 
     for (int j = 0; j < im.cols; j++) {
       if (mean_data) {
@@ -137,7 +137,7 @@ void GetImageFile(const std::string image_file,
 
 // LoadSynsets
 // Code from : https://github.com/pertusa/mxnet_predict_cc/blob/master/mxnet_predict.cc
-std::vector <std::string> LoadSynset(std::string synset_file) {
+std::vector<std::string> LoadSynset(const std::string& synset_file) {
   std::ifstream fi(synset_file.c_str());
 
   if (!fi.is_open()) {
@@ -145,7 +145,7 @@ std::vector <std::string> LoadSynset(std::string synset_file) {
     assert(false);
   }
 
-  std::vector <std::string> output;
+  std::vector<std::string> output;
 
   std::string synset, lemma;
   while (fi >> synset) {
@@ -158,16 +158,16 @@ std::vector <std::string> LoadSynset(std::string synset_file) {
   return output;
 }
 
-void PrintOutputResult(const std::vector<float>& data, const std::vector <std::string>& synset) {
+void PrintOutputResult(const std::vector<float>& data, const std::vector<std::string>& synset) {
   if (data.size() != synset.size()) {
-    std::cerr << "Result data and synset size does not match!" << std::endl;
+    std::cerr << "Result data and synset size do not match!" << std::endl;
   }
 
   float best_accuracy = 0.0;
-  int best_idx = 0;
+  std::size_t best_idx = 0;
 
-  for (int i = 0; i < static_cast<int>(data.size()); i++) {
-    printf("Accuracy[%d] = %.8f\n", i, data[i]);
+  for (std::size_t i = 0; i < data.size(); ++i) {
+    std::cout << "Accuracy[" << i << "] = " << std::setprecision(8) << data[i] << std::endl;
 
     if (data[i] > best_accuracy) {
       best_accuracy = data[i];
@@ -175,19 +175,18 @@ void PrintOutputResult(const std::vector<float>& data, const std::vector <std::s
     }
   }
 
-  printf("Best Result: [%s] id = %d, accuracy = %.8f\n",
-         synset[best_idx].c_str(), best_idx, best_accuracy);
+  std::cout << "Best Result: " << trim(synset[best_idx]) << " (id=" << best_idx << ", " <<
+            "accuracy=" << std::setprecision(8) << best_accuracy << ")" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cout << "No test image here." << std::endl
               << "Usage: ./image-classification-predict apple.jpg" << std::endl;
-    return 0;
+    return EXIT_FAILURE;
   }
 
-  std::string test_file;
-  test_file = std::string(argv[1]);
+  std::string test_file(argv[1]);
 
   // Models path for your model, you have to modify it
   std::string json_file = "model/Inception/Inception-BN-symbol.json";
@@ -199,7 +198,7 @@ int main(int argc, char* argv[]) {
   BufferFile param_data(param_file);
 
   // Parameters
-  int dev_type = 1;  // 1: cpu, 2: gpu
+  int dev_type = 2;  // 1: cpu, 2: gpu
   int dev_id = 0;  // arbitrary.
   mx_uint num_input_nodes = 1;  // 1 for feedforward
   const char* input_key[1] = { "data" };
@@ -215,17 +214,16 @@ int main(int argc, char* argv[]) {
                                         static_cast<mx_uint>(channels),
                                         static_cast<mx_uint>(height),
                                         static_cast<mx_uint>(width) };
-  PredictorHandle pred_hnd = 0;
+  PredictorHandle pred_hnd = nullptr;
 
-  if (json_data.GetLength() == 0 ||
-      param_data.GetLength() == 0) {
+  if (json_data.GetLength() == 0 || param_data.GetLength() == 0) {
     return EXIT_FAILURE;
   }
 
   // Create Predictor
   MXPredCreate(static_cast<const char*>(json_data.GetBuffer()),
                static_cast<const char*>(param_data.GetBuffer()),
-               static_cast<size_t>(param_data.GetLength()),
+               static_cast<int>(param_data.GetLength()),
                dev_type,
                dev_id,
                num_input_nodes,
@@ -235,35 +233,34 @@ int main(int argc, char* argv[]) {
                &pred_hnd);
   assert(pred_hnd);
 
-  int image_size = width * height * channels;
+  auto image_size = static_cast<std::size_t>(width * height * channels);
 
   // Read Mean Data
-  const mx_float* nd_data = NULL;
-  NDListHandle nd_hnd = 0;
+  const mx_float* nd_data = nullptr;
+  NDListHandle nd_hnd = nullptr;
   BufferFile nd_buf(nd_file);
 
   if (nd_buf.GetLength() > 0) {
     mx_uint nd_index = 0;
     mx_uint nd_len;
-    const mx_uint* nd_shape = 0;
-    const char* nd_key = 0;
+    const mx_uint* nd_shape = nullptr;
+    const char* nd_key = nullptr;
     mx_uint nd_ndim = 0;
 
     MXNDListCreate(static_cast<const char*>(nd_buf.GetBuffer()),
-                   nd_buf.GetLength(),
+                   static_cast<int>(nd_buf.GetLength()),
                    &nd_hnd, &nd_len);
 
     MXNDListGet(nd_hnd, nd_index, &nd_key, &nd_data, &nd_shape, &nd_ndim);
   }
 
   // Read Image Data
-  std::vector <mx_float> image_data(image_size);
+  std::vector<mx_float> image_data(image_size);
 
-  GetImageFile(test_file, image_data.data(),
-               channels, cv::Size(width, height), nd_data);
+  GetImageFile(test_file, image_data.data(), channels, cv::Size(width, height), nd_data);
 
   // Set Input Image
-  MXPredSetInput(pred_hnd, "data", image_data.data(), image_size);
+  MXPredSetInput(pred_hnd, "data", image_data.data(), static_cast<mx_uint>(image_size));
 
   // Do Predict Forward
   MXPredForward(pred_hnd);
@@ -276,7 +273,7 @@ int main(int argc, char* argv[]) {
   // Get Output Result
   MXPredGetOutputShape(pred_hnd, output_index, &shape, &shape_len);
 
-  size_t size = 1;
+  mx_uint size = 1;
   for (mx_uint i = 0; i < shape_len; ++i) { size *= shape[i]; }
 
   std::vector<float> data(size);
@@ -292,7 +289,7 @@ int main(int argc, char* argv[]) {
   MXPredFree(pred_hnd);
 
   // Synset path for your model, you have to modify it
-  std::vector <std::string> synset = LoadSynset(synset_file);
+  auto synset = LoadSynset(synset_file);
 
   // Print Output Data
   PrintOutputResult(data, synset);
