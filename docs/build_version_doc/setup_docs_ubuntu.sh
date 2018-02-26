02e09d4e9ea4db4e94788895b0e1148d29c42d73
Aaron Markham <markhama@amazon.com>
2018-02-26 04:42:53 +0000

    used license header tool this time

--- docs/build_version_doc/setup_docs_ubuntu.sh
+++ docs/build_version_doc/setup_docs_ubuntu.sh
@@ -17,6 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+
 # If you need to build <= v0.12.0 then use a Python 2 environment
 # mxdoc.py - a sphinx extension, was not Python 3 compatible in the old versions
 # source activate mxnet_p27
@@ -38,8 +39,8 @@ pip install --user \
     mock==1.0.1 \
     pypandoc \
     recommonmark==0.4.0 \
-    sphinx==1.5.6 
-    
+    sphinx==1.5.6
+
 # Recommonmark/Sphinx errors: https://github.com/sphinx-doc/sphinx/issues/3800
 
 
@@ -57,5 +58,5 @@ sudo apt autoremove -y
 # Make docs using the manual way
 # cd .. && make html USE_OPENMP=0
 # using the docker way
-# sudo make docs 
+# sudo make docs
 
