d87468e563825e8f6d48cc8352b7db65e302ab2c
Xingjian Shi <xshiab@ust.hk>
2018-02-28 15:19:35 -0800

    set a lower success_rate (#9927)

--- python/mxnet/test_utils.py
+++ python/mxnet/test_utils.py
@@ -1845,7 +1845,7 @@ def chi_square_check(generator, buckets, probs, nsamples=1000000):
     _, p = ss.chisquare(f_obs=obs_freq, f_exp=expected_freq)
     return p, obs_freq, expected_freq
 
-def verify_generator(generator, buckets, probs, nsamples=1000000, nrepeat=5, success_rate=0.25):
+def verify_generator(generator, buckets, probs, nsamples=1000000, nrepeat=5, success_rate=0.15):
     """Verify whether the generator is correct using chi-square testing.
 
     The test is repeated for "nrepeat" times and we check if the success rate is
