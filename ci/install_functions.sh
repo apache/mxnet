18b4b47c68443819d82356f7761c26b98c9621e6
Marco de Abreu <marco.g.abreu+github@gmail.com>
2018-03-01 18:01:18 +0100

    Add verbose

--- ci/install_functions.sh
+++ ci/install_functions.sh
@@ -122,7 +122,7 @@ ubuntu_install_lint() {
 ubuntu_install_clang() {
     set -ex
     # Install clang 3.9 (the same version as in XCode 8.*) and 5.0 (latest major release)
-    RUN wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - && \
+    RUN wget -vO - http://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add - && \
         apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-3.9 main" && \
         apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-5.0 main" && \
         apt-get update && \
