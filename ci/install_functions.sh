e398d1155eb777dcba00eba180ddc0b78db7a3a3
Marco de Abreu <marco.g.abreu+github@gmail.com>
2018-03-01 18:11:13 +0100

    Add package

--- ci/install_functions.sh
+++ ci/install_functions.sh
@@ -49,6 +49,7 @@ ubuntu_install_core() {
         wget \
         unzip \
         sudo \
+        software-properties-common \
         ninja-build \
         python-pip
 
@@ -129,6 +130,12 @@ ubuntu_install_clang() {
         apt-get install -y clang-3.9 clang-5.0 && \
         clang-3.9 --version && \
         clang-5.0 --version
+}
+
+ubuntu_install_mklml() {
+    set -ex
+    wget --no-check-certificate -O /tmp/mklml.tgz https://github.com/01org/mkl-dnn/releases/download/v0.12/mklml_lnx_2018.0.1.20171227.tgz
+    tar -zxvf /tmp/mklml.tgz && cp -rf mklml_*/* /usr/local/ && rm -rf mklml_*
 }
 
 
