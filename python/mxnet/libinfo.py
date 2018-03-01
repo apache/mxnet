062a337fa49c22edcaf22e1b576aa56201485b2c
mxnet-ci <mxnet-ci>
2018-03-01 16:09:06 +0000

    Fix lint

--- python/mxnet/libinfo.py
+++ python/mxnet/libinfo.py
@@ -34,7 +34,8 @@ def find_lib_path():
     if lib_from_env:
         if os.path.isfile(lib_from_env):
             if not os.path.isabs(lib_from_env):
-                logging.warn("MXNET_LIBRARY_PATH should be an absolute path, instead of: %s", lib_from_env)
+                logging.warn("MXNET_LIBRARY_PATH should be an absolute path, instead of: %s",
+                              lib_from_env)
             else:
                 return [lib_from_env]
         else:
