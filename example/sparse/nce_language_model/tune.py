bec493eaadc900d69f56346354ab8d075b2ed0ef
Ubuntu <ubuntu@ip-172-31-25-246.us-west-2.compute.internal>
2018-03-02 13:39:07 +0000

    update tuner

--- example/sparse/nce_language_model/tune.py
+++ example/sparse/nce_language_model/tune.py
@@ -9,51 +9,38 @@ parser.add_argument('--gpu', type=int, default=0,
                     help='which gpu')
 
 
-num_gpus = 8
+num_gpus = 1
 
 args = parser.parse_args()
 gpu = args.gpu
 print("total num gpus = %d, current gpu = %d" % (num_gpus, gpu))
 
-LR = [1]
-LR_DECAY = [0.25, 0.5]
-BPTT = [35]
-K = [60]
-CLIP = [0.2]
-DROPOUT = [0.5]
-MOM = [0.9, 0.95]
-BETA1 = [0.9]
-WD = [0, 1e-5]
-HID = 650
-USE_DENSE = False
-OPTIM = 'sgd'
+LR = [0.05, 0.1, 0.2]
+BPTT = [20, 35]
+K = [8192, 10240]
+CLIP = [1, 5, 10, 15]
+DROPOUT = [0.01, 0.05, 0.1, 0.2]
 
-niter = 0
-total_iter = len(LR) * len(LR_DECAY) * len(BPTT) * len(K) * len(CLIP) * len(DROPOUT) * len(MOM) * len(WD)
+#LR = [0.2]
+#BPTT = [20]
+#K = [8192]
+#CLIP = [10]
+#DROPOUT = [0.1]
+total_iter = len(LR) * len(BPTT) * len(K) * len(CLIP) * len(DROPOUT)
 
 for lr in LR:
     for bptt in BPTT:
         for k in K:
             for clip in CLIP:
                 for dropout in DROPOUT:
-                    for lr_decay in LR_DECAY:
-                        for mom in MOM:
-                            for wd in WD:
-                                for beta1 in BETA1:
-                                    if niter % num_gpus == gpu:
-                                        my_env = os.environ.copy()
-                                        my_env["PYTHONPATH"] = "/home/ubuntu/nce/python:" + my_env["PYTHONPATH"]
+                    my_env = os.environ.copy()
+                    my_env["PYTHONPATH"] = "/home/ubuntu/tf/python:" + my_env["PYTHONPATH"]
 
-                                        config = ["--bptt", str(bptt), "--k", str(k), '--dropout', str(dropout), \
-                                                  '--clip', str(clip), '--lr', str(lr), '--lr-decay', str(lr_decay), \
-                                                  '--mom', str(mom), '--wd', str(wd), '--optimizer', str(OPTIM), '--beta1', str(beta1)]
-                                        if USE_DENSE:
-                                            config += ['--dense']
-                                        cmd = ["python", "train.py", "--nhid", str(HID), "--emsize", str(HID), "--log-interval=999999", \
-                                               "--gpus=%d" % gpu, '--epoch=80', '--tied'] + config
-                                        filename = 'logs-sparse/tune' + '-'.join(config) + ".tunelog"
-                                        with open(filename, "w") as outfile:
-                                            subprocess.check_call(cmd, stderr=outfile, env=my_env)
-                                            #subprocess.call(cmd)
-                                    niter += 1
-                                    print("%d of %d" %(niter, total_iter))
+                    config = ["--bptt", str(bptt), "--k", str(k), '--dropout', str(dropout), \
+                              '--clip', str(clip), '--lr', str(lr)]
+                    cmd = ["python", "train.py", "--gpus=0,1,2,3", "--data=/home/ubuntu/gbw-5/training-monolingual.tokenized.shuffled/*",
+                           "--per-ctx-clip", "--epoch=1", "--checkpoint-interval=100"] + config
+                    filename = 'logs-5/tune' + '-'.join(config) + ".tunelog"
+                    with open(filename, "w") as outfile:
+                        subprocess.check_call(cmd, stderr=outfile, env=my_env)
+                    print("%d of %d" %(niter, total_iter))
