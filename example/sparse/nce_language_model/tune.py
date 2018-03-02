53d9ecb52e24f7b2d4716bc5b4b5cee09355e888
Ubuntu <ubuntu@ip-172-31-25-246.us-west-2.compute.internal>
2018-03-02 13:50:48 +0000

    update uner w/ eps

--- example/sparse/nce_language_model/tune.py
+++ example/sparse/nce_language_model/tune.py
@@ -20,6 +20,7 @@ BPTT = [20, 35]
 K = [8192, 10240]
 CLIP = [1, 5, 10, 15]
 DROPOUT = [0.01, 0.05, 0.1, 0.2]
+EPS = [1, 0.1, 0.01, 0.001]
 
 #LR = [0.2]
 #BPTT = [20]
@@ -33,14 +34,14 @@ for lr in LR:
         for k in K:
             for clip in CLIP:
                 for dropout in DROPOUT:
-                    my_env = os.environ.copy()
-                    my_env["PYTHONPATH"] = "/home/ubuntu/tf/python:" + my_env["PYTHONPATH"]
+                    for eps in EPS:
+                        my_env = os.environ.copy()
+                        my_env["PYTHONPATH"] = "/home/ubuntu/tf/python:" + my_env["PYTHONPATH"]
 
-                    config = ["--bptt", str(bptt), "--k", str(k), '--dropout', str(dropout), \
-                              '--clip', str(clip), '--lr', str(lr)]
-                    cmd = ["python", "train.py", "--gpus=0,1,2,3", "--data=/home/ubuntu/gbw-5/training-monolingual.tokenized.shuffled/*",
-                           "--per-ctx-clip", "--epoch=1", "--checkpoint-interval=100"] + config
-                    filename = 'logs-5/tune' + '-'.join(config) + ".tunelog"
-                    with open(filename, "w") as outfile:
-                        subprocess.check_call(cmd, stderr=outfile, env=my_env)
-                    print("%d of %d" %(niter, total_iter))
+                        config = ["--bptt", str(bptt), "--k", str(k), '--dropout', str(dropout), \
+                                  '--clip', str(clip), '--lr', str(lr), '--eps', str(eps)]
+                        cmd = ["python", "train.py", "--gpus=0,1,2,3", "--data=/home/ubuntu/gbw-5/training-monolingual.tokenized.shuffled/*",
+                               "--per-ctx-clip", "--epoch=1", "--checkpoint-interval=100"] + config
+                        filename = 'logs-5/tune' + '-'.join(config) + ".tunelog"
+                        with open(filename, "w") as outfile:
+                            subprocess.check_call(cmd, stderr=outfile, env=my_env)
