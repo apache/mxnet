6bf36c527344f93ee11807a15698d5907b8fcc66
Ubuntu <ubuntu@ip-172-31-25-246.us-west-2.compute.internal>
2018-03-07 14:48:56 +0000

    ffix license

--- example/rnn/large_word_lm/model.py
+++ example/rnn/large_word_lm/model.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 # Licensed to the Apache Software Soundation (ASS) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -84,7 +101,7 @@ def sampled_softmax(num_classes, num_samples, in_dim, inputs, weight, bias,
         target_w = S.slice(sample_target_w, begin=(num_samples, 0), end=(None, None))
         sample_b = S.slice(sample_target_b, begin=(0, 0), end=(num_samples, None))
         target_b = S.slice(sample_target_b, begin=(num_samples, 0), end=(None, None))
-    
+
         # target
         # (n, 1)
         true_pred = S.sum(target_w * inputs, axis=1, keepdims=True) + target_b
