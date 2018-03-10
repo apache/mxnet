ef1e19d5446e7d210d8987af190e535a55944712
Jerry Zhang <jerryzh.cn@gmail.com>
2018-03-10 01:48:25 +0000

    change magic number scope

--- tests/python/unittest/test_gluon_rnn.py
+++ tests/python/unittest/test_gluon_rnn.py
@@ -67,13 +67,12 @@ def test_lstm_forget_bias():
                                forget_bias * np.ones(100, ), np.zeros((2 * 100,))])
     assert_allclose(mod.get_params()[0][bias_argument].asnumpy(), expected_bias)
 
-EXPECTED_LSTM_OUTPUT = np.array([[[0.72045636, 0.72045636, 0.95215213, 0.95215213],
+def test_lstm_cpu_inference():
+    # should behave the same as lstm cell
+    EXPECTED_LSTM_OUTPUT = np.array([[[0.72045636, 0.72045636, 0.95215213, 0.95215213],
                                   [0.72045636, 0.72045636, 0.95215213, 0.95215213]],
                                  [[0.95215213, 0.95215213, 0.72045636, 0.72045636],
                                   [0.95215213, 0.95215213, 0.72045636, 0.72045636]]])
-def test_lstm_cpu_inference():
-    # should behave the same as lstm cell
-    atol = 1e-6
     x = mx.nd.ones(shape=(2, 2, 2))
     model = mx.gluon.rnn.LSTM(2, num_layers=6, bidirectional=True)
     model.initialize(mx.init.One())
