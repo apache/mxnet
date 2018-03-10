13ae4d1dbbd117a5b5484bf593d24469b4d2d345
Jerry Zhang <zhangz6@cs.washington.edu>
2018-03-09 21:46:34 -0800

    Cpu lstm inference (#9977)
    * fix autograd import path
    
    * cpu lstm working
    
    * remove fatal log
    
    * add simple unittest
    remove redundant log
    enable openmp
    
    * fused input2hidden gemm
    
    * fix lint
    
    * fix pylint
    
    * fix windows build error
    
    * fix gluon rnn interface
    
    * Update dataloader.py
    
    * address cr
    
    * address cr
    
    * fix import
    
    * revert some cosmetic change
    
    * fix typo
    
    * remove newline
    
    * rm virtual
    mv hardcoded number to constant
    
    * address cr
    add tests
    
    * simplify test
    
    * fix test
    
    * fix tests
    
    * change magic number scope

--- tests/python/unittest/test_gluon_rnn.py
+++ tests/python/unittest/test_gluon_rnn.py
@@ -67,6 +67,20 @@ def test_lstm_forget_bias():
                                forget_bias * np.ones(100, ), np.zeros((2 * 100,))])
     assert_allclose(mod.get_params()[0][bias_argument].asnumpy(), expected_bias)
 
+def test_lstm_cpu_inference():
+    # should behave the same as lstm cell
+    EXPECTED_LSTM_OUTPUT = np.array([[[0.72045636, 0.72045636, 0.95215213, 0.95215213],
+                                  [0.72045636, 0.72045636, 0.95215213, 0.95215213]],
+                                 [[0.95215213, 0.95215213, 0.72045636, 0.72045636],
+                                  [0.95215213, 0.95215213, 0.72045636, 0.72045636]]])
+    x = mx.nd.ones(shape=(2, 2, 2))
+    model = mx.gluon.rnn.LSTM(2, num_layers=6, bidirectional=True)
+    model.initialize(mx.init.One())
+    y = model(x).asnumpy()
+
+    mx.test_utils.assert_almost_equal(y, EXPECTED_LSTM_OUTPUT,
+                                      rtol=1e-3, atol=1e-5)
+    
 
 def test_gru():
     cell = gluon.rnn.GRUCell(100, prefix='rnn_')
