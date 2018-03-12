c402b5637dfca38bcdbad1af643f87993d9d2e02
Hao Jin <haoj@andrew.cmu.edu>
2018-03-12 23:46:33 +0000

    address code reviews: get rid of unnecessary checks

--- src/operator/l2_normalization.cc
+++ src/operator/l2_normalization.cc
@@ -37,10 +37,6 @@ Operator* CreateOp<cpu>(L2NormalizationParam param, int dtype) {
 // DO_BIND_DISPATCH comes from static_operator_common.h
 Operator* L2NormalizationProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
                                                 std::vector<int> *in_type) const {
-  std::vector<TShape> out_shape, aux_shape;
-  std::vector<int> out_type, aux_type;
-  CHECK(InferType(in_type, &out_type, &aux_type));
-  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
   DO_BIND_DISPATCH(CreateOp, param_, in_type->at(0));
 }
 
