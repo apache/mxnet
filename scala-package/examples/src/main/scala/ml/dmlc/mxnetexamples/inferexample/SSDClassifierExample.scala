09beb982ab1fd9dd49fa66d8fbcb70926e7606cf
Lanking <lanking520@live.com>
2018-03-20 20:12:04 -0700

    Update SSDClassifierExample.scala

--- scala-package/examples/src/main/scala/ml/dmlc/mxnetexamples/inferexample/SSDClassifierExample.scala
+++ scala-package/examples/src/main/scala/ml/dmlc/mxnetexamples/inferexample/SSDClassifierExample.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package SSDClassifierExample
 
 import ml.dmlc.mxnet._
@@ -11,7 +28,7 @@ import java.nio.file.{Files, Paths}
 class SSDClassifierExample {
   @Option(name = "--model-dir", usage = "the input model directory")
   private val modelPath: String = "/model"
-  @Option(name = "--model-prefix", usage = "The prefix of the model")
+  @Option(name = "--model-prefix", usage = "the prefix of the model")
   private val modelPrefix: String = "/ssd_resnet50_512"
   @Option(name = "--input-image", usage = "the input image")
   private val inputImagePath: String = "/images/Cat-hd-wallpapers.jpg"
