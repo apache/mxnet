c904e79c894088ef404ddbebe23cbaa2d1ad8b01
Lv Tao <tao.a.lv@intel.com>
2018-03-25 21:39:28 +0800

    remove make test

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -194,8 +194,7 @@ build_centos7_mkldnn() {
         USE_PROFILER=1 \
         USE_MKLDNN=1 \
         USE_BLAS=openblas \
-        -j$(nproc) \
-        test
+        -j$(nproc)
 }
 
 build_centos7_gpu() {
