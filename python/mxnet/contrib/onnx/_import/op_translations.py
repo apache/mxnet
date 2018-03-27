c7af14ab6ffc607cca0ff76dbc581437ac446aca
spidyDev <singh.rajan28@gmail.com>
2018-03-27 00:17:42 -0700

    - Added user warning for unsupported attributes

--- python/mxnet/contrib/onnx/_import/op_translations.py
+++ python/mxnet/contrib/onnx/_import/op_translations.py
@@ -357,7 +357,9 @@ def squeeze(attrs, inputs, cls):
 
 def flatten(attrs, inputs, cls):
     """Flattens the input array into a 2-D array by collapsing the higher dimensions."""
-    #Mxnet does not have axis support.
+    #Mxnet does not have axis support. By default uses axis=1
+    if 'axis' in attrs and attrs['axis'] != 1:
+        raise RuntimeError("Flatten operator only supports axis=1")
     new_attrs = translation_utils._remove_attributes(attrs, ['axis'])
     return 'Flatten', new_attrs, inputs
 
