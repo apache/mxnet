1a7c619773b14b48edc18a81f0a2d16b7158db37
Delteil, Thomas <tdelteil@amazon.com>
2018-03-30 18:15:31 -0700

    adding sparse array

--- tests/python/unittest/test_ndarray.py
+++ tests/python/unittest/test_ndarray.py
@@ -1142,28 +1142,28 @@ def test_ndarray_astype():
     # Test that a new ndarray has been allocated
     assert (id(x) != id(y))
 
-    x = mx.nd.zeros((2, 3), dtype='int32', copy=False)
-    y = x.astype('float32')
+    x = mx.nd.zeros((2, 3), dtype='int32')
+    y = x.astype('float32', copy=False)
     assert (y.dtype==np.float32)
     # Test that a new ndarray has been allocated
     assert (id(x) != id(y))
 
     x = mx.nd.zeros((2, 3), dtype='int32')
     y = x.astype('int32')
-    assert (y.dtype==np.float32)
+    assert (y.dtype==np.int32)
     # Test that a new ndarray has been allocated
     # even though they have same dtype
     assert (id(x) != id(y))
 
     # Test that a new ndarray has not been allocated
-    x = mx.nd.zeros((2, 3), dtype='int32', copy=False)
-    y = x.astype('int32')
+    x = mx.nd.zeros((2, 3), dtype='int32')
+    y = x.astype('int32', copy=False)
     assert (id(x) == id(y))
     
     # Test the string version 'int32'
     # has the same behaviour as the np.int32
-    x = mx.nd.zeros((2, 3), dtype='int32', copy=False)
-    y = x.astype(np.int32)
+    x = mx.nd.zeros((2, 3), dtype='int32')
+    y = x.astype(np.int32, copy=False)
     assert (id(x) == id(y))
 
 if __name__ == '__main__':
