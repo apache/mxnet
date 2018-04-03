62a615d72f042b0558e8e3a5232456e004de5d00
Da Zheng <zhengda1936@gmail.com>
2018-04-03 10:24:02 -0700

    [MXNET-235] add axis support and gradient for L2norm (#9740)
    * Add axis support for norm.
    
    * Add tests on norm in diff cases.
    
    * Fix a compilation error.
    
    * Fix a lint complain.
    
    * Fix a compilation error.
    
    * Implement norm backward.
    
    * Fix a bug on computing norm on entire sparse arrays.
    
    * Fix tests.
    
    * Fix a bug in L2Norm on dense arrays.
    
    * Fix a lint error.
    
    * Remove square.
    
    * Add ord in Norm.
    
    * Fix a compile error.
    
    * Use NormParam.
    
    * Fix tests.
    
    * Update broadcast_reduce_op_value.cc

--- tests/python/unittest/test_ndarray.py
+++ tests/python/unittest/test_ndarray.py
@@ -942,7 +942,7 @@ def test_ndarray_fluent():
     check_fluent_regular('pad', {'mode': 'constant', 'pad_width': (0,0,0,0,3,0,0,4)}, shape=(5, 17, 2, 3))
     check_fluent_regular('reshape_like', {'rhs': mx.nd.ones((30, 17))}, shape=(5, 17, 2, 3))
 
-    for func in ['sum', 'nansum', 'prod', 'nanprod', 'mean', 'max', 'min']:
+    for func in ['sum', 'nansum', 'prod', 'nanprod', 'mean', 'max', 'min', 'norm']:
         check_fluent_regular(func, {'axis': (1, 2)})
 
     check_fluent_regular('reshape', {'shape': (17, 1, 5)})
@@ -1165,6 +1165,38 @@ def test_ndarray_astype():
     x = mx.nd.zeros((2, 3), dtype='int32')
     y = x.astype(np.int32, copy=False)
     assert (id(x) == id(y))
+
+
+@with_seed()
+def test_norm(ctx=default_context()):
+    np_arr = np.random.uniform(size=(3, 3, 3, 3))
+    mx_arr = mx.nd.array(np_arr, ctx=ctx)
+    arr1 = np.linalg.norm(np_arr, keepdims=False)
+    arr2 = mx.nd.norm(mx_arr, keepdims=False)
+    print(arr1)
+    print(arr2.asnumpy())
+    mx.test_utils.assert_almost_equal(arr1, arr2.asnumpy()[0])
+
+    for i in range(4):
+        arr1 = np.linalg.norm(np_arr, axis=i, keepdims=False)
+        arr2 = mx.nd.norm(mx_arr, axis=i, keepdims=False)
+        assert arr1.shape == arr2.shape
+        mx.test_utils.assert_almost_equal(arr1, arr2.asnumpy())
+
+        arr1 = np.linalg.norm(np_arr, axis=i, keepdims=True)
+        arr2 = mx.nd.norm(mx_arr, axis=i, keepdims=True)
+        assert arr1.shape == arr2.shape
+        mx.test_utils.assert_almost_equal(arr1, arr2.asnumpy())
+        if (i < 3):
+            arr1 = np.linalg.norm(np_arr, axis=(i, i+1), keepdims=False)
+            arr2 = mx.nd.norm(mx_arr, axis=(i, i+1), keepdims=False)
+            assert arr1.shape == arr2.shape
+            mx.test_utils.assert_almost_equal(arr1, arr2.asnumpy())
+            arr1 = np.linalg.norm(np_arr, axis=(i, i+1), keepdims=True)
+            arr2 = mx.nd.norm(mx_arr, axis=(i, i+1), keepdims=True)
+            assert arr1.shape == arr2.shape
+            mx.test_utils.assert_almost_equal(arr1, arr2.asnumpy())
+
 
 if __name__ == '__main__':
     import nose
