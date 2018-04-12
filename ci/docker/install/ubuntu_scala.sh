d31d9e4e2e7f7bc15a54d7e391af89afa203b1e5
Aaron Markham <markhama@amazon.com>
2018-04-11 17:06:03 -0700

    fix scala install

--- ci/docker/install/ubuntu_scala.sh
+++ ci/docker/install/ubuntu_scala.sh
@@ -22,12 +22,16 @@
 
 set -ex
 # install libraries for mxnet's scala package on ubuntu
+echo 'Installing Scala...'
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:webupd8team/java
 apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install -y oracle-java8-installer
 apt-get install -y oracle-java8-set-default
+
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list	+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823	+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 apt-get update && apt-get install -y \
     maven \
     sbt \
