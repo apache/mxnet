078fffdb9b65ce12fcc6052bc1bd3b45ed400426
Gautam Kumar <gautamnitc@gmail.com>
2018-04-12 13:51:54 -0700

    Remove the python-devel

--- ci/docker/install/centos7_python.sh
+++ ci/docker/install/centos7_python.sh
@@ -25,7 +25,6 @@ set -ex
  # Python 2.7 is installed by default, install 3.6 on top
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 yum -y install python36u-devel
-yum -y install python-devel
 
 # Install PIP
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
