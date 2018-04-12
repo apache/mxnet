0186bd98055227f9c132f165d17c0edcd62d408f
Gautam Kumar <gautamnitc@gmail.com>
2018-04-12 11:41:17 -0700

    Replacing the python36u with python36-devel

--- ci/docker/install/centos7_python.sh
+++ ci/docker/install/centos7_python.sh
@@ -24,7 +24,7 @@ set -ex
 
  # Python 2.7 is installed by default, install 3.6 on top
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-yum -y install python36u
+yum -y install python36u-devel
 yum -y install python-devel
 
 # Install PIP
@@ -32,5 +32,5 @@ curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python2.7 get-pip.py
 python3.6 get-pip.py
 
-pip2 install nose pylint numpy nose-timer requests h5py scipy
-pip3 install nose pylint numpy nose-timer requests h5py scipy
+pip2 install nose pylint numpy==1.7.1 nose-timer requests h5py scipy
+pip3 install nose pylint numpy==1.7.1 nose-timer requests h5py scipy
