a4998f6a6b1cf6226047326e30ddf823a1d878a1
Da Zheng <zhengda1936@gmail.com>
2018-04-13 18:43:14 +0000

    Get all input symbols of a subgraph.

--- python/mxnet/contrib/control_flow.py
+++ python/mxnet/contrib/control_flow.py
@@ -15,44 +15,54 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import mxnet as mx
+import ctypes
+
+from .. import symbol
+from ..base import _LIB, c_str, c_array, check_call
+from ..base import SymbolHandle, NDArrayHandle
+from ..attribute import AttrScope
+
+def _get_graph_inputs(subg, name, prefix):
+    num_handles = ctypes.c_int(1000)
+    handles = c_array(SymbolHandle, [SymbolHandle(0) for i in range(1000)])
+    check_call(_LIB.MXSymbolGetInputSymbols(subg.handle, handles,
+        ctypes.byref(num_handles)))
+
+    syms = []
+    for i in range(num_handles.value):
+        s = symbol.Symbol(handles[i])
+        syms.append(s)
+    return syms
 
 def foreach(func, input, init_states, back_prop=False, name="foreach"):
-    in_ele = mx.sym.var("in")
-    gin_names = ["in"]
-    states = []
-    i = 0
     assert isinstance(init_states, list), "init_states should be a list"
-    for s in init_states:
-        states.append(mx.sym.var(s.name))
-        gin_names.append(s.name)
-        i = i + 1
-    with mx.AttrScope(subgraph_name=name):
+    states = []
+    with AttrScope(subgraph_name=name):
+        in_ele = symbol.var("in")
+        for s in init_states:
+            states.append(symbol.var(s.name))
+
         sym_out = func(in_ele, states)
         # The function should return a tuple. The first element goes to
         # the output of the function. The second element is a list.
         assert isinstance(sym_out, tuple), "func should return a tuple (out, states)"
         assert isinstance(sym_out[1], list), \
                 "the second element in the returned tuple should be a list"
+        assert len(sym_out[1]) == len(init_states), \
+                "the number of output states (%d) should be the same as input states (%d)" \
+                % (len(sym_out[1]), len(init_states))
 
-        flat_out = [sym_out[0]]
+        if (isinstance(sym_out[0], list)):
+            flat_out = sym_out[0]
+        else:
+            flat_out = [sym_out[0]]
         for s in sym_out[1]:
             # There is a problem if the outputs are the same as the inputs
             # or the first output.
             # TODO this is a temp fix.
-            flat_out.append(mx.sym.identity(s))
-    g = mx.sym.Group(flat_out)
-
-    # Find free variables in the python that are symbols.
-    freevars = dict(zip(func.func_code.co_freevars,
-        (c.cell_contents for c in func.func_closure)))
-    sym_freevars = []
-    for name in freevars:
-        val = freevars[name]
-        if isinstance(val, mx.sym.Symbol):
-            # We need to save the original symbol first.
-            sym_freevars.append(val)
-            gin_names.append(name)
+            flat_out.append(symbol.identity(s))
+    g = symbol.Group(flat_out)
+    input_syms = _get_graph_inputs(g, name, "ro_var")
 
     if (isinstance(input, list)):
         num_inputs = len(input)
@@ -61,17 +71,26 @@ def foreach(func, input, init_states, back_prop=False, name="foreach"):
 
     # Here we need to find out how the input symbols are ordered as well as
     # where the loop states are located in the list of inputs.
-    ins = init_states + sym_freevars
-    ins = {sym.name:sym for sym in ins}
+
+    # This dict contains the symbols of the subgraph.
+    input_syms = {sym.name:sym for sym in input_syms}
+    gin_names = input_syms.keys()
+    # This array contains the symbols for the inputs of foreach.
     ordered_ins = []
+    states_map = {sym.name:sym for sym in init_states}
+    state_names = states_map.keys()
     in_state_locs = [-1] * len(init_states)
     for in_name in g.list_inputs():
-        assert in_name in gin_names, "The input graph contains variables we can't find"
-        if in_name in ins:
-            ordered_ins.append(ins[in_name])
-            for i in range(len(init_states)):
-                if (init_states[i].name == in_name):
-                    in_state_locs[i] = len(ordered_ins) - 1 + num_inputs
+        assert in_name in gin_names, "The input variable %s can't be found in graph inputs: %s" \
+                % (in_name, str(gin_names))
+        if (in_name in state_names):
+            ordered_ins.append(states_map[in_name])
+        elif (in_name != "in"):
+            ordered_ins.append(input_syms[in_name])
 
-    return mx.sym._internal._foreach(g, input, *ordered_ins, num_outputs=len(flat_out),
+        for i in range(len(init_states)):
+            if (init_states[i].name == in_name):
+                in_state_locs[i] = len(ordered_ins) - 1 + num_inputs
+
+    return symbol._internal._foreach(g, input, *ordered_ins, num_outputs=len(flat_out),
                                      in_state_locs=in_state_locs)
