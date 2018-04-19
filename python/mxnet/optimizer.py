ec54d83ac963adf15c3f07bc39650ddb94142efb
ZiyueHuang <zyhuang94@gmail.com>
2018-04-19 17:40:56 +0000

    standard sgd_update

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -516,7 +516,7 @@ class SGD(Optimizer):
                 sgd_mom_update(weight, grad, state, out=weight,
                                lr=lr, wd=wd, **kwargs)
             else:
-                sgd_update(weight, grad, out=weight,
+                sgd_update(weight, grad, out=weight, lazy_update=self.lazy_update,
                            lr=lr, wd=wd, **kwargs)
         else:
             if state[0] is not None:
