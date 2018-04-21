c1c68a140ad42515a2270dd55f543631f090ae71
Da Zheng <zhengda1936@gmail.com>
2018-04-21 06:51:51 +0000

    Add C++ unit test for NDArray.

--- tests/cpp/operator/mkldnn.cc
+++ tests/cpp/operator/mkldnn.cc
@@ -28,6 +28,8 @@
 #include "gtest/gtest.h"
 #include "../../src/operator/nn/mkldnn/mkldnn_base-inl.h"
 
+using namespace mxnet;
+
 #if __GNUC__ >= 5
 bool test_mem_align(void *mem, size_t size, size_t alignment, size_t space) {
   void *ret1, *ret2;
@@ -84,4 +86,98 @@ TEST(MKLDNN_UTIL_FUNC, MemFormat) {
   CHECK_EQ(mkldnn_nchw, 5);
   CHECK_EQ(mkldnn_oihw, 12);
 }
+
+static void InitArray(NDArray &arr) {
+  const TBlob &blob = arr.data();
+  mshadow::default_real_t *data = blob.dptr<mshadow::default_real_t>();
+  size_t size = blob.Size();
+  for (size_t i = 0; i < size; i++)
+    data[i] = i;
+}
+
+static void VerifyMem(const mkldnn::memory &mem) {
+  mkldnn::memory::primitive_desc pd = mem.get_primitive_desc();
+  if (pd.desc().data.format == GetDefaultFormat(pd.desc())) {
+    mshadow::default_real_t *data
+        = static_cast<mshadow::default_real_t *>(mem.get_data_handle());
+    size_t size = pd.get_size() / sizeof(mshadow::default_real_t);
+    for (size_t i = 0; i < size; i++)
+      CHECK_EQ(data[i], static_cast<mshadow::default_real_t>(i));
+  }
+}
+
+static mkldnn::memory::primitive_desc GetMemPD(const TShape s, int dtype,
+                                               mkldnn::memory::format format) {
+  mkldnn::memory::dims dims(s.ndim());
+  for (size_t i = 0; i < dims.size(); i++)
+    dims[i] = s[i];
+  mkldnn::memory::desc desc{dims, get_mkldnn_type(dtype), format};
+  return mkldnn::memory::primitive_desc(desc, CpuEngine::Get()->get_engine());
+}
+
+TEST(MKLDNN_GET_DATA_REORDER, DataReorder) {
+  std::vector<TShape> shapes;
+  std::vector<mkldnn::memory::primitive_desc> pds;
+  int dtype = mshadow::DataType<mshadow::default_real_t>::kFlag;
+  {
+    // 1D
+    TShape s(1);
+    s[0] = 100;
+    shapes.push_back(s);
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::x));
+  }
+  {
+    // 2D
+    TShape s(2);
+    s[0] = 25;
+    s[1] = 4;
+    shapes.push_back(s);
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::nc));
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::io));
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::oi));
+  }
+  {
+    // 4D
+    TShape s(4);
+    s[0] = 5;
+    s[1] = 5;
+    s[2] = 2;
+    s[3] = 2;
+    shapes.push_back(s);
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::nchw));
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::oihw));
+  }
+  {
+    // 5D
+    TShape s(5);
+    s[0] = 5;
+    s[1] = 5;
+    s[2] = 2;
+    s[3] = 2;
+    s[4] = 1;
+    shapes.push_back(s);
+    pds.push_back(GetMemPD(s, dtype, mkldnn::memory::format::goihw));
+  }
+
+  // Reorder from the default to any other layout.
+  for (auto s : shapes) {
+    NDArray arr(s, Context());
+    InitArray(arr);
+    for (auto pd : pds) {
+      const mkldnn::memory *mem = arr.GetMKLDNNDataReorder(pd);
+      VerifyMem(*mem);
+    }
+  }
+
+  // Reorder from a special layout to another layout.
+//  for (auto s : shapes) {
+//    NDArray arr(s, Context());
+//    InitMKLDNNArray(arr);
+//    for (auto pd : pds) {
+//      const mkldnn::memory *mem = arr.GetMKLDNNDataReorder(pd);
+//      VerifyMem(*mem);
+//    }
+//  }
+}
+
 #endif
