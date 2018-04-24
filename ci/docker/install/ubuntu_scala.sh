db591424d3b2836959035483b48804797e424d2a
Aaron Markham <markhama@amazon.com>
2018-04-24 14:34:57 -0700

    scala dependencies merge

--- ci/docker/install/ubuntu_scala.sh
+++ ci/docker/install/ubuntu_scala.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+  #!/bin/bash
 
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -25,6 +25,8 @@ set -ex
 echo 'Installing Scala...'
 apt-get install -y software-properties-common
 apt-get update
+apt-get install -y openjdk-8-jdk
+apt-get install -y openjdk-8-jre
 
 echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
