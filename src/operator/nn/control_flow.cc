191fec203f0ce575de412fef919c3a84fa5e0ecb
Da Zheng <zhengda1936@gmail.com>
2018-04-30 18:56:46 +0000

    Use CachedOp to execute subgraph.

--- src/operator/nn/control_flow.cc
+++ src/operator/nn/control_flow.cc
@@ -30,132 +30,21 @@
 namespace mxnet {
 namespace op {
 
-void RunGraph(const nnvm::IndexedGraph& idx,
-    const std::vector<NDArray*> arrays,
-    size_t node_start, size_t node_end,
-    std::vector<OpReqType>&& array_reqs,
-    std::vector<uint32_t>&& ref_count,
-    std::vector<OpStatePtr> *p_states,
-    const DispatchModeVector &dispatch_modes) {
-  using namespace nnvm;
-  using namespace imperative;
-  static auto& createop = nnvm::Op::GetAttr<FCreateOpState>("FCreateOpState");
-  static auto& is_layer_backward = Op::GetAttr<bool>("TIsLayerOpBackward");
-
-  std::vector<OpStatePtr>& states = *p_states;
-  std::vector<NDArray*> ndinputs, ndoutputs;
-  ShapeVector arg_shapes;
-  DTypeVector arg_dtypes;
-  std::vector<OpReqType> req;
-
-  for (size_t i = node_start; i < node_end; ++i) {
-    const nnvm::IndexedGraph::Node& node = idx[i];
-    if (node.source->op() == nullptr) continue;
-    auto num_outputs = node.source->num_outputs();
-    ndinputs.clear();
-    ndinputs.reserve(node.inputs.size());
-    for (const auto& j : node.inputs) {
-      ndinputs.emplace_back(arrays[idx.entry_id(j)]);
-      CHECK(!ndinputs.back()->is_none()) << idx[j.node_id].source->attrs.name
-          << " " << j.index;
-    }
-    ndoutputs.clear();
-    ndoutputs.reserve(num_outputs);
-    req.clear();
-    req.reserve(num_outputs);
-    for (size_t j = 0; j < num_outputs; ++j) {
-      size_t eid = idx.entry_id(i, j);
-      ndoutputs.emplace_back(arrays[eid]);
-      req.push_back(array_reqs[eid]);
-      CHECK(!ndoutputs.back()->is_none());
-    }
-    const Context& ctx = ndoutputs[0]->ctx();
-    const DispatchMode dispatch_mode = dispatch_modes[i];
-    if (createop.count(node.source->op())) {
-      arg_shapes.clear();
-      arg_dtypes.clear();
-      arg_shapes.reserve(ndinputs.size());
-      arg_dtypes.reserve(ndinputs.size());
-      for (size_t i = 0; i < ndinputs.size(); ++i) {
-        arg_shapes.emplace_back(ndinputs[i]->shape());
-        arg_dtypes.emplace_back(ndinputs[i]->dtype());
-      }
-      states[i] = createop[node.source->op()](
-          node.source->attrs, ctx, arg_shapes, arg_dtypes);
-      Imperative::InvokeOp(ctx, node.source->attrs, ndinputs, ndoutputs, req,
-                           dispatch_mode, states[i]);
-    } else if (is_layer_backward.get(node.source->op(), false)) {
-      nnvm::Node* fwd_node = node.source->control_deps[0].get();
-      auto fwd_node_id = idx.node_id(fwd_node);
-      Imperative::InvokeOp(ctx, node.source->attrs, ndinputs, ndoutputs,
-                           req, dispatch_mode, states[fwd_node_id]);
-    } else {
-      Imperative::InvokeOp(ctx, node.source->attrs, ndinputs, ndoutputs,
-                           req, dispatch_mode);
-    }
-  }
-}
-
-static void ExecSubgraph(nnvm::Graph &g, const OpContext& ctx,
-                         const std::vector<NDArray>& cinputs,
+static void ExecSubgraph(nnvm::Symbol &sym, const OpContext& ctx,
+                         std::vector<NDArray> cinputs,
                          const std::vector<OpReqType>& req,
-                         const std::vector<NDArray>& coutputs) {
+                         std::vector<NDArray> coutputs) {
   using namespace nnvm;
   using namespace imperative;
-  const auto& idx = g.indexed_graph();
-  size_t num_inputs = idx.input_nodes().size();
 
-  CHECK_EQ(num_inputs, cinputs.size())
-      << "The subgraph requires " << num_inputs << " but got " << cinputs.size();
-
-  Context default_ctx = cinputs[0].ctx();
-  for (size_t i = 0; i < cinputs.size(); ++i) {
-    CHECK_EQ(cinputs[i].ctx(), default_ctx)
-        << "The subgraph requires all inputs to live on the same context. But "
-        << idx[idx.input_nodes()[0]].source->attrs.name << " is on " << default_ctx
-        << " while " << idx[idx.input_nodes()[i]].source->attrs.name << " is on "
-        << cinputs[i].ctx();
-  }
-
-  // TODO(zhengda) we might want to buffer them.
-  std::vector<NDArray> buff;
-  std::vector<OpStatePtr> states;
-  std::vector<NDArray> inputs = cinputs;
-  std::vector<NDArray> outputs = coutputs;
-
-  // Allocate entries
-  states.resize(idx.num_nodes());
-  buff.resize(idx.num_node_entries());
-  states.reserve(idx.num_nodes());
-  std::vector<NDArray*> arrays;
-  arrays.reserve(buff.size());
-  for (size_t i = 0; i < buff.size(); ++i) arrays.push_back(&buff[i]);
-  for (size_t i = 0; i < num_inputs; ++i) {
-    arrays[idx.entry_id(idx.input_nodes()[i], 0)] = &inputs[i];
-  }
-  for (size_t i = 0; i < idx.outputs().size(); ++i) {
-    auto eid = idx.entry_id(idx.outputs()[i]);
-    if (!arrays[eid]->is_none()) outputs[i] = arrays[eid]->Detach();
-    arrays[eid] = &outputs[i];
-  }
-
-  // Allocate memory for the NDArrays
-  std::vector<uint32_t> ref_count = g.GetAttr<std::vector<uint32_t> >(
-      ctx.is_train ? "full_ref_count" : "forward_ref_count");
-
-  std::vector<OpReqType> array_reqs(arrays.size(), kWriteTo);
-  for (size_t i = 0; i < idx.num_node_entries(); ++i) {
-    if (ref_count[i] == 0) array_reqs[i] = kNullOp;
-  }
-
-  const auto& mem_plan = g.GetAttr<MemoryPlanVector>(
-      ctx.is_train ? "full_mem_plan" : "forward_mem_plan");
-  AllocateMemory(g, idx, default_ctx, 0, idx.num_node_entries(),
-                 mem_plan, arrays, &array_reqs);
-
-  const auto& dispatch_modes = g.GetAttr<DispatchModeVector>("dispatch_mode");
-  RunGraph(idx, arrays, 0, idx.num_nodes(), std::move(array_reqs),
-      std::move(ref_count), &states, dispatch_modes);
+  std::vector<NDArray *> inputs(cinputs.size());
+  std::vector<NDArray *> outputs(coutputs.size());
+  for (size_t i = 0; i < inputs.size(); i++)
+    inputs[i] = &cinputs[i];
+  for (size_t i = 0; i < outputs.size(); i++)
+    outputs[i] = &coutputs[i];
+  Imperative::CachedOp op(sym, std::vector<std::pair<std::string, std::string> >());
+  op.Forward(nullptr, inputs, outputs);
 }
 
 struct ForeachParam : public dmlc::Parameter<ForeachParam> {
@@ -205,33 +94,6 @@ static void ForeachComputeExCPU(const nnvm::NodeAttrs& attrs,
   const ForeachParam& params = nnvm::get<ForeachParam>(attrs.parsed);
   CHECK_EQ(outputs.size(), (size_t) params.num_outputs);
   CHECK_EQ(attrs.subgraphs.size(), 1U);
-  nnvm::Graph &g = *attrs.subgraphs[0];
-  const auto& idx = g.indexed_graph();
-
-  // If this is inference, we only need the forward memory plan.
-  bool has_mem_plan = !ctx.is_train && g.attrs.count("forward_mem_plan");
-  // If this is training, we need the full memory plan.
-  has_mem_plan = has_mem_plan || (ctx.is_train && g.attrs.count("full_mem_plan"));
-  // If we don't have a memory plan yet, we need to create a memory plan.
-  if (!has_mem_plan) {
-    nnvm::StorageVector storage(idx.num_node_entries(), exec::kBadStorageID);
-    for (const auto i : idx.input_nodes())
-      storage[idx.entry_id(i, 0)] = exec::kExternalStorageID;
-    const auto& stypes = g.GetAttr<StorageTypeVector>("storage_type");
-    CHECK_EQ(stypes.size(), storage.size());
-    for (size_t i = 0; i < stypes.size(); i++) {
-      if (stypes[i] != kDefaultStorage)
-        storage[i] = exec::kDynamicStorageID;
-    }
-
-    auto mem_plan = imperative::PlanMemory(
-        &g, std::move(storage), g.GetAttr<std::vector<uint32_t> >(
-          ctx.is_train ? "full_ref_count" : "forward_ref_count"));
-    // TODO(zhengda) we need to be careful of changing graph attributes.
-    // It's not thread-safe.
-    g.attrs[ctx.is_train ? "full_mem_plan" : "forward_mem_plan"]
-      = std::make_shared<dmlc::any>(std::move(mem_plan));
-  }
   size_t len = inputs[0].shape()[0];
   CHECK_EQ(inputs[0].shape()[0], outputs[0].shape()[0]);
 
@@ -284,7 +146,7 @@ static void ForeachComputeExCPU(const nnvm::NodeAttrs& attrs,
       }
     }
 
-    ExecSubgraph(g, ctx, subg_inputs, req, *subg_out_curr);
+    ExecSubgraph(*attrs.subgraphs[0], ctx, subg_inputs, req, *subg_out_curr);
     // We need to wait for the iteration to complete before executing
     // the next one or return from the loop. In this way, we can reuse
     // the memory in the subgraph.
@@ -302,8 +164,8 @@ static bool ForeachShape(const nnvm::NodeAttrs& attrs,
   // foreach iterates over the first input NDArray over the first dimension.
   shape_inputs[0] = TShape(in_shape->at(0).begin() + 1, in_shape->at(0).end());
   CHECK_EQ(attrs.subgraphs.size(), 1U);
-  auto g = attrs.subgraphs[0];
-  CHECK(g);
+  auto g = std::make_shared<nnvm::Graph>();
+  g->outputs = attrs.subgraphs[0]->outputs;
   const auto& idx = g->indexed_graph();
   CHECK_EQ(idx.input_nodes().size(), in_shape->size());
   CHECK_EQ(idx.outputs().size(), out_shape->size());
@@ -346,8 +208,8 @@ static bool ForeachType(const nnvm::NodeAttrs& attrs,
   CHECK_EQ(out_type->size(), (size_t) params.num_outputs);
   nnvm::DTypeVector dtype_inputs = *in_type;
   CHECK_EQ(attrs.subgraphs.size(), 1U);
-  auto g = attrs.subgraphs[0];
-  CHECK(g);
+  auto g = std::make_shared<nnvm::Graph>();
+  g->outputs = attrs.subgraphs[0]->outputs;
   const auto& idx = g->indexed_graph();
   CHECK_EQ(idx.input_nodes().size(), in_type->size());
   CHECK_EQ(idx.outputs().size(), out_type->size());
@@ -380,8 +242,8 @@ static bool ForeachStorageType(const nnvm::NodeAttrs& attrs,
   const ForeachParam& params = nnvm::get<ForeachParam>(attrs.parsed);
   CHECK_EQ(out_attrs->size(), (size_t) params.num_outputs);
   CHECK_EQ(attrs.subgraphs.size(), 1U);
-  auto g = attrs.subgraphs[0];
-  CHECK(g);
+  auto g = std::make_shared<nnvm::Graph>();
+  g->outputs = attrs.subgraphs[0]->outputs;
   const auto& idx = g->indexed_graph();
   CHECK_EQ(idx.input_nodes().size(), in_attrs->size());
   CHECK_EQ(idx.outputs().size(), out_attrs->size());
