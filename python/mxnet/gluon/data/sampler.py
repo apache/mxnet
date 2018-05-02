23934cf62ea92bf12876ac27fb63bbc1bf4a9490
Leonard Lausen <leonard@lausen.nl>
2018-05-01 23:51:47 -0700

    Use numpy.arange in RandomSampler (#10768)
    Significant speedup for large datasets:
    
    In [2]: %timeit current_sample(1529*8192)
    12.3 s ± 721 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    
    In [3]: %timeit np_sample(1529*8192)
    641 ms ± 6.7 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)

--- python/mxnet/gluon/data/sampler.py
+++ python/mxnet/gluon/data/sampler.py
@@ -20,7 +20,7 @@
 """Dataset sampler."""
 __all__ = ['Sampler', 'SequentialSampler', 'RandomSampler', 'BatchSampler']
 
-import random
+import numpy as np
 
 class Sampler(object):
     """Base class for samplers.
@@ -65,8 +65,8 @@ class RandomSampler(Sampler):
         self._length = length
 
     def __iter__(self):
-        indices = list(range(self._length))
-        random.shuffle(indices)
+        indices = np.arange(self._length)
+        np.random.shuffle(indices)
         return iter(indices)
 
     def __len__(self):
