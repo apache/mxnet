0111a36793df919ae490299a0edaaddedc9f1287
Da Zheng <zhengda1936@gmail.com>
2018-05-03 10:27:01 -0700

    Fix a bug in getting MKLDNN memory (#10731)
    * test inference multiple times.
    
    * Fix a bug in GetMKLDNNData().
    
    * Update comments.
    
    * Handle all cases for GetMKLDNNDataReorder
    
    * avoid unnecessary message.
    
    * Add C++ unit test for NDArray.
    
    * Fix a minor bug.
    
    * Unit tests on GetMKLDNNDataReorder.
    
    * Fix lint error.
    
    * Add more test cases.
    
    * add comments for the test code.
    
    * Reorganize test code.
    
    * Fix cpp tests.
    
    * test.
    
    * Add a new Jenkins compile task.
    
    * Update jenkins.
    
    * update jenkins.
    
    * Fix a Jenkins.
    
    * Fix jenkins.
    
    * Fix jenkins.
    
    * Fix CMake for MKLDNN.
    
    * Fix jenkins.
    
    * update jenkins.
    
    * update CMake.
    
    * Fix cmake.
    
    * update CI.
    
    * add comment.
    
    * add comments.
    
    * cmake builds mkldnn with -mtune=generic by default.
    
    * adjust comments.
    
    remove unnecessary tests.

--- src/operator/nn/mkldnn/mkldnn_base.cc
+++ src/operator/nn/mkldnn/mkldnn_base.cc
@@ -25,6 +25,11 @@
 
 namespace mxnet {
 
+MKLDNNStream *MKLDNNStream::Get() {
+  static thread_local MKLDNNStream stream;
+  return &stream;
+}
+
 void *AlignMem(void *mem, size_t size, size_t alignment, size_t *space) {
   if (size > *space)
     return nullptr;
@@ -57,8 +62,11 @@ mkldnn::memory *TmpMemMgr::Alloc(const mkldnn::memory::primitive_desc &pd) {
     this->curr_mem = static_cast<char *>(mem) + pd.get_size();
     return ret.get();
   } else {
-    LOG(WARNING) << "Allocate " << pd.get_size()
-        << " bytes with malloc directly";
+    // If curr_mem has been initialized and we still reach here. It means
+    // the current allocated memory isn't enough.
+    if (this->curr_mem)
+      LOG(WARNING) << "Allocate " << pd.get_size()
+          << " bytes with malloc directly";
     mkldnn_mem_ptr ret(new mkldnn::memory(pd));
     MKLDNNStream::Get()->RegisterMem(ret);
     return ret.get();
