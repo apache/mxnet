ca187ab88644e7314fe7418d08b7f42855723de2
Ashok Emani <ashok.emani@intel.com>
2018-05-03 20:26:53 -0700

    [MXNET-367] update mkldnn to v0.14 and disable building test examples (#10736)
    * update mkldnn to v0.14 and disable building test examples
    
    * update mkldnn to v0.14 release
    
    * use git submodule tagging for mkldnn
    
    * update mklml to latest version

--- ci/docker/install/ubuntu_mklml.sh
+++ ci/docker/install/ubuntu_mklml.sh
@@ -21,5 +21,5 @@
 # the whole docker cache for the image
 
 set -ex
-wget --no-check-certificate -O /tmp/mklml.tgz https://github.com/intel/mkl-dnn/releases/download/v0.12/mklml_lnx_2018.0.1.20171227.tgz
+wget --no-check-certificate -O /tmp/mklml.tgz https://github.com/intel/mkl-dnn/releases/download/v0.14/mklml_lnx_2018.0.3.20180406.tgz
 tar -zxvf /tmp/mklml.tgz && cp -rf mklml_*/* /usr/local/ && rm -rf mklml_*
