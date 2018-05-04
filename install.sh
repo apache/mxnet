750ec10315f40ce1887b6c89bfe28e0b2544ebc5
Dong Liu <dongeliu@ustc.edu.cn>
2018-05-04 18:41:53 +0800

    Add install.sh for Linux

---
+++ install.sh
@@ -0,0 +1,5 @@
+﻿cp -r ./3rdparty/dmlc-core/include/dmlc /usr/include/
+cp -r ./3rdparty/nnvm/include/nnvm /usr/include/
+cp -r ./include/mxnet /usr/include/
+cp -r ./cpp-package/include/mxnet-cpp /usr/include/
+cp ./lib/* /usr/lib/
