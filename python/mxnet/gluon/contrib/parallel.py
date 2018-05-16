6ae71c792a197b44decaa0ebe4bd42d4f40a16a8
Zhang <zhang.hang@rutgers.edu>
2018-05-16 14:23:42 -0700

    tuplemap

--- python/mxnet/gluon/contrib/parallel.py
+++ python/mxnet/gluon/contrib/parallel.py
@@ -153,7 +153,7 @@ class DataParallelModel(object):
         inputs, kwargs = split_load_kwargs(inputs, kwargs, self.ctx_list)
         assert(len(inputs) == len(self.ctx_list))
         if len(self.ctx_list) == 1:
-            return tuple([tuple([self.module(*inputs[0], **kwargs[0])])])
+            return tuple([tuple_map(self.module(*inputs[0], **kwargs[0]))])
         return parallel_apply(self.module, inputs, kwargs, self.sync)
 
     def __repr__(self):
@@ -200,7 +200,7 @@ class DataParallelCriterion(object):
         targets, kwargs = split_load_kwargs(targets, kwargs, self.ctx_list)
         assert(len(targets) == len(self.ctx_list))
         if len(self.ctx_list) == 1:
-            return tuple([self.module(*(inputs[0] + targets[0]), **kwargs[0])])
+            return tuple_map(self.module(*(inputs[0] + targets[0]), **kwargs[0]))
         assert(len(inputs) == len(self.ctx_list))
         return criterion_parallel_apply(self.module, inputs, targets, kwargs, self.sync)
 
@@ -228,6 +228,14 @@ def split_load_kwargs(inputs, kwargs, ctx_list, batch_axis=0):
     return inputs, kwargs
 
 
+def tuple_map(obj):
+    if isinstance(obj, NDArray):
+        return (obj,)
+    if isinstance(obj, list) and len(obj) > 0:
+        return tuple(obj)
+    return obj
+
+
 def parallel_apply(module, inputs, kwargs_tup=None, sync=False):
     """Parallel applying model forward"""
     if kwargs_tup is not None:
@@ -242,11 +250,11 @@ def parallel_apply(module, inputs, kwargs_tup=None, sync=False):
         try:
             if is_recording:
                 with autograd.record(is_training):
-                    output = output = tuple([module(*input, **kwargs)])
+                    output =tuple_map(module(*input, **kwargs))
                     for out in output:
                         out.wait_to_read()
             else:
-                output = module(*input, **kwargs)
+                output = tuple_map(module(*input, **kwargs))
                 for out in output:
                     out.wait_to_read()
             with lock:
@@ -277,7 +285,8 @@ def parallel_apply(module, inputs, kwargs_tup=None, sync=False):
             outputs.append(output)
         return tuple(outputs)
     else:
-        outputs = [tuple([module(*input, **kwargs)]) for (input, kwargs) in zip(inputs, kwargs_tup)]
+        outputs = [tuple_map(module(*input, **kwargs))
+                   for (input, kwargs) in zip(inputs, kwargs_tup)]
         return tuple(outputs)
 
 
