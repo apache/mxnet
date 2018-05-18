7f81fd7383f4c1dc63b41adff336d0900f2a2c9b
Zhang <zhang.hang@rutgers.edu>
2018-05-17 17:44:37 -0700

    omp on channels

--- src/operator/contrib/roi_align.cc
+++ src/operator/contrib/roi_align.cc
@@ -156,10 +156,7 @@ void ROIAlignForward(
   int n_rois = nthreads / channels / pooled_width / pooled_height;
   // (n, c, ph, pw) is an element in the pooled output
   // can be parallelized using omp
-  int n;
-#pragma omp parallel for private(n) \
-num_threads(engine::OpenMP::Get()->GetRecommendedOMPThreadCount())
-  for (n = 0; n < n_rois; n++) {
+  for (int n = 0; n < n_rois; n++) {
     int index_n = n * channels * pooled_width * pooled_height;
 
     // roi could have 4 or 5 columns
