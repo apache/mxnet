6d02ddd2d4f68fa1f0f1abf2d80d4b80d2fe676e
Anton Chernov <mechernov@gmail.com>
2018-05-25 14:11:39 +0200

    Fixed bash default value substitution

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -36,8 +36,8 @@ build_wheel() {
     set -ex
     pushd .
 
-    PYTHON_DIR = ${1:/work/mxnet/python}
-    BUILD_DIR = ${2:/work/build}
+    PYTHON_DIR = ${1:-/work/mxnet/python}
+    BUILD_DIR = ${2:-/work/build}
 
     # build
 
