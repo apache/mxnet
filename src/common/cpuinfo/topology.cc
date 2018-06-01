89ed054c43c853c1575935120a0a483335e1de11
Alexander Zai <alexzai@amazon.com>
2018-05-31 18:51:16 -0700

    add license to cpuinfo files

--- src/common/cpuinfo/topology.cc
+++ src/common/cpuinfo/topology.cc
@@ -1,3 +1,30 @@
+//Copyright (c) 2017-2018 Facebook Inc.
+//Copyright (C) 2012-2017 Georgia Institute of Technology
+//Copyright (C) 2010-2012 Marat Dukhan
+//
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//    modification, are permitted provided that the following conditions are met:
+//
+//* Redistributions of source code must retain the above copyright notice, this
+//list of conditions and the following disclaimer.
+//
+//* Redistributions in binary form must reproduce the above copyright notice,
+//this list of conditions and the following disclaimer in the documentation
+//and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -5,7 +32,7 @@
 #include "./utils.h"
 #include "./api.h"
 #include "./cpuid.h"
-#include "./log.h"
+#include <dmlc/logging.h>
 
 
 enum topology_type {
@@ -36,7 +63,6 @@ void cpuinfo_x86_detect_topology(
 			topology->core_bits_offset = log2_max_threads_per_core;
 			topology->thread_bits_length = log2_max_threads_per_core;
 		}
-		cpuinfo_log_debug("HTT: APIC ID = %08, logical processors = %", apic_id, logical_processors);
 	}
 
 	/*
