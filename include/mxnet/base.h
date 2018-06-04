9a5f8821cb611288eb3a61886b8fba34ffc805a6
eric-haibin-lin <linhaibin.eric@gmail.com>
2018-06-04 23:50:48 +0000

    bump version for python & cpp

--- include/mxnet/base.h
+++ include/mxnet/base.h
@@ -104,7 +104,7 @@
 /*! \brief minor version */
 #define MXNET_MINOR 2
 /*! \brief patch version */
-#define MXNET_PATCH 0
+#define MXNET_PATCH 1
 /*! \brief mxnet version */
 #define MXNET_VERSION (MXNET_MAJOR*10000 + MXNET_MINOR*100 + MXNET_PATCH)
 /*! \brief helper for making version number */
