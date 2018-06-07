3a13ae2844dd5c5e0b92ce5b1e2131b93bc2368a
Krishnan <krishnan.amazon.murali@gmail.com>
2018-06-07 20:18:57 +0000

    Re-arranging the versionString local variable

--- docs/_static/js/versions_drop_down.js
+++ docs/_static/js/versions_drop_down.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
       function loadVersionURL() {
-          var el = $(this);
+          let el = $(this);
           console.log("loadVersionURL");
-          window.location.pathname = '/versions/' + $(this).text().substr(1) + window.location.pathname ;
+          let versionString = '';
+          if ($(this).text().includes("master")) {
+              versionString = $(this).text();
+          } else {
+              //Remove the character v at the beginning
+              versionString = $(this).text().substr(1);
+          }
+          window.location.pathname = '/versions/' + versionString + window.location.pathname ;
       }
       $('.opt-group').on('click', '.versions', loadVersionURL);
 });
