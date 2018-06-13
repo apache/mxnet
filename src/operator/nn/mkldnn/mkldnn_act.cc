7ffb3bf298d1d6f843d995aa3353ef78d97c6195
Alexander Zai <alexzai@amazon.com>
2018-06-13 11:01:28 -0700

    fix order

--- src/operator/nn/mkldnn/mkldnn_act.cc
+++ src/operator/nn/mkldnn/mkldnn_act.cc
@@ -173,11 +173,11 @@ void MKLDNNActivationForward(const nnvm::NodeAttrs& attrs, const OpContext &ctx,
   if (out_mem != nullptr) {
     fwd.SetNewMem(*input_mem, *out_mem);
     stream->RegisterPrim(fwd.GetFwd());
-    CommitOutput(out_data, out_mem);
   } else {
     auto tmp_mem = CreateMKLDNNMem(out_data, fwd.fwd_pd.dst_primitive_desc(), req);
     fwd.SetNewMem(*input_mem, *tmp_mem.second);
     stream->RegisterPrim(fwd.GetFwd());
+    CommitOutput(out_data, out_mem);
   }
   stream->Submit();
 }
