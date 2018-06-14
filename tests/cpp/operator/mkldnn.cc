9b60c24e543eb578937cb7cfc4639aa0161ad3a9
Alexander Zai <alexzai@amazon.com>
2018-06-14 12:35:45 -0700

    out_mem can be null

--- tests/cpp/operator/mkldnn.cc
+++ tests/cpp/operator/mkldnn.cc
@@ -828,7 +828,7 @@ TEST(MKLDNN_BASE, CreateMKLDNNMem) {
                                                              InitDefaultArray);
     for (auto out_arr : out_arrs) {
       auto in_mem = in_arr.arr.GetMKLDNNData();
-      auto out_mem = out_arr.arr.GetMKLDNNData();
+      auto out_mem = out_arr.arr.GetMKLDNNData(in_mem->get_primitive_desc());
 
       // TODO(alexzai) : remove this noop when by reordering in MKLDNNSum
       if (out_mem == nullptr)
