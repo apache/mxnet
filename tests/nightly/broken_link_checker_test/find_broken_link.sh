3271cd1743bf4c3ea1e0c5c134577531c4fcdd38
Amol Lele <19983848+leleamol@users.noreply.github.com>
2018-06-15 12:47:18 -0700

    Trying npm installation without sudo

--- tests/nightly/broken_link_checker_test/find_broken_link.sh
+++ tests/nightly/broken_link_checker_test/find_broken_link.sh
@@ -4,7 +4,8 @@ set -e
 
 # Install npmi
 echo "Installing npm"
-sudo apt-get install -y npm
+apt-get update
+apt-get install -y npm
 
 # Install nodejs- v 7.10.1
 #apt-get install -y curl python-software-properties
@@ -12,7 +13,7 @@ echo "Obtaining NodeJS version 8.x"
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
 echo "Installing nodejs"
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Install broken link checker utility
 npm install broken-link-checker -g
