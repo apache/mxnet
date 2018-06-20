8e2b4a82285607ef50c073b262aec338407e278e
Anirudh Subramanian <anirudh2290@ufl.edu>
2018-06-20 05:09:05 +0000

    Add elemwise binary broadcast op cuh file

---
+++ src/operator/tensor/elemwise_binary_broadcast_op-inl.cuh
@@ -0,0 +1,46 @@
+
+#ifndef MXNET_OPERATOR_TENSOR_ELEMWISE_BINARY_BROADCAST_OP_CUH_
+#define MXNET_OPERATOR_TENSOR_ELEMWISE_BINARY_BROADCAST_OP_CUH_
+#include <mxnet/operator_util.h>
+#include <mxnet/op_attr_types.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <utility>
+#include "broadcast_reduce-inl.h"
+namespace mxnet {
+namespace op {
+template<typename xpu, typename LOP, typename ROP>
+inline typename std::enable_if<std::is_same<xpu, gpu>::value, void>::type
+BinaryBroadcastBackwardUseNone(const nnvm::NodeAttrs& attrs,
+                                    const OpContext& ctx,
+                                    const std::vector<TBlob>& inputs,
+                                    const std::vector<OpReqType>& req,
+                                    const std::vector<TBlob>& outputs) {
+  using namespace broadcast;
+  TShape new_lshape, new_rshape, new_oshape;
+  int ndim = BinaryBroadcastShapeCompact(outputs[0].shape_, outputs[1].shape_, inputs[0].shape_,
+                                         &new_lshape, &new_rshape, &new_oshape);
+  if (!ndim) {
+    ElemwiseBinaryOp::BackwardUseNone<xpu, LOP, ROP>(attrs, ctx, inputs, req, outputs);
+  } else {
+    MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+      Stream<xpu> *s = ctx.get_stream<xpu>();
+      const TBlob lhs = outputs[0].reshape(new_lshape);
+      const TBlob rhs = outputs[1].reshape(new_rshape);
+      const TBlob out = inputs[0].reshape(new_oshape);
+      BROADCAST_NDIM_SWITCH(ndim, NDim, {
+        // Request temporary storage
+        size_t workspace_size = new_oshape.Size();
+        Tensor<xpu, 1, char> workspace =
+            ctx.requested[0].get_space_typed<xpu, 1, char>(
+                Shape1(workspace_size * sizeof(index_t)), s);
+        Reduce<red::sum, NDim, DType, LOP>(s, lhs, req[0], workspace, out);
+        Reduce<red::sum, NDim, DType, ROP>(s, rhs, req[1], workspace, out);
+      });
+    });
+  }
+}
+} // namespace op
+} // namespace mxnet
+#endif
