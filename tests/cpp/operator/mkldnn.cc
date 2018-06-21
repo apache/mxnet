c6b8b9fc1fc586194ceadfe05a5fc2117b3a9ff5
Alexander Zai <alexzai@amazon.com>
2018-06-20 17:06:47 -0700

    use sum_pd when calling CreateMKLDNNData

--- tests/cpp/operator/mkldnn.cc
+++ tests/cpp/operator/mkldnn.cc
@@ -823,12 +823,9 @@ TEST(MKLDNN_BASE, MKLDNNSum) {
     for (auto out_arr : out_arrs) {
       auto in_mem1 = in_arr.arr.GetMKLDNNData();
       auto in_mem2 = in_arr2.arr.GetMKLDNNData();
-
       if (out_arr.arr.IsView())
         continue;
-
       auto out_mem = out_arr.arr.GetMKLDNNData(in_mem1->get_primitive_desc());
-
       // TODO(alexzai) : remove this noop when by reordering in MKLDNNSum
       if (out_mem == nullptr)
         continue;
@@ -861,6 +858,15 @@ TEST(MKLDNN_BASE, MKLDNNSum) {
   }
 }
 
+mkldnn::sum::primitive_desc GetSumPD(const mkldnn::memory *in_mem, const mkldnn::memory *in_mem2) {
+  std::vector<mkldnn::memory::primitive_desc> input_pds(2);
+  std::vector<float> scales(2, 1);
+  std::vector<mkldnn::primitive::at> inputs;
+  input_pds[0] = in_mem->get_primitive_desc();
+  input_pds[1] = in_mem2->get_primitive_desc();
+  return mkldnn::sum::primitive_desc(scales, input_pds);
+}
+
 TEST(MKLDNN_BASE, CreateMKLDNNMem) {
   std::vector<NDArrayAttrs> in_arrs = GetTestInputArrays();
   std::vector<NDArrayAttrs> in_arrs2 = GetTestInputArrays(true);
@@ -884,7 +890,8 @@ TEST(MKLDNN_BASE, CreateMKLDNNMem) {
       NDArray orig_output = out_arr.arr.Copy(out_arr.arr.ctx());
       orig_output.WaitToRead();
       PrintVerifyMsg(in_arr, out_arr);
-      auto output_mem_t = CreateMKLDNNMem(out_arr.arr, in_mem->get_primitive_desc(), kWriteTo);
+      auto sum_pd = GetSumPD(in_mem, in_mem2);
+      auto output_mem_t = CreateMKLDNNMem(out_arr.arr, sum_pd.dst_primitive_desc(), kWriteTo);
       op::MKLDNNSum(*in_mem, *in_mem2, *output_mem_t.second);
       CommitOutput(out_arr.arr, output_mem_t);
       stream->Submit();
