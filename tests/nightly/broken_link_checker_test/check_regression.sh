eaa6996cf6a48e0649a848e68fc602121b403356
Amol Lele <19983848+leleamol@users.noreply.github.com>
2018-06-21 09:42:08 -0700

    Added the license header to the files

--- tests/nightly/broken_link_checker_test/check_regression.sh
+++ tests/nightly/broken_link_checker_test/check_regression.sh
@@ -1,5 +1,23 @@
 #! /bin/sh
 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 echo "Running the check_regression.sh script"
 cat blc_output.txt | uniq | grep -Eo "(http|https).* " | sort| uniq > unique_current_urls.txt
 
@@ -22,6 +40,6 @@ rm -rf unique_current_urls.txt
 rm -rf blc_output.txt
 if [ $regression ]; then
 	echo "FAIL: Found Regression in broken link checker"
-else 
+else
 	echo "SUCCESS: No Regression found"
 fi
--- tests/nightly/broken_link_checker_test/test_broken_links.py
+++ tests/nightly/broken_link_checker_test/test_broken_links.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 import sys
 from subprocess import check_output, STDOUT, CalledProcessError
 
@@ -9,7 +27,7 @@ def prepare_link_test_result(command_output):
     NEW_PAGE_TEST_START_REGEX = "Getting links from:"
     BROKEN_PAGE_START_REGEX = "BROKEN"
     PAGE_TEST_END_REGEX = "Finished! "
-    
+
     # Whitelisted broken links patterns
     HTTP_403_REGEX = "(HTTP_403)"
     HTTP_401_REGEX = "(HTTP_401)"
