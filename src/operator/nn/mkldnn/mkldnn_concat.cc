1573f24a06e830963ce634d60a7693a1d8ab0022
Alexander Zai <alexzai@amazon.com>
2018-06-21 15:02:07 -0700

    reshape if view and mkldnn

--- src/operator/nn/mkldnn/mkldnn_concat.cc
+++ src/operator/nn/mkldnn/mkldnn_concat.cc
@@ -107,13 +107,18 @@ void MKLDNNConcatForward(const nnvm::NodeAttrs& attrs, const OpContext &ctx,
   std::vector<const mkldnn::memory *> data_mem;
   data_md.reserve(num_in_data);
   data_mem.reserve(num_in_data);
-  for (int i =0; i < num_in_data; i++) {
-    const mkldnn::memory *tmp_mem = in_data[i].GetMKLDNNData();
+  std::vector<NDArray> in_buffer;
+  for (int i = 0; i < num_in_data; i++) {
+    NDArray nd = in_data[i];
+    if (nd.IsView() && nd.IsMKLDNNData())
+      nd = nd.Reorder2Default();
+    const mkldnn::memory *tmp_mem = nd.GetMKLDNNData();
     mkldnn::memory::primitive_desc tmp_pd = tmp_mem->get_primitive_desc();
     data_md.push_back(tmp_pd);
     data_mem.push_back(tmp_mem);
+    in_buffer.push_back(nd);
   }
-  MKLDNNConcatFwd &fwd = GetConcatForward(concat_dim, in_data, data_md);
+  MKLDNNConcatFwd &fwd = GetConcatForward(concat_dim, in_buffer, data_md);
   mxnet::mkldnn_output_t out_mem = CreateMKLDNNMem(out_data[concat_enum::kOut],
                                                    fwd.fwd_pd.dst_primitive_desc(),
                                                    req[concat_enum::kOut]);
