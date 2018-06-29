a4054cd5b20ebc12409effa398b1a32329bb91bf
kpmurali <37911926+kpmurali@users.noreply.github.com>
2018-06-28 20:25:14 -0700

    [MXNET-607] Fix the broken reported by the new BLC (#11465)
    * Fixing the broken for the moved directories in ap/python and scala imageclassifier and SSDClassifier
    
    * Fixing the broken for the moved directories in ap/python and scala imageclassifier and SSDClassifier

--- scala-package/examples/src/main/scala/org/apache/mxnetexamples/infer/imageclassifier/ImageClassifierExample.scala
+++ scala-package/examples/src/main/scala/org/apache/mxnetexamples/infer/imageclassifier/ImageClassifierExample.scala
@@ -31,9 +31,9 @@ import scala.collection.mutable.ListBuffer
 /**
   * <p>
   * Example inference showing usage of the Infer package on a resnet-152 model.
-  * @see <a href="https://github.com/apache/incubator-mxnet/tree/m\
-  * aster/scala-package/examples/src/main/scala/org/apache/mxnetexamples/in\
-  * fer/imageclassifier" target="_blank">Instructions to run this example</a>
+  * @see <pre><a href="https://github.com/apache/incubator-mxnet/tree/master/s
+    cala-package/examples/src/main/scala/org/apache/mxnetexamples/infer/im
+    ageclassifier" target="_blank">Instructions to run this example</a></pre>
   */
 object ImageClassifierExample {
 
