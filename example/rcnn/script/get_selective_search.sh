a844b34e9ca81819a3bbba714a2315d11d4c277c
Christos Smailis <smailisxristos@yahoo.com>
2018-06-29 01:10:34 -0500

    # updated the link as the previous one was corrupted
    # updated the selective_search_data link as the previous one was corrupted
    the new link is wget https://dl.dropboxusercontent.com/s/orrt7o6bp6ae0tc/selective_search_data.tgz

--- example/rcnn/script/get_selective_search.sh
+++ example/rcnn/script/get_selective_search.sh
@@ -27,7 +27,7 @@ fi
 pushd data
 
 # the result is selective_search_data
-wget http://www.cs.berkeley.edu/~rbg/fast-rcnn-data/selective_search_data.tgz
-tar xf selective_search_data.tgz
+wget https://dl.dropboxusercontent.com/s/orrt7o6bp6ae0tc/selective_search_data.tgz
+tar -xf selective_search_data.tgz
 
 popd
