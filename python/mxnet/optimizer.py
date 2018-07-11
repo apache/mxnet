57e4d03ddb6af6e461bf0c61d514438c6f64758f
eric-haibin-lin <linhaibin.eric@gmail.com>
2018-07-11 22:16:59 +0000

    add doc for clarification;

--- python/mxnet/optimizer.py
+++ python/mxnet/optimizer.py
@@ -550,7 +550,7 @@ class SGD(Optimizer):
 class Signum(Optimizer):
     """The Signum optimizer that takes the sign of gradient or momentum.
 
-    The optimizer updates the weight by:
+    The optimizer updates the weight by::
 
         rescaled_grad = rescale_grad * clip(grad, clip_gradient) + wd * weight
         state = momentum * state + (1-momentum)*rescaled_grad
