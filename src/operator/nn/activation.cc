9d550a11397835dd5c34519ae48dd02ffd790266
luobao-intel <luobao.zou@intel.com>
2018-07-17 15:14:49 +0800

    test_try

--- src/operator/nn/activation.cc
+++ src/operator/nn/activation.cc
@@ -38,7 +38,6 @@ namespace mxnet {
 namespace op {
 
 DMLC_REGISTER_PARAMETER(ActivationParam);
-
 // This will determine the order of the inputs for backward computation.
 struct ActivationGrad {
   const char *op_name;
