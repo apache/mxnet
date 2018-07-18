5cbadda482f1ac84148fd831d7a42a2963a7f854
Anirudh Subramanian <anirudh2290@ufl.edu>
2018-07-18 17:43:48 +0000

    Add comment for cublassgemmex

--- src/operator/linalg_impl.h
+++ src/operator/linalg_impl.h
@@ -186,6 +186,8 @@ void linalg_gemm<cpu, mshadow::half::half_t>(const Tensor<cpu, 2, mshadow::half:
         A.stride_, &beta, C.dptr_, C.stride_))                             \
   }
 
+// Use cublasSgemmEx when it is available (CUDA >= 7.5). Resolves precision issues with
+// cublasSgemm. Please see https://github.com/apache/incubator-mxnet/pull/11630
 #if CUDA_VERSION >= 7050
 template <>
 inline void linalg_gemm<gpu, float>(const Tensor<gpu, 2, float>& A,
