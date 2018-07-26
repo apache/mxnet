3e22e7f5d130f82d275084031ec67b3d97db71f2
luobao-intel <luobao.zou@intel.com>
2018-07-26 14:30:26 +0800

    activation second

--- src/operator/nn/activation.cc
+++ src/operator/nn/activation.cc
@@ -110,14 +110,14 @@ inline static bool ActivationStorageType(const nnvm::NodeAttrs& attrs,
                                          std::vector<int> *out_attrs) {
   CHECK_EQ(in_attrs->size(), 1);
   CHECK_EQ(out_attrs->size(), 1);
-  bool ret = ElemwiseStorageType<1, 1, false, false, false>(attrs, dev_mask,
-                                                            dispatch_mode,
-                                                            in_attrs, out_attrs);
 #if MXNET_USE_MKLDNN == 1
   const ActivationParam& param = nnvm::get<ActivationParam>(attrs.parsed);
-  bool support_mkldnn_act = SupportMKLDNNAct(param);
-  ret = MKLDNNStorageType(attrs, dev_mask, support_mkldnn_act, dispatch_mode,
-                          in_attrs, out_attrs);
+  bool support_mkldnn = SupportMKLDNNAct(param);
+  bool ret = MKLDNNStorageType(attrs, dev_mask, support_mkldnn, dispatch_mode,
+                               in_attrs, out_attrs);
+#else
+  bool ret = ElemwiseStorageType<1, 1, false, false, false>(
+      attrs, dev_mask, dispatch_mode, in_attrs, out_attrs);
 #endif
   return ret;
 }
