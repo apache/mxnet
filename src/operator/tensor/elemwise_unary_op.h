0b02dbe6da7553c97cb667a7cb331d456148c775
Sebastian Bodenstein <sebastianb@wolfram.com>
2018-07-30 03:15:20 +0300

    first commit

--- src/operator/tensor/elemwise_unary_op.h
+++ src/operator/tensor/elemwise_unary_op.h
@@ -476,6 +476,30 @@ void HardSigmoidBackward(const nnvm::NodeAttrs& attrs,
   });
 }
 
+struct ReshapeLikeParam : public dmlc::Parameter<ReshapeLikeParam> {
+  int lhs_begin, rhs_begin;
+  dmlc::optional<int> lhs_end, rhs_end;
+  DMLC_DECLARE_PARAMETER(ReshapeLikeParam) {
+    DMLC_DECLARE_FIELD(lhs_begin).set_default(0).describe(
+        "Defaults to 0. "
+        "The beginning index along the lhs dimensions to be reshaped. "
+        "Supports negative indices.");
+    DMLC_DECLARE_FIELD(lhs_end)
+        .set_default(dmlc::optional<int>())
+        .describe("Defaults to None. The ending index to be used, "
+                  "The ending index along the lhs dimensions to be reshaped. "
+                  "Supports negative indices.");
+    DMLC_DECLARE_FIELD(rhs_begin).set_default(0).describe(
+        "Defaults to 0. "
+        "The ending index along the rhs dimensions to be reshaped. "
+        "Supports negative indices.");
+    DMLC_DECLARE_FIELD(rhs_end)
+        .set_default(dmlc::optional<int>())
+        .describe("Defaults to None."
+                  " . Supports negative indices.");
+  }
+};
+
 /*! \brief Unary compute */
 #define MXNET_OPERATOR_REGISTER_UNARY(__name$)                      \
   NNVM_REGISTER_OP(__name$)                                         \
