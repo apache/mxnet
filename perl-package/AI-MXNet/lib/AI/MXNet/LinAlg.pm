cdda5c11de2872f01907db3a4be1825db9d4c2b9
Sergey Kolychev <sergeykolychev.github@gmail.com>
2018-08-08 09:38:32 -0700

    MXNET-776 [Perl] Better documentation/bug fixes. (#12038)
    * MXNET-776
    1) Several new metric classes.
    2) Improved documentation.
    3) Bugfixes.
    
    * added links and fixed a typo.

--- perl-package/AI-MXNet/lib/AI/MXNet/LinAlg.pm
+++ perl-package/AI-MXNet/lib/AI/MXNet/LinAlg.pm
@@ -21,6 +21,54 @@ use warnings;
 use AI::MXNet::LinAlg::Symbol;
 use AI::MXNet::LinAlg::NDArray;
 
+=head1 NAME
+
+    AI::MXNet::LinAlg - Linear Algebra routines for NDArray and Symbol.
+=cut
+
+=head1 DESCRIPTION
+
+    The Linear Algebra API, provides imperative/symbolic linear algebra tensor operations on CPU/GPU.
+
+    mx->linalg-><sym|nd>->gemm  Performs general matrix multiplication and accumulation.
+    mx->linalg-><sym|nd>->gemm2 Performs general matrix multiplication.
+    mx->linalg-><sym|nd>->potrf Performs Cholesky factorization of a symmetric positive-definite matrix.
+    mx->linalg-><sym|nd>->potri Performs matrix inversion from a Cholesky factorization.
+    mx->linalg-><sym|nd>->trmm  Performs multiplication with a lower triangular matrix.
+    mx->linalg-><sym|nd>->trsm  Solves matrix equation involving a lower triangular matrix.
+    mx->linalg-><sym|nd>->sumlogdiag    Computes the sum of the logarithms of the diagonal elements of a square matrix.
+    mx->linalg-><sym|nd>->syrk  Multiplication of matrix with its transpose.
+    mx->linalg-><sym|nd>->gelqf LQ factorization for general matrix.
+    mx->linalg-><sym|nd>->syevd Eigendecomposition for symmetric matrix.
+    L<NDArray Python Docs|http://mxnet.incubator.apache.org/api/python/ndarray/linalg.html>
+    L<Symbol Python Docs|http://mxnet.incubator.apache.org/api/python/symbol/linalg.html>
+
+    Examples:
+
+    ## NDArray
+    my $A = mx->nd->array([[1.0, 1.0], [1.0, 1.0]]);
+    my $B = mx->nd->array([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]);
+    ok(almost_equal(
+        mx->nd->linalg->gemm2($A, $B, transpose_b=>1, alpha=>2.0)->aspdl,
+        pdl([[4.0, 4.0, 4.0], [4.0, 4.0, 4.0]])
+    ));
+
+    ## Symbol
+    my $sym_gemm2 = mx->sym->linalg->gemm2(
+        mx->sym->var('A'),
+        mx->sym->var('B'),
+        transpose_b => 1,
+        alpha => 2.0
+    );
+    my $A = mx->nd->array([[1.0, 1.0], [1.0, 1.0]]);
+    my $B = mx->nd->array([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]);
+    ok(almost_equal(
+        $sym_gemm2->eval(args => { A => $A, B => $B })->[0]->aspdl,
+        pdl([[4.0, 4.0, 4.0], [4.0, 4.0, 4.0]])
+    ));
+
+=cut
+
 sub sym    { 'AI::MXNet::LinAlg::Symbol'  }
 sub symbol { 'AI::MXNet::LinAlg::Symbol'  }
 sub nd     { 'AI::MXNet::LinAlg::NDArray' }
