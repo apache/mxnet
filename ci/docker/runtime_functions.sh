8ccc5f23ee4d4cc7e40e6d00c471a15d59ae1bed
Deokjae Lee <lee.deokjae@gmail.com>
2018-08-08 14:36:52 +0900

    Temporaily disable cudnn for debugging

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -537,35 +537,21 @@ build_ubuntu_gpu_cmake_mkldnn() {
 
 build_ubuntu_gpu_cmake() {
     set -ex
-    # cd /work/build
-    # cmake \
-    #     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    #     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    #     -DUSE_CUDA=1               \
-    #     -DUSE_CUDNN=1              \
-    #     -DUSE_MKLML_MKL=0          \
-    #     -DUSE_MKLDNN=0             \
-    #     -DUSE_DIST_KVSTORE=1       \
-    #     -DCMAKE_BUILD_TYPE=Release \
-    #     -G Ninja                   \
-    #     /work/mxnet
+    cd /work/build
+    cmake \
+        -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+        -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+        -DUSE_CUDA=1               \
+        -DUSE_CUDNN=0              \
+        -DUSE_MKLML_MKL=0          \
+        -DUSE_MKLDNN=0             \
+        -DUSE_DIST_KVSTORE=1       \
+        -DCMAKE_BUILD_TYPE=Release \
+        -G Ninja                   \
+        /work/mxnet
 
-    # ninja -v
-    # build_cython
-
-    # TMP for debugging
-    make \
-        DEV=1                         \
-        USE_CUDA=1                    \
-        USE_CUDNN=1                   \
-        USE_CUDA_PATH=/usr/local/cuda \
-        USE_MKLML_MKL=0               \
-        USE_MKLDNN=0                  \
-        USE_DIST_KVSTORE=1            \
-        -j$(nproc)
-    
-    make cython PYTHON=python2
-    make cython PYTHON=python3
+    ninja -v
+    build_cython
 }
 
 
