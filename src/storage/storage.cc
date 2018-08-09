3cbf4ebc253b40600a07b161ce40b80a02294aa9
Carl Yang <ctcyang@ucdavis.edu>
2018-08-09 22:06:06 +0000

    add guard

--- src/storage/storage.cc
+++ src/storage/storage.cc
@@ -150,7 +150,9 @@ void StorageImpl::Alloc(Storage::Handle* handle) {
 
   // Restores device to before active device before ActivateDevice
   if (handle->ctx.dev_type == Context::kCPUPinned || handle->ctx.dev_type == Context::kGPU) {
+#if MXNET_USE_CUDA
     mxnet::common::cuda::SetDevice set_device;
+#endif
     this->ActivateDevice(handle->ctx);
     manager->Alloc(handle);
     profiler_.OnAlloc(*handle);
@@ -172,7 +174,9 @@ void StorageImpl::Free(Storage::Handle handle) {
 
   // Restores device to before active device before ActivateDevice
   if (ctx.dev_type == Context::kCPUPinned || ctx.dev_type == Context::kGPU) {
+#if MXNET_USE_CUDA
     mxnet::common::cuda::SetDevice set_device;
+#endif
     this->ActivateDevice(ctx);
     manager->Free(handle);
     profiler_.OnFree(handle);
@@ -194,7 +198,9 @@ void StorageImpl::DirectFree(Storage::Handle handle) {
 
   // Restores device to before active device before ActivateDevice
   if (ctx.dev_type == Context::kCPUPinned || ctx.dev_type == Context::kGPU) {
+#if MXNET_USE_CUDA
     mxnet::common::cuda::SetDevice set_device;
+#endif
     this->ActivateDevice(ctx);
     manager->DirectFree(handle);
     profiler_.OnFree(handle);
