9310725b8ebc4572a0df5f34937d518a693fcbf1
Joshua Z. Zhang <cheungchih@gmail.com>
2018-08-10 13:03:11 -0700

    take custom dataset into consideration (#12093)

--- python/mxnet/gluon/data/dataloader.py
+++ python/mxnet/gluon/data/dataloader.py
@@ -160,7 +160,8 @@ def _as_in_context(data, ctx):
 
 def worker_loop(dataset, key_queue, data_queue, batchify_fn):
     """Worker loop for multiprocessing DataLoader."""
-    dataset._fork()
+    if hasattr(dataset, '_fork') and callable(dataset._fork):
+        dataset._fork()
     while True:
         idx, samples = key_queue.get()
         if idx is None:
