e86b4a5ab4028e06161ee4bb6f4f104387ee365c
Junyuan Xie <eric.jy.xie@gmail.com>
2018-08-13 11:41:53 -0700

    fix

--- src/imperative/cached_op.cc
+++ src/imperative/cached_op.cc
@@ -518,10 +518,16 @@ void CachedOp::StaticAllocMemory(
     }
   }
 
-  auto& reuse_pool = keep_fwd ? state.bwd_reuse_pool : state.fwd_reuse_pool;
-  reuse_pool = imperative::AllocateMemory(
-      g, idx, default_ctx, start_eid, end_eid, mem_plan,
-      state.arrays, &state.array_reqs, std::move(reuse_pool));
+  if (state.context.dev_mask() == kCPU) {
+    imperative::AllocateMemory(
+        g, idx, default_ctx, start_eid, end_eid, mem_plan,
+        state.arrays, &state.array_reqs);
+  } else {
+    auto& reuse_pool = keep_fwd ? state.bwd_reuse_pool : state.fwd_reuse_pool;
+    reuse_pool = imperative::AllocateMemory(
+        g, idx, default_ctx, start_eid, end_eid, mem_plan,
+        state.arrays, &state.array_reqs, std::move(reuse_pool));
+  }
 
   state.recording = recording;
   if (keep_fwd) {
