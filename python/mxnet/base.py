88e2e58f40f23bcd667452cde2a6a1c3d8110e8e
cclauss <cclauss@bluewin.ch>
2018-08-13 08:42:29 +0200

    Use dict comprehension to avoid backslash
    One whitespace character to the right of the backslash breaks the script because of a change that is invisible to the reader.

--- python/mxnet/base.py
+++ python/mxnet/base.py
@@ -688,19 +688,16 @@ def _generate_op_module_signature(root_namespace, module_name, op_code_gen_func)
 
     check_call(_LIB.MXListAllOpNames(ctypes.byref(size),
                                      ctypes.byref(plist)))
-    op_names = []
-    for i in range(size.value):
-        op_names.append(py_str(plist[i]))
+    op_names = [py_str(plist[i]) for i in range(size.value)]
 
     module_op_file = get_module_file("%s.%s.op" % (root_namespace, module_name))
     module_op_all = []
     module_internal_file = get_module_file("%s.%s._internal"%(root_namespace, module_name))
     module_internal_all = []
-    submodule_dict = {}
-    for op_name_prefix in _OP_NAME_PREFIX_LIST:
-        submodule_dict[op_name_prefix] =\
-            (get_module_file("%s.%s.%s" % (root_namespace, module_name,
-                                           op_name_prefix[1:-1])), [])
+    submodule_dict = {op_name_prefix:
+                      (get_module_file("%s.%s.%s" % (root_namespace, module_name,
+                                                     op_name_prefix[1:-1])), [])}
+                      for op_name_prefix in _OP_NAME_PREFIX_LIST}
     for name in op_names:
         hdl = OpHandle()
         check_call(_LIB.NNGetOpHandle(c_str(name), ctypes.byref(hdl)))
