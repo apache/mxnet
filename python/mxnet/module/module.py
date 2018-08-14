c743b70e4b94b2b3c81d56e6af7d6faff7e18c23
Alexander Zai <alexzai@amazon.com>
2018-08-14 12:25:54 -0700

    update comments

--- python/mxnet/module/module.py
+++ python/mxnet/module/module.py
@@ -584,7 +584,7 @@ class Module(BaseModule):
 
         Parameters
         ----------
-        data_batch : DataBatch
+        data_batch : DataBatch or NDArray or ndarray
             Could be anything with similar API implemented.
         is_train : bool
             Default is ``None``, which means ``is_train`` takes the value of ``self.for_training``.
