291bfb7cbf7e74fda1817584def7dc7b716adc1c
Alexander Zai <alexzai@amazon.com>
2018-08-15 11:57:26 -0700

    fix lint

--- src/operator/tensor/elemwise_unary_op.h
+++ src/operator/tensor/elemwise_unary_op.h
@@ -299,8 +299,8 @@ class UnaryOp : public OpBase {
         }
         break;
       case kWriteInplace:
-// cannot check if ptrs are the same for MKLDNN because we may have created copies of input when reordering.
-// WriteInPlace will still write to original array
+// cannot check if ptrs are the same for MKLDNN because we may have
+// created copies of input when reordering. WriteInPlace will still write to original array
 #if MXNET_USE_MKLDNN != 1
         CHECK_EQ(inputs[0].dptr_, outputs[0].dptr_);
 #endif
