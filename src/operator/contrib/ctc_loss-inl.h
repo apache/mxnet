5e99e7ee6f8e0911269c74f6a4ad17696e69194e
Lin Yuan <apeforest@gmail.com>
2018-09-05 15:47:05 -0700

    Enable integer type in labels and fix lint errors

--- src/operator/contrib/ctc_loss-inl.h
+++ src/operator/contrib/ctc_loss-inl.h
@@ -255,68 +255,71 @@ class CTCLossOp : public Operator {
     exceed_cudnn_limit = false;
     Stream<xpu> *s = ctx.get_stream<xpu>();
 
-	  MSHADOW_TYPE_SWITCH(in_data[0].type_flag_, DType, {
-  		Tensor<xpu, 3, DType> data =
-  			in_data[ctc_loss::kData].get<xpu, 3, DType>(s);
-  		Tensor<xpu, 2, DType> labels =
-  			in_data[ctc_loss::kLabel].get<xpu, 2, DType>(s);
+    MSHADOW_REAL_TYPE_SWITCH(in_data[ctc_loss::kData].type_flag_, DType, {
+      MSHADOW_TYPE_SWITCH(in_data[ctc_loss::kLabel].type_flag_, LType, {
+        Tensor<xpu, 3, DType> data =
+          in_data[ctc_loss::kData].get<xpu, 3, DType>(s);
+        Tensor<xpu, 2, LType> labels =
+          in_data[ctc_loss::kLabel].get<xpu, 2, LType>(s);
 
-  		Tensor<xpu, 1, DType> costs =
-  			out_data[ctc_loss::kOut].get<xpu, 1, DType>(s);
-  		Tensor<xpu, 3, DType> grad =
-  			out_data[ctc_loss::kGrad].get<xpu, 3, DType>(s);
+        Tensor<xpu, 1, DType> costs =
+          out_data[ctc_loss::kOut].get<xpu, 1, DType>(s);
+        Tensor<xpu, 3, DType> grad =
+          out_data[ctc_loss::kGrad].get<xpu, 3, DType>(s);
 
-  		int max_seq_len = data.size(0);
-  		int batch_size = data.size(1);
-  		int alphabet_size = data.size(2);
+        int max_seq_len = data.size(0);
+        int batch_size = data.size(1);
+        int alphabet_size = data.size(2);
 
-  		// data_lengths
-  		std::vector<int> data_lengths(batch_size, max_seq_len);
-  		if (param_.use_data_lengths) {
-  			int kInputLength = 2;
-  			IndexTensorToVector(in_data[kInputLength].get<xpu, 1, DType>(s), &data_lengths);
-  		}
+        // data_lengths
+        std::vector<int> data_lengths(batch_size, max_seq_len);
+        if (param_.use_data_lengths) {
+          int kInputLength = 2;
+          IndexTensorToVector(in_data[kInputLength].get<xpu, 1, DType>(s), &data_lengths);
+        }
 
-  		// label_lengths
-  		std::vector<int> packed_labels;
-  		std::vector<int> label_lengths(batch_size);
+        // label_lengths
+        std::vector<int> packed_labels;
+        std::vector<int> label_lengths(batch_size);
 
-  		if (param_.use_label_lengths) {
-  			int kLabelLength = 2 + param_.use_data_lengths;
-  			exceed_cudnn_limit = PackLabelByLength(labels, in_data[kLabelLength].get<xpu, 1, DType>(s),
-  													&packed_labels, &label_lengths);
-  		} else {
-  			exceed_cudnn_limit = LabelTensorToPackedVector(labels, param_.blank_label == 0?0:-1,
-  				                                         &packed_labels, &label_lengths);
-  		}
+        if (param_.use_label_lengths) {
+          int kLabelLength = 2 + param_.use_data_lengths;
+          exceed_cudnn_limit =
+            PackLabelByLength(labels, in_data[kLabelLength].get<xpu, 1, LType>(s),
+                             &packed_labels, &label_lengths);
+        } else {
+          exceed_cudnn_limit = LabelTensorToPackedVector(labels, param_.blank_label == 0?0:-1,
+                                                        &packed_labels, &label_lengths);
+        }
 
-  		// CUDNN is disabled due to lack of support for input lengths
-  		/* #if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 7 */
-  		/*     if (!exceed_cudnn_limit) { */
-  		/*       cudnn_forward(ctx, s, data, costs, grad, */
-  		/*                     &data_lengths, &label_lengths, &packed_labels, */
-  		/*                     max_seq_len, batch_size, alphabet_size, */
-  		/*                     req[ctc_loss::kGrad] != mxnet::kNullOp); */
-  		/*     } else { */
-  		/*       baidu_forward(ctx, s, data, costs, grad, */
-  		/*                     &data_lengths, &label_lengths, &packed_labels, */
-  		/*                     batch_size, alphabet_size, req[ctc_loss::kGrad] != mxnet::kNullOp); */
-  		/*     } */
-  		/* #else */
+        // CUDNN is disabled due to lack of support for input lengths
+        /* #if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 7 */
+        /*     if (!exceed_cudnn_limit) { */
+        /*       cudnn_forward(ctx, s, data, costs, grad, */
+        /*                     &data_lengths, &label_lengths, &packed_labels, */
+        /*                     max_seq_len, batch_size, alphabet_size, */
+        /*                     req[ctc_loss::kGrad] != mxnet::kNullOp); */
+        /*     } else { */
+        /*       baidu_forward(ctx, s, data, costs, grad, */
+        /*                     &data_lengths, &label_lengths, &packed_labels, */
+        /*                     batch_size, alphabet_size, req[ctc_loss::kGrad] != mxnet::kNullOp);*/
+        /*     } */
+        /* #else */
 
-  		baidu_forward(ctx, s, data, costs, grad,
-                    &data_lengths, &label_lengths, &packed_labels,
-  				          batch_size, alphabet_size, req[ctc_loss::kGrad] != mxnet::kNullOp);
+        baidu_forward(ctx, s, data, costs, grad,
+                      &data_lengths, &label_lengths, &packed_labels,
+                      batch_size, alphabet_size, req[ctc_loss::kGrad] != mxnet::kNullOp);
 
-  		if (param_.use_data_lengths) {
-  		// baidu warp CTC implementation sometimes includes undefined gradients
-  		// for data outside of length mask. Setting to 0 to make it consistent
-  		// with CPU implementation.
-  			int kInputLength = 2;
-  			mxnet_op::SequenceMask(grad, in_data[kInputLength].get<xpu, 1, DType>(s),
-  				                    static_cast<DType>(0));
-  		}
-  	});
+        if (param_.use_data_lengths) {
+          // baidu warp CTC implementation sometimes includes undefined gradients
+          // for data outside of length mask. Setting to 0 to make it consistent
+          // with CPU implementation.
+          int kInputLength = 2;
+          mxnet_op::SequenceMask(grad, in_data[kInputLength].get<xpu, 1, DType>(s),
+                                static_cast<DType>(0));
+        }
+      });
+    });
   }
 
   virtual void Backward(const OpContext &ctx,
@@ -331,7 +334,7 @@ class CTCLossOp : public Operator {
 
     Stream<xpu> *s = ctx.get_stream<xpu>();
 
-    MSHADOW_TYPE_SWITCH(in_data[0].type_flag_, DType, {
+    MSHADOW_REAL_TYPE_SWITCH(in_data[ctc_loss::kData].type_flag_, DType, {
       Tensor<xpu, 3, DType> data_grad =
         in_grad[ctc_loss::kData].get<xpu, 3, DType>(s);
       Tensor<xpu, 1, DType> output_grad =
@@ -348,7 +351,6 @@ class CTCLossOp : public Operator {
  private:
   CTCLossParam param_;
   bool exceed_cudnn_limit;
-
 #if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 7
   cudnnDataType_t dtype_;
   cudnnCTCLossDescriptor_t ctc_desc_;
@@ -436,6 +438,7 @@ class CTCLossOp : public Operator {
     }
   }
 #endif  // __CUDACC__ && CUDNN
+
   template<typename DType>
   inline void baidu_forward(const OpContext &ctx,
                             mshadow::Stream<xpu>* s,
@@ -537,11 +540,23 @@ class CTCLossProp : public OperatorProperty {
     TShape oshape(1);
     oshape[0] = dshape[1];  // batch size
     out_shape->clear();
-    out_shape->push_back(oshape);
+    out_shape->push_back(oshape);  // forward output
     out_shape->push_back(dshape);  // grad output
     return true;
   }
 
+  bool InferType(std::vector<int> *in_type,
+    std::vector<int> *out_type,
+    std::vector<int> *aux_type) const override {
+    CHECK_LE(in_type->size(), this->ListArguments().size());
+    int dtype = (*in_type)[ctc_loss::kData];
+    CHECK_NE(dtype, -1) << "Input data must have specified type";
+
+    out_type->clear();
+    out_type->push_back(dtype);  // forward output
+    out_type->push_back(dtype);  // grad output
+    return true;
+  }
   OperatorProperty *Copy() const override {
     auto ptr = new CTCLossProp();
     ptr->param_ = param_;
