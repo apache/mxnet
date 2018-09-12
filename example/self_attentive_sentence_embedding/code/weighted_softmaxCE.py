1ca228fd0edb2dd9963ecb1cb95da7567437a4b0
kenjewu <1026639175@qq.com>
2018-09-13 01:38:25 +0800

    Add Implementation of Structured-Self-Attentive-Sentence-Embedding

---
+++ example/self_attentive_sentence_embedding/code/weighted_softmaxCE.py
@@ -0,0 +1,43 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# weighted softmax cross entropy layer
+# author: kenjewu
+
+import mxnet as mx
+from mxnet.gluon import nn
+
+
+class WeightedSoftmaxCE(nn.HybridBlock):
+    def __init__(self, sparse_label=True, from_logits=False,  **kwargs):
+        super(WeightedSoftmaxCE, self).__init__(**kwargs)
+        with self.name_scope():
+            self.sparse_label = sparse_label
+            self.from_logits = from_logits
+
+    def hybrid_forward(self, F, pred, label, class_weight, depth=None):
+        if self.sparse_label:
+            label = F.reshape(label, shape=(-1, ))
+            label = F.one_hot(label, depth)
+        if not self.from_logits:
+            pred = F.log_softmax(pred, -1)
+
+        weight_label = F.broadcast_mul(label, class_weight)
+        loss = -F.sum(pred * weight_label, axis=-1)
+
+        # return F.mean(loss, axis=0, exclude=True)
+        return loss
