094528bf2ad4fabb29178232816cdc8ca7d36868
xmj <xumengjie@smartahc.com>
2018-09-18 15:55:58 +0800

    add data folder and readme.md and convert all to English

--- example/gluon/gluon_speech_recognition/sequential_CNN_CTC.py
+++ example/gluon/gluon_speech_recognition/sequential_CNN_CTC.py
@@ -163,35 +163,32 @@ def get_iter(batch_size):
         yield nd.array(x), nd.array(y)
 
 
+
 class ShowProcess():
     """
-    显示处理进度的类
-    调用该类相关函数即可实现处理进度的显示
+    process bar to show the process and loss
     """
-    i = 0 # 当前的处理进度
-    max_steps = 0 # 总共需要处理的次数
-    max_arrow = 50 #进度条的长度
+    i = 0 
+    max_steps = 0 
+    max_arrow = 50 
     infoDone = 'done'
 
-    # 初始化函数，需要知道总共的处理次数
     def __init__(self, max_steps, infoDone = 'Done'):
         self.max_steps = max_steps
         self.i = 0
         self.infoDone = infoDone
 
-    # 显示函数，根据当前的处理进度i显示进度
-    # 效果为[>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>]100.00%
     def show_process(self, loss, i=None):
         if i is not None:
             self.i = i
         else:
             self.i += 1
-        num_arrow = int(self.i * self.max_arrow / self.max_steps) #计算显示多少个'>'
-        num_line = self.max_arrow - num_arrow #计算显示多少个'-'
-        percent = self.i * 100.0 / self.max_steps #计算完成进度，格式为xx.xx%
+        num_arrow = int(self.i * self.max_arrow / self.max_steps) 
+        num_line = self.max_arrow - num_arrow 
+        percent = self.i * 100.0 / self.max_steps
         process_bar = '[' + '>' * num_arrow + '-' * num_line + ']'\
-                      + '%.2f' % percent + '%, loss:' + str(loss) + '\r' #带输出的字符串，'\r'表示不换行回到最左边
-        sys.stdout.write(process_bar) #这两句打印字符到终端
+                      + '%.2f' % percent + '%, loss:' + str(loss) + '\r'
+        sys.stdout.write(process_bar)
         sys.stdout.flush()
         if self.i >= self.max_steps:
             self.close()
@@ -202,7 +199,6 @@ class ShowProcess():
         self.i = 0
 
 
-
 my_ctcloss = loss.CTCLoss()
 def train(net, num_epochs, lr, batch_size):
     with mx.Context(mx.cpu(0)):
@@ -211,7 +207,7 @@ def train(net, num_epochs, lr, batch_size):
             'learning_rate': lr,
         })
         for epoch in range(num_epochs):
-            max_steps = len(os.listdir('/media/xmj/ubt_2t/中文语音识别/data_thchs30/data'))//3 //batch_size
+            max_steps = len(os.listdir('./data'))//3 //batch_size
             process_bar = ShowProcess(max_steps, 'OK')
             train_iter = get_iter(batch_size)
             for x, y in train_iter:
