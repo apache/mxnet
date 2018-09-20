0f0b85ca1dff74cd0547ddb8f18e56a602430313
Aaron Markham <aaron.s.markham@gmail.com>
2018-09-20 21:55:21 +0000

    handle rewrites for anchor links

--- docs/_static/js/docversion.js
+++ docs/_static/js/docversion.js
@@ -1,25 +1,24 @@
+function setVersion(){
+        let doc = window.location.pathname.match(/^\/versions\/[^*]+\/(api\/.*)$/);
+        if (doc) {
+            if (document.getElementById('dropdown-menu-position-anchor-version')) {
+                    versionNav = $('#dropdown-menu-position-anchor-version a.main-nav-link');
+                    $(versionNav).each( function( index, el ) {
+                            currLink = $( el ).attr('href');
+                            version = currLink.match(/\/versions\/([0-9.]+)\//);
+                            if (version) {
+                                    versionedDoc = '/versions/' + version[1] + '/' + doc[1] + (window.location.hash || '');
+                                    $( el ).attr('href', versionedDoc);
+                            }
+                    });
+            }        
+        }
+}
+
 $(document).ready(function () {
-	function setVersion(){
-  		console.log('Applying current file to links: ', doc[1]);
-  		if (document.getElementById('dropdown-menu-position-anchor-version')) {
-          		console.log('found the versions element');
-          		versionNav = $('#dropdown-menu-position-anchor-version a.main-nav-link');
-          		console.log(versionNav);
-          		$(versionNav).each( function( index, el ) {
-                  		currLink = $( el ).attr('href');
-                  		console.log('current link: ', currLink);
-                  		version = currLink.match(/\/versions\/([0-9.]+)\//);
-                  		if (version) {
-                          		console.log('version: ', version[1]);
-                          		versionedDoc = '/versions/' + version[1] + '/' + doc[1];
-                          		console.log('versionedDoc: ', versionedDoc);
-                          		$( el ).attr('href', versionedDoc);
-                  		}
-          		});
-  		}
-	}
-	let doc = window.location.pathname.match(/^\/versions\/[^*]+\/(api\/.*)$/);
-	if (doc) {
-  		setVersion();
-	};
+    setVersion();
+});
+
+$('a.reference.internal').click(function(){
+    setVersion();
 });
