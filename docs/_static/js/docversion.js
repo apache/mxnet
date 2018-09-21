17fbcc1c2aa39c764833bdb3404e3d0c9043ce35
Aaron Markham <aaron.s.markham@gmail.com>
2018-09-21 18:33:54 +0000

    support master version redirects

--- docs/_static/js/docversion.js
+++ docs/_static/js/docversion.js
@@ -1,11 +1,11 @@
 function setVersion(){
-        let doc = window.location.pathname.match(/^\/versions\/[^*]+\/(api\/.*)$/);
+        let doc = window.location.pathname.match(/^\/(api\/.*)$/) || window.location.pathname.match(/^\/versions\/[^*]+\/(api\/.*)$/);
         if (doc) {
             if (document.getElementById('dropdown-menu-position-anchor-version')) {
                     versionNav = $('#dropdown-menu-position-anchor-version a.main-nav-link');
                     $(versionNav).each( function( index, el ) {
                             currLink = $( el ).attr('href');
-                            version = currLink.match(/\/versions\/([0-9.]+)\//);
+                            version = currLink.match(/\/versions\/([0-9.master]+)\//);
                             if (version) {
                                     versionedDoc = '/versions/' + version[1] + '/' + doc[1] + (window.location.hash || '');
                                     $( el ).attr('href', versionedDoc);
