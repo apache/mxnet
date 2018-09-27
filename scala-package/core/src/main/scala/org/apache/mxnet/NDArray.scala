bf8792df76cc5672b63c37cea7f6a8ed47e62780
Qing <lanking520@live.com>
2018-09-27 10:12:12 -0700

    remove unrelated method to Java

--- scala-package/core/src/main/scala/org/apache/mxnet/NDArray.scala
+++ scala-package/core/src/main/scala/org/apache/mxnet/NDArray.scala
@@ -584,53 +584,6 @@ class NDArray private[mxnet](private[mxnet] val handle: NDArrayHandle,
     this.set(arr)
   }
 
-  /**
-    * Visualize the internal structure of NDArray
-    * @return String that show the structure
-    */
-  def visualize: String = {
-    buildStringHelper(this, this.shape.length) + "\n"
-  }
-
-  /**
-    * Helper function to create formatted NDArray output
-    * The NDArray will be represented in a reduced version if too large
-    * @param nd NDArray as the input
-    * @param totalSpace totalSpace of the lowest dimension
-    * @return String format of NDArray
-    */
-  private def buildStringHelper(nd : NDArray, totalSpace : Int) : String = {
-    var result = ""
-    val THRESHOLD = 100000      // longest NDArray to show in full
-    val ARRAYTHRESHOLD = 1000   // longest array to show in full
-    val shape = nd.shape
-    val space = totalSpace - shape.length
-    if (shape.length != 1) {
-      val (length, postfix) =
-        if (shape.product > THRESHOLD) {
-          // reduced NDArray
-          (1, s"\n${" " * (space + 1)}... with length ${shape(0)}\n")
-      } else {
-          (shape(0), "")
-        }
-      for (num <- 0 until length) {
-        val output = buildStringHelper(nd.at(num), totalSpace)
-        result += s"$output\n"
-      }
-      result = s"${" " * space}[\n$result${" " * space}$postfix]"
-    } else {
-      if (shape(0) > ARRAYTHRESHOLD) {
-        // reduced Array
-        val front = nd.slice(0, 10)
-        val back = nd.slice(shape(0) - 10, shape(0) - 1)
-        result = s"${" " * space}[${front.toArray.mkString(",")} ... ${back.toArray.mkString(",")}]"
-      } else {
-        result = s"${" " * space}[${nd.toArray.mkString(",")}]"
-      }
-    }
-    result
-  }
-
   // record arrays who construct this array instance
   // we use weak reference to prevent gc blocking
   private[mxnet] val dependencies = mutable.HashMap.empty[Long, WeakReference[NDArray]]
