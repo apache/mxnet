2892691485182db9f716dd2259cc3d5e74f6cf8c
apeforest <apeforest@gmail.com>
2018-10-03 06:14:40 +0000

    check for float exception

--- src/engine/engine.cc
+++ src/engine/engine.cc
@@ -26,6 +26,25 @@
 #include <memory>
 #include <cstdlib>
 #include "./engine_impl.h"
+#include <fenv.h>
+#include <signal.h>
+
+static void 
+float_num_err_handler (int sig, siginfo_t *siginfo, void *context) {
+  fprintf(stderr, ">>> Floating point error occur\n");
+  switch(siginfo->si_code) {
+  case FPE_INTDIV:
+    fprintf(stderr, ">>> Integer division by zero\n");
+    break;
+  case FPE_FLTOVF:
+    fprintf(stderr, ">>> Floating overflow trap\n");
+    break;
+  case FPE_FLTDIV:
+    fprintf(stderr, ">>> Floating/decimal division by zero\n");
+    break;
+  }
+  assert(0);
+}
 
 namespace mxnet {
 namespace engine {
@@ -34,6 +53,16 @@ inline Engine* CreateEngine() {
   const bool default_engine = (type == nullptr);
   if (type == nullptr) type = "ThreadedEnginePerDevice";
   std::string stype = type;
+
+  struct sigaction act;
+  memset(&act, '\0', sizeof(act));
+  act.sa_sigaction = &float_num_err_handler;
+  act.sa_flags = SA_SIGINFO;
+  if (sigaction(SIGFPE, &act, NULL) < 0) {
+    fprintf(stderr, "Cannot register signal handler\n");
+    exit(-1);
+  }
+  feenableexcept(FE_ALL_EXCEPT);
 
   Engine *ret = nullptr;
   #if MXNET_PREDICT_ONLY == 0
@@ -48,6 +77,10 @@ inline Engine* CreateEngine() {
   ret = CreateNaiveEngine();
   #endif
 
+  int e = std::fetestexcept(FE_ALL_EXCEPT);
+  if (e & FE_DIVBYZERO) {
+    LOG(FATAL) << "divide by zero" << type;
+  }
   if (ret ==nullptr) {
     LOG(FATAL) << "Cannot find Engine " << type;
   }
