e93af41a6f02e902b94576f7efda47f380ce9919
Piyush Ghai <ghai.8@osu.edu>
2018-10-05 13:03:11 -0700

    [MXNET-716] Adding Scala Inference Benchmarks (#12721)
    * Adding Scala Inference Benchmark base class + an example of how to run it
    
    * Fixed scalastyle issues
    
    * Added platform check to the classpath
    
    * Formatting the metrics to print upto 2 decimal digits in float
    
    * Added bash script to fetch resnet-18 data and params
    
    * Added flag for cpu/gpu for running the script
    
    * Fixed duplicate if check

--- scala-package/examples/src/test/scala/org/apache/mxnetexamples/benchmark/ScalaInferenceBenchmarkSuite.scala
+++ scala-package/examples/src/test/scala/org/apache/mxnetexamples/benchmark/ScalaInferenceBenchmarkSuite.scala
@@ -14,35 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.apache.mxnetexamples.benchmark
 
-package org.apache.mxnetexamples.infer.imageclassifier
+import java.io.File
 
+import org.apache.mxnetexamples.Util
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import org.slf4j.LoggerFactory
-import java.io.File
-import java.net.URL
 
-import org.apache.commons.io.FileUtils
-import org.apache.mxnet.{Context, NDArrayCollector}
-import org.apache.mxnetexamples.Util
+import scala.sys.process.Process
 
-import sys.process.Process
+class ScalaInferenceBenchmarkSuite  extends FunSuite with BeforeAndAfterAll {
+  private val logger = LoggerFactory.getLogger(classOf[ScalaInferenceBenchmarkSuite])
+  override def beforeAll(): Unit = {
+  }
 
-/**
-  * Integration test for imageClassifier example.
-  * This will run as a part of "make scalatest"
-  */
-class ImageClassifierExampleSuite extends FunSuite with BeforeAndAfterAll {
-  private val logger = LoggerFactory.getLogger(classOf[ImageClassifierExampleSuite])
-
-  test("testImageClassifierExample") {
+  test("Testing Benchmark -- Image Classification") {
     logger.info("Downloading resnet-18 model")
-
     val tempDirPath = System.getProperty("java.io.tmpdir")
     logger.info("tempDirPath: %s".format(tempDirPath))
-
     val baseUrl = "https://s3.us-east-2.amazonaws.com/scala-infer-models"
-
     Util.downloadUrl(baseUrl + "/resnet-18/resnet-18-symbol.json",
       tempDirPath + "/resnet18/resnet-18-symbol.json")
     Util.downloadUrl(baseUrl + "/resnet-18/resnet-18-0000.params",
@@ -51,28 +42,19 @@ class ImageClassifierExampleSuite extends FunSuite with BeforeAndAfterAll {
       tempDirPath + "/resnet18/synset.txt")
     Util.downloadUrl("https://s3.amazonaws.com/model-server/inputs/Pug-Cookie.jpg",
       tempDirPath + "/inputImages/resnet18/Pug-Cookie.jpg")
-
     val modelDirPath = tempDirPath + File.separator + "resnet18/"
     val inputImagePath = tempDirPath + File.separator +
       "inputImages/resnet18/Pug-Cookie.jpg"
     val inputImageDir = tempDirPath + File.separator + "inputImages/resnet18/"
-
-    var context = Context.cpu()
-    if (System.getenv().containsKey("SCALA_TEST_ON_GPU") &&
-      System.getenv("SCALA_TEST_ON_GPU").toInt == 1) {
-      context = Context.gpu()
-    }
-
-    val output = ImageClassifierExample.runInferenceOnSingleImage(modelDirPath + "resnet-18",
-     inputImagePath, context)
-
-    val outputList = ImageClassifierExample.runInferenceOnBatchOfImage(modelDirPath + "resnet-18",
-        inputImageDir, context)
-
-    Process("rm -rf " + modelDirPath + " " + inputImageDir) !
-
-    assert(output(0).toList.head._1 === "n02110958 pug, pug-dog")
-    assert(outputList(0).toList.head._1 === "n02110958 pug, pug-dog")
-
+    val args = Array(
+      "--example", "ImageClassifierExample",
+      "--count", "1",
+      "--batchSize", "10",
+      "--model-path-prefix", s"$modelDirPath/resnet-18",
+      "--input-image", inputImagePath,
+      "--input-dir", inputImageDir
+    )
+    ScalaInferenceBenchmark.main(args)
   }
+
 }
