76ffd54322050fd24f0cc0b4895d1bc43a7a9bae
Alexander Zai <alexzai@amazon.com>
2018-10-09 15:37:55 -0700

    fix log message

--- src/common/utils.h
+++ src/common/utils.h
@@ -474,7 +474,7 @@ inline void LogStorageFallback(const nnvm::NodeAttrs& attrs,
 #if MXNET_USE_MKLDNN == 1
   if (!MKLDNNEnvSet()) common::LogOnce("MXNET_MKLDNN_ENABLED flag is off. "
                                        "You can re-enable by setting MXNET_MKLDNN_ENABLED=1");
-  if (!MKLDNNCacheSet()) common::LogOnce("MXNET_MKLDNN_CACHE_ENABLED flag is off. "
+  if (MKLDNNCacheSize() != -1) common::LogOnce("MXNET_MKLDNN_CACHE_SIZE is set. "
                                        "If you have static input shapes for best performance set MXNET_MKLDNN_CACHE_ENABLED=1");
 #endif
 }
