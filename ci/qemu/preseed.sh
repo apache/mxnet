4024bc490ec0711416f75f4ea67a11c9fd713f0e
Pedro Larroy <pllarroy@amazon.com>
2018-10-11 15:20:47 +0200

    Address CR comments wrt comments.

--- ci/qemu/preseed.sh
+++ ci/qemu/preseed.sh
@@ -17,10 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-#set -xeuo pipefail
 set -ex
-#cp installer-initrd.gz initrd
-#gunzip installer-initrd.gz
 rm -rf initrd
 mkdir -p initrd
 cd initrd
@@ -29,9 +26,4 @@ cp ../preseed.cfg .
 cp ../initrd_modif/inittab etc/inittab
 cp ../initrd_modif/S10syslog lib/debian-installer-startup.d/S10syslog
 find .  | cpio --create --format 'newc'  | gzip -c > ../installer-initrd_automated.gz
-#find .  | cpio --create --format 'newc'  | gzip -c > ../installer-initrd.gz
-#echo preseed.cfg | cpio -H newc -o -A -F installer-initrd
-#gzip installer-initrd
-#cd ..
-#cp initrd/installer-initrd.gz .
 echo "Done!"
