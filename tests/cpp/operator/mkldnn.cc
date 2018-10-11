0b990fbeda68f23eef953ac3cfdbdb8dfc0704ba
Alexander Zai <alexzai@amazon.com>
2018-10-11 15:22:37 -0700

    set bias with string

--- tests/cpp/operator/mkldnn.cc
+++ tests/cpp/operator/mkldnn.cc
@@ -533,7 +533,7 @@ OpAttrs GetConvOp(int kernel, int num_filters, int dim, int stride, int pad) {
   attrs.attrs.dict.insert({"num_filter" , std::to_string(num_filters)});
   attrs.attrs.dict.insert({"stride" , CreateShapeString(stride, dim)});
   attrs.attrs.dict.insert({"pad" , CreateShapeString(pad, dim)});
-  attrs.attrs.dict.insert({"bias" , 1});
+  attrs.attrs.dict.insert({"bias" , "1"});
   attrs.attrs.op->attr_parser(&attrs.attrs);
   attrs.input_types = ArrayTypes::Normal |
       ArrayTypes::MKLDNN |
@@ -562,7 +562,7 @@ OpAttrs GetConvBackwardOp(int kernel, int num_filters, int dim, int stride, int 
   attrs.attrs.dict.insert({"num_filter" , std::to_string(num_filters)});
   attrs.attrs.dict.insert({"stride" , CreateShapeString(stride, dim)});
   attrs.attrs.dict.insert({"pad" , CreateShapeString(pad, dim)});
-  attrs.attrs.dict.insert({"bias" , 1});
+  attrs.attrs.dict.insert({"bias" , "1"});
   attrs.attrs.op->attr_parser(&attrs.attrs);
   return attrs;
 }
