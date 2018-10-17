371828dab3dc25f5bf0bcdf2416ab80b564758cb
pengxin99 <yc316ypx@126.com>
2018-10-17 15:01:25 +0800

    fix review comments

---
+++ example/gluon/DCGAN/README.md
@@ -0,0 +1,51 @@
+# DCGAN in MXNet
+
+[Deep Convolutional Generative Adversarial Networks(DCGAN)](https://arxiv.org/abs/1511.06434) implementation with Apache MXNet GLUON.
+This implementation uses [inception_score](https://github.com/openai/improved-gan) to evaluate the model.
+
+You can use this reference implementation on MNIST and CIFAR10 dataset.
+
+
+#### Generated pic(use dataset cifar10)
+![Generated pic](https://github.com/pengxin99/incubator-mxnet/blob/dcgan-inception_score/example/gluon/DCgan/pic/fake_img_iter_13900.png)
+
+#### Generated pic(use dataset mnist)
+![Generated pic](https://github.com/pengxin99/incubator-mxnet/blob/dcgan-inception_score/example/gluon/DCgan/pic/fake_img_iter_21700.png)
+
+#### inception_score in cpu and gpu (the real image`s score is around 3.3)
+CPU & GPU
+
+![inception_socre_with_cpu](https://github.com/pengxin99/incubator-mxnet/blob/dcgan-inception_score/example/gluon/DCgan/pic/inception_score_cifar10_cpu.png)
+![inception_score_with_gpu](https://github.com/pengxin99/incubator-mxnet/blob/dcgan-inception_score/example/gluon/DCgan/pic/inception_score_cifar10.png)
+## Quick start
+use below code to see the configurations you can set:
+```python
+python dcgan.py -h
+```
+    
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --dataset DATASET     dataset to use. options are cifar10 and mnist.
+      --batch-size BATCH_SIZE  input batch size, default is 64
+      --nz NZ               size of the latent z vector, default is 100
+      --ngf NGF             the channel of each generator filter layer, default is 64.
+      --ndf NDF             the channel of each descriminator filter layer, default is 64.
+      --nepoch NEPOCH       number of epochs to train for, default is 25.
+      --niter NITER         save generated images and inception_score per niter iters, default is 100.
+      --lr LR               learning rate, default=0.0002
+      --beta1 BETA1         beta1 for adam. default=0.5
+      --cuda                enables cuda
+      --netG NETG           path to netG (to continue training)
+      --netD NETD           path to netD (to continue training)
+      --outf OUTF           folder to output images and model checkpoints
+      --check-point CHECK_POINT
+                            save results at each epoch or not
+      --inception_score INCEPTION_SCORE
+                            To record the inception_score, default is True.
+
+
+use below code to train DCGAN model with default configurations and dataset(cifar10), and metric with inception_score:
+```python
+python dcgan.py
+```
--- example/gluon/DCGAN/inception_score.py
+++ example/gluon/DCGAN/inception_score.py
@@ -16,33 +16,39 @@
 # under the License.
 
 from mxnet.gluon.model_zoo import vision as models
-from mxnet import gluon
 import mxnet as mx
 from mxnet import nd
 import numpy as np
 import math
 import sys
 
+import cv2
+
 
 inception_model = None
 
 
 def get_inception_score(images, splits=10):
-
+    """
+    Inception_score function.
+        The images will be divided into 'splits' parts, and calculate each inception_score separately,
+        then return the mean and std of inception_scores of these parts.
+    :param images: Images(num x c x w x h) that needs to calculate inception_score.
+    :param splits:
+    :return: mean and std of inception_score
+    """
     assert (images.shape[1] == 3)
-    # assert (nd.max(images[0]) > 10)
-    # assert (nd.min(images[0]) >= 0.0)
 
     # load inception model
     if inception_model is None:
         _init_inception()
 
+    # resize images to adapt inception model(inceptionV3)
     if images.shape[2] != 299:
         images = resize(images, 299, 299)
 
     preds = []
     bs = 4
-    # scores = []
     n_batches = int(math.ceil(float(images.shape[0])/float(bs)))
 
     # to get the predictions/picture of inception model
@@ -50,15 +56,18 @@ def get_inception_score(images, splits=10):
         sys.stdout.write(".")
         sys.stdout.flush()
         inps = images[(i * bs):min((i + 1) * bs, len(images))]
+        # inps size. bs x 3 x 299 x 299
         pred = nd.softmax(inception_model(inps))
+        # pred size. bs x 1000
         preds.append(pred.asnumpy())
 
     # list to array
     preds = np.concatenate(preds, 0)
     scores = []
 
-    # to calculate the inception_score each split of 10 splits
+    # to calculate the inception_score each split.
     for i in range(splits):
+        # extract per split image pred
         part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
         kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
         kl = np.mean(np.sum(kl, 1))
@@ -79,7 +88,10 @@ def resize(images, w, h):
     for i in range(nums):
         img = images[i, :, :, :]
         img = mx.nd.transpose(img, (1, 2, 0))
-        img = mx.image.imresize(img, 299, 299)
+        # Replace 'mx.image.imresize()' with 'cv2.resize()' because : Operator _cvimresize is not implemented for GPU.
+        # img = mx.image.imresize(img, w, h)
+        img = cv2.resize(img.asnumpy(), (299, 299))
+        img = nd.array(img)
         img = mx.nd.transpose(img, (2, 0, 1))
         res[i, :, :, :] = img
 
@@ -90,7 +102,7 @@ if __name__ == '__main__':
     if inception_model is None:
         _init_inception()
     # dummy data
-    images = nd.random.uniform(0, 255, (15, 3, 64, 64))
+    images = nd.random.uniform(0, 255, (64, 3, 64, 64))
     print(images.shape[0])
     # resize(images,299,299)
 
