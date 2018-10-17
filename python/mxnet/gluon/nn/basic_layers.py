db5b393437f00ec99e30c006233a05db7ffe80d9
Zhang <zhang.hang@rutgers.edu>
2018-10-17 11:16:02 -0700

    gn

--- python/mxnet/gluon/nn/basic_layers.py
+++ python/mxnet/gluon/nn/basic_layers.py
@@ -700,3 +700,82 @@ class HybridLambda(HybridBlock):
     def __repr__(self):
         return '{name}({function})'.format(name=self.__class__.__name__,
                                            function=self._func_name)
+
+
+class GropNorm(Block):
+    """GropNorm normalization layer (Wu and He, 2014).
+    Parameters
+    ----------
+    ngroups : int
+        Numnber of channel groups in GN.
+    in_channels : int, default 0
+        Number of channels (feature maps) in input data. If not specified,
+        initialization will be deferred to the first time `forward` is called
+        and `in_channels` will be inferred from the shape of input data.
+    axis : int, default 1
+        The axis that should be normalized. This is typically the channels
+        (C) axis. For instance, after a `Conv2D` layer with `layout='NCHW'`,
+        set `axis=1` in `GropNorm`. If `layout='NHWC'`, then set `axis=3`.
+    epsilon: float, default 1e-5
+        Small float added to variance to avoid dividing by zero.
+    beta_initializer: str or `Initializer`, default 'zeros'
+        Initializer for the beta weight.
+    gamma_initializer: str or `Initializer`, default 'ones'
+        Initializer for the gamma weight.
+
+    Inputs:
+        - **data**: input tensor with arbitrary shape.
+
+    Outputs:
+        - **out**: output tensor with the same shape as `data`.
+    """
+    def __init__(self, ngroups, in_channels=0, axis=1, epsilon=1e-5,
+                 beta_initializer='zeros', gamma_initializer='ones', **kwargs):
+        super(GropNorm, self).__init__(**kwargs)
+        self._kwargs = {'axis': axis, 'eps': epsilon, 'momentum': 0,
+                        'fix_gamma': True, 'use_global_stats': True}
+        self.ngroups = ngroups
+        assert in_channels % ngroups == 0, "Channel number should be divisible by groups."
+        if in_channels != 0:
+            self.in_channels = in_channels
+
+        self.gamma = self.params.get('gamma', grad_req='write' if scale else 'null',
+                                     shape=(in_channels,), init=gamma_initializer,
+                                     allow_deferred_init=True, differentiable=scale)
+        self.beta = self.params.get('beta', grad_req='write' if center else 'null',
+                                    shape=(in_channels,), init=beta_initializer,
+                                    allow_deferred_init=True, differentiable=center)
+        # hacky
+        self.running_mean = self.params.get('running_mean', grad_req='null',
+                                            shape=(in_channels,), init='zeros',
+                                            allow_deferred_init=True, differentiable=False)
+        self.running_var = self.params.get('running_var', grad_req='null',
+                                           shape=(in_channels,), init='ones',
+                                           allow_deferred_init=True, differentiable=False)
+        
+
+    def cast(self, dtype):
+        if np.dtype(dtype).name == 'float16':
+            dtype = 'float32'
+        super(GropNorm, self).cast(dtype)
+
+    def forward(self, x):
+        xshape = x.shape
+        # normalization
+        y = F.BatchNorm(x.reshape(xshape[0], self.ngroups, -1),
+                        unning_mean, running_var, unning_mean, running_var,
+                        name='fwd', **self._kwargs)
+        # scale and shift
+        y = y.reshape(xshape[0], xshape[1], -1)
+        y = y * self.gamma.data().reshape(1, -1, 1) + self.beta.data().reshape(1, -1, 1)
+        return y.reshape(xshape)
+
+    def __repr__(self):
+        s = '{name}({content}'
+        in_channels = self.gamma.shape[0]
+        s += ', in_channels={0}'.format(in_channels if in_channels else None)
+        s += ')'
+        return s.format(name=self.__class__.__name__,
+                        content=', '.join(['='.join([k, v.__repr__()])
+                                           for k, v in self._kwargs.items()]))
+
