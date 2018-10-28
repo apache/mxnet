473abd6c3e9847ae99b6bf7b03da40d581b88d42
Lieven Govaerts <lgo@mobsol.be>
2018-10-28 19:28:23 +0100

    Raise an exception when passing an invalid data buffer to imdecode.
    * src/io/image_io.cc: Raise an exception when the image could not be decoded instead of just logging.
    * tests/python/unittest/test_image.py: Add a new test test_imdecode_invalid_image.

--- src/io/image_io.cc
+++ src/io/image_io.cc
@@ -143,11 +143,8 @@ void ImdecodeImpl(int flag, bool to_rgb, void* data, size_t size,
   cv::Mat dst;
   if (out->is_none()) {
     cv::Mat res = cv::imdecode(buf, flag);
-    if (res.empty()) {
-      LOG(INFO) << "Decoding failed. Invalid image file.";
-      *out = NDArray();
-      return;
-    }
+    CHECK(!res.empty()) << "Decoding failed. Invalid image file.";
+
     *out = NDArray(mshadow::Shape3(res.rows, res.cols, flag == 0 ? 1 : 3),
                    Context::CPU(), false, mshadow::kUint8);
     dst = cv::Mat(out->shape()[0], out->shape()[1], flag == 0 ? CV_8U : CV_8UC3,
