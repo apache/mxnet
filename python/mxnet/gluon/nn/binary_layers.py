bugfix in scaling

python/mxnet/gluon/nn/binary_layers.py
@@ -222,8 +222,8 @@ class _QConv(_Conv):
         h = F.Convolution(padded, quantized_weight, name='fwd', **self._kwargs)
         if self.scaling:
             scale = weight.abs().mean(axis=0, exclude=True, keepdims=True).transpose(self._scaling_transpose)
-        if self.stop_weight_scale_grad:
-            scale = F.stop_gradient(scale)
+            if self.stop_weight_scale_grad:
+                scale = F.stop_gradient(scale)
             h = F.broadcast_mul(h, scale)
         if self.bits == 1 and not self.no_offset and not self.scaling:
             h = (h + self._offset) / 2