3dd1632989c212d57fc6bd6cc00f1b569aa145e4
Qing <lanking520@live.com>
2018-11-15 00:13:19 -0800

    update build script and chmod +x

--- ci/docker/install/centos7_scala.sh
+++ ci/docker/install/centos7_scala.sh
@@ -29,4 +29,3 @@ tar xzf apache-maven-3.3.9-bin.tar.gz
 mkdir /usr/local/maven
 mv apache-maven-3.3.9/ /usr/local/maven/
 alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
-alternatives --config mvn
