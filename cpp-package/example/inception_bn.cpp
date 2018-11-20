1031691838ac7904e36129fcea744815f196ee76
Lai Wei <royweilai@gmail.com>
2018-11-19 17:05:21 -0800

    make max epoch an argument

--- cpp-package/example/inception_bn.cpp
+++ cpp-package/example/inception_bn.cpp
@@ -144,7 +144,7 @@ Symbol InceptionSymbol(int num_classes) {
 
 int main(int argc, char const *argv[]) {
   int batch_size = 40;
-  int max_epoch = 100;
+  int max_epoch = argc > 1 ? strtol(argv[1], NULL, 10) : 100;
   float learning_rate = 1e-2;
   float weight_decay = 1e-4;
 
