995076e3d008a3688e24f4b804e5b5e2d83e86e3
Lin Yuan <apeforest@gmail.com>
2018-11-27 15:10:29 -0800

    address reviewer comment

--- python/mxnet/libinfo.py
+++ python/mxnet/libinfo.py
@@ -86,7 +86,7 @@ def find_include_path():
     """
     incl_from_env = os.environ.get('MXNET_INCLUDE_PATH')
     if incl_from_env:
-        if os.path.isfile(incl_from_env):
+        if os.path.isdir(incl_from_env):
             if not os.path.isabs(incl_from_env):
                 logging.warning("MXNET_INCLUDE_PATH should be an absolute path, instead of: %s",
                                 incl_from_env)
@@ -96,12 +96,11 @@ def find_include_path():
                 return incl_from_env
         else:
             logging.warning("MXNET_INCLUDE_PATH '%s' doesn't exist", incl_from_env)
+
     curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
     incl_path = os.path.join(curr_path, '../../include/')
     if not os.path.isdir(incl_path):
         raise RuntimeError('Cannot find the MXNet include path.\n')
-    if os.name == 'nt':
-        os.environ['PATH'] = os.environ['PATH'] + ';' + os.path.dirname(incl_path)
     return incl_path
 
 
