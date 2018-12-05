d16dd07cfb88e7959343f9f0adb104dea227ec30
ChaiBapchya <chai.bapat@gmail.com>
2018-12-04 22:08:09 -0800

    count instead of sum to solve attribute error

--- python/mxnet/test_utils.py
+++ python/mxnet/test_utils.py
@@ -1916,7 +1916,7 @@ def chi_square_check(generator, buckets, probs, nsamples=1000000):
         sample_bucket_ids = sample_bucket_ids // 2
     obs_freq = np.zeros(shape=len(buckets), dtype=np.int)
     for i, _ in enumerate(buckets):
-        obs_freq[i] = (sample_bucket_ids == buckets[i]).astype('uint8').sum()
+        obs_freq[i] = sample_bucket_ids.count(buckets[i])
     _, p = ss.chisquare(f_obs=obs_freq, f_exp=expected_freq)
     return p, obs_freq, expected_freq
 
