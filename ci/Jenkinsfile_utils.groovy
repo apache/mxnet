9c0d1731008ee7a63d9e9c886c40e254991e4a0a
Marco de Abreu <marcoabreu@users.noreply.github.com>
2018-12-07 01:51:01 +0000

    Add workspace cleaning after job finished (#13490)
    * Add workspace cleaning after job finished
    
    * Update Jenkinsfile_utils.groovy
    
    * Update Jenkinsfile_utils.groovy

--- ci/Jenkinsfile_utils.groovy
+++ ci/Jenkinsfile_utils.groovy
@@ -263,7 +263,10 @@ def main_wrapper(args) {
     node(NODE_UTILITY) {
       // Call failure handler
       args['failure_handler']()
-      
+
+      // Clean workspace to reduce space requirements
+      cleanWs()
+
       // Remember to rethrow so the build is marked as failing
       if (err) {
         throw err
