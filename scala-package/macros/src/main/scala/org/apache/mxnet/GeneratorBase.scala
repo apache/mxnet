8bd6e46a28b10266fd93cf1c311ef0ee66ba922a
Qing <lanking520@live.com>
2018-12-14 15:51:36 -0800

    fix generator base

--- scala-package/macros/src/main/scala/org/apache/mxnet/GeneratorBase.scala
+++ scala-package/macros/src/main/scala/org/apache/mxnet/GeneratorBase.scala
@@ -191,7 +191,7 @@ private[mxnet] trait RandomHelpers {
   // unify call targets (random_xyz and sample_xyz) and unify their argument types
   private def unifyRandom(func: Func, isSymbol: Boolean): Func = {
     var typeConv = Set("org.apache.mxnet.NDArray", "org.apache.mxnet.Symbol",
-      "java.lang.Float", "java.lang.Integer")
+      "Float", "Int")
 
     func.copy(
       name = func.name.replaceAll("(random|sample)_", ""),
