faf4fef56e5f2f0376b28035b38c0ca5ebd6a45f
XiaotaoChen <xiaotao.chen@intel.com>
2018-12-14 08:49:09 +0800

    for retrigger

--- src/operator/tensor/elemwise_unary_op.h
+++ src/operator/tensor/elemwise_unary_op.h
@@ -370,8 +370,8 @@ class UnaryOp : public OpBase {
                          const std::vector<OpReqType>& req,
                          const std::vector<TBlob>& outputs) {
     if (req[0] == kNullOp) return;
-    // if defined MSHADOW_USE_MKL then call mkl log when req is KWriteTo, type_flag is
-    // mshadow::kFloat32 or mshadow::kFloat64 and data size less than or equal MKL_INT_MAX
+    // if defined MSHADOW_USE_MKL then call mkl log when req is KWriteTo, type_flag
+    // is mshadow::kFloat32 or mshadow::kFloat64 and data size less than or equal MKL_INT_MAX
 #if MSHADOW_USE_MKL == 1
     auto type_flag = inputs[0].type_flag_;
     const size_t MKL_INT_MAX = (sizeof(MKL_INT) == sizeof(int)) ? INT_MAX : LLONG_MAX;
