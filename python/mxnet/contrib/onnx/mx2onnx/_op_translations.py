1deadd4478a658a4221a5ada5ed02e1bfff7b7c7
Vandana Kannan <vandana268@gmail.com>
2018-12-18 15:01:15 -0800

    ONNX export: MaxRoiPool

--- python/mxnet/contrib/onnx/mx2onnx/_op_translations.py
+++ python/mxnet/contrib/onnx/mx2onnx/_op_translations.py
@@ -1790,3 +1790,24 @@ def convert_random_normal(node, **kwargs):
         name=name
     )
     return [node]
+
+
+@mx_op.register("ROIPooling")
+def convert_roipooling(node, **kwargs):
+    """Map MXNet's ROIPooling operator attributes to onnx's MaxRoiPool
+    operator and return the created node.
+    """
+    name, input_nodes, attrs = get_inputs(node, kwargs)
+
+    pooled_shape = convert_string_to_list(attrs.get('pooled_size'))
+    scale = float(attrs.get("spatial_scale"))
+
+    node = onnx.helper.make_node(
+        'MaxRoiPool',
+        input_nodes,
+        [name],
+        pooled_shape=pooled_shape,
+        spatial_scale=scale,
+        name=name
+    )
+    return [node]
