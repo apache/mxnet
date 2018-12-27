f36f5628ee750a2c1c02a5c9b93493308b4dba5f
shesung <shesung.sam@gmail.com>
2018-12-27 10:31:31 +0800

    fix the typo

--- src/operator/contrib/roi_align.cc
+++ src/operator/contrib/roi_align.cc
@@ -563,9 +563,9 @@ He, Kaiming, et al. "Mask R-CNN." ICCV, 2017
   // out: [num_rois, c, pooled_h, pooled_w]
   out_shape->clear();
   if (param.position_sensitive) {
+    CHECK_EQ(dshape[1] % (param.pooled_size[0]*param.pooled_size[1]), 0) <<
+      "Input channels shuold be divided by pooled_size[0]*pooled_size[1]";
     out_shape->push_back(
-         CHECK_EQ(dshape[1] % (param.pooled_size[0]*param.pooled_size[1]), 0) <<
-           "Input channels shuold be divided by pooled_size[0]*pooled_size[1]";
          Shape4(bshape[0], dshape[1]/param.pooled_size[0]/param.pooled_size[1],
                 param.pooled_size[0], param.pooled_size[1]));
   } else {
