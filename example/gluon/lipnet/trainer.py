ac509a5420a180e626753ee8f23498c6596e9c63
soeque1 <soeque1@gmail.com>
2018-12-31 17:56:38 +0900

    Fix description

--- example/gluon/lipnet/trainer.py
+++ example/gluon/lipnet/trainer.py
@@ -1,6 +1,3 @@
-"""
-Description : Training module for LipNet
-"""
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -17,6 +14,11 @@ Description : Training module for LipNet
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+"""
+Description : Training module for LipNet
+"""
+
 
 import sys
 import mxnet as mx
