722a5733e99a60298997f2a522853a7a2034b08f
rondogency <dd208@sina.com>
2019-01-13 18:22:58 -0500

    edit copyright

--- src/operator/contrib/erfinv-inl.h
+++ src/operator/contrib/erfinv-inl.h
@@ -1,29 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-/*!
- * Copyright (c) 2019 by Contributors
- * \file erfinv-inl.h
- * \brief
- * \author Ziyi Mu
-*/
-
 /* The next function is taken from 
 https://github.com/antelopeusersgroup/antelope_contrib/blob/master/lib/location/libgenloc/erfinv.c.
 Below is the copyright. Output was modified to be inf or -inf when input is 1 or -1.
@@ -67,6 +41,9 @@ Below is the copyright. Output was modified to be inf or -inf when input is 1 or
 #ifndef MXNET_OPERATOR_CONTRIB_ERFINV_INL_H_
 #define MXNET_OPERATOR_CONTRIB_ERFINV_INL_H_
 
+#define _USE_MATH_DEFINES
+#define CENTRAL_RANGE 0.7
+
 #include <mxnet/base.h>
 #include "math.h"
 
@@ -74,7 +51,8 @@ namespace mxnet {
 namespace op {
 namespace mshadow_op {
 
-#define CENTRAL_RANGE 0.7
+using std::copysign;
+using std::atof;
 
 /*! \brief inverse gauss error function */
 struct erfinv : public mxnet_op::tunable {
