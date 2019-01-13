9e6ca5ee530b92b3772bdaaef2b4af60be7afc30
Ubuntu <ubuntu@ip-172-31-6-178.ec2.internal>
2019-01-13 01:52:00 +0000

    configurable random states for cuda

--- src/common/random_generator.cu
+++ src/common/random_generator.cu
@@ -32,10 +32,12 @@ namespace common {
 namespace random {
 
 template<>
-const int RandGenerator<gpu, float>::kMinNumRandomPerThread = 64;
+const int RandGenerator<gpu, float>::kMinNumRandomPerThread =
+  dmlc::GetEnv("MXNET_CUDA_NUM_RAND_STATES_PER_THREAD", 64);
 
 template<>
-const int RandGenerator<gpu, float>::kNumRandomStates = 32768;
+const int RandGenerator<gpu, float>::kNumRandomStates =
+  dmlc::GetEnv("MXNET_CUDA_NUM_RAND_STATES", 32768);
 
 __global__ void rand_generator_seed_kernel(curandStatePhilox4_32_10_t *states_,
                                            const int size,
