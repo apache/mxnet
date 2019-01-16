b81b3d751a4cfd1117d7d6d277958ef914da4977
Jake Lee <gstu1130@gmail.com>
2019-01-16 11:17:59 +0800

    Update example/cnn_chinese_text_classification/data_helpers.py
    Co-Authored-By: cchung100m <cchung100m@cs.ccu.edu.tw>

--- example/cnn_chinese_text_classification/data_helpers.py
+++ example/cnn_chinese_text_classification/data_helpers.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 """Helper functions to support for implementing CNN + Highway Network for Chinese Text Classification in MXNet"""
 
 import codecs
