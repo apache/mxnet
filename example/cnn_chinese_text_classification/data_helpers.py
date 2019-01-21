62f33d0a3d3a2659caac0d386a97bb34036e7288
Neil Chien <cchung100m@cs.ccu.edu.tw>
2019-01-21 10:21:19 +0800

    Correct the misuse of tenses
    Correct the misuse of tenses from preprocessed to preprocesses

--- example/cnn_chinese_text_classification/data_helpers.py
+++ example/cnn_chinese_text_classification/data_helpers.py
@@ -123,9 +123,7 @@ def build_input_data(sentences, labels, vocabulary):
 
 
 def build_input_data_with_word2vec(sentences, labels, word2vec_list):
-    """
-    Map sentences and labels to vectors based on a pre-trained word2vec
-    """
+    """Map sentences and labels to vectors based on a pre-trained word2vec"""
     x_vec = []
     for sent in sentences:
         vec = []
