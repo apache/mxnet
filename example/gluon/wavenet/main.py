8a3a55b9a64bc2282f7ce482b817694a29db68c0
seujung <digit82@gmail.com>
2019-01-24 14:50:26 +0900

    add descriptions

--- example/gluon/wavenet/main.py
+++ example/gluon/wavenet/main.py
@@ -22,20 +22,29 @@ from trainer import Train
 
 def main():
     """
-    Description : run code using argument info
+    Description : run wavenet code using argument info
+    - batch_size : Define batch size (default=64)
+    - epochs : Define the total number of epochs (default=1000)
+    - mu : Define mu value for mu-law algorithm (default=128)
+    - n_residue : Define number of residue (default=24)
+    - n_skip : Define number of skip (default=128)
+    - dilation_depth : Define dilation depth (default=10)
+    - use_gpu : whether or not to use the GPU (default=True)
+    - generation : whether or not to generate a wave file for model (default=True)
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, default=64)
-    parser.add_argument('--epoches', type=int, default=10)
-    parser.add_argument('--mu', type=int, default=128)
-    parser.add_argument('--n_residue', type=int, default=24)
-    parser.add_argument('--n_skip', type=int, default=128)
-    parser.add_argument('--dilation_depth', type=int, default=10)
-    parser.add_argument('--n_repeat', type=int, default=2)
-    parser.add_argument('--seq_size', type=int, default=20000)
-    parser.add_argument('--use_gpu', type=str, default="True")
-    parser.add_argument('--generation', type=bool, default=True)
+    parser = argparse.ArgumentParser(description='argument for wavenet hyperparameters')
+    parser.add_argument('--batch_size', type=int, default=64, help="batch size")
+    parser.add_argument('--epochs', type=int, default=10, help="total number of epochs")
+    parser.add_argument('--mu', type=int, default=128, help="mu value for mu-law algorithm")
+    parser.add_argument('--n_residue', type=int, default=24, help="number of residue")
+    parser.add_argument('--n_skip', type=int, default=128, help="number of skip")
+    parser.add_argument('--dilation_depth', type=int, default=10, help="number of dilation depth")
+    parser.add_argument('--n_repeat', type=int, default=2, help="number of repeat")
+    parser.add_argument('--seq_size', type=int, default=20000, help="number of sequence size")
+    parser.add_argument('--use_gpu', type=str, default="True", help="use gpu")
+    parser.add_argument('--generation', type=bool, default=True, help="generate a wave file")
     config = parser.parse_args()
+    print(config)
 
     trainer = Train(config)
 
