11217c29446b3db84d221fd52f5863c007c6bd1f
Zhennan Qin <zhennan.qin@intel.com>
2019-02-04 12:18:58 +0800

    Add default value for monitor_all for cpp header.

--- src/executor/graph_executor.h
+++ src/executor/graph_executor.h
@@ -68,7 +68,7 @@ class GraphExecutor : public Executor {
   const std::unordered_map<std::string, NDArray>& arg_grad_map() const override;
   const std::unordered_map<std::string, NDArray>& aux_state_map() const override;
   void Print(std::ostream &os) const override; // NOLINT(*)
-  void SetMonitorCallback(const MonitorCallback& callback, bool monitor_all) override;
+  void SetMonitorCallback(const MonitorCallback& callback, bool monitor_all = false) override;
   // Initialize the rest of attributes
   // after setting up arguments.
   void FinishInitGraph(nnvm::Symbol symbol, nnvm::Graph g,
