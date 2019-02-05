b35d8c9021446df3f0c6a117db78fa4a143a8656
Piyush Ghai <ghai.8@osu.edu>
2019-02-05 11:50:59 -0800

    updating scala docs

--- scala-package/infer/src/main/scala/org/apache/mxnet/infer/ObjectDetector.scala
+++ scala-package/infer/src/main/scala/org/apache/mxnet/infer/ObjectDetector.scala
@@ -28,7 +28,8 @@ import org.apache.mxnet.Context
 import scala.collection.mutable.ListBuffer
 
 /**
-  * A class for object detection tasks
+  * The ObjectDetector class helps to run ObjectDetection tasks where the goal
+  * is to find bounding boxes and corresponding labels for objects in a image.
   *
   * @param modelPathPrefix    Path prefix from where to load the model artifacts.
   *                           These include the symbol, parameters, and synset.txt.
