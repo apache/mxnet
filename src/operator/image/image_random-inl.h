d58f50c28efe42698163bfd3b82f48c8bcd20571
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-02-09 22:59:18 -0800

    disable warning on windows

--- src/operator/image/image_random-inl.h
+++ src/operator/image/image_random-inl.h
@@ -25,6 +25,9 @@
 #ifndef MXNET_OPERATOR_IMAGE_IMAGE_RANDOM_INL_H_
 #define MXNET_OPERATOR_IMAGE_IMAGE_RANDOM_INL_H_
 
+#ifdef _MSC_VER
+  #pragma warning(disable:4503)  // disable warning: decorated name length exceeded.
+#endif
 
 #include <algorithm>
 #include <cmath>
