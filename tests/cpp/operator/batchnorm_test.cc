88b3741756379c4ce8cec429601a0848a3dfae57
perdasilva <perdasilva@gmail.com>
2019-03-14 10:45:10 +0100

    Disables flaky TestStochasticTiming_2D test (#14412)

--- tests/cpp/operator/batchnorm_test.cc
+++ tests/cpp/operator/batchnorm_test.cc
@@ -980,7 +980,9 @@ static void timingTest(const std::string& label,
 #endif  // MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
 
 /*! \brief Stress-test random batch size/channels/dimension(s) */
-TEST(BATCH_NORM, TestStochasticTiming_2D) {
+TEST(BATCH_NORM, DISABLED_TestStochasticTiming_2D) {
+  // Test is disabled due to suspected flakiness
+  // https://github.com/apache/incubator-mxnet/issues/14411
   MSHADOW_REAL_TYPE_SWITCH_EX(
     mshadow::kFloat32, DType, AccReal,
     {
