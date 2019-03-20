0f64b095d692f2c8535e0897501bf1cc54feeb92
stu1130 <gstu1130@gmail.com>
2019-03-19 18:16:21 -0700

    adjust the success rate

--- tests/python/unittest/test_random.py
+++ tests/python/unittest/test_random.py
@@ -642,14 +642,14 @@ def test_multinomial_generator():
         generator_mx = lambda x: mx.nd.random.multinomial(data=mx.nd.array(quantized_probs, ctx=ctx, dtype=dtype),
                                                           shape=x).asnumpy()
         verify_generator(generator=generator_mx, buckets=buckets, probs=quantized_probs,
-                         nsamples=samples, nrepeat=trials)
+                         nsamples=samples, nrepeat=trials, success_rate=0.20)
         generator_mx_same_seed = \
             lambda x: np.concatenate(
                 [mx.nd.random.multinomial(data=mx.nd.array(quantized_probs, ctx=ctx, dtype=dtype),
                                                           shape=x // 10).asnumpy()
                  for _ in range(10)])
         verify_generator(generator=generator_mx_same_seed, buckets=buckets, probs=quantized_probs,
-                         nsamples=samples, nrepeat=trials)
+                         nsamples=samples, nrepeat=trials, success_rate=0.20)
 
 
 @with_seed()
