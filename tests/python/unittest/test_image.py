938c908acdfda3ddb8aa66d2f11db6b4ff1b4e15
Sharma <abhinav0301@gmail.com>
2019-04-02 10:19:21 -0700

    fix test

--- tests/python/unittest/test_image.py
+++ tests/python/unittest/test_image.py
@@ -361,7 +361,7 @@ class TestImage(unittest.TestCase):
         width = np.random.randint(100, 500)
         height = np.random.randint(100, 500)
         src = np.random.rand(height, width, 3) * 255.
-        ratio = (3/4, 1)
+        ratio = (0.75, 1)
         out, (x0, y0, new_w, new_h) = mx.image.random_size_crop(mx.nd.array(src), size=(width, height), area=0.08, ratio=ratio)
         _, pts = mx.image.center_crop(mx.nd.array(src), size=(width, height))
         if (x0, y0, new_w, new_h) != pts:
