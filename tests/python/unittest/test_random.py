a6fd0e9ac500e15a98a9e21a132fc14f75c6340d
Jake Lee <gstu1130@gmail.com>
2019-04-03 09:55:25 +0800

    Fix flaky test poisson generator & test_negative_binomial_generator (#14571)
    * set success_rate to 0.2
    
    * fix the flaky test
    
    * reenable the test

--- tests/python/unittest/test_random.py
+++ tests/python/unittest/test_random.py
@@ -568,7 +568,6 @@ def test_exponential_generator():
                      for _ in range(10)])
             verify_generator(generator=generator_mx_same_seed, buckets=buckets, probs=probs, success_rate=0.20)
 
-@unittest.skip("Flaky test. Tracked in https://github.com/apache/incubator-mxnet/issues/13584")
 @with_seed()
 def test_poisson_generator():
     ctx = mx.context.current_context()
@@ -584,7 +583,6 @@ def test_poisson_generator():
                      for _ in range(10)])
             verify_generator(generator=generator_mx_same_seed, buckets=buckets, probs=probs)
 
-@unittest.skip("Flaky test. Tracked in https://github.com/apache/incubator-mxnet/issues/13506")
 @with_seed()
 def test_negative_binomial_generator():
     ctx = mx.context.current_context()
