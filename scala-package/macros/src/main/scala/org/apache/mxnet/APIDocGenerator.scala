800a2e61abf108c691210a9278abd2c61f4d2c2f
Qing <lanking520@live.com>
2019-04-29 13:14:45 -0700

    add fixes to code gen

--- scala-package/macros/src/main/scala/org/apache/mxnet/APIDocGenerator.scala
+++ scala-package/macros/src/main/scala/org/apache/mxnet/APIDocGenerator.scala
@@ -23,12 +23,16 @@ import java.security.MessageDigest
 import scala.collection.mutable.ListBuffer
 
 /**
-  * This object will generate the Scala documentation of the new Scala API
-  * Two file namely: SymbolAPIBase.scala and NDArrayAPIBase.scala
+  * This object will generate the Scala documentation of the Scala/Java APIs
   * The code will be executed during Macros stage and file live in Core stage
   */
 private[mxnet] object APIDocGenerator extends GeneratorBase {
 
+  /**
+    * Main method used to generate code and write to files
+    * A hash check placed at the end to verify changes
+    * @param args Input args
+    */
   def main(args: Array[String]): Unit = {
     val FILE_PATH = args(0)
     val hashCollector = ListBuffer[String]()
@@ -40,6 +44,12 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
     val finalHash = hashCollector.mkString("\n")
   }
 
+  /**
+    * Generate MD5 result from an input string
+    * Encoded in UTF-8
+    * @param input The input string
+    * @return A MD5 value from the string
+    */
   def MD5Generator(input: String): String = {
     val md = MessageDigest.getInstance("MD5")
     md.update(input.getBytes("UTF-8"))
@@ -47,6 +57,12 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
     org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(digest)
   }
 
+  /**
+    * Type-safe class body generation for NDArray/Symbol
+    * @param FILE_PATH File path write the file to
+    * @param isSymbol Check if write the Symbol API, NDArray otherwise
+    * @return MD5 String
+    */
   def typeSafeClassGen(FILE_PATH: String, isSymbol: Boolean): String = {
     val generated = typeSafeFunctionsToGenerate(isSymbol, isContrib = false)
       .map { func =>
@@ -57,11 +73,22 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
 
     writeFile(
       FILE_PATH,
-      if (isSymbol) "SymbolAPIBase" else "NDArrayAPIBase",
       "package org.apache.mxnet",
+      if (isSymbol) "SymbolAPIBase" else "NDArrayAPIBase",
+      "import org.apache.mxnet.annotation.Experimental",
       generated)
   }
 
+  /**
+    * Non Type-safe interface of Scala Symbol/NDArray
+    * It includes class definition : e.g class SymbolBase
+    * and function definitions : e.g def softmax(...)(...)(...) : NDArray
+    * Users can directly use the api by calling NDArray.<function_name>
+    * It support both positional input or Map input
+    * @param FILE_PATH File path write the file to
+    * @param isSymbol Check if write the Symbol API, NDArray otherwise
+    * @return MD5 String
+    */
   def nonTypeSafeClassGen(FILE_PATH: String, isSymbol: Boolean): String = {
     val absFuncs = functionsToGenerate(isSymbol, isContrib = false)
       .map { func =>
@@ -85,28 +112,34 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
 
     writeFile(
       FILE_PATH,
-      if (isSymbol) "SymbolBase" else "NDArrayBase",
       "package org.apache.mxnet",
+      if (isSymbol) "SymbolBase" else "NDArrayBase",
+      "import org.apache.mxnet.annotation.Experimental",
       absFuncs)
   }
 
-  def javaClassGen(filePath : String) : String = {
+  /**
+    * Type-safe interface of Java NDArray
+    * @param FILE_PATH File path write the file to
+    * @return MD5 String
+    */
+  def javaClassGen(FILE_PATH : String) : String = {
     val notGenerated = Set("Custom")
     val absClassFunctions = functionsToGenerate(false, false, true)
     val (absFuncs, paramClassUncleaned) =
       absClassFunctions.filterNot(ele => notGenerated.contains(ele.name))
-      .groupBy(_.name.toLowerCase).map(ele => {
-      /* Pattern matching for not generating deprecated method
-       * Group all method name in lowercase
-       * Kill the capital lettered method such as Cast vs cast
-       * As it defined by default it deprecated
-       */
-      if (ele._2.length == 1) ele._2.head
-      else {
-        if (ele._2.head.name.head.isLower) ele._2.head
-        else ele._2.last
-      }
-    }).map(absClassFunction => {
+        .groupBy(_.name.toLowerCase).map(ele => {
+        /* Pattern matching for not generating deprecated method
+         * Group all method name in lowercase
+         * Kill the capital lettered method such as Cast vs cast
+         * As it defined by default it deprecated
+         */
+        if (ele._2.length == 1) ele._2.head
+        else {
+          if (ele._2.head.name.head.isLower) ele._2.head
+          else ele._2.last
+        }
+      }).map(absClassFunction => {
         generateJavaAPISignature(absClassFunction)
       }).toSeq.unzip
     val paramClass = paramClassUncleaned.filterNot(_.isEmpty)
@@ -120,6 +153,12 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
       absFuncs, Some(paramClass))
   }
 
+  /**
+    * Generate Scala docs from the function description
+    * @param func The function case class
+    * @param withParam Whether to generate param field
+    * @return A formatted string for the function description
+    */
   def generateAPIDocFromBackend(func: Func, withParam: Boolean = true): String = {
     def fixDesc(desc: String): String = {
       var curDesc = desc
@@ -153,7 +192,15 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
     }
   }
 
-  def generateAPISignature(func: Func, isSymbol: Boolean): String = {
+  /**
+    * Generate the function interface
+    * e.g: def softmax(data: NDArray, name ...): NDArrayFunctionReturn
+    * @param func The function case class
+    * @param isSymbol Check if generate Symbol function, NDArray otherwise
+    * @param typeParameter Type param specifically used in Random Module
+    * @return Formatted string for the function
+    */
+  def generateAPISignature(func: Func, isSymbol: Boolean, typeParameter: String = ""): String = {
     val argDef = ListBuffer[String]()
 
     argDef ++= typedFunctionCommonArgDef(func)
@@ -169,7 +216,7 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
     val returnType = func.returnType
 
     s"""@Experimental
-       |def ${func.name} (${argDef.mkString(", ")}): $returnType""".stripMargin
+       |def ${func.name}$typeParameter (${argDef.mkString(", ")}): $returnType""".stripMargin
   }
 
   /**
@@ -217,22 +264,22 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
            | def getOut() = this.out
            | """.stripMargin
       (s"""$scalaDocNoParam
-         | $experimentalTag
-         | def ${func.name}(po: ${func.name}Param) : $returnType
-         | """.stripMargin,
+          | $experimentalTag
+          | def ${func.name}(po: ${func.name}Param) : $returnType
+          | """.stripMargin,
         s"""/**
-         | * This Param Object is specifically used for ${func.name}
-         | ${requiredParam.mkString("\n")}
-         | */
-         | class ${func.name}Param(${argDef.mkString(",")}) {
-         |  ${classDef.mkString("\n  ")}
-         | }""".stripMargin)
+           | * This Param Object is specifically used for ${func.name}
+           | ${requiredParam.mkString("\n")}
+           | */
+           | class ${func.name}Param(${argDef.mkString(",")}) {
+           |  ${classDef.mkString("\n  ")}
+           | }""".stripMargin)
     } else {
       argDef += "out : NDArray"
       (s"""$scalaDoc
-         |$experimentalTag
-         | def ${func.name}(${argDef.mkString(", ")}) : $returnType
-         | """.stripMargin, "")
+          |$experimentalTag
+          | def ${func.name}(${argDef.mkString(", ")}) : $returnType
+          | """.stripMargin, "")
     }
   }
 
@@ -251,25 +298,25 @@ private[mxnet] object APIDocGenerator extends GeneratorBase {
 
     val finalStr =
       s"""/*
-         |* Licensed to the Apache Software Foundation (ASF) under one or more
-         |* contributor license agreements.  See the NOTICE file distributed with
-         |* this work for additional information regarding copyright ownership.
-         |* The ASF licenses this file to You under the Apache License, Version 2.0
-         |* (the "License"); you may not use this file except in compliance with
-         |* the License.  You may obtain a copy of the License at
-         |*
-         |*    http://www.apache.org/licenses/LICENSE-2.0
-         |*
-         |* Unless required by applicable law or agreed to in writing, software
-         |* distributed under the License is distributed on an "AS IS" BASIS,
-         |* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-         |* See the License for the specific language governing permissions and
-         |* limitations under the License.
-         |*/
+         | * Licensed to the Apache Software Foundation (ASF) under one or more
+         | * contributor license agreements.  See the NOTICE file distributed with
+         | * this work for additional information regarding copyright ownership.
+         | * The ASF licenses this file to You under the Apache License, Version 2.0
+         | * (the "License"); you may not use this file except in compliance with
+         | * the License.  You may obtain a copy of the License at
+         | *
+         | *    http://www.apache.org/licenses/LICENSE-2.0
+         | *
+         | * Unless required by applicable law or agreed to in writing, software
+         | * distributed under the License is distributed on an "AS IS" BASIS,
+         | * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+         | * See the License for the specific language governing permissions and
+         | * limitations under the License.
+         | */
          |
          |$packageDef
          |
-         |import org.apache.mxnet.annotation.Experimental
+         |$imports
          |
          |// scalastyle:off
          |abstract class $className {
