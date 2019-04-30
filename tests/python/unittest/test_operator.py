Fix the index_array gradient checks in the unit tests
In the previous implementation, the output gradient had an incorrect
shape. This commit fixes the shapes and makes the tests more readable.

tests/python/unittest/test_operator.py
@@ -8433,11 +8433,12 @@ def test_index_array_default():
         data  = mx.symbol.Variable("data")
         index_array = mx.sym.contrib.index_array(data)
 
+        input_array = np.ones(shape)
         mgrid = np.mgrid[tuple(slice(0, x) for x in shape)]
         expected = np.stack(mgrid, axis=-1)
-        
-        check_symbolic_forward(index_array, [np.ones(shape)], [expected])
-        check_symbolic_backward(index_array, [np.ones(shape)], [np.ones(shape)], [np.zeros(shape)])
+
+        check_symbolic_forward(index_array, [input_array], [expected])
+        check_symbolic_backward(index_array, [input_array], [np.ones(expected.shape)], [np.zeros_like(input_array)])
 
 @with_seed()
 def test_index_array_select_axes():
@@ -8446,12 +8447,12 @@ def test_index_array_select_axes():
         data  = mx.symbol.Variable("data")
         index_array = mx.sym.contrib.index_array(data, axes=axes)
 
+        input_array = np.ones(shape)
         mgrid = np.mgrid[tuple(slice(0, x) for x in shape)]
         expected = np.stack(mgrid, axis=-1)[..., axes]
 
-        check_symbolic_forward(index_array, [np.ones(shape)], [expected])
-        check_symbolic_backward(index_array, [np.ones(shape)], [np.ones(shape)], [np.zeros(shape)])
-
+        check_symbolic_forward(index_array, [input_array], [expected])
+        check_symbolic_backward(index_array, [input_array], [np.ones(expected.shape)], [np.zeros_like(input_array)])
 
 @with_seed()
 def test_scalar_tensor_creation():