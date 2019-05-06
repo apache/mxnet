1be6c4d865a159ce0d2dad2a8f557127c7d5fda3
Pedro Larroy <pedro.larroy.lists@gmail.com>
2019-05-06 13:18:33 -0700

    Fix warning

--- tests/cpp/misc/libinfo_test.cc
+++ tests/cpp/misc/libinfo_test.cc
@@ -30,4 +30,5 @@ using namespace mxnet::features;
 TEST(RuntimeTest, RuntimeTestAll) {
   EXPECT_EQ(EnumNames::names.size(), MAX_FEATURES);
   const auto& features = LibInfo::getInstance()->getFeatures();
+  EXPECT_FALSE(features.empty());
 }
