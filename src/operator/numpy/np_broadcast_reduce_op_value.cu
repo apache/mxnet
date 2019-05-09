6c461f22714b194b25853b1cbc5e5b3ae94045d1
Hao Jin <hjjn.amzn@gmail.com>
2019-05-09 13:51:48 -0700

    numpy-compatible mean (#14859)

--- src/operator/numpy/np_broadcast_reduce_op_value.cu
+++ src/operator/numpy/np_broadcast_reduce_op_value.cu
@@ -26,11 +26,19 @@
 
 namespace mxnet {
 namespace op {
+
 NNVM_REGISTER_OP(_numpy_sum)
 .set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesCompute<gpu, mshadow_op::sum, true>);
 
 NNVM_REGISTER_OP(_backward_numpy_sum)
 .set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesBackwardUseNone<gpu>);
+
+NNVM_REGISTER_OP(_numpy_mean)
+.set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesCompute<gpu, mshadow_op::sum, true, true>);
+
+NNVM_REGISTER_OP(_backward_numpy_mean)
+.set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesBackwardUseNone<gpu, true>);
+
 
 }  // namespace op
 }  // namespace mxnet
