c557788ff0666df80ccc1665f261d85e09582c03
Tao Lv <tao.a.lv@intel.com>
2019-05-17 10:55:30 +0800

    fix windows build

--- src/operator/mkl_functions-inl.h
+++ src/operator/mkl_functions-inl.h
@@ -136,7 +136,7 @@ MSHADOW_XINLINE static void sum_(index_t n, DType *in, DType *dst) {
 }
 
 template <typename DType>
-MSHADOW_XINLINE static void max_(int n, DType * __restrict__ in, DType *dst) {
+MSHADOW_XINLINE static void max_(int n, DType *in, DType *dst) {
   dst[0] = in[0];
   for (int i = 1; i < n; i++)
     dst[0] = (dst[0] < in[i]) ? in[i] : dst[0];
