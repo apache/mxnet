0b44ff220c94309f90672e6b1477aa5e3182b23d
SSE4 <tomskside@gmail.com>
2019-05-21 01:11:30 +0700

    - remove lapack (OpenBLAS provides it)

--- conanfile.py
+++ conanfile.py
@@ -21,8 +21,3 @@ class IncubatorMXNetConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "openblas/0.2.20@conan/stable", "opencv/3.4.3@conan/stable"
     generators = ["cmake"]
-
-    def configure(self):
-        if self.settings.compiler == "Visual Studio":
-            self.options["lapack"].visual_studio = True
-            self.options["lapack"].shared = True
