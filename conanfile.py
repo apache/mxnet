5cc9b415da917fe3259d5d8db63d79370b6807c4
SSE4 <tomskside@gmail.com>
2019-05-21 01:11:09 +0700

    - remove lapack (OpenBLAS provides it)

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ from conans import ConanFile
 
 class IncubatorMXNetConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "openblas/0.2.20@conan/stable", "opencv/3.4.3@conan/stable", "lapack/3.7.1@conan/stable"
+    requires = "openblas/0.2.20@conan/stable", "opencv/3.4.3@conan/stable"
     generators = ["cmake"]
 
     def configure(self):
