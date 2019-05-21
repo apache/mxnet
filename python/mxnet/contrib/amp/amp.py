f8ed3602246da716eb4ec1f2e38e918f1c332aee
Per Goncalves da Silva <perdasilva@gmail.com>
2019-05-21 08:37:23 +0200

    Fixes lint issue in AMP

--- python/mxnet/contrib/amp/amp.py
+++ python/mxnet/contrib/amp/amp.py
@@ -264,7 +264,7 @@ def init(target_dtype='float16', target_precision_ops=None,
     global _amp_initialized
     global _loss_scaler
     if not _amp_initialized:
-        assert target_dtype == 'float16' or target_dtype == np.float16, \
+        assert target_dtype in ['float16', np.float16], \
                "AMP currently supports only float16 as a target_dtype"
         _amp_initialized = True
         logging.info("Using AMP")
