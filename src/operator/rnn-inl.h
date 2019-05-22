bc66cdf2f86d1514764f57e6858834496c7c3f77
Wei <zixuan.wei@intel.com>
2019-05-22 16:31:53 +0800

    Remove MKLDNN GRU

--- src/operator/rnn-inl.h
+++ src/operator/rnn-inl.h
@@ -903,7 +903,7 @@ class RNNOp {
                                   param_.mode);
       } else {
         #if MXNET_USE_MKLDNN == 1 && !defined(__CUDACC__)
-        if (dmlc::GetEnv("MXNET_USE_MKLDNN_RNN", 1)) {
+        if (dmlc::GetEnv("MXNET_USE_MKLDNN_RNN", 1) && param_.mode != rnn_enum::kGru) {
           // TODO(zixuanweeei): MKLDNN GRU has precision issue. A stable one
           //   will be added to MXNet when we figure out the issue.
           int dtype = in_data[rnn_enum::kData].type_flag_;
