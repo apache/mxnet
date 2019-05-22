02ee00574693965b2d3d860ce6698a80f580e9ad
Wei <zixuan.wei@intel.com>
2019-05-22 17:45:56 +0800

    Fix bug for rnn forward

--- src/operator/rnn-inl.h
+++ src/operator/rnn-inl.h
@@ -969,9 +969,10 @@ class RNNOp {
                                     hy_ptr,
                                     cy_ptr,
                                     param_.mode);
-      #if MXNET_USE_MKLDNN == 1 && !defined(__CUDACC__)
+        #if MXNET_USE_MKLDNN == 1 && !defined(__CUDACC__)
+        }
+        #endif
       }
-      #endif
     }
   }
 
