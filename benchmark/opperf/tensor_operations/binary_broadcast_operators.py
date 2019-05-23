f9b6cd721b0539b93a45520121e5aa711c223310
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-05-23 16:28:26 -0700

    Add ability to query MXNet operator registry for operators and run benchmarks

---
+++ benchmark/opperf/tensor_operations/binary_broadcast_operators.py
@@ -0,0 +1,67 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Performance benchmark tests for MXNet NDArray Binary Broadcast Operations.
+1. Operators are automatically fetched from MXNet operator registry.
+2. Default Inputs are generated. See rules/input_shapes.py. You can override the default values.
+
+Below 20 Operators are covered:
+
+['broadcast_add', 'broadcast_div', 'broadcast_equal', 'broadcast_greater', 'broadcast_greater_equal',
+'broadcast_hypot', 'broadcast_lesser', 'broadcast_lesser_equal', 'broadcast_logical_and',
+'broadcast_logical_or', 'broadcast_logical_xor', 'broadcast_maximum', 'broadcast_minimum',
+'broadcast_minus', 'broadcast_mod', 'broadcast_mul', 'broadcast_not_equal', 'broadcast_plus',
+'broadcast_power', 'broadcast_sub']
+
+"""
+import mxnet as mx
+
+from benchmark.opperf.utils.benchmark_utils import run_performance_test
+from benchmark.opperf.utils.common_utils import merge_map_list
+from benchmark.opperf.utils.op_registry_utils import get_all_broadcast_binary_operators, prepare_op_inputs
+from benchmark.opperf.rules.input_shapes import DEFAULT_BINARY_BROADCAST_OP_INPUTS
+
+
+def run_mx_binary_operators_benchmarks(ctx=mx.cpu(), dtype='float32', warmup=10, runs=50):
+    """Runs benchmarks with the given context and precision (dtype)for all the binary
+    broadcast operators in MXNet.
+
+    :param ctx: Context to run benchmarks
+    :param dtype: Precision to use for benchmarks
+    :param warmup: Number of times to run for warmup
+    :param runs: Number of runs to capture benchmark results
+    :return: Dictionary of results. Key -> Name of the operator, Value -> Benchmark results.
+
+    """
+    # Fetch all Binary Broadcast Operators
+    mx_binary_ops = get_all_broadcast_binary_operators()
+
+    # For each operator, run benchmarks
+    mx_binary_op_results = []
+    for _, op_params in mx_binary_ops.items():
+        # Prepare inputs for the operator
+        inputs = prepare_op_inputs(op_params, DEFAULT_BINARY_BROADCAST_OP_INPUTS)
+        # Run benchmarks
+        cur_op_res = run_performance_test(op_params["nd_op_handle"], run_backward=op_params["has_backward"],
+                                          dtype=dtype, ctx=ctx,
+                                          inputs=inputs,
+                                          warmup=warmup, runs=runs)
+        mx_binary_op_results += cur_op_res
+
+    # Prepare combined results for Arithmetic operators
+    mx_binary_op_results = merge_map_list(mx_binary_op_results)
+    return mx_binary_op_results
---
+++ benchmark/opperf/utils/op_registry_utils.py
@@ -0,0 +1,165 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Utilities to interact with MXNet operator registry."""
+import ctypes
+import sys
+from mxnet.base import _LIB, check_call, py_str, OpHandle, c_str, mx_uint
+
+# We will use all operators inside NDArray Module
+mx_nd_module = sys.modules["mxnet.ndarray.op"]
+
+
+def _get_all_registered_ops(filters=("_backward", "_contrib", "_")):
+    """Get all registered MXNet operators.
+
+    By default, filter out all backward operators that starts with  '_backward',
+    Contrib operators that starts with '_contrib' and internal operators that 
+    starts with '_'.
+
+    Parameters
+    ----------
+    filters: tuple(str)
+        List of operator name prefix to ignore from benchmarking.
+        Default - ("_backward", "_contrib", "_")
+    
+    Returns
+    -------
+    {"operator_name": {"has_backward", "nd_op_handle"}}
+    """
+    plist = ctypes.POINTER(ctypes.c_char_p)()
+    size = ctypes.c_uint()
+
+    check_call(_LIB.MXListAllOpNames(ctypes.byref(size),
+                                     ctypes.byref(plist)))
+    mx_operators = {}
+    operators_with_backward = []
+
+    # Prepare master list of all operators.
+    for i in range(size.value):
+        cur_op_name = py_str(plist[i])
+        if not cur_op_name.startswith(filters):
+            mx_operators[cur_op_name] = {"has_backward": False,
+                                         "nd_op_handle": getattr(mx_nd_module, cur_op_name)}
+
+        if cur_op_name.startswith("_backward_"):
+            operators_with_backward.append(cur_op_name)
+
+    # Identify all operators that can run backward.
+    for op_with_backward in operators_with_backward:
+        op_name = op_with_backward.split("_backward_")[1]
+        if op_name in mx_operators:
+            mx_operators[op_name]["has_backward"] = True
+
+    return mx_operators
+
+
+def _get_op_handles(op_name):
+    """Get handle for an operator with given name - op_name.
+
+    Parameters
+    ----------
+    op_name: str
+        Name of operator to get handle for.
+    """
+    op_handle = OpHandle()
+    check_call(_LIB.NNGetOpHandle(c_str(op_name), ctypes.byref(op_handle)))
+    return op_handle
+
+
+def _get_op_arguments(op_name, op_handle):
+    """Given operator name and handle, fetch operator arguments - number of arguments,
+    argument names, argument types.
+
+    Parameters
+    ----------
+    op_name: str
+        Name of the operator
+    
+    op_handle: OpHandle
+        Handle for the operator
+    
+    Returns
+    -------
+    (narg, arg_names, arg_types)
+    """
+    real_name = ctypes.c_char_p()
+    desc = ctypes.c_char_p()
+    num_args = mx_uint()
+    arg_names = ctypes.POINTER(ctypes.c_char_p)()
+    arg_types = ctypes.POINTER(ctypes.c_char_p)()
+    arg_descs = ctypes.POINTER(ctypes.c_char_p)()
+    key_var_num_args = ctypes.c_char_p()
+    ret_type = ctypes.c_char_p()
+
+    check_call(_LIB.MXSymbolGetAtomicSymbolInfo(
+        op_handle, ctypes.byref(real_name), ctypes.byref(desc),
+        ctypes.byref(num_args),
+        ctypes.byref(arg_names),
+        ctypes.byref(arg_types),
+        ctypes.byref(arg_descs),
+        ctypes.byref(key_var_num_args),
+        ctypes.byref(ret_type)))
+
+    narg = int(num_args.value)
+    arg_names = [py_str(arg_names[i]) for i in range(narg)]
+    arg_types = [py_str(arg_types[i]) for i in range(narg)]
+
+    return narg, arg_names, arg_types
+
+
+def _set_op_arguments(mx_operators):
+    """Fetch and set operator arguments - nargs, arg_names, arg_types
+    """
+    for op_name in mx_operators:
+        op_handle = _get_op_handles(op_name)
+        narg, arg_names, arg_types = _get_op_arguments(op_name, op_handle)
+        mx_operators[op_name]["params"] = {"narg": narg,
+                                           "arg_names": arg_names,
+                                           "arg_types": arg_types}
+
+
+def prepare_op_inputs(arg_params, arg_values):
+    # For each default value combination, prepare inputs
+    inputs = []
+    for arg_value in arg_values:
+        inp = {}
+        for arg_name in arg_params["params"]["arg_names"]:
+            inp[arg_name] = arg_value[arg_name]
+        inputs.append(inp)
+    return inputs
+
+
+def get_all_broadcast_binary_operators():
+    """Gets all binary operators registered with MXNet.
+
+    Returns
+    -------
+    {"operator_name": {"has_backward", "nd_op_handle", "params"}}
+    """
+    # Step 1 - Get all registered op names
+    mx_operators = _get_all_registered_ops()
+
+    # Step 2 - Get all parameters for the operators
+    _set_op_arguments(mx_operators)
+    binary_mx_operators = {}
+    for op_name, op_params in mx_operators.items():
+        if op_name.startswith("broadcast_") and op_params["params"]["narg"] == 2 and \
+                "lhs" in op_params["params"]["arg_names"] and \
+                "lhs" in op_params["params"]["arg_names"]:
+            binary_mx_operators[op_name] = mx_operators[op_name]
+    return binary_mx_operators
