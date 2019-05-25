f689042686b6c502e3b906680a31a0980aa381ef
Nick Guletskii <nick@nickguletskii.com>
2019-05-25 23:54:30 +0300

    [MXNET-1382] Add the index_array operator (#14638)
    * Implement the index_array operator
    
    * Add index_array operator tests
    
    * Add index_array operator GPU tests
    
    * Add the index_array operator to the Python docs autosummary
    
    * Add the author of the index_array operator to CONTRIBUTORS.md
    
    * Make index_array compatible with zero-dim and zero-size arrays
    
    Changes the implementation of index_array to be compatible with the
    recently merged support for zero-dim and zero-size arrays. Resolves the
    incompatibilities with #14661.
    
    * Fix the index_array gradient checks in the unit tests
    
    In the previous implementation, the output gradient had an incorrect
    shape. This commit fixes the shapes and makes the tests more readable.
    
    * Add zero-dim and zero-size array tests for index_array
    
    * Use mxnet::Tuple<int> instead of TShape for the axes parameter
    
    * Fix incorrect array indexing in index_array
    
    Solves access violations when compiling with MSVC++ 14.0.
    
    * Avoid copying the input shape array in the index_array shape function
    
    * Add unknown shape handling to index_array
    
    * Use SHAPE_ASSIGN_CHECK to assign the shape in index_array
    
    * Remove the redundant index_array GPU tests from test_operator_gpu.py
    
    * Move the index_array tests into a single function (test_index_array)
    
    * Use @mx.use_np_compat instead of mx.np_compat in index_array op tests
    
    * Remove the use of template specialization for IndexArrayForward
    
    * Add the index_array operator to the AMP symbol list
    
    * Retrigger CI

---
+++ src/operator/contrib/index_array.cu
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+#include <mshadow/tensor.h>
+#include "./index_array-inl.h"
+
+namespace mxnet {
+namespace op {
+
+using namespace mshadow::cuda;
+
+void IndexArrayForwardGPU(const nnvm::NodeAttrs &attrs,
+                          const OpContext &ctx,
+                          const std::vector<TBlob> &inputs,
+                          const std::vector<OpReqType> &req,
+                          const std::vector<TBlob> &outputs) {
+  using namespace mshadow;
+  CHECK_EQ(inputs.size(), 1U);
+  CHECK_EQ(outputs.size(), 1U);
+  CHECK_EQ(req.size(), 1U);
+  const TBlob& in_data = inputs[0];
+  const TBlob& out_data = outputs[0];
+
+  const IndexArrayParam& param = nnvm::get<IndexArrayParam>(attrs.parsed);
+
+  const TShape inshape = in_data.shape_;
+  const int ndim = inshape.ndim();
+
+  Stream<gpu> *stream = ctx.get_stream<gpu>();
+
+  using namespace mxnet_op;
+
+  if (param.axes.has_value()) {
+    const mxnet::Tuple<int>& axes = param.axes.value();
+    const int naxes = axes.ndim();
+
+    std::vector<int64_t> index_products = IndexArrayComputeIndexProducts(inshape);
+
+    std::vector<int64_t> cpu_workspace(2 * naxes);
+    IndexArrayBuildSelectedAxesWorkspace(axes, index_products, cpu_workspace.data(), ndim);
+
+    Tensor<gpu, 1, int64_t> workspace =
+        ctx.requested[0].get_space_typed<gpu, 1, int64_t>(Shape1(2 * naxes), stream);
+
+    CUDA_CALL(cudaMemcpy(workspace.dptr_, cpu_workspace.data(), sizeof(int64_t) * (2 * naxes),
+                         cudaMemcpyHostToDevice));
+
+    MXNET_ASSIGN_REQ_SWITCH(req[0], req_type, {
+      Kernel<IndexArrayKernel<req_type>, gpu>::Launch(stream, in_data.Size(),
+          out_data.dptr<int64_t>(), naxes, workspace.dptr_);
+    });
+  } else {
+    Tensor<gpu, 1, dim_t> workspace =
+        ctx.requested[0].get_space_typed<gpu, 1, dim_t>(Shape1(ndim), stream);
+
+    CUDA_CALL(cudaMemcpy(workspace.dptr_, inshape.data(), sizeof(dim_t) * ndim,
+        cudaMemcpyHostToDevice));
+
+    MXNET_ASSIGN_REQ_SWITCH(req[0], req_type, {
+      Kernel<IndexArrayDefaultKernel<req_type>, gpu>::Launch(stream, in_data.Size(),
+          out_data.dptr<int64_t>(), ndim, workspace.dptr_);
+    });
+  }
+}
+
+NNVM_REGISTER_OP(_contrib_index_array)
+.set_attr<FCompute>("FCompute<gpu>", IndexArrayForwardGPU);
+
+}  // namespace op
+}  // namespace mxnet
