5435c5c2e17d394d99b2cde8a8b4363d58d240d7
reminisce <wujun.nju@gmail.com>
2019-05-26 21:19:43 -0700

    [numpy] Refactor np module (example runs through) (#15055)
    * Refactor notebook
    
    * notebook working with hybrid block
    
    * More refactoring
    
    * Remove unnecessary use_np_compat
    
    * Use class decorator to initialize numpy ndarrays in parameter.py
    
    * Clear notebook outputs
    
    * Improve np decorator
    
    * Remove npe op from optimizer
    
    * Fix CI
    
    * Fix functools.wraps issue in Python2
    
    * Fix ci

--- python/mxnet/gluon/block.py
+++ python/mxnet/gluon/block.py
@@ -33,7 +33,8 @@ from ..symbol import Symbol
 from ..ndarray import NDArray
 from .. import name as _name
 from .parameter import Parameter, ParameterDict, DeferredInitializationError
-from .utils import _indent, _brief_print_list, HookHandle, _check_same_symbol_type
+from .utils import _indent, _brief_print_list, HookHandle
+from .utils import _check_same_symbol_type, _check_all_np_ndarrays
 from .. import numpy as _mx_np
 
 
@@ -550,7 +551,8 @@ class Block(object):
 
         for hook in self._forward_hooks.values():
             hook(self, args, out)
-
+        if _mx_np.is_np_compat():
+            _check_all_np_ndarrays(_flatten(out, "output")[0])
         return out
 
     def forward(self, *args):
