28c528e16be70b31287b65d949205396bbfec6e8
Arthur Caillau <Chouffe@users.noreply.github.com>
2019-06-04 15:37:04 +0200

    [clojure] clojurify function names in image.clj namespace (#15121)
    * [clojure] clojurify function names in image.clj namespace
    
    * move deprecated to the proper location for defn
    
    * rename color-flag to color and use :color :grayscale as values
    
    * add rm dest-path in with-file
    
    * change `color-flag` to `color` in `color->int`

--- contrib/clojure-package/test/org/apache/clojure_mxnet/image_test.clj
+++ contrib/clojure-package/test/org/apache/clojure_mxnet/image_test.clj
@@ -50,50 +50,71 @@
 (use-fixtures :once (with-file image-src-path image-path))
 
 (deftest test-decode-image
-  (let [img-arr (image/decode-image
-                 (io/input-stream image-path))
-        img-arr-2 (image/decode-image
-                   (io/input-stream image-path)
-                   {:color-flag image/GRAYSCALE})]
+  (let [img-arr (image/decode-image (io/input-stream image-path))
+        img-arr-2 (image/decode-image (io/input-stream image-path)
+                                      {:color-flag image/GRAYSCALE})]
+    (is (= [576 1024 3] (ndarray/shape-vec img-arr)))
+    (is (= [576 1024 1] (ndarray/shape-vec img-arr-2)))))
+
+(deftest test-decode
+  (let [img-arr (image/decode (io/input-stream image-path))
+        img-arr-2 (image/decode (io/input-stream image-path)
+                                {:color :grayscale})]
     (is (= [576 1024 3] (ndarray/shape-vec img-arr)))
     (is (= [576 1024 1] (ndarray/shape-vec img-arr-2)))))
 
 (deftest test-read-image
   (let [img-arr (image/read-image image-path)
-        img-arr-2 (image/read-image
-                   image-path
-                   {:color-flag image/GRAYSCALE})]
+        img-arr-2 (image/read-image image-path {:color-flag image/GRAYSCALE})]
+    (is (= [576 1024 3] (ndarray/shape-vec img-arr)))
+    (is (= [576 1024 1] (ndarray/shape-vec img-arr-2)))))
+
+(deftest test-read
+  (let [img-arr (image/read image-path)
+        img-arr-2 (image/read image-path {:color :grayscale})]
     (is (= [576 1024 3] (ndarray/shape-vec img-arr)))
     (is (= [576 1024 1] (ndarray/shape-vec img-arr-2)))))
 
 (deftest test-resize-image
-  (let [img-arr (image/read-image image-path)
+  (let [img-arr (image/read image-path)
         resized-arr (image/resize-image img-arr 224 224)]
     (is (= [224 224 3] (ndarray/shape-vec resized-arr)))))
 
-(deftest test-crop-image
-  (let [img-arr (image/read-image image-path)
+(deftest test-resize
+  (let [img-arr (image/read image-path)
+        resized-arr (image/resize img-arr 224 224)]
+    (is (= [224 224 3] (ndarray/shape-vec resized-arr)))))
+
+(deftest test-fixed-crop
+  (let [img-arr (image/read image-path)
         cropped-arr (image/fixed-crop img-arr 0 0 224 224)]
     (is (= [224 224 3] (ndarray/shape-vec cropped-arr)))))
 
 (deftest test-apply-border
-  (let [img-arr (image/read-image image-path)
+  (let [img-arr (image/read image-path)
         padded-arr (image/apply-border img-arr 1 1 1 1)]
     (is (= [578 1026 3] (ndarray/shape-vec padded-arr)))))
 
 (deftest test-to-image
-  (let [img-arr (image/read-image image-path)
-        resized-arr (image/resize-image img-arr 224 224)
+  (let [img-arr (image/read image-path)
+        resized-arr (image/resize img-arr 224 224)
         new-img (image/to-image resized-arr)]
+    (is (ImageIO/write new-img "png" (io/file tmp-dir "out.png")))))
+
+(deftest test-ndarray->image
+  (let [img-arr (image/read image-path)
+        resized-arr (image/resize img-arr 224 224)
+        new-img (image/ndarray->image resized-arr)]
     (is (ImageIO/write new-img "png" (io/file tmp-dir "out.png")))))
 
 (deftest test-draw-bounding-box!
   (let [orig-img (ImageIO/read (new File image-path))
-        new-img  (-> orig-img
-                     (image/draw-bounding-box! [{:x-min 190 :x-max 850 :y-min 50 :y-max 450}
-                                                {:x-min 200 :x-max 350 :y-min 440 :y-max 530}]
-                                               {:stroke 2
-                                                :names ["pug" "cookie"]
-                                                :transparency 0.8
-                                                :font-size-mult 2.0}))]
+        new-img  (image/draw-bounding-box!
+                   orig-img
+                   [{:x-min 190 :x-max 850 :y-min 50 :y-max 450}
+                    {:x-min 200 :x-max 350 :y-min 440 :y-max 530}]
+                   {:stroke 2
+                    :names ["pug" "cookie"]
+                    :transparency 0.8
+                    :font-size-mult 2.0})]
     (is (ImageIO/write new-img "png" (io/file tmp-dir "out.png")))))
