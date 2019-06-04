f7d051defa33113ee3e9b427c128826666fb74c7
Anirudh Subramanian <anirudh2290@ufl.edu>
2019-06-04 22:05:05 +0000

    Clean up AMP tests

--- tests/python/unittest/test_contrib_amp.py
+++ tests/python/unittest/test_contrib_amp.py
@@ -101,7 +101,8 @@ def test_amp_conversion():
         siny = mx.sym.sin(y)
         res = z + siny
         # Compare symbols with similar computation graphs created using convert_symbol and manually.
-        res_converted = amp.convert_symbol(res, target_dtype="float16", target_dtype_ops=["FullyConnected"],
+        res_converted = amp.convert_symbol(res, target_dtype="float16",
+                                           target_dtype_ops=["FullyConnected"],
                                            fp32_ops=["sin"])
 
         x_fp16 = mx.sym.amp_cast(x, dtype="float16")
@@ -110,35 +111,47 @@ def test_amp_conversion():
         z = mx.sym.FullyConnected(x_fp16, y_fp16, num_hidden=10, no_bias=True)
         outs = mx.sym.amp_multicast(z, amp_casted_siny, num_outputs=2)
         res_expected = outs[0] + outs[1]
-        assert compare_symbol_structure(res_converted, res_expected), "convert_symbol generating wrong computation graph"
+        assert compare_symbol_structure(res_converted, res_expected), \
+            "convert_symbol generating wrong computation graph"
 
         # convert_symbol called with incorrect inputs
-        assert_raises(AssertionError, amp.convert_symbol, res, target_dtype="float16", target_dtype_ops=["FullyConnected"],
+        assert_raises(AssertionError, amp.convert_symbol, res,
+                      target_dtype="float16", target_dtype_ops=["FullyConnected"],
                       fp32_ops=["elemwise_add"])
-        assert_raises(AssertionError, amp.convert_symbol, res, target_dtype="float16", target_dtype_ops=["FullyConnected"],
-                      fp32_ops=["Activation"], conditional_fp32_ops=[('Activation', 'act_type', ['selu'])])
-        assert_raises(AssertionError, amp.convert_symbol, res, target_dtype="float16", target_dtype_ops=["Activation"],
-                      fp32_ops=["Activation"], conditional_fp32_ops=[('Activation', 'act_type', ['selu'])])
-        assert_raises(AssertionError, amp.convert_symbol, res, target_dtype="float16", target_dtype_ops=["FullyConnected"],
+        assert_raises(AssertionError, amp.convert_symbol, res,
+                      target_dtype="float16", target_dtype_ops=["FullyConnected"],
+                      fp32_ops=["Activation"],
+                      conditional_fp32_ops=[('Activation', 'act_type', ['selu'])])
+        assert_raises(AssertionError, amp.convert_symbol, res,
+                      target_dtype="float16", target_dtype_ops=["Activation"],
+                      fp32_ops=["Activation"],
+                      conditional_fp32_ops=[('Activation', 'act_type', ['selu'])])
+        assert_raises(AssertionError, amp.convert_symbol, res,
+                      target_dtype="float16", target_dtype_ops=["FullyConnected"],
                       fp32_ops=["FullyConnected"])
 
         # Test for op in conditional ops with condition not satisfied
         x = mx.sym.var("x")
         y = mx.sym.var("y")
         fc_cond = mx.sym.FullyConnected(x, y, num_hidden=10, no_bias=True)
-        res_converted = amp.convert_symbol(fc_cond, target_dtype="float16", target_dtype_ops=[],
-                                           fp32_ops=["sin"], conditional_fp32_ops=[("FullyConnected", "no_bias", ["False"])])
+        res_converted = amp.convert_symbol(fc_cond, target_dtype="float16",
+                                           target_dtype_ops=[],
+                                           fp32_ops=["sin"],
+                                           conditional_fp32_ops=[("FullyConnected", "no_bias", ["False"])])
 
         res_expected = mx.sym.FullyConnected(x, y, num_hidden=10, no_bias=True)
-        assert compare_symbol_structure(res_converted, res_expected), "convert_symbol generating wrong computation graph when conditional ops is used"
+        assert compare_symbol_structure(res_converted, res_expected), \
+            "convert_symbol generating wrong computation graph when conditional ops is used"
 
         # Test for op in conditional ops with condition satisfied
         res_converted = amp.convert_symbol(fc_cond, target_dtype="float16", target_dtype_ops=[],
-                                           fp32_ops=["sin"], conditional_fp32_ops=[("FullyConnected", "no_bias", ["True"])])
+                                           fp32_ops=["sin"],
+                                           conditional_fp32_ops=[("FullyConnected", "no_bias", ["True"])])
         x_fp32 = mx.sym.amp_cast(x, dtype="float32")
         y_fp32 = mx.sym.amp_cast(y, dtype="float32")
         res_expected = mx.sym.FullyConnected(x_fp32, y_fp32, num_hidden=10, no_bias=True)
-        assert compare_symbol_structure(res_converted, res_expected), "convert_symbol generating wrong computation graph when conditional ops used with satisfying condition"
+        assert compare_symbol_structure(res_converted, res_expected), \
+            "convert_symbol generating wrong computation graph when conditional ops used with satisfying condition"
 
         # Test with a real world model, default inputs for convert_symbol
         dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -160,7 +173,8 @@ def test_amp_conversion():
         inputs['fc1_bias'] = inputs['fc1_bias'].astype(np.float16)
 
         # Test with a real world model, tweak inputs for convert_symbol
-        converted_sym = amp.convert_symbol(sym, target_dtype="float16", target_dtype_ops=["Convolution"], data_names=["data"])
+        converted_sym = amp.convert_symbol(sym, target_dtype="float16",
+                                           target_dtype_ops=["Convolution"], data_names=["data"])
         exe = converted_sym.simple_bind(mx.gpu(0), data=(1, 3, 224, 224), grad_req='null')
         exe.forward(is_train=False, **inputs)
         exe.outputs[0].wait_to_read()
@@ -176,13 +190,17 @@ def test_amp_conversion():
         sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
 
         # Test with real world model, tweak inputs for convert_model
-        result_sym, result_arg_params, result_aux_params = amp.convert_model(sym, arg_params, aux_params, target_dtype="float16", target_dtype_ops=["Convolution"])
-        mod = mx.mod.Module(result_sym, data_names=["data"], label_names=["softmax_label"], context=mx.gpu(0))
+        result_sym, result_arg_params, result_aux_params = amp.convert_model(sym,
+                                                                             arg_params,
+                                                                             aux_params,
+                                                                             target_dtype="float16",
+                                                                             target_dtype_ops=["Convolution"])
+        mod = mx.mod.Module(result_sym, data_names=["data"], label_names=["softmax_label"])
         mod.bind(data_shapes=[['data', (1, 3, 224, 224)]], label_shapes=[['softmax_label', (1,)]])
 
         mod.set_params(result_arg_params, result_aux_params)
-        mod.forward(mx.io.DataBatch(data=[mx.nd.ones((1, 3, 224, 224), ctx=mx.gpu(0))],
-                                    label=[mx.nd.ones((1,), ctx=mx.gpu(0))]))
+        mod.forward(mx.io.DataBatch(data=[mx.nd.ones((1, 3, 224, 224))],
+                                    label=[mx.nd.ones((1,))]))
         mod.get_outputs()[0].asnumpy()
 
     def check_amp_convert_hybrid_block():
@@ -192,14 +210,18 @@ def test_amp_conversion():
         model.hybridize()
         model(mx.nd.zeros((1, 3, 224, 224)))
         converted_model = amp.convert_hybrid_block(model)
-        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224), dtype=np.float32, ctx=mx.gpu(0)))
-        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224), dtype=np.float32, ctx=mx.gpu(0)))
+        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224),
+                                                     dtype=np.float32))
+        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224),
+                                                     dtype=np.float32))
 
         # Test with real world model, tweak inputs for convert_hybrid_block
         converted_model = amp.convert_hybrid_block(model, target_dtype="float16",
                                                    target_dtype_ops=["Convolution"])
-        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224), dtype=np.float32, ctx=mx.gpu(0)))
-        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224), dtype=np.float32, ctx=mx.gpu(0)))
+        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224),
+                                                      dtype=np.float32))
+        result = converted_model.forward(mx.nd.zeros((1, 3, 224, 224),
+                                                     dtype=np.float32))
 
         # Check symbolic block
         dir_path = os.path.dirname(os.path.realpath(__file__))
