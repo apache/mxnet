48f1b945ab665bc2044774d803bb1a4c9bd610a4
Przemek Tredak <ptredak@nvidia.com>
2019-06-05 13:50:09 -0700

    Do fusion only if default context is GPU

--- src/executor/graph_executor.cc
+++ src/executor/graph_executor.cc
@@ -983,7 +983,7 @@ Graph GraphExecutor::InitGraph(nnvm::Symbol symbol,
   // setup gradient
   nnvm::Graph g = InitFullGraph(symbol, grad_req_types);
 
-  if (dmlc::GetEnv("MXNET_USE_FUSION", true)) {
+  if (dmlc::GetEnv("MXNET_USE_FUSION", true) && default_ctx.dev_mask() == Context::kGPU) {
     g.attrs["num_forward_outputs"] = std::make_shared<nnvm::any>(num_forward_outputs_);
     g = FusePointwiseForward(std::move(g));
     g.attrs["num_forward_outputs"] = std::make_shared<nnvm::any>(num_forward_outputs_);
