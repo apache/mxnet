abffb4cb03247d9a92548837b4275d8a7371eaac
Lai Wei <royweilai@gmail.com>
2019-06-06 10:50:57 -0700

    update copy right

--- example/recommenders/movielens_data.py
+++ example/recommenders/movielens_data.py
@@ -46,7 +46,9 @@ def ensure_local_data(prefix):
     if not os.path.exists("%s.zip" % prefix):
         print("Downloading MovieLens data: %s" % prefix)
         # MovieLens 100k dataset from https://grouplens.org/datasets/movielens/
-        # for full text of the usage license, see http://files.grouplens.org/datasets/movielens/ml-100k-README.txt
+        # This dataset is copy right to GroupLens Research Group at the University of Minnesota,
+        # and licensed under their usage license.
+        # For full text of the usage license, see http://files.grouplens.org/datasets/movielens/ml-100k-README.txt
         os.system("wget http://files.grouplens.org/datasets/movielens/%s.zip" % prefix)
         os.system("unzip %s.zip" % prefix)
 
