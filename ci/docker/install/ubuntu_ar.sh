f28bcfa2359a8526bf8c72f4823c6e65c8c51753
Anirudh Subramanian <anirudh2290@ufl.edu>
2019-06-07 19:39:16 +0000

    Fix AR paths

--- ci/docker/install/ubuntu_ar.sh
+++ ci/docker/install/ubuntu_ar.sh
@@ -30,7 +30,7 @@ apt-get install -y \
 mkdir /opt/binutils_install && mkdir /opt/binutils_install && mkdir /opt/binutils && cd /opt/binutils
 wget -nv https://mirror.clarkson.edu/gnu/binutils/binutils-2.27.tar.gz
 tar -xvf binutils-2.27.tar.gz && cd binutils-2.27
-./configure --prefix=/opt/binutils_install --exec-prefix=/opt/binutils_other
+./configure --prefix=/opt/binutils_other --exec-prefix=/opt/binutils_install
 make -j$(nproc)
 make install
 ln -s /opt/binutils_install/bin/ar /usr/local/bin/ar
