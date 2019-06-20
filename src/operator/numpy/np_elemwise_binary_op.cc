e637b0c957b7644c528827da9762cc1f1c8d8305
Ruoyi Wang <endvroy@gmail.com>
2019-06-20 16:23:32 +0800

    now can build

---
+++ src/operator/numpy/np_elemwise_binary_op.cc
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file np_elemwise_binary_op.cc
+ * \brief CPU Implementation of numpy-compatible bitwise OR
+ */
+
+#include <mxnet/base.h>
+#include "../mshadow_op.h"
+#include "../operator_common.h"
+#include "../tensor/elemwise_binary_op.h"
+#include "../tensor/elemwise_binary_broadcast_op.h"
+
+namespace mxnet {
+    namespace op {
+        NNVM_REGISTER_OP(_np_bitwise_or)
+                .set_num_inputs(2)
+                .set_num_outputs(1)
+                .set_attr<mxnet::FInferShape>("FInferShape", ElemwiseShape<2, 1>)
+                .set_attr<nnvm::FInferType>("FInferType", ElemwiseType<2, 1>)
+                .set_attr<nnvm::FListInputNames>("FListInputNames",
+                                                 [](const NodeAttrs &attrs) {
+                                                     return std::vector<std::string>{"x1", "x2"};
+                                                 })
+                .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::bitwise_or>)
+                .set_attr<nnvm::FGradient>("FGradient", MakeZeroGradNodes)
+                .add_argument("x1", "NDArray-or-Symbol", "The input array.")
+                .add_argument("x2", "NDArray-or-Symbol", "The input array.");
+    }  // namespace op
+}  // namespace mxnet
