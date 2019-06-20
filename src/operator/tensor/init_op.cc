68bcdc006615aa4e8898890a588ad85cfc2095dd
Jake Lee <gstu1130@gmail.com>
2019-06-20 10:39:30 -0700

    Numpy compatible linspace (#15256)
    * draft
    
    * finish linspace implementation
    
    * finish linspace
    
    * delete newline
    
    * fix pylint
    
    * add more unit test
    
    * address comment
    
    * add more test case
    
    * disable too-many-arguments
    
    * resolve confliction
    
    * add ctx

--- src/operator/tensor/init_op.cc
+++ src/operator/tensor/init_op.cc
@@ -101,6 +101,7 @@ NNVM_REGISTER_OP(_arange)
 .add_arguments(RangeParam::__FIELDS__());
 
 NNVM_REGISTER_OP(_linspace)
+.add_alias("_npi_linspace")
 .describe("Return evenly spaced numbers over a specified interval. Similar to Numpy")
 .set_num_inputs(0)
 .set_num_outputs(1)
