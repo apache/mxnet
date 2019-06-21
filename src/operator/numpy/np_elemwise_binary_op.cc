81187d8739786a5c252472e7a294454db9adf3d8
Ruoyi Wang <endvroy@gmail.com>
2019-06-21 10:06:13 +0800

    reformatted with google style

--- src/operator/numpy/np_elemwise_binary_op.cc
+++ src/operator/numpy/np_elemwise_binary_op.cc
@@ -29,19 +29,19 @@
 #include "../tensor/elemwise_binary_broadcast_op.h"
 
 namespace mxnet {
-    namespace op {
-        NNVM_REGISTER_OP(_np_bitwise_or)
-                .set_num_inputs(2)
-                .set_num_outputs(1)
-                .set_attr<mxnet::FInferShape>("FInferShape", BinaryBroadcastShape)
-                .set_attr<nnvm::FInferType>("FInferType", ElemwiseType<2, 1>)
-                .set_attr<nnvm::FListInputNames>("FListInputNames",
-                                                 [](const NodeAttrs &attrs) {
-                                                     return std::vector<std::string>{"x1", "x2"};
-                                                 })
-                .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::bitwise_or>)
-                .set_attr<nnvm::FGradient>("FGradient", MakeZeroGradNodes)
-                .add_argument("x1", "NDArray-or-Symbol", "The input array.")
-                .add_argument("x2", "NDArray-or-Symbol", "The input array.");
-    }  // namespace op
+namespace op {
+NNVM_REGISTER_OP(_np_bitwise_or)
+    .set_num_inputs(2)
+    .set_num_outputs(1)
+    .set_attr<mxnet::FInferShape>("FInferShape", BinaryBroadcastShape)
+    .set_attr<nnvm::FInferType>("FInferType", ElemwiseType<2, 1>)
+    .set_attr<nnvm::FListInputNames>("FListInputNames",
+                                     [](const NodeAttrs &attrs) {
+                                       return std::vector<std::string>{"x1", "x2"};
+                                     })
+    .set_attr<FCompute>("FCompute<cpu>", BinaryBroadcastCompute<cpu, mshadow_op::bitwise_or>)
+    .set_attr<nnvm::FGradient>("FGradient", MakeZeroGradNodes)
+    .add_argument("x1", "NDArray-or-Symbol", "The input array.")
+    .add_argument("x2", "NDArray-or-Symbol", "The input array.");
+}  // namespace op
 }  // namespace mxnet
