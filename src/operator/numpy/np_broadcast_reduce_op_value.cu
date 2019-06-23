ec4fc14dda355f2e4df7e0f9526aafaa1db2072c
reminisce <wujun.nju@gmail.com>
2019-06-23 14:16:31 -0700

    [numpy] Misc fix for other chapters (#15332)
    * Add np.prod
    
    * Fix ndarray.reshape accepting positional integers as arguments
    
    * Rebase
    
    * Fix rebase error
    
    * Add np.ndarray.flatten
    
    * Fix
    
    * Add broadcast_to
    
    * Add meshgrid and broadcast_arrays
    
    * Fix sin, cos, sinh, cosh not supporting scalars
    
    * Add more unary ops supporting python scalars
    
    * Fix
    
    * Fix
    
    * Fix ci
    
    * Fix sanity

--- src/operator/numpy/np_broadcast_reduce_op_value.cu
+++ src/operator/numpy/np_broadcast_reduce_op_value.cu
@@ -45,5 +45,17 @@ NNVM_REGISTER_OP(_np_max)
 NNVM_REGISTER_OP(_backward_np_max)
 .set_attr<FCompute>("FCompute<gpu>", NumpyMaxBackward<gpu, mshadow_op::eq>);
 
+NNVM_REGISTER_OP(_np_prod)
+.set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesCompute<gpu, mshadow_op::product, true>);
+
+NNVM_REGISTER_OP(_backward_np_prod)
+.set_attr<FCompute>("FCompute<gpu>", NumpyReduceAxesBackwardUseInOut<gpu, mshadow_op::rdiv>);
+
+NNVM_REGISTER_OP(_np_broadcast_to)
+.set_attr<FCompute>("FCompute<gpu>", NumpyBroadcastToForward<gpu>);
+
+NNVM_REGISTER_OP(_backward_np_broadcast_to)
+.set_attr<FCompute>("FCompute<gpu>", NumpyBroadcastToBackward<gpu>);
+
 }  // namespace op
 }  // namespace mxnet
