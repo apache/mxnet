514e6bdaa0820b66f334cf989ea6bc6d0687dd78
Ruoyi Wang <endvroy@gmail.com>
2019-06-27 10:02:31 +0800

    added doc for ones

--- python/mxnet/_numpy_op_doc.py
+++ python/mxnet/_numpy_op_doc.py
@@ -272,3 +272,38 @@ def _np_squeeze(a, axis=None):
     """
     pass
 
+
+def _npi_ones(shape, dtype=None, order='C'):
+    """
+    Return a new array of given shape and type, filled with ones.
+    Parameters
+    ----------
+    shape : int or sequence of ints
+        Shape of the new array, e.g., ``(2, 3)`` or ``2``.
+    dtype : data-type, optional
+        The desired data-type for the array, e.g., `int`.  Default is
+        `float32`.
+    order : {'C', 'F'}, optional, default: C
+        Whether to store multi-dimensional data in row-major
+        (C-style) or column-major (Fortran-style) order in
+        memory.
+
+        Not Supported yet.
+    Returns
+    -------
+    out : ndarray
+        Array of ones with the given shape, dtype, and order.
+    Examples
+    --------
+    >>> np.ones(5)
+    array([ 1.,  1.,  1.,  1.,  1.])
+    >>> np.ones((5,), dtype=int)
+    array([1, 1, 1, 1, 1])
+    >>> np.ones((2, 1))
+    array([[ 1.],
+           [ 1.]])
+    >>> s = (2,2)
+    >>> np.ones(s)
+    array([[ 1.,  1.],
+           [ 1.,  1.]])
+    """
