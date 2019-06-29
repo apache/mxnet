093e854ecf20d20650431de4fed9403a7d82b240
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-06-29 13:35:04 +0530

    add relevant tests

--- tests/python/unittest/test_higher_order_grad.py
+++ tests/python/unittest/test_higher_order_grad.py
@@ -106,6 +106,34 @@ def test_log10():
         check_second_order_unary(array, log10, grad_grad_op)
 
 
+@with_seed()
+def test_sinh():
+    def sinh(x):
+        return nd.sinh(x)
+
+    def grad_grad_op(x):
+        return sinh(x)
+
+    for dim in range(1, 5):
+        shape = rand_shape_nd(dim)
+        array = random_arrays(shape)
+        check_second_order_unary(array, sinh, grad_grad_op)
+
+
+@with_seed()
+def test_cosh():
+    def cosh(x):
+        return nd.cosh(x)
+
+    def grad_grad_op(x):
+        return cosh(x)
+
+    for dim in range(1, 5):
+        shape = rand_shape_nd(dim)
+        array = random_arrays(shape)
+        check_second_order_unary(array, cosh, grad_grad_op)
+
+
 def check_second_order_unary(x, op, grad_grad_op):
     x = nd.array(x)
     grad_grad_x = grad_grad_op(x)
