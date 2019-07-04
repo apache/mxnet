fff9a9c646421b0623b62cba933bda4d306666af
Ying <tinying@38f9d3dae8d9.ant.amazon.com>
2019-07-04 13:47:30 +0800

    add np_around_op.cu

---
+++ src/operator/numpy/np_around_op.cu
@@ -0,0 +1,10 @@
+#include <"./np_around_op.h">
+
+namespace mxnet {
+namespace op {
+
+NNVM_REGISTER_OP(_npi_around)
+.set_attr<FCompute>("FCompute<gpu>", AroundOpForward<gpu>);
+
+}  // namespace op
+}  // namespace mxnet
