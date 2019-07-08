66becd921ef0bd8e59f993e14b1471972c244316
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-07-09 00:57:18 +0530

    update test for same variable

--- tests/python/unittest/test_higher_order_grad.py
+++ tests/python/unittest/test_higher_order_grad.py
@@ -134,6 +134,7 @@ def test_abs():
         check_second_order_unary(array, abs, grad_grad_op)
 
 
+@with_seed()
 def test_sigmoid():
     def sigmoid(x):
         return nd.sigmoid(x)
@@ -167,6 +168,21 @@ def test_elemwise_mul():
 
 
 @with_seed()
+def test_elemwise_mul_same_var():
+    def fourth_power(x):
+        y = nd.elemwise_mul(x, x)
+        return nd.elemwise_mul(y, y)
+
+    def grad_grad_op(x):
+        return 12 * (x**2)
+
+    for dim in range(1, 5):
+        shape = rand_shape_nd(dim)
+        array_x = random_arrays(shape)
+        check_second_order_unary(array_x, fourth_power, grad_grad_op)
+
+
+@with_seed()
 def test_elemwise_div():
     def elemwise_div(x, y):
         return nd.elemwise_div(x, y)
@@ -183,6 +199,25 @@ def test_elemwise_div():
         inputs = (array_x, array_y)
         grad_grad_ops = (grad_grad_op_x, grad_grad_op_y)
         check_second_order_binary(inputs, elemwise_div, grad_grad_ops)
+
+
+@with_seed()
+def test_elemwise_div_same_var():
+    def f(x):
+        # f = x / (x^2 + 1)
+        return nd.elemwise_div(x, nd.elemwise_mul(x, x) + 1)
+
+    def grad_grad_op(x):
+        # d^2f    -2*x*(-x^2 + 3)
+        # ---- = ----------------
+        # dx^2     (x^2 + 1)^3
+        return (-2*x*(-x**2 + 3))/((x**2+1)**3)
+
+    for dim in range(1, 5):
+        shape = rand_shape_nd(dim)
+        x = random_arrays(shape)
+        x += 10
+        check_second_order_unary(x, f, grad_grad_op)
 
 
 def check_second_order_unary(x, op, grad_grad_op):
@@ -228,12 +263,6 @@ def check_second_order_binary(inputs, binary_op, grad_grad_ops):
                                        head_grads=z_grad,
                                        create_graph=True, retain_graph=True)
 
-    x_grad.backward(head_grad_grads, retain_graph=True)
-
-    # The line below follower by above
-    # line leads to (y.grad == zeros_like(y))
-    # y_grad.backward(head_grad_grads)
-
     # Compute expected values.
     expected_grad_grad_x = grad_grad_x.asnumpy() * head_grad_grads.asnumpy() *\
         z_grad.asnumpy()
@@ -241,7 +270,17 @@ def check_second_order_binary(inputs, binary_op, grad_grad_ops):
         z_grad.asnumpy()
 
     # Validate the gradients.
+    x_grad.backward(head_grad_grads, retain_graph=True)
     assert_almost_equal(expected_grad_grad_x, x.grad.asnumpy())
+
+    # TODO Figure what is happening.
+    # Somehow gradients of y are affected with this backward.
+    # Only in case of `test_elemwise_mul`
+    # As y.grad == head_grad_grads * z_grad
+    # Temporary Hack: Reset them.
+    nd.zeros_like(y.grad, out=y.grad)
+
+    y_grad.backward(head_grad_grads)
     assert_almost_equal(expected_grad_grad_y, y.grad.asnumpy())
 
 
