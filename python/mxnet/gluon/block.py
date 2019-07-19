24e69dedc6e90df7b4f7438cb1c251de3849b4ac
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-07-20 00:52:59 +0530

    use format

--- python/mxnet/gluon/block.py
+++ python/mxnet/gluon/block.py
@@ -843,7 +843,8 @@ class HybridBlock(Block):
         self._clear_cached_op()
         if active and self._forward_hooks or self._forward_pre_hooks:
             warnings.warn('"{}" is being hybridized while still having forward hook/pre-hook. '
-                          'If "{}" is a child of HybridBlock, the hooks will not take effect.')
+                          'If "{}" is a child of HybridBlock, the hooks will not take effect.'
+                          .format(self, self))
         super(HybridBlock, self).hybridize(active, **kwargs)
 
     def cast(self, dtype):
