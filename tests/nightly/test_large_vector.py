48d981d1c4c2c782b1975ad78b1e201a3a128abf
Rohit Kumar Srivastava <srivastava.141@buckeyemail.osu.edu>
2019-07-19 21:29:11 +0000

    Adding Large Index Support for slice operator

---
+++ tests/nightly/test_large_vector.py
@@ -0,0 +1,37 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import numpy as np
+import mxnet as mx
+from mxnet.test_utils import rand_ndarray, assert_almost_equal, rand_coord_2d
+from mxnet import gluon, nd
+from tests.python.unittest.common import with_seed
+
+# dimension constants
+LARGE_X = 5000000000
+SMALL_Y = 1
+
+
+def test_slice():
+    a = nd.ones(LARGE_X)
+    res = nd.slice(a, begin=(LARGE_X-1000000000), end=(LARGE_X))
+    assert res.shape[0] == 1000000000
+
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule()
