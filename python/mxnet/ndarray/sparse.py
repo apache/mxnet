d599bc3c1356b3471d27026711f67467122992f9
Neo Chien <cchung100m@cs.ccu.edu.tw>
2019-07-21 16:07:39 +0800

    [MXNET-1411] solve pylint error issue#14851 (#15113)
    * fix pylint error: no-else-raise in _export_helper.py
    
    * fix pylint error: no-else-raise in _translation_utils.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in vocab.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in trainer.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in utils.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in detection.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in image.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in model.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in sparse.py
    
    * fix pylint error: Bad option value 'no-else-raise' (bad-option-value) in test_utils.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for vocab.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for model.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for _translation_utils.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for _export_helper.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for test_utils.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for image.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for trainer.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for detection.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for utils.py
    
    * fix pylint error: R1720: Unnecessary else after raise (no-else-raise) for sparse.py
    
    * fix pylint error:R1719: The if expression can be replaced with 'bool(test)' (simplifiable-if-expression)

--- python/mxnet/ndarray/sparse.py
+++ python/mxnet/ndarray/sparse.py
@@ -639,10 +639,10 @@ class RowSparseNDArray(BaseSparseNDArray):
         if isinstance(key, int):
             raise Exception("__getitem__ with int key is not implemented for RowSparseNDArray yet")
         if isinstance(key, py_slice):
-            if key.step is not None or key.start is not None or key.stop is not None: # pylint: disable=no-else-raise
+            if key.step is not None or key.start is not None or key.stop is not None:
                 raise Exception('RowSparseNDArray only supports [:] for __getitem__')
-            else:
-                return self
+
+            return self
         if isinstance(key, tuple):
             raise ValueError('Multi-dimension indexing is not supported')
         raise ValueError('Undefined behaviour for {}'.format(key))
@@ -1102,9 +1102,9 @@ def row_sparse_array(arg1, shape=None, ctx=None, dtype=None):
     # construct a row sparse array from (D0, D1 ..) or (data, indices)
     if isinstance(arg1, tuple):
         arg_len = len(arg1)
-        if arg_len < 2: # pylint: disable=no-else-raise
+        if arg_len < 2:
             raise ValueError("Unexpected length of input tuple: " + str(arg_len))
-        elif arg_len > 2:
+        if arg_len > 2:
             # empty ndarray with shape
             _check_shape(arg1, shape)
             return empty('row_sparse', arg1, ctx=ctx, dtype=dtype)
