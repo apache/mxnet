1682af3e5929f05a25aa5311f376065a873758fd
mseth10 <sethman@amazon.com>
2019-07-25 21:07:35 +0000

    getting error message from DL library

--- src/common/library.cc
+++ src/common/library.cc
@@ -42,14 +42,30 @@ void* load_lib(const char* path) {
   void *handle;
 #if defined(_WIN32) || defined(_WIN64) || defined(__WINDOWS__)
   handle = LoadLibrary(path);
-#else
-  handle = dlopen(path, RTLD_LAZY);
-#endif  // _WIN32 or _WIN64 or __WINDOWS__
-
   if (!handle) {
-    LOG(FATAL) << "Error loading library: '" << path << "'";
+    char *lpMsgBuf;
+    unsigned long dw = GetLastError();
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        &lpMsgBuf,
+        0, NULL );
+    LOG(FATAL) << "Error loading library: '" << path << "'\n" << lpMsgBuf;
+    LocalFree(lpMsgBuf);
     return nullptr;
   }
+#else
+  handle = dlopen(path, RTLD_LAZY);
+  if (!handle) {
+    LOG(FATAL) << "Error loading library: '" << path << "'\n" << dlerror();
+    return nullptr;
+  }
+#endif  // _WIN32 or _WIN64 or __WINDOWS__
+
   return handle;
 }
 
