76dfdaffa66b03e3bd477d7bf17ff5d7605b54b3
Rohit Kumar Srivastava <srivastava.141@buckeyemail.osu.edu>
2019-08-01 23:08:00 +0000

    fixing base.py

--- python/mxnet/base.py
+++ python/mxnet/base.py
@@ -215,10 +215,7 @@ _LIB = _load_lib()
 # type definitions
 mx_int = ctypes.c_int
 mx_uint = ctypes.c_uint
-if sys.version_info[0] > 2:
-    mx_int64 = ctypes.c_int64
-else:
-    mx_int64 = ctypes.c_int32
+mx_int64 = ctypes.c_int64
 mx_float = ctypes.c_float
 mx_float_p = ctypes.POINTER(mx_float)
 mx_real_t = _np.float32
