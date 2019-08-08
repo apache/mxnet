067aabbed3d545a11d2aab546c67c5672f25da36
sguangyo <1360024032@qq.com>
2019-08-08 09:38:21 +0800

    fix some comments

--- src/operator/numpy/np_window_op.h
+++ src/operator/numpy/np_window_op.h
@@ -45,18 +45,18 @@ struct NumpyWindowsParam : public dmlc::Parameter<NumpyWindowsParam> {
   std::string ctx;
   int dtype;
   DMLC_DECLARE_PARAMETER(NumpyWindowsParam) {
-      DMLC_DECLARE_FIELD(M)
-          .set_default(dmlc::optional<nnvm::dim_t>())
-          .describe("Number of points in the output window. "
-                    "If zero or less, an empty array is returned.");
-      DMLC_DECLARE_FIELD(ctx)
-      .set_default("")
-      .describe("Context of output, in format [cpu|gpu|cpu_pinned](n)."
-      "Only used for imperative calls.");
-      DMLC_DECLARE_FIELD(dtype)
-      .set_default(mshadow::kFloat64)
-      MXNET_ADD_ALL_TYPES
-      .describe("Data-type of the returned array.");
+    DMLC_DECLARE_FIELD(M)
+    .set_default(dmlc::optional<nnvm::dim_t>())
+    .describe("Number of points in the output window. "
+              "If zero or less, an empty array is returned.");
+    DMLC_DECLARE_FIELD(ctx)
+    .set_default("")
+    .describe("Context of output, in format [cpu|gpu|cpu_pinned](n)."
+    "Only used for imperative calls.");
+    DMLC_DECLARE_FIELD(dtype)
+    .set_default(mshadow::kFloat64)
+    MXNET_ADD_ALL_TYPES
+    .describe("Data-type of the returned array.");
   }
 };
 
@@ -116,24 +116,20 @@ void NumpyWindowCompute(const nnvm::NodeAttrs& attrs,
   using namespace mxnet_op;
   mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
   const NumpyWindowsParam& param = nnvm::get<NumpyWindowsParam>(attrs.parsed);
-  if (param.M.has_value()) {
-    if (param.M.value() <= 0) {
-      return;
-    }
-  }
+  if (param.M.has_value() && param.M.value() <= 0) return;
   MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
-      if (window_select == 0) {
-        Kernel<hanning_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
-                                         req[0], outputs[0].dptr<DType>());
-      } else if (window_select == 1) {
-        Kernel<hamming_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
-                                         req[0], outputs[0].dptr<DType>());
-      } else if (window_select == 2) {
-        Kernel<blackman_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
-                                          req[0], outputs[0].dptr<DType>());
-      } else {
-        LOG(FATAL) << "window_select must be (0, 1, 2)";
-      }
+    if (window_select == 0) {
+      Kernel<hanning_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
+                                       req[0], outputs[0].dptr<DType>());
+    } else if (window_select == 1) {
+      Kernel<hamming_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
+                                       req[0], outputs[0].dptr<DType>());
+    } else if (window_select == 2) {
+      Kernel<blackman_fwd, xpu>::Launch(s, outputs[0].Size(), static_cast<int>(param.M.value()),
+                                        req[0], outputs[0].dptr<DType>());
+    } else {
+      LOG(FATAL) << "window_select must be (0, 1, 2)";
+    }
   });
 }
 
