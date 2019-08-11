53b2438200a137d634a99966217c1cb828c14020
reminisce <wujun.nju@gmail.com>
2019-08-11 14:45:59 -0700

    Enable test_sparse_nd_elemwise_add

--- tests/python/unittest/test_sparse_ndarray.py
+++ tests/python/unittest/test_sparse_ndarray.py
@@ -33,8 +33,8 @@ def sparse_nd_ones(shape, stype):
     return mx.nd.ones(shape).tostype(stype)
 
 
+# @unittest.skip("skip test_sparse_nd_elemwise_add")
 @with_seed()
-@unittest.skip("skip test_sparse_nd_elemwise_add")
 def test_sparse_nd_elemwise_add():
     def check_sparse_nd_elemwise_binary(shapes, stypes, f, g):
         # generate inputs
