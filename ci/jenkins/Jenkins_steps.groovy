16d2cc9cc9e52d3ba53a2f92540124a70f4c51e3
Aaron Markham <markhama@amazon.com>
2019-08-12 23:34:06 -0700

    unstash website archive

--- ci/jenkins/Jenkins_steps.groovy
+++ ci/jenkins/Jenkins_steps.groovy
@@ -1552,7 +1552,7 @@ def docs_publish() {
         ws('workspace/docs') {
           timeout(time: max_time, unit: 'MINUTES') {
             utils.init_git()
-            //unstash 'full_website'
+            unstash 'full_website'
             //sh 'tar -xzf docs/_build/full_website.tgz --directory .'
             // TODO: Make sure this 'test-website-publish' understand the new structure
             build 'test-website-publish-master'
