9668f1e048276cca265f9a002ea0c5d3c1340ffd
benhe2011 <benhe.che@gmail.com>
2019-08-14 16:01:40 -0700

    modified scala demo to help with debugging scala test case

--- scala-package/mxnet-demo/scala-demo/src/main/scala/sample/HelloWorld.scala
+++ scala-package/mxnet-demo/scala-demo/src/main/scala/sample/HelloWorld.scala
@@ -22,5 +22,59 @@ object HelloWorld {
     println("hello World")
     val arr = NDArray.ones(2, 3)
     println(arr.shape)
+    for (rootScale <- 1 to 3) {
+      for (scale <- 1 to 3) {
+        for (numShape <- 1 to 3) {
+          for (base <- 1 to 3) {
+            val shapes = (0 until numShape).map(i =>
+              Shape(1, 3, base * rootScale * Math.pow(scale, numShape - 1 - i).toInt,
+                base * rootScale * Math.pow(scale, numShape - 1 - i).toInt))
+            checkNearestUpSamplingWithShape(shapes, scale, rootScale)
+          }
+        }
+      }
+    }
+  }
+
+  def checkNearestUpSamplingWithShape(shapes: Seq[Shape],
+                                              scale: Int,
+                                              rootScale: Int): Unit = {
+    val arr = shapes.zipWithIndex.map { case (shape, i) =>
+      (s"arg_$i", Random.uniform(-10, 10, shape))
+    }.toMap
+
+    val arrGrad = shapes.zipWithIndex.map { case (shape, i) =>
+      (s"arg_$i", NDArray.zeros(shape))
+    }.toMap
+
+    val upArgs = (0 until shapes.size).map(i => Symbol.Variable(s"arg_$i"))
+    val up = Symbol.UpSampling()(upArgs: _*)(Map("sample_type" -> "nearest", "scale" -> rootScale))
+    val exe = up.bind(Context.cpu(), args = arr, argsGrad = arrGrad)
+    exe.forward(isTrain = true)
+    exe.backward(exe.outputs)
+    for (k <- 0 until shapes.size) {
+      val name = s"arg_$k"
+      val expected =
+        arr(name).toArray.map(_ * Math.pow(rootScale, 2).toFloat * Math.pow(scale, 2 * k).toFloat)
+      val real = arrGrad(name).toArray
+      (expected zip real) foreach { case (e, r) =>
+        assert(r === e +- 0.1f)
+      }
+    }
+  }
+
+  test("nearest upsampling") {
+    for (rootScale <- 1 to 3) {
+      for (scale <- 1 to 3) {
+        for (numShape <- 1 to 3) {
+          for (base <- 1 to 3) {
+            val shapes = (0 until numShape).map(i =>
+              Shape(1, 3, base * rootScale * Math.pow(scale, numShape - 1 - i).toInt,
+                base * rootScale * Math.pow(scale, numShape - 1 - i).toInt))
+            checkNearestUpSamplingWithShape(shapes, scale, rootScale)
+          }
+        }
+      }
+    }
   }
 }
