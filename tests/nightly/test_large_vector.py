eb0d52433e60277e0cc3739959882064f1cdd0cb
ChaiBapchya <chai.bapat@gmail.com>
2019-08-16 16:24:31 -0700

    add random ops

--- tests/nightly/test_large_vector.py
+++ tests/nightly/test_large_vector.py
@@ -27,10 +27,96 @@ LARGE_X = 5000000000
 MEDIUM_X = 1000000000
 
 
+def create_large_vector(size, dtype="int64"):
+    a = nd.arange(0, size, dtype=dtype)
+    # Implicitly calling nd.waitall()
+    assert a[0] == 0
+    return a
+
+
 def test_slice():
     a = nd.ones(LARGE_X)
     res = nd.slice(a, begin=(LARGE_X - MEDIUM_X), end=LARGE_X)
     assert res.shape[0] == MEDIUM_X
+
+
+@with_seed()
+def test_ndarray_random_exponential():
+    a = nd.random.exponential(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_gamma():
+    a = nd.random.gamma(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_generalized_negative_binomial():
+    a = nd.random.generalized_negative_binomial(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_multinomial():
+    a = nd.random.generalized_negative_binomial(probs=create_large_vector(LARGE_X))
+    assert a[-1] >= 0.
+    assert a.shape[0] == 1
+
+
+@with_seed()
+def test_ndarray_random_negative_binomial():
+    a = nd.random.negative_binomial(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_normal():
+    a = nd.random.normal(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_poisson():
+    a = nd.random.poisson(shape=LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_randint():
+    a = nd.random.randint(1500, 9000, shape=LARGE_X, dtype="int64")
+    assert a[-1] >= 1500 and a[-1] < 9000
+    assert a[-1] == np.int64
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_randn():
+    a = nd.random.randn(LARGE_X)
+    assert a[-1] >= 0.
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_uniform():
+    a = nd.random.uniform(1500, 9000, shape=LARGE_X)
+    assert a[-1] >= 1500 and a[-1] < 9000
+    assert a.shape[0] == LARGE_X
+
+
+@with_seed()
+def test_ndarray_random_shuffle():
+    a = nd.ones(shape=LARGE_X)
+    a = nd.random.shuffle(a)
+    assert a[-1] in np.unique(a.asnumpy())
+    assert a.shape[0] == LARGE_X
 
 
 if __name__ == '__main__':
