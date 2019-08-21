392448f2dd6d2e463ec356ea54af634f6423a56f
Xi Wang <xidulu@gmail.com>
2019-08-21 03:20:57 +0000

    randint completed

--- python/mxnet/symbol/numpy/random.py
+++ python/mxnet/symbol/numpy/random.py
@@ -18,5 +18,66 @@
 """Namespace for operators used in Gluon dispatched by F=symbol."""
 
 from __future__ import absolute_import
+from ...context import current_context
+from . import _internal as _npi
 
-__all__ = []
+__all__ = ['randint']
+
+
+def randint(low, high=None, size=None, dtype=None, **kwargs):
+    """Return random integers from `low` (inclusive) to `high` (exclusive).
+
+    Return random integers from the "discrete uniform" distribution of
+    the specified dtype in the "half-open" interval [`low`, `high`). If
+    `high` is None (the default), then results are from [0, `low`).
+
+    Parameters
+    ----------
+    low : int
+        Lowest (signed) integer to be drawn from the distribution (unless
+        ``high=None``, in which case this parameter is one above the
+        *highest* such integer).
+    high : int, optional
+        If provided, one above the largest (signed) integer to be drawn
+        from the distribution (see above for behavior if ``high=None``).
+    size : int or tuple of ints, optional
+        Output shape.  If the given shape is, e.g., ``(m, n, k)``, then
+        ``m * n * k`` samples are drawn.  Default is None, in which case a
+        single value is returned.
+    dtype : dtype, optional
+        Desired dtype of the result. All dtypes are determined by their
+        name, i.e., 'int64', 'int', etc, so byteorder is not available
+        and a specific precision may have different C types depending
+        on the platform. The default value is 'np.int'.
+
+    Returns
+    -------
+    out : symbol
+        `size`-shaped array of random integers from the appropriate
+        distribution, or a single such random int if `size` not provided.
+
+    Examples
+    --------
+    >>> np.random.randint(2, size=10)
+    array([1, 0, 0, 0, 1, 1, 0, 0, 1, 0])
+    >>> np.random.randint(1, size=10)
+    array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    Generate a 2 x 4 array of ints between 0 and 4, inclusive:
+
+    >>> np.random.randint(5, size=(2, 4))
+    array([[4, 0, 2, 1],
+        [3, 2, 2, 0]])
+    """
+    ctx = kwargs.pop('ctx', None)
+    out = kwargs.pop('out', None)
+    if dtype is None:
+        dtype = 'int'
+    if ctx is None:
+        ctx = current_context()
+    if size is None:
+        size = 1
+    if high is None:
+        high = low
+        low = 0
+    return _npi.random_randint(low, high, shape=size, dtype=dtype, ctx=ctx, out=out)
