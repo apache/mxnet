31baff63417d53e40c31d1296009c20f14c562a3
Xi Wang <xidulu@gmail.com>
2019-08-22 15:05:08 +0800

    Update runtime_functions.sh

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -542,7 +542,6 @@ build_ubuntu_cpu_clang60_mkldnn() {
     build_ccache_wrappers
 
     make \
-
         USE_CPP_PACKAGE=1             \
         USE_BLAS=openblas             \
         USE_OPENMP=1                  \
