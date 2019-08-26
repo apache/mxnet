a2782e798a5ca1cb9c6c867e5f31fa2e158048d9
zoeygxy <xinyige@umich.edu>
2019-08-26 19:45:33 +0800

    fix symbol calc

--- python/mxnet/symbol/numpy/_symbol.py
+++ python/mxnet/symbol/numpy/_symbol.py
@@ -34,7 +34,8 @@ __all__ = ['zeros', 'ones', 'add', 'subtract', 'multiply', 'divide', 'mod', 'pow
            'expm1', 'arcsin', 'arccos', 'arctan', 'sign', 'log', 'degrees', 'log2', 'log1p',
            'rint', 'radians', 'reciprocal', 'square', 'negative', 'fix', 'ceil', 'floor',
            'trunc', 'logical_not', 'arcsinh', 'arccosh', 'arctanh', 'tensordot',
-           'linspace', 'expand_dims', 'tile', 'arange', 'split', 'concatenate', 'stack']
+           'linspace', 'expand_dims', 'tile', 'arange', 'split', 'concatenate', 'stack',
+           'bitwise_and']
 
 
 def _num_outputs(sym):
