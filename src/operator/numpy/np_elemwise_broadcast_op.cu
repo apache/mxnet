00769312246ebb18bb92a657a3a7fdf88d0eafb7
Zoey Xinyi Ge <xinyige@umich.edu>
2019-08-27 00:54:25 +0800

    Add gpu implementation

--- src/operator/numpy/np_elemwise_broadcast_op.cu
+++ src/operator/numpy/np_elemwise_broadcast_op.cu
@@ -48,7 +48,7 @@ NNVM_REGISTER_OP(_npi_maximum)
 NNVM_REGISTER_OP(_npi_minimum)
 .set_attr<FCompute>("FCompute<gpu>", BinaryBroadcastCompute<gpu, mshadow_op::minimum>);
 
-NNVM_REGISTER_OP(_np_bitwise_and)
+NNVM_REGISTER_OP(_npi_bitwise_and)
 .set_attr<FCompute>("FCompute<gpu>", BinaryBroadcastCompute<gpu, mshadow_op::bitwise_and>);
 
 NNVM_REGISTER_OP(_npi_add_scalar)
@@ -80,6 +80,9 @@ NNVM_REGISTER_OP(_npi_maximum_scalar)
 
 NNVM_REGISTER_OP(_npi_minimum_scalar)
 .set_attr<FCompute>("FCompute<gpu>", BinaryScalarOp::Compute<gpu, mshadow_op::minimum>);
+
+NNVM_REGISTER_OP(_npi_bitwise_and_scalar)
+.set_attr<FCompute>("FCompute<gpu>", BinaryScalarOp::Compute<gpu, mshadow_op::bitwise_and>);
 
 }  // namespace op
 }  // namespace mxnet
