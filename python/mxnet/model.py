0b18d6b87706c846dcbfd276d1f3f6bdd0b35280
JiadingGai <jiading.gai@gmail.com>
2019-09-01 01:32:42 -0700

    Allow loading from model with empty weights.
    For example, when the model graph contains only NDArray operators.

--- python/mxnet/model.py
+++ python/mxnet/model.py
@@ -438,6 +438,8 @@ def load_checkpoint(prefix, epoch):
     save_dict = nd.load('%s-%04d.params' % (prefix, epoch))
     arg_params = {}
     aux_params = {}
+    if not save_dict:
+        return (symbol, arg_params, aux_params)
     for k, v in save_dict.items():
         tp, name = k.split(':', 1)
         if tp == 'arg':
