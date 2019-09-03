ab094f74131a4d2e0caf5433cc8d82124c718405
Haibin Lin <linhaibin.eric@gmail.com>
2019-09-03 14:11:23 -0700

    Update dataset.py

--- python/mxnet/gluon/data/dataset.py
+++ python/mxnet/gluon/data/dataset.py
@@ -180,7 +180,7 @@ class _FilteredDataset(Dataset):
     """Dataset with a filter applied"""
     def __init__(self, dataset, fn):
         self._dataset = dataset
-        self._indices = [i for for i, sample in enumerate(dataset) if fn(sample)]
+        self._indices = [i for i, sample in enumerate(dataset) if fn(sample)]
 
     def __len__(self):
         return len(self._indices)
