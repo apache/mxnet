69217f50ee987627af8191348eb03631ce2139b9
Rohit Kumar Srivastava <srivastava.141@buckeyemail.osu.edu>
2019-09-06 22:11:39 +0000

    Revert "Temporarily Commenting out Flaky Test (#15436)"
    This reverts commit 512a491547c17f4caffc4cc962d95a6467186a3d.

--- tests/python/unittest/test_profiler.py
+++ tests/python/unittest/test_profiler.py
@@ -24,7 +24,6 @@ import os
 import json
 from collections import OrderedDict
 from common import run_in_spawned_process
-import unittest
 
 def enable_profiler(profile_filename, run=True, continuous_dump=False, aggregate_stats=False):
     profiler.set_config(profile_symbolic=True,
@@ -428,7 +427,6 @@ def test_custom_operator_profiling_multiple_custom_ops_imperative(seed = None, \
         and '_plus_scalar' in target_dict['Time']['operator']
     profiler.set_state('stop')
 
-@unittest.skip("Flaky test https://github.com/apache/incubator-mxnet/issues/15406")
 def test_custom_operator_profiling_multiple_custom_ops_symbolic():
     run_in_spawned_process(test_custom_operator_profiling_multiple_custom_ops_imperative, \
             {'MXNET_EXEC_BULK_EXEC_INFERENCE' : 0, \
@@ -436,7 +434,6 @@ def test_custom_operator_profiling_multiple_custom_ops_symbolic():
             'symbolic', \
             'test_custom_operator_profiling_multiple_custom_ops_symbolic.json')
 
-@unittest.skip("Flaky test https://github.com/apache/incubator-mxnet/issues/15406")
 def test_custom_operator_profiling_naive_engine():
     # run the three tests above using Naive Engine
     run_in_spawned_process(test_custom_operator_profiling, \
