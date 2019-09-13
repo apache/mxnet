6656398c885f73a97c73ea2d56c12b1b4aa0fee2
Delteil, Thomas <tdelteil@amazon.com>
2019-09-13 16:43:45 -0700

    linting of cpp in R

--- R-package/src/export.cc
+++ R-package/src/export.cc
@@ -30,7 +30,6 @@
 
 namespace mxnet {
 namespace R {
-    
 // docstring related function.
 std::string MakeDocString(mx_uint num_args,
                           const char **arg_names,
@@ -79,9 +78,9 @@ std::string ExportDocString(const std::string& docstring) {
 
 std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
     size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+        start_pos += to.length();  // Handles case where 'to' is a substring of 'from'
     }
     return str;
 }
