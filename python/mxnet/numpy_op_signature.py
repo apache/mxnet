5a9e64a784d9ea307a22f866525983bac20bf1a0
reminisce <wujun.nju@gmail.com>
2019-09-27 14:50:58 -0700

    Add test

--- python/mxnet/numpy_op_signature.py
+++ python/mxnet/numpy_op_signature.py
@@ -27,6 +27,34 @@ from . import numpy_extension as mx_npx
 from .base import _NP_OP_SUBMODULE_LIST, _NP_EXT_OP_SUBMODULE_LIST, _get_op_submodule_name
 
 
+def _get_builtin_op(op_name):
+    if op_name.startswith('_np_'):
+        root_module = mx_np
+        op_name_prefix = '_np_'
+        submodule_name_list = _NP_OP_SUBMODULE_LIST
+    elif op_name.startswith('_npx_'):
+        root_module = mx_npx
+        op_name_prefix = '_npx_'
+        submodule_name_list = _NP_EXT_OP_SUBMODULE_LIST
+    else:
+        return None
+
+    submodule_name = _get_op_submodule_name(op_name, op_name_prefix, submodule_name_list)
+    if len(submodule_name) > 0:
+        op_module = getattr(root_module, submodule_name[1:-1], None)
+        if op_module is None:
+            raise ValueError('Cannot find submodule {} in module {}'
+                             .format(submodule_name[1:-1], root_module.__name__))
+    else:
+        op_module = root_module
+
+    op = getattr(op_module, op_name[(len(op_name_prefix)+len(submodule_name)):], None)
+    if op is None:
+        raise ValueError('Cannot find operator {} in module'
+                         .format(op_name[op_name_prefix:], root_module.__name__))
+    return op
+
+
 def _register_op_signatures():
     if sys.version_info.major < 3 or sys.version_info.minor < 5:
         warnings.warn('Some mxnet.numpy operator signatures may not be displayed consistently with '
@@ -36,31 +64,9 @@ def _register_op_signatures():
         return
 
     for op_name in dir(_numpy_op_doc):
-        if op_name.startswith('_np_'):
-            root_module = mx_np
-            op_name_prefix = '_np_'
-            submodule_name_list = _NP_OP_SUBMODULE_LIST
-        elif op_name.startswith('_npx_'):
-            root_module = mx_npx
-            op_name_prefix = '_npx_'
-            submodule_name_list = _NP_EXT_OP_SUBMODULE_LIST
-        else:
-            continue
-
-        submodule_name = _get_op_submodule_name(op_name, op_name_prefix, submodule_name_list)
-        if len(submodule_name) > 0:
-            op_module = getattr(root_module, submodule_name[1:-1], None)
-            if op_module is None:
-                raise ValueError('Cannot find submodule {} in module {}'
-                                 .format(submodule_name[1:-1], root_module.__name__))
-        else:
-            op_module = root_module
-
-        op = getattr(op_module, op_name[(len(op_name_prefix)+len(submodule_name)):], None)
-        if op is None:
-            raise ValueError('Cannot find operator {} in module'
-                             .format(op_name[op_name_prefix:], root_module.__name__))
-        op.__signature__ = inspect.signature(getattr(_numpy_op_doc, op_name))
+        op = _get_builtin_op(op_name)
+        if op is not None:
+            op.__signature__ = inspect.signature(getattr(_numpy_op_doc, op_name))
 
 
 _register_op_signatures()
