99575b22eaa27758a9fd74cb34c207d9e1a18dd2
cfujitsang <cfujitsang@nvidia.com>
2019-10-02 12:30:13 -0700

    fix typo

--- ci/docker/install/tensorrt.sh
+++ ci/docker/install/tensorrt.sh
@@ -41,7 +41,7 @@ popd
 
 # Install TensorRT
 echo "TensorRT build enabled. Installing TensorRT."
-iwget -qO tensorrt.deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnvinfer5_5.1.5-1+cuda10.0_amd64.deb
+wget -qO tensorrt.deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libnvinfer5_5.1.5-1+cuda10.0_amd64.deb
 dpkg -i tensorrt.deb
 apt-get update
 apt-get install -y --allow-downgrades libnvinfer5=5.1.5-1+cuda10.0
