ac3df18d155e92c91ec7680bf3d53863e29d6e49
Joshua Z. Zhang <cheungchih@gmail.com>
2019-10-03 17:16:56 -0700

    address comments

--- tests/python/unittest/test_contrib_operator.py
+++ tests/python/unittest/test_contrib_operator.py
@@ -23,7 +23,7 @@ import random
 import itertools
 from numpy.testing import assert_allclose, assert_array_equal
 from mxnet.test_utils import *
-from common import with_seed
+from common import with_seed, assert_raises_cudnn_not_satisfied
 import unittest
 
 def test_box_nms_op():
@@ -410,6 +410,7 @@ def test_op_mrcnn_mask_target():
     assert_almost_equal(mask_cls.asnumpy(), gt_mask_cls.asnumpy())
 
 @with_seed()
+@assert_raises_cudnn_not_satisfied(min_version='5.1.10')
 def test_modulated_deformable_convolution():
     for num_batch in [1, 2]:
         for num_channel_data, num_deformable_group in itertools.product([4, 8], [1, 2]):
@@ -444,9 +445,8 @@ def test_modulated_deformable_convolution():
                         else:
                             rtol, atol = 0.05, 1e-3
                         # By now we only have gpu implementation
-                        if default_context().device_type == 'gpu':
-                            check_numeric_gradient(op, [im_data, offset_data, mask_data, weight, bias], rtol=rtol, atol=atol,
-                                                   grad_nodes=grad_nodes, ctx=mx.gpu(0))
+                        check_numeric_gradient(op, [im_data, offset_data, mask_data, weight, bias], rtol=rtol, atol=atol,
+                                                grad_nodes=grad_nodes, ctx=mx.gpu(0))
 
 
 if __name__ == '__main__':
