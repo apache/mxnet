1d0d1e687fdf436896f8ca106c4915adfd29c8cb
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2019-10-09 17:49:12 -0700

    Faster Transpose 2D (#16104)
    * 2d transpose naive
    
    * omp pragma
    
    * omp pragma unroll
    
    * blocksize
    
    * make it 2d tile
    
    * loop peeling
    
    * better loop peeling
    
    * redundancy
    
    * removed bool
    
    * removing excess for loops, memory save
    
    * fix internal forloop
    
    * remove commented code, lint fix
    
    * Trigger notification
    
    * explain params, indent fix, explain blocksize
    
    * fix p,n and reduce for loop computation j+a,i+b
    
    * kernel
    
    * gpu thread 1
    
    * remove gpu implementation
    
    * fix internal for loop
    
    * unittest to catch the previous error
    
    * optimizations
    
    * microsoft cpp doesn't support omp collapse

--- tests/python/unittest/test_operator.py
+++ tests/python/unittest/test_operator.py
@@ -2875,6 +2875,13 @@ def test_transpose():
 
 
 @with_seed()
+def test_larger_transpose():
+    x = mx.nd.random.normal(shape=(50,51))
+    y = mx.nd.transpose(x)
+    assert_allclose(np.transpose(x.asnumpy()), y.asnumpy())
+
+
+@with_seed()
 def test_expand_dims():
     for ndim in range(1, 6):
         for axis in range(-ndim + 1, ndim):
