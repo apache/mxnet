4dee4eebdb2136744a0333ea08282b1385c6a0ce
Zhennan Qin <zhennan.qin@intel.com>
2019-10-12 18:17:21 +0800

    Fix mkldnn reshape (#16455)
    Change-Id: I7e3ed84d02eaac4dbc0637167519e53e7eb8e168

--- src/operator/nn/mkldnn/mkldnn_base-inl.h
+++ src/operator/nn/mkldnn/mkldnn_base-inl.h
@@ -194,7 +194,6 @@ bool SupportMKLDNNDeconv(const DeconvolutionParam& params, const NDArray &input)
 bool SupportMKLDNNSoftmax(const SoftmaxParam& param, const NDArray &input, const NDArray &output);
 bool SupportMKLDNNSoftmaxOutput(const SoftmaxOutputParam &param);
 bool SupportMKLDNNTranspose(const TransposeParam& param, const NDArray &data);
-bool SupportMKLDNNReshape(const ReshapeParam &param, const NDArray &data);
 }  // namespace op
 
 static int GetTypeSize(int dtype) {
