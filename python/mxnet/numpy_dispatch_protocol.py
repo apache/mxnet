bdced3f66a78ad18883f2636b33927c35870de40
JiangZhaoh <jiang_zhaoh@163.com>
2019-10-14 21:53:22 +0800

    greater and greater_equal

--- python/mxnet/numpy_dispatch_protocol.py
+++ python/mxnet/numpy_dispatch_protocol.py
@@ -159,6 +159,8 @@ def _register_array_function():
 # https://docs.scipy.org/doc/numpy/reference/ufuncs.html#available-ufuncs
 _NUMPY_ARRAY_UFUNC_LIST = [
     'add',
+    'greater',
+    'greater_equal',
     'subtract',
     'multiply',
     # Uncomment divide when mxnet.numpy.true_divide is added
