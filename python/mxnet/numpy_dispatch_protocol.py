fcae4ceb4a1b61befaf2bd17d473e4ea3cd00a32
JiangZhaoh <jiang_zhaoh@163.com>
2019-10-16 15:51:31 +0800

    test without l le g ge e ne

--- python/mxnet/numpy_dispatch_protocol.py
+++ python/mxnet/numpy_dispatch_protocol.py
@@ -159,10 +159,10 @@ def _register_array_function():
 # https://docs.scipy.org/doc/numpy/reference/ufuncs.html#available-ufuncs
 _NUMPY_ARRAY_UFUNC_LIST = [
     'add',
-    'equal',
-    'not_equal',
-    'greater',
-    'greater_equal',
+    #'equal',
+    #'not_equal',
+    #'greater',
+    #'greater_equal',
     'subtract',
     'multiply',
     # Uncomment divide when mxnet.numpy.true_divide is added
@@ -174,8 +174,8 @@ _NUMPY_ARRAY_UFUNC_LIST = [
     'rint',
     'sign',
     'exp',
-    'less',
-    'less_equal',
+    #'less',
+    #'less_equal',
     'log',
     'log2',
     'log10',
