fc81c64f7f4d09d28978ab72d3cfa60ae6b831f0
Sheng Zha <szha@users.noreply.github.com>
2019-10-19 09:41:36 -0700

    Correct Google Analytics Tracker (#16490)

--- docs/python_docs/_static/google_analytics.js
+++ docs/python_docs/_static/google_analytics.js
@@ -22,5 +22,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-96378503-11', 'auto');
+  ga('create', 'UA-96378503-1', 'auto');
   ga('send', 'pageview');
