95bee18f7078c1d2f49a1d681be03f4f510994b0
cfujitsang <cfujitsang@nvidia.com>
2019-10-21 11:59:39 -0700

    remove a wrong copy-paste

--- src/operator/contrib/transformer.cc
+++ src/operator/contrib/transformer.cc
@@ -238,16 +238,7 @@ output = mx.nd.batch_dot(q_proj, k_proj, transpose_b=True)
 .set_attr<mxnet::FInferShape>("FInferShape", InterleavedMatMulEncDecQKShape)
 .set_attr<nnvm::FInferType>("FInferType", ElemwiseType<2, 1>)
 .set_attr<nnvm::FGradient>("FGradient",
-    ElemwiseGradUseIthe equivalent code would be:
-tmp = mx.nd.reshape(queries_keys_values, shape=(0, 0, num_heads, 3, -1))
-q_proj = mx.nd.transpose(q_proj, axes=(1, 2, 0, 3))
-q_proj = mx.nd.reshape(q_proj, shape=(-1, 0, 0), reverse=True)
-q_proj = mx.nd.contrib.div_sqrt_dim(q_proj)
-k_proj = mx.nd.transpose(k_proj, axes=(1, 2, 0, 3))
-k_proj = mx.nd.reshap(k_proj, shape=(-1, 0, 0), reverse=True)
-output = mx.nd.batch_dot(q_proj, k_proj, transpose_b=True)
-
-n{"_backward_interleaved_matmul_encdec_qk"})
+    ElemwiseGradUseIn{"_backward_interleaved_matmul_encdec_qk"})
 .add_argument("queries", "NDArray-or-Symbol", "Queries")
 .add_argument("keys_values", "NDArray-or-Symbol", "Keys and values interleaved")
 .add_arguments(InterleavedMatMulParam::__FIELDS__());
