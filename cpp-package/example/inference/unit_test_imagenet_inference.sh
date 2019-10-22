6742e9b68e7224b3ebd0a42eb062ae88b7d7103c
ChaiBapchya <chai.bapat@gmail.com>
2019-10-22 13:19:30 -0700

    lots of prints

--- cpp-package/example/inference/unit_test_imagenet_inference.sh
+++ cpp-package/example/inference/unit_test_imagenet_inference.sh
@@ -38,7 +38,11 @@ wget -nc https://raw.githubusercontent.com/dmlc/gluon-cv/master/gluoncv/model_zo
 cd ../data
 wget -nc http://data.mxnet.io/data/val_256_q90.rec
 cd ..
-
+echo $PWD
+echo "lib path"
+ls -al ../../../lib
+echo "current file"
+ls -al .
 # Running inference on imagenet.
 if [ "$(uname)" == "Darwin" ]; then
     echo ">>> INFO: FP32 real data"
