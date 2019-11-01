f689f5ce58f8ac48f37f387a9ce4ee942f8c18cd
reminisce <wujun.nju@gmail.com>
2019-11-01 11:01:49 -0700

    Fix lint

--- python/mxnet/numpy_op_fallback.py
+++ python/mxnet/numpy_op_fallback.py
@@ -22,8 +22,8 @@ import functools
 import ast
 import numpy as np
 from . import operator
-from . import numpy as _mx_np
-from .util import np_array, use_np_shape
+from . import numpy as _mx_np  # pylint: disable=reimported
+from .util import np_array, use_np
 from .ndarray.numpy import _internal as _nd_npi
 from .symbol.numpy import _internal as _sym_npi
 
@@ -49,22 +49,24 @@ def register(op_name, imperative=True, symbolic=True):
     return _register_helper
 
 
+@use_np  # enforce np shape and array semantics for all the methods in this class
 class Resize(operator.CustomOp):
+    """Fallback to NumPy resize operator."""
     def __init__(self, new_shape):
+        super(Resize, self).__init__()
         self._new_shape = new_shape
 
-    @use_np_shape
     def forward(self, is_train, req, in_data, out_data, aux):
         out = np.resize(in_data[0].asnumpy(), self._new_shape)
         self.assign(out_data[0], req[0], _mx_np.array(out, dtype=out.dtype, ctx=out_data[0].ctx))
 
-    @use_np_shape
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
         raise NotImplementedError('Operator resize does not support gradient computation')
 
 
 @register('resize_fallback')
 class ResizeProp(operator.CustomOpProp):
+    """Fallback resize operator properties."""
     def __init__(self, new_shape):
         super(ResizeProp, self).__init__(need_top_grad=True)
         self._new_shape = ast.literal_eval(new_shape)
