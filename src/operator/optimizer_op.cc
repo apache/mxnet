52f8c9b307ee89ab6f45672f0bcbfb9bd3b562ba
Rohit Kumar Srivastava <srivastava.141@buckeyemail.osu.edu>
2019-11-13 19:56:38 +0000

    fixing base lamb optimizer

--- src/operator/optimizer_op.cc
+++ src/operator/optimizer_op.cc
@@ -43,7 +43,8 @@ DMLC_REGISTER_PARAMETER(FtrlParam);
 DMLC_REGISTER_PARAMETER(SignSGDParam);
 DMLC_REGISTER_PARAMETER(SignumParam);
 DMLC_REGISTER_PARAMETER(AdagradParam);
-DMLC_REGISTER_PARAMETER(LAMBParam);
+DMLC_REGISTER_PARAMETER(LambUpdatePhaseOneParam);
+DMLC_REGISTER_PARAMETER(LambUpdatePhaseTwoParam);
 
 NNVM_REGISTER_OP(signsgd_update)
 .describe(R"code(Update function for SignSGD optimizer.
@@ -922,20 +923,39 @@ Note that non-zero values for the weight decay option are not supported.
 .add_argument("history", "NDArray-or-Symbol", "History")
 .add_arguments(AdagradParam::__FIELDS__());
 
-NNVM_REGISTER_OP(lamb_update)
+NNVM_REGISTER_OP(lamb_update_phase1)
 .describe(R"code(Update function for lamb optimizer.
 )code" ADD_FILELINE)
 .set_num_inputs(4)
 .set_num_outputs(1)
-.set_attr_parser(ParamParser<LAMBParam>)
-.set_attr<mxnet::FInferShape>("FInferShape", ElemwiseShape<4,1>)
-.set_attr<nnvm::FInferType>("FInferType", ElemwiseType<4,1>)
-.set_attr<FCompute>("FCompute<cpu>", LAMBUpdate<cpu>)
+.set_attr_parser(ParamParser<LambUpdatePhaseOneParam>)
+.set_attr<mxnet::FInferShape>("FInferShape", ElemwiseShape<4, 1>)
+.set_attr<nnvm::FInferType>("FInferType", ElemwiseType<4, 1>)
+.set_attr<FCompute>("FCompute<cpu>", LambUpdatePhaseOne<cpu>)
+.set_attr<nnvm::FMutateInputs>("FMutateInputs",
+  [](const nnvm::NodeAttrs& attrs) {
+    return std::vector<uint32_t>{2, 3};
+  })
 .add_argument("weight", "NDArray-or-Symbol", "Weight")
 .add_argument("grad", "NDArray-or-Symbol", "Gradient")
 .add_argument("mean", "NDArray-or-Symbol", "Moving mean")
 .add_argument("var", "NDArray-or-Symbol", "Moving variance")
-.add_arguments(LAMBParam::__FIELDS__());
+.add_arguments(LambUpdatePhaseOneParam::__FIELDS__());
+
+NNVM_REGISTER_OP(lamb_update_phase2)
+.describe(R"code(Update function for lamb optimizer.
+)code" ADD_FILELINE)
+.set_num_inputs(4)
+.set_num_outputs(1)
+.set_attr_parser(ParamParser<LambUpdatePhaseTwoParam>)
+.set_attr<mxnet::FInferShape>("FInferShape", LambUpdatePhaseTwoShape)
+.set_attr<nnvm::FInferType>("FInferType", ElemwiseType<4, 1>)
+.set_attr<FCompute>("FCompute<cpu>", LambUpdatePhaseTwo<cpu>)
+.add_argument("weight", "NDArray-or-Symbol", "Weight")
+.add_argument("g", "NDArray-or-Symbol", "Output of lamb_update_phase 1")
+.add_argument("r1", "NDArray-or-Symbol", "r1")
+.add_argument("r2", "NDArray-or-Symbol", "r2")
+.add_arguments(LambUpdatePhaseTwoParam::__FIELDS__());
 
 }  // namespace op
 }  // namespace mxnet
--- tests/python/unittest/test_optimizer.py
+++ tests/python/unittest/test_optimizer.py
@@ -428,7 +428,7 @@ class PyLAMB(mx.optimizer.Optimizer):
 	Python reference implementation of LAMB optimizer.
     """
     def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-6,
-                 lower_bound=1e-3, upper_bound=10.0, bias_correction=False, **kwargs):
+                 lower_bound=None, upper_bound=None, bias_correction=False, **kwargs):
         super(PyLAMB, self).__init__(learning_rate=learning_rate, **kwargs)
         self.beta1 = beta1
         self.beta2 = beta2
@@ -450,30 +450,33 @@ class PyLAMB(mx.optimizer.Optimizer):
 
         grad *= self.rescale_grad
         if self.clip_gradient is not None:
-            grad = clip(grad, -self.clip_gradient, self.clip_gradient)
+            grad = mx.nd.clip(grad, -self.clip_gradient, self.clip_gradient)
 
         mean, var = state
         mean[:] = self.beta1 * mean + (1. - self.beta1) * grad
         var[:] = self.beta2 * var + (1. - self.beta2) * mx.nd.square(grad)
 
+        mean_hat = mean
+        var_hat = var
         r1 = weight.norm()
-        if not self.bias_correction:
-            r1 = mx.nd.minimum(mx.nd.maximum(r1, self.lower_bound), self.upper_bound)
-            g = mean / (mx.nd.sqrt(var) + self.epsilon) + wd * weight
-
-        else:
+        if self.lower_bound:
+            r1 = mx.nd.maximum(r1, self.lower_bound)
+        if self.upper_bound:
+            r1 = mx.nd.minimum(r1, self.upper_bound)
+        if self.bias_correction:
             mean_hat = mean / (1. - mx.nd.power(self.beta1, t))
             var_hat = var / (1. - mx.nd.power(self.beta2, t))
-            g = mean_hat / mx.nd.sqrt(var_hat + self.epsilon) + wd * weight
 
+        g = mean_hat / (mx.nd.sqrt(var_hat) + self.epsilon) + wd * weight
         r2 = g.norm()
-
         # calculate lamb_trust_ratio
         r = 1. if r1 == 0. or r2 == 0. else r1 / r2
         lr *= r
-
         # update weight
         weight[:] -= lr * g
+
+    def update_multi_precision(self, index, weight, grad, state):
+        self.update(index, weight, grad, state)
 
 @with_seed()
 def test_lamb():
@@ -484,7 +487,9 @@ def test_lamb():
     rg_options = [{}, {'rescale_grad': 0.14}, {'rescale_grad': 0.8}]
     wd_options = [{}, {'wd': 0.03}, {'wd': 0.05}, {'wd': 0.07}]
     bc_options = [{}, {'bias_correction': False}, {'bias_correction': True}]
-    for params in itertools.product(cg_options, rg_options, wd_options, bc_options):
+    lb_options = [{}, {'lower_bound': None}, {'lower_bound': 1e-3}]
+    ub_options = [{}, {'upper_bound': None}, {'upper_bound': 10}]
+    for params in itertools.product(cg_options, rg_options, wd_options, bc_options, lb_options, ub_options):
         kwarg = {k: v for param in params for k, v in param.items()}
         compare_optimizer(opt1(**kwarg), opt2(**kwarg), shape, np.float32)
 
