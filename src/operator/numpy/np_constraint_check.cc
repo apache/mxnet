1a88ec1bd51bca8f83759daed2f341583eaffe3b
Xi Wang <xidulu@gmail.com>
2019-11-18 03:24:58 +0000

    code polished

--- src/operator/numpy/np_constraint_check.cc
+++ src/operator/numpy/np_constraint_check.cc
@@ -1,3 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file np_constraint_check.cc
+ * \brief helper function for constraint check
+ */
+
 #include "./np_constraint_check.h"
 
 namespace mxnet {
@@ -17,7 +41,7 @@ inline bool ConstraintCheckShape(const nnvm::NodeAttrs& attrs,
     return false;
   }
   // Only 1-D support is supported.
-  CHECK_EQ(in_attrs->at(0).ndim(), 1U) << "Only 1-D input is supported.";
+  // CHECK_EQ(in_attrs->at(0).ndim(), 1U) << "Only 1-D input is supported.";
   SHAPE_ASSIGN_CHECK(*out_attrs, 0, TShape(0, -1))
   return true;
 }
@@ -35,6 +59,9 @@ inline bool ConstraintCheckType(const nnvm::NodeAttrs& attrs,
 DMLC_REGISTER_PARAMETER(ConstraintCheckParam);
 
 NNVM_REGISTER_OP(_npx_constraint_check)
+.describe(R"code(Check if all the elements in a 1-D boolean array is true.
+If not, exception will be raised with given error message.
+)code" ADD_FILELINE)
 .set_attr_parser(ParamParser<ConstraintCheckParam>)
 .set_num_inputs(1)
 .set_num_outputs(1)
@@ -42,10 +69,6 @@ NNVM_REGISTER_OP(_npx_constraint_check)
   [](const NodeAttrs& attrs) {
     return std::vector<std::string>{"input"};
   })
-.set_attr<nnvm::FNumVisibleOutputs>("FNumVisibleOutputs",
-    [](const NodeAttrs& attrs) {
-  return 0;
-})
 .set_attr<mxnet::FInferShape>("FInferShape", ConstraintCheckShape)
 .set_attr<nnvm::FInferType>("FInferType", ConstraintCheckType)
 .set_attr<FResourceRequest>("FResourceRequest",
@@ -53,7 +76,7 @@ NNVM_REGISTER_OP(_npx_constraint_check)
     return std::vector<ResourceRequest>{ResourceRequest::kTempSpace};
   })
 .set_attr<FCompute>("FCompute<cpu>", ConstraintCheckForward<cpu>)
-.add_argument("input", "NDArray-or-Symbol", "Input ndarray")
+.add_argument("input", "NDArray-or-Symbol", "Input boolean array")
 .add_arguments(ConstraintCheckParam::__FIELDS__());
 
 }  // namespace op
--- src/operator/numpy/np_constraint_check.h
+++ src/operator/numpy/np_constraint_check.h
@@ -1,9 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*!
+ * \file np_constraint_check.h
+ * \brief helper function for constraint check
+ */
+
+#ifndef MXNET_OPERATOR_NUMPY_NP_CONSTRAINT_CHECK_H_
+#define MXNET_OPERATOR_NUMPY_NP_CONSTRAINT_CHECK_H_
+
 #include <algorithm>
 #include <string>
 #include <vector>
-#include "../mshadow_op.h"
-#include "../mxnet_op.h"
-#include "../operator_common.h"
 #include "./np_broadcast_reduce_op.h"
 
 namespace mxnet {
@@ -16,7 +40,7 @@ struct ConstraintCheckParam : public dmlc::Parameter<ConstraintCheckParam> {
   std::string msg;
   DMLC_DECLARE_PARAMETER(ConstraintCheckParam) {
     DMLC_DECLARE_FIELD(msg)
-    .set_default("Constraint violated!")
+    .set_default("Constraint violated.")
     .describe("Error message raised when constraint violated");
   }
 };
@@ -30,8 +54,6 @@ void ConstraintCheckForward(const nnvm::NodeAttrs& attrs, const OpContext& ctx,
   using namespace mxnet_op;
   CHECK_EQ(inputs.size(), 1U);
   CHECK_EQ(outputs.size(), 1U);
-  // ???
-  CHECK(false);
   const ConstraintCheckParam& param =
       nnvm::get<ConstraintCheckParam>(attrs.parsed);
   ReduceAxesComputeImpl<xpu, mshadow_op::product, false, false,
@@ -45,3 +67,5 @@ void ConstraintCheckForward(const nnvm::NodeAttrs& attrs, const OpContext& ctx,
 
 }  // namespace op
 }  // namespace mxnet
+
+#endif  // MXNET_OPERATOR_NUMPY_NP_CONSTRAINT_CHECK_H_
