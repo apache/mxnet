39ab96b99c56f8794713cc3eaef41c651fa2d7af
Haibin Lin <linhaibin.eric@gmail.com>
2019-12-13 14:16:17 -0800

    Update trainer.py

--- python/mxnet/gluon/trainer.py
+++ python/mxnet/gluon/trainer.py
@@ -72,7 +72,7 @@ class Trainer(object):
     def __init__(self, params, optimizer, optimizer_params=None, kvstore='device',
                  compression_params=None, update_on_kvstore=None):
         param_list = []
-        if isinstance(params, mx.gluon.ParameterDict):
+        if isinstance(params, (dict, mx.gluon.ParameterDict)):
             for key in sorted(list(params.keys())):
                 param_list.append(params[key])
         params = param_list
