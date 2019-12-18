49bb79ed3a50ab8a35227afe5dca2ca73b48e068
Zheng <shzheng@a483e789dd93.ant.amazon.com>
2019-12-18 11:01:26 -0800

    update

--- python/mxnet/gluon/probability/distributions/distribution.py
+++ python/mxnet/gluon/probability/distributions/distribution.py
@@ -2,7 +2,7 @@ import mxnet as mx
 from mxnet import np, npx
 
 
-def getF(*params):
+def _getF(*params):
     for param in params:
         if isinstance(param, np.ndarray):
             return mx.ndarray
@@ -10,20 +10,37 @@ def getF(*params):
             return mx.symbol.numpy._Symbol
     return mx.ndarray
 
-class Distribution:
+
+class Distribution(object):
     has_grad = False
 
     def __init__(self, F=None):
         self.F = F
 
     def log_prob(self, x):
+        """
+        Returns the log of the probability density/mass function evaluated at `x`.
+        """
         raise NotImplementedError()
 
     def prob(self, x):
+        """
+        Returns the probability density/mass function evaluated at `x`.
+        """
         raise NotImplementedError
 
-    def sample(self, size):
+    def sample(self, shape):
+        """
+        Generates a `shape` shaped sample.
+        """
         raise NotImplementedError
 
-    def sample_n(self, batch_size):
+    def sample_n(self, n):
+        """
+        As MxNet does not support symbolic shape, the following
+        code cannot be hybridized.
+            normal = Normal(loc=loc, scale=scale)
+            samples = normal.sample(size=(n,) + loc.shape)
+        Thus, we introduce another function sample_n that infers loc.shape in the backend.
+        """
         raise NotImplementedError
--- python/mxnet/gluon/probability/distributions/normal.py
+++ python/mxnet/gluon/probability/distributions/normal.py
@@ -1,26 +1,26 @@
 from mxnet import np, npx
 from .exp_family import ExponentialFamily
-from .distribution import getF
+from .distribution import _getF
 
 
 class Normal(ExponentialFamily):
 
     def __init__(self, loc=0.0, scale=1.0, F=None):
-        self.F = F if F is not None else getF([loc, scale])
+        self.F = F if F is not None else _getF([loc, scale])
         super(Normal, self).__init__(F=F)
         self._loc = loc
         self._scale = scale
         self.F = F
 
-    def sample(self, size=None):
+    def sample(self, size):
         return self.F.np.random.normal(self._loc,
-                                    self._scale,
-                                    size)
+                                       self._scale,
+                                       size)
 
-    def sample_n(self, batch_size=None):
+    def sample_n(self, n):
         return self.F.npx.random.normal_n(self._loc,
-                                        self._scale,
-                                        batch_size)
+                                          self._scale,
+                                          n)
 
     def log_prob(self, value):
         F = self.F
