48f885de1f49c93cc2b5984a724655d01ca94f12
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-12-22 06:53:54 +0000

    add test cases

--- julia/test/unittest/context.jl
+++ julia/test/unittest/context.jl
@@ -26,8 +26,69 @@ function test_num_gpus()
   @test num_gpus() >= 0
 end
 
+function test_with_context()
+  @info "Context::@with_context"
+
+  @with_context mx.CPU 42 begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.CPU
+    @test ctx.device_id   == 42
+
+    @with_context mx.GPU 24 begin
+      ctx = mx.current_context()
+      @test ctx.device_type == mx.GPU
+      @test ctx.device_id   == 42
+    end
+
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.CPU
+    @test ctx.device_id   == 42
+  end
+
+  function f()
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.GPU
+    @test ctx.device_id   == 123
+  end
+
+  @with_context mx.GPU 123 begin
+    f()
+  end
+
+  @with_context mx.GPU begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.GPU
+    @test ctx.device_id   == 0
+  end
+
+  @with_context mx.CPU begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.CPU
+    @test ctx.device_id   == 0
+  end
+
+  @with_gpu 123 f()
+  @with_gpu begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.GPU
+    @test ctx.device_id   == 0
+  end
+
+  @with_cpu 123 begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.CPU
+    @test ctx.device_id   == 123
+  end
+  @with_cpu begin
+    ctx = mx.current_context()
+    @test ctx.device_type == mx.CPU
+    @test ctx.device_id   == 0
+  end
+end
+
 @testset "Context Test" begin
   test_num_gpus()
+  test_with_context()
 end
 
 
