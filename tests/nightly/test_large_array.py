e6170bb6a1a7de064c34e8e6d77ea8b05f21d5c0
ChaiBapchya <chai.bapat@gmail.com>
2019-12-29 23:27:35 +0000

    fix test

--- tests/nightly/test_large_array.py
+++ tests/nightly/test_large_array.py
@@ -1690,12 +1690,9 @@ def test_binary_broadcast():
 
 
 def test_sparse_dot():
-    shape = (2, LARGE_X)
-   # density = 0.5
-    #matrix = rand_ndarray(shape, 'csr', density)
-    #sp_mat1 = nd.sparse.csr_matrix((matrix.data, matrix.indices, matrix.indptr), shape)
-    sp_mat1 = nd.sparse.csr_matrix(([2],[6],[0,1]),shape=shape)
-    mat2 = nd.ones((LARGE_X, 2))
+    shape = (2, VLARGE_X)
+    sp_mat1 = nd.sparse.csr_matrix(([2], [6], [0, 1, 1]), shape=shape)
+    mat2 = nd.ones((VLARGE_X, 2))
     out = nd.dot(sp_mat1, mat2)
     assert out.asnumpy()[0][0] == 2
     assert out.shape == (2, 2)
