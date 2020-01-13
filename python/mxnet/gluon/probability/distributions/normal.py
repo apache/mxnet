27096800d1f6a2407e5949c993c9de7189c490d2
Xi Wang <xidulu@gmail.com>
2020-01-13 04:18:35 +0000

    fix normal F

--- python/mxnet/gluon/probability/distributions/normal.py
+++ python/mxnet/gluon/probability/distributions/normal.py
@@ -19,6 +19,7 @@
 # pylint: disable=wildcard-import
 """Normal distribution"""
 __all__ = ['Normal']
+import math
 from .exp_family import ExponentialFamily
 from .utils import getF
 
@@ -41,11 +42,10 @@ class Normal(ExponentialFamily):
     has_grad = True
 
     def __init__(self, loc=0.0, scale=1.0, F=None):
-        self.F = F if F is not None else getF([loc, scale])
-        super(Normal, self).__init__(F=F)
+        _F = F if F is not None else getF([loc, scale])
+        super(Normal, self).__init__(F=_F)
         self._loc = loc
         self._scale = scale
-        self.F = F
 
     def log_prob(self, value):
         """Compute the log likehood of `value`.
@@ -65,7 +65,7 @@ class Normal(ExponentialFamily):
         log_scale = F.np.log(self._scale)
         log_prob = -((value - self._loc) ** 2) / (2 * var)
         log_prob = log_prob - log_scale
-        log_prob = log_prob + F.np.log(F.np.sqrt(2 * F.np.pi))
+        log_prob = log_prob + F.np.log(F.np.sqrt(2 * math.pi))
         return log_prob
 
     def sample(self, size=None):
@@ -143,5 +143,5 @@ class Normal(ExponentialFamily):
             the log_normalizer term
         """
         F = self.F
-        return -0.25 * F.np.pow(2) / y + 0.5 * F.np.log(-F.np.pi / y)
+        return -0.25 * F.np.pow(2) / y + 0.5 * F.np.log(-math.pi / y)
 
