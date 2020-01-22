6f2df4721e24a77ba326b2c46eaa445f440b095d
ChaiBapchya <chai.bapat@gmail.com>
2020-01-22 20:12:16 +0000

    remove template parameter for ndim,aux_ndim since internally its always int

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -943,17 +943,17 @@ int MXNDArrayCreateEx(const uint32_t *shape,
   API_END();
 }
 
-template<typename DType, typename dimtype>
+template<typename DType>
 void CreateSparseNDArray(int storage_type,
                          const DType *shape,
-                         dimtype ndim,
+                         int ndim,
                          int dev_type,
                          int dev_id,
                          int delay_alloc,
                          int dtype,
                          uint32_t num_aux,
                          int *aux_type,
-                         dimtype *aux_ndims,
+                         int *aux_ndims,
                          const DType *aux_shape,
                          NDArrayHandle *out) {
   std::vector<int> aux_types;
@@ -987,9 +987,9 @@ int MXNDArrayCreateSparseEx(int storage_type,
                             const uint32_t *aux_shape,
                             NDArrayHandle *out) {
   API_BEGIN();
-  CreateSparseNDArray<uint32_t, uint32_t>(storage_type, shape, ndim, dev_type, dev_id,
+  CreateSparseNDArray<uint32_t>(storage_type, shape, static_cast<int>(ndim), dev_type, dev_id,
                                 delay_alloc, dtype, num_aux, aux_type,
-                                aux_ndims, aux_shape, out);
+                                reinterpret_cast<int *>(aux_ndims), aux_shape, out);
   API_END();
 }
 
@@ -1007,9 +1007,9 @@ int MXNDArrayCreateSparseEx64(int storage_type,
                             const int64_t *aux_shape,
                             NDArrayHandle *out) {
   API_BEGIN();
-  CreateSparseNDArray<int64_t, int>(storage_type, shape, ndim, dev_type, dev_id,
+  CreateSparseNDArray<int64_t>(storage_type, shape, static_cast<int>(ndim), dev_type, dev_id,
                                delay_alloc, dtype, num_aux, aux_type,
-                               aux_ndims, aux_shape, out);
+                               reinterpret_cast<int *>(aux_ndims), aux_shape, out);
   API_END();
 }
 
