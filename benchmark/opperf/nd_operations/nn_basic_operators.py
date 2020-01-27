ffb6dded90d1c874c9032cae206249ff7a2236c4
Connor Goggins <cgoggins0@gmail.com>
2020-01-27 15:54:25 -0800

    Added L2, layer and instance norm

--- benchmark/opperf/nd_operations/nn_basic_operators.py
+++ benchmark/opperf/nd_operations/nn_basic_operators.py
@@ -124,6 +124,33 @@ def run_nn_basic_operators_benchmarks(ctx=mx.cpu(), dtype='float32', profiler='n
                                                             "regularization_coefficient": .5}],
                                                    warmup=warmup,
                                                    runs=runs)
+    # L2Normalization benchmarks
+    l2_benchmark_res = run_performance_test([getattr(MX_OP_MODULE, "L2Normalization")],
+                                                   run_backward=True,
+                                                   dtype=dtype,
+                                                   ctx=ctx,
+                                                   profiler=profiler,
+                                                   inputs=[{"data": (32, 3, 256, 256)},
+                                                           {"data": (32, 3, 10000, 10),
+                                                            "eps": .01}],
+                                                   warmup=warmup,
+                                                   runs=runs)
+    # LayerNorm and InstanceNorm benchmarks
+    layerinstancenorm_benchmark_res = run_performance_test([getattr(MX_OP_MODULE, "LayerNorm"),
+                                                            getattr(MX_OP_MODULE, "InstanceNorm")],
+                                                   run_backward=True,
+                                                   dtype=dtype,
+                                                   ctx=ctx,
+                                                   profiler=profiler,
+                                                   inputs=[{"data": (32, 3, 256, 256),
+                                                            "gamma": (256),
+                                                            "beta": (256)},
+                                                           {"data": (32, 3, 10000, 10),
+                                                            "gamma": (10),
+                                                            "beta": (10),
+                                                            "eps": .01}],
+                                                   warmup=warmup,
+                                                   runs=runs)
     # Prepare combined results
-    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res)
+    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res + l2_benchmark_res + layerinstancenorm_benchmark_res)
     return mx_basic_nn_results
