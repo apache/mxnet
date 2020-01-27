67842cd0fce10082e0eedf885f6219dba6023a33
Connor Goggins <cgoggins0@gmail.com>
2020-01-27 15:39:42 -0800

    Added SVMOutput

--- benchmark/opperf/nd_operations/nn_basic_operators.py
+++ benchmark/opperf/nd_operations/nn_basic_operators.py
@@ -110,6 +110,20 @@ def run_nn_basic_operators_benchmarks(ctx=mx.cpu(), dtype='float32', profiler='n
                                                             "grad_scale": .5}],
                                                    warmup=warmup,
                                                    runs=runs)
+    # SVMOutput benchmarks
+    svmoutput_benchmark_res = run_performance_test([getattr(MX_OP_MODULE, "SVMOutput")],
+                                                   run_backward=True,
+                                                   dtype=dtype,
+                                                   ctx=ctx,
+                                                   profiler=profiler,
+                                                   inputs=[{"data": (32, 3, 256, 256),
+                                                            "label": (32, 3, 256)},
+                                                           {"data": (32, 3, 10000, 10),
+                                                            "label": (32, 3, 10000),
+                                                            "margin": .5,
+                                                            "regularization_coefficient": .5}],
+                                                   warmup=warmup,
+                                                   runs=runs)
     # Prepare combined results
-    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res)
+    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res)
     return mx_basic_nn_results
