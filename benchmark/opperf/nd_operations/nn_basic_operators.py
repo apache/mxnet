81a8d60f516ff9d6ab012feb3defb34541265991
Connor Goggins <cgoggins0@gmail.com>
2020-01-27 16:16:49 -0800

    Added Embedding

--- benchmark/opperf/nd_operations/nn_basic_operators.py
+++ benchmark/opperf/nd_operations/nn_basic_operators.py
@@ -165,6 +165,22 @@ def run_nn_basic_operators_benchmarks(ctx=mx.cpu(), dtype='float32', profiler='n
                                                             "eps": .01}],
                                                    warmup=warmup,
                                                    runs=runs)
+    # Embedding benchmarks
+    embedding_benchmark_res = run_performance_test([getattr(MX_OP_MODULE, "Embedding")],
+                                                   run_backward=True,
+                                                   dtype=dtype,
+                                                   ctx=ctx,
+                                                   profiler=profiler,
+                                                   inputs=[{"data": (32, 3, 256, 256),
+                                                            "weight": (3, 4),
+                                                            "input_dim": 3,
+                                                            "output_dim": 4},
+                                                           {"data": (32, 3, 10000, 10),
+                                                            "weight": (16, 9),
+                                                            "input_dim": 16,
+                                                            "output_dim": 9}],
+                                                   warmup=warmup,
+                                                   runs=runs)
     # Prepare combined results
-    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res + l2_benchmark_res + layernorm_benchmark_res + instancenorm_benchmark_res)
+    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res + l2_benchmark_res + layernorm_benchmark_res + instancenorm_benchmark_res + embedding_benchmark_res)
     return mx_basic_nn_results
