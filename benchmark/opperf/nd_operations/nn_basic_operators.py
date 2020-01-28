903c8dd224caaa7754eda32788930b458d739597
Connor Goggins <cgoggins0@gmail.com>
2020-01-27 22:54:08 -0800

    Added preloaded_multi_mp_sgd_mom_update

--- benchmark/opperf/nd_operations/nn_basic_operators.py
+++ benchmark/opperf/nd_operations/nn_basic_operators.py
@@ -308,6 +308,19 @@ def run_nn_basic_operators_benchmarks(ctx=mx.cpu(), dtype='float32', profiler='n
                                                             "beta": .2}],
                                                    warmup=warmup,
                                                    runs=runs)
+    # preloaded_multi_mp_sgd_mom_update benchmarks
+    pmmsmu_benchmark_res = run_performance_test([getattr(MX_OP_MODULE, "preloaded_multi_mp_sgd_mom_update")],
+                                                   run_backward=True,
+                                                   dtype=dtype,
+                                                   ctx=ctx,
+                                                   profiler=profiler,
+                                                   inputs=[{"data": (32, 3, 256, 256)},
+                                                           {"data": (32,3,10000,10),
+                                                            "momentum": 5,
+                                                            "rescale_grad": .9,
+                                                            "clip_gradient": .99}],
+                                                   warmup=warmup,
+                                                   runs=runs)
     # Prepare combined results
-    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res + l2_benchmark_res + layernorm_benchmark_res + instancenorm_benchmark_res + embedding_benchmark_res + correlation_benchmark_res + st_benchmark_res + im2col_benchmark_res + col2im_benchmark_res + groupnorm_benchmark_res + rnn_benchmark_res + lrn_benchmark_res)
+    mx_basic_nn_results = merge_map_list(fc_benchmark_res + dropout_benchmark_res + batchnorm_benchmark_res + softmaxoutput_benchmark_res + regressionoutput_benchmark_res + svmoutput_benchmark_res + l2_benchmark_res + layernorm_benchmark_res + instancenorm_benchmark_res + embedding_benchmark_res + correlation_benchmark_res + st_benchmark_res + im2col_benchmark_res + col2im_benchmark_res + groupnorm_benchmark_res + rnn_benchmark_res + lrn_benchmark_res + pmmsmu_benchmark_res)
     return mx_basic_nn_results
