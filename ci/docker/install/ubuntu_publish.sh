9410207321a8ea69980258633d715ed7a2c85d5a
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2020-01-28 23:58:02 -0800

    Don't use Numpy 1.18 on Py2

--- ci/docker/install/ubuntu_publish.sh
+++ ci/docker/install/ubuntu_publish.sh
@@ -65,5 +65,5 @@ pip3 install --upgrade pip
 
 apt-get remove -y python3-urllib3
 
-pip2 install --upgrade --ignore-installed nose cpplint==1.3.0 'numpy>1.16.0,<2.0.0' nose-timer 'requests<2.19.0,>=2.18.4' h5py==2.8.0rc1 scipy==1.0.1 boto3
+pip2 install --upgrade --ignore-installed nose cpplint==1.3.0 'numpy>1.16.0,<1.18' nose-timer 'requests<2.19.0,>=2.18.4' h5py==2.8.0rc1 scipy==1.0.1 boto3
 pip3 install --upgrade --ignore-installed nose cpplint==1.3.0 pylint==2.3.1 'numpy>1.16.0,<2.0.0' nose-timer 'requests<2.19.0,>=2.18.4' h5py==2.8.0rc1 scipy==1.0.1 boto3
