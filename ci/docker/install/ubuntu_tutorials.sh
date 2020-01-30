37572aaa3beb58a835b64d55f20a4b644cce4006
Pedro Larroy <pedro.larroy.lists@gmail.com>
2020-01-30 11:01:53 +0000

    remove pip2

--- ci/docker/install/ubuntu_tutorials.sh
+++ ci/docker/install/ubuntu_tutorials.sh
@@ -25,5 +25,4 @@ apt-get update || true
 apt-get install graphviz python-opencv
 
 # sckit-learn past version 0.20 does not support python version 2 and 3.4
-pip2 install jupyter matplotlib Pillow opencv-python "scikit-learn<0.21.0" graphviz==0.8.4 tqdm mxboard scipy gluoncv
 pip3 install jupyter matplotlib Pillow opencv-python scikit-learn graphviz==0.8.4 tqdm mxboard scipy gluoncv
