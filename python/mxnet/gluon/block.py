919879b404b4f055bf6e06b9282e6f7ad975e5b8
samskalicky <samskalicky@gmail.com>
2020-02-04 02:32:30 +0000

    fixed whitespace

--- python/mxnet/gluon/block.py
+++ python/mxnet/gluon/block.py
@@ -987,7 +987,7 @@ class HybridBlock(Block):
             ctx = args[0].context
             # get list of params in the order of out.list_arguments
             arg_array = [args[data_names[name]] if name in data_names.keys() else params[name].data()
-                              for name in out.list_arguments()]
+                         for name in out.list_arguments()]
             # Partition the graph.
             out = out.optimize_for(self._backend, arg_array, ctx, **self._backend_args)
 
