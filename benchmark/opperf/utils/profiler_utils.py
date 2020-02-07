171dae06f123e5e2401fcc94c1ba63aeaa43c4f7
Connor Goggins <cgoggins0@gmail.com>
2020-02-07 13:03:20 -0800

    Fixed missing and incorrect alias issues

--- benchmark/opperf/utils/profiler_utils.py
+++ benchmark/opperf/utils/profiler_utils.py
@@ -48,9 +48,9 @@ def _get_operator_profile(operator_name, operator_profile_results):
     # alias map : dictionary of the form {"alias" : "registered_name"}
     # allows to retrieve alias operator profile from the profiler results
     # TODO handling - "identity" : "_copy"
-    alias_map = {"broadcast_plus": "broadcast_add", "broadcast_minus": "broadcast_sub", "flatten": "Flatten", "max_axis": "max", "Custom": "DeleteVariable",
-                 "swapaxes": "SwapAxis", "flip": "reverse", "reshape": "Reshape", "crop": "slice", "sum_axis": "sum", "min_axis": "min", "CTCLoss": "ctc_loss",
-                 "fill_element_0index": "TernaryOp"}
+    alias_map = {"broadcast_plus": "broadcast_add", "broadcast_minus": "broadcast_sub", "flatten": "Flatten", "max_axis": "max", "Custom": "SetValueOp",
+                 "swapaxes": "SwapAxis", "flip": "reverse", "reshape": "Reshape", "crop": "slice", "sum_axis": "sum", "min_axis": "min", "ctc_loss": "CTCLoss",
+                 "fill_element_0index": "TernaryOp", "identity": "_copy", "ElementWiseSum": "add_n"}
 
     op_name = None
 
