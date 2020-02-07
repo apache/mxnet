26632aec2fec0b17d680ac12130d9832f788bbef
Hao Jin <hjjn.amzn@gmail.com>
2020-02-06 22:08:51 -0800

    add np.amin (#17538)

--- src/operator/numpy/np_broadcast_reduce_op_value.cc
+++ src/operator/numpy/np_broadcast_reduce_op_value.cc
@@ -190,6 +190,7 @@ NNVM_REGISTER_OP(_backward_np_max)
 .set_attr<FCompute>("FCompute<cpu>", NumpyReduceAxesNoDTypeBackward<cpu, mshadow_op::eq>);
 
 NNVM_REGISTER_OP(_np_min)
+.add_alias("_np_amin")
 .describe(R"code()code" ADD_FILELINE)
 .set_num_inputs(1)
 .set_num_outputs(1)
