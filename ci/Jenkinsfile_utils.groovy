67070a31315757467a3e967cf58af78ba643b8d7
ChaiBapchya <chai.bapat@gmail.com>
2020-02-14 13:40:10 -0800

    move error to console rather than system.err

--- ci/Jenkinsfile_utils.groovy
+++ ci/Jenkinsfile_utils.groovy
@@ -278,7 +278,7 @@ def main_wrapper(args) {
     currentBuild.result = "SUCCESS"
     update_github_commit_status('SUCCESS', 'Job succeeded')
   } catch (caughtError) {
-    caughtError.printStackTrace();  
+    caughtError.printStackTrace(System.out);
     node(NODE_UTILITY) {
       echo "caught ${caughtError}"
       err = caughtError
