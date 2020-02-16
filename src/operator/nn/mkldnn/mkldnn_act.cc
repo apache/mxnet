ec5d4527a81dc60fba181fdfcb31d962d418705a
Tao Lv <tao.a.lv@intel.com>
2020-02-16 15:26:40 +0800

    fix activation backward

--- src/operator/nn/mkldnn/mkldnn_act.cc
+++ src/operator/nn/mkldnn/mkldnn_act.cc
@@ -244,26 +244,31 @@ void MKLDNNActivationBackward(const nnvm::NodeAttrs &attrs, const OpContext &ctx
   const NDArray &in_grad = outputs[0];
   MKLDNNActParam param_;
   param_.alg = GetMKLDNNActAlgo(param);
-  TmpMemMgr::Get()->Init(ctx.requested[activation::kTempSpace]);
+  // TmpMemMgr::Get()->Init(ctx.requested[activation::kTempSpace]);
   auto diff_dst_memory = out_buffer.GetMKLDNNData();
   auto input_mem = in_buffer.GetMKLDNNData();
   // We need to make sure the two inputs to eltwise_backward has the same memory
   // descriptor. Otherwise, the perf will suffer.
-  if (input_mem->get_desc() != diff_dst_memory->get_desc())
+  if (input_mem->get_desc() != diff_dst_memory->get_desc()) {
     input_mem = in_buffer.GetMKLDNNDataReorder(diff_dst_memory->get_desc());
-  MKLDNNActBackward &bwd =
-      GetActBackward(param_, ctx, in_buffer, out_buffer, *input_mem);
+  }
+
+  MKLDNNActBackward &bwd = GetActBackward(param_, ctx, in_buffer, out_buffer, *input_mem);
   MKLDNNStream *stream = MKLDNNStream::Get();
-  mkldnn_output_t diff_src_memory =
-      CreateMKLDNNMem(in_grad, bwd.bwd_pd.diff_src_desc(), req[0]);
-  mkldnn_args_map_t args = {
-    { MKLDNN_ARG_SRC, *input_mem },
-    { MKLDNN_ARG_DIFF_DST, *diff_dst_memory },
-    { MKLDNN_ARG_DIFF_SRC, *diff_src_memory.second },
-  };
-  stream->RegisterPrimArgs(bwd.GetBwd(), args);
-  CommitOutput(in_grad, diff_src_memory);
-  stream->Submit();
+  mkldnn_args_map_t args = {{MKLDNN_ARG_SRC, *input_mem},
+                            {MKLDNN_ARG_DIFF_DST, *diff_dst_memory}};
+  if (req[0] != kAddTo) {
+    auto diff_src_memory = in_grad.GetMKLDNNData(bwd.bwd_pd.diff_src_desc());
+    args.insert({MKLDNN_ARG_DIFF_SRC, *diff_src_memory});
+    stream->RegisterPrimArgs(bwd.GetBwd(), args);
+    stream->Submit();
+  } else {
+    auto diff_src_memory = CreateMKLDNNMem(in_grad, bwd.bwd_pd.diff_src_desc(), req[0]);
+    args.insert({MKLDNN_ARG_DIFF_SRC, *diff_src_memory.second});
+    stream->RegisterPrimArgs(bwd.GetBwd(), args);
+    CommitOutput(in_grad, diff_src_memory);
+    stream->Submit();
+  }
 }
 
 void MKLDNNLeakyReluBackward(const nnvm::NodeAttrs& attrs,
