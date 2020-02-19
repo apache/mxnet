d2e718c192bbc5a7d750bf0c908765a9ce72a803
samskalicky <samskalicky@gmail.com>
2020-02-19 01:09:54 +0000

    fixed whitespace

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -728,12 +728,12 @@ int MXLoadLib(const char *path) {
                                 const std::vector<OpReqType>& req,
                                 const std::vector<NDArray>& outputs) {
         if (ctx.run_ctx.ctx.dev_mask() == Context::kCPU) {
-          CHECK(forward_ctx_map.count("cpu") > 0);
+          CHECK_GT(forward_ctx_map.count("cpu"), 0);
           fcomp_t fcomp = forward_ctx_map.at("cpu");
           CustomFComputeDispatcher(name_str, callFComp, fcomp, &attrs,
                                    nullptr, 0, nullptr, ctx, inputs, req, outputs);
         } else if (ctx.run_ctx.ctx.dev_mask() == Context::kGPU) {
-          CHECK(forward_ctx_map.count("gpu") > 0);
+          CHECK_GT(forward_ctx_map.count("gpu"), 0);
           fcomp_t fcomp = forward_ctx_map.at("gpu");
           CustomFComputeDispatcher(name_str, callFComp, fcomp, &attrs,
                                    nullptr, 0, nullptr, ctx, inputs, req, outputs);
