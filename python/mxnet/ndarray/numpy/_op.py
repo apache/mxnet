c493a5adb70b0b2a5a708e27aa9457c84d9e09d1
JiangZhaoh <jiang_zhaoh@163.com>
2020-02-25 11:31:15 +0000

    consistent with FFI

--- python/mxnet/ndarray/numpy/_op.py
+++ python/mxnet/ndarray/numpy/_op.py
@@ -123,8 +123,8 @@ def zeros(shape, dtype=None, order='C', ctx=None):  # pylint: disable=redefined-
         ctx = str(current_context())
     else:
         ctx = str(ctx)
-    if dtype is None:
-        dtype = _np.float64 if is_np_default_dtype() else _np.float32
+    if dtype is not None and not isinstance(dtype, str):
+        dtype = _np.dtype(dtype).name
     return _api_internal.zeros(shape, dtype, ctx)
 
 
