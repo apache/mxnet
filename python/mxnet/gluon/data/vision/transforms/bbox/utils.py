56bb1c5ee7a55e3d0ec484aac3bd61d92c88439b
Joshua Z. Zhang <cheungchih@gmail.com>
2020-03-10 14:42:05 -0700

    add more bbox util funcs

--- python/mxnet/gluon/data/vision/transforms/bbox/utils.py
+++ python/mxnet/gluon/data/vision/transforms/bbox/utils.py
@@ -77,6 +77,106 @@ def bbox_crop(bbox, crop_box=None, allow_outside_center=True):
     bbox = bbox[mask]
     return bbox
 
+def bbox_flip(bbox, size, flip_x=False, flip_y=False):
+    """Flip bounding boxes according to image flipping directions.
+
+    Parameters
+    ----------
+    bbox : numpy.ndarray
+        Numpy.ndarray with shape (N, 4+) where N is the number of bounding boxes.
+        The second axis represents attributes of the bounding box.
+        Specifically, these are :math:`(x_{min}, y_{min}, x_{max}, y_{max})`,
+        we allow additional attributes other than coordinates, which stay intact
+        during bounding box transformations.
+    size : tuple
+        Tuple of length 2: (width, height).
+    flip_x : bool
+        Whether flip horizontally.
+    flip_y : bool
+        Whether flip vertically.
+
+    Returns
+    -------
+    numpy.ndarray
+        Flipped bounding boxes with original shape.
+    """
+    if not len(size) == 2:
+        raise ValueError("size requires length 2 tuple, given {}".format(len(size)))
+    width, height = size
+    bbox = bbox.copy()
+    if flip_y:
+        ymax = height - bbox[:, 1]
+        ymin = height - bbox[:, 3]
+        bbox[:, 1] = ymin
+        bbox[:, 3] = ymax
+    if flip_x:
+        xmax = width - bbox[:, 0]
+        xmin = width - bbox[:, 2]
+        bbox[:, 0] = xmin
+        bbox[:, 2] = xmax
+    return bbox
+
+def bbox_resize(bbox, in_size, out_size):
+    """Resize bouding boxes according to image resize operation.
+
+    Parameters
+    ----------
+    bbox : numpy.ndarray
+        Numpy.ndarray with shape (N, 4+) where N is the number of bounding boxes.
+        The second axis represents attributes of the bounding box.
+        Specifically, these are :math:`(x_{min}, y_{min}, x_{max}, y_{max})`,
+        we allow additional attributes other than coordinates, which stay intact
+        during bounding box transformations.
+    in_size : tuple
+        Tuple of length 2: (width, height) for input.
+    out_size : tuple
+        Tuple of length 2: (width, height) for output.
+
+    Returns
+    -------
+    numpy.ndarray
+        Resized bounding boxes with original shape.
+    """
+    if not len(in_size) == 2:
+        raise ValueError("in_size requires length 2 tuple, given {}".format(len(in_size)))
+    if not len(out_size) == 2:
+        raise ValueError("out_size requires length 2 tuple, given {}".format(len(out_size)))
+
+    bbox = bbox.copy().astype(float)
+    x_scale = out_size[0] / in_size[0]
+    y_scale = out_size[1] / in_size[1]
+    bbox[:, 1] = y_scale * bbox[:, 1]
+    bbox[:, 3] = y_scale * bbox[:, 3]
+    bbox[:, 0] = x_scale * bbox[:, 0]
+    bbox[:, 2] = x_scale * bbox[:, 2]
+    return bbox
+
+def bbox_translate(bbox, x_offset=0, y_offset=0):
+    """Translate bounding boxes by offsets.
+
+    Parameters
+    ----------
+    bbox : numpy.ndarray
+        Numpy.ndarray with shape (N, 4+) where N is the number of bounding boxes.
+        The second axis represents attributes of the bounding box.
+        Specifically, these are :math:`(x_{min}, y_{min}, x_{max}, y_{max})`,
+        we allow additional attributes other than coordinates, which stay intact
+        during bounding box transformations.
+    x_offset : int or float
+        Offset along x axis.
+    y_offset : int or float
+        Offset along y axis.
+
+    Returns
+    -------
+    numpy.ndarray
+        Translated bounding boxes with original shape.
+    """
+    bbox = bbox.copy()
+    bbox[:, :2] += (x_offset, y_offset)
+    bbox[:, 2:4] += (x_offset, y_offset)
+    return bbox
+
 def bbox_iou(bbox_a, bbox_b, offset=0):
     """Calculate Intersection-Over-Union(IOU) of two bounding boxes.
 
