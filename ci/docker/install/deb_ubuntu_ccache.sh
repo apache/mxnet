c1dc4ab9aebfdc3a9edcb825a560263788eb5dd2
Leonard Lausen <lausen@amazon.com>
2020-03-16 03:36:04 +0000

    Set -mno-avx instead as libzstd in package archives is too old

--- ci/docker/install/deb_ubuntu_ccache.sh
+++ ci/docker/install/deb_ubuntu_ccache.sh
@@ -23,17 +23,8 @@ set -ex
 
 pushd .
 
-dist=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-if [ $dist == "Ubuntu" ]; then
-    zstd="libzstd1-dev"
-else  # Debian
-    zstd="libzstd-dev"
-fi
-
 apt update || true
 apt install -y \
-    $zstd \
-    libb2-dev \
     autoconf \
     xsltproc
 
@@ -63,8 +54,11 @@ cd ccache
 # ccache 4 contains fixes for caching nvcc output: https://github.com/ccache/ccache/pull/381
 git checkout 2e7154e67a5dd56852dae29d4c418d4ddc07c230
 
+
+export CFLAGS="-mno-avx"
+export CXXFLAGS="-mno-avx"
 ./autogen.sh
-./configure --disable-man
+./configure --disable-man --with-libzstd-from-internet --with-libb2-from-internet
 make -j$(nproc)
 make install
 
