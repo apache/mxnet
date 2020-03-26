70619b08d2ff3db42e2a6738823a0ab44b6f0995
Lin Yuan <apeforest@gmail.com>
2020-03-26 01:03:36 -0700

    Fix error in test script

--- tests/nightly/test_distributed_training-gpu.sh
+++ tests/nightly/test_distributed_training-gpu.sh
@@ -22,16 +22,23 @@ export MXNET_STORAGE_FALLBACK_LOG_VERBOSE=0
 export MXNET_SUBGRAPH_VERBOSE=0
 export DMLC_LOG_STACK_TRACE_DEPTH=10
 
-test_args=(
-    "-n 4 --launcher local python3 dist_device_sync_kvstore.py"
-    "-n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
-    "--p3 -n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
-    "-n 4 --launcher local python3 dist_sync_kvstore.py --type=init_gpu" 
-)
+test_kvstore() {
+    test_args=(
+        "-n 4 --launcher local python3 dist_device_sync_kvstore.py"
+        "-n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
+        "--p3 -n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
+        "-n 4 --launcher local python3 dist_sync_kvstore.py --type=init_gpu" 
+    )
 
-for arg in "${test_args[@]}"; do
-    python3 ../../tools/launch.py "$arg"
-    if [ $? -ne 0 ]; then
-        return $?
-    fi 
-done
+    for arg in "${test_args[@]}"; do
+        echo $arg
+        python3 ../../tools/launch.py $arg
+        if [ $? -ne 0 ]; then
+            return $?
+        fi 
+    done
+}
+
+test_kvstore
+
+exit $errors
