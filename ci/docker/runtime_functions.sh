5d28a1b5c497c902de70c302aaaf707591c7b79d
Lin Yuan <apeforest@gmail.com>
2020-03-25 22:46:03 -0700

    Add check for error

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -1341,17 +1341,7 @@ integrationtest_ubuntu_gpu_scala() {
 
 integrationtest_ubuntu_gpu_dist_kvstore() {
     set -ex
-    pushd .
-    export PYTHONPATH=./python/
-    export MXNET_STORAGE_FALLBACK_LOG_VERBOSE=0
-    export MXNET_SUBGRAPH_VERBOSE=0
-    export DMLC_LOG_STACK_TRACE_DEPTH=10
-    cd tests/nightly/
-    python3 ../../tools/launch.py -n 4 --launcher local python3 dist_device_sync_kvstore.py
-    python3 ../../tools/launch.py -n 4 --launcher local python3 dist_device_sync_kvstore_custom.py
-    python3 ../../tools/launch.py --p3 -n 4 --launcher local python3 dist_device_sync_kvstore_custom.py
-    python3 ../../tools/launch.py -n 4 --launcher local python3 dist_sync_kvstore.py --type=init_gpu
-    popd
+    ./test_distributed_training-gpu.sh
 }
 
 test_ubuntu_cpu_python3() {
---
+++ tests/nightly/test_distributed_training-gpu.sh
@@ -0,0 +1,23 @@
+pushd .
+export PYTHONPATH=./python/
+export MXNET_STORAGE_FALLBACK_LOG_VERBOSE=0
+export MXNET_SUBGRAPH_VERBOSE=0
+export DMLC_LOG_STACK_TRACE_DEPTH=10
+cd tests/nightly/
+
+test_args=(
+    "-n 4 --launcher local python3 dist_device_sync_kvstore.py"
+    "-n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
+    "--p3 -n 4 --launcher local python3 dist_device_sync_kvstore_custom.py"
+    "-n 4 --launcher local python3 dist_sync_kvstore.py --type=init_gpu" 
+)
+
+for arg in "${test_args[@]}"; do
+    echo "$i"
+    python3 ../../tools/launch.py "$arg"
+    if [ $? -ne 0 ]; then
+        return $?
+    fi 
+done
+
+popd
