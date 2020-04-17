Another fix for broadcast and fix INT64 compilation

tests/python/unittest/test_operator.py
@@ -9937,6 +9937,7 @@ def test_elemwise_sum_for_gradient_accumulation():
         assert stored_grad['write'] == stored_grad['add']
         assert stored_grad['write'] == 2 * nrepeat
 
+@with_seed()
 def test_elementwise_ops_on_misaligned_input():
     a = mx.nd.array([1,2,3,4], dtype='float16')
     b = mx.nd.array([1,2,3,4], dtype='float16')
@@ -9957,29 +9958,63 @@ def test_elementwise_ops_on_misaligned_input():
     mx.nd.waitall()
     assert a[3].asscalar() == 4.0
 
+@with_seed()
 def test_broadcast_ops_on_misaligned_input():
-    a = mx.nd.array([1,2,3,4,5,6,7,8])
-    b = mx.nd.array([1,2,3,4,5,6,7,8])
-    c = a[1:7].reshape((3,2))
-    d = b[1:3]
-    e = mx.nd.arange(7)
-    f = e[1:].reshape((3,2))
-    mx.nd.broadcast_add(c, d, out=f)
-    expected = np.array([[4,6],[6,8],[8,10]])
-    mx.nd.waitall()
-    assert_almost_equal(f, expected)
+    dtypes = ['float16', 'float32', 'float64']
+    lead_dims = [2,4,6,10]
 
-    a = mx.nd.array([1,2,3,4,5,6,7,8])
-    b = mx.nd.array([1,2,3,4,5,6,7,8])
+    for dtype in dtypes:
+        for lead_dim in lead_dims:
+            for both_ways in [False, True]:
+                shape = list(rand_shape_2d()) + [lead_dim]
+                small_shape = [shape[0], 1, lead_dim]
+                if both_ways:
+                    # Broadcast in both ways [1, K, L] x [M, 1, L]
+                    big_shape = [1, shape[1], lead_dim]
+                else:
+                    big_shape = shape
+                size = np.product(shape)
+                small_size = np.product(small_shape)
+                big_size = np.product(big_shape)
+                a = mx.nd.arange(5000)
+                b = mx.nd.arange(5000)
+                e = mx.nd.arange(5000)
+                c = a[1:big_size + 1].reshape(big_shape)
+                d = b[1:small_size + 1].reshape(small_shape)
+                f = e[1:size + 1].reshape(shape)
+                mx.nd.broadcast_add(c, d, out=f)
+                expected = c.asnumpy() + d.asnumpy()
+                mx.nd.waitall()
+                assert_almost_equal(f, expected)
 
-    c = a[1:7].reshape((3,2))
-    d = b[1:4].reshape((3,1))
-    e = mx.nd.arange(7)
-    f = e[1:].reshape((3,2))
-    mx.nd.broadcast_add(c, d, out=f)
-    expected = np.array([[4,5],[7,8],[10,11]])
-    mx.nd.waitall()
-    assert_almost_equal(f, expected)
+@with_seed()
+def test_broadcast_ops_on_misaligned_input_oneside():
+    dtypes = ['float16', 'float32', 'float64']
+    lead_dims = [2,4,6,10]
+
+    for dtype in dtypes:
+        for lead_dim in lead_dims:
+            for both_ways in [False, True]:
+                shape = list(rand_shape_2d()) + [lead_dim]
+                small_shape = [shape[0], shape[1], 1]
+                if both_ways:
+                    # Broadcast in both ways [1, K, L] x [M, 1, 1]
+                    big_shape = [1, shape[1], lead_dim]
+                else:
+                    big_shape = shape
+                size = np.product(shape)
+                small_size = np.product(small_shape)
+                big_size = np.product(big_shape)
+                a = mx.nd.arange(5000)
+                b = mx.nd.arange(5000)
+                e = mx.nd.arange(5000)
+                c = a[1:big_size + 1].reshape(big_shape)
+                d = b[1:small_size + 1].reshape(small_shape)
+                f = e[1:size + 1].reshape(shape)
+                mx.nd.broadcast_add(c, d, out=f)
+                expected = c.asnumpy() + d.asnumpy()
+                mx.nd.waitall()
+                assert_almost_equal(f, expected)
 
 if __name__ == '__main__':
     import nose