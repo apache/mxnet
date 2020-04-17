5542d03695b4a2589afb88acf128d4ba8ac94d0d
Przemyslaw Tredak <ptredak@nvidia.com>
2020-04-17 16:33:52 -0700

    Fix and optimize handling of vectorized memory accesses (#17767)
    * Vectorized loads for binary elemwise kernel
    
    * More generalization
    
    * Add backwardusenone
    
    * Remove the unused _backward_add op
    
    * Add vectorized backwardusein
    
    * Extending vectorization to more binary ops, binary ops with scalar and
    unary ops
    
    * Handling ElementwiseSum
    
    * Get rid of half2 in mshadow
    
    * Remove backward_elemwiseaddex
    
    * Revert "Remove the unused _backward_add op"
    
    This reverts commit f86da86f809c8cbad07db76a3554f23890fe05a3.
    
    * Revert "Remove backward_elemwiseaddex"
    
    This reverts commit 7729114caf6a1718c08ce1f35529d2267057d515.
    
    * Add back the backward_add since C++ test relies on it
    
    * Test bcast implementations
    
    * First version of vecotrized bcast
    
    * Adding single side vectorized bcast kernel
    
    * Removing debug prints
    
    * Actually run the single side kernel
    
    * Move the default implementation of bcast to the vectorized one
    
    * Limit the new implementation to GPU only
    
    * Enabling vectorization when broadcast does not actually do broadcast
    
    * Cleaning
    
    * Cleaning part 2
    
    * Fix for numpy ops using stuff from broadcast
    
    * Fix
    
    * Fix lint
    
    * Try to debug pinv numpy test
    
    * Fix
    
    * Fix the vectorized broadcast implementation for misaligned input
    pointers
    
    * Added tests
    
    * Added docs to cuda_vectorization.cuh
    
    * Another fix for broadcast and fix INT64 compilation
    
    * Optimize for aligned=true
    
    * 1 more addition to test
    
    * Reverting the change to Numpy op test
    
    * Trying mcmodel=medium to fix the failure in CMake static build
    
    * Revert "Trying mcmodel=medium to fix the failure in CMake static build"
    
    This reverts commit 1af684c507dd5b2c7ab7ffe89d21799320e3d9c6.
    
    * Limiting the PR to just elementwise ops

--- src/operator/mshadow_op.h
+++ src/operator/mshadow_op.h
@@ -742,21 +742,7 @@ MXNET_BINARY_MATH_OP(rminus, b - a);
 
 MXNET_BINARY_MATH_OP(div_grad, 1.0f / math::id(b));
 
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t div_grad::Map<mshadow::half::half2_t>
-                                               (mshadow::half::half2_t a,
-                                                mshadow::half::half2_t b) {
-  return mshadow::half::half2_t(1) / b;
-}
-
 MXNET_BINARY_MATH_OP(div_rgrad, -math::id(a) / math::sqr(b));
-
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t div_rgrad::Map<mshadow::half::half2_t>
-                                               (mshadow::half::half2_t a,
-                                                mshadow::half::half2_t b) {
-  return -a / (b * b);
-}
 
 MXNET_BINARY_MATH_OP(rdiv, math::id(b) / math::id(a));
 
@@ -829,13 +815,6 @@ struct rfmod : public mxnet_op::tunable {
   }
 };
 
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t mod::Map<mshadow::half::half2_t>
-                                               (mshadow::half::half2_t a,
-                                                mshadow::half::half2_t b) {
-  return a%b;
-}
-
 struct mod_grad : public mxnet_op::tunable  {
   template<typename DType>
   MSHADOW_XINLINE static DType Map(DType a, DType b) {
@@ -856,19 +835,6 @@ MSHADOW_XINLINE mshadow::half::half_t mod_grad::Map<mshadow::half::half_t>
                                                    (mshadow::half::half_t a,
                                                     mshadow::half::half_t b) {
   return mshadow::half::half_t(1.0f);
-}
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t mod_grad::Map<mshadow::half::half2_t>
-                                                    (mshadow::half::half2_t a,
-                                                     mshadow::half::half2_t b) {
-  mshadow::half::half2_t result = mshadow::half::half2_t();
-#if (defined(__CUDACC__) && MSHADOW_CUDA_HALF2)
-  result.half2_ = ::__float2half2_rn(1.0f);
-#else
-  result.half_t2[0] = mshadow::half::half_t(0.0f);
-  result.half_t2[1] = mshadow::half::half_t(1.0f);
-#endif
-  return result;
 }
 
 struct mod_rgrad : public mxnet_op::tunable {
@@ -891,19 +857,6 @@ MSHADOW_XINLINE mshadow::half::half_t mod_rgrad::Map<mshadow::half::half_t>
                                                     (mshadow::half::half_t a,
                                                      mshadow::half::half_t b) {
   return mshadow::half::half_t(-::floorf(static_cast<float>(a/b)));
-}
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t mod_rgrad::Map<mshadow::half::half2_t>
-                                                     (mshadow::half::half2_t a,
-                                                      mshadow::half::half2_t b) {
-#if (defined(__CUDACC__) && MSHADOW_CUDA_HALF2)
-  return mshadow::half::half2_t(__hneg2(::h2floor((a/b).half2_)));
-#else
-  return mshadow::half::half2_t(mshadow::half::half_t(-::floorf(
-                                  static_cast<float>(a.half_t2[0]/b.half_t2[0]))),
-                                mshadow::half::half_t(-::floorf(
-                                  static_cast<float>(a.half_t2[1]/b.half_t2[1]))));
-#endif
 }
 
 struct rmod : public mxnet_op::tunable {
@@ -941,13 +894,6 @@ struct rmod : public mxnet_op::tunable {
   }
 };
 
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t rmod::Map<mshadow::half::half2_t>
-                                                (mshadow::half::half2_t a,
-                                                 mshadow::half::half2_t b) {
-  return b%a;
-}
-
 struct rmod_grad {
   template<typename DType>
   MSHADOW_XINLINE static DType Map(DType a, DType b) {
@@ -968,19 +914,6 @@ MSHADOW_XINLINE mshadow::half::half_t rmod_grad::Map<mshadow::half::half_t>
                                                    (mshadow::half::half_t a,
                                                     mshadow::half::half_t b) {
   return mshadow::half::half_t(-::floorf(static_cast<float>(b/a)));
-}
-template<>
-MSHADOW_XINLINE mshadow::half::half2_t rmod_grad::Map<mshadow::half::half2_t>
-                                                     (mshadow::half::half2_t a,
-                                                      mshadow::half::half2_t b) {
-#if (defined(__CUDACC__) && MSHADOW_CUDA_HALF2)
-  return mshadow::half::half2_t(::__hneg2(::h2floor((b/a).half2_)));
-#else
-  return mshadow::half::half2_t(mshadow::half::half_t(-::floorf(
-                                  static_cast<float>(b.half_t2[0]/a.half_t2[0]))),
-                                mshadow::half::half_t(-::floorf(
-                                  static_cast<float>(b.half_t2[1]/a.half_t2[1]))));
-#endif
 }
 
 struct clip : public mxnet_op::tunable {
