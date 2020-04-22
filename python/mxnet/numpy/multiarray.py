462b7b6303e671be056700ca23658388b10c411c
cassiniXu <cassini.Xu@outlook.com>
2020-04-22 20:13:01 +0800

    Fix positional error

--- python/mxnet/numpy/multiarray.py
+++ python/mxnet/numpy/multiarray.py
@@ -10797,7 +10797,7 @@ def pad(x, pad_width=None, mode="constant", **kwargs): # pylint: disable=too-man
 
 # pylint: disable=redefined-outer-name
 @set_module('mxnet.numpy')
-def prod(a, axis=None, dtype=None, out=None, keepdims=False, initial=None): # pylint: disable=too-many-arguments
+def prod(a, axis=None, dtype=None, keepdims=False, initial=None, out=None): # pylint: disable=too-many-arguments
     """
     Return the product of array elements over a given axis.
 
