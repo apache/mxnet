7a86240bfea4f2b2428ce64f95aff8c11b4f024a
Lai Wei <royweilai@gmail.com>
2020-04-22 22:49:51 -0700

    Revert "Fix cudnn Dropout reproducibility (#17547)"
    This reverts commit 249b9a13002cf0ee1d3d530e31b69b15aae433bb.

--- src/operator/nn/dropout.cc
+++ src/operator/nn/dropout.cc
@@ -157,7 +157,6 @@ Example::
           && !(param.cudnn_off && param.cudnn_off.value())
           && param.axes.ndim() == 0) {
         request.emplace_back(ResourceRequest::kCuDNNDropoutDesc);
-        request.emplace_back(ResourceRequest::kRandom);
         return request;
       }
 #endif
