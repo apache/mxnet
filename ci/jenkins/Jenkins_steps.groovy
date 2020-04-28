2e7e5fa44efbf39995810cb0c4a27367332e740c
Ubuntu <ubuntu@ip-172-31-8-137.us-east-2.compute.internal>
2020-04-28 05:11:00 +0000

    remove comments

--- ci/jenkins/Jenkins_steps.groovy
+++ ci/jenkins/Jenkins_steps.groovy
@@ -566,9 +566,6 @@ def compile_unix_asan_cpu() {
         ws('workspace/build-cpu-asan') {
           timeout(time: max_time, unit: 'MINUTES') {
             utils.init_git()
-            // TODO(leezu) ubuntu_nightly_cpu temporarily used ASAN tests fail
-            // with Ubuntu 18.04 used on ubuntu_cpu image. ubuntu_nightly_cpu
-            // still uses Ubuntu 16.04
             utils.docker_run('ubuntu_cpu', 'build_ubuntu_cpu_cmake_asan', false)
             utils.pack_lib('cpu_asan', mx_lib_cpp_examples_cpu)
           }
@@ -1722,9 +1719,6 @@ def misc_asan_cpu() {
       node(NODE_LINUX_CPU) {
         ws('workspace/ut-python3-cpu-asan') {
             utils.unpack_and_init('cpu_asan', mx_lib_cpp_examples_cpu)
-            // TODO(leezu) ubuntu_nightly_cpu temporarily used ASAN tests fail
-            // with Ubuntu 18.04 used on ubuntu_cpu image. ubuntu_nightly_cpu
-            // still uses Ubuntu 16.04
             utils.docker_run('ubuntu_cpu', 'integrationtest_ubuntu_cpu_asan', false)
         }
       }
