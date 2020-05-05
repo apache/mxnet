5a47a6547649476bd0e3352983bda4fff102050c
ys2843 <ys2843@nyu.edu>
2020-05-05 14:30:31 -0700

    add hover effect to icon and refactor js

--- docs/static_site/src/assets/js/globalSearch.js
+++ docs/static_site/src/assets/js/globalSearch.js
@@ -1,7 +1,26 @@
-docsearch({
-    apiKey: '500f8e78748bd043cc6e4ac130e8c0e7',
-    indexName: 'apache_mxnet',
-    inputSelector: '#global-search',
-    algoliaOptions: { 'facetFilters': ["version:master"] },
-    debug: true// Set debug to true if you want to inspect the dropdown
+$(document).ready(function () {
+    docsearch({
+        apiKey: '500f8e78748bd043cc6e4ac130e8c0e7',
+        indexName: 'apache_mxnet',
+        inputSelector: '#global-search',
+        algoliaOptions: { 'facetFilters': ["version:master"] },
+        debug: true// Set debug to true if you want to inspect the dropdown
+    });
+    
+    $("#search-icon").hover(function () {
+        $(".trigger").hide();
+        $("#global-search-form").css("display", "inline-block");
+        $("#global-search").animate({
+            width: "300px"
+        }).focus();
+    });
+
+    $("#global-search").blur(function () {
+        $(this).animate({
+            width: "0px"
+        }, function () {
+            $("#global-search-form").hide();
+            $(".trigger").show();
+        });
+    });
 });
