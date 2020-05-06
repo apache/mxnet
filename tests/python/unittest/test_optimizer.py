35916f10cd5c768de79c4647662fa1dbc5542c19
Shuai Zheng <szhengac@users.noreply.github.com>
2020-05-05 20:40:36 -0700

    relax alto rtol for ftrl test when dtype is fp16 (#18226)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-92-136.ec2.internal>

--- tests/python/unittest/test_optimizer.py
+++ tests/python/unittest/test_optimizer.py
@@ -690,9 +690,10 @@ def test_ftrl():
             if (dtype == np.float16 and
                     ('multi_precision' not in kwarg or not kwarg['multi_precision'])):
                 continue
+            rtol, atol = (1e-3, 1e-3) if dtype is np.float16 else (1e-4, 1e-4)
             compare_optimizer(opt1(use_fused_step=False, **kwarg),
                               opt2(use_fused_step=True, **kwarg), shapes, dtype,
-                              rtol=1e-4, atol=1e-4)
+                              rtol=rtol, atol=atol)
 
 
 @with_seed()
