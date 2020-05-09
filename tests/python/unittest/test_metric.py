ef3058adba8353fd8fa81b39264b543d64e0dac7
acphile <phile_999@126.com>
2020-05-09 14:59:04 +0800

    remove nose

--- tests/python/unittest/test_metric.py
+++ tests/python/unittest/test_metric.py
@@ -384,6 +384,3 @@ def test_single_array_input():
     _, rmse_res = rmse.get()
     np.testing.assert_almost_equal(rmse_res, 0.1)
 
-if __name__ == '__main__':
-    import nose
-    nose.runmodule()
