3e676fc2c88bec75e4463c8fa9b5532664d518c2
Leonard Lausen <lausen@amazon.com>
2020-05-15 10:00:56 -0700

    Fix memory leaks in Gluon (#18328)
    Fix leak of ndarray objects in the frontend due to reference cycle.

--- tests/python/unittest/test_thread_local.py
+++ tests/python/unittest/test_thread_local.py
@@ -125,8 +125,9 @@ def test_blockscope():
     status = [False]
     event = threading.Event()
     def f():
-        with block._BlockScope(dummy_block("spawned_")):
-            x= NameManager.current.get(None, "hello")
+        net = dummy_block("spawned_")  # BlockScope only keeps a weakref to the Block
+        with block._BlockScope(net):
+            x = NameManager.current.get(None, "hello")
             event.wait()
             if x == "spawned_hello0":
                 status[0] = True
