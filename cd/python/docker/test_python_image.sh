0b5bdf79918ff50ff875e6d7bfe7e03dca8be5aa
Manu Seth <sethman@amazon.com>
2020-05-17 09:23:40 +0000

    run mkldnn test for cu* variants

--- cd/python/docker/test_python_image.sh
+++ cd/python/docker/test_python_image.sh
@@ -37,7 +37,7 @@ if [[ $mxnet_variant == cu* ]]; then
     test_conv_params="--gpu"
 fi
 
-if [[ $mxnet_variant == cpu ]]; then
+if [[ $mxnet_variant != native ]]; then
     python3 tests/python/mkl/test_mkldnn.py
 fi
 
