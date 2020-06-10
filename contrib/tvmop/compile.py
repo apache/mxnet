46256d4897ed6ecdb6dbc2f5a0093547104f39f7
Ubuntu <ubuntu@ip-172-31-37-194.ap-northeast-1.compute.internal>
2020-06-10 02:33:25 +0000

    fixing bugs

--- contrib/tvmop/compile.py
+++ contrib/tvmop/compile.py
@@ -152,6 +152,10 @@ if __name__ == "__main__":
     # we create libtvmop.o first, which gives us chance to link tvm_runtime together with the libtvmop
     # to allow mxnet find external helper functions in libtvm_runtime
     func_binary.save(arguments.target_path + "/libtvmop.o")
+    try:
+        imported_modules
+    except NameError:
+        imported_modules = []
     if len(imported_modules):
         func_binary.imported_modules[0].save(arguments.target_path + "/libtvmop.cubin")
     ld_path = arguments.target_path if arguments.ld_path is None else arguments.ld_path
