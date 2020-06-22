63df50d8f67e0a2fe41ee11541dbaba32b1daebf
Yang Shi <yangshia@amazon.com>
2020-06-22 11:31:17 -0700

    make lang getter more extensible

--- docs/static_site/src/assets/js/copycode.js
+++ docs/static_site/src/assets/js/copycode.js
@@ -20,7 +20,7 @@
 /* Copy code to clipboard */
 
 $(document).ready(function () {
-  // Omitted prompts in Regex for each language
+  // Regex of prompts to be omitted when copy
   const LANG_GP = {
     default: [">>> ", "\\.\\.\\."],
     python: [">>> ", "\\.\\.\\."],
@@ -32,6 +32,16 @@ $(document).ready(function () {
     cpp: [""],
     bash: ["\\$ "],
   };
+
+  /* Functions to get the language of a code block related to a copy button
+   * called one by one until a valid lang is returned 
+   * new callbacks should be added before "default"
+   */
+  const LANG_GETTER = [
+    (copyBtn) => copyBtn.nextElementSibling.children[0].dataset.lang,
+    (copyBtn) => copyBtn.parentNode.parentNode.classList[0].split("-")[1],
+    () => "default",
+  ];
 
   // Append a copy button to each code block on the page
   $("figure.highlight, div.highlight").each(function () {
@@ -58,16 +68,16 @@ $(document).ready(function () {
     return res + "\n";
   };
 
+  const getCodeBlockLang = function (copyBtn, langGetFunc) {
+    return langGetFunc.reduce((res, getter) => res || getter(copyBtn), "");
+  }
+
   const clipboard = new ClipboardJS(".copy-btn", {
     text: function (trigger) {
-      const lang = trigger.nextElementSibling.children[0].dataset.lang ||
-        trigger.parentNode.parentNode.classList[0].split("-")[1] ||
-        "default";
+      const lang = getCodeBlockLang(trigger, LANG_GETTER);
       const langPrompts = LANG_GP[lang] || [];
       const lines = trigger.parentNode.querySelector("code").textContent.split("\n");
-      const cleanedCode = lines.reduce(
-        (content, line) => content.concat(cleanPrompt(line, langPrompts)),
-        "");
+      const cleanedCode = lines.reduce((content, line) => content.concat(cleanPrompt(line, langPrompts)), "");
       return cleanedCode.replace(/\n$/, "");
     },
   });
