cc709ec7efcf1c1e3b3c4c6af38c449ee1f89b98
Jinbo Ci <32102965+jinboci@users.noreply.github.com>
2020-07-08 18:32:00 +0800

    Update c_api.cc

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -1363,11 +1363,7 @@ int MXGetVersion(int *out) {
 #if MXNET_USE_TVM_OP
 int MXLoadTVMOp(const char *libpath) {
   API_BEGIN();
-  tvm::runtime::TVMOpModule *libpath_module =  tvm::runtime::TVMOpModule::Get();
-  libpath_module->Load(libpath);
-  tvm::runtime::TVMOpModule cubin_module;
-  cubin_module.Load("/home/ubuntu/Documents/mxnet/build/libtvmop.cubin");
-  libpath_module->Import(cubin_module);
+  tvm::runtime::TVMOpModule::Get()->Load(libpath);
   API_END();
 }
 
