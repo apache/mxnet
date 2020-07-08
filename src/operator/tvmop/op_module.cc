38d35b733ab595b07f5b2aa07e074aee3574d889
Jinbo Ci <32102965+jinboci@users.noreply.github.com>
2020-07-08 18:17:00 +0800

    Update op_module.cc

--- src/operator/tvmop/op_module.cc
+++ src/operator/tvmop/op_module.cc
@@ -50,7 +50,7 @@ void TVMOpModule::Import(const TVMOpModule& module) {
   CHECK(module_ptr_ != nullptr) << "module_ptr_ is not initialized.";
   std::lock_guard<std::mutex> lock(mutex_);
   module_ptr_->Import(*(module.module_ptr_));
- }
+}
 
 PackedFunc GetFunction(const std::shared_ptr<Module> &module,
                        const std::string &op_name,
