8bd6a649383972a985ce1e8fd7c7cc998c0884a3
Joe Evans <joeev@amazon.com>
2020-09-03 00:41:32 +0000

    Re-enable squeezenet model testings in onnxruntime.

--- tests/python-pytest/onnx/test_onnxruntime.py
+++ tests/python-pytest/onnx/test_onnxruntime.py
@@ -87,7 +87,7 @@ def test_cv_model_inference_onnxruntime():
         'mobilenetv2_1.0', 'mobilenetv2_0.75', 'mobilenetv2_0.5', 'mobilenetv2_0.25',
         'resnet18_v1', 'resnet18_v2', 'resnet34_v1', 'resnet34_v2', 'resnet50_v1', 'resnet50_v2',
         'resnet101_v1', 'resnet101_v2', 'resnet152_v1', 'resnet152_v2',
-        #'squeezenet1.0', 'squeezenet1.1', 
+        'squeezenet1.0', 'squeezenet1.1', 
         'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn'
     ]
     labels = load_imgnet_labels()
