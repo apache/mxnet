bd0846a69addf18529d5dcf856b28bed1a38cdbc
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2020-09-24 13:46:17 -0700

    [BUGFIX] Pip Build: use runtime.Features instead of manual check for mkldnn headers (#19195)
    * use runtime Features instead of manual check
    
    * move mxnet package closer to where its used

--- tools/pip/setup.py
+++ tools/pip/setup.py
@@ -135,14 +135,15 @@ else:
     elif variant.startswith('CU92'):
         libraries.append('CUDA-9.2')
 
-if variant != 'NATIVE':
+from mxnet.runtime import Features
+if Features().is_enabled("MKLDNN"):
     libraries.append('MKLDNN')
 
 short_description += ' This version uses {0}.'.format(' and '.join(libraries))
 
 package_data = {'mxnet': [os.path.join('mxnet', os.path.basename(LIB_PATH[0]))],
                 'dmlc_tracker': []}
-if variant != 'NATIVE':
+if Features().is_enabled("MKLDNN"):
     shutil.copytree(os.path.join(CURRENT_DIR, 'mxnet-build/3rdparty/mkldnn/include'),
                     os.path.join(CURRENT_DIR, 'mxnet/include/mkldnn'))
 if platform.system() == 'Linux':
