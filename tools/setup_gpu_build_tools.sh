a917acac834a22b2b3c143b9b4ac4ce9f36d7b8d
Wei Chu <weichu@amazon.com>
2020-10-02 10:24:01 -0700

    add new line at end of files, add set -e

--- tools/setup_gpu_build_tools.sh
+++ tools/setup_gpu_build_tools.sh
@@ -23,6 +23,8 @@
 # the following environment variables:
 # PATH, CPLUS_INCLUDE_PATH, C_INCLUDE_PATH, LIBRARY_PATH, LD_LIBRARY_PATH, NVCC
 
+set -e
+
 VARIANT=$1
 DEPS_PATH=$2
 
@@ -358,3 +360,4 @@ if [[ ! -d $DEPS_PATH/usr/local/cuda-${CUDA_MAJOR_VERSION} ]]; then
     cp -f ${prefix}/usr/include/nccl.h ${prefix}/include/nccl.h
     ln -sf libnccl_static.a ${prefix}/usr/lib/x86_64-linux-gnu/libnccl.a
 fi
+
