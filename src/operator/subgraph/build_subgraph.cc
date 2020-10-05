18b2098c654fd682174d3ebe35c17b05e8d117d7
Wei Chu <weichu@amazon.com>
2020-10-04 21:55:14 -0700

    fix sanity

--- src/operator/subgraph/build_subgraph.cc
+++ src/operator/subgraph/build_subgraph.cc
@@ -894,7 +894,7 @@ nnvm::Graph BuildSubgraph(nnvm::Graph&& g) {
         nnvm::Graph subg_g = Symbol2Graph(*subg_sym);
         // find param_indice for the subgraph
         std::vector<int> subg_param_indice_list;
-        std::vector<std::string> input_names = 
+        std::vector<std::string> input_names =
                     subg_sym->ListInputNames(nnvm::Symbol::ListInputOption(0));
         for (int i = 0; i < input_names.size(); i++) {
           if (param_name_set.count(input_names[i]) > 0) {
