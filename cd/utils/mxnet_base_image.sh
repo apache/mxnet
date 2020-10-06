3042df0b77049257a0b853606e69778cec24fd4b
waytrue17 <52505574+waytrue17@users.noreply.github.com>
2020-10-06 09:56:16 -0700

    Enable CUDA 11.0 on nightly development builds (#19295)
    Remove CUDA 9.2 and CUDA 10.0

--- cd/utils/mxnet_base_image.sh
+++ cd/utils/mxnet_base_image.sh
@@ -21,17 +21,14 @@
 mxnet_variant=${1:?"Please specify the mxnet variant as the first parameter"}
 
 case ${mxnet_variant} in
-    cu92*)
-    echo "nvidia/cuda:9.2-cudnn7-runtime-ubuntu16.04"
-    ;;
-    cu100*)
-    echo "nvidia/cuda:10.0-cudnn7-runtime-ubuntu16.04"
-    ;;
     cu101*)
     echo "nvidia/cuda:10.1-cudnn7-runtime-ubuntu16.04"
     ;;
     cu102*)
     echo "nvidia/cuda:10.2-cudnn7-runtime-ubuntu16.04"
+    ;;
+    cu110*)
+    echo "nvidia/cuda:11.0-cudnn8-runtime-ubuntu16.04"
     ;;
     cpu)
     echo "ubuntu:16.04"
