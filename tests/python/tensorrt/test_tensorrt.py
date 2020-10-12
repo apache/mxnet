6729cf3bf4edd6837b0feb6417691ce2e00dbcee
Dick Carter <dcarter@nvidia.com>
2020-10-12 12:18:52 -0700

    [v1.x] TRT test update (#19296)
    * Bypass test_tensorrt.py:test_tensorrt_symbol_int8 on arch < 70
    
    * Adapt test_tensorrt.py:test_tensorrt_symbol for A100
    
    * Fix test_numpy_op.py:test_np_mixed_precision_binary_funcs with portion of (#18660)

--- tests/python/tensorrt/test_tensorrt.py
+++ tests/python/tensorrt/test_tensorrt.py
@@ -16,16 +16,22 @@
 # under the License.
 
 import os
+import sys
 import ctypes
 import mxnet as mx
 from mxnet.base import SymbolHandle, check_call, _LIB, mx_uint, c_str_array, c_str, mx_real_t
 from mxnet.symbol import Symbol
 import numpy as np
 from mxnet.test_utils import assert_almost_equal
+from mxnet.numpy_extension import get_cuda_compute_capability
 from mxnet import gluon
 from mxnet.gluon import nn
 from mxnet import nd
 from mxnet.gluon.model_zoo import vision
+
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, os.path.join(curr_path, '../unittest'))
+from common import setup_module, with_seed, teardown
 
 ####################################
 ######### FP32/FP16 tests ##########
@@ -60,7 +66,7 @@ def get_baseline(input_data):
     return output
 
 
-def check_tensorrt_symbol(baseline, input_data, fp16_mode, tol):
+def check_tensorrt_symbol(baseline, input_data, fp16_mode, rtol=None, atol=None):
     sym, arg_params, aux_params = get_model(batch_shape=input_data.shape)
     trt_sym = sym.optimize_for('TensorRT', args=arg_params, aux=aux_params, ctx=mx.gpu(0),
                                precision='fp16' if fp16_mode else 'fp32')
@@ -69,17 +75,18 @@ def check_tensorrt_symbol(baseline, input_data, fp16_mode, tol):
                                    grad_req='null', force_rebind=True)
 
     output = executor.forward(is_train=False, data=input_data)
-    assert_almost_equal(output[0].asnumpy(), baseline[0].asnumpy(), atol=tol[0], rtol=tol[1])
+    assert_almost_equal(output[0], baseline[0], rtol=rtol, atol=atol)
 
+@with_seed()
 def test_tensorrt_symbol():
     batch_shape = (32, 3, 224, 224)
     input_data = mx.nd.random.uniform(shape=(batch_shape), ctx=mx.gpu(0))
     baseline = get_baseline(input_data)
     print("Testing resnet50 with TensorRT backend numerical accuracy...")
     print("FP32")
-    check_tensorrt_symbol(baseline, input_data, fp16_mode=False, tol=(1e-4, 1e-4))
+    check_tensorrt_symbol(baseline, input_data, fp16_mode=False)
     print("FP16")
-    check_tensorrt_symbol(baseline, input_data, fp16_mode=True, tol=(1e-1, 1e-2))
+    check_tensorrt_symbol(baseline, input_data, fp16_mode=True, rtol=1e-2, atol=1e-1)
 
 ##############################
 ######### INT8 tests ##########
@@ -135,17 +142,25 @@ def get_top1(logits):
 
 
 def test_tensorrt_symbol_int8():
+    ctx = mx.gpu(0)
+    cuda_arch = get_cuda_compute_capability(ctx)
+    cuda_arch_min = 70
+    if cuda_arch < cuda_arch_min:
+        print('Bypassing test_tensorrt_symbol_int8 on cuda arch {}, need arch >= {}).'.format(
+              cuda_arch, cuda_arch_min))
+        return
+
     # INT8 engine output are not lossless, so we don't expect numerical uniformity,
     # but we have to compare the TOP1 metric
 
     batch_shape=(1,3,224,224)
     sym, arg_params, aux_params = get_model(batch_shape=batch_shape)
     calibration_iters = 700
-    trt_sym = sym.optimize_for('TensorRT', args=arg_params, aux=aux_params, ctx=mx.gpu(0),
+    trt_sym = sym.optimize_for('TensorRT', args=arg_params, aux=aux_params, ctx=ctx,
                                precision='int8',
                                calibration_iters=calibration_iters)
     
-    executor = trt_sym.simple_bind(ctx=mx.gpu(), data=batch_shape,
+    executor = trt_sym.simple_bind(ctx=ctx, data=batch_shape,
                                grad_req='null', force_rebind=True)
     
     dali_val_iter = get_dali_iter()
