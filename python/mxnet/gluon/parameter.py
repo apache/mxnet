81fc7006b74e7bb8bb6e39a23a73fd2bcf37e4e4
Sam Skalicky <samskalicky@gmail.com>
2020-10-21 00:08:19 +0000

    changed to copy to context

--- python/mxnet/gluon/parameter.py
+++ python/mxnet/gluon/parameter.py
@@ -406,7 +406,7 @@ class Parameter(object):
                 else:
                     data = ndarray.add_n(*(w.copyto(ctx) for w in block)) / len(block)
             else:
-                data = self.data(ctx)
+                data = self.data().copyto(ctx)
         else:
             # fetch all rows for 'row_sparse' param
             all_row_ids = ndarray.arange(0, self.shape[0], dtype='int64', ctx=ctx)
