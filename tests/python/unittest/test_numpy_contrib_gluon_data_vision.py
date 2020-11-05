8720de914ac5f3e98bdde3a7f2cb7cb06dc39b71
Leonard Lausen <lausen@amazon.com>
2020-11-05 08:46:39 -0800

    Skip test_bbox_augmenters on windows

--- tests/python/unittest/test_numpy_contrib_gluon_data_vision.py
+++ tests/python/unittest/test_numpy_contrib_gluon_data_vision.py
@@ -21,6 +21,7 @@ from mxnet.test_utils import *
 import shutil
 import tempfile
 import unittest
+import pytest
 
 def _get_data(url, dirname):
     import os, tarfile
@@ -129,6 +130,7 @@ class TestImage(unittest.TestCase):
         ]
 
     @use_np
+    @pytest.mark.skipif(sys.platform == "win32", reason='https://github.com/apache/incubator-mxnet/issues/18986')
     def test_bbox_augmenters(self):
         # only test if all augmenters will work
         im_list = [_generate_objects() + [x] for x in self.IMAGES]
