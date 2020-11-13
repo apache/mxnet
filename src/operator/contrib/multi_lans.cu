8d6b7520000a6a91d09bef8b003f858fcb71232d
Joe Evans <joeev@amazon.com>
2020-11-12 22:39:12 -0800

    Properly case exponent.

--- src/operator/contrib/multi_lans.cu
+++ src/operator/contrib/multi_lans.cu
@@ -54,9 +54,9 @@ __global__ void KernelStep1(const MultiLANSKernelParam<DType, MPDType> kernel_pa
   MPDType biascorrection1, biascorrection2;
 
   biascorrection1 = 1.0 -
-                    static_cast<MPDType>(pow(beta1, (float)kernel_params.step_count[tensor_id]));
+                    static_cast<MPDType>(pow(beta1, static_cast<float>(kernel_params.step_count[tensor_id])));
   biascorrection2 = 1.0 -
-                    static_cast<MPDType>(pow(beta2, (float)kernel_params.step_count[tensor_id]));
+                    static_cast<MPDType>(pow(beta2, static_cast<float>(kernel_params.step_count[tensor_id])));
 
   MPDType r_weight[ILP_LAMB];
   MPDType r_grad[ILP_LAMB];
