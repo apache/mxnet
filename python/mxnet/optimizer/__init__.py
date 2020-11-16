013f9d580a5935899f8075a2eb0bd6d5519fd388
Ubuntu <ubuntu@ip-172-31-31-87.us-east-2.compute.internal>
2020-11-16 05:41:00 +0000

    add adamW

--- python/mxnet/optimizer/__init__.py
+++ python/mxnet/optimizer/__init__.py
@@ -19,9 +19,9 @@
 from . import (optimizer, contrib, updater, utils, sgd,
                sgld, signum, dcasgd, nag, adagrad,
                adadelta, adam, adamax, nadam, ftrl,
-               ftml, lars, lamb, rmsprop, lans,bert_adam)
+               ftml, lars, lamb, rmsprop, lans,adamW)
 # pylint: disable=wildcard-import
-from .bert_adam import *
+from .adamW import *
 
 from .optimizer import *
 
