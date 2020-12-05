896ecb1219effd3fa71f00c2f70a1eba100e6ac9
Ubuntu <ubuntu@ip-172-31-60-22.us-west-2.compute.internal>
2020-12-05 03:43:12 +0000

    fix InvokeOperator

--- src/imperative/cached_op.cc
+++ src/imperative/cached_op.cc
@@ -327,9 +327,10 @@ bool CachedOp::SetBackwardGraph(
   node_range = {num_forward_nodes, idx.num_nodes()};
   entry_range = {num_forward_entries, idx.num_node_entries()};
 
+  bool contain_dynamic_shape = false;
   bool match = true;
   match &= CheckAndInferShape(&g, std::move(shapes), false,
-                              node_range, entry_range);
+                              node_range, entry_range, &contain_dynamic_shape;);
   match &= CheckAndInferType(&g, std::move(dtypes), false,
                              node_range, entry_range);
   exec::DevMaskVector dev_mask(idx.num_nodes(), default_ctx.dev_mask());
