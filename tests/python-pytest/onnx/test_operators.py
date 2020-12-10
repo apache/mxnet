a86dec3eedaed3b7b208319618d97e5a5eb9b28f
Joe Evans <joeev@amazon.com>
2020-12-10 16:16:42 +0000

    Pytest version in 1.x branch doesn't support tmp_path, so do it inside the tests.

--- tests/python-pytest/onnx/test_operators.py
+++ tests/python-pytest/onnx/test_operators.py
@@ -21,6 +21,7 @@ import numpy as np
 import onnxruntime as rt
 from mxnet.test_utils import assert_almost_equal
 import pytest
+import tempfile
 
 def op_export_test(op_name, Model, inputs, tmp_path):
     def export_to_onnx(model, op_name, inputs):
@@ -49,80 +50,85 @@ def op_export_test(op_name, Model, inputs, tmp_path):
     assert_almost_equal(pred_nat, pred_onx)
 
 
-def test_onnx_export_abs(tmp_path):
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x):
-            out = F.abs(x)
-            return out
-    x = mx.nd.array([[-2, -1], [0, 99]], dtype='float32')
-    op_export_test('abs', Model, [x], tmp_path)
+def test_onnx_export_abs():
+    with tempfile.TemporaryDirectory() as tmp_path:
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x):
+                out = F.abs(x)
+                return out
+        x = mx.nd.array([[-2, -1], [0, 99]], dtype='float32')
+        op_export_test('abs', Model, [x], tmp_path)
 
-def test_onnx_export_slice(tmp_path):
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x):
-            out = F.slice(x, begin=(0,1), end=(2,4))
-            return out
-    x = mx.nd.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]], dtype='float32')
-    op_export_test('slice', Model, [x], tmp_path)
+def test_onnx_export_slice():
+    with tempfile.TemporaryDirectory() as tmp_path:
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x):
+                out = F.slice(x, begin=(0,1), end=(2,4))
+                return out
+        x = mx.nd.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]], dtype='float32')
+        op_export_test('slice', Model, [x], tmp_path)
 
-def test_onnx_export_stack(tmp_path):
-    dtype = 'float32'
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x, y):
-            out = F.stack(x, y)
-            return out
-    x = mx.nd.array([1, 2], dtype=dtype)
-    y = mx.nd.array([3, 4], dtype=dtype)
-    op_export_test('stack', Model, [x, y], tmp_path)
+def test_onnx_export_stack():
+    with tempfile.TemporaryDirectory() as tmp_path:
+        dtype = 'float32'
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x, y):
+                out = F.stack(x, y)
+                return out
+        x = mx.nd.array([1, 2], dtype=dtype)
+        y = mx.nd.array([3, 4], dtype=dtype)
+        op_export_test('stack', Model, [x, y], tmp_path)
 
-def test_onnx_export_zeros_like(tmp_path):
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x):
-            out = F.zeros_like(x)
-            return out
-    x = mx.nd.array([[-2,-1,0],[0,50,99],[4,5,6],[7,8,9]], dtype='float32')
-    op_export_test('zeros_like', Model, [x], tmp_path)
+def test_onnx_export_zeros_like():
+    with tempfile.TemporaryDirectory() as tmp_path:
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x):
+                out = F.zeros_like(x)
+                return out
+        x = mx.nd.array([[-2,-1,0],[0,50,99],[4,5,6],[7,8,9]], dtype='float32')
+        op_export_test('zeros_like', Model, [x], tmp_path)
 
 @pytest.mark.parametrize("dtype", ["float32", "double"])
-def test_onnx_export_arange_like(dtype, tmp_path):
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x):
-            out = F.contrib.arange_like(x)
-            return out
-    x = mx.nd.array([[-2,-1,0],[0,50,99],[4,5,6],[7,8,9]], dtype=dtype)
-    op_export_test('arange_like', Model, [x], tmp_path)
+def test_onnx_export_arange_like(dtype):
+    with tempfile.TemporaryDirectory() as tmp_path:
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x):
+                out = F.contrib.arange_like(x)
+                return out
+        x = mx.nd.array([[-2,-1,0],[0,50,99],[4,5,6],[7,8,9]], dtype=dtype)
+        op_export_test('arange_like', Model, [x], tmp_path)
 
-def test_onnx_export_layernorm(tmp_path):
-    dtype = 'float32'
-    class Model(HybridBlock):
-        def __init__(self, **kwargs):
-            super(Model, self).__init__(**kwargs)
-        def hybrid_forward(self, F, x, gamma, beta):
-            out = F.LayerNorm(x, gamma, beta, axis=1)
-            return out
-    x = mx.nd.array([[1,3],[2,4]], dtype=dtype)
-    gamma = mx.random.uniform(0, 1, x[0].shape).astype(dtype)
-    beta = mx.random.uniform(0, 1, x[0].shape).astype(dtype)
-    op_export_test('LayerNorm', Model, [x, gamma, beta], tmp_path)
+def test_onnx_export_layernorm():
+    with tempfile.TemporaryDirectory() as tmp_path:
+        dtype = 'float32'
+        class Model(HybridBlock):
+            def __init__(self, **kwargs):
+                super(Model, self).__init__(**kwargs)
+            def hybrid_forward(self, F, x, gamma, beta):
+                out = F.LayerNorm(x, gamma, beta, axis=1)
+                return out
+        x = mx.nd.array([[1,3],[2,4]], dtype=dtype)
+        gamma = mx.random.uniform(0, 1, x[0].shape).astype(dtype)
+        beta = mx.random.uniform(0, 1, x[0].shape).astype(dtype)
+        op_export_test('LayerNorm', Model, [x, gamma, beta], tmp_path)
 
 
 if __name__ == '__main__':
-    import tempfile
-    with tempfile.TemporaryDirectory() as tmp_path:
-        test_onnx_export_abs(tmp_path)
-        test_onnx_export_slice(tmp_path)
-        test_onnx_export_stack(tmp_path)
-        test_onnx_export_zeros_like(tmp_path)
-        test_onnx_export_arange_like(tmp_path)
-        test_onnx_export_layernorm(tmp_path)
+    test_onnx_export_abs()
+    test_onnx_export_slice()
+    test_onnx_export_stack()
+    test_onnx_export_zeros_like()
+    test_onnx_export_arange_like('float32')
+    test_onnx_export_arange_like('double')
+    test_onnx_export_layernorm()
 
