3746babc8fdb211584a9a661207061cb646b01a8
bgawrych <bartlomiej.gawrych@intel.com>
2020-12-12 05:31:00 +0100

    [FEATURE] Restore Quantization API to MXNet (#19587)
    * Restore quantization files
    
    * Adapt quantization.py - Add/Remove modules
    
    * Adapt part of quantization tests to new API
    
    * fuse fc+tanh
    
    * Replace Module API with SymbolBlock in quantize_model
    
    * enabled test_quantization_mkldnn.py
    
    * Revert "fuse fc+tanh"
    
    This reverts commit a8b737a473ca6529a1969b748ea03c40e12c0798.
    Needs refactor of conv and fc common part
    
    * Enable tests from test_subgraph.py
    
    * Enable test_mobilenetv2_struct
    
    * Refactor test_subgraph.py
    
    * Reorder of conditions
    
    'if calib_data is not None' and 'if not data_shapes'
    
    * Utilize optimize_for in quantization flow
    
    * remove duplicate imports
    
    * Add variable monitor callback
    
    * fix sanity
    
    * wip
    
    merge with bgawrych
    
    cannot do inplace convolution and the sum and input tesnsors are shared already
    
    remove cout
    
    spaces after if
    
    refactor if else
    
    * Rebase to master - remove with_seed
    
    * Add numpy support for quantization
    
    Conflicts:
    	src/operator/subgraph/mkldnn/mkldnn_conv_property.h
    
    * enabled examples/quantization/imagenet_gen_qsym_mkldnn.py
    
    review fixes
    remove unused parameters
    change rgb
    
    small fix
    
    add alexnet exclude
    
    fix filename suffix
    
    refactor first conv exclude v1
    
    v2
    
    v3
    
    fix names of layers
    
    fix bug
    
    * Add test to check different way of data generation for hybridize
    
    * Copy original network
    
    * Change num_calib_examples to num_calib_batches
    
    * enabling imagenet_inference.py
    
    * Add base class for collectors and feed custom with calib_layers
    
    * Some doc fixes after discussion
    
    * anko review - change all quantize_net_v2 to quantize_net
    
    * Make -s argument required
    
    * review fixes by mozga and anko
    
    * Fix bugs
    
    * Fix channel-wise quantization
    
    * Fix documentation formatting
    
    * mozga: fix review
    
    * Fix lint
    
    * Refactor calibration for variables
    
    * fix sanity
    
    * fix clang tidy
    
    * ciyong review fixes
    
    * Add verified models
    
    * Fix review: Tao and Xinyu
    
    Co-authored-by: Sylwester Fraczek <sylwester.fraczek@intel.com>
    Co-authored-by: grygielski <adam.grygielski@gmail.com>

--- tests/python/test_quantization_gpu.py
+++ tests/python/test_quantization_gpu.py
@@ -14,21 +14,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import os
+import sys
+import mxnet as mx
 
-# coding: utf-8
-"""Experimental contributions"""
 
-from . import symbol
-from . import ndarray
+curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+sys.path.insert(0, os.path.join(curr_path, '../quantization'))
+from mxnet.test_utils import set_default_context
+from test_quantization import *
 
-from . import symbol as sym
-from . import ndarray as nd
-
-from . import tensorboard
-
-from . import text
-from . import onnx
-from . import io
-from . import quantization
-from . import quantization as quant
-from . import tensorrt
+set_default_context(mx.gpu(0))
