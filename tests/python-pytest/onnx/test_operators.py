b3d9af2351cc1c08ef3df8476bda8ea1483a4717
Zhaoqi Zhu <zhaoqizhu96@gmail.com>
2020-12-15 11:07:07 -0800

    onnx support more ops (#19653)
    * add sequencemask broadcast_axis, add reshape reverse option
    
    * new ops
    
    * add return
    
    * fix matmul
    
    * fix broacast_axis
    
    * fix sequencemask
    
    * add test cases
    
    * clean up
    
    * fix sanity

--- tests/python-pytest/onnx/test_operators.py
+++ tests/python-pytest/onnx/test_operators.py
@@ -90,7 +90,7 @@ def test_onnx_export_zeros_like(tmp_path):
     op_export_test('zeros_like', M, [x], tmp_path)
 
 
-@pytest.mark.parametrize("dtype", ["float32", "double"])
+@pytest.mark.parametrize("dtype", ["float32", "float64"])
 def test_onnx_export_arange_like(tmp_path, dtype):
     M = def_model('contrib.arange_like')
     x = mx.nd.array([[-2,-1,0],[0,50,99],[4,5,6],[7,8,9]], dtype=dtype)
@@ -104,3 +104,40 @@ def test_onnx_export_layernorm(tmp_path):
     beta = mx.random.uniform(0, 1, x[0].shape, dtype='float32')
     op_export_test('LayerNorm', M, [x, gamma, beta], tmp_path)
 
+
+@pytest.mark.parametrize('dtype', ['float32', 'float64', 'int32'])
+def test_onnx_export_broadcast_axis(tmp_path, dtype):
+    M1 = def_model('broadcast_axis', axis=(0, 2), size=(3, 4))
+    M2 = def_model('broadcast_axis', axis=(0, 2), size=(1, 5))
+    x1 = mx.nd.array([[[1], [2]]], dtype=dtype)
+    op_export_test('broadcast_axis_1', M1, [x1], tmp_path)
+    op_export_test('broadcast_axis_2', M2, [x1], tmp_path)
+    M3 = def_model('broadcast_axis', axis=(1, 4), size=(3, 5))
+    x2 = mx.nd.ones((1, 1, 3, 1, 1, 1), dtype=dtype)
+    op_export_test('broadcast_axis_3', M3, [x2], tmp_path)
+
+
+@pytest.mark.parametrize('dtype', ['float32'])
+def test_onnx_export_SequenceMask(tmp_path, dtype):
+    M1 = def_model('SequenceMask', use_sequence_length=True, axis=1, value=-5)
+    M2 = def_model('SequenceMask', use_sequence_length=True, axis=0, value=-99)
+    x = mx.nd.array([[[[  1.,   2.,   3.,  3.5]],
+                      [[  4.,   5.,   6.,  6.5]]],
+                     [[[  7.,   8.,   9.,  9.5]],
+                      [[ 10.,  11.,  12., 12.5]]],
+                     [[[ 13.,  14.,  15., 15.5]],
+                      [[ 16.,  17.,  18., 18.5]]]], dtype=dtype)
+    seq_len1 = mx.nd.array([1, 2, 1], dtype=dtype)
+    seq_len2 = mx.nd.array([1, 2], dtype=dtype)
+    op_export_test('SequenceMask_1', M1, [x, seq_len1], tmp_path)
+    op_export_test('SequenceMask_2', M2, [x, seq_len2], tmp_path)
+
+
+@pytest.mark.parametrize('dtype', ['float32', 'float64', 'int32'])
+def test_onnx_export_contrib_interleaved_matmul_selfatt_qk(tmp_path, dtype):
+    M1 = def_model('contrib.interleaved_matmul_selfatt_qk', heads=3)
+    x1 = mx.nd.random.uniform(0, 1, (3, 3, 3*3*3))
+    op_export_test('contrib_interleaved_matmul_selfatt_qk_1', M1, [x1], tmp_path)
+    M2 = def_model('contrib.interleaved_matmul_selfatt_qk', heads=5)
+    x2 = mx.nd.random.uniform(0, 1, (7, 5, 4*5*6))
+    op_export_test('contrib_interleaved_matmul_selfatt_qk_2', M2, [x2], tmp_path)
