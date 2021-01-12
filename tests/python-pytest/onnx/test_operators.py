9e34ff0bd02802a9c99a6752def4611ee12e598b
Joe Evans <joeev@amazon.com>
2021-01-12 22:13:01 +0000

    Add unit test for where onnx export operator.

--- tests/python-pytest/onnx/test_operators.py
+++ tests/python-pytest/onnx/test_operators.py
@@ -385,10 +385,20 @@ def test_onnx_export_contrib_BilinearResize2D(tmp_path, dtype, params):
 def test_onnx_export_greater_scalar(tmp_path, dtype, scalar):
     if 'int' in dtype:
         scalar = int(scalar)
-        x = mx.nd.array([[1., 2., 3.,  4.],
-                         [5., 6., 7.,  8.],
-                         [9., 10, 11., 12.]], dtype=dtype)
+        x = mx.nd.array([[1., 2.,  3.,  4.],
+                         [5., 6.,  7.,  8.],
+                         [9., 10., 11., 12.]], dtype=dtype)
     else:
         x = mx.random.uniform(0, 9999, (5,10), dtype=dtype)
     M = def_model('_internal._greater_scalar', scalar=scalar)
     op_export_test('_internal._greater_scalar', M, [x], tmp_path)
+
+
+@pytest.mark.parametrize("dtype", ["float16", "float32", "float64", "int32", "int64"])
+@pytest.mark.parametrize("shape", [(1,1), (3,3), (10,2), (20,30,40)])
+def test_onnx_export_where(tmp_path, dtype, shape):
+    M = def_model('where')
+    x = mx.nd.zeros(shape, dtype=dtype)
+    y = mx.nd.ones(shape, dtype=dtype)
+    cond = mx.nd.random.randint(low=0, high=1, shape=shape, dtype='int32')
+    op_export_test('where', M, [cond, x, y], tmp_path)
