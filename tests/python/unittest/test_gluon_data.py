23cbe5adf5e7547a04abc5292e1c606cdab211ae
Neutron3529 <qweytr_1@163.com>
2021-01-15 12:02:03 +0800

    add an auto_reload flag into the dataloader
    the added flag is set to `True` rather than the default `False` since in mxnet 2.0, the default `nopython` mode prefetch data and auto reload it.

--- tests/python/unittest/test_gluon_data.py
+++ tests/python/unittest/test_gluon_data.py
@@ -24,7 +24,7 @@ import random
 from mxnet import gluon
 import platform
 from common import setup_module, with_seed, teardown
-from mxnet.gluon.data import DataLoader, PrefetchedDataLoader
+from mxnet.gluon.data import DataLoader
 import mxnet.ndarray as nd
 from mxnet import context
 from mxnet.gluon.data.dataset import Dataset
@@ -158,18 +158,11 @@ class Dataset(gluon.data.Dataset):
 def test_multi_worker():
     data = Dataset()
     for thread_pool in [True, False]:
-        loader = gluon.data.DataLoader(data, batch_size=1, num_workers=5, thread_pool=thread_pool)
-        for i, batch in enumerate(loader):
-            assert (batch.asnumpy() == i).all()
-
-@with_seed()
-def test_prefetched_multi_worker():
-    data = Dataset()
-    for thread_pool in [True, False]:
-        loader = gluon.data.PrefetchedDataLoader(data, batch_size=1, num_workers=5, thread_pool=thread_pool)
-        for i, batch in enumerate(loader):
-            assert (batch.asnumpy() == i).all()
-
+        for auto_reload in [True, False]:
+            loader = gluon.data.DataLoader(data, batch_size=1, num_workers=5,
+                                           thread_pool=thread_pool,auto_reload=auto_reload)
+            for i, batch in enumerate(loader):
+                assert (batch.asnumpy() == i).all()
 
 @with_seed()
 def test_multi_worker_shape():
