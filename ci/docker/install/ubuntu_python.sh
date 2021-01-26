127ad5165474c042516ee9f0ff78b427d8f16360
Joe Evans <joeev@amazon.com>
2021-01-26 01:42:31 +0000

    Install python3.6 from deadsnakes repo, since 3.5 is EOL'd and get-pip.py no longer works with 3.5.

--- ci/docker/install/ubuntu_python.sh
+++ ci/docker/install/ubuntu_python.sh
@@ -23,7 +23,10 @@
 set -ex
 # install libraries for mxnet's python package on ubuntu
 apt-get update || true
-apt-get install -y python-dev python3-dev virtualenv wget
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update || true
+apt-get install -y python-dev python3.6-dev virtualenv wget
 
 # the version of the pip shipped with ubuntu may be too lower, install a recent version here
 wget -nv https://bootstrap.pypa.io/get-pip.py
