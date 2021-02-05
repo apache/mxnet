00bc04bd7395fe206373608575c5b753b2d85381
Wei Chu <weichu@amazon.com>
2021-02-04 16:59:54 -0800

    enable defalut input_type

--- python/mxnet/contrib/onnx/mx2onnx/export_model.py
+++ python/mxnet/contrib/onnx/mx2onnx/export_model.py
@@ -28,7 +28,7 @@ from .export_onnx import MXNetGraph
 from ._export_helper import load_module
 
 
-def export_model(sym, params, input_shape, input_type,
+def export_model(sym, params, input_shape, input_type=np.float32,
                  onnx_file_path='model.onnx', verbose=False, opset_version=None):
     """Exports the MXNet model file, passed as a parameter, into ONNX model.
     Accepts both symbol,parameter objects as well as json and params filepaths as input.
@@ -73,6 +73,8 @@ def export_model(sym, params, input_shape, input_type,
         # default is to use latest opset version the onnx package supports
         opset_version = onnx_opset_version()
 
+    if not isinstance(input_type, list) then:
+        input_type = [input_type for _ in range(len(input_shapes))]
     input_dtype = [mapping.NP_TYPE_TO_TENSOR_TYPE[np.dtype(inp_type)] for inp_type in input_type]
     # if input parameters are strings(file paths), load files and create symbol parameter objects
     if isinstance(sym, string_types) and isinstance(params, string_types):
