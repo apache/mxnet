7cfebc32fda91e6c09b703f142193cfc0eeffd80
Joe Evans <joeev@amazon.com>
2021-02-11 18:24:02 +0000

    python3.6-distutils is not available from deadsnakes.

--- ci/docker/install/ubuntu_python.sh
+++ ci/docker/install/ubuntu_python.sh
@@ -26,7 +26,7 @@ apt-get update || true
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update || true
-apt-get install -y python3.6-dev python3.6-distutils virtualenv wget
+apt-get install -y python3.6-dev virtualenv wget
 # setup symlink in /usr/local/bin to override python3 version
 ln -sf /usr/bin/python3.6 /usr/local/bin/python3
 
