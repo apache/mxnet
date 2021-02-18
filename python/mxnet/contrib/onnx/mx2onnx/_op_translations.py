cc7166a572e7e1829c0351e8103dd08d8834fa0f
zha0q1 <zhaoqizhu96@gmail.com>
2021-02-18 05:25:11 +0000

    remove return

--- python/mxnet/contrib/onnx/mx2onnx/_op_translations.py
+++ python/mxnet/contrib/onnx/mx2onnx/_op_translations.py
@@ -162,10 +162,8 @@ def create_const_scalar_node(input_name, value, kwargs):
     from onnx.helper import make_tensor, make_tensor_value_info
     initializer = kwargs["initializer"]
     input_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[value.dtype]
-    #value_node = make_tensor_value_info(input_name, input_type, ())
     tensor_node = make_tensor(input_name, input_type, (), ([value]))
     initializer.append(tensor_node)
-    #return value_node
 
 def create_const_node(input_name, value, kwargs):
     """Helper function to create a tensor value node and a
@@ -174,10 +172,8 @@ def create_const_node(input_name, value, kwargs):
     initializer = kwargs["initializer"]
     input_type = onnx.mapping.NP_TYPE_TO_TENSOR_TYPE[value.dtype]
     input_shape = value.shape
-    value_node = make_tensor_value_info(input_name, input_type, input_shape)
     tensor_node = make_tensor(input_name, input_type, input_shape, value)
     initializer.append(tensor_node)
-    return value_node
 
 def create_tensor(tensor_list, tensor_name, initializer, dtype='int64'):
     """Helper function to create a tensor value node and a
