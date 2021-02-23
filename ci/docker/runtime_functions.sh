1d4c90b90f80b2657686e86dc77303a4363ec84c
Zhaoqi Zhu <zhaoqizhu96@gmail.com>
2021-02-22 17:35:40 -0800

    Update runtime_functions.sh

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -1311,13 +1311,17 @@ cd_pypi_publish() {
 
 cd_s3_publish() {
     set -ex
-    pip3 install --user awscli
+    pip3 install --upgrade --user awscli
     filepath=$(readlink -f wheel_build/dist/*.whl)
     filename=$(basename $filepath)
     variant=$(echo $filename | cut -d'-' -f1 | cut -d'_' -f2 -s)
     if [ -z "${variant}" ]; then
         variant="cpu"
     fi
+    echo $SHELL
+    echo $PATH
+    ls /usr/local/bin
+    set
     aws s3 cp ${filepath} s3://apache-mxnet/dist/python/${variant}/${filename} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=id=43f628fab72838a4f0b929d7f1993b14411f4b0294b011261bc6bd3e950a6822
 }
 
