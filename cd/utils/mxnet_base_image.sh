b3028e9a1349d36caa6aa2589a6ad7dfb49aa10e
Manu Seth <sethman@amazon.com>
2021-03-05 22:44:51 +0000

    remove cu112 changes

--- cd/utils/mxnet_base_image.sh
+++ cd/utils/mxnet_base_image.sh
@@ -33,9 +33,6 @@ case ${mxnet_variant} in
     cu110*)
     echo "nvidia/cuda:11.0-cudnn8-runtime-ubuntu18.04"
     ;;
-    cu112*)
-    echo "nvidia/cuda:11.2.1-cudnn8-runtime-ubuntu18.04"
-    ;;
     cpu)
     echo "ubuntu:18.04"
     ;;
