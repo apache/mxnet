ca57a5da29b33c5cdbf478b86ff2a9a81c023b8f
barry-jin <barryjin1995@gmail.com>
2021-03-30 10:52:55 -0700

    fix lint

--- python/mxnet/numpy_extension/_op.py
+++ python/mxnet/numpy_extension/_op.py
@@ -451,9 +451,9 @@ def pooling(data=None, kernel=None, stride=None, pad=None, pool_type="max",
     pool_type : {'avg', 'lp', 'max', 'sum'},optional, default='max'
         Pooling type to be applied.
     global_pool : boolean, optional, default=0
-        Ignore kernel size, do global pooling based on current input feature map. 
+        Ignore kernel size, do global pooling based on current input feature map.
     cudnn_off : boolean, optional, default=0
-        Turn off cudnn pooling and use MXNet pooling operator. 
+        Turn off cudnn pooling and use MXNet pooling operator.
     pooling_convention : {'full', 'same', 'valid'},optional, default='valid'
         Pooling convention to be applied.
     stride : Shape(tuple), optional, default=[]
@@ -485,83 +485,27 @@ def pooling(data=None, kernel=None, stride=None, pad=None, pool_type="max",
 # pylint: disable=too-many-arguments, unused-argument
 @set_module('mxnet.numpy_extension')
 def dropout(data, p=0.5, mode="training", axes=None, cudnn_off=True, **kwargs):
-    r"""Performs pooling on the input.
+    r"""Applies dropout operation to input array.
 
-    The shapes for 1-D pooling are
+    - During training, each element of the input is set to zero with probability p.
+      The whole array is rescaled by :math:`1/(1-p)` to keep the expected
+      sum of the input unchanged.
 
-    - **data** and **out**: *(batch_size, channel, width)* (NCW layout) or
-      *(batch_size, width, channel)* (NWC layout),
-
-    The shapes for 2-D pooling are
-
-    - **data** and **out**: *(batch_size, channel, height, width)* (NCHW layout) or
-      *(batch_size, height, width, channel)* (NHWC layout),
-
-        out_height = f(height, kernel[0], pad[0], stride[0])
-        out_width = f(width, kernel[1], pad[1], stride[1])
-
-    The definition of *f* depends on ``pooling_convention``, which has two options:
-
-    - **valid** (default)::
-
-        f(x, k, p, s) = floor((x+2*p-k)/s)+1
-
-    - **full**, which is compatible with Caffe::
-
-        f(x, k, p, s) = ceil((x+2*p-k)/s)+1
-
-    When ``global_pool`` is set to be true, then global pooling is performed. It will reset
-    ``kernel=(height, width)`` and set the appropiate padding to 0.
-
-    Three pooling options are supported by ``pool_type``:
-
-    - **avg**: average pooling
-    - **max**: max pooling
-    - **sum**: sum pooling
-    - **lp**: Lp pooling
-
-    For 3-D pooling, an additional *depth* dimension is added before
-    *height*. Namely the input data and output will have shape *(batch_size, channel, depth,
-    height, width)* (NCDHW layout) or *(batch_size, depth, height, width, channel)* (NDHWC layout).
-
-    Notes on Lp pooling:
-
-    Lp pooling was first introduced by this paper: https://arxiv.org/pdf/1204.3968.pdf.
-    L-1 pooling is simply sum pooling, while L-inf pooling is simply max pooling.
-    We can see that Lp pooling stands between those two, in practice the most common value for p is 2.
-
-    For each window ``X``, the mathematical expression for Lp pooling is:
-
-    :math:`f(X) = \sqrt[p]{\sum_{x}^{X} x^p}`
+    - During testing, this operator does not change the input if mode is 'training'.
+      If mode is 'always', the same computaion as during training will be applied.
 
     Parameters
     ----------
     data : NDArray
-        Input data to the pooling operator.
-    kernel : Shape(tuple), optional, default=[]
-        Pooling kernel size: (y, x) or (d, y, x)
-    pool_type : {'avg', 'lp', 'max', 'sum'},optional, default='max'
-        Pooling type to be applied.
-    global_pool : boolean, optional, default=0
-        Ignore kernel size, do global pooling based on current input feature map. 
-    cudnn_off : boolean, optional, default=0
-        Turn off cudnn pooling and use MXNet pooling operator. 
-    pooling_convention : {'full', 'same', 'valid'},optional, default='valid'
-        Pooling convention to be applied.
-    stride : Shape(tuple), optional, default=[]
-        Stride: for pooling (y, x) or (d, y, x). Defaults to 1 for each dimension.
-    pad : Shape(tuple), optional, default=[]
-        Pad for pooling: (y, x) or (d, y, x). Defaults to no padding.
-    p_value : int or None, optional, default='None'
-        Value of p for Lp pooling, can be 1 or 2, required for Lp Pooling.
-    count_include_pad : boolean or None, optional, default=None
-        Only used for AvgPool, specify whether to count padding elements for averagecalculation.
-        For example, with a 5*5 kernel on a 3*3 corner of a image,the sum of the 9 valid elements will
-        be divided by 25 if this is set to true,or it will be divided by 9 if this is set to false.
-        Defaults to true.
-    layout : {None, 'NCDHW', 'NCHW', 'NCW', 'NDHWC', 'NHWC', 'NWC'},optional, default='None'
-        Set layout for input and output. Empty for
-        default layout: NCW for 1d, NCHW for 2d and NCDHW for 3d.
+        Input array to which dropout will be applied.
+    p : float, optional, default=0.5
+        Fraction of the input that gets dropped out during training time.
+    mode : {'always', 'training'},optional, default='training'
+        Whether to only turn on dropout during training or to also turn on for inference.
+    axes : Shape(tuple), optional, default=[]
+        Axes for variational dropout kernel.
+    cudnn_off : boolean or None, optional, default=0
+        Whether to turn off cudnn in dropout operator. This option is ignored if axes is specified.
 
     Returns
     -------
@@ -573,7 +517,7 @@ def dropout(data, p=0.5, mode="training", axes=None, cudnn_off=True, **kwargs):
 
 # pylint: disable=too-many-arguments
 @set_module('mxnet.numpy_extension')
-def one_hot(data, depth=None, on_value=None, off_value=None, dtype=None):
+def one_hot(data, depth=None, on_value=1.0, off_value=0.0, dtype="float32"):
     r"""Returns a one-hot array.
 
     The locations represented by `indices` take value `on_value`, while all
