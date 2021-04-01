6c4e38d88ddceb45f559e34eac8a818e31b99db3
zha0q1 <zhaoqizhu96@gmail.com>
2021-04-01 00:34:37 +0000

    fix return

--- python/mxnet/contrib/onnx/__init__.py
+++ python/mxnet/contrib/onnx/__init__.py
@@ -26,7 +26,7 @@ def import_model(*args, **kwargs):
     print('Please be advised that importing ONNX models into MXNet is going to be deprecated '
           'in the upcoming MXNet v1.10 release. The following apis will be deleted: '
           'mxnet.contrib.onnx.import_model/get_model_metadata/import_to_gluon.')
-    import_model_(*args, **kwargs)
+    return import_model_(*args, **kwargs)
 
 
 def get_model_metadata(*args, **kwargs):
@@ -34,7 +34,7 @@ def get_model_metadata(*args, **kwargs):
     print('Please be advised that importing ONNX models into MXNet is going to be deprecated '
           'in the upcoming MXNet v1.10 release. The following apis will be deleted: '
           'mxnet.contrib.onnx.import_model/get_model_metadata/import_to_gluon.')
-    get_model_metadata_(*args, **kwargs)
+    return get_model_metadata_(*args, **kwargs)
 
 
 def import_to_gluon(*args, **kwargs):
@@ -42,7 +42,7 @@ def import_to_gluon(*args, **kwargs):
     print('Please be advised that importing ONNX models into MXNet is going to be deprecated '
           'in the upcoming MXNet v1.10 release. The following apis will be deleted: '
           'mxnet.contrib.onnx.import_model/get_model_metadata/import_to_gluon.')
-    import_to_gluon_(*args, **kwargs)
+    return import_to_gluon_(*args, **kwargs)
 
 
 def export_model(*args, **kwargs):
@@ -50,4 +50,4 @@ def export_model(*args, **kwargs):
     print('Please be advised that the ONNX module has been moved to mxnet.onnx and '
           'mxnet.onnx.export_model is the preferred path. The current path will be deprecated '
           'in the upcoming MXNet v1.10 release.')
-    export_model_(*args, **kwargs)
+    return export_model_(*args, **kwargs)
