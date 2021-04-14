931948984b8552d48ca16473d6361d41d97f7b41
zha0q1 <zhaoqizhu96@gmail.com>
2021-04-14 21:45:15 +0000

    fix

--- python/mxnet/onnx/mx2onnx/_op_translations/__init__.py
+++ python/mxnet/onnx/mx2onnx/_op_translations/__init__.py
@@ -16,7 +16,7 @@
 # under the License.
 
 # coding: utf-8
-"""ONNX Export module"""
+"""ONNX export op translation"""
 
 from . import _op_translations_opset12
 from . import _op_translations_opset13
