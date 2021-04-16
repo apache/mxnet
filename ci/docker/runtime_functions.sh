b3075a9ef2b1fcd2e355610defd9ba599caa8889
barry-jin <barryjin1995@gmail.com>
2021-04-16 14:48:19 -0700

    update build

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -626,7 +626,7 @@ build_ubuntu_gpu_onednn_nocudnn() {
 build_ubuntu_gpu() {
     set -ex
     cd /work/build
-    ln -s libcuda.so.1 /usr/local/cuda-11.1/targets/x86_64-linux/lib/stubs/libcuda.so 
+    ln -f -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/stubs/libcuda.so libcuda.so.1
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.1/targets/x86_64-linux/lib/stubs/
     CC=gcc-7 CXX=g++-7 cmake \
         -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
