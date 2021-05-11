081aa58f81a536cc2a6168c51bdfb3b98bb5a17d
Paweł Głomski <pawel.glomski@intel.com>
2021-05-11 04:33:51 +0200

    [v1.x][BUGFIX] Implement oneDNN deconvolution primitives to deconvolution 2D (#20107)
    * Use mkldnn deconvolution primitive in deconvolution
    
    * Apply clang-format
    
    * Refactor deconvolution version 1
    
    * Refactor deconvolution version 2 and use permute_axes in IOLogicalSwapDesc
    
    * Refactor deconvolution version 3
    
    * Enable Deconvolution2D test
    
    * Fix sanity
    
    * Fix windows builds
    
    * Fix deconvolution with bias test

--- tests/python/mkl/test_mkldnn.py
+++ tests/python/mkl/test_mkldnn.py
@@ -469,10 +469,9 @@ def test_convolution():
 
 
 @with_seed()
-@unittest.skip("Flaky test https://github.com/apache/incubator-mxnet/issues/12579")
 def test_Deconvolution():
     def check_Deconvolution_training(stype):
-        for shape in [(3, 3, 10), (3, 3, 10, 10)]:
+        for shape in [(3, 3, 10, 10)]: # testing only 2D for now
             data_tmp = np.random.randint(256, size=shape)
             data = mx.symbol.Variable('data', stype=stype)
 
