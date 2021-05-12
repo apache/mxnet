b446fd8e914bac1a2f1e7b17c75ed334feca8bb8
Manu Seth <manuseth1010@gmail.com>
2021-05-12 02:37:52 +0000

    remove libquadmath due to licensing issues #19053

--- tools/staticbuild/build_lib_cmake.sh
+++ tools/staticbuild/build_lib_cmake.sh
@@ -44,18 +44,7 @@ rm -rf lib; mkdir lib;
 if [[ $PLATFORM == 'linux' ]]; then
     cp -L build/libmxnet.so lib/libmxnet.so
     cp -L staticdeps/lib/libopenblas.so lib/libopenblas.so.0
-    if [[ $ARCH == 'aarch64' ]]; then
-        cp -L /usr/lib/aarch64-linux-gnu/libgfortran.so.5 lib/libgfortran.so.5
-    else
-        if [[ -f /usr/lib/gcc/x86_64-linux-gnu/4.8/libgfortran.so ]]; then
-            cp -L /usr/lib/gcc/x86_64-linux-gnu/4.8/libgfortran.so lib/libgfortran.so.3
-        elif [[ -f /usr/lib/x86_64-linux-gnu/libgfortran.so.3 ]]; then
-            cp -L /usr/lib/x86_64-linux-gnu/libgfortran.so.3 lib/libgfortran.so.3
-        else
-            cp -L /usr/lib/x86_64-linux-gnu/libgfortran.so.4 lib/libgfortran.so.4
-        fi
-        cp -L /usr/lib/x86_64-linux-gnu/libquadmath.so.0 lib/libquadmath.so.0
-    fi
+    cp -L $(ldd lib/libmxnet.so | grep libgfortran | awk '{print $3}') lib/
 elif [[ $PLATFORM == 'darwin' ]]; then
     cp -L build/libmxnet.dylib lib/libmxnet.dylib
 fi
