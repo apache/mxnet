e4730ebe07d688347b909379595c9abfb71d037a
Bartosz Kuncer <bartosz.kuncer@intel.com>
2021-05-21 10:49:16 +0200

    Add log sigmoid to subgraph tests

--- src/operator/tensor/elemwise_unary_op_basic.cc
+++ src/operator/tensor/elemwise_unary_op_basic.cc
@@ -149,6 +149,23 @@ MXNET_OPERATOR_REGISTER_BINARY_WITH_SPARSE_CPU(_backward_sigmoid,
       return ret;
     });
 
+// log_sigmoid
+MXNET_OPERATOR_REGISTER_UNARY(log_sigmoid)
+MXNET_ADD_SPARSE_OP_ALIAS(log_sigmoid)
+.describe(R"code(Computes log_sigmoid of x element-wise.
+
+.. math::
+   y = log(1 / (1 + exp(-x)))
+
+The storage type of ``log_sigmoid`` output is always dense
+
+)code" ADD_FILELINE)
+.set_attr<FCompute>("FCompute<cpu>", UnaryOp::Compute<cpu, mshadow_op::log_sigmoid>)
+.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_log_sigmoid"});
+
+MXNET_OPERATOR_REGISTER_BINARY_WITH_SPARSE_CPU(_backward_log_sigmoid,
+                                               unary_bwd<mshadow_op::log_sigmoid_grad>);
+
 
 
 DMLC_REGISTER_PARAMETER(HardSigmoidParam);
@@ -195,23 +212,6 @@ The storage type of ``softsign`` output is always dense
 MXNET_OPERATOR_REGISTER_BINARY(_backward_softsign)
 .set_attr<FCompute>("FCompute<cpu>", ElemwiseBinaryOp::Compute<cpu,
   unary_bwd<mshadow_op::softsign_grad> >);
-
-// log_sigmoid
-MXNET_OPERATOR_REGISTER_UNARY(log_sigmoid)
-MXNET_ADD_SPARSE_OP_ALIAS(log_sigmoid)
-.describe(R"code(Computes log_sigmoid of x element-wise.
-
-.. math::
-   y = log(1 / (1 + exp(-x)))
-
-The storage type of ``log_sigmoid`` output is always dense
-
-)code" ADD_FILELINE)
-.set_attr<FCompute>("FCompute<cpu>", UnaryOp::Compute<cpu, mshadow_op::log_sigmoid>)
-.set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_log_sigmoid"});
-
-MXNET_OPERATOR_REGISTER_BINARY_WITH_SPARSE_CPU(_backward_log_sigmoid,
-                                               unary_bwd<mshadow_op::log_sigmoid_grad>);
 
 // copy
 static void CopyEx(const nnvm::NodeAttrs& attrs,
