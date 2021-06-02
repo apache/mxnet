a6fdc7ae11ab1590f2b2a9a47379e7ab41479c72
Matteo Salvarezza <matteo.salvarezza@gmail.com>
2021-06-02 17:15:10 +0200

    Fix ChooseBlas.cmake for CMake build dir name (#20072)

--- cmake/ChooseBlas.cmake
+++ cmake/ChooseBlas.cmake
@@ -80,7 +80,7 @@ set(FORTRAN_DIR \\\"\$\{CMAKE_Fortran_IMPLICIT_LINK_DIRECTORIES\}\\\")
         COMMAND ${CMAKE_COMMAND} .
       )
       set(FORTRAN_DIR "")
-      include(build/temp/FortranDir.cmake)
+      include(${CMAKE_CURRENT_BINARY_DIR}/temp/FortranDir.cmake)
       find_library(FORTRAN_LIB NAMES gfortran HINTS ${FORTRAN_DIR})
       message("FORTRAN_DIR is ${FORTRAN_DIR}")
       message("FORTRAN_LIB is ${FORTRAN_LIB}")
