1e451f0baaea9d0127f97a586a4dc57b5c10ec79
B. Gawrych <bartlomiej.gawrych@intel.com>
2021-06-09 12:47:55 +0200

    Add direct imports

--- src/c_api/c_api.cc
+++ src/c_api/c_api.cc
@@ -62,6 +62,11 @@
 #include "miniz.h"
 #include "nnvm/pass_functions.h"
 
+#if defined(__x86_64__) || defined(_M_X64)
+#include <immintrin.h>
+#include <xmmintrin.h>
+#endif
+
 using namespace mxnet;
 
 // Internal function to get the information
