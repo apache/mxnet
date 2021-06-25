94ac95afc27006b27fa2368e273d3e9ea9e05256
B. Gawrych <bartlomiej.gawrych@intel.com>
2021-06-25 10:42:53 +0200

    Disable FTZ for numpy_interoperability case

--- tests/python/unittest/test_numpy_interoperability.py
+++ tests/python/unittest/test_numpy_interoperability.py
@@ -3341,7 +3341,11 @@ def test_np_array_function_protocol():
 @with_array_ufunc_protocol
 @pytest.mark.serial
 def test_np_array_ufunc_protocol():
-    check_interoperability(_NUMPY_ARRAY_UFUNC_LIST)
+    prev_state = util.set_flush_denorms(False)
+    try:
+        check_interoperability(_NUMPY_ARRAY_UFUNC_LIST)
+    finally:
+        util.set_flush_denorms(prev_state)
 
 
 @use_np
