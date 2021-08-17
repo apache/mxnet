283b7acef62b6fbfb65c966ad16cbe7230f80677
fenkexin <fengx463@umn.edu>
2021-08-17 17:32:31 -0500

    sanity

--- python/mxnet/numpy/multiarray.py
+++ python/mxnet/numpy/multiarray.py
@@ -1409,7 +1409,7 @@ class ndarray(NDArray):  # pylint: disable=invalid-name
             1, ctypes.pointer(self.handle),
             ctypes.pointer(mx_uint(grad_req)),
             ctypes.pointer(grad.handle)))
-            
+
     def drop_grad(self):
         """Free the memory of the marked ndarray."""
         check_call(_LIB.MXAutogradDropGrads(
