ffb764f0c0ccdccbbe75e625ad197506481d2acf
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2021-08-16 18:37:57 -0700

    [License] Remove mistakenly placed ASF headers (#20520)
    https://lists.apache.org/thread.html/r9cb5611653594f7cb68ee55d8be7e5edbb0858fd287337d5fece7e04%40%3Cgeneral.incubator.apache.org%3E

--- src/operator/contrib/modulated_deformable_convolution-inl.h
+++ src/operator/contrib/modulated_deformable_convolution-inl.h
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 /*!
  * Copyright (c) 2018 Microsoft
  * Licensed under The MIT License [see LICENSE for details]
@@ -25,7 +6,8 @@
  * \ref: https://github.com/Yangqing/caffe/wiki/Convolution-in-Caffe:-a-memo
  * \ref: https://arxiv.org/abs/1811.11168
  * \author Yuwen Xiong, Haozhi Qi, Jifeng Dai, Xizhou Zhu, Han Hu
-*/
+ */
+
 #ifndef MXNET_OPERATOR_CONTRIB_MODULATED_DEFORMABLE_CONVOLUTION_INL_H_
 #define MXNET_OPERATOR_CONTRIB_MODULATED_DEFORMABLE_CONVOLUTION_INL_H_
 
