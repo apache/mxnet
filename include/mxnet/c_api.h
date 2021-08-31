0732f4bcdd002b63702ae323e327da4f383caa05
Przemek Tredak <ptredak@nvidia.com>
2021-08-31 10:45:11 -0700

    Add C API

--- include/mxnet/c_api.h
+++ include/mxnet/c_api.h
@@ -3153,6 +3153,29 @@ MXNET_DLL int MXEnginePushSyncND(EngineSyncFunc sync_func, void* func_param,
 MXNET_DLL int MXCheckDynamicShapeOp(SymbolHandle sym_handle,
                                     bool* has_dynamic_shape);
 
+/*!
+  * \brief Push a new NVTX range. Requires building with CUDA and NVTX.
+  * \param name Name of the range.
+  * \param color Color used to display the range in the visual profiling tools.
+  *              Encoded as 256*256*R + 256*G + B.
+  */
+MXNET_DLL int MXNVTXRangePush(const char * name, mx_uint color);
+
+/*!
+  * \brief End the NVTX range. Requires building with CUDA and NVTX.
+  */
+MXNET_DLL int MXNVTXRangePop();
+
+/*!
+  * \brief Start CUDA profiling session. Requires building with CUDA and NVTX.
+  */
+MXNET_DLL int MXCUDAProfilerStart();
+
+/*!
+  * \brief End CUDA profiling session. Requires building with CUDA and NVTX.
+  */
+MXNET_DLL int MXCUDAProfilerStop();
+
 #ifdef __cplusplus
 }
 #endif  // __cplusplus
