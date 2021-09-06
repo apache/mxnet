4f31840f7b7bf39a584eb74c382ae0866a56ad9b
channel960608 <997476274@qq.com>
2021-09-06 11:34:37 -0400

    FIX: sanity check

--- src/initialize.cc
+++ src/initialize.cc
@@ -375,7 +375,7 @@ std::shared_ptr<void(int)> HANDLER_NAME(                             \
   }),                                                                \
   [](auto f) { signal(SIGNAL, f); });
 
-// TODO: avoid jvm exit with code 139. By now, we just comment it
+// TODO(cspchen): avoid jvm exit with code 139. By now, we just comment it
 // SIGNAL_HANDLER(SIGSEGV, SIGSEGVHandler, true);
 SIGNAL_HANDLER(SIGFPE, SIGFPEHandler, false);
 SIGNAL_HANDLER(SIGBUS, SIGBUSHandler, false);
