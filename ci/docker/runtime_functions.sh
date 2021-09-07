24d0df709507fed9db712bd388dcb923ddd4ae92
barry-jin <barryjin1995@gmail.com>
2021-09-07 10:03:00 -0700

    update ci

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -808,14 +808,14 @@ unittest_ubuntu_python3_cpu_onednn() {
 
 unittest_array_api_standardization() {
     set -ex
-    export PYTHONPATH=./python/
+    python3 -m pip install -e /work/mxnet/python --user
     cd ..
     git clone https://github.com/data-apis/array-api-tests.git
-    cd array-api-tests
+    pushd /work/array-api-tests
     export ARRAY_API_TESTS_MODULE=mxnet.numpy pytest
     export MXNET_ENABLE_CYTHON=0
     export DMLC_LOG_STACK_TRACE_DEPTH=100
-    python3 -m pytest --durations=50 --cov-report xml:tests_unittest.xml --cov-append \
+    python3 -m pytest --durations=50 --cov-report xml:tests_api.xml --verbose \
         array_api_tests/test_type_promotion.py::test_elementwise_function_two_arg_bool_type_promotion
     popd
 }
