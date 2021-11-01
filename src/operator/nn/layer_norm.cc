a9d5062899289b33161a8615a7acfe4073fb5e17
Joe Evans <joseph.evans@gmail.com>
2021-11-01 14:09:58 -0700

    Move LayerNormCPUKernel function to own file, since it is licensed under MIT. Update LICENSE to reflect this.

--- src/operator/nn/layer_norm.cc
+++ src/operator/nn/layer_norm.cc
@@ -15,41 +15,9 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
- * Function LayerNormCPUKernel is adapated from Marian
- * https://github.com/marian-nmt/marian-dev/blob/master/src/tensors/cpu/tensor_operators.cpp
- * under the MIT license
- * MIT License
- *
- * Copyright (c) 2016 Marcin Junczys-Dowmunt, the University of Edinburgh, Adam
- * Mickiewicz University
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- * 
- * All or part of this file was contributed by Intel under license:
- *   Copyright (C) 2017-2018 Intel Corporation
- *   SPDX-License-Identifier: MIT
- *
  */
 
 /*!
- * Copyright (c) 2015 by Contributors
  * \file layer_norm.cc
  * \brief Implements Ba et. al, Layer Normalization (https://arxiv.org/abs/1607.06450).
 */
@@ -57,6 +25,7 @@
 #include "layer_norm-inl.h"
 #include <nnvm/op_attr_types.h>
 #include "../elemwise_op_common.h"
+#include "layer_norm_cpu.h"
 
 namespace mxnet {
 namespace op {
