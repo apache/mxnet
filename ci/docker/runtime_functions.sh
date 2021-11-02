9cfbdacd83ba408ab618df5c0bc36807227c0b52
barry-jin <barryjin1995@gmail.com>
2021-11-01 21:28:26 -0700

    fix onnx build

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -553,9 +553,8 @@ build_ubuntu_gpu_tensorrt() {
     rm -rf build
     mkdir -p build
     cd build
-    ln -s /usr/bin/python3 /usr/bin/python
     export DPYTHON_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-    cmake -DCMAKE_CXX_FLAGS=-I${DPYTHON_INCLUDE_DIR} -DBUILD_SHARED_LIBS=ON ..
+    cmake -DCMAKE_CXX_FLAGS=-I${DPYTHON_INCLUDE_DIR} -DBUILD_SHARED_LIBS=ON -DPYTHON_EXECUTABLE=/usr/bin/python3 ..
     make -j$(nproc)
     export LIBRARY_PATH=`pwd`:`pwd`/onnx/:$LIBRARY_PATH
     export CPLUS_INCLUDE_PATH=`pwd`:$CPLUS_INCLUDE_PATH
