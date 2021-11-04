mozga-intel/incubator-mxnet

Memory alignment

include/mxnet/base.h
@@ -542,7 +542,11 @@ inline std::ostream& operator<<(std::ostream &out, const Context &ctx) {
 
 
 #if MXNET_USE_ONEDNN == 1 || MXNET_USE_INTGEMM == 1
-constexpr size_t kMKLDNNAlign = 64;
+#ifdef __linux__
+  constexpr size_t kMKLDNNAlign = 1 << 21;
+#else
+  constexpr size_t kMKLDNNAlign = 4096ul;
+#endif
 #endif
 
 }  // namespace mxnet

apache/mxnet

include/mxnet/base.h
@@ -541,7 +541,11 @@ inline std::ostream& operator<<(std::ostream &out, const Context &ctx) {
 
 
 #if MXNET_USE_ONEDNN == 1 || MXNET_USE_INTGEMM == 1
-constexpr size_t kDNNLAlign = 64;
+#ifdef __linux__
+  constexpr size_t kDNNLAlign = 1 << 21;
+#else
+  constexpr size_t kDNNLAlign = 4096ul;
+#endif
 #endif
 
 }  // namespace mxnet