92332f4f672f56b270e2fcace49e5b0fca68b6e7
barry-jin <barryjin1995@gmail.com>
2021-11-06 12:16:34 -0700

    fix elemwise binary

--- src/operator/tensor/elemwise_binary_broadcast_op.h
+++ src/operator/tensor/elemwise_binary_broadcast_op.h
@@ -293,7 +293,7 @@ void BinaryBroadcastCompute(const nnvm::NodeAttrs& attrs,
     if (outputs[0].type_flag_ == mshadow::kBool) {
       LOG(FATAL) << "Operator " << attrs.op->name << " does not support boolean type";
     }
-    MSHADOW_TYPE_SWITCH(outputs[0].type_flag_, DType, {
+    MSHADOW_TYPE_SWITCH_EXT(outputs[0].type_flag_, DType, {
       BROADCAST_NDIM_SWITCH(ndim, NDim, {
         broadcast::BinaryBroadcastComputeImpl<NDim, DType, OP>(s,
                                                                req[0],
