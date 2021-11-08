0c54f6b0d06f10ce53538f3d0729419e3c9c2b17
Rafal Litka <rafal.litka@intel.com>
2021-11-08 13:28:29 +0100

    switch include order

--- src/operator/nn/dnnl/dnnl_transpose.cc
+++ src/operator/nn/dnnl/dnnl_transpose.cc
@@ -25,9 +25,9 @@
 
 #if MXNET_USE_ONEDNN == 1
 
-#include "./dnnl_transpose-inl.h"
-
 #include "../../tensor/matrix_op-inl.h"
+
+#include "./dnnl_transpose-inl.h"
 
 namespace mxnet {
 namespace op {
