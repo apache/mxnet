4a19f7f5294c381abeb8da097245ddf53e16fdfb
mozga <mateusz.ozga@intel.com>
2021-11-21 04:05:47 +0100

    [master][ci][feature] Static code checker for CMake files (#20706)
    * CmakeLint initial commit
    
    * Skip upstream, and module dir
    
    * Rollback

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -703,9 +703,16 @@ sanity_check() {
     set -ex
     sanity_clang
     sanity_license
+    sanity_cmakelint
     sanity_tutorial
     sanity_python_prospector
     sanity_cpp
+}
+
+sanity_cmakelint() {
+    set -exu
+    
+    git ls-files -z -- bootstrap '*.cmake' '*.cmake.in' '*CMakeLists.txt' | grep -E -z -v '^(3rdparty)|cmake/Modules/|cmake/upstream/' | xargs -0 cmakelint --config=.cmakelintrc --quiet
 }
 
 sanity_tutorial() {
