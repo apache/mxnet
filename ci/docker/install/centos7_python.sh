f7a7be2c3c4e24bbc3a7ca27f90d8a0e25f4d161
Joe Evans <joseph.evans@gmail.com>
2022-02-03 12:25:31 -0800

    [v1.9.x] Fix pip installation in containers (#20864)
    * Fix pip install inside docker.
    
    * Update ci/docker/install/centos7_python.sh
    
    Co-authored-by: bgawrych <bartlomiej.gawrych@intel.com>
    
    * Update ci/docker/install/ubuntu_python.sh
    
    Co-authored-by: bgawrych <bartlomiej.gawrych@intel.com>
    
    Co-authored-by: bgawrych <bartlomiej.gawrych@intel.com>

--- ci/docker/install/centos7_python.sh
+++ ci/docker/install/centos7_python.sh
@@ -27,7 +27,7 @@ yum -y install https://repo.ius.io/ius-release-el7.rpm
 yum -y install python36u
 
 # Install PIP
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+curl "https://bootstrap.pypa.io/pip/3.6/get-pip.py" -o "get-pip.py"
 python3.6 get-pip.py
 # Restrict numpy version to < 1.19.0 due to https://github.com/apache/incubator-mxnet/issues/18600
-pip3 install nose pylint 'numpy>1.16.0,<1.19.0' nose-timer requests 'h5py<3' scipy==1.2.3
+python3.6 -m pip install nose pylint 'numpy>1.16.0,<1.19.0' nose-timer requests 'h5py<3' scipy==1.2.3
