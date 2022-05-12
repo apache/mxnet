6f350077c54e5120dd9cdfcd287f507b91f84d35
Wei Chu <weichu@amazon.com>
2022-05-12 11:07:28 -0700

    update nccl version for cu110 build

--- tools/setup_gpu_build_tools.sh
+++ tools/setup_gpu_build_tools.sh
@@ -47,7 +47,7 @@ elif [[ $VARIANT == cu110* ]]; then
     CUDA_NVTX_VERSION='11.0.167-1'
     LIBCUDA_VERSION='450.36.06-0ubuntu1'
     LIBCUDNN_VERSION='8.0.5.39-1+cuda11.0'
-    LIBNCCL_VERSION='2.7.8-1+cuda11.0'
+    LIBNCCL_VERSION='2.12.10-1+cuda11.0'
 elif [[ $VARIANT == cu102* ]]; then
     CUDA_VERSION='10.2.89-1'
     CUDA_PATCH_VERSION='10.2.2.89-1'
