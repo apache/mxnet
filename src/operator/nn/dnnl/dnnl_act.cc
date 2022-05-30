ab30c56bce902988a58c1578298060835ca5c1f9
PiotrWolinski - Intel <piotr.wolinski@intel.com>
2022-05-30 13:36:45 +0200

    [master] Fix issue with fc_eltwise fusing (#20958)
    * Fixed issue with fc_eltwise
    
    * Disabled exp quantization and improved functions readability
    
    * Update src/operator/nn/dnnl/dnnl_act.cc
    
    Suggested change
    
    Co-authored-by: Andrzej Kotłowski <Andrzej.Kotlowski@intel.com>
    
    * Fixed linting
    
    * Enabled exp for test_fc_eltwise
    
    Co-authored-by: Andrzej Kotłowski <Andrzej.Kotlowski@intel.com>

--- src/operator/nn/dnnl/dnnl_act.cc
+++ src/operator/nn/dnnl/dnnl_act.cc
@@ -70,9 +70,9 @@ bool SupportDNNLLeakyRelu(const LeakyReLUParam& param, const NDArray& input) {
 }
 
 bool SupportQuantizedDNNLAct(const ActivationParam& param) {
-  // TODO(zhennan): Add more activation type when dnnl supports.
-  //                Remove this when it's identity to SupportDNNLAct.
-  return param.act_type == activation::kReLU;
+  // Although it is the same as SupportDNNLAct i left it here, so when new activations
+  // will be introduced it will be easier to handle.
+  return SupportDNNLAct(param);
 }
 
 dnnl::algorithm GetDNNLActAlgo(const ActivationParam& param) {
