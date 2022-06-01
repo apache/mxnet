9fe13b555ce7ccba686caab751b782f9e7606aed
waytrue17 <52505574+waytrue17@users.noreply.github.com>
2022-05-31 19:55:14 -0700

    [v1.9.x] Fix CD pipeline for python docker (#21049)
    * pass arg to dockerfile
    
    * fix LD path for cuda files
    
    * update gpu base image in ci
    
    Co-authored-by: Paweł Głomski <pawel.glomski@intel.com>
    Co-authored-by: Wei Chu <weichu@amazon.com>

--- cd/python/docker/python_images.sh
+++ cd/python/docker/python_images.sh
@@ -44,8 +44,15 @@ if [ ! -z "${RELEASE_PUBLIC_ECR_REPOSITORY}" ]; then
 fi
 
 build() {
+    # use this flag to set LD_LIBRARY_PATH for cuda files
+    if [[ $mxnet_variant = cu* ]]; then
+        cuda_variant=${mxnet_variant:2:-1}.${mxnet_variant: -1}
+    else
+        cuda_variant="false"
+    fi
+
     # NOTE: Ensure the correct context root is passed in when building - Dockerfile expects ./wheel_build
-    docker build -t "${image_name}" --build-arg BASE_IMAGE="${base_image}" --build-arg MXNET_COMMIT_ID=${GIT_COMMIT} --build-arg MXNET_VARIANT=${mxnet_variant} -f ${resources_path}/Dockerfile ./wheel_build
+    docker build -t "${image_name}" --build-arg BASE_IMAGE="${base_image}" --build-arg MXNET_COMMIT_ID=${GIT_COMMIT} --build-arg MXNET_VARIANT=${mxnet_variant} --build-arg CUDA_VARIANT=${cuda_variant} -f ${resources_path}/Dockerfile ./wheel_build
 }
 
 test() {
