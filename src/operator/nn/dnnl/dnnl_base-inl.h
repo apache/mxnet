d1e1028b8725f4a380b786adcb847390e9273d55
Adam Grabowski <adam.grabowski@intel.com>
2022-07-20 12:48:45 +0000

    Fix SupportDNNL for multiple inputs

--- src/operator/nn/dnnl/dnnl_base-inl.h
+++ src/operator/nn/dnnl/dnnl_base-inl.h
@@ -184,10 +184,10 @@ enum DNNLTensorsDtypes { AllSame = 0, Mixed = 1 };
 
 template <int MinNdim, int MaxNdim, DNNLTypeMode TypeMode, DNNLTensorsDtypes MixedTensors>
 static inline bool SupportDNNL(const std::vector<NDArray>& inputs) {
-  int dtype = MixedTensors ? -1 : inputs[0].dtype();
-  if (!SupportDNNLType<TypeMode>(dtype)) {
+  if (!SupportDNNLType<TypeMode>(inputs[0].dtype())) {
     return false;
   }
+  int dtype = MixedTensors ? -1 : inputs[0].dtype();
   for (NDArray input : inputs) {
     if (dtype == -1) {
       if (!SupportDNNL<MinNdim, MaxNdim, TypeMode>(input))
