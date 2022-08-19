cb5b9d4108f787d96a57816f54cbd10feba34f4f
Joe Evans <joseph.evans@gmail.com>
2022-08-19 11:26:02 -0700

    Revert "Set CUB_IGNORE_DEPRECATED_CPP_DIALECT to prevent build failures with gcc-4.8 + Cuda 11.7."
    This reverts commit e6496607c0673965c2153ae7167e1785eac26bfb.

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -750,7 +750,6 @@ build_ubuntu_gpu_mkldnn() {
         CUDA_ARCH="$CI_CUDA_COMPUTE_CAPABILITIES" \
         USE_SIGNAL_HANDLER=1                      \
         USE_LAPACK_PATH=/usr/lib/x86_64-linux-gnu \
-        CUB_IGNORE_DEPRECATED_CPP_DIALECT=1       \
         -j$(nproc)
 }
 
@@ -769,7 +768,6 @@ build_ubuntu_gpu_mkldnn_nocudnn() {
         CUDA_ARCH="$CI_CUDA_COMPUTE_CAPABILITIES" \
         USE_SIGNAL_HANDLER=1                      \
         USE_LAPACK_PATH=/usr/lib/x86_64-linux-gnu \
-        CUB_IGNORE_DEPRECATED_CPP_DIALECT=1       \
         -j$(nproc)
 }
 
