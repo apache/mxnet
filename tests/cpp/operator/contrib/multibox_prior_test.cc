4973889525485af515d3b4cc877a34f4dde9d51e
Julian Gamble <mail@juliangamble.com>
2023-08-05 14:19:36 +1000

    [Feedback] Add Apache header

--- tests/cpp/operator/contrib/multibox_prior_test.cc
+++ tests/cpp/operator/contrib/multibox_prior_test.cc
@@ -1,5 +1,24 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 //
-// The goal of this class is to be the same as this
+// The goal of this file multibox_prior_test.cc is to be the same as this test class MultiboxPriorTest.java
 // https://github.com/deepjavalibrary/djl/blob/master/integration/src/main/java/ai/djl/integration/tests/modality/cv/MultiBoxPriorTest.java
 //
 
